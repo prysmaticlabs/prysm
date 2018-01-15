d3b4c7777503ddcd769a3e2e9979a0fc78fe5f54
Raul Jordan <raulitojordan@gmail.com>
2018-01-15 16:53:42 -0600

    incl blocks, transactions, and vm protocol interface overrides

---
+++ sharding/transactions.go
@@ -0,0 +1,6 @@
+// 2018 Prysmatic Labs
+// This file is part of the prysmaticlabs/geth-sharding library.
+//
+// This file overrides the default protocol transaction interface
+
+package sharding
---
+++ sharding/vm.go
@@ -0,0 +1,6 @@
+// 2018 Prysmatic Labs
+// This file is part of the prysmaticlabs/geth-sharding library.
+//
+// This file overrides the default protocol VM interface
+
+package sharding
