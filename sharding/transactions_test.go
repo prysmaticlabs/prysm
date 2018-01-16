b336b6adfa628494784dcdb2a33ec5675f24f8cc
Raul Jordan <raulitojordan@gmail.com>
2018-01-16 10:41:00 -0600

    sharding tx base types override
    Former-commit-id: b000e066b08f84dd40c251b30f36af125e2beaa8 [formerly 918447dfb6071e7346a737de737b510fad9607df]
    Former-commit-id: 821ec50c9b6149362ef3f7d3994cd23b99f0ded1

---
+++ sharding/transactions_test.go
@@ -0,0 +1,15 @@
+package sharding
+
+import (
+	//"flag"
+	//"fmt"
+	//"math/rand"
+	//"os"
+	"testing"
+)
+
+func TestCreation(t *testing.T) {
+	if 1 != 2 {
+		t.Fatalf("Values are not equal")
+	}
+}
