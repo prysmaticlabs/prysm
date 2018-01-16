04271a89fe84fa7d90fe81a506f7e520e8e9d039
Raul Jordan <raulitojordan@gmail.com>
2018-01-16 11:56:21 -0600

    sharding transaction tests
    Former-commit-id: 05978a0028b27bc99cdbd2cdf8f66c78a48444be [formerly c05cd8f789a0f2a3b24b4a41eda2d6d89c33e41b]
    Former-commit-id: 04955bc0a35f4932b5ff28c98393ff79d0356d4a

--- sharding/transactions_test.go
+++ sharding/transactions_test.go
@@ -5,11 +5,33 @@ import (
 	//"fmt"
 	//"math/rand"
 	//"os"
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
 	"testing"
 )
 
+var (
+	txSimple = NewShardingTransaction(
+		0,
+		common.HexToAddress("095e7baea6a6c7c4c2dfeb977efac326af552d87"),
+		// amount
+		big.NewInt(10),
+		// gasLimit
+		1000000,
+		// gasPrice
+		big.NewInt(1),
+		// data
+		common.FromHex("hello world this is the data"),
+		// access list
+		[]common.Address{common.HexToAddress("032e7baea6a6c7c4c2dfe98392932326af552d87"), common.HexToAddress("083e7baea6a6c7c4c2dfeb97710293843f552d87")},
+	)
+)
+
 func TestCreation(t *testing.T) {
-	if 1 != 2 {
-		t.Fatalf("Values are not equal")
+	if txSimple.ChainID().Cmp(big.NewInt(1)) != 0 {
+		t.Fatalf("ChainID invalid")
+	}
+	if txSimple.ShardID().Cmp(big.NewInt(1)) != 0 {
+		t.Fatalf("ShardID invalid")
 	}
 }
