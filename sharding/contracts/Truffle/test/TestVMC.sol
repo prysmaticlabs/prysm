03690f918e6cb079365842b71f651a747998ace3
nisdas <nish1993@hotmail.com>
2018-02-07 18:39:40 +0800

    Added Unit Tests

--- sharding/contracts/Truffle/test/TestVMC.sol
+++ sharding/contracts/Truffle/test/TestVMC.sol
@@ -5,5 +5,12 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/VMC.sol";
 
 contract TestVMC { 
+    function testGasCollationLimit() public {
+
+        VMC instance = VMC(DeployedAddresses.VMC());
+        uint expected = 10000000;
+        Assert.equal(instance.getCollationGasLimit(), expected, "Collation Gas Limit Should be 10000000");
+
+    }
 
 }
--- sharding/contracts/Unit Tests/migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-var vmc = artifacts.require("./validator_manager.sol")
-
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
