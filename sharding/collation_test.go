e65be3456d14d7ad685eb6f0be6e40ac44d35cbe
Preston Van Loon <preston@machinepowered.com>
2018-02-21 22:39:51 -0500

    Add getter/setter methods for Collation
    Former-commit-id: 7d76aa16fac553b57023f91e808653b202308ad7 [formerly 46a739d7c1caff57beb6abb6285b8ad70db990c6]
    Former-commit-id: 1c428340e16ce4b8dad92d89a0f2a6e69872ca0a

--- sharding/collation_test.go
+++ sharding/collation_test.go
@@ -9,6 +9,39 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
+func TestCollation_AddTransactions(t *testing.T) {
+	tests := []struct {
+		transactions []*types.Transaction
+	}{
+		{
+			transactions: []*types.Transaction{
+				makeTxWithGasLimit(0),
+				makeTxWithGasLimit(1),
+				makeTxWithGasLimit(2),
+				makeTxWithGasLimit(3),
+			},
+		}, {
+			transactions: []*types.Transaction{},
+		},
+	}
+
+	for _, tt := range tests {
+		c := &Collation{}
+		for _, tx := range tt.transactions {
+			c.AddTransaction(tx)
+		}
+		results := c.Transactions()
+		if len(results) != len(tt.transactions) {
+			t.Fatalf("Wrong number of transactions. want=%d. got=%d", len(tt.transactions), len(results))
+		}
+		for i, tx := range tt.transactions {
+			if results[i] != tx {
+				t.Fatalf("Mismatched transactions. wanted=%+v. got=%+v", tt.transactions, results)
+			}
+		}
+	}
+}
+
 func TestCollation_GasUsed(t *testing.T) {
 	tests := []struct {
 		transactions []*types.Transaction
