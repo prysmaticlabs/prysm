04dd3e175ed6d59fc7cb1c7e1047989e03f1676a
Zypherx <raul@zk.capital>
2018-02-22 18:23:39 -0600

    process pending tx's requires implementation in upstream

--- sharding/transactions.go
+++ sharding/transactions.go
@@ -8,6 +8,7 @@ import (
 	ethereum "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/sharding/contracts"
@@ -24,5 +25,12 @@ type collatorClient interface {
 // that ask for too much gas adds a the tx into the collation and
 // throws on failure
 func processPendingTransactions(c collatorClient) error {
-
+	txpool := NewTxPool(config TxPoolConfig, chainconfig *params.ChainConfig, chain blockChain)
+	pendingTxs, err := txpool.Pending()
+	if err != nil {
+		return fmt.Errorf("Could not fetch pending tx's: %v", err)
+	}
+	for k, v := range pendingTxs {
+		...
+	}
 }
