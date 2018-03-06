cda91606411d36daaec7570c074fc7523395a4ca
Terence Tsao <tsao.terence@gmail.com>
2018-03-05 21:57:42 -0800

    add cli entrypoint for sharding-collator

--- cmd/geth/shardingcmd.go
+++ cmd/geth/shardingcmd.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/ethereum/go-ethereum/sharding"
 
+	"fmt"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	cli "gopkg.in/urfave/cli.v1"
 )
 
 var (
-	shardingClientCommand = cli.Command{
-		Action:    utils.MigrateFlags(shardingClient),
+	validatorClientCommand = cli.Command{
+		Action:    utils.MigrateFlags(validatorClient),
 		Name:      "sharding-validator",
 		Aliases:   []string{"shard-validator"},
 		Usage:     "Start a sharding validator client",
@@ -17,16 +18,33 @@ var (
 		Flags:     []cli.Flag{utils.DataDirFlag, utils.PasswordFileFlag, utils.NetworkIdFlag, utils.IPCPathFlag, utils.DepositFlag},
 		Category:  "SHARDING COMMANDS",
 		Description: `
-Launches a sharding client that connects to a running geth node and proposes collations to a Validator Manager Contract. This feature is a work in progress.
+Launches a sharding validator client that connects to a running geth node and proposes collations to a Validator Manager Contract. This feature is a work in progress.
+`,
+	}
+	collatorClientCommand = cli.Command{
+		Action:    utils.MigrateFlags(collatorClient),
+		Name:      "sharding-collator",
+		Aliases:   []string{"shard-collator"},
+		Usage:     "Start a sharding collator client",
+		ArgsUsage: "[endpoint]",
+		Flags:     []cli.Flag{utils.DataDirFlag, utils.PasswordFileFlag, utils.NetworkIdFlag, utils.IPCPathFlag, utils.CollateFlag},
+		Category:  "SHARDING COMMANDS",
+		Description: `
+Launches a sharding collator client that connects to a running geth node and proposes collations to validator node. This feature is a work in progress.
 `,
 	}
 )
 
-func shardingClient(ctx *cli.Context) error {
+func validatorClient(ctx *cli.Context) error {
 	c := sharding.MakeShardingClient(ctx)
 	if err := c.Start(); err != nil {
 		return err
 	}
 	c.Wait()
+	return nil
+}
+
+func collatorClient(ctx *cli.Context) error {
+	fmt.Println("Starting collator client")
 	return nil
 }
