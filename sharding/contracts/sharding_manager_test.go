c2f9ccb2e7a54e40755324661355d52b7a168fe6
Terence Tsao <tsao.terence@gmail.com>
2018-04-02 07:03:06 -0700

    sharding: align config with phase 1 spec

--- sharding/contracts/sharding_manager_test.go
+++ sharding/contracts/sharding_manager_test.go
@@ -15,7 +15,7 @@ import (
 var (
 	key, _                   = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
 	addr                     = crypto.PubkeyToAddress(key.PublicKey)
-	accountBalance1001Eth, _ = new(big.Int).SetString("1000000000000000000001", 10)
+	accountBalance1001Eth, _ = new(big.Int).SetString("1001000000000000000000", 10)
 	collatorDeposit, _       = new(big.Int).SetString("1000000000000000000000", 10)
 )
 
