19665aca8bdb545bd3c4c339792a93be417c4a0e
Terence Tsao <tsao.terence@gmail.com>
2018-04-11 07:45:24 -0700

    sharding: align collation header to minimal sharding protocol
    Former-commit-id: 1c630baa8d0cb0602dbbd4073998d78e2be6502f [formerly 236e508800ed9dbcacdf6fdf3375e538294b356e]
    Former-commit-id: 00f8ddf9246d3378279325e53824c1f451a2c11f

--- sharding/collation.go
+++ sharding/collation.go
@@ -15,22 +15,16 @@ type Collation struct {
 
 type CollationHeader struct {
 	shardID           *big.Int        //the shard ID of the shard
-	parentHash        *common.Hash    //the hash of the parent collation
 	chunkRoot         *common.Hash    //the root of the chunk tree which identifies collation body
 	period            *big.Int        //the period number in which collation to be included
 	proposerAddress   *common.Address //address of the collation proposer
-	proposerBid       *big.Int        //the reward from proposer to collator for a winning proposal
-	proposerSignature []byte          //the proposer's signature as part of a proposal
 }
 
 func (c *Collation) Header() *CollationHeader           { return c.header }
 func (c *Collation) Transactions() []*types.Transaction { return c.transactions }
 func (c *Collation) ShardID() *big.Int                  { return c.header.shardID }
-func (c *Collation) ParentHash() *common.Hash           { return c.header.parentHash }
 func (c *Collation) Period() *big.Int                   { return c.header.period }
 func (c *Collation) ProposerAddress() *common.Address   { return c.header.proposerAddress }
-func (c *Collation) ProposerBid() *big.Int              { return c.header.proposerBid }
-func (c *Collation) ProposerSignature() []byte          { return c.header.proposerSignature }
 
 func (c *Collation) SetHeader(h *CollationHeader) { c.header = h }
 
