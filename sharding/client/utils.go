92c44d455f6a57d450dae9b39ab362279b8f9e2c
nisdas <nish1993@hotmail.com>
2018-05-02 00:36:16 +0800

    sharding/client: Change to serialize blobs(#92)
    Former-commit-id: c2dea5d8ba8c469a304a1855e1f262892b3ea0a9 [formerly 4d77a3e8815c3cd4803f4f785131c9aac86a7795]
    Former-commit-id: 8b5d6485900122b46d88418a7c7f8ddabae6dd09

--- sharding/client/utils.go
+++ sharding/client/utils.go
@@ -2,18 +2,25 @@ package client
 
 import (
 	"fmt"
+	"math"
 )
 
-type collationbody []byte
-
 var (
-	collationsizelimit = int64(2 ^ 20)
+	collationsizelimit = int64(math.Pow(float64(2), float64(20)))
 	chunkSize          = int64(32)
 	indicatorSize      = int64(1)
 	numberOfChunks     = collationsizelimit / chunkSize
 	chunkDataSize      = chunkSize - indicatorSize
 	totalDatasize      = numberOfChunks * chunkDataSize
 )
+
+type collationbody []byte
+
+type body interface {
+	length() int64
+	validateblob() error
+	ParseBlob()
+}
 
 func (cb collationbody) length() int64 {
 
@@ -39,13 +46,36 @@ func (cb collationbody) validateBody() error {
 
 // Parse Collation body and modify it accordingly
 
-func (cb collationbody) ParseBlob() {
+func (cb collationbody) serializeBlob() []byte {
 
 	terminalLength := cb.length() % chunkDataSize
 	chunksNumber := cb.length() / chunkDataSize
 	indicatorByte := make([]byte, 1)
 	indicatorByte[0] = 0
-	var tempbody collationbody
+	var tempbody []byte
+
+	// if blob is less than 31 bytes, it adds the indicator chunk and pads the remaining empty bytes to the right
+
+	if chunksNumber == 0 {
+		paddedbytes := make([]byte, cb.length()-terminalLength)
+		indicatorByte[0] = byte(terminalLength)
+		tempbody = append(indicatorByte, append(cb, paddedbytes...)...)
+		return tempbody
+	}
+
+	//if there is no need to pad empty bytes, then the indicator byte is added as 00011111
+
+	if terminalLength == 0 {
+
+		for i := int64(1); i < chunksNumber; i++ {
+			tempbody = append(tempbody, append(indicatorByte, cb[(i-1)*chunkDataSize:i*chunkDataSize]...)...)
+
+		}
+		indicatorByte[0] = byte(chunkDataSize)
+		tempbody = append(tempbody, append(indicatorByte, cb[(chunksNumber-1)*chunkDataSize:chunksNumber*chunkDataSize]...)...)
+		return tempbody
+
+	}
 
 	// Appends empty indicator bytes to non terminal-chunks
 	for i := int64(1); i <= chunksNumber; i++ {
@@ -53,17 +83,11 @@ func (cb collationbody) ParseBlob() {
 
 	}
 	// Appends indicator bytes to terminal-chunks , and if the index of the chunk delimiter is non-zero adds it to the chunk
-	if terminalLength != 0 {
-		indicatorByte[0] = byte(terminalLength)
-		tempbody = append(tempbody, append(indicatorByte, cb[chunksNumber*chunkDataSize:(chunksNumber*chunkDataSize)+(terminalLength+1)]...)...)
+	indicatorByte[0] = byte(terminalLength)
+	tempbody = append(tempbody, append(indicatorByte, cb[chunksNumber*chunkDataSize:(chunksNumber*chunkDataSize)+(terminalLength+1)]...)...)
+	emptyBytes := make([]byte, (chunkDataSize - cb.length()))
+	cb = append(cb, emptyBytes...)
 
-	}
-	cb = tempbody
+	return tempbody
 
-	// Pad the collation body with empty bytes until it is equal to 1 Mib
-	if cb.length() < collationsizelimit {
-		emptyBytes := make([]byte, (collationsizelimit - cb.length()))
-		cb = append(cb, emptyBytes...)
-
-	}
 }
