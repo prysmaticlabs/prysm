1ab7b5fd51c610753e79521f6d343c1013509f82
Raul Jordan <rauljordan@college.harvard.edu>
2018-05-04 14:18:48 -0500

    sharding: fix shard methods and lookup keys

--- sharding/db.go
+++ sharding/db.go
@@ -10,12 +10,24 @@ type shardKV struct {
 	kv map[common.Hash][]byte
 }
 
+func makeShardKV() *shardKV {
+	return &shardKV{kv: make(map[common.Hash][]byte)}
+}
+
 func (sb *shardKV) Get(k common.Hash) ([]byte, error) {
 	v := sb.kv[k]
 	if v == nil {
 		return nil, fmt.Errorf("Key Not Found")
 	}
 	return v, nil
+}
+
+func (sb *shardKV) Has(k common.Hash) bool {
+	v := sb.kv[k]
+	if v == nil {
+		return false
+	}
+	return true
 }
 
 func (sb *shardKV) Put(k common.Hash, v []byte) {
