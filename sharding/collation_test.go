fafc95ac49569387ea2837f85361ccd6155670c3
nisdas <nish1993@hotmail.com>
2018-05-16 00:38:24 +0800

    sharding: Adding roundtrip tests for tx serialization(#92)

--- sharding/collation_test.go
+++ sharding/collation_test.go
@@ -4,12 +4,22 @@ import (
 	"math/big"
 	//"github.com/ethereum/go-ethereum/rlp"
 	//"reflect"
+	"reflect"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
+// fieldAccess is to access unexported fields in structs in another package
+func fieldAccess(i interface{}, fields []string) reflect.Value {
+	val := reflect.ValueOf(i)
+	for i := 0; i < len(fields); i++ {
+		val = reflect.Indirect(val).FieldByName(fields[i])
+	}
+	return val
+
+}
 func TestCollation_Transactions(t *testing.T) {
 	header := NewCollationHeader(big.NewInt(1), nil, big.NewInt(1), nil, []byte{})
 	body := []byte{}
@@ -31,20 +41,19 @@ func TestCollation_Transactions(t *testing.T) {
 
 //TODO: Add test for converting *types.Transaction into raw blobs
 
-//Tests thta Transactions can be serialised
-func TestSerialize(t *testing.T) {
+//Tests that Transactions can be serialised
+func TestSerialize_Deserialize(t *testing.T) {
+
 	tests := []struct {
 		transactions []*types.Transaction
 	}{
 		{
 			transactions: []*types.Transaction{
 				makeTxWithGasLimit(0),
-				makeTxWithGasLimit(1),
-				makeTxWithGasLimit(2),
-				makeTxWithGasLimit(3),
+				makeTxWithGasLimit(5),
+				makeTxWithGasLimit(20),
+				makeTxWithGasLimit(100),
 			},
-		}, {
-			transactions: []*types.Transaction{},
 		},
 	}
 
@@ -54,30 +63,76 @@ func TestSerialize(t *testing.T) {
 			c.AddTransaction(tx)
 		}
 
-		/*var tests *types.Transaction
-		yadd := reflect.ValueOf(*c.transactions[3])
-		d := yadd.FieldByName("data").FieldByName("Hash")
-
-		test, err := rlp.EncodeToBytes(c.transactions[3])
-		if err != nil {
-			t.Fatalf("%v\n %v\n %v", err, test, *(c.transactions[0]))
-		}
-		erx := rlp.DecodeBytes(test, &tests)
-
-		dd := reflect.ValueOf(*tests)
-		cv := dd.FieldByName("data").FieldByName("Hash")
-
-		if cv != d {
-			t.Fatalf("%v\n %v\n %v", erx, cv, d)
-		} */
+		tx := c.transactions
 
 		results, err := c.Serialize()
 
-		t.Errorf("%v\n %v\n %v", err, results, c.transactions)
+		if err != nil {
+			t.Errorf("Unable to Serialize transactions, %v", err)
+		}
 
 		err = c.Deserialize(results)
 
-		t.Errorf("%v\n %v\n %v", err, results, c.transactions)
+		if err != nil {
+			t.Errorf("Unable to deserialize collation body, %v", err)
+		}
+
+		if len(tx) != len(c.transactions) {
+			t.Errorf("Transaction length is different before and after serialization: %v, %v", len(tx), len(c.transactions))
+		}
+
+		for i := 0; i < len(tx); i++ {
+
+			aval := fieldAccess(tx[i], []string{"data", "AccountNonce"})
+			aval2 := fieldAccess(c.transactions[i], []string{"data", "AccountNonce"})
+
+			if aval != aval2 {
+
+				t.Errorf("Data before serialization and after deserialization are not the same: %v, %v", aval, aval2)
+
+			}
+
+			gval := fieldAccess(tx[i], []string{"data", "GasLimit"})
+			gval2 := fieldAccess(c.transactions[i], []string{"data", "GasLimit"})
+			if gval != gval2 {
+
+				t.Errorf("Data before serialization and after deserialization are not the same: %v, %v", gval, gval2)
+
+			}
+
+			pval := fieldAccess(tx[i], []string{"data", "Price"})
+			pval2 := fieldAccess(c.transactions[i], []string{"data", "Price"})
+			if pval != pval2 {
+
+				t.Errorf("Data before serialization and after deserialization are not the same: %v, %v", pval, pval2)
+
+			}
+
+			rval := fieldAccess(tx[i], []string{"data", "Recipient"})
+			rval2 := fieldAccess(c.transactions[i], []string{"data", "Recipient"})
+			if rval != rval2 {
+
+				t.Errorf("Data before serialization and after deserialization are not the same: %v, %v", rval, rval2)
+
+			}
+
+			amval := fieldAccess(tx[i], []string{"data", "Amount"})
+			amval2 := fieldAccess(c.transactions[i], []string{"data", "Amount"})
+			if amval != amval2 {
+
+				t.Errorf("Data before serialization and after deserialization are not the same: %v, %v", amval, amval2)
+
+			}
+
+			paval := fieldAccess(tx[i], []string{"data", "Payload"})
+			paval2 := fieldAccess(c.transactions[i], []string{"data", "Payload"})
+			if paval != paval2 {
+
+				t.Errorf("Data before serialization and after deserialization are not the same: %v, %v", paval, paval2)
+
+			}
+
+		}
 
 	}
 
