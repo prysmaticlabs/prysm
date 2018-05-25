7377447126298c70ba93320f501fd0e0829315a9
Raul Jordan <rauljordan@college.harvard.edu>
2018-05-24 18:03:24 -0600

    sharding: generalized shardbackend, refactor database package and tests

--- sharding/database/database.go
+++ sharding/database/database.go
@@ -3,22 +3,21 @@ package database
 import (
 	"path/filepath"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethdb"
 )
 
 // ShardBackend defines an interface for a shardDB's necessary method
 // signatures.
 type ShardBackend interface {
-	Get(k common.Hash) (*[]byte, error)
-	Has(k common.Hash) bool
-	Put(k common.Hash, val []byte) error
-	Delete(k common.Hash) error
+	Get(k []byte) ([]byte, error)
+	Has(k []byte) (bool, error)
+	Put(k []byte, val []byte) error
+	Delete(k []byte) error
 }
 
 // NewShardDB initializes a shardDB that writes to local disk.
 // TODO: make it return ShardBackend but modify interface methods.
-func NewShardDB(dataDir string, name string) (*ethdb.LDBDatabase, error) {
+func NewShardDB(dataDir string, name string) (ShardBackend, error) {
 	// Uses default cache and handles values.
 	// TODO: allow these to be set based on cli context.
 	// TODO: fix interface - lots of methods do not match.
