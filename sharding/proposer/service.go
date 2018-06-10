b6e79787a4860a8c55bbf0755217d8dc510411e8
Terence Tsao <terence@prysmaticlabs.com>
2018-06-10 09:13:57 -0700

    sharding/proposer: resolve merge conflicts

--- sharding/proposer/service.go
+++ sharding/proposer/service.go
@@ -19,10 +19,7 @@ import (
 // in a sharded system. Must satisfy the Service interface defined in
 // sharding/service.go.
 type Proposer struct {
-	client   *mainchain.SMCClient
-	shardp2p sharding.ShardP2P
-	txpool   sharding.TXPool
-	client       mainchain.Client
+	client       *mainchain.SMCClient
 	shardp2p     sharding.ShardP2P
 	txpool       sharding.TXPool
 	shardChainDb ethdb.Database
@@ -31,7 +28,7 @@ type Proposer struct {
 // NewProposer creates a struct instance of a proposer service.
 // It will have access to a mainchain client, a shardp2p network,
 // and a shard transaction pool.
-func NewProposer(client mainchain.Client, shardp2p sharding.ShardP2P, txpool sharding.TXPool, shardChainDb ethdb.Database) (*Proposer, error) {
+func NewProposer(client *mainchain.SMCClient, shardp2p sharding.ShardP2P, txpool sharding.TXPool, shardChainDb ethdb.Database) (*Proposer, error) {
 	// Initializes a  directory persistent db.
 	return &Proposer{client, shardp2p, txpool, shardChainDb}, nil
 }
