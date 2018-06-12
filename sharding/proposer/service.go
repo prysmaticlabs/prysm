e95eededd1018d9406ebcc90ac0e9bd62309f178
Raul Jordan <rauljordan@college.harvard.edu>
2018-06-11 23:54:40 -0500

    sharding: basic sync example

--- sharding/proposer/service.go
+++ sharding/proposer/service.go
@@ -5,6 +5,7 @@ package proposer
 import (
 	"context"
 	"crypto/rand"
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -13,6 +14,7 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/sharding"
 	"github.com/ethereum/go-ethereum/sharding/mainchain"
+	"github.com/ethereum/go-ethereum/sharding/p2p"
 )
 
 // Proposer holds functionality required to run a collation proposer
@@ -29,13 +31,43 @@ type Proposer struct {
 // It will have access to a mainchain client, a shardp2p network,
 // and a shard transaction pool.
 func NewProposer(client *mainchain.SMCClient, shardp2p sharding.ShardP2P, txpool sharding.TXPool, shardChainDb ethdb.Database) (*Proposer, error) {
-	// Initializes a  directory persistent db.
 	return &Proposer{client, shardp2p, txpool, shardChainDb}, nil
 }
 
 // Start the main loop for proposing collations.
 func (p *Proposer) Start() error {
 	log.Info("Starting proposer service")
+	go p.proposeCollations()
+	go p.handleCollationBodyRequests()
+	return nil
+}
+
+// Stop the main loop for proposing collations.
+func (p *Proposer) Stop() error {
+	log.Info("Stopping proposer service")
+	return nil
+}
+
+// handleCollationBodyRequests subscribes to messages from the shardp2p
+// network and responds to a specific peer that requested the body using
+// the feed exposed by the p2p server's API.
+func (p *Proposer) handleCollationBodyRequests() {
+	feed := p.shardp2p.Feed(sharding.CollationBodyRequest{})
+	ch := make(chan p2p.Message, 100)
+	sub := feed.Subscribe(ch)
+	// TODO: close chan and unsubscribe in Stop()
+	for {
+		req := <-ch
+		// TODO: fetch data from db
+		// res := buildResponse(req.Data)
+		// Reply to that specific peer only.
+		p2pserver.Send(nil, req.Peer)
+	}
+}
+
+// proposeCollations is the main event loop of a proposer service that listens for
+// incoming transactions and adds them to the SMC.
+func (p *Proposer) proposeCollations() {
 
 	// TODO: Receive TXs from shard TX generator or TXpool (Github Issues 153 and 161)
 	var txs []*types.Transaction
@@ -52,30 +84,25 @@ func (p *Proposer) Start() error {
 	// Get current block number.
 	blockNumber, err := p.client.ChainReader().BlockByNumber(context.Background(), nil)
 	if err != nil {
-		return err
+		log.Error(fmt.Sprintf("Could not fetch current block number: %v", err))
+		return
 	}
 	period := new(big.Int).Div(blockNumber.Number(), big.NewInt(sharding.PeriodLength))
 
 	// Create collation.
 	collation, err := createCollation(p.client, shardID, period, txs)
 	if err != nil {
-		return err
+		log.Error(fmt.Sprintf("Could not create collation: %v", err))
+		return
 	}
 
 	// Check SMC if we can submit header before addHeader
 	canAdd, err := checkHeaderAdded(p.client, shardID, period)
 	if err != nil {
-		return err
+		log.Error(fmt.Sprintf("Could not check if we can submit header: %v", err))
+		return
 	}
 	if canAdd {
 		addHeader(p.client, collation)
 	}
-
-	return nil
-}
-
-// Stop the main loop for proposing collations.
-func (p *Proposer) Stop() error {
-	log.Info("Stopping proposer service")
-	return nil
 }
