9ed39451ed0090d4e4180815e796b4db3984e043
Preston Van Loon <preston@machinepowered.com>
2018-06-12 08:33:00 -0400

    Add feed method for getting event feeds from the p2p server

--- sharding/p2p/service.go
+++ sharding/p2p/service.go
@@ -2,15 +2,22 @@
 package p2p
 
 import (
+	"reflect"
+
+	"github.com/ethereum/go-ethereum/event"
 	"github.com/ethereum/go-ethereum/log"
 )
 
 // Server is a placeholder for a shardp2p service. To be designed.
-type Server struct{}
+type Server struct {
+	feeds map[reflect.Type]*event.Feed
+}
 
 // NewServer creates a new shardp2p service instance.
 func NewServer() (*Server, error) {
-	return &Server{}, nil
+	return &Server{
+		feeds: make(map[reflect.Type]*event.Feed),
+	}, nil
 }
 
 // Start the main routine for an shardp2p server.
@@ -23,4 +30,14 @@ func (s *Server) Start() error {
 func (s *Server) Stop() error {
 	log.Info("Stopping shardp2p server")
 	return nil
+}
+
+// Feed returns a event feed for the given message type.
+// TODO(prestonvanloon): Add more to this GoDoc before merging.
+func (s *Server) Feed(msg interface{}) (*event.Feed, error) {
+	t := reflect.TypeOf(msg)
+	if s.feeds[t] != nil {
+		s.feeds[t] = new(event.Feed)
+	}
+	return s.feeds[t], nil
 }
