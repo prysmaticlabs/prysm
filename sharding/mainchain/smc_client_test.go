912b3b65bd3f4375f096f1c66c55259af9045be0
Preston Van Loon <preston@machinepowered.com>
2018-06-16 22:26:03 -0400

    FIx start/stop of ShardEthereum (#185)
    * sharding: Fix mainchain.Client starting RPC connections during ShardEthereum.New. Fix graceful stop
    
    * sharding: Just pass the cli.Context rather than keeping it on the shardEthereum
    
    * sharding: add doc
    
    * sharding: add doc
    
    * Sharding: remove exgtra newline
    
    * sharding:fix lint
    
    
    Former-commit-id: fdaf8160245d9233b693f685ba6078e4b15fa279 [formerly f7fa71912b7d8340ede6cd08b357056fafbab014]
    Former-commit-id: 0e8bfbbc579451178f76263364fdbcd00a91d651

--- sharding/mainchain/smc_client_test.go
+++ sharding/mainchain/smc_client_test.go
@@ -1,4 +1,9 @@
 package mainchain
 
+import "github.com/ethereum/go-ethereum/sharding"
+
 // Verifies that SMCClient implements the Client interface.
 var _ = Client(&SMCClient{})
+
+// Verifies that SMCCLient implements the sharding Service inteface.
+var _ = sharding.Service(&SMCClient{})
--- sharding/node/backend_test.go
+++ sharding/node/backend_test.go
@@ -1,6 +1,26 @@
 package node
 
-import "github.com/ethereum/go-ethereum/sharding"
+import (
+	"flag"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/sharding"
+
+	cli "gopkg.in/urfave/cli.v1"
+)
 
 // Verifies that ShardEthereum implements the Node interface.
 var _ = sharding.Node(&ShardEthereum{})
+
+// Test that the sharding node can build with default flag values.
+func TestNode_Builds(t *testing.T) {
+	app := cli.NewApp()
+	set := flag.NewFlagSet("test", 0)
+
+	context := cli.NewContext(app, set, nil)
+
+	_, err := New(context)
+	if err != nil {
+		t.Fatalf("Failed to create ShardEthereum: %v", err)
+	}
+}
