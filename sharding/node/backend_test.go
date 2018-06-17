13616ed2058401a281431bf8e20b968b799d2081
Preston Van Loon <preston@machinepowered.com>
2018-06-16 21:36:30 -0400

    sharding: add doc

--- sharding/node/backend_test.go
+++ sharding/node/backend_test.go
@@ -12,16 +12,15 @@ import (
 // Verifies that ShardEthereum implements the Node interface.
 var _ = sharding.Node(&ShardEthereum{})
 
+// Test that the sharding node can build with default flag values.
 func TestNode_Builds(t *testing.T) {
 	app := cli.NewApp()
 	set := flag.NewFlagSet("test", 0)
 
 	context := cli.NewContext(app, set, nil)
 
-	s, err := New(context)
+	_, err := New(context)
 	if err != nil {
 		t.Fatalf("Failed to create ShardEthereum: %v", err)
 	}
-
-	_ = s
 }
