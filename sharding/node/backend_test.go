76a0a4a55efdcd17efaec278183391fc760d25d0
Preston Van Loon <preston@machinepowered.com>
2018-06-16 21:28:31 -0400

    sharding: Fix mainchain.Client starting RPC connections during ShardEthereum.New. Fix graceful stop

--- sharding/node/backend_test.go
+++ sharding/node/backend_test.go
@@ -1,6 +1,27 @@
 package node
 
-import "github.com/ethereum/go-ethereum/sharding"
+import (
+	"testing"
+
+	"flag"
+	"github.com/ethereum/go-ethereum/sharding"
+
+	cli "gopkg.in/urfave/cli.v1"
+)
 
 // Verifies that ShardEthereum implements the Node interface.
 var _ = sharding.Node(&ShardEthereum{})
+
+func TestNode_Builds(t *testing.T) {
+	app := cli.NewApp()
+	set := flag.NewFlagSet("test", 0)
+
+	context := cli.NewContext(app, set, nil)
+
+	s, err := New(context)
+	if err != nil {
+		t.Fatalf("Failed to create ShardEthereum: %v", err)
+	}
+
+	_ = s
+}
