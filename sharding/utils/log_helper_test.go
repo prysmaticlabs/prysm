4e46ffa4249d1067fd5bf07740408fa41ed933a7
Preston Van Loon <preston@machinepowered.com>
2018-06-17 13:16:50 -0400

    Remove Test prefix

--- sharding/utils/log_helper_test.go
+++ sharding/utils/log_helper_test.go
@@ -6,35 +6,35 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 )
 
-var _ = log.Handler(&TestLogHandler{})
+var _ = log.Handler(&LogHandler{})
 
-// TestLogHandler provides methods for testing ethereum logs.
-type TestLogHandler struct {
+// LogHandler provides methods for testing ethereum logs.
+type LogHandler struct {
 	records []*log.Record
 	t       *testing.T
 }
 
 // Log adds records to the record slice.
-func (t *TestLogHandler) Log(r *log.Record) error {
+func (t *LogHandler) Log(r *log.Record) error {
 	t.records = append(t.records, r)
 	return nil
 }
 
 // Pop the record at index 0.
-func (t *TestLogHandler) Pop() *log.Record {
+func (t *LogHandler) Pop() *log.Record {
 	var r *log.Record
 	r, t.records = t.records[0], t.records[1:]
 	return r
 }
 
 // Length of the records slice.
-func (t *TestLogHandler) Len() int {
+func (t *LogHandler) Len() int {
 	return len(t.records)
 }
 
 // VerifyLogMsg verfies that the log at index 0 matches the string exactly.
 // This method removes the verified message from the slice.
-func (h *TestLogHandler) VerifyLogMsg(str string) {
+func (h *LogHandler) VerifyLogMsg(str string) {
 	if h.Len() == 0 {
 		h.t.Error("Expected a log, but there were none!")
 	}
