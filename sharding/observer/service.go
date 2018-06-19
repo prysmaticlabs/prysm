1e1a7ce366ca8cc13e0feee848c1ece8fb7014b7
Terence Tsao <terence@prysmaticlabs.com>
2018-06-19 10:20:13 -0700

    align observer instance with rest of the actors

--- sharding/observer/service.go
+++ sharding/observer/service.go
@@ -5,32 +5,39 @@ package observer
 import (
 	"fmt"
 
+	"context"
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/sharding/database"
+	"github.com/ethereum/go-ethereum/sharding"
 	"github.com/ethereum/go-ethereum/sharding/p2p"
+	"math/big"
 )
 
 // Observer holds functionality required to run an observer service
 // in a sharded system. Must satisfy the Service interface defined in
 // sharding/service.go.
 type Observer struct {
-	p2p          *p2p.Server
-	shardChainDb *database.ShardDB
-	shardID      int
+	p2p    *p2p.Server
+	shard  *sharding.Shard
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
-// NewObserver creates a new observer instance.
-func NewObserver(p2p *p2p.Server, shardChainDb *database.ShardDB, shardID int) (*Observer, error) {
-	return &Observer{p2p, shardChainDb, shardID}, nil
+// NewObserver creates a struct instance of a observer service.
+// It will have access to a p2p server and a shardChainDb.
+func NewObserver(p2p *p2p.Server, shardChainDb ethdb.Database, shardID int) (*Observer, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	shard := sharding.NewShard(big.NewInt(int64(shardID)), shardChainDb)
+	return &Observer{p2p, shard, ctx, cancel}, nil
 }
 
-// Start the main routine for an observer.
+// Start the main loop for observer service.
 func (o *Observer) Start() {
-	log.Info(fmt.Sprintf("Starting observer service in shard %d", o.shardID))
+	log.Info(fmt.Sprintf("Starting observer service."))
 }
 
-// Stop the main loop for observing the shard network.
+// Stop the main loop for observer service.
 func (o *Observer) Stop() error {
-	log.Info(fmt.Sprintf("Starting observer service in shard %d", o.shardID))
+	log.Info(fmt.Sprintf("Stopping observer service."))
 	return nil
 }
