281db36faa8e6e961e7d4380337a10056f40b99e
Raul Jordan <rauljordan@college.harvard.edu>
2018-07-08 16:02:00 -0500

    re-add internal/debug. leaving glog addition to separate PR

--- sharding/main.go
+++ sharding/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
+	"github.com/prysmaticlabs/geth-sharding/internal/debug"
 	"github.com/prysmaticlabs/geth-sharding/sharding/node"
 	"github.com/prysmaticlabs/geth-sharding/sharding/utils"
 	"github.com/urfave/cli"
 )
 
 func startNode(ctx *cli.Context) error {
+	if err := debug.Setup(ctx); err != nil {
+		return err
+	}
 	shardingNode, err := node.New(ctx)
 	if err != nil {
 		return err
@@ -45,6 +50,19 @@ VERSION:
 `
 	app.Action = startNode
 	app.Flags = []cli.Flag{utils.ActorFlag, utils.DataDirFlag, utils.PasswordFileFlag, utils.NetworkIdFlag, utils.IPCPathFlag, utils.DepositFlag, utils.ShardIDFlag}
+
+	app.Flags = append(app.Flags, debug.Flags...)
+
+	app.Before = func(ctx *cli.Context) error {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		return debug.Setup(ctx)
+	}
+
+	app.After = func(ctx *cli.Context) error {
+		debug.Exit()
+		return nil
+	}
+
 	if err := app.Run(os.Args); err != nil {
 		if _, err := fmt.Fprintln(os.Stderr, err); err != nil {
 			panic(err)
