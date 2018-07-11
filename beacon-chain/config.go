620564cd6166513ba33221969587a1e8f366ed30
Nishant Das <nish1993@hotmail.com>
2018-07-12 06:29:31 +0800

    beacon: move beacon-chain to geth-sharding repo (#250)
    Former-commit-id: 689de6935d724226eb9125d597831d7b274d79c1 [formerly 4e0c0d671c4e6284cd33300231cc0f7aba99d314]
    Former-commit-id: c3079b352bc938c12708b81cdc0130909456ea27

---
+++ beacon-chain/config.go
@@ -0,0 +1,20 @@
+package beacon
+
+const (
+	// AttesterCount is the number of attesters per committee/
+	AttesterCount = 32
+	// AttesterReward determines how much ETH attesters get for performing their duty.
+	AttesterReward = 1
+	// EpochLength is the beacon chain epoch length in blocks.
+	EpochLength = 5
+	// ShardCount is a fixed number.
+	ShardCount = 20
+	// DefaultBalance of a validator.
+	DefaultBalance = 32000
+	// DefaultSwitchDynasty value.
+	DefaultSwitchDynasty = 9999999999999999999
+	// MaxValidators in the protocol.
+	MaxValidators = 2 ^ 24
+	// NotariesPerCrosslink fixed to 100.
+	NotariesPerCrosslink = 100
+)
