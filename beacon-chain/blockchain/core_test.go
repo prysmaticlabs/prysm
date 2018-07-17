90c1dd807999179a7c25865be4001c9de28afe1f
Raul Jordan <rauljordan@college.harvard.edu>
2018-07-17 15:51:18 -0500

    fix tests

---
+++ beacon-chain/blockchain/core_test.go
@@ -0,0 +1,19 @@
+package blockchain
+
+import "testing"
+
+func TestNewBeaconChain(t *testing.T) {
+
+}
+
+func TestMutateActiveState(t *testing.T) {
+
+}
+
+func TestMutateCrystallizedState(t *testing.T) {
+
+}
+
+func TestPersist(t *testing.T) {
+
+}
