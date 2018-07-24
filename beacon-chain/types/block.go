413a89bade3959582972afcbab2a1ba4a99b6837
Terence Tsao <terence@prysmaticlabs.com>
2018-07-24 08:50:52 -0700

    processing incoming block should check hashes

--- beacon-chain/types/block.go
+++ beacon-chain/types/block.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"hash"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -31,15 +32,15 @@ func NewGenesisBlock() *Block {
 
 // Data contains the fields in a beacon chain block.
 type Data struct {
-	ParentHash              common.Hash     // ParentHash is the hash of the parent beacon block.
+	ParentHash              hash.Hash       // ParentHash is the hash of the parent beacon block.
 	SlotNumber              uint64          // Slot number is the number a client should check to know when it creates block.
-	RandaoReveal            common.Hash     // RandaoReveal is used for Randao commitment reveal.
+	RandaoReveal            hash.Hash       // RandaoReveal is used for Randao commitment reveal.
 	AttestationBitmask      []byte          // AttestationBitmask is the bit field of who from the attestation committee participated.
 	AttestationAggregateSig []uint          // AttestationAggregateSig is validator's aggregate sig.
 	ShardAggregateVotes     []AggregateVote // ShardAggregateVotes is shard aggregate votes.
 	MainChainRef            common.Hash     // MainChainRef is the reference to main chain block.
-	ActiveStateHash         []byte          // ActiveStateHash is the state that changes every block.
-	CrystallizedStateHash   []byte          // CrystallizedStateHash is the state that changes every epoch.
+	ActiveStateHash         hash.Hash       // ActiveStateHash is the state that changes every block.
+	CrystallizedStateHash   hash.Hash       // CrystallizedStateHash is the state that changes every epoch.
 	Timestamp               time.Time
 }
 
@@ -49,4 +50,12 @@ type AggregateVote struct {
 	ShardBlockHash common.Hash // ShardBlockHash is the shard block hash of the voted shard.
 	SignerBitmask  []byte      // SignerBitmask is the bit mask of every validator that signed.
 	AggregateSig   []uint      // AggregateSig is the aggregated signatures of individual shard.
+}
+
+func (b *Block) InsertActiveHash(hash hash.Hash) {
+	b.data.ActiveStateHash = hash
+}
+
+func (b *Block) InsertCrystallizedHash(hash hash.Hash) {
+	b.data.CrystallizedStateHash = hash
 }
