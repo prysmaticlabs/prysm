91191af1aaecba9ed7df49e9d07ff50c395442e1
Raul Jordan <rauljordan@college.harvard.edu>
2018-08-03 00:02:00 -0500

    service mock

--- client/attester/service_test.go
+++ client/attester/service_test.go
@@ -1,6 +1,20 @@
 package attester
 
-import "testing"
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
+	"google.golang.org/grpc"
+)
+
+type mockBeaconServiceClient struct{}
+
+func (mb *mockBeaconServiceClient) LatestBeaconHashHeight(ctx context.Context, emp *empty.Empty, opts ...grpc.CallOption) (pb.BeaconService_LatestBeaconHashHeightClient, error) {
+	return nil, errors.New("error setting up")
+}
 
 func TestLifecycle(t *testing.T) {
 
