9f891873b3446ba0952935cee7dca23f5202e15b
Raul Jordan <rauljordan@college.harvard.edu>
2018-08-02 23:01:24 -0500

    service_test.go

---
+++ client/attester/service_test.go
@@ -0,0 +1,15 @@
+package attester
+
+import "testing"
+
+func TestLifecycle(t *testing.T) {
+
+}
+
+func TestFetchCrystallizedState(t *testing.T) {
+
+}
+
+func TestFetchBeaconHashHeight(t *testing.T) {
+
+}
