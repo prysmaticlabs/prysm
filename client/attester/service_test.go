a023315a0e6cc1a93d542741df73495e73b33776
Raul Jordan <rauljordan@college.harvard.edu>
2018-08-08 17:37:52 -0500

    lifecycle test

--- client/attester/service_test.go
+++ client/attester/service_test.go
@@ -2,6 +2,7 @@ package attester
 
 import (
 	"context"
+	"io"
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
@@ -12,34 +13,36 @@ import (
 	logTest "github.com/sirupsen/logrus/hooks/test"
 )
 
-type faultyClient struct{}
+type faultyClient struct {
+	ctrl *gomock.Controller
+}
 
 func (fc *faultyClient) BeaconServiceClient() pb.BeaconServiceClient {
-	return internal.NewMockBeaconServiceClient(nil)
+	return internal.NewMockBeaconServiceClient(fc.ctrl)
 }
 
 func TestLifecycle(t *testing.T) {
-	// hook := logTest.NewGlobal()
-	// at := NewAttester(context.Background(), &faultyClient{})
-	// at.Start()
-	// testutil.AssertLogsContain(t, hook, "Starting service")
-	// at.Stop()
-	// testutil.AssertLogsContain(t, hook, "Stopping service")
-}
-
-func TestFetchCrystallizedState(t *testing.T) {
 	hook := logTest.NewGlobal()
-	// Testing using a faulty client.
-	at := NewAttester(context.Background(), &faultyClient{})
-
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
+	at := NewAttester(context.Background(), &faultyClient{ctrl})
+	at.Stop()
+	testutil.AssertLogsContain(t, hook, "Stopping service")
+}
+
+func TestFetchBeaconBlocks(t *testing.T) {
+	hook := logTest.NewGlobal()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	at := NewAttester(context.Background(), &faultyClient{ctrl})
 
 	// Create mock for the stream returned by LatestBeaconBlock.
 	stream := internal.NewMockBeaconService_LatestBeaconBlockClient(ctrl)
 
 	// Set expectation on receiving.
 	stream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{SlotNumber: 10}, nil)
+	stream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{}, io.EOF)
 
 	mockServiceClient := internal.NewMockBeaconServiceClient(ctrl)
 	mockServiceClient.EXPECT().LatestBeaconBlock(
@@ -49,10 +52,10 @@ func TestFetchCrystallizedState(t *testing.T) {
 
 	at.fetchBeaconBlocks(mockServiceClient)
 
-	testutil.AssertLogsContain(t, hook, "Could not setup beacon chain block streaming client")
+	testutil.AssertLogsContain(t, hook, "Latest beacon block slot number")
 }
 
-func TestFetchBeaconHashHeight(t *testing.T) {
+func TestFetchCrystallizedState(t *testing.T) {
 	// hook := logTest.NewGlobal()
 	// // Testing using a faulty client.
 	// at := NewAttester(context.Background(), &faultyClient{})
