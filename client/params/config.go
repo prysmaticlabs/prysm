076f6f094b7cd93136e01d98fb7193fbf2fdeed9
Terence Tsao <terence@prysmaticlabs.com>
2018-08-12 23:42:07 -0700

    removed outdated configs

--- client/params/config.go
+++ client/params/config.go
@@ -5,37 +5,13 @@ package params
 
 import (
 	"math"
-	"math/big"
-
-	"github.com/ethereum/go-ethereum/common"
-)
-
-const (
-	// DefaultPeriodLength is the default value for period lengths in sharding.
-	DefaultPeriodLength = 5
-	// DefaultAttesterLockupLength is the default number of blocks to lock up
-	// an attesters deposit before they can withdraw it.
-	DefaultAttesterLockupLength = 16128
 )
 
 // DefaultConfig returns pointer to a Config value with same defaults.
 func DefaultConfig() *Config {
 	return &Config{
-		SMCAddress:              common.HexToAddress("0x0"),
-		PeriodLength:            DefaultPeriodLength,
-		AttesterDeposit:         DefaultAttesterDeposit(),
-		AttesterLockupLength:    DefaultAttesterLockupLength,
-		ProposerLockupLength:    48,
-		AttesterCommitteeSize:   135,
-		AttesterQuorumSize:      90,
-		AttesterChallengePeriod: 25,
-		CollationSizeLimit:      DefaultCollationSizeLimit(),
+		CollationSizeLimit: DefaultCollationSizeLimit(),
 	}
-}
-
-// DefaultAttesterDeposit required to be an attester.
-func DefaultAttesterDeposit() *big.Int {
-	return new(big.Int).Exp(big.NewInt(10), big.NewInt(21), nil) // 1000 ETH
 }
 
 // DefaultCollationSizeLimit is the integer value representing the maximum
@@ -44,21 +20,7 @@ func DefaultCollationSizeLimit() int64 {
 	return int64(math.Pow(float64(2), float64(20)))
 }
 
-// DefaultChainConfig contains default chain configs of an individual shard.
-var DefaultChainConfig = &ChainConfig{}
-
 // Config contains configs for node to participate in the sharded universe.
 type Config struct {
-	SMCAddress              common.Address // SMCAddress is the address of SMC in mainchain.
-	PeriodLength            int64          // PeriodLength is num of blocks in period.
-	AttesterDeposit         *big.Int       // AttesterDeposit is a required deposit size in wei.
-	AttesterLockupLength    int64          // AttesterLockupLength to lockup attester deposit from time of deregistration.
-	ProposerLockupLength    int64          // ProposerLockupLength to lockup proposer deposit from time of deregistration.
-	AttesterCommitteeSize   int64          // AttesterCommitSize sampled per block from the attesters pool per period per shard.
-	AttesterQuorumSize      int64          // AttesterQuorumSize votes the collation needs to get accepted to the canonical chain.
-	AttesterChallengePeriod int64          // AttesterChallengePeriod is the duration a attester has to store collations for.
-	CollationSizeLimit      int64          // CollationSizeLimit is the maximum size the serialized blobs in a collation can take.
+	CollationSizeLimit int64 // CollationSizeLimit is the maximum size the serialized blobs in a collation can take.
 }
-
-// ChainConfig contains chain config of an individual shard. Still to be designed.
-type ChainConfig struct{}
