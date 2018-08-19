1da3adb9fc3a3bc00cde3a8caf680be0ab4e6e2c
Preston Van Loon <preston@machinepowered.com>
2018-08-19 16:15:21 -0400

    split topics

--- beacon-chain/node/p2p_config.go
+++ beacon-chain/node/p2p_config.go
@@ -2,6 +2,8 @@ package node
 
 import (
 	"github.com/prysmaticlabs/prysm/shared/p2p"
+
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
 func configureP2P() (*p2p.Server, error) {
@@ -10,12 +12,9 @@ func configureP2P() (*p2p.Server, error) {
 		return nil, err
 	}
 
-	// Configure adapters
 	var adapters []p2p.Adapter
-	type TestInterface struct {
-	}
 
-	s.RegisterTopic("test_topic", TestInterface{}, adapters...)
+	s.RegisterTopic(v1.Topic_BEACON_BLOCK_HASH_ANNOUNCE.String(), pb.BeaconBlockHashAnnounce{}, adapters...)
 
 	return s, nil
 }
