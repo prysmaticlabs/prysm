6b07bf56fe2c7e49ccfe6ed0e439e527612986b3
nisdas <nishdas93@gmail.com>
2018-09-02 15:46:18 +0800

    removing decodeslotnumber

--- beacon-chain/blockchain/schema_test.go
+++ beacon-chain/blockchain/schema_test.go
@@ -5,22 +5,9 @@ import (
 	"testing"
 )
 
-func TestEncodeAndDecodeSlotNumber(t *testing.T) {
-	testValues := []uint64{20, 200, 88, 90, 19, 8029, 36209, 202928}
-	for _, val := range testValues {
-
-		encodednum := encodeSlotNumber(val)
-		expectednumber := decodeSlotNumber(encodednum)
-
-		if val != expectednumber {
-			t.Errorf("encoding leads to different expected and generated slotnumber %v, %v", val, expectednumber)
-		}
-	}
-}
-
 func TestBlockKeys(t *testing.T) {
-	var testhash [32]byte
-	testhash = [32]byte{1, 2, 4, 5, 6, 7, 8, 9, 10}
+
+	testhash := [32]byte{1, 2, 4, 5, 6, 7, 8, 9, 10}
 	testkey := append(blockPrefix, testhash[:]...)
 	generatedKey := blockKey(testhash)
 
@@ -29,7 +16,7 @@ func TestBlockKeys(t *testing.T) {
 	}
 
 	testslotnumber := uint64(4)
-	expectedKey := append(canonicalPrefix, []byte{0, 0, 0, 0, 0, 0, 0, byte(testslotnumber)}...)
+	expectedKey := append(canonicalPrefix, encodeSlotNumber(testslotnumber)...)
 	generatedkey := canonicalBlockKey(testslotnumber)
 
 	if !bytes.Equal(generatedkey, expectedKey) {
