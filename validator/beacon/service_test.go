719eac91c36480c1fbf9849844df8a5f7571ebe4
Raul Jordan <rauljordan@college.harvard.edu>
2018-09-04 22:57:22 -0400

    clear up tests

--- validator/beacon/service_test.go
+++ validator/beacon/service_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	gomock "github.com/golang/mock/gomock"
+	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/empty"
 	pbp2p "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
@@ -30,15 +31,9 @@ type mockClient struct {
 func (fc *mockClient) BeaconServiceClient() pb.BeaconServiceClient {
 	mockServiceClient := internal.NewMockBeaconServiceClient(fc.ctrl)
 
-	blockStream := internal.NewMockBeaconService_LatestBeaconBlockClient(fc.ctrl)
-	blockStream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{}, io.EOF)
 	stateStream := internal.NewMockBeaconService_LatestCrystallizedStateClient(fc.ctrl)
 	stateStream.EXPECT().Recv().Return(&pbp2p.CrystallizedState{}, io.EOF)
 
-	mockServiceClient.EXPECT().LatestBeaconBlock(
-		gomock.Any(),
-		&empty.Empty{},
-	).Return(blockStream, nil)
 	mockServiceClient.EXPECT().LatestCrystallizedState(
 		gomock.Any(),
 		&empty.Empty{},
@@ -53,15 +48,9 @@ type mockLifecycleClient struct {
 func (fc *mockLifecycleClient) BeaconServiceClient() pb.BeaconServiceClient {
 	mockServiceClient := internal.NewMockBeaconServiceClient(fc.ctrl)
 
-	blockStream := internal.NewMockBeaconService_LatestBeaconBlockClient(fc.ctrl)
-	blockStream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{}, io.EOF)
 	stateStream := internal.NewMockBeaconService_LatestCrystallizedStateClient(fc.ctrl)
 	stateStream.EXPECT().Recv().Return(&pbp2p.CrystallizedState{}, io.EOF)
 
-	mockServiceClient.EXPECT().LatestBeaconBlock(
-		gomock.Any(),
-		&empty.Empty{},
-	).Return(blockStream, nil)
 	mockServiceClient.EXPECT().LatestCrystallizedState(
 		gomock.Any(),
 		&empty.Empty{},
@@ -75,19 +64,12 @@ func (fc *mockLifecycleClient) BeaconServiceClient() pb.BeaconServiceClient {
 		CurrentDynasty: 5,
 	}
 
-	mockServiceClient.EXPECT().CanonicalHeadAndState(
+	mockServiceClient.EXPECT().GenesisTimeAndCanonicalState(
 		gomock.Any(),
 		gomock.Any(),
-	).Return(&pb.CanonicalResponse{
-		CrystallizedState: crystallized,
-		CanonicalBlock:    &pbp2p.BeaconBlock{SlotNumber: 1},
-	}, nil)
-
-	mockServiceClient.EXPECT().FetchShuffledValidatorIndices(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(&pb.ShuffleResponse{
-		ShuffledValidatorIndices: []uint64{0, 1, 2},
+	).Return(&pb.GenesisTimeAndStateResponse{
+		LatestCrystallizedState: crystallized,
+		GenesisTimestamp:        ptypes.TimestampNow(),
 	}, nil)
 
 	return mockServiceClient
@@ -112,78 +94,6 @@ func TestLifecycle(t *testing.T) {
 	testutil.AssertLogsContain(t, hook, "Starting service")
 	b.Stop()
 	testutil.AssertLogsContain(t, hook, "Stopping service")
-}
-
-func TestListenForBeaconBlocks(t *testing.T) {
-	hook := logTest.NewGlobal()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	b := NewBeaconValidator(context.Background(), &mockClient{ctrl})
-
-	// Create mock for the stream returned by LatestBeaconBlock.
-	stream := internal.NewMockBeaconService_LatestBeaconBlockClient(ctrl)
-
-	// If the block's slot number from the stream matches the assigned attestation slot,
-	// trigger a log.
-	stream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{SlotNumber: 10}, nil)
-	stream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{}, io.EOF)
-	b.assignedSlot = 10
-	b.responsibility = "attester"
-
-	mockServiceClient := internal.NewMockBeaconServiceClient(ctrl)
-	mockServiceClient.EXPECT().LatestBeaconBlock(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(stream, nil)
-
-	b.listenForBeaconBlocks(mockServiceClient)
-
-	testutil.AssertLogsContain(t, hook, "Latest beacon block slot number")
-	testutil.AssertLogsContain(t, hook, "Assigned attestation slot number reached")
-
-	// If the validator is assigned to be a proposer, trigger a log upon next
-	// SlotNumber being reached.
-	stream = internal.NewMockBeaconService_LatestBeaconBlockClient(ctrl)
-
-	stream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{SlotNumber: 1}, nil)
-	stream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{}, io.EOF)
-	b.responsibility = "proposer"
-
-	mockServiceClient = internal.NewMockBeaconServiceClient(ctrl)
-	mockServiceClient.EXPECT().LatestBeaconBlock(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(stream, nil)
-
-	b.listenForBeaconBlocks(mockServiceClient)
-
-	testutil.AssertLogsContain(t, hook, "Latest beacon block slot number")
-	testutil.AssertLogsContain(t, hook, "Assigned proposal slot number reached")
-
-	// Testing an error coming from the stream.
-	stream = internal.NewMockBeaconService_LatestBeaconBlockClient(ctrl)
-	stream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{}, errors.New("stream error"))
-	stream.EXPECT().Recv().Return(&pbp2p.BeaconBlock{}, io.EOF)
-
-	mockServiceClient = internal.NewMockBeaconServiceClient(ctrl)
-	mockServiceClient.EXPECT().LatestBeaconBlock(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(stream, nil)
-
-	b.listenForBeaconBlocks(mockServiceClient)
-
-	testutil.AssertLogsContain(t, hook, "stream error")
-
-	// Creating a faulty stream will trigger error.
-	mockServiceClient = internal.NewMockBeaconServiceClient(ctrl)
-	mockServiceClient.EXPECT().LatestBeaconBlock(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(stream, errors.New("stream creation failed"))
-
-	b.listenForBeaconBlocks(mockServiceClient)
-	testutil.AssertLogsContain(t, hook, "stream creation failed")
 }
 
 func TestListenForCrystallizedStates(t *testing.T) {
@@ -219,21 +129,6 @@ func TestListenForCrystallizedStates(t *testing.T) {
 
 	testutil.AssertLogsContain(t, hook, "recv error")
 
-	// Being unable to marshal the received crystallized state should log an error.
-	stream = internal.NewMockBeaconService_LatestCrystallizedStateClient(ctrl)
-	stream.EXPECT().Recv().Return(nil, nil)
-	stream.EXPECT().Recv().Return(&pbp2p.CrystallizedState{}, io.EOF)
-
-	mockServiceClient = internal.NewMockBeaconServiceClient(ctrl)
-	mockServiceClient.EXPECT().LatestCrystallizedState(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(stream, nil)
-
-	b.listenForCrystallizedStates(mockServiceClient)
-
-	testutil.AssertLogsContain(t, hook, "Could not marshal crystallized state proto")
-
 	// If the current validator is not found within the active validators list, log a debug message.
 	validator := &pbp2p.ValidatorRecord{WithdrawalAddress: []byte("0x01"), StartDynasty: 1, EndDynasty: 10}
 	stream = internal.NewMockBeaconService_LatestCrystallizedStateClient(ctrl)
@@ -261,10 +156,6 @@ func TestListenForCrystallizedStates(t *testing.T) {
 		gomock.Any(),
 		gomock.Any(),
 	).Return(stream, nil)
-	mockServiceClient.EXPECT().FetchShuffledValidatorIndices(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(nil, errors.New("something went wrong"))
 
 	b.listenForCrystallizedStates(mockServiceClient)
 
@@ -283,14 +174,6 @@ func TestListenForCrystallizedStates(t *testing.T) {
 		gomock.Any(),
 		gomock.Any(),
 	).Return(stream, nil)
-	mockServiceClient.EXPECT().FetchShuffledValidatorIndices(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(&pb.ShuffleResponse{
-		AssignedAttestationSlots: []uint64{0, 1, 2},
-		CutoffIndices:            []uint64{0, 1, 2},
-		ShuffledValidatorIndices: []uint64{2, 1, 0},
-	}, nil)
 
 	b.listenForCrystallizedStates(mockServiceClient)
 
@@ -310,12 +193,6 @@ func TestListenForCrystallizedStates(t *testing.T) {
 		gomock.Any(),
 		gomock.Any(),
 	).Return(stream, nil)
-	mockServiceClient.EXPECT().FetchShuffledValidatorIndices(
-		gomock.Any(),
-		gomock.Any(),
-	).Return(&pb.ShuffleResponse{
-		ShuffledValidatorIndices: []uint64{0, 1, 2},
-	}, nil)
 
 	b.listenForCrystallizedStates(mockServiceClient)
 
