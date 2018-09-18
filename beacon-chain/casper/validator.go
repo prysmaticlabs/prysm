aa4b2cbc46bc236a78d0ceb46766cf9e24562741
Terence Tsao <terence@prysmaticlabs.com>
2018-09-18 10:50:39 -0700

    removed unncessary comments

--- beacon-chain/casper/validator.go
+++ beacon-chain/casper/validator.go
@@ -99,13 +99,6 @@ func GetAttestersTotalDeposit(validatorIndices []uint32) uint64 {
 
 // GetShardAndCommitteesForSlot returns the attester set of a given slot.
 func GetShardAndCommitteesForSlot(shardCommittees []*pb.ShardAndCommitteeArray, lastStateRecalc uint64, slot uint64) (*pb.ShardAndCommitteeArray, error) {
-	//
-	//if lastStateRecalc < params.CycleLength {
-	//	lastStateRecalc = 0
-	//} else {
-	//	lastStateRecalc = lastStateRecalc - params.CycleLength
-	//}
-
 	if !(lastStateRecalc <= slot && slot < lastStateRecalc+params.CycleLength*2) {
 		return nil, fmt.Errorf("can not return attester set of given slot, input slot %v has to be in between %v and %v", slot, lastStateRecalc, lastStateRecalc+params.CycleLength*2)
 	}
