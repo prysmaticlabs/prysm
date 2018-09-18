820ab2e2755b327c769fe6b6a208035bca8f676a
Preston Van Loon <preston@machinepowered.com>
2018-09-18 00:09:30 -0400

    words

--- beacon-chain/types/active_state.go
+++ beacon-chain/types/active_state.go
@@ -120,8 +120,8 @@ func (a *ActiveState) calculateNewAttestations(add []*pb.AggregatedAttestation, 
 //   [0xF, 0x7, 0x0, 0x0, 0x5]
 //
 // Where 0x0 is an empty or missing hash where no block was produced in the
-// alloted slot. When storing (or at least when computing the hash of the
-// active state), the list should be backfilled as such:
+// alloted slot. When storing the list (or at least when computing the hash of
+// the active state), the list should be backfilled as such:
 //
 //   [0xF, 0x7, 0x5, 0x5, 0x5]
 //
