23feff22cc8e637f8a40d65d81b16731bd609b11
Zach Zundel <me@zachzundel.com>
2018-09-18 12:45:53 -0700

    Remove blame from todo check

--- scripts/check-todo.sh
+++ scripts/check-todo.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-declare -a VIOLATION 
-RETURN=0
-
 # Continuous integration script to check that TODOs are in the correct format
-while IFS=: read file line rest;
-do
-    git blame -L $line,$line $file
-    RETURN=1
-done <<<$(grep -PrinH '(?<!context\.)todo(?!\(#{0,1}\d+\))' --include \*.go *)
-
-if [[ $RETURN ]];
-then
+if grep -Priq '(?<!context\.)todo(?!\(#{0,1}\d+\))' --include \*.go *;
+then 
     echo "Invalid TODOs found. Failing." >&2
+    exit 1;
 fi
-
-
-exit $RETURN
