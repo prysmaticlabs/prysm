061cb7792dd3d94b9a68920fcd69473164ca0fb3
Zach Zundel <me@zachzundel.com>
2018-09-18 15:21:28 -0700

    Print failing todo comments

--- scripts/check-todo.sh
+++ scripts/check-todo.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
 
 # Continuous integration script to check that TODOs are in the correct format
-if grep -Priq '(?<!context\.)todo(?!\(#{0,1}\d+\))' --include \*.go *;
+OUTPUT="$(grep -PrinH '(?<!context\.)todo(?!\(#{0,1}\d+\))' --include \*.go *)";
+if [[ "$OUTPUT" -ne "" ]] ;
 then 
-    echo "Invalid TODOs found. Failing." >&2
+    echo "Invalid TODOs found. Failing." >&2;
+    echo $OUTPUT >&2;
     exit 1;
 fi
