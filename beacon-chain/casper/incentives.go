1b8856f76d443eef020aa70baec272a390936c34
nisdas <nishdas93@gmail.com>
2018-09-19 23:37:37 +0800

    Adding rewards

--- beacon-chain/casper/incentives.go
+++ beacon-chain/casper/incentives.go
@@ -1,6 +1,8 @@
 package casper
 
 import (
+	"math"
+
 	"github.com/prysmaticlabs/prysm/beacon-chain/params"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared"
@@ -31,10 +33,27 @@ func CalculateRewards(attestations []*pb.AggregatedAttestation, validators []*pb
 	return validators, nil
 }
 
-func OfflinePenalty() error {
-	return nil
+func CalculateReward(attestations []*pb.AggregatedAttestation, validators []*pb.ValidatorRecord, dynasty uint64, totalDeposit uint64) {
+
 }
 
-func UpdateValidatorRewards() {
+func RewardQuotient(dynasty uint64, validators []*pb.ValidatorRecord) uint64 {
+	totalDepositETH := TotalActiveValidatorDepositInEth(dynasty, validators)
+	return params.BaseRewardQuotient * uint64(math.Pow(float64(totalDepositETH), 0.5))
+}
 
+func SlotMaxInterestRate(dynasty uint64, validators []*pb.ValidatorRecord) float64 {
+	rewardQuotient := float64(RewardQuotient(dynasty, validators))
+	return 1 / rewardQuotient
+}
+
+func QuadraticPenaltyQuotient() uint64 {
+	dropTimeFactor := float64(params.SqrtDropTime / params.SlotDuration)
+	return uint64(math.Pow(dropTimeFactor, 0.5))
+}
+
+func QuadraticPenalty(numberOfSlots uint64) uint64 {
+	slotFactor := (numberOfSlots * numberOfSlots) / 2
+	penaltyQuotient := QuadraticPenaltyQuotient()
+	return slotFactor / penaltyQuotient
 }
