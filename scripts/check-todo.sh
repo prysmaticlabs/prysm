c5be720b5f5ba4862c16dc86af20e4735f4c69f1
Zach Zundel <me@zachzundel.com>
2018-09-19 21:24:21 -0600

    Fail on failure

--- scripts/check-todo.sh
+++ scripts/check-todo.sh
@@ -3,9 +3,9 @@ set -e
 
 # Continuous integration script to check that TODOs are in the correct format
 OUTPUT="$(grep -PrinH '(?<!context\.)todo(?!\(#{0,1}\d+\))' --include \*.go *)";
-if [[ "$OUTPUT" -ne "" ]] ;
+if [ "$OUTPUT" != "" ] ;
 then 
     echo "Invalid TODOs found. Failing." >&2;
-    echo $OUTPUT >&2;
+    echo "$OUTPUT" >&2;
     exit 1;
 fi
