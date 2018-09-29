e70ad50bf84f5f7b40554f84033651d538892fdc
Raul Jordan <rauljordan@college.harvard.edu>
2018-09-28 21:30:11 -0500

    log fix

--- beacon-chain/blockchain/service.go
+++ beacon-chain/blockchain/service.go
@@ -321,7 +321,7 @@ func (c *ChainService) blockProcessing() {
 
 		// Listen for a newly received incoming block from the sync service.
 		case block := <-c.incomingBlockChan:
-			log.Infof("Incoming block slot number: %d", block.SlotNumber())
+			log.WithField("slotNumber", block.SlotNumber()).Info("Received an incoming block for processing")
 			blockHash, err := block.Hash()
 			if err != nil {
 				log.Errorf("Failed to get hash of block: %v", err)
