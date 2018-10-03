763d0d6bdcf110eacab646d3b7af40d82cbaafd2
Preston Van Loon <preston@machinepowered.com>
2018-10-02 22:04:36 -0400

    Add Go tools in bazel (#608)
    * add atlassian bazel tools
    
    * Add bazel entrypoints for native go stuff
    
    * gazelle

--- beacon-chain/blockchain/service_test.go
+++ beacon-chain/blockchain/service_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum"
+	ethereum "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	gethTypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/prysmaticlabs/prysm/beacon-chain/params"
