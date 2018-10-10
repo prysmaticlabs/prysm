ae11371294e0b5ca47d5a177b42566d986728bcf
nisdas <nishdas93@gmail.com>
2018-10-10 14:28:20 +0800

    add back randao issue

--- validator/proposer/service.go
+++ validator/proposer/service.go
@@ -168,7 +168,7 @@ func (p *Proposer) run(done <-chan struct{}, client pb.ProposerServiceClient) {
 			p.lock.Lock()
 
 			bitmask := p.GenerateBitmask(p.pendingAttestation)
-
+			// TODO(#619): Implement real proposals with randao reveals and attestation fields.
 			req := &pb.ProposeRequest{
 				ParentHash: latestBlockHash[:],
 				// TODO(#511): Fix to be the actual, timebased slot number instead.
