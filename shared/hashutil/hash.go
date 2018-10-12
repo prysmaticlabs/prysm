8074ae2b0352997a99ceac6195623c676150cdbd
thatkawaiiguy <ivanthegreat15@gmail.com>
2018-10-12 04:06:43 -0400

    Add comment to function

--- shared/hashutil/hash.go
+++ shared/hashutil/hash.go
@@ -4,6 +4,8 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
+// Hash defines a function that returns the
+// blake2b hash of the data passed in.
 func Hash(data []byte) [32]byte {
 	var hash [32]byte
 	h := blake2b.Sum512(data)
