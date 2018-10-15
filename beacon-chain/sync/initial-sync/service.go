a809e6f7175e056eb07265c4ed691c5e6025d874
Yutaro Mori <ymori123@gmail.com>
2018-10-15 15:00:22 +0900

    Replace issue number

--- beacon-chain/sync/initial-sync/service.go
+++ beacon-chain/sync/initial-sync/service.go
@@ -148,8 +148,7 @@ func (s *InitialSync) run(delaychan <-chan time.Time) {
 		case <-delaychan:
 			if highestObservedSlot == s.currentSlot {
 				log.Info("Exiting initial sync and starting normal sync")
-				// TODO(#426): Resume sync after completion of initial sync.
-				// See comment in Sync service's Start function for explanation.
+				// TODO(#661): Resume sync after completion of initial sync.
 				return
 			}
 		case msg := <-s.blockBuf:
