65a486b046bc6c5fd961e9ea9bfe93f3e4c95c8b
Raul Jordan <raul@prysmaticlabs.com>
2018-11-06 14:40:20 -0500

    refactor for configurability

--- beacon-chain/chaintest/main.go
+++ beacon-chain/chaintest/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
+	"path"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -12,33 +14,52 @@ import (
 )
 
 func main() {
+	var yamlDir = flag.String("tests-dir", "", "path to directory of yaml tests")
+	flag.Parse()
+
 	customFormatter := new(prefixed.TextFormatter)
 	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
 	customFormatter.FullTimestamp = true
 	log.SetFormatter(customFormatter)
 
-	data, err := ioutil.ReadFile("./sampletests/basic_fork_choice.yaml")
+	var chainTests []*backend.ChainTest
+
+	files, err := ioutil.ReadDir(*yamlDir)
 	if err != nil {
-		log.Fatalf("Could not read file: %v", err)
+		log.Fatalf("Could not read yaml tests directory: %v", err)
 	}
-	chainTests := &backend.ChainTest{}
-	if err := yaml.Unmarshal(data, chainTests); err != nil {
-		log.Fatalf("Could not unmarshal YAML file into test struct: %v", err)
+
+	for _, file := range files {
+		data, err := ioutil.ReadFile(path.Join(*yamlDir, file.Name()))
+		if err != nil {
+			log.Fatalf("Could not read yaml file: %v", err)
+		}
+		decoded := &backend.ChainTest{}
+		if err := yaml.Unmarshal(data, decoded); err != nil {
+			log.Fatalf("Could not unmarshal YAML file into test struct: %v", err)
+		}
+		chainTests = append(chainTests, decoded)
 	}
+
 	sb, err := backend.NewSimulatedBackend()
 	if err != nil {
 		log.Fatalf("Could not create backend: %v", err)
 	}
+
 	log.Info("----Running Chain Tests----")
-	log.Infof("Title: %v", chainTests.Title)
-	log.Infof("Summary: %v", chainTests.Summary)
-	log.Infof("Test Suite: %v", chainTests.TestSuite)
 	startTime := time.Now()
-	for _, tt := range chainTests.TestCases {
-		if err := sb.RunChainTest(tt); err != nil {
-			log.Fatalf("Could not run chain test: %v", err)
+
+	for _, tt := range chainTests {
+		log.Infof("Title: %v", tt.Title)
+		log.Infof("Summary: %v", tt.Summary)
+		log.Infof("Test Suite: %v", tt.TestSuite)
+		for _, testCase := range tt.TestCases {
+			if err := sb.RunChainTest(testCase); err != nil {
+				log.Fatalf("Could not run chain test: %v", err)
+			}
 		}
 	}
+
 	endTime := time.Now()
 	log.Infof("Test Runs Finished In: %v Seconds", endTime.Sub(startTime).Seconds())
 }
