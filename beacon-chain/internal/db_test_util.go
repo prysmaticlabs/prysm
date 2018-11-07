4a8b8b4fa1b97a81b6d1952fbbe051c1c221d73a
Raul Jordan <raul@prysmaticlabs.com>
2018-11-07 13:43:54 -0500

    fix lint

--- beacon-chain/internal/db_test_util.go
+++ beacon-chain/internal/db_test_util.go
@@ -13,7 +13,7 @@ import (
 )
 
 // SetupDB instantiates and returns a BeaconDB instance.
-func SetupDB(t *testing.T) (*db.BeaconDB, error) {
+func SetupDB(t *testing.T) *db.BeaconDB {
 	randPath, err := rand.Int(rand.Reader, big.NewInt(1000000))
 	if err != nil {
 		t.Fatalf("Could not generate random file path: %v", err)
@@ -22,7 +22,11 @@ func SetupDB(t *testing.T) (*db.BeaconDB, error) {
 	if err := os.RemoveAll(path); err != nil {
 		t.Fatalf("Failed to remove directory: %v", err)
 	}
-	return db.NewDB(path)
+	db, err := db.NewDB(path)
+	if err != nil {
+		t.Fatalf("Could not setup DB: %v", err)
+	}
+	return db
 }
 
 // TeardownDB cleans up a BeaconDB instance.
