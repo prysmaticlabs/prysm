9ae8f2e0328c234b8831d984abbc56d456c29f3a
Terence Tsao <terence@prysmaticlabs.com>
2018-11-08 07:50:50 -0800

    bazel gazelle

--- shared/bytes/bytes.go
+++ shared/bytes/bytes.go
@@ -5,30 +5,35 @@ import (
 	"encoding/binary"
 )
 
+// Bytes1 returns integer x to bytes in big-endian format, x.to_bytes(1, 'big').
 func Bytes1(x uint64) []byte {
 	bytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(bytes, x)
 	return bytes[7:]
 }
 
+// Bytes2 returns integer x to bytes in big-endian format, x.to_bytes(2, 'big').
 func Bytes2(x uint64) []byte {
 	bytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(bytes, x)
 	return bytes[6:]
 }
 
+// Bytes3 returns integer x to bytes in big-endian format, x.to_bytes(3, 'big').
 func Bytes3(x uint64) []byte {
 	bytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(bytes, x)
 	return bytes[5:]
 }
 
+// Bytes4 returns integer x to bytes in big-endian format, x.to_bytes(4, 'big').
 func Bytes4(x uint64) []byte {
 	bytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(bytes, x)
 	return bytes[4:]
 }
 
+// Bytes8 returns integer x to bytes in big-endian format, x.to_bytes(8, 'big').
 func Bytes8(x uint64) []byte {
 	bytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(bytes, x)
