9b15d23bc2aee7c162177e7cbd78918be091dc5f
terence tsao <terence@prysmaticlabs.com>
2018-11-15 19:14:33 -0800

    Fixed broken reference link to eth2.0 spec (#784)
    * Fixed broken link
    
    * updated more broken links

--- shared/hashutil/hash.go
+++ shared/hashutil/hash.go
@@ -6,7 +6,7 @@ import (
 
 // Hash defines a function that returns the
 // blake2b hash of the data passed in.
-// https://github.com/ethereum/eth2.0-specs/blob/master/specs/beacon-chain.md#appendix-a---hash-function
+// https://github.com/ethereum/eth2.0-specs/blob/master/specs/core/0_beacon-chain.md#appendix
 func Hash(data []byte) [32]byte {
 	var hash [32]byte
 	h := blake2b.Sum512(data)
