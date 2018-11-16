a42bfd5fc6ff79a16332509d00c98e48b3cfd6db
nisdas <nishdas93@gmail.com>
2018-11-16 18:54:06 +0800

    new changes

--- beacon-chain/sync/sync-querier/service.go
+++ beacon-chain/sync/sync-querier/service.go
@@ -2,10 +2,9 @@ package syncquerier
 
 import (
 	"context"
-	"time"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/prysmaticlabs/prysm/beacon-chain/types"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/event"
 	"github.com/prysmaticlabs/prysm/shared/p2p"
 	"github.com/sirupsen/logrus"
@@ -17,7 +16,6 @@ var log = logrus.WithField("prefix", "syncQuerier")
 //
 type Config struct {
 	ResponseBufferSize int
-	BeaconDB           beaconDB
 	P2P                p2pAPI
 }
 
@@ -35,24 +33,20 @@ type p2pAPI interface {
 	Broadcast(msg proto.Message)
 }
 
-type beaconDB interface {
-	SaveBlock(*types.Block) error
-}
-
 // SyncQuerier defines the main class in this package.
 // See the package comments for a general description of the service's functions.
 type SyncQuerier struct {
-	ctx         context.Context
-	cancel      context.CancelFunc
-	p2p         p2pAPI
-	syncService syncService
-	db          beaconDB
-	responseBuf chan p2p.Message
+	ctx             context.Context
+	cancel          context.CancelFunc
+	p2p             p2pAPI
+	curentHeadSlot  uint64
+	currentHeadHash uint64
+	responseBuf     chan p2p.Message
 }
 
 // NewSyncQuerierService constructs a new Sync Querier Service.
 // This method is normally called by the main node.
-func NewInitialSyncService(ctx context.Context,
+func NewSyncQuerierService(ctx context.Context,
 	cfg Config,
 ) *SyncQuerier {
 	ctx, cancel := context.WithCancel(ctx)
@@ -63,29 +57,46 @@ func NewInitialSyncService(ctx context.Context,
 		ctx:         ctx,
 		cancel:      cancel,
 		p2p:         cfg.P2P,
-		db:          cfg.BeaconDB,
 		responseBuf: responseBuf,
 	}
 }
 
 // Start begins the goroutine.
-func (s *InitialSync) Start() {
-	if s.syncService.IsSyncedWithNetwork() {
-		// TODO(#661): Bail out of the sync service if the chain is only partially synced.
-		log.Info("Chain state detected, exiting initial sync")
-		return
-	}
-
-	go func() {
-		ticker := time.NewTicker(s.syncPollingInterval)
-		s.run(ticker.C)
-		ticker.Stop()
-	}()
+func (s *SyncQuerier) Start() {
+	go s.run()
 }
 
-// Stop kills the initial sync goroutine.
-func (s *InitialSync) Stop() error {
+// Stop kills the sync querier goroutine.
+func (s *SyncQuerier) Stop() error {
 	log.Info("Stopping service")
 	s.cancel()
 	return nil
+}
+
+func (s *SyncQuerier) run() {
+	responseSub := s.p2p.Subscribe(&pb.ChainHeadResponse{}, s.responseBuf)
+	defer func() {
+		responseSub.Unsubscribe()
+		close(s.responseBuf)
+	}()
+
+	s.RequestLatestHead()
+
+	for {
+		select {
+		case <-s.ctx.Done():
+			log.Debug("Exiting goroutine")
+			return
+		case msg := <-s.responseBuf:
+			response := msg.Data.(*pb.ChainHeadResponse)
+			log.Debugf("Latest Chain head is at slot: %d and hash %#x", response.Slot, response.Hash)
+
+			responseSub.Unsubscribe()
+		}
+	}
+}
+
+func (s *SyncQuerier) RequestLatestHead() {
+	request := &pb.ChainHeadRequest{}
+	s.p2p.Broadcast(request)
 }
