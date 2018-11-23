cfda851c92aaf5e3bc5cdea41a52af66cc419346
Raul Jordan <raul@prysmaticlabs.com>
2018-11-23 09:59:03 -0600

    begin state merge

--- beacon-chain/core/types/state.go
+++ beacon-chain/core/types/state.go
@@ -32,3 +32,43 @@ func (b *BeaconState) Hash() ([32]byte, error) {
 	}
 	return hashutil.Hash(data), nil
 }
+
+// ValidatorSetChangeSlot returns the slot of last time validator set changes.
+func (b *BeaconState) ValidatorSetChangeSlot() uint64 {
+	return b.data.ValidatorSetChangeSlot
+}
+
+// ShardAndCommitteesForSlots returns the shard committee object.
+func (b *BeaconState) ShardAndCommitteesForSlots() []*pb.ShardAndCommitteeArray {
+	return b.data.ShardAndCommitteesForSlots
+}
+
+// Crosslinks returns the cross link records of the all the shards.
+func (b *BeaconState) Crosslinks() []*pb.CrosslinkRecord {
+	return b.data.Crosslinks
+}
+
+// Validators returns list of validators.
+func (b *BeaconState) Validators() []*pb.ValidatorRecord {
+	return b.data.Validators
+}
+
+// LastStateRecalculationSlot returns when the last time crystallized state recalculated.
+func (b *BeaconState) LastStateRecalculationSlot() uint64 {
+	return b.data.LastStateRecalculationSlot
+}
+
+// LastFinalizedSlot returns the last finalized Slot of the beacon chain.
+func (b *BeaconState) LastFinalizedSlot() uint64 {
+	return b.data.LastFinalizedSlot
+}
+
+// LastJustifiedSlot return the last justified slot of the beacon chain.
+func (b *BeaconState) LastJustifiedSlot() uint64 {
+	return b.data.LastJustifiedSlot
+}
+
+// JustifiedStreak returns number of consecutive justified slots ending at head.
+func (b *BeaconState) JustifiedStreak() uint64 {
+	return b.data.JustifiedStreak
+}
