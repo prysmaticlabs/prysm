f061029db6e4aa871b842de23fc97186e6a72cfc
Raul Jordan <raul@prysmaticlabs.com>
2018-11-25 14:24:04 -0600

    yay it buildssssss

--- beacon-chain/utils/genesis_json.go
+++ beacon-chain/utils/genesis_json.go
@@ -19,10 +19,10 @@ func InitialValidatorsFromJSON(genesisJSONPath string) ([]*pb.ValidatorRecord, e
 		return nil, err
 	}
 
-	cState := &pb.CrystallizedState{}
-	if err := jsonpb.Unmarshal(f, cState); err != nil {
+	beaconState := &pb.BeaconState{}
+	if err := jsonpb.Unmarshal(f, beaconState); err != nil {
 		return nil, fmt.Errorf("error converting JSON to proto: %v", err)
 	}
 
-	return cState.Validators, nil
+	return beaconState.Validators, nil
 }
