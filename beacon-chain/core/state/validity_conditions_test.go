5e8e6ca2798d114f10a2d62c92b301e5148d0ffa
Raul Jordan <raul@prysmaticlabs.com>
2018-11-26 17:48:38 -0600

    block test

--- beacon-chain/core/state/validity_conditions_test.go
+++ beacon-chain/core/state/validity_conditions_test.go
@@ -3,7 +3,6 @@ package state
 import (
 	"testing"
 
-	"github.com/golang/protobuf/ptypes"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/types"
 	"github.com/prysmaticlabs/prysm/beacon-chain/utils"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
@@ -30,46 +29,6 @@ func (f *mockDB) ReadBlockVoteCache(blockHashes [][32]byte) (utils.BlockVoteCach
 
 func (f *mockDB) loadMockBlockVoteCache(blockVoteCache utils.BlockVoteCache) {
 	f.blockVoteCache = blockVoteCache
-}
-
-func TestGenesisBlock(t *testing.T) {
-	stateRoot := [32]byte{0}
-	b1 := types.NewGenesisBlock(stateRoot)
-	b2 := types.NewGenesisBlock(stateRoot)
-
-	// We ensure that initializing a proto timestamp from
-	// genesis time will lead to no error.
-	if _, err := ptypes.TimestampProto(params.BeaconConfig().GenesisTime); err != nil {
-		t.Errorf("could not create proto timestamp, expected no error: %v", err)
-	}
-
-	h1, err1 := b1.Hash()
-	h2, err2 := b2.Hash()
-	if err1 != nil || err2 != nil {
-		t.Fatalf("failed to hash genesis block: %v %v", err1, err2)
-	}
-
-	if h1 != h2 {
-		t.Fatalf("genesis block hash should be identical: %#x %#x", h1, h2)
-	}
-
-	if b1.AncestorHashes() == nil {
-		t.Fatal("genesis block missing ParentHash field")
-	}
-
-	if b1.StateRoot() != stateRoot {
-		t.Fatal("genesis block state root isn't initialized correctly")
-	}
-
-	b3 := types.NewBlock(nil)
-	h3, err3 := b3.Hash()
-	if err3 != nil {
-		t.Fatalf("failed to hash genesis block: %v", err3)
-	}
-
-	if h1 == h3 {
-		t.Fatalf("genesis block and new block should not have identical hash: %#x", h1)
-	}
 }
 
 func TestBlockValidity(t *testing.T) {
