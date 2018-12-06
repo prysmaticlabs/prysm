2c321ee8215cf71fbc2d07c41d335f9401069865
Nishant Das <nish1993@hotmail.com>
2018-12-07 01:01:51 +0800

    Change to Golang Crypto Library (#1054)
    * deprecate hashutil
    
    * Revert "deprecate hashutil"
    
    This reverts commit 6ee40f916c6fcf73fe372b980ba5645fe0456f9d.
    
    * changing to golang crypto lib
    
    * Add comment
    
    * add test

--- shared/hashutil/hash.go
+++ shared/hashutil/hash.go
@@ -1,7 +1,7 @@
 package hashutil
 
 import (
-	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"golang.org/x/crypto/sha3"
 )
 
 // Hash defines a function that returns the
@@ -10,7 +10,15 @@ import (
 func Hash(data []byte) [32]byte {
 	var hash [32]byte
 
-	h := sha3.Sum256(data)
-	copy(hash[:], h[:32])
+	h := sha3.NewLegacyKeccak256()
+
+	// The hash interface never returns an error, for that reason
+	// we are not handling the error below. For reference, it is
+	// stated here https://golang.org/pkg/hash/#Hash
+
+	// #nosec G104
+	h.Write(data)
+	h.Sum(hash[:0])
+
 	return hash
 }
