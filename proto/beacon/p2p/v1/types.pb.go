b2fc85872e4f1ff79c82e9394002e87a2c8140fe
Terence Tsao <terence@prysmaticlabs.com>
2018-12-09 08:11:31 -0800

    fixed import path

--- proto/beacon/p2p/v1/types.pb.go
+++ proto/beacon/p2p/v1/types.pb.go
@@ -7,7 +7,7 @@ import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 import timestamp "github.com/golang/protobuf/ptypes/timestamp"
-import common "proto/common"
+import common "github.com/prysmaticlabs/prysm/proto/common"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
