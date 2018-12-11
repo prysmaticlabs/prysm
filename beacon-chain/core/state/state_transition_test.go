8402a1dd61cc98c38b887b8000516e2b9f4dceb9
terence tsao <terence@prysmaticlabs.com>
2018-12-11 07:46:01 -0800

    Part 1 of Cleaning Up State Proto (#1070)

--- beacon-chain/core/state/state_transition_test.go
+++ beacon-chain/core/state/state_transition_test.go
@@ -141,7 +141,7 @@ func TestProcessLatestCrosslinks(t *testing.T) {
 	// Set up crosslink record for every shard.
 	var clRecords []*pb.CrosslinkRecord
 	for i := uint64(0); i < params.BeaconConfig().ShardCount; i++ {
-		clRecord := &pb.CrosslinkRecord{ShardBlockHash: []byte{'A'}, Slot: 1}
+		clRecord := &pb.CrosslinkRecord{ShardBlockHash32: []byte{'A'}, Slot: 1}
 		clRecords = append(clRecords, clRecord)
 	}
 
@@ -188,8 +188,8 @@ func TestProcessLatestCrosslinks(t *testing.T) {
 	if newLatestCrosslinks[1].Slot != params.BeaconConfig().CycleLength {
 		t.Errorf("Slot did not change for new cross link. Wanted: %d. Got: %d", params.BeaconConfig().CycleLength, newLatestCrosslinks[0].Slot)
 	}
-	if !bytes.Equal(newLatestCrosslinks[1].ShardBlockHash, []byte{'a'}) {
-		t.Errorf("ShardBlockHash did not change for new cross link. Wanted a. Got: %s", newLatestCrosslinks[0].ShardBlockHash)
+	if !bytes.Equal(newLatestCrosslinks[1].ShardBlockHash32, []byte{'a'}) {
+		t.Errorf("ShardBlockHash did not change for new cross link. Wanted a. Got: %s", newLatestCrosslinks[0].ShardBlockHash32)
 	}
 	//TODO(#538) Implement tests on balances of the validators in committee once big.Int is introduced.
 }
@@ -297,7 +297,7 @@ func TestPenalizedETH(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to initialize state: %v", err)
 	}
-	beaconState.SetDepositsPenalizedInPeriod([]uint64{100, 200, 300, 400, 500})
+	beaconState.SetLatestPenalizedExitBalances([]uint64{100, 200, 300, 400, 500})
 	beaconState.PenalizedETH(2)
 
 	tests := []struct {
