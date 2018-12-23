db8b4c928ce511aa7cfba1f9d28387367f5547f2
Terence Tsao <terence@prysmaticlabs.com>
2018-12-22 21:00:33 -0800

    lint

--- beacon-chain/core/epoch/epoch_processing_test.go
+++ beacon-chain/core/epoch/epoch_processing_test.go
@@ -97,7 +97,7 @@ func TestProcessReceipt(t *testing.T) {
 		CandidatePowReceiptRoots: []*pb.CandidatePoWReceiptRootRecord{
 			{VoteCount: 0, CandidatePowReceiptRootHash32: []byte{'A'}},
 			// CandidatePowReceiptRootHash32 ['B'] gets to process with sufficient vote count.
-			{VoteCount: requiredVoteCount / 2 + 1, CandidatePowReceiptRootHash32: []byte{'B'}},
+			{VoteCount: requiredVoteCount/2 + 1, CandidatePowReceiptRootHash32: []byte{'B'}},
 			{VoteCount: requiredVoteCount / 2, CandidatePowReceiptRootHash32: []byte{'C'}},
 		},
 	}
@@ -109,7 +109,7 @@ func TestProcessReceipt(t *testing.T) {
 
 	// Adding a new receipt root ['D'] which should be the new processed receipt root.
 	state.CandidatePowReceiptRoots = append(state.CandidatePowReceiptRoots,
-		&pb.CandidatePoWReceiptRootRecord{VoteCount:requiredVoteCount,
+		&pb.CandidatePoWReceiptRootRecord{VoteCount: requiredVoteCount,
 			CandidatePowReceiptRootHash32: []byte{'D'}})
 	newState = ProcessReceipt(state)
 	if !bytes.Equal(newState.ProcessedPowReceiptRootHash32, []byte{'D'}) {
@@ -129,20 +129,20 @@ func TestProcessJustification(t *testing.T) {
 	}
 
 	state := &pb.BeaconState{
-		Slot: 300,
-		JustifiedSlot: 200,
+		Slot:                  300,
+		JustifiedSlot:         200,
 		JustificationBitfield: 4,
 	}
-	newState := ProcessJustification(state, 1,1,1)
+	newState := ProcessJustification(state, 1, 1, 1)
 
 	if newState.PreviousJustifiedSlot != state.JustifiedSlot {
 		t.Errorf("New state's prev justified slot %d != old state's justified slot %d",
 			newState.PreviousJustifiedSlot, state.JustifiedSlot)
 	}
 	// Since this epoch was justified (not prev), justified_slot = state.slot - EPOCH_LENGTH.
-	if newState.JustifiedSlot != state.Slot - params.BeaconConfig().EpochLength{
+	if newState.JustifiedSlot != state.Slot-params.BeaconConfig().EpochLength {
 		t.Errorf("New state's justified slot %d != state's slot - EPOCH_LENGTH %d",
-			newState.JustifiedSlot, state.Slot - params.BeaconConfig().EpochLength)
+			newState.JustifiedSlot, state.Slot-params.BeaconConfig().EpochLength)
 	}
 	// The new JustificationBitfield is 11, it went from 0100 to 1011. Two 1's were appended because both
 	// prev epoch and this epoch were justified.
@@ -152,10 +152,10 @@ func TestProcessJustification(t *testing.T) {
 
 	// Assume for the case where only prev epoch got justified. Verify
 	// justified_slot = state.slot - 2 * EPOCH_LENGTH.
-	newState = ProcessJustification(state, 0,1,1)
-	if newState.JustifiedSlot != state.Slot - 2 * params.BeaconConfig().EpochLength{
+	newState = ProcessJustification(state, 0, 1, 1)
+	if newState.JustifiedSlot != state.Slot-2*params.BeaconConfig().EpochLength {
 		t.Errorf("New state's justified slot %d != state's slot - 2 * EPOCH_LENGTH %d",
-			newState.JustifiedSlot, state.Slot - params.BeaconConfig().EpochLength)
+			newState.JustifiedSlot, state.Slot-params.BeaconConfig().EpochLength)
 	}
 }
 
@@ -168,9 +168,9 @@ func TestProcessFinalization(t *testing.T) {
 	// 2 consecutive justified slot in a row,
 	// and previous justified slot is state slot - 2 * EPOCH_LENGTH.
 	state := &pb.BeaconState{
-		Slot: 200,
-		JustifiedSlot: 200 - epochLength,
-		PreviousJustifiedSlot: 200 - 2 * epochLength,
+		Slot:                  200,
+		JustifiedSlot:         200 - epochLength,
+		PreviousJustifiedSlot: 200 - 2*epochLength,
 		JustificationBitfield: 3,
 	}
 	newState := ProcessFinalization(state)
@@ -182,9 +182,9 @@ func TestProcessFinalization(t *testing.T) {
 	// 3 consecutive justified slot in a row.
 	// and previous justified slot is state slot - 3 * EPOCH_LENGTH.
 	state = &pb.BeaconState{
-		Slot: 300,
-		JustifiedSlot: 300 - epochLength,
-		PreviousJustifiedSlot: 300 - 3 * epochLength,
+		Slot:                  300,
+		JustifiedSlot:         300 - epochLength,
+		PreviousJustifiedSlot: 300 - 3*epochLength,
 		JustificationBitfield: 7,
 	}
 	newState = ProcessFinalization(state)
@@ -196,14 +196,27 @@ func TestProcessFinalization(t *testing.T) {
 	// 4 consecutive justified slot in a row.
 	// and previous justified slot is state slot - 3 * EPOCH_LENGTH.
 	state = &pb.BeaconState{
-		Slot: 400,
-		JustifiedSlot: 400 - epochLength,
-		PreviousJustifiedSlot: 400 - 4 * epochLength,
+		Slot:                  400,
+		JustifiedSlot:         400 - epochLength,
+		PreviousJustifiedSlot: 400 - 4*epochLength,
 		JustificationBitfield: 15,
 	}
 	newState = ProcessFinalization(state)
 	if newState.FinalizedSlot != state.JustifiedSlot {
 		t.Errorf("Wanted finalized slot to be %d, got %d:",
 			state.JustifiedSlot, newState.FinalizedSlot)
+	}
+
+	// if nothing gets finalized it just returns the same state.
+	state = &pb.BeaconState{
+		Slot:                  100,
+		JustifiedSlot:         65,
+		PreviousJustifiedSlot: 0,
+		JustificationBitfield: 1,
+	}
+	newState = ProcessFinalization(state)
+	if newState.FinalizedSlot != 0 {
+		t.Errorf("Wanted finalized slot to be %d, got %d:",
+			0, newState.FinalizedSlot)
 	}
 }
