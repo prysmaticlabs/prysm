a4caf09d98bbcfae5718d2c619d3a0007955d9d4
Terence Tsao <terence@prysmaticlabs.com>
2018-12-23 17:28:44 -0800

    fixed test

--- beacon-chain/core/balances/rewards_penalties_test.go
+++ beacon-chain/core/balances/rewards_penalties_test.go
@@ -20,12 +20,11 @@ func TestBaseRewardQuotient(t *testing.T) {
 		b uint64
 	}{
 		{0, 0},
-		{16384 * params.BeaconConfig().MaxDepositInGwei, 0},
-		{50000 * params.BeaconConfig().MaxDepositInGwei, 0},
-		{100000 * params.BeaconConfig().MaxDepositInGwei, 0},
-		{500000 * params.BeaconConfig().MaxDepositInGwei, 0},
-		{1000000 * params.BeaconConfig().MaxDepositInGwei, 0},
-		{2000000 * params.BeaconConfig().MaxDepositInGwei, 0},
+		{1e6 * params.BeaconConfig().Gwei, 1024000},  //1M ETH staked, 9.76% interest.
+		{2e6 * params.BeaconConfig().Gwei, 1447936},  //2M ETH staked, 6.91% interest.
+		{5e6 * params.BeaconConfig().Gwei, 2289664},  //5M ETH staked, 4.36% interest.
+		{10e6 * params.BeaconConfig().Gwei, 3237888}, // 10M ETH staked, 3.08% interest.
+		{20e6 * params.BeaconConfig().Gwei, 4579328}, // 20M ETH staked, 2.18% interest.
 	}
 	for _, tt := range tests {
 		b := BaseRewardQuotient(tt.a)
@@ -43,16 +42,17 @@ func TestBaseReward(t *testing.T) {
 		b uint64
 	}{
 		{0, 0},
-		{params.BeaconConfig().MinOnlineDepositSize * params.BeaconConfig().Gwei, 0},
-		{30 * 1e9, 0},
-		{params.BeaconConfig().MaxDepositInGwei, 0},
-		{40 * 1e9, 0},
+		{params.BeaconConfig().MinOnlineDepositSize * params.BeaconConfig().Gwei, 988},
+		{30 * 1e9, 1853},
+		{params.BeaconConfig().MaxDepositInGwei, 1976},
+		{40 * 1e9, 1976},
 	}
 	for _, tt := range tests {
 		state := &pb.BeaconState{
 			ValidatorBalances: []uint64{tt.a},
 		}
-		b := BaseReward(state, 0, params.BeaconConfig().BaseRewardQuotient)
+		// Assume 10M Eth staked (base reward quotient: 3237888).
+		b := BaseReward(state, 0, 3237888)
 		if b != tt.b {
 			t.Errorf("BaseReward(%d) = %d, want = %d",
 				tt.a, b, tt.b)
@@ -66,17 +66,18 @@ func TestInactivityPenalty(t *testing.T) {
 		a uint64
 		b uint64
 	}{
-		{1, 0},
-		{2, 0},
-		{5, 0},
-		{10, 0},
-		{50, 0},
+		{1, 2929},
+		{2, 3883},
+		{5, 6744},
+		{10, 11512},
+		{50, 49659},
 	}
 	for _, tt := range tests {
 		state := &pb.BeaconState{
 			ValidatorBalances: []uint64{params.BeaconConfig().MaxDepositInGwei},
 		}
-		b := InactivityPenalty(state, 0, params.BeaconConfig().BaseRewardQuotient, tt.a)
+		// Assume 10 ETH staked (base reward quotient: 3237888).
+		b := InactivityPenalty(state, 0, 3237888, tt.a)
 		if b != tt.b {
 			t.Errorf("InactivityPenalty(%d) = %d, want = %d",
 				tt.a, b, tt.b)
