67c024c6af616a364d85f95b1b67e56ad372a229
nisdas <nishdas93@gmail.com>
2018-12-28 14:07:44 +0800

    removing misc package

--- beacon-chain/core/blocks/block.go
+++ beacon-chain/core/blocks/block.go
@@ -2,6 +2,7 @@ package blocks
 
 import (
 	"fmt"
+	"math"
 	"time"
 
 	"github.com/gogo/protobuf/proto"
@@ -102,4 +103,31 @@ func ProcessBlockRoots(state *pb.BeaconState, prevBlockRoot [32]byte) *pb.Beacon
 	}
 
 	return state
+}
+
+// ForkVersion Spec:
+//	def get_fork_version(fork_data: ForkData,
+//                     slot: int) -> int:
+//    if slot < fork_data.fork_slot:
+//        return fork_data.pre_fork_version
+//    else:
+//        return fork_data.post_fork_version
+func ForkVersion(data *pb.ForkData, slot uint64) uint64 {
+	if slot < data.ForkSlot {
+		return data.PreForkVersion
+	}
+	return data.PostForkVersion
+}
+
+// DomainVersion Spec:
+//	def get_domain(fork_data: ForkData,
+//               slot: int,
+//               domain_type: int) -> int:
+//    return get_fork_version(
+//        fork_data,
+//        slot
+//    ) * 2**32 + domain_type
+func DomainVersion(data *pb.ForkData, slot uint64, domainType uint64) uint64 {
+	constant := uint64(math.Pow(2, 32))
+	return ForkVersion(data, slot)*constant + domainType
 }
