5a9764ae550de08a966fff661817c3237a0a013f
Preston Van Loon <preston@machinepowered.com>
2019-01-02 22:28:39 -0500

    lint

--- beacon-chain/core/state/stateutils/validator_index_map.go
+++ beacon-chain/core/state/stateutils/validator_index_map.go
@@ -2,6 +2,8 @@ package stateutils
 
 import pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 
+// ValidatorIndexMap builds a lookup map for quickly determining the index of
+// a validator by their public key.
 func ValidatorIndexMap(state *pb.BeaconState) map[[32]byte]int {
 	m := make(map[[32]byte]int)
 	for idx, record := range state.ValidatorRegistry {
@@ -12,6 +14,9 @@ func ValidatorIndexMap(state *pb.BeaconState) map[[32]byte]int {
 	return m
 }
 
+// BytesToBytes32 is a convenience method for converting a byte slice to a fix
+// sized 32 byte array. This method will truncate the input if it is larger
+// than 32 bytes.
 func BytesToBytes32(a []byte) [32]byte {
 	var b [32]byte
 	copy(b[:], a)
