f4b840a9a2d9d81f5006b4a73bb9300365a7a616
Kushagra Sharma <ksharm01@gmail.com>
2019-01-06 06:55:10 -0800

    Add script to generate pb.go files (#1254)
    * Add script to generate pb.go files
    
    * Fix script paths and bazel

---
+++ scripts/update-go-pbs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script to copy pb.go files from bazel build folder to appropriate location.
+# Bazel builds to bazel-bin/... folder, script copies them back to original folder where .proto is.
+
+bazel build //proto/...
+
+# Get locations of pb.go files.
+
+file_list=()
+while IFS= read -d $'\0' -r file ; do
+    file_list=("${file_list[@]}" "$file")
+done < <(find -L $(bazel info bazel-bin)/proto -type f -name "*pb.go" -print0)
+ 
+arraylength=${#file_list[@]}
+searchstring="prysm/"
+
+# Copy pb.go files from bazel-bin to original folder where .proto is.
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+  destination=${file_list[i]#*$searchstring}
+  cp -R -L "${file_list[i]}" "$destination"
+done
