55b4fdab528bfa48ee8f0cd27412170fa6c90529
Raul Jordan <raul@prysmaticlabs.com>
2019-01-10 20:45:46 +0800

    gazelle

---
+++ shared/trie/deposit_trie_test.go
@@ -0,0 +1,55 @@
+package trie
+
+import (
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
+)
+
+func TestDepositTrie_UpdateDepositTrie(t *testing.T) {
+	tests := []struct {
+		deposits [][]byte
+	}{
+		{
+			[][]byte{
+				{1, 2, 3, 4},
+				{5, 6, 7, 8},
+			},
+		},
+		{
+			[][]byte{
+				{0, 0, 0, 0},
+				{0, 0, 0, 0},
+			},
+		},
+	}
+	for _, tt := range tests {
+		d := NewDepositTrie()
+		d.UpdateDepositTrie(tt.deposits[0])
+		if d.depositCount != 1 {
+			t.Errorf("Expected deposit count to increase by 1, received %d", d.depositCount)
+		}
+		root := hashutil.Hash(tt.deposits[0])
+		for i := uint64(0); i < params.BeaconConfig().DepositContractTreeDepth; i++ {
+			root = hashutil.Hash(root[:])
+		}
+		if d.Root() != root {
+			t.Errorf("Expected root to equal %#x, received %#x", d.Root(), root)
+		}
+		d.UpdateDepositTrie(tt.deposits[1])
+		if d.depositCount != 2 {
+			t.Errorf("Expected deposit count to increase to 2, received %d", d.depositCount)
+		}
+		left := d.merkleHashes[2]
+		right := d.merkleHashes[3]
+		if right == [32]byte{} {
+			root = hashutil.Hash(left[:])
+		} else {
+			root = hashutil.Hash(append(left[:], right[:]...))
+		}
+		if d.Root() != root {
+			t.Errorf("Expected root to equal %#x, received %#x", d.Root(), root)
+		}
+	}
+}
