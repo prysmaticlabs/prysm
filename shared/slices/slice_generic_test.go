7c4ad6a20296f4a3dd71c040812e6dc6fe2c5ffd
ratikesh9 <ratikesh@koinex.in>
2019-01-13 10:19:52 +0530

    linter & test cases fix

--- shared/slices/slice_generic_test.go
+++ shared/slices/slice_generic_test.go
@@ -14,6 +14,7 @@ func TestGenericIntersection(t *testing.T) {
 		setB []uint32
 		out  []uint32
 	}{
+
 		{[]uint32{2, 3, 5}, []uint32{3}, []uint32{3}},
 		{[]uint32{2, 3, 5}, []uint32{3, 5}, []uint32{3, 5}},
 		{[]uint32{2, 3, 5}, []uint32{5, 3, 2}, []uint32{5, 3, 2}},
