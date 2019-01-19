0b9b2f31ab29c67a1677da117fcaeead94a44258
nisdas <nishdas93@gmail.com>
2019-01-19 11:20:07 +0800

    contract change

--- contracts/deposit-contract/depositContract.v.py
+++ contracts/deposit-contract/depositContract.v.py
@@ -1,7 +1,9 @@
-## compiled with v0.1.0-beta.6 ##
+## compiled with v0.1.0-beta.7 ##
 
+MIN_DEPOSIT: constant(uint256(wei)) = as_wei_value(1, "ether")
+MAX_DEPOSIT: constant(uint256(wei)) = as_wei_value(32, "ether")
 GWEI_PER_ETH: constant(uint256) = 1000000000  # 10**9
-CHAIN_START_FULL_DEPOSIT_THRESHOLD: constant(uint256) = 8  # The actual number is 2**14, but we are keeping it at 8 for the testnet.
+CHAIN_START_FULL_DEPOSIT_THRESHOLD: constant(uint256) = 8  # We have hardcoded, this value for the testnet. But on the mainnet it will be 2**14.
 DEPOSIT_CONTRACT_TREE_DEPTH: constant(uint256) = 32
 TWO_TO_POWER_OF_TREE_DEPTH: constant(uint256) = 4294967296  # 2**32
 SECONDS_PER_DAY: constant(uint256) = 86400
@@ -16,13 +18,13 @@ full_deposit_count: uint256
 @payable
 @public
 def deposit(deposit_input: bytes[2048]):
-    assert msg.value >= as_wei_value(1, "ether")
-    assert msg.value <= as_wei_value(32, "ether")
+    assert msg.value >= MIN_DEPOSIT
+    assert msg.value <= MAX_DEPOSIT
 
     index: uint256 = self.deposit_count + TWO_TO_POWER_OF_TREE_DEPTH
-    msg_gwei_bytes8: bytes[8] = slice(concat("", convert(msg.value / GWEI_PER_ETH, bytes32)), start=24, len=8)
-    timestamp_bytes8: bytes[8] = slice(concat("", convert(block.timestamp, bytes32)), start=24, len=8)
-    deposit_data: bytes[2064] = concat(msg_gwei_bytes8, timestamp_bytes8, deposit_input)
+    deposit_amount: bytes[8] = slice(concat("", convert(msg.value / GWEI_PER_ETH, bytes32)), start=24, len=8)
+    deposit_timestamp: bytes[8] = slice(concat("", convert(block.timestamp, bytes32)), start=24, len=8)
+    deposit_data: bytes[2064] = concat(deposit_amount, deposit_timestamp, deposit_input)
     merkle_tree_index: bytes[8] = slice(concat("", convert(index, bytes32)), start=24, len=8)
 
     log.Deposit(self.deposit_tree[1], deposit_data, merkle_tree_index)
@@ -34,12 +36,12 @@ def deposit(deposit_input: bytes[2048]):
         self.deposit_tree[index] = sha3(concat(self.deposit_tree[index * 2], self.deposit_tree[index * 2 + 1]))
 
     self.deposit_count += 1
-    if msg.value == as_wei_value(32, "ether"):
+    if msg.value == MAX_DEPOSIT:
         self.full_deposit_count += 1
         if self.full_deposit_count == CHAIN_START_FULL_DEPOSIT_THRESHOLD:
             timestamp_day_boundary: uint256 = as_unitless_number(block.timestamp) - as_unitless_number(block.timestamp) % SECONDS_PER_DAY + SECONDS_PER_DAY
-            timestamp_day_boundary_bytes8: bytes[8] = slice(concat("", convert(timestamp_day_boundary, bytes32)), start=24, len=8)
-            log.ChainStart(self.deposit_tree[1], timestamp_day_boundary_bytes8)
+            chainstart_time: bytes[8] = slice(concat("", convert(timestamp_day_boundary, bytes32)), start=24, len=8)
+            log.ChainStart(self.deposit_tree[1], chainstart_time)
 
 @public
 @constant
