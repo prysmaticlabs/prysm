bb4814f7f089ef2766c18f461750e0393a506dbd
thatkawaiiguy <ivanthegreat15@gmail.com>
2019-01-27 21:27:29 -0500

    Remove method usage

--- beacon-chain/core/blocks/block_operations.go
+++ beacon-chain/core/blocks/block_operations.go
@@ -100,8 +100,8 @@ func ProcessEth1Data(beaconState *pb.BeaconState, block *pb.BeaconBlock) (*pb.Be
 	var eth1DataVoteAdded bool
 
 	for _, Eth1DataVote := range beaconState.Eth1DataVotes {
-		if bytes.Equal(Eth1DataVote.Eth1Data.BlockHash32, block.Eth1Data.GetBlockHash32()) {
-			if bytes.Equal(Eth1DataVote.Eth1Data.DepositRootHash32, block.Eth1Data.GetDepositRootHash32()) {
+		if bytes.Equal(Eth1DataVote.Eth1Data.BlockHash32, block.Eth1Data.BlockHash32()) {
+			if bytes.Equal(Eth1DataVote.Eth1Data.DepositRootHash32, block.Eth1Data.DepositRootHash32) {
 				Eth1DataVote.VoteCount++
 				eth1DataVoteAdded = true
 				break
