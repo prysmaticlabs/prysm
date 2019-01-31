a3cc807b7f1c9450dd0ddd8c7508bb84a58065bc
wildmolasses <changes@gmail.com>
2019-01-31 15:34:51 -0800

    update-mockgen.sh: use for loop

--- scripts/update-mockgen.sh
+++ scripts/update-mockgen.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # Script to update mock files after proto/beacon/rpc/v1/services.proto changes.
+# Use a space to separate mock destination from its interfaces.
 
-mockgen -package=internal -destination=./validator/internal/attester_service_mock.go github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1 AttesterServiceClient
-mockgen -package=internal -destination=./validator/internal/beacon_service_mock.go github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1 BeaconServiceClient,BeaconService_LatestAttestationClient,BeaconService_WaitForChainStartClient
-mockgen -package=internal -destination=./validator/internal/proposer_service_mock.go github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1 ProposerServiceClient
-mockgen -package=internal -destination=./validator/internal/validator_service_mock.go github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1 ValidatorServiceClient
+mocks=("./validator/internal/attester_service_mock.go AttesterServiceClient"
+       "./validator/internal/beacon_service_mock.go BeaconServiceClient,BeaconService_LatestAttestationClient,BeaconService_WaitForChainStartClient"
+       "./validator/internal/proposer_service_mock.go ProposerServiceClient"
+       "./validator/internal/validator_service_mock.go ValidatorServiceClient")
+
+for mock in ${mocks[@]}; do
+    file=${mock% *};
+    interfaces=${mock#* };
+    echo "generating $file for interfaces: $interfaces";
+    mockgen -package=internal -destination=$file github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1 $val
+done
