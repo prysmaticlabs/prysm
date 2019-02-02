3c83b43cd1a8c8e55d029f67b49c1a96aaf66f9b
Preston Van Loon <preston@prysmaticlabs.com>
2019-02-02 17:14:07 -0500

    broadcaster comment

--- shared/p2p/interfaces.go
+++ shared/p2p/interfaces.go
@@ -4,6 +4,9 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
+// Broadcaster represents a subset of the p2p.Server. This interface is useful
+// for testing or when the calling code only needs access to the broadcast
+// method.
 type Broadcaster interface {
 	Broadcast(proto.Message)
 }
