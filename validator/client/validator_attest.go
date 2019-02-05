04e78fc9238bbb6cc4b6b078b672e8799194d20c
Raul Jordan <raul@prysmaticlabs.com>
2019-02-04 22:35:52 -0600

    finish entire attester client logic

--- validator/client/validator_attest.go
+++ validator/client/validator_attest.go
@@ -2,18 +2,16 @@ package client
 
 import (
 	"context"
-	ptypes "github.com/gogo/protobuf/types"
 	"github.com/prysmaticlabs/prysm/shared/params"
 
 	"github.com/opentracing/opentracing-go"
 	pbp2p "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
-	"github.com/prysmaticlabs/prysm/shared/ssz"
 )
 
 // AttestToBlockHead
 //
-// WIP - not done. TODO: needs to attest at slot + 0.5*SLOT_DURATION.
+// WIP - not done.
 func (v *validator) AttestToBlockHead(ctx context.Context, slot uint64) {
 	span, ctx := opentracing.StartSpanFromContext(ctx, "validator.AttestToBlockHead")
 	defer span.Finish()
@@ -37,34 +35,75 @@ func (v *validator) AttestToBlockHead(ctx context.Context, slot uint64) {
 
 	// Fetch other necessary information from the beacon node in order to attest
 	// including the justified epoch, epoch boundary information, and more.
-	req := &pb.AttestationInfoRequest{
+	infoReq := &pb.AttestationInfoRequest{
 		Slot: slot,
 	}
-	resp, err := v.attesterClient.AttestationInfoAtSlot(ctx, req)
+	infoRes, err := v.attesterClient.AttestationInfoAtSlot(ctx, infoReq)
 	if err != nil {
 		log.Errorf("Could not fetch necessary info to produce attestation at slot %d: %v", err)
 		return
 	}
 	// Set the attestation data's beacon block root = hash_tree_root(head) where head
 	// is the validator's view of the head block of the beacon chain during the slot.
-	attData.BeaconBlockRootHash32 = resp.BeaconBlockRootHash32[:]
+	attData.BeaconBlockRootHash32 = infoRes.BeaconBlockRootHash32[:]
 	// Set the attestation data's epoch boundary root = hash_tree_root(epoch_boundary)
 	// where epoch_boundary is the block at the most recent epoch boundary in the
 	// chain defined by head -- i.e. the BeaconBlock where block.slot == get_epoch_start_slot(head.slot).
 	// On the server side, this is fetched by calling get_block_root(state, get_epoch_start_slot(head.slot)).
-	attData.EpochBoundaryRootHash32 = resp.EpochBoundaryRootHash32[:]
+	attData.EpochBoundaryRootHash32 = infoRes.EpochBoundaryRootHash32[:]
 	// Set the attestation data's justified epoch = state.justified_epoch where state
 	// is the beacon state at the head.
-	attData.JustifiedSlot = resp.JustifiedEpoch
+	attData.JustifiedSlot = infoRes.JustifiedEpoch
 	// Set the attestation data's justified block root = hash_tree_root(justified_block) where
 	// justified_block is the block at state.justified_epoch in the chain defined by head.
 	// On the server side, this is fetched by calling get_block_root(state, justified_epoch).
-	attData.JustifiedBlockRootHash32 = resp.JustifiedBlockRootHash32[:]
+	attData.JustifiedBlockRootHash32 = infoRes.JustifiedBlockRootHash32[:]
 
 	// The validator now creates an Attestation object using the AttestationData as
 	// set in the code above after all properties have been set.
 	attestation := &pbp2p.Attestation{
 		Data: attData,
 	}
-	_ = attestation
+
+	// We set the custody bitfield to an slice of zero values as a stub for phase 0
+	// of length len(committee)+7 // 8.
+	attestation.CustodyBitfield = make([]byte, (len(resp.Committee)+7)/8)
+
+	// We set the aggregation bitfield for the attestation.
+	idxReq := &pb.ValidatorIndexRequest{
+		PublicKey: v.pubKey,
+	}
+	// We fetch the validator index as it is necessary to generate the aggregation
+	// bitfield of the attestation itself.
+	validatorIndexRes, err := v.validatorClient.ValidatorIndex(ctx, idxReq)
+	if err != nil {
+		log.Errorf("Could not fetch validator index: %v", err)
+		return
+	}
+	// We set the attestation's aggregation bitfield by determining the index in the committee
+	// corresponding to the validator and modifying the bitfield itself.
+	aggregationBitfield := make([]byte, (len(resp.Committee)+7)/8)
+    var indexIntoCommittee uint
+	for i, validator := range resp.Committee {
+		if validator == validatorIndexRes.Index {
+			indexIntoCommittee = uint(i)
+		}
+	}
+	aggregationBitfield[indexIntoCommittee/8] |= 1<<(indexIntoCommittee %8)
+	// Note: calling get_attestation_participants(state, attestation.data, attestation.aggregation_bitfield)
+	// should return a list of length equal to 1, containing validator_index.
+	attestation.AggregationBitfield = aggregationBitfield
+
+	// TODO(#1366): Use BLS to generate an aggregate signature.
+	attestation.AggregateSignature = []byte("signed")
+
+	attestReq := &pb.AttestRequest{
+		Attestation: attestation,
+	}
+	attestRes, err := v.attesterClient.AttestHead(ctx, attestReq)
+	if err != nil {
+		log.Errorf("Could not submit attestation to beacon node: %v", err)
+		return
+	}
+    log.Infof("Submitted attestation successfully with hash %#x", attestRes.AttestationHash)
 }
