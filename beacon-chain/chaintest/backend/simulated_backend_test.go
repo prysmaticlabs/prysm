74a1f36c419215ffe06ebb98ccf41f4b9d019c4f
nisdas <nishdas93@gmail.com>
2019-02-05 16:06:34 +0800

    adding test

--- beacon-chain/chaintest/backend/simulated_backend_test.go
+++ beacon-chain/chaintest/backend/simulated_backend_test.go
@@ -45,3 +45,33 @@ func TestGenerateBlocks(t *testing.T) {
 	}
 
 }
+
+func TestGenerateNilBlocks(t *testing.T) {
+	backend, err := NewSimulatedBackend()
+	if err != nil {
+		t.Fatalf("Could not create a new simulated backedn %v", err)
+	}
+
+	if err := backend.InitializeChain(); err != nil {
+		t.Fatalf("Could not initialize simulated backend %v", err)
+	}
+
+	slotLimit := 100
+
+	for i := 0; i < slotLimit; i++ {
+		if err := backend.GenerateNilBlockAndAdvanceChain(); err != nil {
+			t.Fatalf("Could not generate block and transition state successfully %v for slot %d", err, backend.state.Slot+1)
+		}
+	}
+
+	for _, block := range backend.inMemoryBlocks {
+		if block != nil {
+			t.Errorf("There are in memory blocks saved despit only nil blocks being provided %v", block)
+		}
+	}
+
+	if backend.state.Slot != uint64(slotLimit) {
+		t.Errorf("Unequal state slot and expected slot %d %d", backend.state.Slot, slotLimit)
+	}
+
+}
