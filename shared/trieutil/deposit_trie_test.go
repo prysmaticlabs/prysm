1058de67d3d8b0a75dda01c07f54fc6f97c63dbe
nisdas <nishdas93@gmail.com>
2019-02-07 15:10:18 +0800

    fix test

--- shared/trieutil/deposit_trie_test.go
+++ shared/trieutil/deposit_trie_test.go
@@ -2,6 +2,8 @@ package trieutil
 
 import (
 	"testing"
+
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
 )
 
 func TestDepositTrie_UpdateDepositTrie(t *testing.T) {
@@ -27,9 +29,31 @@ func TestDepositTrie_UpdateDepositTrie(t *testing.T) {
 		if d.depositCount != 1 {
 			t.Errorf("Expected deposit count to increase by 1, received %d", d.depositCount)
 		}
+
+		hashedData := hashutil.Hash(tt.deposits[0])
+
+		for i := 0; i < 32; i++ {
+			hashedData = hashutil.Hash(append(hashedData[:], d.zeroHashes[i][:]...))
+		}
+		if d.Root() != hashedData {
+			t.Errorf("Expected %#x but got %#x", hashedData, d.Root())
+		}
+
 		d.UpdateDepositTrie(tt.deposits[1])
 		if d.depositCount != 2 {
 			t.Errorf("Expected deposit count to increase by 1, received %d", d.depositCount)
+		}
+
+		hash1 := hashutil.Hash(tt.deposits[0])
+		hash2 := hashutil.Hash(tt.deposits[1])
+
+		hashedData = hashutil.Hash(append(hash1[:], hash2[:]...))
+
+		for i := 0; i < 31; i++ {
+			hashedData = hashutil.Hash(append(hashedData[:], d.zeroHashes[i+1][:]...))
+		}
+		if d.Root() != hashedData {
+			t.Errorf("Expected %#x but got %#x", hashedData, d.Root())
 		}
 
 	}
