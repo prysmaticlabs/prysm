3d8bb73f150d7605d4836b815e855937e727cde2
Raul Jordan <raul@prysmaticlabs.com>
2019-02-11 10:15:25 -0600

    Attester RPC Server Functions (#1505)
    * initial validator attesthead rewrite based on proposer rewrite
    
    * proceed with fetching committees and tree hashing the canonical head at assigned attester slot
    
    * complete filling the properties of attestation data and all associated root hashes
    
    * add when to attest todo
    
    * finish entire attester client logic
    
    * tests with mocks checked in
    
    * tests passing in client
    
    * stubbed out server implementation
    
    * fixed build due to old property
    
    * regen mocks with new mockgen version
    
    * fixed broken tests
    
    * complete bazel build fix
    
    * address some review comments
    
    * deep proto test
    
    * server implementation logic
    
    * attestation info tests
    
    * completed passing tests, tree hash still blocked by error with nil fields unfortunately
    
    * fix todo failure
    
    * gazelle rerun and master merge
    
    * testing full behavior of attestation info server
    
    * finished all required server tests and implementation
    
    * break loop
    
    * fix tests due to genesis slot updates

--- beacon-chain/rpc/validator_server_test.go
+++ beacon-chain/rpc/validator_server_test.go
@@ -3,6 +3,7 @@ package rpc
 import (
 	"context"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -14,7 +15,7 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
-func TestValidatorIndex(t *testing.T) {
+func TestValidatorIndex_Ok(t *testing.T) {
 	db := internal.SetupDB(t)
 	defer internal.TeardownDB(t, db)
 
@@ -56,7 +57,7 @@ func TestValidatorIndex(t *testing.T) {
 	}
 }
 
-func TestValidatorEpochAssignments(t *testing.T) {
+func TestValidatorEpochAssignments_Ok(t *testing.T) {
 	db := internal.SetupDB(t)
 	defer internal.TeardownDB(t, db)
 
@@ -121,5 +122,101 @@ func TestValidatorEpochAssignments(t *testing.T) {
 			req.PublicKey,
 			res.Assignment.AttesterSlot,
 		)
+	}
+}
+
+func TestValidatorCommitteeAtSlot_CrosslinkCommitteesFailure(t *testing.T) {
+	db := internal.SetupDB(t)
+	defer internal.TeardownDB(t, db)
+	genesis := b.NewGenesisBlock([]byte{})
+	if err := db.SaveBlock(genesis); err != nil {
+		t.Fatalf("Could not save genesis block: %v", err)
+	}
+
+	genesisTime := params.BeaconConfig().GenesisTime.Unix()
+	deposits := make([]*pbp2p.Deposit, params.BeaconConfig().DepositsForChainStart)
+	for i := 0; i < len(deposits); i++ {
+		var pubKey [48]byte
+		copy(pubKey[:], []byte(strconv.Itoa(i)))
+		depositInput := &pbp2p.DepositInput{
+			Pubkey: pubKey[:],
+		}
+		depositData, err := b.EncodeDepositData(
+			depositInput,
+			params.BeaconConfig().MaxDepositAmount,
+			genesisTime,
+		)
+		if err != nil {
+			t.Fatalf("Could not encode initial block deposits: %v", err)
+		}
+		deposits[i] = &pbp2p.Deposit{DepositData: depositData}
+	}
+	beaconState, err := state.InitialBeaconState(deposits, uint64(genesisTime), nil)
+	if err != nil {
+		t.Fatalf("Could not instantiate initial state: %v", err)
+	}
+
+	if err := db.UpdateChainHead(genesis, beaconState); err != nil {
+		t.Fatalf("Could not save genesis state: %v", err)
+	}
+	validatorServer := &ValidatorServer{
+		beaconDB: db,
+	}
+	req := &pb.CommitteeRequest{
+		Slot: params.BeaconConfig().EpochLength * 10,
+	}
+	want := "could not get crosslink committees at slot"
+	if _, err := validatorServer.ValidatorCommitteeAtSlot(context.Background(), req); !strings.Contains(err.Error(), want) {
+		t.Errorf("Expected %v, received %v", want, err)
+	}
+}
+
+func TestValidatorCommitteeAtSlot_Ok(t *testing.T) {
+	db := internal.SetupDB(t)
+	defer internal.TeardownDB(t, db)
+	genesis := b.NewGenesisBlock([]byte{})
+	if err := db.SaveBlock(genesis); err != nil {
+		t.Fatalf("Could not save genesis block: %v", err)
+	}
+
+	genesisTime := params.BeaconConfig().GenesisTime.Unix()
+	deposits := make([]*pbp2p.Deposit, params.BeaconConfig().DepositsForChainStart)
+	for i := 0; i < len(deposits); i++ {
+		var pubKey [48]byte
+		copy(pubKey[:], []byte(strconv.Itoa(i)))
+		depositInput := &pbp2p.DepositInput{
+			Pubkey: pubKey[:],
+		}
+		depositData, err := b.EncodeDepositData(
+			depositInput,
+			params.BeaconConfig().MaxDepositAmount,
+			genesisTime,
+		)
+		if err != nil {
+			t.Fatalf("Could not encode initial block deposits: %v", err)
+		}
+		deposits[i] = &pbp2p.Deposit{DepositData: depositData}
+	}
+	beaconState, err := state.InitialBeaconState(deposits, uint64(genesisTime), nil)
+	if err != nil {
+		t.Fatalf("Could not instantiate initial state: %v", err)
+	}
+
+	if err := db.UpdateChainHead(genesis, beaconState); err != nil {
+		t.Fatalf("Could not save genesis state: %v", err)
+	}
+	validatorServer := &ValidatorServer{
+		beaconDB: db,
+	}
+	req := &pb.CommitteeRequest{
+		Slot:           params.BeaconConfig().GenesisSlot + 1,
+		ValidatorIndex: 31,
+	}
+	res, err := validatorServer.ValidatorCommitteeAtSlot(context.Background(), req)
+	if err != nil {
+		t.Fatalf("Unable to fetch committee at slot: %v", err)
+	}
+	if res.Shard != 0 {
+		t.Errorf("Shard for validator at index 31 should be 2, received %d", res.Shard)
 	}
 }
--- validator/internal/attester_service_mock.go
+++ validator/internal/attester_service_mock.go
@@ -75,23 +75,3 @@ func (mr *MockAttesterServiceClientMockRecorder) AttestationInfoAtSlot(arg0, arg
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AttestationInfoAtSlot", reflect.TypeOf((*MockAttesterServiceClient)(nil).AttestationInfoAtSlot), varargs...)
 }
-
-// CrosslinkCommitteesAtSlot mocks base method
-func (m *MockAttesterServiceClient) CrosslinkCommitteesAtSlot(arg0 context.Context, arg1 *v10.CrosslinkCommitteeRequest, arg2 ...grpc.CallOption) (*v10.CrosslinkCommitteeResponse, error) {
-	m.ctrl.T.Helper()
-	varargs := []interface{}{arg0, arg1}
-	for _, a := range arg2 {
-		varargs = append(varargs, a)
-	}
-	ret := m.ctrl.Call(m, "CrosslinkCommitteesAtSlot", varargs...)
-	ret0, _ := ret[0].(*v10.CrosslinkCommitteeResponse)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// CrosslinkCommitteesAtSlot indicates an expected call of CrosslinkCommitteesAtSlot
-func (mr *MockAttesterServiceClientMockRecorder) CrosslinkCommitteesAtSlot(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	varargs := append([]interface{}{arg0, arg1}, arg2...)
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CrosslinkCommitteesAtSlot", reflect.TypeOf((*MockAttesterServiceClient)(nil).CrosslinkCommitteesAtSlot), varargs...)
-}
