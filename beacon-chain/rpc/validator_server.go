f707f14cb2caf3ec005a68aa7379d77116c116e2
Raul Jordan <raul@prysmaticlabs.com>
2019-02-13 12:08:25 -0600

    Change BLS Public Key Length Check in Beacon RPC Server (#1571)

--- beacon-chain/rpc/validator_server.go
+++ beacon-chain/rpc/validator_server.go
@@ -43,8 +43,12 @@ func (vs *ValidatorServer) ValidatorEpochAssignments(
 	ctx context.Context,
 	req *pb.ValidatorEpochAssignmentsRequest,
 ) (*pb.ValidatorEpochAssignmentsResponse, error) {
-	if len(req.PublicKey) != 48 {
-		return nil, fmt.Errorf("expected 48 byte public key, received %d", len(req.PublicKey))
+	if len(req.PublicKey) != params.BeaconConfig().BLSPubkeyLength {
+		return nil, fmt.Errorf(
+			"expected public key to have length %d, received %d",
+			params.BeaconConfig().BLSPubkeyLength,
+			len(req.PublicKey),
+		)
 	}
 	beaconState, err := vs.beaconDB.State()
 	if err != nil {
