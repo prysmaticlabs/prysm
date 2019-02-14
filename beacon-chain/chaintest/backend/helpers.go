5671efe2ab082946a8f0431c2c126d519ac93694
Raul Jordan <raul@prysmaticlabs.com>
2019-02-14 16:12:33 -0600

    cleaned up conf

--- beacon-chain/chaintest/backend/helpers.go
+++ beacon-chain/chaintest/backend/helpers.go
@@ -118,7 +118,7 @@ func generateSimulatedBlock(
 // Generates initial deposits for creating a beacon state in the simulated
 // backend based on the yaml configuration.
 func generateInitialSimulatedDeposits(numDeposits uint64) ([]*pb.Deposit, error) {
-	genesisTime := time.Unix(0, 0).Unix()
+	genesisTime := time.Date(2018, 9, 0, 0, 0, 0, 0, time.UTC).Unix()
 	deposits := make([]*pb.Deposit, numDeposits)
 	for i := 0; i < len(deposits); i++ {
 		depositInput := &pb.DepositInput{
