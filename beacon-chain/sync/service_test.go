2f69174134fadd3b9816ce7f461fdef0a10a89ed
nisdas <nishdas93@gmail.com>
2019-02-14 19:46:39 +0800

    clean up test

--- beacon-chain/sync/service_test.go
+++ beacon-chain/sync/service_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 	"github.com/prysmaticlabs/prysm/beacon-chain/internal"
-	initialsync "github.com/prysmaticlabs/prysm/beacon-chain/sync/initial-sync"
 )
 
 func NotSyncQuerierConfig() *QuerierConfig {
@@ -24,30 +23,16 @@ func initializeTestSyncService(ctx context.Context, cfg *Config, synced bool) *S
 	} else {
 		sqCfg = NotSyncQuerierConfig()
 	}
+
+	services := NewSyncService(ctx, cfg)
+
 	sqCfg.BeaconDB = cfg.BeaconDB
 	sqCfg.P2P = cfg.P2P
-
-	isCfg := initialsync.DefaultConfig()
-	isCfg.BeaconDB = cfg.BeaconDB
-	isCfg.P2P = cfg.P2P
-
-	rsCfg := DefaultRegularSyncConfig()
-	rsCfg.ChainService = cfg.ChainService
-	rsCfg.BeaconDB = cfg.BeaconDB
-	rsCfg.P2P = cfg.P2P
-
 	sq := NewQuerierService(ctx, sqCfg)
-	rs := NewRegularSyncService(ctx, rsCfg)
 
-	isCfg.SyncService = rs
-	is := initialsync.NewInitialSyncService(ctx, isCfg)
+	services.Querier = sq
 
-	return &Service{
-		RegularSync: rs,
-		InitialSync: is,
-		Querier:     sq,
-	}
-
+	return services
 }
 
 func setupTestSyncService(t *testing.T, synced bool) (*Service, *db.BeaconDB) {
