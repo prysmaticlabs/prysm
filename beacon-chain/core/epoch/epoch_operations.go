de4c8667fd53ddbfe29bd25916395f2206b1e5c8
terence tsao <terence@prysmaticlabs.com>
2019-02-15 15:19:36 -0800

    Fixed Get Block Root (#1616)
    * fixed epoch_processing
    
    * test p2p
    
    * fixed get block root and tests
    
    * in test nightmare, yay
    
    * fixed all the tests
    
    * lint

--- beacon-chain/core/epoch/epoch_operations.go
+++ beacon-chain/core/epoch/epoch_operations.go
@@ -47,11 +47,10 @@ func CurrentBoundaryAttestations(
 	state *pb.BeaconState,
 	currentEpochAttestations []*pb.PendingAttestation,
 ) ([]*pb.PendingAttestation, error) {
-	var boundarySlot uint64
 	var boundaryAttestations []*pb.PendingAttestation
 
 	for _, attestation := range currentEpochAttestations {
-		boundaryBlockRoot, err := block.BlockRoot(state, boundarySlot)
+		boundaryBlockRoot, err := block.BlockRoot(state, helpers.StartSlot(helpers.CurrentEpoch(state)))
 		if err != nil {
 			return nil, err
 		}
@@ -59,6 +58,7 @@ func CurrentBoundaryAttestations(
 		attestationData := attestation.Data
 		sameRoot := bytes.Equal(attestationData.JustifiedBlockRootHash32, boundaryBlockRoot)
 		sameEpoch := attestation.Data.JustifiedEpoch == state.JustifiedEpoch
+
 		if sameRoot && sameEpoch {
 			boundaryAttestations = append(boundaryAttestations, attestation)
 		}
