968574b0b2eec022677ee9d5ec4a0c75459807a8
Raul Jordan <raul@prysmaticlabs.com>
2019-02-18 23:32:42 -0600

    len

--- shared/bls/bls.go
+++ shared/bls/bls.go
@@ -38,7 +38,7 @@ func RandKey(r io.Reader) (*SecretKey, error) {
 // SecretKeyFromBytes creates a BLS private key from a byte slice.
 func SecretKeyFromBytes(priv []byte) (*SecretKey, error) {
 	if len(priv) != 32 {
-		return nil, fmt.Errorf("Expected byte slice of length 32, received %d", len(priv))
+		return nil, fmt.Errorf("Expected byte slice of length 32, received: %d", len(priv))
 	}
 	k := bytesutil.ToBytes32(priv)
 	return &SecretKey{val: gobls.DeserializeSecretKey(k)}, nil
