4a06ea8b288712a737ab12f715e53ee99313b67d
Preston Van Loon <preston@prysmaticlabs.com>
2019-02-18 23:50:17 -0500

    remove a few references that tie the validator binary to the go-ethereum crypto library

---
+++ shared/keystore/keccak256.go
@@ -0,0 +1,12 @@
+package keystore
+
+import "golang.org/x/crypto/sha3"
+
+// Keccak256 calculates and returns the Keccak256 hash of the input data.
+func Keccak256(data ...[]byte) []byte {
+	d := sha3.NewLegacyKeccak256()
+	for _, b := range data {
+		d.Write(b)
+	}
+	return d.Sum(nil)
+}
