8242ce9000b54278caf105fe228c1df57f23ae6b
Preston Van Loon <preston@prysmaticlabs.com>
2019-02-19 00:32:01 -0500

    nosec on this crypto library

--- shared/keystore/keccak256.go
+++ shared/keystore/keccak256.go
@@ -6,6 +6,7 @@ import "golang.org/x/crypto/sha3"
 func Keccak256(data ...[]byte) []byte {
 	d := sha3.NewLegacyKeccak256()
 	for _, b := range data {
+		// #nosec G104
 		d.Write(b)
 	}
 	return d.Sum(nil)
