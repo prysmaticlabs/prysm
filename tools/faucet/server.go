9f405e1023c4a9a938f01022efb320381f6cc0f8
Preston Van Loon <preston@prysmaticlabs.com>
2019-02-19 19:36:23 -0500

    second pass at faucet, no rate limiting yet

--- tools/faucet/server.go
+++ tools/faucet/server.go
@@ -2,18 +2,58 @@ package main
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"errors"
 	"fmt"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/params"
 	recaptcha "github.com/prestonvanloon/go-recaptcha"
 	faucetpb "github.com/prysmaticlabs/prysm/proto/faucet"
 	"google.golang.org/grpc/peer"
 )
 
 var minScore = 0.5
+var fundingAmount = big.NewInt(0.5 * params.Ether)
 
 type faucetServer struct {
-	r recaptcha.Recaptcha
+	r      recaptcha.Recaptcha
+	client *ethclient.Client
+	funder common.Address
+	pk     *ecdsa.PrivateKey
+}
+
+func NewFaucetServer(r recaptcha.Recaptcha, rpcPath, privateKey string) *faucetServer {
+	client, err := ethclient.DialContext(context.Background(), rpcPath)
+	if err != nil {
+		panic(err)
+	}
+
+	pk, err := crypto.HexToECDSA(privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	funder := crypto.PubkeyToAddress(pk.PublicKey)
+
+	bal, err := client.BalanceAt(context.Background(), funder, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Funder is %s\n", funder.Hex())
+	fmt.Printf("Funder has %d\n", bal)
+
+	return &faucetServer{
+		r,
+		client,
+		funder,
+		pk,
+	}
 }
 
 // RequestFunds from the ethereum 1.x faucet. Requires a valid captcha
@@ -37,8 +77,34 @@ func (s *faucetServer) RequestFunds(ctx context.Context, req *faucetpb.FundingRe
 		return &faucetpb.FundingResponse{Error: "Recaptcha score too low"}, nil
 	}
 
+	txHash, err := s.fundAndWait(common.HexToAddress(req.WalletAddress))
+	if err != nil {
+		return &faucetpb.FundingResponse{Error: fmt.Sprintf("Failed to send transaction %v", err)}, nil
+	}
+
 	return &faucetpb.FundingResponse{
-		Amount:          "500000000000000000",
-		TransactionHash: "0xfake",
+		Amount:          fundingAmount.String(),
+		TransactionHash: txHash,
 	}, nil
+}
+
+func (s *faucetServer) fundAndWait(to common.Address) (string, error) {
+	nonce := uint64(0)
+	nonce, err := s.client.PendingNonceAt(context.Background(), s.funder)
+	if err != nil {
+		return "", err
+	}
+
+	tx := types.NewTransaction(nonce, to, fundingAmount, 40000, big.NewInt(1*params.GWei), nil /*data*/)
+
+	tx, err = types.SignTx(tx, types.NewEIP155Signer(big.NewInt(5)), s.pk)
+	if err != nil {
+		return "", err
+	}
+
+	if err := s.client.SendTransaction(context.Background(), tx); err != nil {
+		return "", err
+	}
+
+	return tx.Hash().Hex(), nil
 }
