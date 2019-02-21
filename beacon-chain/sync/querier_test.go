ea13a93c683f4a987dd08896652079be4eb8d8b2
nisdas <nishdas93@gmail.com>
2019-02-21 11:45:33 +0800

    fixing tests

--- beacon-chain/sync/querier_test.go
+++ beacon-chain/sync/querier_test.go
@@ -5,17 +5,44 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/prysmaticlabs/prysm/shared/event"
+
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/p2p"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 	logTest "github.com/sirupsen/logrus/hooks/test"
 )
 
+type genesisPowChain struct {
+	feed *event.Feed
+}
+
+func (mp *genesisPowChain) HasChainStartLogOccurred() (bool, uint64, error) {
+	return false, 0, nil
+}
+
+func (mp *genesisPowChain) ChainStartFeed() *event.Feed {
+	return mp.feed
+}
+
+type afterGenesisPowChain struct {
+	feed *event.Feed
+}
+
+func (mp *afterGenesisPowChain) HasChainStartLogOccurred() (bool, uint64, error) {
+	return true, 0, nil
+}
+
+func (mp *afterGenesisPowChain) ChainStartFeed() *event.Feed {
+	return mp.feed
+}
+
 func TestStartStop(t *testing.T) {
 	hook := logTest.NewGlobal()
 	cfg := &QuerierConfig{
 		P2P:                &mockP2P{},
 		ResponseBufferSize: 100,
+		PowChain:           &afterGenesisPowChain{},
 	}
 	sq := NewQuerierService(context.Background(), cfg)
 
@@ -43,6 +70,7 @@ func TestChainReqResponse(t *testing.T) {
 	cfg := &QuerierConfig{
 		P2P:                &mockP2P{},
 		ResponseBufferSize: 100,
+		PowChain:           &afterGenesisPowChain{},
 	}
 	sq := NewQuerierService(context.Background(), cfg)
 
