99796831e9eed92310ed28537af311d8cc31d420
Preston Van Loon <preston@prysmaticlabs.com>
2019-02-21 18:55:03 -0500

    checkpoint on progress with cluster private key manager

---
+++ tools/cluster-pk-manager/server/logger.go
@@ -0,0 +1,5 @@
+package main
+
+import "github.com/sirupsen/logrus"
+
+var log = logrus.WithField("prefix", "main")
---
+++ tools/cluster-pk-manager/server/watchtower.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+var queryInterval = 10 * time.Second
+var namespace = "beacon-chain"
+var podSelector = "component=validator"
+
+type watchtower struct {
+	db     *db
+	client *kubernetes.Clientset
+}
+
+func NewWatchtower(db *db) *watchtower {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+	client := kubernetes.NewForConfigOrDie(config)
+	return &watchtower{db, client}
+}
+
+// WatchPods for termination, update allocations
+func (wt *watchtower) WatchPods() {
+	for {
+		time.Sleep(queryInterval)
+		if err := wt.queryPodsAndUpdateDB(); err != nil {
+			log.WithField("error", err).Error("Failed to update pods")
+		}
+	}
+}
+
+// Query k8s pods for existence.
+func (wt *watchtower) queryPodsAndUpdateDB() error {
+	log.Debug("Checking pods")
+	ctx := context.Background()
+	podNames, err := wt.db.AllocatedPodNames(ctx)
+	if err != nil {
+		return err
+	}
+	pList, err := wt.client.CoreV1().Pods(namespace).List(metav1.ListOptions{LabelSelector: podSelector})
+	if err != nil {
+		return err
+	}
+	podExist := make(map[string]bool)
+	for _, p := range pList.Items {
+		if p.Status.Phase == v1.PodRunning {
+			podExist[p.Name] = true
+		}
+	}
+
+	for _, p := range podNames {
+		if !podExist[p] {
+			log.WithField("pod", p).Debug("Removing assignment from dead pod")
+			if err := wt.db.RemovePKAssignment(ctx, p); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
