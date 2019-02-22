057c5c8d75804240f73c0ffbc3406a693a6f397c
Andrei Ivasko <cyberbono3@gmail.com>
2019-02-21 21:43:42 -0800

    shuffleIndices and 9 tests fail locally

--- beacon-chain/utils/shuffle_test.go
+++ beacon-chain/utils/shuffle_test.go
@@ -8,20 +8,6 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
-func TestFaultyShuffleIndices(t *testing.T) {
-	var list []uint64
-
-	upperBound := 1<<(params.BeaconConfig().RandBytes*8) - 1
-
-	for i := 0; i < upperBound+1; i++ {
-		list = append(list, uint64(i))
-	}
-
-	if _, err := ShuffleIndices(common.Hash{'a'}, list); err == nil {
-		t.Error("Shuffle should have failed when validator count exceeds ModuloBias")
-	}
-}
-
 func TestShuffleIndices(t *testing.T) {
 	hash1 := common.BytesToHash([]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g'})
 	hash2 := common.BytesToHash([]byte{'1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7'})
@@ -49,12 +35,12 @@ func TestShuffleIndices(t *testing.T) {
 		t.Errorf("2 shuffled lists shouldn't be equal")
 	}
 
-	// if !reflect.DeepEqual(list1, []uint64{3,3,0,3,0,3,0,2,0,3}) {
-	// 	t.Errorf("list 1 was incorrectly shuffled")
-	// }
-	// if !reflect.DeepEqual(list2, []uint64{5,3,5,5,5,5,3,5,5,5}) {
-	// 	t.Errorf("list 2 was incorrectly shuffled")
-	// }
+	if !reflect.DeepEqual(list1, []uint64{2, 5, 5, 5, 2, 5, 2, 5, 5, 5}) {
+		t.Errorf("list 1 was incorrectly shuffled")
+	}
+	if !reflect.DeepEqual(list2, []uint64{5, 5, 5, 5, 5, 0, 0, 5, 5, 5}) {
+		t.Errorf("list 2 was incorrectly shuffled")
+	}
 
 }
 
