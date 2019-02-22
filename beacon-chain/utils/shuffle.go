9a077b43fbd916772eaca4ef351835801e049c2c
Andrei Ivasko <cyberbono3@gmail.com>
2019-02-21 21:43:42 -0800

    shuffleIndices and 9 tests fail locally

--- beacon-chain/utils/shuffle.go
+++ beacon-chain/utils/shuffle.go
@@ -2,14 +2,13 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"math"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/prysmaticlabs/prysm/shared/hashutil"
-	"github.com/prysmaticlabs/prysm/shared/params"
 	//"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/mathutil"
 )
@@ -25,14 +24,14 @@ func GetPermutedIndex(index uint64, listSize uint64, seed common.Hash) (uint64, 
 		return 0, err
 	}
 
-	bs8 := make([]byte, 8)
 	bs4 := make([]byte, 4)
-	bs1 := make([]byte, 1)
+	buf := new(bytes.Buffer)
 
 	for round := 0; round < 90; round++ {
-		if err := binary.Write(bs1, binary.LittleEndian, round); err != nil {
+		if err := binary.Write(buf, binary.LittleEndian, uint8(round)); err != nil {
 			return 0, err
 		}
+		bs1 := buf.Bytes()
 		hashedValue := hashutil.Hash(append(seed[:], bs1...))
 		hashedValue8 := hashedValue[:8]
 		pivot := binary.LittleEndian.Uint64(hashedValue8[:]) % listSize
@@ -55,118 +54,16 @@ func GetPermutedIndex(index uint64, listSize uint64, seed common.Hash) (uint64, 
 	return index, nil
 }
 
-// ShuffleIndices returns a list of pseudorandomly sampled
-// indices. This is used to shuffle validators on ETH2.0 beacon chain.
-func SwapOrNotShuffle(seed common.Hash, indicesList []uint64) ([]uint64, error) {
-	listSize := len(indicesList)
-	// Each entropy is consumed from the seed in randBytes chunks.
-	randBytes := params.BeaconConfig().RandBytes
-
-	maxValidatorsPerRandBytes := params.BeaconConfig().MaxNumLog2Validators / randBytes
-	upperBound := 1<<(randBytes*maxValidatorsPerRandBytes) - 1
-	// Since we are consuming randBytes of entropy at a time in the loop,
-	// we have a bias at 2**24, this check defines our max list size and is used to remove the bias.
-	// more info on modulo bias: https://stackoverflow.com/questions/10984974/why-do-people-say-there-is-modulo-bias-when-using-a-random-number-generator.
-	if listSize >= upperBound {
-		return nil, errors.New("input list exceeded upper bound and reached modulo bias")
-	}
-
-	for round := 0; round < 90; round++ {
-		var hashBytes []byte
-		bs1 := make([]byte, 8)
-		binary.LittleEndian.PutUint64(bs1, uint64(round))
-		fmt.Printf("bs1 is %v\n", bs1)
-		num := int(math.Floor(float64((listSize + 255) / 256)))
-		fmt.Printf("num is %v\n", num)
-		for i := 0; i < num; i++ {
-			bs4 := make([]byte, 8)
-			binary.LittleEndian.PutUint64(bs4, uint64(i))
-			fmt.Printf("bs4 is %v\n", bs4)
-			bs := append(bs1, bs4...)
-			fmt.Printf("bs is %v\n", bs)
-			hash := hashutil.Hash(append(seed[:], bs...))
-			fmt.Printf("hash is %v\n", hash)
-			hashBytes = append(hashBytes, hash[:]...)
-			fmt.Printf("hashBytes is %v\n", hashBytes)
-
-		}
-
-		hash := hashutil.Hash(append(seed[:], bs1...))
-		fmt.Printf("hash is %v\n", hash)
-		hashFromBytes := binary.LittleEndian.Uint64(hash[:])
-		fmt.Printf("hashFromBytes is %v\n", hashFromBytes)
-		pivot := hashFromBytes % uint64(listSize)
-		fmt.Printf("pivot is %v\n", pivot)
-
-		powersOfTwo := []uint64{1, 2, 4, 8, 16, 32, 64, 128}
-
-		for i, index := range indicesList {
-			flip := (pivot - index) % uint64(listSize)
-			fmt.Printf("flip is %v\n", flip)
-			var hashPos uint64
-			if index > flip {
-				hashPos = index
-			} else {
-				hashPos = flip
-			}
-			fmt.Printf("hashPos is %v\n", hashPos)
-			hashBytesIndex := int(math.Floor((float64(hashPos) / 8)))
-			fmt.Printf("hashBytesIndex is %v\n", hashBytesIndex)
-			hByte := hashBytes[hashBytesIndex]
-			fmt.Printf("hByte is %v\n", hByte)
-			hInt := uint64(hByte)
-			fmt.Printf("hByte is %v\n", hByte)
-			p := powersOfTwo[hashPos%8]
-			fmt.Printf("p is %v\n", p)
-			if hInt&p != 0 {
-				indicesList[i] = flip
-			}
-
-		}
-	}
-	return indicesList, nil
-}
-
 func ShuffleIndices(seed common.Hash, indicesList []uint64) ([]uint64, error) {
-	var hashBytes []byte
-	bs1 := make([]byte, 8)
-	bs4 := make([]byte, 8)
-	listSize := len(indicesList)
-	num := int(math.Floor(float64((listSize + 255) / 256)))
-	powersOfTwo := []uint64{1, 2, 4, 8, 16, 32, 64, 128}
-
-	for round := 0; round < 90; round++ {
-		binary.LittleEndian.PutUint64(bs1[:], uint64(round))
-
-		for i := 0; i < num; i++ {
-			binary.LittleEndian.PutUint64(bs4[:], uint64(i))
-			bs := append(bs1, bs4...)
-			hash := hashutil.Hash(append(seed[:], bs...))
-			hashBytes = append(hashBytes, hash[:]...)
+	var permutedIndicesList []uint64
+	for _, index := range indicesList {
+		permutedIndex, err := GetPermutedIndex(index, uint64(len(indicesList)), seed)
+		if err != nil {
+			return nil, errors.New("GetPermutedIndex error")
 		}
-
-		hash := hashutil.Hash(append(seed[:], bs1...))
-		hashFromBytes := binary.LittleEndian.Uint64(hash[:])
-		pivot := hashFromBytes % uint64(listSize)
-
-		for i, index := range indicesList {
-			flip := (pivot - index) % uint64(listSize)
-			var hashPos uint64
-			if index > flip {
-				hashPos = index
-			} else {
-				hashPos = flip
-			}
-			hashBytesIndex := int(math.Floor((float64(hashPos) / 8)))
-			hByte := hashBytes[hashBytesIndex]
-			hInt := uint64(hByte)
-			p := powersOfTwo[hashPos%8]
-			if hInt&p != 0 {
-				indicesList[i] = flip
-			}
-		}
+		permutedIndicesList = append(permutedIndicesList, permutedIndex)
 	}
-	return indicesList, nil
+	return permutedIndicesList, nil
 }
 
 // SplitIndices splits a list into n pieces.
--- beacon-chain/utils/shuffle_test.go
+++ beacon-chain/utils/shuffle_test.go
@@ -8,81 +8,6 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
-
-/*
-def shuffle(list_size, seed):
-    indices = list(range(list_size))
-    for round in range(90):
-        hash_bytes = b''.join([
-            hash(seed + round.to_bytes(1, 'little') + (i).to_bytes(4, 'little'))
-            for i in range((list_size + 255) // 256)
-        ])
-        pivot = int.from_bytes(hash(seed + round.to_bytes(1, 'little')), 'little') % list_size
-
-        powers_of_two = [1, 2, 4, 8, 16, 32, 64, 128]
-
-        for i, index in enumerate(indices):
-            flip = (pivot - index) % list_size
-            hash_pos = index if index > flip else flip
-            byte = hash_bytes[hash_pos // 8]
-            if byte & powers_of_two[hash_pos % 8]:
-                indices[i] = flip
-    return indices
-
-*/
-
-
-
-func TestFaultyShuffleIndices(t *testing.T) {
-	var list []uint64
-
-	upperBound := 1<<(params.BeaconConfig().RandBytes*8) - 1
-
-	for i := 0; i < upperBound+1; i++ {
-		list = append(list, uint64(i))
-	}
-
-	if _, err := ShuffleIndices(common.Hash{'a'}, list); err == nil {
-		t.Error("Shuffle should have failed when validator count exceeds ModuloBias")
-	}
-}
-
-
-func TestSwapOrNotShuffle(t *testing.T)  {
-	hash1 := common.BytesToHash([]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g'})
-	hash2 := common.BytesToHash([]byte{'1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7'})
-	var list1 []uint64
-
-	for i := 0; i < 10; i++ {
-		list1 = append(list1, uint64(i))
-	}
-
-	list2 := make([]uint64, len(list1))
-	copy(list2, list1)
-
-	list1, err := SwapOrNotShuffle(hash1, list1)
-	if err != nil {
-		t.Errorf("Shuffle failed with: %v", err)
-	}
-
-	list2, err = SwapOrNotShuffle(hash2, list2)
-	if err != nil {
-		t.Errorf("Shuffle failed with: %v", err)
-	}
-
-	if reflect.DeepEqual(list1, list2) {
-		t.Errorf("2 shuffled lists shouldn't be equal")
-	}
-	if !reflect.DeepEqual(list1, []uint64{5, 4, 9, 6, 7, 3, 0, 1, 8, 2}) {
-		t.Errorf("list 1 was incorrectly shuffled")
-	}
-	if !reflect.DeepEqual(list2, []uint64{9, 0, 1, 5, 3, 2, 4, 7, 8, 6}) {
-		t.Errorf("list 2 was incorrectly shuffled")
-	}
-
-}
-
-
 func TestShuffleIndices(t *testing.T) {
 	hash1 := common.BytesToHash([]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c', 'd', 'e', 'f', 'g'})
 	hash2 := common.BytesToHash([]byte{'1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7'})
@@ -98,23 +23,24 @@ func TestShuffleIndices(t *testing.T) {
 	list1, err := ShuffleIndices(hash1, list1)
 	if err != nil {
 		t.Errorf("Shuffle failed with: %v", err)
-	}
+	} 
 
 	list2, err = ShuffleIndices(hash2, list2)
 	if err != nil {
 		t.Errorf("Shuffle failed with: %v", err)
-	}
+	} 
+	
 
 	if reflect.DeepEqual(list1, list2) {
 		t.Errorf("2 shuffled lists shouldn't be equal")
 	}
 
-	// if !reflect.DeepEqual(list1, []uint64{3,3,0,3,0,3,0,2,0,3}) {
-	// 	t.Errorf("list 1 was incorrectly shuffled")
-	// }
-	// if !reflect.DeepEqual(list2, []uint64{5,3,5,5,5,5,3,5,5,5}) {
-	// 	t.Errorf("list 2 was incorrectly shuffled")
-	// }
+	if !reflect.DeepEqual(list1, []uint64{2, 5, 5, 5, 2, 5, 2, 5, 5, 5}) {
+		t.Errorf("list 1 was incorrectly shuffled")
+	}
+	if !reflect.DeepEqual(list2, []uint64{5, 5, 5, 5, 5, 0, 0, 5, 5, 5}) {
+		t.Errorf("list 2 was incorrectly shuffled")
+	}
 
 }
 
