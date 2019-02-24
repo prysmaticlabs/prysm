69572d0ada2452d18613970d76d10e47fe3acfed
Preston Van Loon <preston@prysmaticlabs.com>
2019-02-23 23:33:10 -0500

    Add validator balance metric (#1692)
    * Add validator metrics
    
    * Add test
    
    * use hex.EncodeToString for faster processing

---
+++ beacon-chain/core/state/metrics.go
@@ -0,0 +1,27 @@
+package state
+
+import (
+	"encoding/hex"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+)
+
+var (
+	validatorBalancesGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "state_validator_balances",
+		Help: "Balances of validators, updated on epoch transition",
+	}, []string{
+		"validator",
+	})
+)
+
+func reportEpochTransitionMetrics(state *pb.BeaconState) {
+	// Validator balances
+	for i, bal := range state.ValidatorBalances {
+		validatorBalancesGauge.WithLabelValues(
+			"0x" + hex.EncodeToString(state.ValidatorRegistry[i].Pubkey), // Validator
+		).Set(float64(bal))
+	}
+}
