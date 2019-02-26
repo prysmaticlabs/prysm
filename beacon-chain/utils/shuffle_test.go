ac631df0f41432fafd6cffce5445c82bef49a9c0
Andrei Ivasko <cyberbono3@gmail.com>
2019-02-25 19:24:40 -0800

    rebased and  conflicts resolved

--- beacon-chain/utils/shuffle_test.go
+++ beacon-chain/utils/shuffle_test.go
@@ -23,22 +23,20 @@ func TestShuffleIndices(t *testing.T) {
 	list1, err := ShuffleIndices(hash1, list1)
 	if err != nil {
 		t.Errorf("Shuffle failed with: %v", err)
-	} 
+	}
 
 	list2, err = ShuffleIndices(hash2, list2)
 	if err != nil {
 		t.Errorf("Shuffle failed with: %v", err)
-	} 
-	
+	}
 
 	if reflect.DeepEqual(list1, list2) {
 		t.Errorf("2 shuffled lists shouldn't be equal")
 	}
-
-	if !reflect.DeepEqual(list1, []uint64{2, 5, 5, 5, 2, 5, 2, 5, 5, 5}) {
+	if !reflect.DeepEqual(list1, []uint64{8, 6, 8, 8, 8, 8, 8, 6, 8, 6}) {
 		t.Errorf("list 1 was incorrectly shuffled")
 	}
-	if !reflect.DeepEqual(list2, []uint64{5, 5, 5, 5, 5, 0, 0, 5, 5, 5}) {
+	if !reflect.DeepEqual(list2, []uint64{4, 4, 4, 4, 4, 4, 4, 4, 4, 4}) {
 		t.Errorf("list 2 was incorrectly shuffled")
 	}
 
