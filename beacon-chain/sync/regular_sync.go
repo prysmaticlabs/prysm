3badc56a38528421c04ace0f51d8f851c12d85c0
Raul Jordan <raul@prysmaticlabs.com>
2019-03-05 15:36:30 -0600

    weird spacing

--- beacon-chain/sync/regular_sync.go
+++ beacon-chain/sync/regular_sync.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-
 	"github.com/gogo/protobuf/proto"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
