978e45e6ab4c8fb552ad3d242f272316f7a77d24
Raul Jordan <raul@prysmaticlabs.com>
2019-03-05 10:45:06 -0600

    consistent style in promauto counters

--- beacon-chain/sync/initial-sync/metrics.go
+++ beacon-chain/sync/initial-sync/metrics.go
@@ -8,31 +8,31 @@ import (
 var (
 	// Metrics
 	sentBatchedBlockReq = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "initSync_sent_batched_block_req",
+		Name: "initsync_sent_batched_block_req",
 		Help: "The number of sent batched block req",
 	})
 	batchedBlockReq = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "initSync_batched_block_req",
+		Name: "initsync_batched_block_req",
 		Help: "The number of received batch blocks responses",
 	})
 	blockReqSlot = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "initSync_block_req_by_slot",
+		Name: "initsync_block_req_by_slot",
 		Help: "The number of sent block requests by slot",
 	})
 	recBlock = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "initSync_received_blocks",
+		Name: "initsync_received_blocks",
 		Help: "The number of received blocks",
 	})
 	recBlockAnnounce = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "initSync_received_block_announce",
+		Name: "initsync_received_block_announce",
 		Help: "The number of received block announce",
 	})
 	stateReq = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "initSync_state_req",
+		Name: "initsync_state_req",
 		Help: "The number of sent state requests",
 	})
 	recState = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "initSync_received_state",
+		Name: "initsync_received_state",
 		Help: "The number of received state",
 	})
 )
