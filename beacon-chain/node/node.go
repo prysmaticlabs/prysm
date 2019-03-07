f0f5cfc62dab1bd0a6aa58980843689e254cb92e
thatkawaiiguy <ivanthegreat15@gmail.com>
2019-03-06 22:39:20 -0600

    Fix duplicate import from merge

--- beacon-chain/node/node.go
+++ beacon-chain/node/node.go
@@ -10,8 +10,6 @@ import (
 	"sync"
 	"syscall"
 
-	"github.com/prysmaticlabs/prysm/beacon-chain/attestation"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	gethRPC "github.com/ethereum/go-ethereum/rpc"
