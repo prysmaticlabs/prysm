4dd8c4aa5ffe8b6e17aaf6e9f8710a66ecfcd2b5
thatkawaiiguy <ivanthegreat15@gmail.com>
2019-03-08 10:51:55 -0600

    Add comment

--- scripts/apply-all-eth2-k8.sh
+++ scripts/apply-all-eth2-k8.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-kubectl apply -f
-
-kubectl apply -f k8s/beacon-chain/namespace.yaml
+# Make sure you set your contract and private key path in the configs
 kubectl apply -f k8s/priority.yaml
+kubectl apply -f k8s/beacon-chain/namespace.yaml
 kubectl apply -f k8s/beacon-chain/beacon-config.config.yaml
 kubectl apply -f k8s/beacon-chain/beacon-chain.deploy.yaml
 kubectl apply -f k8s/beacon-chain/beacon-chain.service.yaml
