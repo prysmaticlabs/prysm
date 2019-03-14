85c8599a6bdfd175cc4d74df1fd9a4fdda24a222
Raul Jordan <raul@prysmaticlabs.com>
2019-03-14 16:30:48 -0600

    Revert "Update FFG Check Points During Block Processing (#1991)"
    This reverts commit b1ffc693e02b86d6a37095fda474c306eb4bbe4a.

--- beacon-chain/blockchain/block_processing_test.go
+++ beacon-chain/blockchain/block_processing_test.go
@@ -103,12 +103,7 @@ func TestReceiveBlock_ProcessCorrectly(t *testing.T) {
 			Attestations: nil,
 		},
 	}
-	if err := chainService.beaconDB.SaveJustifiedBlock(block); err != nil {
-		t.Fatal(err)
-	}
-	if err := chainService.beaconDB.SaveFinalizedBlock(block); err != nil {
-		t.Fatal(err)
-	}
+
 	if err := chainService.beaconDB.SaveBlock(block); err != nil {
 		t.Fatal(err)
 	}
@@ -181,12 +176,7 @@ func TestReceiveBlock_RemovesPendingDeposits(t *testing.T) {
 			Deposits: pendingDeposits,
 		},
 	}
-	if err := chainService.beaconDB.SaveJustifiedBlock(block); err != nil {
-		t.Fatal(err)
-	}
-	if err := chainService.beaconDB.SaveFinalizedBlock(block); err != nil {
-		t.Fatal(err)
-	}
+
 	for _, dep := range pendingDeposits {
 		db.InsertPendingDeposit(chainService.ctx, dep, big.NewInt(0))
 	}
