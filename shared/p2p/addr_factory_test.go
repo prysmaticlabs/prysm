e423a3004f81616a4c9ccf69ac5075649ba144fb
Preston Van Loon <preston@prysmaticlabs.com>
2019-03-15 23:14:22 -0400

    broadcast local addrs as well as relay addrs

--- shared/p2p/addr_factory_test.go
+++ shared/p2p/addr_factory_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestRelayAddrsOnlyFactory(t *testing.T) {
 	relay := "/ip4/127.0.0.1/tcp/6660/p2p/QmQ7zhY7nGY66yK1n8hLGevfVyjbtvHSgtZuXkCH9oTrgi"
-	f := relayAddrsOnly(relay)
+	f := withRelayAddrs(relay)
 
 	a, err := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/33201/p2p/QmaXZhW44pwQxBSeLkE5FNeLz8tGTTEsRciFg1DNWXXrWG")
 	if err != nil {
