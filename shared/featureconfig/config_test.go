2c7f768bd620f4d840a7f31eb05aed139c45b464
Ivan Martinez <ivanthegreatdev@gmail.com>
2019-03-17 17:40:17 -0400

    Full coverage

--- shared/featureconfig/config_test.go
+++ shared/featureconfig/config_test.go
@@ -1,7 +1,10 @@
 package featureconfig
 
 import (
+	"flag"
 	"testing"
+
+	"github.com/urfave/cli"
 )
 
 func TestInitFeatureConfig(t *testing.T) {
@@ -9,6 +12,28 @@ func TestInitFeatureConfig(t *testing.T) {
 		VerifyAttestationSigs: true,
 	}
 	InitFeatureConfig(cfg)
+	if c := FeatureConfig(); !c.VerifyAttestationSigs {
+		t.Errorf("VerifyAttestationSigs in FeatureFlags incorrect. Wanted true, got false")
+	}
+}
+
+func TestConfigureBeaconConfig(t *testing.T) {
+	app := cli.NewApp()
+	set := flag.NewFlagSet("test", 0)
+	set.Bool(VerifyAttestationSigsFlag.Name, true, "enable attestation verification")
+	context := cli.NewContext(app, set, nil)
+	ConfigureBeaconFeatures(context)
+	if c := FeatureConfig(); !c.VerifyAttestationSigs {
+		t.Errorf("VerifyAttestationSigs in FeatureFlags incorrect. Wanted true, got false")
+	}
+}
+
+func TestConfigureValidatorConfig(t *testing.T) {
+	app := cli.NewApp()
+	set := flag.NewFlagSet("test", 0)
+	set.Bool(VerifyAttestationSigsFlag.Name, true, "enable attestation verification")
+	context := cli.NewContext(app, set, nil)
+	ConfigureValidatorFeatures(context)
 	if c := FeatureConfig(); !c.VerifyAttestationSigs {
 		t.Errorf("VerifyAttestationSigs in FeatureFlags incorrect. Wanted true, got false")
 	}
