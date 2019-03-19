bdf862675695d208b404982eb463b50d5674ce1f
Terence Tsao <terence@prysmaticlabs.com>
2019-03-18 23:00:12 -0700

    infof/info

--- beacon-chain/blockchain/fork_choice.go
+++ beacon-chain/blockchain/fork_choice.go
@@ -44,7 +44,7 @@ func (c *ChainService) updateFFGCheckPts(state *pb.BeaconState) error {
 		if err != nil {
 			return err
 		}
-		log.Info("newJustifiedState's slot is: %d", newJustifiedState.Slot-params.BeaconConfig().GenesisSlot)
+		log.Infof("newJustifiedState's slot is: %d", newJustifiedState.Slot-params.BeaconConfig().GenesisSlot)
 		log.WithFields(logrus.Fields{
 			"new_justified_state_slot": newJustifiedState.Slot - params.BeaconConfig().GenesisSlot,
 		}).Info("Updating justified checkpoint in DB")
@@ -78,7 +78,7 @@ func (c *ChainService) updateFFGCheckPts(state *pb.BeaconState) error {
 		if err != nil {
 			return err
 		}
-		log.Info("newFinalizedState's slot is: %d", newFinalizedState.Slot-params.BeaconConfig().GenesisSlot)
+		log.Infof("newFinalizedState's slot is: %d", newFinalizedState.Slot-params.BeaconConfig().GenesisSlot)
 		log.WithFields(logrus.Fields{
 			"new_finalized_state_slot": newFinalizedState.Slot - params.BeaconConfig().GenesisSlot,
 		}).Info("Updating justified checkpoint in DB")
