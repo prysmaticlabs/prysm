b0caf06e0c9faec4db2a1c38c7f1f42500297bae
terence tsao <terence@prysmaticlabs.com>
2019-03-20 15:28:11 -0700

    Update start-beacon-chain.sh

--- scripts/start-beacon-chain.sh
+++ scripts/start-beacon-chain.sh
@@ -2,11 +2,6 @@
 
 DEPOSIT_CONTRACT=DEPOSITCONTRACTHERE
 
-DATA_DIR=/tmp/beacon
-rm -rf $DATA_DIR
-mkdir -p $DATA_DIR
-
-CMD="bazel run //beacon-chain -- --web3provider wss://goerli.prylabs.net/websocket"
-CMD+=" --datadir $DATA_DIR --deposit-contract $DEPOSIT_CONTRACT --demo-config"
+CMD="bazel run //beacon-chain -- --deposit-contract $DEPOSIT_CONTRACT"
 
 $CMD
