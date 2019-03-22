e214d2597ee46edd13d0ebfbe0565faa86200100
shayzluf <thezluf@gmail.com>
2019-03-22 11:48:52 +0530

    fix Potential file inclusion try2

--- shared/keystore/keystore.go
+++ shared/keystore/keystore.go
@@ -95,6 +95,7 @@ func (ks Store) GetKeys(directory, fileprefix, password string) ([]*Key, error) 
 	for _, f := range files {
 		n := f.Name()
 		filePath := filepath.Join(directory, n)
+		filePath = filepath.Clean(filePath)
 		if f.Mode().IsRegular() && strings.Contains(n, strings.TrimPrefix(fileprefix, "/")) {
 			keyjson, err := ioutil.ReadFile(filePath)
 			if err != nil {
