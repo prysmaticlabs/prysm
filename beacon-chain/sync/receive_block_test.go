329101b8fa709c56eed9cad39e10048a71ad215d
Raul Jordan <raul@prysmaticlabs.com>
2019-03-27 10:42:52 -0500

    tests for parent root requests

--- beacon-chain/sync/receive_block_test.go
+++ beacon-chain/sync/receive_block_test.go
@@ -2,6 +2,7 @@ package sync
 
 import (
 	"context"
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"testing"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
@@ -11,17 +12,27 @@ func TestReceiveBlock_RecursivelyProcessesChildren(t *testing.T) {
 	rs := NewRegularSyncService(context.Background(), DefaultRegularSyncConfig())
 
 	parents := []*pb.BeaconBlock{
+		{
 
+		}
+	}
+    parentRoots := make([][]byte, len(parents))
+    for i := range parents {
+    	h, err := hashutil.HashBeaconBlock(parents[i])
+    	if err != nil {
+    		t.Fatal(err)
+		}
+		parentRoots[i] = h[:]
 	}
 
 	blocksMissingParent := []*pb.BeaconBlock{
 		{
 			Slot: params.BeaconConfig().GenesisSlot + 10,
-			ParentRootHash32: []byte("dad1"),
+			ParentRootHash32: parentRoots[0],
 		},
 		{
 			Slot: params.BeaconConfig().GenesisSlot + 20,
-			ParentRootHash32: []byte("dad2"),
+			ParentRootHash32: parentRoots[1],
 		},
 	}
 }
