cb550b6ddacaae9e356827e3ea3513c61fcb6b77
Terence Tsao <terence@prysmaticlabs.com>
2019-03-30 09:54:42 -0700

    add prometheus stats for validators active/exit/slashed....

--- beacon-chain/db/state_metrics.go
+++ beacon-chain/db/state_metrics.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"encoding/hex"
+	"strconv"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -13,6 +14,24 @@ var (
 	validatorBalancesGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "state_validator_balances",
 		Help: "Balances of validators, updated on epoch transition",
+	}, []string{
+		"validator",
+	})
+	validatorActivatedGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "state_validator_activated_epoch",
+		Help: "Activated epoch of validators, updated on epoch transition",
+	}, []string{
+		"validator",
+	})
+	validatorExitedGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "state_validator_exited_epoch",
+		Help: "Exited epoch of validators, updated on epoch transition",
+	}, []string{
+		"validator",
+	})
+	validatorSlashedGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "state_validator_slashed_epoch",
+		Help: "Slashed epoch of validators, updated on epoch transition",
 	}, []string{
 		"validator",
 	})
@@ -32,17 +51,44 @@ var (
 		Name: "state_last_finalized_epoch",
 		Help: "Last finalized epoch of the processed state",
 	})
+	activeValidatorsGauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "state_active_validators",
+		Help: "Total number of active validators",
+	})
 )
 
 func reportStateMetrics(state *pb.BeaconState) {
+	s := params.BeaconConfig().GenesisSlot
+	e := params.BeaconConfig().GenesisEpoch
+	currentEpoch := state.Slot / params.BeaconConfig().SlotsPerEpoch
 	// Validator balances
 	for i, bal := range state.ValidatorBalances {
 		validatorBalancesGauge.WithLabelValues(
 			"0x" + hex.EncodeToString(state.ValidatorRegistry[i].Pubkey), // Validator
 		).Set(float64(bal))
 	}
-	s := params.BeaconConfig().GenesisSlot
-	e := params.BeaconConfig().GenesisEpoch
+
+	var active float64
+	for i, v := range state.ValidatorRegistry {
+		// Track individual Validator's activation epochs
+		validatorActivatedGauge.WithLabelValues(
+			strconv.Itoa(i), //Validator index
+		).Set(float64(v.ActivationEpoch - e))
+		// Track individual Validator's exited epochs
+		validatorExitedGauge.WithLabelValues(
+			strconv.Itoa(i), //Validator index
+		).Set(float64(v.ExitEpoch - e))
+		// Track individual Validator's slashed epochs
+		validatorSlashedGauge.WithLabelValues(
+			strconv.Itoa(i), //Validator index
+		).Set(float64(v.SlashedEpoch - e))
+		// Total number of active validators
+		if v.ActivationEpoch <= currentEpoch && currentEpoch < v.ExitEpoch {
+			active++
+		}
+	}
+	activeValidatorsGauge.Set(active)
+
 	// Slot number
 	lastSlotGauge.Set(float64(state.Slot - s))
 	// Last justified slot
