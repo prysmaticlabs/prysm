edd9a8a5141157a9660d349b8f24bb058b5f4b4c
Raul Jordan <raul@prysmaticlabs.com>
2019-03-31 22:18:49 -0500

    spacing

--- beacon-chain/sync/querier.go
+++ beacon-chain/sync/querier.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
