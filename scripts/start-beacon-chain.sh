bad5ceffe0149f134816b1512b6808cb1ff8d5c9
Ivan Martinez <ivanthegreatdev@gmail.com>
2019-04-02 14:17:12 -0400

    Fix scripts for startup

--- scripts/start-beacon-chain.sh
+++ scripts/start-beacon-chain.sh
@@ -13,4 +13,4 @@ while test $# -gt 0; do
     esac
 done
 
-bazel run //beacon-chain -- --clear-db --deposit-contract $DEPOSIT_CONTRACT
+bazel run //beacon-chain -- --clear-db --deposit-contract $DEPOSIT_CONTRACT --web3provider=wss://goerli.infura.io/ws/v3/be3fb7ed377c418087602876a40affa1
