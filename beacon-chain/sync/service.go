e6cbec06c8ed340c7fe1f27bf3d2ebc22445b025
nisdas <nishdas93@gmail.com>
2019-04-08 13:53:29 +0800

    spacing

--- beacon-chain/sync/service.go
+++ beacon-chain/sync/service.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/prysmaticlabs/prysm/shared/bytesutil"
-
-	"github.com/prysmaticlabs/prysm/shared/params"
-
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 	"github.com/prysmaticlabs/prysm/beacon-chain/operations"
 	initialsync "github.com/prysmaticlabs/prysm/beacon-chain/sync/initial-sync"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/sirupsen/logrus"
 )
 
