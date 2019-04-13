45835c64394fba079dfa39b230670baf7a5be988
Ivan Martinez <ivanthegreatdev@gmail.com>
2019-04-13 17:49:41 -0400

    Fix test

--- beacon-chain/powchain/log_processing.go
+++ beacon-chain/powchain/log_processing.go
@@ -174,12 +174,14 @@ func (w *Web3Service) processPastLogs() error {
 	}
 	w.lastRequestedBlock.Set(w.blockHeight)
 
-	currentState, err := w.beaconDB.HeadState(w.ctx)
-	if err != nil {
-		return fmt.Errorf("could not get head state: %v", err)
-	}
-	if currentState != nil && currentState.DepositIndex > 0 {
-		w.beaconDB.PrunePendingDeposits(w.ctx, currentState.DepositIndex)
+	if w.beaconDB != nil {
+		currentState, err := w.beaconDB.HeadState(w.ctx)
+		if err != nil {
+			return fmt.Errorf("could not get head state: %v", err)
+		}
+		if currentState != nil && currentState.DepositIndex > 0 {
+			w.beaconDB.PrunePendingDeposits(w.ctx, currentState.DepositIndex)
+		}
 	}
 
 	return nil
