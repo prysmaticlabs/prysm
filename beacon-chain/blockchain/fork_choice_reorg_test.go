fe70f189c14029535cf9b7c10f46c837717ebf38
Raul Jordan <raul@prysmaticlabs.com>
2019-04-15 16:50:57 -0500

    balance deviations advance chains correctly

--- beacon-chain/blockchain/fork_choice_reorg_test.go
+++ beacon-chain/blockchain/fork_choice_reorg_test.go
@@ -19,33 +19,12 @@ type assignment struct {
 	committee []uint64
 }
 
-// This function tests the following: when two nodes A and B are running at slot 10
-// and node A reorgs back to slot 7 (an epoch boundary), while node B remains the same,
-// once the nodes catch up a few blocks later, we expect their state and validator
-// balances to remain the same. That is, we expect no deviation in validator balances.
-func TestEpochReorg_MatchingStates(t *testing.T) {
-	params.UseDemoBeaconConfig()
-	// First we setup two independent db's for node A and B.
-	ctx := context.Background()
-	beaconDB1 := internal.SetupDB(t)
-	beaconDB2 := internal.SetupDB(t)
-	defer internal.TeardownDB(t, beaconDB1)
-	defer internal.TeardownDB(t, beaconDB2)
-
-	chainService1 := setupBeaconChain(t, beaconDB1, nil)
-	//chainService2 := setupBeaconChain(t, beaconDB2, nil)
-	unixTime := uint64(time.Now().Unix())
+func advanceChain(t *testing.T, chainService *ChainService) ([]*pb.BeaconBlock, []*pb.BeaconState) {
+	unixTime := time.Unix(0, 0)
 	deposits, _ := setupInitialDeposits(t, 8)
-	if err := beaconDB1.InitializeState(ctx, unixTime, deposits, &pb.Eth1Data{}); err != nil {
-		t.Fatalf("Could not initialize beacon state to disk: %v", err)
-	}
-	if err := beaconDB2.InitializeState(ctx, unixTime, deposits, &pb.Eth1Data{}); err != nil {
-		t.Fatalf("Could not initialize beacon state to disk: %v", err)
-	}
-
-	genesisState, err := beaconDB1.HeadState(ctx)
+	genesisState, err := chainService.initializeBeaconChain(unixTime, deposits, &pb.Eth1Data{})
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("Could not initialize beacon state to disk: %v", err)
 	}
 	stateRoot, err := hashutil.HashProto(genesisState)
 	if err != nil {
@@ -54,14 +33,16 @@ func TestEpochReorg_MatchingStates(t *testing.T) {
 	genesisBlock := b.NewGenesisBlock(stateRoot[:])
 
 	// Then, we create the chain up to slot 10 in both.
-    blocks := []*pb.BeaconBlock{genesisBlock}
-	states := []*pb.BeaconState{genesisState}
-    assignments := make(map[uint64]*assignment)
-    for idx := range genesisState.ValidatorRegistry {
+	blocks := make([]*pb.BeaconBlock, 11)
+	blocks[0] = genesisBlock
+	states := make([]*pb.BeaconState, 11)
+	states[0] = genesisState
+	assignments := make(map[uint64]*assignment)
+	for idx := range genesisState.ValidatorRegistry {
 		committee, shard, slot, _, err :=
 			helpers.CommitteeAssignment(genesisState, genesisBlock.Slot, uint64(idx), false)
 		if err != nil {
-            t.Fatal(err)
+			t.Fatal(err)
 		}
 		assignments[slot] = &assignment{
 			shard,
@@ -69,11 +50,11 @@ func TestEpochReorg_MatchingStates(t *testing.T) {
 			committee,
 		}
 	}
-	for i := uint64(1); i <= uint64(10); i++ {
+	for i := uint64(1); i <= 10; i++ {
 		if i % params.BeaconConfig().SlotsPerEpoch == 0 {
 			for idx := range genesisState.ValidatorRegistry {
 				committee, shard, slot, _, err :=
-					helpers.CommitteeAssignment(genesisState, i, uint64(idx), false)
+					helpers.CommitteeAssignment(states[i-1], i+params.BeaconConfig().GenesisSlot, uint64(idx), false)
 				if err != nil {
 					t.Fatal(err)
 				}
@@ -130,7 +111,7 @@ func TestEpochReorg_MatchingStates(t *testing.T) {
 		} else {
 			epochBoundaryRoot, err = b.BlockRoot(states[i-1], epochStartSlot)
 			if err != nil {
-			   t.Fatal(err)
+				t.Fatal(err)
 			}
 		}
 		// epoch_start_slot = get_epoch_start_slot(slot_to_epoch(head.slot))
@@ -155,21 +136,56 @@ func TestEpochReorg_MatchingStates(t *testing.T) {
 		attestation.Data.CrosslinkDataRootHash32 = params.BeaconConfig().ZeroHash[:]
 
 		block.Body.Attestations = []*pb.Attestation{attestation}
-		beaconState, err := chainService1.ApplyBlockStateTransition(ctx, block, states[i-1])
+		beaconState, err := chainService.ApplyBlockStateTransition(context.Background(), block, states[i-1])
 		if err != nil {
 			t.Fatal(err)
 		}
-		if err := beaconDB1.SaveBlock(block); err != nil {
+		if err := chainService.beaconDB.SaveBlock(block); err != nil {
 			t.Fatal(err)
 		}
-		if err := beaconDB1.UpdateChainHead(ctx, block, beaconState); err != nil {
+		if err := chainService.beaconDB.UpdateChainHead(context.Background(), block, beaconState); err != nil {
 			t.Fatal(err)
 		}
 		blocks[i] = block
 		states[i] = beaconState
 	}
+	return blocks, states
+}
 
-	t.Log(blocks)
+// This function tests the following: when two nodes A and B are running at slot 10
+// and node A reorgs back to slot 7 (an epoch boundary), while node B remains the same,
+// once the nodes catch up a few blocks later, we expect their state and validator
+// balances to remain the same. That is, we expect no deviation in validator balances.
+func TestEpochReorg_MatchingStates(t *testing.T) {
+	params.UseDemoBeaconConfig()
+	// First we setup two independent db's for node A and B.
+	beaconDB1 := internal.SetupDB(t)
+	beaconDB2 := internal.SetupDB(t)
+	defer internal.TeardownDB(t, beaconDB1)
+	defer internal.TeardownDB(t, beaconDB2)
+
+	chainService1 := setupBeaconChain(t, beaconDB1, nil)
+	chainService2 := setupBeaconChain(t, beaconDB2, nil)
+	_, states1 := advanceChain(t, chainService1)
+	_, states2 := advanceChain(t, chainService2)
+
+	lastState1 := states1[len(states1)-1]
+	lastState2 := states2[len(states2)-1]
+
+	// We expect both nodes to have matching balances after generating N
+	// blocks, including attestations, and applying N state transitions.
+	balancesNodeA := lastState1.ValidatorBalances
+	balancesNodeB := lastState2.ValidatorBalances
+	for i := range balancesNodeA {
+		if balancesNodeA[i] != balancesNodeB[i] {
+			t.Errorf(
+				"Expected balance to match at index %d, received %v = %v",
+				i,
+				balancesNodeA[i],
+				balancesNodeB[i],
+			)
+		}
+	}
 
 	// We update attestation targets for node A such that validators point to the block
 	// at slot 7 as canonical - then, a reorg to that slot will occur.
