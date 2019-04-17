af9533021dfe0264cde25ef71aa7411f55574ad5
Terence Tsao <terence@prysmaticlabs.com>
2019-04-17 14:30:29 -0700

    Use WithError

--- beacon-chain/rpc/proposer_server.go
+++ beacon-chain/rpc/proposer_server.go
@@ -114,7 +114,7 @@ func (ps *ProposerServer) PendingAttestations(ctx context.Context, req *pb.Pendi
 	validAtts := make([]*pbp2p.Attestation, 0, len(atts))
 	for _, att := range atts {
 		if err := blocks.VerifyAttestation(beaconState, att, false); err != nil {
-			log.WithField("slot", att.Data.Slot-
+			log.WithError(err).WithField("slot", att.Data.Slot-
 				params.BeaconConfig().GenesisSlot).Error("Skipping, pending attestation failed verification")
 			continue
 		}
