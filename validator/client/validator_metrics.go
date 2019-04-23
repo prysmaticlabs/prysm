e2b363c5295efd5caf391efc5b33b1afbd3a5873
frederickalcantara <frederickaalcantara@gmail.com>
2019-04-22 21:18:07 -0400

    Added validator conditional for logging penalties & rewards

--- validator/client/validator_metrics.go
+++ validator/client/validator_metrics.go
@@ -8,7 +8,6 @@ import (
 
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
 	"github.com/prysmaticlabs/prysm/shared/params"
-	"github.com/prysmaticlabs/prysm/validator/types"
 	"github.com/sirupsen/logrus"
 )
 
@@ -25,88 +24,56 @@ func (v *validator) LogValidatorGainsAndLosses(ctx context.Context, slot uint64)
 	if epoch == params.BeaconConfig().GenesisEpoch {
 		v.prevBalance = params.BeaconConfig().MaxDepositAmount
 	}
-	req := &pb.ValidatorPerformanceRequest{
-		Slot:      slot,
-		PublicKey: v.key.PublicKey.Marshal(),
-	}
-	resp, err := v.validatorClient.ValidatorPerformance(ctx, req)
-	if err != nil {
-		return err
-	}
-	newBalance := float64(resp.Balance) / float64(params.BeaconConfig().GweiPerEth)
-	log.WithFields(logrus.Fields{
-		"slot":  slot - params.BeaconConfig().GenesisSlot,
-		"epoch": (slot / params.BeaconConfig().SlotsPerEpoch) - params.BeaconConfig().GenesisEpoch,
-	}).Info("Start of a new epoch!")
-	log.WithFields(logrus.Fields{
-		"totalValidators":     resp.TotalValidators,
-		"numActiveValidators": resp.TotalActiveValidators,
-	}).Infof("Validator registry information")
-	log.Info("Generating validator performance report from the previous epoch...")
-	log.WithFields(logrus.Fields{
-		"ethBalance": newBalance,
-	}).Info("New validator balance")
-	avgBalance := resp.AverageValidatorBalance / float32(params.BeaconConfig().GweiPerEth)
-	if v.prevBalance > 0 {
-		prevBalance := float64(v.prevBalance) / float64(params.BeaconConfig().GweiPerEth)
-		percentNet := (newBalance - prevBalance) / prevBalance
-		log.WithField("prevEthBalance", prevBalance).Info("Previous validator balance")
-		if !v.ctxCli.GlobalBool(types.DisablePenaltyRewardLogFlag.Name) {
-			log.WithFields(logrus.Fields{
-				"eth":           fmt.Sprintf("%f", newBalance-prevBalance),
-				"percentChange": fmt.Sprintf("%.2f%%", percentNet*100),
-			}).Info("Net gains/losses in eth")
+	var totalPrevBalance uint64
+	reported := false
+	for _, pkey := range v.pubkeys {
+		req := &pb.ValidatorPerformanceRequest{
+			Slot:      slot,
+			PublicKey: pkey,
 		}
-		var totalPrevBalance uint64
-		reported := false
-		for _, pkey := range v.pubkeys {
-			req := &pb.ValidatorPerformanceRequest{
-				Slot:      slot,
-				PublicKey: pkey,
+		resp, err := v.validatorClient.ValidatorPerformance(ctx, req)
+		if err != nil {
+			if strings.Contains(err.Error(), "could not get validator index") {
+				continue
 			}
-			resp, err := v.validatorClient.ValidatorPerformance(ctx, req)
-			if err != nil {
-				if strings.Contains(err.Error(), "could not get validator index") {
-					continue
-				}
-				return err
-			}
-			tpk := hex.EncodeToString(pkey)[:12]
-			if !reported {
-				log.WithFields(logrus.Fields{
-					"slot":  slot - params.BeaconConfig().GenesisSlot,
-					"epoch": (slot / params.BeaconConfig().SlotsPerEpoch) - params.BeaconConfig().GenesisEpoch,
-				}).Info("Start of a new epoch!")
-				log.WithFields(logrus.Fields{
-					"totalValidators":     resp.TotalValidators,
-					"numActiveValidators": resp.TotalActiveValidators,
-				}).Infof("Validator registry information")
-				log.Info("Generating validator performance report from the previous epoch...")
-				avgBalance := resp.AverageValidatorBalance / float32(params.BeaconConfig().GweiPerEth)
-				log.WithField(
-					"averageEthBalance", fmt.Sprintf("%f", avgBalance),
-				).Info("Average eth balance per validator in the beacon chain")
-				reported = true
-			}
-			newBalance := float64(resp.Balance) / float64(params.BeaconConfig().GweiPerEth)
+			return err
+		}
+		tpk := hex.EncodeToString(pkey)[:12]
+		if !reported {
 			log.WithFields(logrus.Fields{
-				"ethBalance": newBalance,
-			}).Infof("%v New validator balance", tpk)
+				"slot":  slot - params.BeaconConfig().GenesisSlot,
+				"epoch": (slot / params.BeaconConfig().SlotsPerEpoch) - params.BeaconConfig().GenesisEpoch,
+			}).Info("Start of a new epoch!")
+			log.WithFields(logrus.Fields{
+				"totalValidators":     resp.TotalValidators,
+				"numActiveValidators": resp.TotalActiveValidators,
+			}).Infof("Validator registry information")
+			log.Info("Generating validator performance report from the previous epoch...")
+			avgBalance := resp.AverageValidatorBalance / float32(params.BeaconConfig().GweiPerEth)
+			log.WithField(
+				"averageEthBalance", fmt.Sprintf("%f", avgBalance),
+			).Info("Average eth balance per validator in the beacon chain")
+			reported = true
+		}
+		newBalance := float64(resp.Balance) / float64(params.BeaconConfig().GweiPerEth)
+		log.WithFields(logrus.Fields{
+			"ethBalance": newBalance,
+		}).Infof("%v New validator balance", tpk)
 
-			if v.prevBalance > 0 {
-				prevBalance := float64(v.prevBalance) / float64(params.BeaconConfig().GweiPerEth)
-				percentNet := (newBalance - prevBalance) / prevBalance
-				log.WithField("prevEthBalance", prevBalance).Infof("%v Previous validator balance", tpk)
-				if !v.ctxCli.GlobalBool(types.DisablePenaltyRewardLogFlag.Name) {
-					log.WithFields(logrus.Fields{
-						"eth":           fmt.Sprintf("%f", newBalance-prevBalance),
-						"percentChange": fmt.Sprintf("%.2f%%", percentNet*100),
-					}).Infof("%v Net gains/losses in eth", tpk)
-				}
+		if v.prevBalance > 0 {
+			prevBalance := float64(v.prevBalance) / float64(params.BeaconConfig().GweiPerEth)
+			percentNet := (newBalance - prevBalance) / prevBalance
+			log.WithField("prevEthBalance", prevBalance).Infof("%v Previous validator balance", tpk)
+			if !v.ctxCli.GlobalBool(types.DisablePenaltyRewardLogFlag.Name) {
+				log.WithFields(logrus.Fields{
+					"eth":           fmt.Sprintf("%f", newBalance-prevBalance),
+					"percentChange": fmt.Sprintf("%.2f%%", percentNet*100),
+				}).Infof("%v Net gains/losses in eth", tpk)
 			}
-			totalPrevBalance += resp.Balance
 		}
-		v.prevBalance = totalPrevBalance
+		totalPrevBalance += resp.Balance
 	}
+
+	v.prevBalance = totalPrevBalance
 	return nil
 }
