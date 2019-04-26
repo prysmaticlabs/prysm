fcc4c7f343bb7ae28d3567eaa203c3ff21260eae
Terence Tsao <terence@prysmaticlabs.com>
2019-04-26 11:25:09 -0700

    add space

--- beacon-chain/core/helpers/committee.go
+++ beacon-chain/core/helpers/committee.go
@@ -160,6 +160,7 @@ func CrosslinkCommitteesAtSlot(
 	currentEpoch := CurrentEpoch(state)
 	prevEpoch := PrevEpoch(state)
 	nextEpoch := NextEpoch(state)
+
 	switch wantedEpoch {
 	case currentEpoch:
 		return currEpochCommitteesAtSlot(state, slot)
