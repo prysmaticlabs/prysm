26a43c6cd3d53534e5040df1783a44c84c725507
Terence Tsao <terence@prysmaticlabs.com>
2019-04-29 11:55:27 -0700

    space

--- beacon-chain/operations/service_test.go
+++ beacon-chain/operations/service_test.go
@@ -7,11 +7,10 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/prysmaticlabs/prysm/shared/hashutil"
-
 	"github.com/gogo/protobuf/proto"
 	"github.com/prysmaticlabs/prysm/beacon-chain/internal"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/sirupsen/logrus"
