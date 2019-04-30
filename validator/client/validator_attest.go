8587d52e4fd75a142f5ef5e83952c58f465bdf12
Raul Jordan <raul@prysmaticlabs.com>
2019-04-30 16:24:27 -0500

    att logging

--- validator/client/validator_attest.go
+++ validator/client/validator_attest.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"time"
 
 	pbp2p "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
@@ -138,12 +139,9 @@ func (v *validator) AttestToBlockHead(ctx context.Context, slot uint64, idx stri
 		return
 	}
 	log.WithFields(logrus.Fields{
-		"headHash":        fmt.Sprintf("%#x", attData.BeaconBlockRootHash32),
-		"sourceHash":      fmt.Sprintf("%#x", attData.JustifiedBlockRootHash32),
-		"targetHash":      fmt.Sprintf("%#x", attData.EpochBoundaryRootHash32),
+		"headRoot":        fmt.Sprintf("%#x", bytesutil.Trunc(attData.BeaconBlockRootHash32)),
 		"slot":            attData.Slot - params.BeaconConfig().GenesisSlot,
 		"shard":           attData.Shard,
-		"attestationHash": fmt.Sprintf("%#x", attResp.AttestationHash),
 		"validator":       truncatedPk,
 	}).Info("Attested latest head")
 	span.AddAttributes(
