b68def802a15be7091393fab171c7abf9912ef6f
Terence Tsao <terence@prysmaticlabs.com>
2019-05-02 10:11:15 -0700

    move exported function above

--- beacon-chain/core/epoch/epoch_processing.go
+++ beacon-chain/core/epoch/epoch_processing.go
@@ -671,6 +671,25 @@ func CrosslinkAttestingIndices(state *pb.BeaconState, crosslink *pb.Crosslink, a
 	return UnslashedAttestingIndices(state, crosslinkAtts)
 }
 
+// BaseReward takes state and validator index and calculate
+// individual validator's base reward quotient.
+//
+// Spec pseudocode definition:
+//	def get_base_reward(state: BeaconState, index: ValidatorIndex) -> Gwei:
+//    adjusted_quotient = integer_squareroot(get_total_active_balance(state)) // BASE_REWARD_QUOTIENT
+//    if adjusted_quotient == 0:
+//        return 0
+//    return state.validator_registry[index].effective_balance // adjusted_quotient // BASE_REWARDS_PER_EPOCH
+func BaseReward(state *pb.BeaconState, index uint64) uint64 {
+	adjustedQuotient := mathutil.IntegerSquareRoot(totalActiveBalance(state) /
+		params.BeaconConfig().BaseRewardQuotient)
+	if adjustedQuotient == 0 {
+		return 0
+	}
+	baseReward := state.ValidatorRegistry[index].EffectiveBalance / adjustedQuotient
+	return baseReward / params.BeaconConfig().BaseRewardsPerEpoch
+}
+
 // attsForCrosslink returns the attestations of the input crosslink.
 func attsForCrosslink(state *pb.BeaconState, crosslink *pb.Crosslink, atts []*pb.PendingAttestation) []*pb.PendingAttestation {
 	var crosslinkAtts []*pb.PendingAttestation
@@ -689,23 +708,4 @@ func attsForCrosslink(state *pb.BeaconState, crosslink *pb.Crosslink, atts []*pb
 //    return get_total_balance(state, get_active_validator_indices(state, get_current_epoch(state)))
 func totalActiveBalance(state *pb.BeaconState) uint64 {
 	return TotalBalance(state, helpers.ActiveValidatorIndices(state.ValidatorRegistry, helpers.CurrentEpoch(state)))
-}
-
-// BaseReward takes state and validator index and calculate
-// individual validator's base reward quotient.
-//
-// Spec pseudocode definition:
-//	def get_base_reward(state: BeaconState, index: ValidatorIndex) -> Gwei:
-//    adjusted_quotient = integer_squareroot(get_total_active_balance(state)) // BASE_REWARD_QUOTIENT
-//    if adjusted_quotient == 0:
-//        return 0
-//    return state.validator_registry[index].effective_balance // adjusted_quotient // BASE_REWARDS_PER_EPOCH
-func BaseReward(state *pb.BeaconState, index uint64) uint64 {
-	adjustedQuotient := mathutil.IntegerSquareRoot(totalActiveBalance(state) /
-		params.BeaconConfig().BaseRewardQuotient)
-	if adjustedQuotient == 0 {
-		return 0
-	}
-	baseReward := state.ValidatorRegistry[index].EffectiveBalance / adjustedQuotient
-	return baseReward / params.BeaconConfig().BaseRewardsPerEpoch
 }
