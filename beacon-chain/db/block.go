379e0425f743c5c73a1c63998f090774b0a1efe3
Preston Van Loon <preston@prysmaticlabs.com>
2019-05-08 17:57:16 -0500

    Update beacon-chain/db/block.go
    Co-Authored-By: rauljordan <raul@prysmaticlabs.com>

--- beacon-chain/db/block.go
+++ beacon-chain/db/block.go
@@ -294,7 +294,7 @@ func (db *BeaconDB) CanonicalBlockBySlot(ctx context.Context, slot uint64) (*pb.
 }
 
 // BlocksBySlot accepts a slot number and returns the corresponding blocks in the db.
-// Returns nil if no blocks were recorded for the given slot.
+// Returns empty list if no blocks were recorded for the given slot.
 func (db *BeaconDB) BlocksBySlot(ctx context.Context, slot uint64) ([]*pb.BeaconBlock, error) {
 	_, span := trace.StartSpan(ctx, "BeaconDB.BlocksBySlot")
 	defer span.End()
