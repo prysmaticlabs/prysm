005d7ef496050d6f8c3964e054220753a5e5a6f7
shayzluf <thezluf@gmail.com>
2019-05-09 07:50:17 +0530

    get_domain DomainVersion refactoring

--- shared/forkutil/signature.go
+++ shared/forkutil/signature.go
@@ -1,7 +1,9 @@
 package forkutil
 
 import (
-	"math"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
@@ -36,7 +38,29 @@ func ForkVersion(fork *pb.Fork, epoch uint64) uint64 {
 //    """
 //    fork_version = get_fork_version(fork, epoch)
 //    return fork_version * 2**32 + domain_type
-func DomainVersion(fork *pb.Fork, epoch uint64, domainType uint64) uint64 {
-	offset := uint64(math.Pow(2, 32))
-	return ForkVersion(fork, epoch)*offset + domainType
+//
+//
+//  def get_domain(state: BeaconState,
+//               domain_type: int,
+//               message_epoch: int=None) -> int:
+//    """
+//    Return the signature domain (fork version concatenated with domain type) of a message.
+//    """
+//    epoch = get_current_epoch(state) if message_epoch is None else message_epoch
+//    fork_version = state.fork.previous_version if epoch < state.fork.epoch else state.fork.current_version
+//    return bytes_to_int(fork_version + int_to_bytes(domain_type, length=4))
+func DomainVersion(state *pb.BeaconState, domainType uint64, epoch uint64) uint64 {
+	if epoch == 0 {
+		epoch = helpers.CurrentEpoch(state)
+	}
+	var forkVersion []byte
+	if epoch < state.Fork.Epoch {
+		forkVersion = state.Fork.PreviousVersion
+	} else {
+		forkVersion = state.Fork.CurrentVersion
+	}
+	by := []byte{}
+	by = append(by, forkVersion...)
+	by = append(by, bytesutil.Bytes4(domainType)...)
+	return bytesutil.FromBytes8(by)
 }
