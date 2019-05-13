8b39d8a02b7a67de797adfe288bf55d385f4a0bd
Terence Tsao <terence@prysmaticlabs.com>
2019-05-13 11:33:07 -0700

    initial implementation of BeaconProposerIndex

--- beacon-chain/core/helpers/validators.go
+++ beacon-chain/core/helpers/validators.go
@@ -1,9 +1,12 @@
 package helpers
 
 import (
+	"encoding/binary"
 	"fmt"
 
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
@@ -81,38 +84,62 @@ func DelayedActivationExitEpoch(epoch uint64) uint64 {
 //        len(get_active_validator_indices(state, get_current_epoch(state))) // CHURN_LIMIT_QUOTIENT
 //    )
 func ChurnLimit(state *pb.BeaconState) uint64 {
-	validatorCount := uint64(len(ActiveValidatorIndices(state.ValidatorRegistry, CurrentEpoch(state))))
+	validatorCount := uint64(len(ActiveValidatorIndices(state, CurrentEpoch(state))))
 	if validatorCount/params.BeaconConfig().ChurnLimitQuotient > params.BeaconConfig().MinPerEpochChurnLimit {
 		return validatorCount / params.BeaconConfig().ChurnLimitQuotient
 	}
 	return params.BeaconConfig().MinPerEpochChurnLimit
 }
 
-// BeaconProposerIndex returns the index of the proposer of the block at a
-// given slot.
-// TODO(2307): Update BeaconProposerIndex to v0.6
+// BeaconProposerIndex returns proposer index of a current slot.
 //
 // Spec pseudocode definition:
-//  def get_beacon_proposer_index(state: BeaconState,slot: int) -> int:
+//  def get_beacon_proposer_index(state: BeaconState) -> ValidatorIndex:
 //    """
-//    Returns the beacon proposer index for the ``slot``.
+//    Return the current beacon proposer index.
 //    """
-//    first_committee, _ = get_crosslink_committees_at_slot(state, slot)[0]
-//    return first_committee[slot % len(first_committee)]
+//    epoch = get_current_epoch(state)
+//    committees_per_slot = get_epoch_committee_count(state, epoch) // SLOTS_PER_EPOCH
+//    offset = committees_per_slot * (state.slot % SLOTS_PER_EPOCH)
+//    shard = (get_epoch_start_shard(state, epoch) + offset) % SHARD_COUNT
+//    first_committee = get_crosslink_committee(state, epoch, shard)
+//    MAX_RANDOM_BYTE = 2**8 - 1
+//    seed = generate_seed(state, epoch)
+//    i = 0
+//    while True:
+//        candidate_index = first_committee[(epoch + i) % len(first_committee)]
+//        random_byte = hash(seed + int_to_bytes(i // 32, length=8))[i % 32]
+//        effective_balance = state.validator_registry[candidate_index].effective_balance
+//        if effective_balance * MAX_RANDOM_BYTE >= MAX_EFFECTIVE_BALANCE * random_byte:
+//            return candidate_index
+//        i += 1
 func BeaconProposerIndex(state *pb.BeaconState, slot uint64) (uint64, error) {
-	// RegistryChange is false because BeaconProposerIndex is only written
-	// to be able to get proposers from current and previous epoch following
-	// ETH2.0 beacon chain spec.
-	committeeArray, err := CrosslinkCommitteesAtSlot(state, slot)
+	e := CurrentEpoch(state)
+	committesPerSlot := EpochCommitteeCount(state, e) / params.BeaconConfig().SlotsPerEpoch
+	offSet := committesPerSlot * (state.Slot % params.BeaconConfig().SlotsPerEpoch)
+	startShard, err := EpochStartShard(state, e)
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("could not get start shard: %v", err)
 	}
-	firstCommittee := committeeArray[0].Committee
-
-	if len(firstCommittee) == 0 {
-		return 0, fmt.Errorf("empty first committee at slot %d",
-			slot-params.BeaconConfig().GenesisSlot)
+	shard := (startShard + offSet) % params.BeaconConfig().ShardCount
+	firstCommittee, err := CrosslinkCommitteeAtEpoch(state, e, shard)
+	if err != nil {
+		return 0, fmt.Errorf("could not get first committee: %v", err)
 	}
-
-	return firstCommittee[slot%uint64(len(firstCommittee))], nil
+	maxRandomByte := uint64(8<<1 - 1)
+	seed, err := GenerateSeed(state, e)
+	if err != nil {
+		return 0, fmt.Errorf("could not generate seed: %v", err)
+	}
+	for i := uint64(0); ; i++ {
+		candidateIndex := firstCommittee[(e+i)%uint64(len(firstCommittee))]
+		b := append(seed[:], bytesutil.Bytes8(i)...)
+		randomByte := hashutil.Hash(b)[i%32]
+		effectiveBal := state.ValidatorRegistry[candidateIndex].EffectiveBalance
+		binary.LittleEndian.Uint64([]byte{randomByte})
+		if effectiveBal*maxRandomByte >=
+			params.BeaconConfig().MaxEffectiveBalance*binary.LittleEndian.Uint64([]byte{randomByte}) {
+			return candidateIndex, nil
+		}
+	}
 }
