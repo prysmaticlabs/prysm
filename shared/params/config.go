164b89c18c52e7258a2a69ec43b8841f64ec9db3
Preston Van Loon <preston@prysmaticlabs.com>
2019-05-17 19:11:02 -0400

    go back to 8

--- shared/params/config.go
+++ shared/params/config.go
@@ -212,7 +212,7 @@ func DemoBeaconConfig() *BeaconChainConfig {
 	demoConfig.MinAttestationInclusionDelay = 1
 	demoConfig.TargetCommitteeSize = 1
 	demoConfig.DepositsForChainStart = 8
-	demoConfig.SlotsPerEpoch = 16
+	demoConfig.SlotsPerEpoch = 8
 	demoConfig.GenesisEpoch = demoConfig.GenesisSlot / demoConfig.SlotsPerEpoch
 	demoConfig.MinDepositAmount = 100
 	demoConfig.MaxDepositAmount = 3.2 * 1e9
