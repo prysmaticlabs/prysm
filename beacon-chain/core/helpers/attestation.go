cb61280d8e73f0a4e4b2fd123c6f98f61438f2df
Andrei Ivasko <cyberbono3@gmail.com>
2019-05-17 15:33:24 -0400

    attestation.go is ready, tests not

---
+++ beacon-chain/core/helpers/attestation.go
@@ -0,0 +1,21 @@
+package helpers
+
+import (
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+)	
+
+// AttestationDataSlot returns current slot of AttestationData
+//
+// Spec pseudocode definition:
+// def get_attestation_data_slot(state: BeaconState, data: AttestationData) -> Slot:
+// committee_count = get_epoch_committee_count(state, data.target_epoch)
+// offset = (data.crosslink.shard + SHARD_COUNT - get_epoch_start_shard(state, data.target_epoch)) % SHARD_COUNT
+// return get_epoch_start_slot(data.target_epoch) + offset // (committee_count // SLOTS_PER_EPOCH)
+
+func AttestionDataSlot(state *pb.BeaconState, data *pb.AttestationData) (uint64)   {
+    commiteeCount := EpochCommitteeCount(state, data.TargetEpoch)
+	offset := (data.Crosslink.Shard  + params.BeaconConfig().ShardCount - 
+	EpochStartShard(state, data.TargetEpoch))%params.BeaconConfig().ShardCount
+
+	return StartSlot(data.TargetEpoch) + offset / (commiteeCount / params.BeaconConfig().SlotsPerEpoch)
+}
