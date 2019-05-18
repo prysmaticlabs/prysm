a37b49c19e443e9c9beabdefc89a4dbd118eb70a
Andrei Ivasko <cyberbono3@gmail.com>
2019-05-18 14:27:49 -0400

    fixing linter issues

--- beacon-chain/core/helpers/attestation.go
+++ beacon-chain/core/helpers/attestation.go
@@ -12,7 +12,7 @@ import (
 // committee_count = get_epoch_committee_count(state, data.target_epoch)
 // offset = (data.crosslink.shard + SHARD_COUNT - get_epoch_start_shard(state, data.target_epoch)) % SHARD_COUNT
 // return get_epoch_start_slot(data.target_epoch) + offset // (committee_count // SLOTS_PER_EPOCH)
-func AttestionDataSlot(state *pb.BeaconState, data *pb.AttestationData) uint64 {
+func AttestationDataSlot(state *pb.BeaconState, data *pb.AttestationData) uint64 {
 	commiteeCount := EpochCommitteeCount(state, data.TargetEpoch)
 	offset := (data.Crosslink.Shard + params.BeaconConfig().ShardCount -
 		EpochStartShard(state, data.TargetEpoch)) % params.BeaconConfig().ShardCount
