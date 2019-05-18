3330ab08770911d1aa1df85f1ce81bd20c7dd889
Raul Jordan <raul@prysmaticlabs.com>
2019-05-18 16:15:00 -0400

    Update beacon-chain/core/blocks/block_operations_test.go
    Co-Authored-By: terence tsao <terence@prysmaticlabs.com>

--- beacon-chain/core/blocks/block_operations_test.go
+++ beacon-chain/core/blocks/block_operations_test.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/prysmaticlabs/prysm/shared/hashutil"
-
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
