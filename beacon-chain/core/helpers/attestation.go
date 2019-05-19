f3a0c6fd542cf87cfff3526fe30cba7d78ee57cc
Andrei Ivasko <cyberbono3@gmail.com>
2019-05-18 20:49:14 -0400

    fixed error

--- beacon-chain/core/helpers/attestation.go
+++ beacon-chain/core/helpers/attestation.go
@@ -18,7 +18,7 @@ func AttestationDataSlot(state *pb.BeaconState, data *pb.AttestationData) (uint6
 	commiteeCount := EpochCommitteeCount(state, data.TargetEpoch)
 	epochStartShardNumber, err := EpochStartShard(state, data.TargetEpoch)
 	if err != nil {
-		return 0, fmt.Errorf("EpochStartShard error: %v", err)
+		return 0, fmt.Errorf("epochStartShard error: %v", err)
 	}
 	offset := (data.Crosslink.Shard + params.BeaconConfig().ShardCount -
 		epochStartShardNumber) % params.BeaconConfig().ShardCount
