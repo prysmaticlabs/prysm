61f95a1252a2a09e73c5413b48295e2ec4cff49e
Terence Tsao <terence@prysmaticlabs.com>
2019-06-07 12:51:35 -0400

    run mockgen

--- validator/internal/proposer_service_mock.go
+++ validator/internal/proposer_service_mock.go
@@ -136,3 +136,23 @@ func (mr *MockProposerServiceClientMockRecorder) ProposeBlock(arg0, arg1 interfa
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ProposeBlock", reflect.TypeOf((*MockProposerServiceClient)(nil).ProposeBlock), varargs...)
 }
+
+// RequestBlock mocks base method
+func (m *MockProposerServiceClient) RequestBlock(arg0 context.Context, arg1 *v10.BlockRequest, arg2 ...grpc.CallOption) (*v1.BeaconBlock, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "RequestBlock", varargs...)
+	ret0, _ := ret[0].(*v1.BeaconBlock)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// RequestBlock indicates an expected call of RequestBlock
+func (mr *MockProposerServiceClientMockRecorder) RequestBlock(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RequestBlock", reflect.TypeOf((*MockProposerServiceClient)(nil).RequestBlock), varargs...)
+}
