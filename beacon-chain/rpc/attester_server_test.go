d7de1f3ad77fbdab6131ac5b60e471cc2542274f
Terence Tsao <terence@prysmaticlabs.com>
2019-06-10 09:51:19 -0700

    fixed bazel imports

--- beacon-chain/rpc/attester_server_test.go
+++ beacon-chain/rpc/attester_server_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
-	"github.com/prysmaticlabs/go-ssz"
 	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/internal"
@@ -14,6 +13,7 @@ import (
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
 	"github.com/prysmaticlabs/prysm/shared/hashutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
+	"github.com/prysmaticlabs/prysm/shared/ssz"
 )
 
 type mockBroadcaster struct{}
