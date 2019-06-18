db710b19f7eafb0afb92a82bcd203b667862612c
Terence Tsao <terence@prysmaticlabs.com>
2019-06-18 14:20:18 -0700

    process registry updates passing!

--- beacon-chain/core/epoch/spectest/epoch_processing_registry_test.go
+++ beacon-chain/core/epoch/spectest/epoch_processing_registry_test.go
@@ -3,35 +3,63 @@ package spectest
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"reflect"
 	"testing"
 
+	"github.com/ghodss/yaml"
 	"github.com/gogo/protobuf/jsonpb"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/epoch"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/params/spectest"
 )
 
 func TestRegistryProcessingYaml(t *testing.T) {
-	for _, testCase := range registryUpdateTests.TestCases {
-		t.Logf("Description: %s", testCase.Description)
+	file, err := ioutil.ReadFile("registry_updates_minimal_formatted.yaml")
+	if err != nil {
+		t.Fatalf("Could not load file %v", err)
+	}
+
+	s := &RegistryUpdatesMinimal{}
+	if err := yaml.Unmarshal(file, s); err != nil {
+		t.Fatalf("Failed to Unmarshal: %v", err)
+	}
+
+	if err := spectest.SetConfig(s.Config); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Running spec test vectors for %s", s.Title)
+	for _, testCase := range s.TestCases {
+		preState := &pb.BeaconState{}
+		t.Logf("Testing testcase %s", testCase.Description)
 		b, err := json.Marshal(testCase.Pre)
 		if err != nil {
 			t.Fatal(err)
 		}
-		var preState *pb.BeaconState
-		var postState *pb.BeaconState
-
 		err = jsonpb.Unmarshal(bytes.NewReader(b), preState)
 		if err != nil {
 			t.Fatal(err)
 		}
+
+		var postState *pb.BeaconState
 
 		postState, err = epoch.ProcessRegistryUpdates(preState)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if !reflect.DeepEqual(postState, testCase.Post) {
+		genPostState := &pb.BeaconState{}
+		b, err = json.Marshal(testCase.Post)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = jsonpb.Unmarshal(bytes.NewReader(b), genPostState)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(postState, genPostState) {
 			t.Error("Process registry updates mutated state differently than yaml output")
 		}
 	}
