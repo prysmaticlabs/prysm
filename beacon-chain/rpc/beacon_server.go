ff614f09a5456cf419c790a9ba25ead2fa5c58be
shayzluf <thezluf@gmail.com>
2019-06-21 14:01:59 +0300

    eth1data comment fix

--- beacon-chain/rpc/beacon_server.go
+++ beacon-chain/rpc/beacon_server.go
@@ -137,11 +137,9 @@ func (bs *BeaconServer) BlockTree(ctx context.Context, _ *ptypes.Empty) (*pb.Blo
 	}, nil
 }
 
-// eth1Data is a mechanism used by block proposers vote on a recent Ethereum 1.0 block hash and an
-// associated deposit root found in the Ethereum 1.0 deposit contract. When consensus is formed,
-// state.latest_eth1_data is updated, and validator deposits up to this root can be processed.
-// The deposit root can be calculated by calling the get_deposit_root() function of
-// the deposit contract using the post-state of the block hash.
+// eth1Data takes all eth1data votes validate the relevant ones,
+// checks that they are valid against eth1 chain data, count them
+// and returns the best vote.
 func (bs *BeaconServer) eth1Data(ctx context.Context) (*pbp2p.Eth1Data, error) {
 	beaconState, err := bs.beaconDB.HeadState(ctx)
 	if err != nil {
