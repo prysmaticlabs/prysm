26ef11479433e3dab5a787ff9cb5ff3ac4760978
nisdas <nishdas93@gmail.com>
2019-06-21 17:53:44 +0530

    add all new changes

---
+++ shared/bls/spectest/g2_uncompressed_test.go
@@ -0,0 +1,42 @@
+package spectest
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/ghodss/yaml"
+	"github.com/prysmaticlabs/prysm/shared/bls"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+)
+
+func TestG2UncompressedHash(t *testing.T) {
+	file, err := ioutil.ReadFile("g2_uncompressed.yaml")
+	if err != nil {
+		t.Fatalf("Failed to read file: %v", err)
+	}
+
+	test := &G2Uncompressed{}
+	if err := yaml.Unmarshal(file, test); err != nil {
+		t.Fatalf("Failed to unmarshal: %v", err)
+	}
+
+	for i, tt := range test.TestCases {
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			hash := bls.HashG2WithDomainUncompressed(bytesutil.ToBytes32(tt.Input.Message), tt.Input.Domain)
+			var buf []byte
+			for _, slice := range tt.Output {
+				for _, innerSlice := range slice {
+					buf = append(buf, innerSlice...)
+				}
+			}
+			if !bytes.Equal(buf, hash[:]) {
+				t.Logf("Domain=%d", tt.Input.Domain)
+				t.Fatalf("Hash does not match the expected output. "+
+					"Expected %#x but received %#x", buf, hash)
+			}
+			t.Logf("Success. Domain=%d", tt.Input.Domain)
+		})
+	}
+}
