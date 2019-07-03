79c73070955e664e782a09dcd760ddcae6ab65e3
Nishant Das <nishdas93@gmail.com>
2019-07-03 08:30:22 +0530

    Slashing Penalty Calculation Change (#2889)
    * lint
    
    * change config val
    
    * add max helper
    
    * changes to slashing and process slashing, add a min function for integers
    
    * gaz
    
    * fix failing tests
    
    * fix test
    
    * fixed all tests
    
    * Change Yaml tag
    
    * lint
    
    * remove gc hack
    
    * fix test
    
    * gaz
    
    * preston's comments
    
    * change failing field
    
    * fix and regen proto
    
    * lint

--- shared/mathutil/math_helper.go
+++ shared/mathutil/math_helper.go
@@ -51,3 +51,27 @@ func ClosestPowerOf2(n uint64) uint64 {
 	exponent := math.Floor(math.Log2(float64(n)))
 	return PowerOf2(uint64(exponent))
 }
+
+// Max returns the larger integer of the two
+// given ones.This is used over the Max function
+// in the standard math library because that max function
+// has to check for some special floating point cases
+// making it slower by a magnitude of 10.
+func Max(a uint64, b uint64) uint64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// Min returns the smaller integer of the two
+// given ones. This is used over the Min function
+// in the standard math library because that min function
+// has to check for some special floating point cases
+// making it slower by a magnitude of 10.
+func Min(a uint64, b uint64) uint64 {
+	if a < b {
+		return a
+	}
+	return b
+}
