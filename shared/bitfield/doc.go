dc639a40f7d1db4b095209b7be49353b38c2d552
Preston Van Loon <preston@prysmaticlabs.com>
2019-07-05 12:27:10 -0400

    comment that it is little endian

--- shared/bitfield/doc.go
+++ shared/bitfield/doc.go
@@ -3,7 +3,7 @@
 // A bitfield is also known as a Bitlist or Bitvector in Ethereum 2.0 spec.
 // Both variants are static arrays in that they cannot dynamically change in
 // size after being constructed. These data types represent a list of bits whose
-// value is treated akin to a boolean.
+// value is treated akin to a boolean. The bits are in little endian order.
 //
 // 	Bitvector - A list of bits that is fixed in size.
 // 	Bitlist - A list of bits that is determined at runtime.
