18d7f909e2f09eafce769f3ee5fe54b22b2bd2f7
Nishant Das <nishdas93@gmail.com>
2019-07-10 17:43:35 +0530

    Revert "Update zero hash to sha256().digest() (#2932)" (#2933)
    This reverts commit b926ae0667b18aef3f7e0e8ec8a9b3e98b3d2ccc.

--- shared/params/config_test.go
+++ shared/params/config_test.go
@@ -1,8 +1,6 @@
 package params
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"testing"
 )
 
@@ -12,12 +10,5 @@ func TestOverrideBeaconConfig(t *testing.T) {
 	OverrideBeaconConfig(cfg)
 	if c := BeaconConfig(); c.ShardCount != 5 {
 		t.Errorf("Shardcount in BeaconConfig incorrect. Wanted %d, got %d", 5, c.ShardCount)
-	}
-}
-
-func TestMainnetConfig_ZeroHashIsAliasOfSha256(t *testing.T) {
-	h := sha256.New()
-	if !bytes.Equal(BeaconConfig().ZeroHash[:], h.Sum(nil)) {
-		t.Error("Zero hash is not sha256.New().Sum()")
 	}
 }
