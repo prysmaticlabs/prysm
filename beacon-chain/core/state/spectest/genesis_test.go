b6f9e73d9206fff7a4f9e085feff0f7ee3dc2740
Ivan Martinez <ivanthegreat15@gmail.com>
2019-07-11 13:39:15 -0400

    Start genesis spec tests

---
+++ beacon-chain/core/state/spectest/genesis_test.go
@@ -0,0 +1,90 @@
+package spectest
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel"
+	"github.com/ghodss/yaml"
+	"github.com/gogo/protobuf/proto"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/params/spectest"
+	"gopkg.in/d4l3k/messagediff.v1"
+)
+
+func TestGenesisInitializationMinimal(t *testing.T) {
+	filepath, err := bazel.Runfile("/eth2_spec_tests/tests/genesis/initialization/genesis_initialization_minimal.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		t.Fatalf("Could not load file %v", err)
+	}
+
+	s := &GenesisInitializationTest{}
+	if err := yaml.Unmarshal(file, s); err != nil {
+		t.Fatalf("Failed to Unmarshal: %v", err)
+	}
+
+	if err := spectest.SetConfig(s.Config); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tt := range s.TestCases {
+		t.Run(tt.Description, func(t *testing.T) {
+			eth1Data := &pb.Eth1Data{
+				BlockHash: tt.Eth1BlockHash,
+			}
+			genesisState, err := state.GenesisBeaconState(tt.Deposits, tt.Eth1Timestamp, eth1Data)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !proto.Equal(genesisState, tt.State) {
+				diff, _ := messagediff.PrettyDiff(genesisState, tt.State)
+				t.Log(diff)
+				t.Fatal("Genesis state does not match expected")
+			}
+		})
+	}
+}
+
+func TestGenesisValidityMinimal(t *testing.T) {
+	filepath, err := bazel.Runfile("/eth2_spec_tests/tests/genesis/validity/genesis_validity_minimal.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		t.Fatalf("Could not load file %v", err)
+	}
+
+	s := &GensisValidityTest{}
+	if err := yaml.Unmarshal(file, s); err != nil {
+		t.Fatalf("Failed to Unmarshal: %v", err)
+	}
+
+	if err := spectest.SetConfig(s.Config); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tt := range s.TestCases {
+		t.Run(tt.Description, func(t *testing.T) {
+			eth1Data := &pb.Eth1Data{
+				BlockHash: tt.Eth1BlockHash,
+			}
+			genesisState, err := state.GenesisBeaconState(tt.Deposits, tt.Eth1Timestamp, eth1Data)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !proto.Equal(genesisState, tt.State) {
+				diff, _ := messagediff.PrettyDiff(genesisState, tt.State)
+				t.Log(diff)
+				t.Fatal("Genesis state does not match expected")
+			}
+		})
+	}
+}
