88767a575fc8467c49135d303ab8c7878a6538c2
Preston Van Loon <preston@prysmaticlabs.com>
2019-07-19 23:40:07 -0400

    use docker tag from environment, if exists

--- scripts/workspace_status.sh
+++ scripts/workspace_status.sh
@@ -2,4 +2,11 @@
 
 echo STABLE_GIT_COMMIT $(git rev-parse HEAD)
 echo DATE $(date --rfc-3339=seconds --utc)
-echo DOCKER_TAG $(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)
+
+# Use DOCKER_TAG from environment, if exists.
+if [ $(printenv DOCKER_TAG) ]
+then
+    echo DOCKER_TAG $(printenv DOCKER_TAG)
+else
+    echo DOCKER_TAG $(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)
+fi
