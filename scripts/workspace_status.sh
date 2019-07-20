c92b2750df0b07df5e3a0c7b99c4d42fad9dec1a
Preston Van Loon <preston@prysmaticlabs.com>
2019-07-20 00:26:24 -0400

    use container bundles to upload multiple image tags

--- scripts/workspace_status.sh
+++ scripts/workspace_status.sh
@@ -2,11 +2,4 @@
 
 echo STABLE_GIT_COMMIT $(git rev-parse HEAD)
 echo DATE $(date --rfc-3339=seconds --utc)
-
-# Use DOCKER_TAG from environment, if exists.
-if [ $(printenv DOCKER_TAG) ]
-then
-    echo DOCKER_TAG $(printenv DOCKER_TAG)
-else
-    echo DOCKER_TAG $(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)
-fi
+echo DOCKER_TAG $(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)
