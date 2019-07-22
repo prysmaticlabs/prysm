6a7f7fd68b27432ab72dfe3a00c571da46d345f6
Preston Van Loon <preston@prysmaticlabs.com>
2019-07-21 22:03:17 -0400

    add nested proto example

--- shared/testutil/json_to_pb_converter_test.go
+++ shared/testutil/json_to_pb_converter_test.go
@@ -26,3 +26,37 @@ func TestOverrideSpecName(t *testing.T) {
 		t.Errorf("Expected output.Bar to be baz")
 	}
 }
+
+func TestOverrideSpecName_NestedProto(t *testing.T) {
+	input := struct {
+		Fuzz string `json:"fuzz"`
+		Msg  struct {
+			Foo string `json:"foo"`
+			Baz string `json:"baz"`
+		}
+	}{
+		Fuzz: "fuzz",
+		Msg: struct {
+			Foo string `json:"foo"`
+			Baz string `json:"baz"`
+		}{
+			Foo: "foo",
+			Baz: "baz",
+		},
+	}
+
+	output := &pb.TestNestedMessage{}
+	if err := ConvertToPb(input, output); err != nil {
+		t.Fatal(err)
+	}
+
+	if output.Fuzz != "fuzz" {
+		t.Error("Expected output.fuzz to be fuzz")
+	}
+	if output.Msg.Foo != "foo" {
+		t.Error("Expected output.Msg.Foo to be foo")
+	}
+	if output.Msg.Bar != "baz" {
+		t.Errorf("Expected output.Msg.Bar to be baz")
+	}
+}
