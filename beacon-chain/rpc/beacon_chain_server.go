b78a888673375247d976eb16c7aed5d3132512e0
Terence Tsao <terence@prysmaticlabs.com>
2019-07-22 17:16:01 -0700

    Tests

--- beacon-chain/rpc/beacon_chain_server.go
+++ beacon-chain/rpc/beacon_chain_server.go
@@ -2,6 +2,8 @@ package rpc
 
 import (
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
@@ -17,42 +19,50 @@ type BeaconChainServer struct {
 // ListValidatorBalances retrieves the validator balances for a given set of public key at
 // a specific epoch in time.
 //
-// TODO(3045): Implement balances at a specific epoch. Current implementation returns latest balances,
-// blocked by DB refactor
+// TODO(3045): Implement balances for a specific epoch. Current implementation returns latest balances,
+// this is blocked by DB refactor
 func (bs *BeaconChainServer) ListValidatorBalances(
 	ctx context.Context,
 	req *ethpb.GetValidatorBalancesRequest) (*ethpb.ValidatorBalances, error) {
 
 	res := make([]*ethpb.ValidatorBalances_Balance, 0, len(req.PublicKeys)+len(req.Indices))
-	filtered := map[uint64]bool{} // track filtered validator to prevent duplication
+	filtered := map[uint64]bool{} // track filtered validators to prevent duplication in respond.
 
 	balances, err := bs.beaconDB.Balances(ctx)
 	if err != nil {
-		// TODO: return grpc error
-		return nil, err
+		return nil, status.Errorf(codes.Internal, "could not retrieve validator balances: %v", err)
 	}
 	validators, err := bs.beaconDB.Validators(ctx)
 	if err != nil {
-		// TODO: return grpc error
-		return nil, err
+		return nil, status.Errorf(codes.Internal, "could not retrieve validator registries: %v", err)
 	}
 
-	for _, pubkey := range req.PublicKeys {
-		index, err := bs.beaconDB.ValidatorIndex(pubkey)
+	for _, pubKey := range req.PublicKeys {
+		index, err := bs.beaconDB.ValidatorIndex(pubKey)
 		if err != nil {
-			// TODO: return grpc error
-			return nil, err
+			return nil, status.Errorf(codes.Internal, "could not retrieve validator index: %v", err)
 		}
 		filtered[index] = true
 
+		if int(index) >= len(balances) {
+			return nil, status.Errorf(codes.OutOfRange, "validator index %d >= balance list %d",
+				index, len(balances))
+		}
+
 		res = append(res, &ethpb.ValidatorBalances_Balance{
-			PublicKey: pubkey,
+			PublicKey: pubKey,
 			Index:     index,
 			Balance:   balances[index],
 		})
 	}
 
 	for _, index := range req.Indices {
+
+		if int(index) >= len(balances) {
+			return nil, status.Errorf(codes.OutOfRange, "validator index %d >= balance list %d",
+				index, len(balances))
+		}
+
 		if !filtered[index] {
 			res = append(res, &ethpb.ValidatorBalances_Balance{
 				PublicKey: validators[index].PublicKey,
