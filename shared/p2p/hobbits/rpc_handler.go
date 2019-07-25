99adcbac0686737333e3cf4c3155feac5f6ac243
rene <41963722+renaynay@users.noreply.github.com>
2019-07-25 17:47:45 -0400

    Test (#4)
    * more tests
    
    * resolves merge conflicts

--- shared/p2p/hobbits/rpc_handler.go
+++ shared/p2p/hobbits/rpc_handler.go
@@ -9,6 +9,7 @@ import (
 	peer "github.com/libp2p/go-libp2p-peer"
 	"github.com/pkg/errors"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	v1alpha1 "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/hashutil"
 	"github.com/prysmaticlabs/prysm/shared/p2p"
 	"github.com/renaynay/go-hobbits/encoding"
@@ -30,11 +31,11 @@ type Hello struct {
 }
 
 type GossipHeader struct {
-	MethodID uint16 `bson:"method_id"`
-	Topic string `bson:"topic"`
-	Timestamp uint64 `bson:"timestamp"`
+	MethodID    uint16   `bson:"method_id"`
+	Topic       string   `bson:"topic"`
+	Timestamp   uint64   `bson:"timestamp"`
 	MessageHash [32]byte `bson:"message_hash"`
-	Hash [32]byte `bson:"hash"`
+	Hash        [32]byte `bson:"hash"`
 }
 
 type Status struct {
@@ -44,18 +45,18 @@ type Status struct {
 
 type BlockBodiesRequest struct {
 	StartRoot []byte `bson:"start_root"`
-	StartSlot uint64   `bson:"start_slot"`
-	Max       uint64   `bson:"max"`
-	Skip      uint64   `bson:"skip"`
-	Direction uint8    `bson:"direction"`
+	StartSlot uint64 `bson:"start_slot"`
+	Max       uint64 `bson:"max"`
+	Skip      uint64 `bson:"skip"`
+	Direction uint8  `bson:"direction"`
 }
 
 type BlockBodiesResponse struct {
-	Bodies []*pb.BeaconBlockBody `bson:"bodies"`
+	Bodies []*v1alpha1.BeaconBlockBody `bson:"bodies"`
 }
 
 type BlockBodyResponse struct {
-	Bodies *pb.BeaconBlock `bson:"bodies"`
+	Bodies *v1alpha1.BeaconBlock `bson:"bodies"`
 }
 
 type AttestationRequest struct {
@@ -63,7 +64,7 @@ type AttestationRequest struct {
 }
 
 type AttestationResponse struct {
-	Attestation pb.Attestation `bson:"attestation"`
+	Attestation v1alpha1.Attestation `bson:"attestation"`
 }
 
 func (h *HobbitsNode) status(id peer.ID, message HobbitsMessage) error {
@@ -146,26 +147,14 @@ func (h *HobbitsNode) rpcHello() Hello {
 }
 
 func (h *HobbitsNode) removePeer(id peer.ID) error {
-	peer := h.PeerConns[id]
+	peerToRemove := h.PeerConns[id]
 	delete(h.PeerConns, id)
 
-	err := peer.Close()
+	err := peerToRemove.Close()
 	if err != nil {
-		return errors.Wrap(err, "error closing connection on peer")
+		return errors.Wrap(err, "error closing connection on peerToRemove")
 	}
-	//
-	//index = 0
-	//
-	//for i, p := range h.StaticPeers {
-	//	if reflect.DeepEqual(peer.RemoteAddr().String(), p) {
-	//		index = i
-	//	}
-	//}
-	//if index == 0 {
-	//	return errors.New("error removing peer from node's static peers")
-	//}
-	//h.StaticPeers = append(h.StaticPeers[:index], h.StaticPeers[index+1:]...)
-	//
+
 	return nil
 }
 
--- shared/p2p/hobbits/server_test.go
+++ shared/p2p/hobbits/server_test.go
@@ -11,6 +11,7 @@ import (
 	ttl "github.com/ReneKroon/ttlcache"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
+	v1alpha1 "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/renaynay/go-hobbits/encoding"
 	"github.com/renaynay/go-hobbits/tcp"
@@ -19,18 +20,95 @@ import (
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
-func TestHobbitsNode_Listen(t *testing.T) {
+func TestHobbitsNode_Hello(t *testing.T) {
 	fakeDB, err := db.NewDB("tmp/rene/")
 	if err != nil {
 		t.Errorf("could not generate new DB, %s", err.Error())
 	}
 
-	deposits, _ := testutil.SetupInitialDeposits(t, 5, false)
+	deposits, _ := testutil.SetupInitialDeposits(t, 5)
 	beaconState, err := state.GenesisBeaconState(deposits, 0, nil)
 
 	err = fakeDB.SaveState(context.Background(), beaconState)
 	if err != nil {
 		t.Errorf("error saving genesis state, %s", err.Error())
+	}
+
+	hobNode := Hobbits("127.0.0.1", 0, []string{}, fakeDB)
+
+	cache := ttl.NewCache()
+	cache.Set(string(make([]byte, 32)), true)
+
+	hobNode.MessageStore = cache
+
+	go func() {
+		hobNode.Listen()
+	}()
+
+	for {
+		if hobNode.Server.Addr() != nil {
+			break
+		}
+
+		time.Sleep(1)
+	}
+
+	conn, err := net.Dial("tcp", hobNode.Server.Addr().String())
+	if err != nil {
+		t.Error("could not connect to TCP server: ", err)
+	}
+
+	responseBody := Hello{
+		NodeID:               "12",
+		LatestFinalizedRoot:  [32]byte{},
+		LatestFinalizedEpoch: 0,
+		BestRoot:             [32]byte{},
+		BestSlot:             0,
+	}
+
+	marshBody, err := bson.Marshal(responseBody)
+	if err != nil {
+		t.Errorf("error bson marshaling response body")
+	}
+
+	responseHeader := RPCHeader{
+		MethodID: 0x00,
+	}
+
+	marshHeader, err := bson.Marshal(responseHeader)
+	if err != nil {
+		fmt.Println("error bson marshaling response header")
+	}
+
+	msg := HobbitsMessage{
+		Version:  CurrentHobbits,
+		Protocol: encoding.RPC,
+		Header:   marshHeader,
+		Body:     marshBody,
+	}
+
+	toSend := encoding.Marshal(encoding.Message(msg))
+
+	_, err = conn.Write(toSend)
+	if err != nil {
+		t.Error("could not write to the TCP server: ", err)
+	}
+
+	fmt.Println("writing...")
+
+	select {}
+}
+
+func TestHobbitsNode_GetBlock(t *testing.T) { //TODO finish this test
+	fakeDB, err := db.NewDB("tmp/rene/")
+	if err != nil {
+		t.Errorf("could not generate new DB, %s", err.Error())
+	}
+
+	cb1 := &v1alpha1.BeaconBlock{Slot: 999, ParentRoot: []byte{'A'}}
+	err = fakeDB.SaveBlock(cb1)
+	if err != nil {
+		t.Error("test block failed to save")
 	}
 
 	hobNode := Hobbits("127.0.0.1", 0, []string{}, fakeDB)
@@ -138,11 +216,11 @@ func TestHobbitsNode_Broadcast(t *testing.T) {
 	}
 
 	header := GossipHeader{
-		MethodID: 0,
-		Topic: "ATTESTATION",
-		Timestamp: uint64(time.Now().Unix()),
+		MethodID:    0,
+		Topic:       "ATTESTATION",
+		Timestamp:   uint64(time.Now().Unix()),
 		MessageHash: [32]byte{240, 2, 6, 253, 232, 158},
-		Hash: [32]byte{},
+		Hash:        [32]byte{},
 	}
 
 	attestation := &pb.AttestationAnnounce{
@@ -151,7 +229,7 @@ func TestHobbitsNode_Broadcast(t *testing.T) {
 
 	hobNode.Broadcast(context.WithValue(context.Background(), "message_hash", header.MessageHash), attestation)
 
-	read := <- ch
+	read := <-ch
 
 	buf := new(GossipHeader)
 
