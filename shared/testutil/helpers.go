ef6fa3de88f4670350e723c42a9ffee17d5f9a1e
Ivan Martinez <ivanthegreat15@gmail.com>
2019-07-24 21:43:07 -0400

    Fix imports

--- shared/testutil/helpers.go
+++ shared/testutil/helpers.go
@@ -7,9 +7,8 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
-
 	"github.com/prysmaticlabs/go-ssz"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/bls"
