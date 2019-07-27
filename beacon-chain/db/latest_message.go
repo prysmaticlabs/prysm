879006b895b15c85b49595216bfb44eff9d45ad5
Terence Tsao <terence@prysmaticlabs.com>
2019-07-27 14:26:29 -0700

    Imp latest msg DB getters and setters

---
+++ beacon-chain/db/latest_message.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/boltdb/bolt"
+	"github.com/gogo/protobuf/proto"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"go.opencensus.io/trace"
+)
+
+// SaveLatestMessage puts the validator's latest message record into the beacon chain db.
+func (db *BeaconDB) SaveLatestMessage(ctx context.Context, index uint64, attestation *pb.LatestMessage) error {
+	ctx, span := trace.StartSpan(ctx, "beaconDB.SaveLatestMessage")
+	defer span.End()
+
+	b := make([]byte, 64)
+	binary.LittleEndian.PutUint64(b, uint64(index))
+
+	encodedAtt, err := proto.Marshal(attestation)
+	if err != nil {
+		return err
+	}
+
+	return db.batch(func(tx *bolt.Tx) error {
+		l := tx.Bucket(latestMessageBucket)
+		return l.Put(b, encodedAtt)
+	})
+}
+
+// LatestMessage retrieves validator's latest message from the db using its index.
+func (db *BeaconDB) LatestMessage(index uint64) (*pb.LatestMessage, error) {
+	var msg *pb.LatestMessage
+
+	b := make([]byte, 64)
+	binary.LittleEndian.PutUint64(b, uint64(index))
+
+	err := db.view(func(tx *bolt.Tx) error {
+		l := tx.Bucket(latestMessageBucket)
+
+		enc := l.Get(b)
+		if enc == nil {
+			return nil
+		}
+
+		var err error
+		msg, err = createLatestMessage(enc)
+		return err
+	})
+
+	return msg, err
+}
+
+// HasLatestMessage checks if the validator has the last message stored.
+func (db *BeaconDB) HasLatestMessage(index uint64) bool {
+	b := make([]byte, 64)
+	binary.LittleEndian.PutUint64(b, uint64(index))
+
+	exists := false
+	if err := db.view(func(tx *bolt.Tx) error {
+		l := tx.Bucket(latestMessageBucket)
+		exists = l.Get(b) != nil
+		return nil
+	}); err != nil {
+		return false
+	}
+
+	return exists
+}
+
+func createLatestMessage(enc []byte) (*pb.LatestMessage, error) {
+	l := &pb.LatestMessage{}
+	if err := proto.Unmarshal(enc, l); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal encoding: %v", err)
+	}
+	return l, nil
+}
---
+++ beacon-chain/db/latest_message_test.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/gogo/protobuf/proto"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+)
+
+func TestSaveAndRetrieveLatestMessage(t *testing.T) {
+	db := setupDB(t)
+	defer teardownDB(t, db)
+
+	validator := uint64(1)
+	wanted := &pb.LatestMessage{
+		Epoch: 100,
+		Root:  []byte{'A'},
+	}
+
+	if err := db.SaveLatestMessage(context.Background(), validator, wanted); err != nil {
+		t.Fatalf("Failed to save latest message: %v", err)
+	}
+
+	received, err := db.LatestMessage(validator)
+	if err != nil {
+		t.Fatalf("Failed to get latest message: %v", err)
+	}
+	if !proto.Equal(wanted, received) {
+		t.Error("Did not receive wanted message")
+	}
+
+	validator = uint64(2)
+	received, err = db.LatestMessage(validator)
+	if err != nil {
+		t.Fatalf("Failed to get latest message: %v", err)
+	}
+	if received != nil {
+		t.Error("Unsaved latest message exists in db")
+	}
+}
+
+func TestHasLatestMessage(t *testing.T) {
+	db := setupDB(t)
+	defer teardownDB(t, db)
+
+	validator := uint64(3)
+	if err := db.SaveLatestMessage(context.Background(), validator, &pb.LatestMessage{}); err != nil {
+		t.Fatalf("Failed to save latest message: %v", err)
+	}
+	if !db.HasLatestMessage(validator) {
+		t.Error("Wanted true for has latest message")
+	}
+
+	validator = uint64(4)
+	if db.HasLatestMessage(validator) {
+		t.Error("Wanted false for has latest message")
+	}
+}
