0289620bf84286ae74fd5c9fa530d89ae43fc9e1
nisdas <nishdas93@gmail.com>
2019-08-05 18:21:49 +0530

    revert config

--- shared/params/config.go
+++ shared/params/config.go
@@ -234,9 +234,9 @@ func MainnetConfig() *BeaconChainConfig {
 func DemoBeaconConfig() *BeaconChainConfig {
 	demoConfig := MinimalSpecConfig()
 	demoConfig.MinDepositAmount = 100
-	demoConfig.MaxEffectiveBalance = 3.2 * 1e6
-	demoConfig.EjectionBalance = 0 * 1e6
-	demoConfig.EffectiveBalanceIncrement = 0.1 * 1e6
+	demoConfig.MaxEffectiveBalance = 3.2 * 1e9
+	demoConfig.EjectionBalance = 1.6 * 1e9
+	demoConfig.EffectiveBalanceIncrement = 0.1 * 1e9
 	demoConfig.SyncPollingInterval = 1 * 10 // Query nodes over the network every slot.
 	demoConfig.MinGenesisTime = 0
 	demoConfig.Eth1FollowDistance = 16
@@ -254,7 +254,7 @@ func MinimalSpecConfig() *BeaconChainConfig {
 	minimalConfig.ChurnLimitQuotient = 65536
 	minimalConfig.BaseRewardsPerEpoch = 5
 	minimalConfig.ShuffleRoundCount = 10
-	minimalConfig.MinGenesisActiveValidatorCount = 16
+	minimalConfig.MinGenesisActiveValidatorCount = 64
 	minimalConfig.DepositContractTreeDepth = 32
 	minimalConfig.MinDepositAmount = 1e9
 	minimalConfig.MaxEffectiveBalance = 32e9
