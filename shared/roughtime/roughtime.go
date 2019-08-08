c27e3791af7ee3414f5d522c4d74e945bc660200
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-08-08 10:10:45 +0700

    Remove copyright preemble, this is not from go-ethereum

--- shared/roughtime/roughtime.go
+++ shared/roughtime/roughtime.go
@@ -1,25 +1,9 @@
-// Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 // Package roughtime is a wrapper for a roughtime clock source
 package roughtime
 
 import (
-	"time"
 	"encoding/base64"
+	"time"
 
 	"github.com/cloudflare/roughtime"
 	"roughtime.googlesource.com/roughtime.git/go/config"
