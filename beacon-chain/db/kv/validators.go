0eac2161ed130d6ac30322ee766e99c00c8f1186
Raul Jordan <raul@prysmaticlabs.com>
2019-08-09 14:40:25 -0500

    proper comments

--- beacon-chain/db/kv/validators.go
+++ beacon-chain/db/kv/validators.go
@@ -6,43 +6,43 @@ import (
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
-// Attestation retrieval by root.
+// ValidatorLatestVote retrieval by validator index.
 // TODO(#3164): Implement.
 func (k *Store) ValidatorLatestVote(ctx context.Context, validatorIdx uint64) (*pb.ValidatorLatestVote, error) {
 	return nil, nil
 }
 
-// Attestations retrieves a list of attestations by filter criteria.
+// HasValidatorLatestVote verifies if a validator index has a latest vote stored in the db.
 // TODO(#3164): Implement.
 func (k *Store) HasValidatorLatestVote(ctx context.Context, validatorIdx uint64) bool {
 	return false
 }
 
-// HasAttestation checks if an attestation by root exists in the db.
+// SaveValidatorLatestVote by validator index.
 // TODO(#3164): Implement.
 func (k *Store) SaveValidatorLatestVote(ctx context.Context, validatorIdx uint64, vote *pb.ValidatorLatestVote) error {
 	return nil
 }
 
-// DeleteAttestation by root.
+// ValidatorIndex by public key.
 // TODO(#3164): Implement.
 func (k *Store) ValidatorIndex(ctx context.Context, publicKey [48]byte) (uint64, error) {
 	return 0, nil
 }
 
-// SaveAttestation to the db.
+// HasValidatorIndex verifies if a validator's index by public key exists in the db.
 // TODO(#3164): Implement.
 func (k *Store) HasValidatorIndex(ctx context.Context, publicKey [48]byte) bool {
 	return false
 }
 
-// SaveAttestations via batch updates to the db.
+// DeleteValidatorIndex clears a validator index from the db by the validator's public key.
 // TODO(#3164): Implement.
 func (k *Store) DeleteValidatorIndex(ctx context.Context, publicKey [48]byte) error {
 	return nil
 }
 
-// SaveAttestations via batch updates to the db.
+// SaveValidatorIndex by public key in the db.
 // TODO(#3164): Implement.
 func (k *Store) SaveValidatorIndex(ctx context.Context, publicKey [48]byte, validatorIdx uint64) error {
 	return nil
