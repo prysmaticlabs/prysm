22df351e89d319afac3a1ae40b81b88c743063cb
Nishant Das <nishdas93@gmail.com>
2019-08-12 16:29:57 +0530

    Request Missing Logs (#3173)
    * add request for missing logs
    
    * fix formatting
    
    * add metric
    
    * add test
    
    * fix test

--- beacon-chain/powchain/log_processing_test.go
+++ beacon-chain/powchain/log_processing_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/binary"
 	"io/ioutil"
+	"math/big"
 	"testing"
 	"time"
 
@@ -375,6 +376,72 @@ func TestProcessETH2GenesisLog(t *testing.T) {
 	testutil.AssertLogsDoNotContain(t, hook, "Receipt root from log doesn't match the root saved in memory")
 	testutil.AssertLogsDoNotContain(t, hook, "Invalid timestamp from log")
 	testutil.AssertLogsContain(t, hook, "Minimum number of validators reached for beacon-chain to start")
+
+	hook.Reset()
+}
+
+func TestWeb3ServiceProcessDepositLog_RequestMissedDeposits(t *testing.T) {
+	hook := logTest.NewGlobal()
+	testAcc, err := contracts.Setup()
+	if err != nil {
+		t.Fatalf("Unable to set up simulated backend %v", err)
+	}
+	web3Service, err := NewWeb3Service(context.Background(), &Web3ServiceConfig{
+		Endpoint:        endpoint,
+		DepositContract: testAcc.ContractAddr,
+		Reader:          &goodReader{},
+		Logger:          &goodLogger{},
+		HTTPLogger:      testAcc.Backend,
+		ContractBackend: testAcc.Backend,
+		BeaconDB:        &db.BeaconDB{},
+		BlockFetcher:    &goodFetcher{},
+	})
+	if err != nil {
+		t.Fatalf("unable to setup web3 ETH1.0 chain service: %v", err)
+	}
+	bConfig := params.MinimalSpecConfig()
+	bConfig.MinGenesisTime = 0
+	params.OverrideBeaconConfig(bConfig)
+
+	testAcc.Backend.Commit()
+	testAcc.Backend.AdjustTime(time.Duration(int64(time.Now().Nanosecond())))
+	depositsWanted := 10
+	deposits, _ := testutil.SetupInitialDeposits(t, uint64(depositsWanted))
+
+	for i := 0; i < depositsWanted; i++ {
+		data := deposits[i].Data
+		testAcc.TxOpts.Value = contracts.Amount32Eth()
+		testAcc.TxOpts.GasLimit = 1000000
+		if _, err := testAcc.Contract.Deposit(testAcc.TxOpts, data.PublicKey, data.WithdrawalCredentials, data.Signature); err != nil {
+			t.Fatalf("Could not deposit to deposit contract %v", err)
+		}
+
+		testAcc.Backend.Commit()
+	}
+
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{
+			web3Service.depositContractAddress,
+		},
+	}
+
+	logs, err := testAcc.Backend.FilterLogs(web3Service.ctx, query)
+	if err != nil {
+		t.Fatalf("Unable to retrieve logs %v", err)
+	}
+
+	logsToBeProcessed := append(logs[:depositsWanted-3], logs[depositsWanted-2:]...)
+	// we purposely miss processing the middle two logs so that the service, re-requests them
+	for _, log := range logsToBeProcessed {
+		if err := web3Service.ProcessLog(log); err != nil {
+			t.Fatal(err)
+		}
+		web3Service.lastRequestedBlock.Set(big.NewInt(int64(log.BlockNumber)))
+	}
+
+	if web3Service.lastReceivedMerkleIndex != int64(depositsWanted-1) {
+		t.Errorf("missing logs were not re-requested. Wanted Index %d but got %d", depositsWanted-1, web3Service.lastReceivedMerkleIndex)
+	}
 
 	hook.Reset()
 }
