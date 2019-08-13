7f915095fbce4d4b238bbdf81c54f75f7b0d4f88
Raul Jordan <raul@prysmaticlabs.com>
2019-08-13 10:38:40 -0500

    save blocks and gen block key done

--- beacon-chain/db/kv/blocks.go
+++ beacon-chain/db/kv/blocks.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/boltdb/bolt"
 	"github.com/gogo/protobuf/proto"
+	"github.com/prysmaticlabs/go-ssz"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db/filters"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 )
@@ -89,9 +90,19 @@ func (k *Store) DeleteBlock(ctx context.Context, blockRoot [32]byte) error {
 }
 
 // SaveBlock to the db.
-// TODO(#3164): Implement.
 func (k *Store) SaveBlock(ctx context.Context, block *ethpb.BeaconBlock) error {
-	return nil
+	key, err := generateBlockKey(block)
+	if err != nil {
+		return err
+	}
+	enc, err := proto.Marshal(block)
+	if err != nil {
+		return err
+	}
+	return k.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(blocksBucket)
+		return bucket.Put(key, enc)
+	})
 }
 
 // SaveBlocks via batch updates to the db.
@@ -106,4 +117,21 @@ func (k *Store) SaveHeadBlockRoot(ctx context.Context, blockRoot [32]byte) error
 		bucket := tx.Bucket(blocksBucket)
 		return bucket.Put(headBlockRootKey, blockRoot[:])
 	})
+}
+
+func generateBlockKey(block *ethpb.BeaconBlock) ([]byte, error) {
+	buf := make([]byte, 0)
+	buf = append(buf, []byte("parent-root")...)
+	buf = append(buf, block.ParentRoot...)
+
+	buf = append(buf, []byte("slot")...)
+	buf = append(buf, uint64ToBytes(block.Slot)...)
+
+	buf = append(buf, []byte("root")...)
+	blockRoot, err := ssz.HashTreeRoot(block)
+	if err != nil {
+		return nil, err
+	}
+	buf = append(buf, blockRoot[:]...)
+	return buf, nil
 }
