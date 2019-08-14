462a2070142d1d6a4468d78f07c3903d007f9ef5
Raul Jordan <raul@prysmaticlabs.com>
2019-08-14 12:06:47 -0500

    transform to variadic

--- shared/sliceutil/slice.go
+++ shared/sliceutil/slice.go
@@ -25,21 +25,42 @@ func SubsetUint64(a []uint64, b []uint64) bool {
 	return true
 }
 
-// IntersectionUint64 of two uint64 slices with time
+// IntersectionUint64 of any number of uint64 slices with time
 // complexity of approximately O(n) leveraging a map to
 // check for element existence off by a constant factor
 // of underlying map efficiency.
-func IntersectionUint64(a []uint64, b []uint64) []uint64 {
+func IntersectionUint64(s ...[]uint64) []uint64 {
+	if len(s) == 0 {
+		return []uint64{}
+	}
+	if len(s) == 1 {
+		return s[1]
+	}
 	set := make([]uint64, 0)
 	m := make(map[uint64]bool)
 
-	for i := 0; i < len(a); i++ {
-		m[a[i]] = true
+	for i := 0; i < len(s[0]); i++ {
+		m[s[0][i]] = true
 	}
-	for i := 0; i < len(b); i++ {
-		if _, found := m[b[i]]; found {
-			set = append(set, b[i])
+	for i := 0; i < len(s[1]); i++ {
+		if _, found := m[s[1][i]]; found {
+			set = append(set, s[1][i])
 		}
+	}
+
+	for i := 0; i < len(s); i++ {
+		tmp := make([]uint64, 0)
+		m := make(map[uint64]bool)
+
+		for j := 0; j < len(set); j++ {
+			m[set[j]] = true
+		}
+		for j := 0; j < len(s[i]); j++ {
+			if _, found := m[s[i][j]]; found {
+				tmp = append(tmp, s[i][j])
+			}
+		}
+		set = tmp
 	}
 	return set
 }
@@ -48,18 +69,39 @@ func IntersectionUint64(a []uint64, b []uint64) []uint64 {
 // complexity of approximately O(n) leveraging a map to
 // check for element existence off by a constant factor
 // of underlying map efficiency.
-func UnionUint64(a []uint64, b []uint64) []uint64 {
+func UnionUint64(s ...[]uint64) []uint64 {
+	if len(s) == 0 {
+		return []uint64{}
+	}
+	if len(s) == 1 {
+		return s[0]
+	}
 	set := make([]uint64, 0)
 	m := make(map[uint64]bool)
 
-	for i := 0; i < len(a); i++ {
-		m[a[i]] = true
-		set = append(set, a[i])
+	for i := 0; i < len(s[0]); i++ {
+		m[s[0][i]] = true
+		set = append(set, s[0][i])
 	}
-	for i := 0; i < len(b); i++ {
-		if _, found := m[b[i]]; !found {
-			set = append(set, b[i])
+	for i := 0; i < len(s[1]); i++ {
+		if _, found := m[s[1][i]]; !found {
+			set = append(set, s[1][i])
 		}
+	}
+
+	for i := 0; i < len(s); i++ {
+		tmp := make([]uint64, 0)
+		m := make(map[uint64]bool)
+		for j := 0; j < len(set); j++ {
+			m[set[j]] = true
+			tmp = append(tmp, set[j])
+		}
+		for j := 0; j < len(s[i]); j++ {
+			if _, found := m[s[i][j]]; !found {
+				tmp = append(tmp, s[i][j])
+			}
+		}
+		set = tmp
 	}
 	return set
 }
@@ -110,17 +152,38 @@ func IsInUint64(a uint64, b []uint64) bool {
 // complexity of approximately O(n) leveraging a map to
 // check for element existence off by a constant factor
 // of underlying map efficiency.
-func IntersectionInt64(a []int64, b []int64) []int64 {
+func IntersectionInt64(s ...[]int64) []int64 {
+	if len(s) == 0 {
+		return []int64{}
+	}
+	if len(s) == 1 {
+		return s[1]
+	}
 	set := make([]int64, 0)
 	m := make(map[int64]bool)
 
-	for i := 0; i < len(a); i++ {
-		m[a[i]] = true
+	for i := 0; i < len(s[0]); i++ {
+		m[s[0][i]] = true
 	}
-	for i := 0; i < len(b); i++ {
-		if _, found := m[b[i]]; found {
-			set = append(set, b[i])
+	for i := 0; i < len(s[1]); i++ {
+		if _, found := m[s[1][i]]; found {
+			set = append(set, s[1][i])
 		}
+	}
+
+	for i := 0; i < len(s); i++ {
+		tmp := make([]int64, 0)
+		m := make(map[int64]bool)
+
+		for j := 0; j < len(set); j++ {
+			m[set[j]] = true
+		}
+		for j := 0; j < len(s[i]); j++ {
+			if _, found := m[s[i][j]]; found {
+				tmp = append(tmp, s[i][j])
+			}
+		}
+		set = tmp
 	}
 	return set
 }
@@ -129,18 +192,39 @@ func IntersectionInt64(a []int64, b []int64) []int64 {
 // complexity of approximately O(n) leveraging a map to
 // check for element existence off by a constant factor
 // of underlying map efficiency.
-func UnionInt64(a []int64, b []int64) []int64 {
+func UnionInt64(s ...[]int64) []int64 {
+	if len(s) == 0 {
+		return []int64{}
+	}
+	if len(s) == 1 {
+		return s[0]
+	}
 	set := make([]int64, 0)
 	m := make(map[int64]bool)
 
-	for i := 0; i < len(a); i++ {
-		m[a[i]] = true
-		set = append(set, a[i])
+	for i := 0; i < len(s[0]); i++ {
+		m[s[0][i]] = true
+		set = append(set, s[0][i])
 	}
-	for i := 0; i < len(b); i++ {
-		if _, found := m[b[i]]; !found {
-			set = append(set, b[i])
+	for i := 0; i < len(s[1]); i++ {
+		if _, found := m[s[1][i]]; !found {
+			set = append(set, s[1][i])
 		}
+	}
+
+	for i := 0; i < len(s); i++ {
+		tmp := make([]int64, 0)
+		m := make(map[int64]bool)
+		for j := 0; j < len(set); j++ {
+			m[set[j]] = true
+			tmp = append(tmp, set[j])
+		}
+		for j := 0; j < len(s[i]); j++ {
+			if _, found := m[s[i][j]]; !found {
+				tmp = append(tmp, s[i][j])
+			}
+		}
+		set = tmp
 	}
 	return set
 }
