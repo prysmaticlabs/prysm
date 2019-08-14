e7a0de1712c23a86255013f64461d67a0a1e89e4
shayzluf <thezluf@gmail.com>
2019-08-14 17:40:02 +0300

    getting ready to pruning

--- beacon-chain/core/slasher/proposer_slash.go
+++ beacon-chain/core/slasher/proposer_slash.go
@@ -3,27 +3,44 @@
 package slasher
 
 import (
+	"sort"
+
 	"github.com/prysmaticlabs/go-bitfield"
 )
 
 var epochProposalBitlist map[uint64]bitfield.Bitlist
+var currentEpoch, weakSubjectivityPeriod uint64
+var epochs []uint64
 
 func init() {
-	epochProposalBitlist = make(map[uint64]bitfield.Bitlist, 100)
-	for i := uint64(0); i < 100; i++ {
-		epochProposalBitlist[i] = bitfield.NewBitlist(300000)
-	}
+	epochProposalBitlist = make(map[uint64]bitfield.Bitlist)
+	weakSubjectivityPeriod = uint64(54000)
+
 }
 
 // CheckNewProposal checks weather a new proposal is allowed or
 // creating a slashable event.
 // returns true if it is the first time this
 // validatorID propose a block in this epoch or not.
-func CheckNewProposal(epoch uint64, validatorID uint64) bool {
+func CheckNewProposal(currentEpoch uint64, epoch uint64, validatorID uint64) bool {
+
+	_, ok := epochProposalBitlist[epoch]
+	if !ok {
+
+		epochProposalBitlist[epoch] = bitfield.NewBitlist(300000)
+	}
 	proposalExists := epochProposalBitlist[epoch].BitAt(validatorID)
 	if !proposalExists {
 		epochProposalBitlist[epoch].SetBitAt(validatorID, true)
 		return true
 	}
 	return false
+}
+
+func insertSort(data []uint64, u uint64) []uint64 {
+	index := sort.Search(len(data), func(i int) bool { return data[i] > u })
+	data = append(data, uint64(0))
+	copy(data[index+1:], data[index:])
+	data[index] = u
+	return data
 }
--- beacon-chain/core/slasher/proposer_slash_test.go
+++ beacon-chain/core/slasher/proposer_slash_test.go
@@ -10,7 +10,7 @@ func TestCheckNewProposal_OK(t *testing.T) {
 	for ep := uint64(0); ep < 100; ep++ {
 		t.Logf("epoch %v", ep)
 		for vi := uint64(0); vi < 300000; vi += 10 {
-			if !CheckNewProposal(ep, vi) {
+			if !CheckNewProposal(10, ep, vi) {
 				t.Fatal("first proposal for epoch by a validator id should always return true")
 			}
 		}
@@ -18,7 +18,7 @@ func TestCheckNewProposal_OK(t *testing.T) {
 	for ep := uint64(0); ep < 100; ep++ {
 		t.Logf("epoch %v", ep)
 		for vi := uint64(1); vi < 300000; vi += 10 {
-			if !CheckNewProposal(ep, vi) {
+			if !CheckNewProposal(10, ep, vi) {
 				t.Fatal("first proposal for epoch by a validator id should always return true")
 			}
 		}
@@ -26,7 +26,7 @@ func TestCheckNewProposal_OK(t *testing.T) {
 	for ep := uint64(0); ep < 100; ep++ {
 		t.Logf("epoch %v", ep)
 		for vi := uint64(0); vi < 300000; vi += 10 {
-			if CheckNewProposal(ep, vi) {
+			if CheckNewProposal(10, ep, vi) {
 				t.Fatal("second proposal for epoch by a validator id should always return false")
 			}
 		}
