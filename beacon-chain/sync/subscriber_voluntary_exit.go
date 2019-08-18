2d71b906d7d1a884de0554bc295af325e40a1d9a
Preston Van Loon <preston@prysmaticlabs.com>
2019-08-18 10:40:28 -0400

    pass through && register

--- beacon-chain/sync/subscriber_voluntary_exit.go
+++ beacon-chain/sync/subscriber_voluntary_exit.go
@@ -4,11 +4,8 @@ import (
 	"context"
 
 	"github.com/gogo/protobuf/proto"
-	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 )
 
 func (s *RegularSync) voluntaryExitSubscriber(ctx context.Context, msg proto.Message) error {
-	exit := msg.(*ethpb.VoluntaryExit)
-
-	return s.operations.HandleValidatorExits(ctx, exit)
+	return s.operations.HandleValidatorExits(ctx, msg)
 }
