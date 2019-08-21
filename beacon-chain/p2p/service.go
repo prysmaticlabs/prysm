3cb32c379220a5aa62932fd0d59d474d57b0a07e
Nishant Das <nishdas93@gmail.com>
2019-08-21 11:38:30 +0530

    Implement Discv5 in Prysm (#3211)
    * add discovery
    
    * gaz
    
    * add build options
    
    * add udpPort
    
    * add more changes
    
    * refactor private key
    
    * added discovery loop
    
    * add ttl
    
    * add ttl
    
    * use ip type instead of string
    
    * tests pass
    
    * gaz and new test file
    
    * add test
    
    * add more tests
    
    * add one more test
    
    * adding multiAddr tests
    
    * adding new protocol , listener
    
    * fix keys
    
    * more fixes
    
    * more changes dialing peers works now
    
    * gaz
    
    * add more changes
    
    * add more changes
    
    * gaz
    
    * add new test helpers
    
    * new test
    
    * fixed all tests
    
    * gaz
    
    * reduce sleep
    
    * lint
    
    * new changes
    
    * change formats
    
    * fix all this stuff
    
    * remove discv5 protocol
    
    * remove protocol
    
    * remove port condition,too restrictive
    
    * preston's feedback
    
    * preston's feedback
    
    * close all peers
    
    * gaz
    
    * remove unused func
    
    * Update beacon-chain/p2p/service.go
    
    Co-Authored-By: Preston Van Loon <preston@prysmaticlabs.com>
    
    * remove build options
    
    * refactor tests

--- beacon-chain/p2p/service.go
+++ beacon-chain/p2p/service.go
@@ -2,7 +2,9 @@ package p2p
 
 import (
 	"context"
+	"time"
 
+	"github.com/ethereum/go-ethereum/p2p/discv5"
 	"github.com/gogo/protobuf/proto"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
@@ -10,6 +12,7 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/protocol"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	ma "github.com/multiformats/go-multiaddr"
 	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p/encoder"
 	"github.com/prysmaticlabs/prysm/shared"
@@ -18,18 +21,18 @@ import (
 )
 
 var _ = shared.Service(&Service{})
+var pollingPeriod = 1 * time.Second
 
 // Service for managing peer to peer (p2p) networking.
 type Service struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-
-	started    bool
-	cfg        *Config
-	startupErr error
-
-	host   host.Host
-	pubsub *pubsub.PubSub
+	ctx         context.Context
+	cancel      context.CancelFunc
+	started     bool
+	cfg         *Config
+	startupErr  error
+	dv5Listener Listener
+	host        host.Host
+	pubsub      *pubsub.PubSub
 }
 
 // NewService initializes a new p2p service compatible with shared.Service interface. No
@@ -51,13 +54,29 @@ func (s *Service) Start() {
 	}
 	s.started = true
 
+	ipAddr := ipAddr(s.cfg)
+	privKey, err := privKey(s.cfg)
+	if err != nil {
+		s.startupErr = err
+		return
+	}
+
 	// TODO(3147): Add host options
-	h, err := libp2p.New(s.ctx)
+	opts := buildOptions(s.cfg, ipAddr, privKey)
+	h, err := libp2p.New(s.ctx, opts...)
 	if err != nil {
 		s.startupErr = err
 		return
 	}
 	s.host = h
+	listener, err := startDiscoveryV5(ipAddr, privKey, s.cfg)
+	if err != nil {
+		s.startupErr = err
+		return
+	}
+	s.dv5Listener = listener
+
+	go s.listenForNewNodes()
 
 	// TODO(3147): Add gossip sub options
 	gs, err := pubsub.NewGossipSub(s.ctx, s.host)
@@ -71,6 +90,7 @@ func (s *Service) Start() {
 // Stop the p2p service and terminate all peer connections.
 func (s *Service) Stop() error {
 	s.started = false
+	s.dv5Listener.Close()
 	return nil
 }
 
@@ -104,6 +124,46 @@ func (s *Service) SetStreamHandler(topic string, handler network.StreamHandler) 
 func (s *Service) Disconnect(pid peer.ID) error {
 	// TODO(3147): Implement disconnect
 	return nil
+}
+
+// listen for new nodes watches for new nodes in the network and adds them to the peerstore.
+func (s *Service) listenForNewNodes() {
+	node, err := discv5.ParseNode(s.cfg.BootstrapNodeAddr)
+	if err != nil {
+		log.Fatalf("could not parse bootstrap address: %v", err)
+	}
+	nodeID := node.ID
+	ticker := time.NewTicker(pollingPeriod)
+	for {
+		select {
+		case <-ticker.C:
+			nodes := s.dv5Listener.Lookup(nodeID)
+			multiAddresses := convertToMultiAddr(nodes)
+			s.connectWithAllPeers(multiAddresses)
+			// store furthest node as the next to lookup
+			nodeID = nodes[len(nodes)-1].ID
+		case <-s.ctx.Done():
+			log.Debug("p2p context is closed, exiting routine")
+			break
+
+		}
+	}
+}
+
+func (s *Service) connectWithAllPeers(multiAddrs []ma.Multiaddr) {
+	addrInfos, err := peer.AddrInfosFromP2pAddrs(multiAddrs...)
+	if err != nil {
+		log.Errorf("Could not convert to peer address info's from multiaddresses: %v", err)
+		return
+	}
+	for _, info := range addrInfos {
+		if info.ID == s.host.ID() {
+			continue
+		}
+		if err := s.host.Connect(s.ctx, info); err != nil {
+			log.Errorf("Could not connect with peer: %v", err)
+		}
+	}
 }
 
 // Subscribe to some topic.
