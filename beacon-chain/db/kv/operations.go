bb2fdb0c2accf2465d6da5a0e8a3da5f6f9b13b9
Raul Jordan <raul@prysmaticlabs.com>
2019-08-21 13:22:26 -0500

    all crud for propoer slashings

--- beacon-chain/db/kv/operations.go
+++ beacon-chain/db/kv/operations.go
@@ -10,12 +10,13 @@ import (
 	"go.opencensus.io/trace"
 )
 
+// ProposerSlashing retrieval by slashing root.
 func (k *Store) ProposerSlashing(ctx context.Context, slashingRoot [32]byte) (*ethpb.ProposerSlashing, error) {
 	ctx, span := trace.StartSpan(ctx, "BeaconDB.ProposerSlashing")
 	defer span.End()
 	var slashing *ethpb.ProposerSlashing
 	err := k.db.View(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(validatorsBucket)
+		bkt := tx.Bucket(proposerSlashingsBucket)
 		enc := bkt.Get(slashingRoot[:])
 		if enc == nil {
 			return nil
@@ -26,21 +27,21 @@ func (k *Store) ProposerSlashing(ctx context.Context, slashingRoot [32]byte) (*e
 	return slashing, err
 }
 
-// HasProposerSlashing --
+// HasProposerSlashing verifies if a slashing is stored in the db.
 func (k *Store) HasProposerSlashing(ctx context.Context, slashingRoot [32]byte) bool {
 	ctx, span := trace.StartSpan(ctx, "BeaconDB.HasProposerSlashing")
 	defer span.End()
 	exists := false
 	// #nosec G104. Always returns nil.
 	k.db.View(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(validatorsBucket)
+		bkt := tx.Bucket(proposerSlashingsBucket)
 		exists = bkt.Get(slashingRoot[:]) != nil
 		return nil
 	})
 	return exists
 }
 
-// SaveProposerSlashing --
+// SaveProposerSlashing to the db by its hash tree root.
 func (k *Store) SaveProposerSlashing(ctx context.Context, slashing *ethpb.ProposerSlashing) error {
 	ctx, span := trace.StartSpan(ctx, "BeaconDB.SaveProposerSlashing")
 	defer span.End()
@@ -53,7 +54,17 @@ func (k *Store) SaveProposerSlashing(ctx context.Context, slashing *ethpb.Propos
 		return err
 	}
 	return k.db.Update(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket(validatorsBucket)
+		bucket := tx.Bucket(proposerSlashingsBucket)
 		return bucket.Put(slashingRoot[:], enc)
+	})
+}
+
+// DeleteProposerSlashing clears a proposer slashing from the db by its hash tree root.
+func (k *Store) DeleteProposerSlashing(ctx context.Context, slashingRoot [32]byte) error {
+	ctx, span := trace.StartSpan(ctx, "BeaconDB.DeleteProposerSlashing")
+	defer span.End()
+	return k.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(proposerSlashingsBucket)
+		return bucket.Delete(slashingRoot[:])
 	})
 }
