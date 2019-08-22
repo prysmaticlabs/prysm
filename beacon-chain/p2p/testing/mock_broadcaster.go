8868a8c441869255b2cc95ff725d836328be496c
Preston Van Loon <preston@prysmaticlabs.com>
2019-08-22 13:56:33 -0400

    add TODO issue numbers

--- beacon-chain/p2p/testing/mock_broadcaster.go
+++ beacon-chain/p2p/testing/mock_broadcaster.go
@@ -6,10 +6,12 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
+// MockBroadcaster implements p2p.Broadcaster for testing.
 type MockBroadcaster struct {
 	BroadcastCalled bool
 }
 
+// Broadcast records a broadcast occurred.
 func (m *MockBroadcaster) Broadcast(context.Context, proto.Message) error {
 	m.BroadcastCalled = true
 	return nil
