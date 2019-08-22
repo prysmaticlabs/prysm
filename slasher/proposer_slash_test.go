173f0cc2eb456b5522efa08f895f210c319d3a48
shayzluf <thezluf@gmail.com>
2019-08-22 12:30:50 +0300

    no log

--- slasher/proposer_slash_test.go
+++ slasher/proposer_slash_test.go
@@ -12,7 +12,6 @@ func TestCheckNewProposal_PopulateAndValidate(t *testing.T) {
 	epochProposalBitlist = make(map[uint64]bitfield.Bitlist)
 
 	for ep := uint64(0); ep < 100; ep++ {
-		t.Logf("epoch %v", ep)
 		for vi := uint64(0); vi < 300000; vi += 10 {
 			if first, err := CheckNewProposal(10, ep, vi); err != nil || !first {
 				t.Fatal("first proposal for epoch by a validator id should always return true")
