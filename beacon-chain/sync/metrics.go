febec4a8ddfb5e7aaeb34d5f22b337e6e1f78a3a
Preston Van Loon <preston@prysmaticlabs.com>
2019-08-28 07:51:49 -0700

    Add going msg metric

--- beacon-chain/sync/metrics.go
+++ beacon-chain/sync/metrics.go
@@ -1,3 +1,20 @@
 package sync
 
+import (
+
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
 // TODO(3147): Add metrics for RPC & subscription success/error.
+
+var (
+	messageSentCounter = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "p2p_message_sent_total",
+			Help: "Count of messages sent.",
+		},
+		[]string{"topic"},
+	)
+)
