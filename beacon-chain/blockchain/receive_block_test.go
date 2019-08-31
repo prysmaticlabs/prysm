4c7cb05c40bfd65ebe5502d27fcd79fd5d3a2a7f
Terence Tsao <terence@prysmaticlabs.com>
2019-08-30 18:26:06 -0700

    Save new validators in DB

--- beacon-chain/blockchain/receive_block_test.go
+++ beacon-chain/blockchain/receive_block_test.go
@@ -3,7 +3,6 @@ package blockchain
 import (
 	"bytes"
 	"context"
-	"encoding/binary"
 	"reflect"
 	"testing"
 
@@ -13,7 +12,6 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
 	v "github.com/prysmaticlabs/prysm/beacon-chain/core/validators"
 	testDB "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
-	"github.com/prysmaticlabs/prysm/beacon-chain/internal"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
@@ -357,86 +355,5 @@ func TestReceiveBlockNoPubsubForkchoice_CanUpdateValidatorDB(t *testing.T) {
 	_, e, _ = db.ValidatorIndex(ctx, bytesutil.ToBytes48([]byte{'Z'}))
 	if e == true {
 		t.Error("Index should not exist in DB")
-	}
-}
-
-func TestSaveValidatorIdx_SaveRetrieveWorks(t *testing.T) {
-	db := internal.SetupDBDeprecated(t)
-	defer internal.TeardownDBDeprecated(t, db)
-	ctx := context.Background()
-	epoch := uint64(1)
-	v.InsertActivatedIndices(epoch+1, []uint64{0, 1, 2})
-	var validators []*ethpb.Validator
-	for i := 0; i < 3; i++ {
-		pubKeyBuf := make([]byte, params.BeaconConfig().BLSPubkeyLength)
-		binary.PutUvarint(pubKeyBuf, uint64(i))
-		validators = append(validators, &ethpb.Validator{
-			PublicKey: pubKeyBuf,
-		})
-	}
-	state := &pb.BeaconState{
-		Validators: validators,
-		Slot:       epoch * params.BeaconConfig().SlotsPerEpoch,
-	}
-	chainService := setupBeaconChain(t, db)
-	if err := chainService.saveValidatorIdx(ctx, state); err != nil {
-		t.Fatalf("Could not save validator idx: %v", err)
-	}
-
-	wantedIdx := uint64(2)
-	idx, _, err := chainService.beaconDB.ValidatorIndex(ctx, bytesutil.ToBytes48(validators[wantedIdx].PublicKey))
-	if err != nil {
-		t.Fatalf("Could not get validator index: %v", err)
-	}
-	if wantedIdx != idx {
-		t.Errorf("Wanted: %d, got: %d", wantedIdx, idx)
-	}
-
-	if v.ActivatedValFromEpoch(epoch) != nil {
-		t.Errorf("Activated validators mapping for epoch %d still there", epoch)
-	}
-}
-
-func TestSaveValidatorIdx_IdxNotInState(t *testing.T) {
-	db := internal.SetupDBDeprecated(t)
-	defer internal.TeardownDBDeprecated(t, db)
-	epoch := uint64(100)
-	ctx := context.Background()
-
-	// Tried to insert 5 active indices to DB with only 3 validators in state
-	v.InsertActivatedIndices(epoch+1, []uint64{0, 1, 2, 3, 4})
-	var validators []*ethpb.Validator
-	for i := 0; i < 3; i++ {
-		pubKeyBuf := make([]byte, params.BeaconConfig().BLSPubkeyLength)
-		binary.PutUvarint(pubKeyBuf, uint64(i))
-		validators = append(validators, &ethpb.Validator{
-			PublicKey: pubKeyBuf,
-		})
-	}
-	state := &pb.BeaconState{
-		Validators: validators,
-		Slot:       epoch * params.BeaconConfig().SlotsPerEpoch,
-	}
-	chainService := setupBeaconChain(t, db)
-	if err := chainService.saveValidatorIdx(ctx, state); err != nil {
-		t.Fatalf("Could not save validator idx: %v", err)
-	}
-
-	wantedIdx := uint64(2)
-	idx, _, err := chainService.beaconDB.ValidatorIndex(ctx, bytesutil.ToBytes48(validators[wantedIdx].PublicKey))
-	if err != nil {
-		t.Fatalf("Could not get validator index: %v", err)
-	}
-	if wantedIdx != idx {
-		t.Errorf("Wanted: %d, got: %d", wantedIdx, idx)
-	}
-
-	if v.ActivatedValFromEpoch(epoch) != nil {
-		t.Errorf("Activated validators mapping for epoch %d still there", epoch)
-	}
-
-	// Verify the skipped validators are included in the next epoch
-	if !reflect.DeepEqual(v.ActivatedValFromEpoch(epoch+2), []uint64{3, 4}) {
-		t.Error("Did not get wanted validator from activation queue")
 	}
 }
