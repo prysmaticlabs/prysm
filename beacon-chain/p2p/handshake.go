876e0ea84dab625cd8241940b22d9684a734fc95
Nishant Das <nishdas93@gmail.com>
2019-09-02 03:59:58 +0530

    Fix Discv5 in Runtime  (#3373)
    * fix bug
    
    * remove logs
    
    * fix test
    
    * add locks
    
    * add ttl
    
    * Apply suggestions from code review
    
    Co-Authored-By: terence tsao <terence@prysmaticlabs.com>
    
    * change to ccache

--- beacon-chain/p2p/handshake.go
+++ beacon-chain/p2p/handshake.go
@@ -13,37 +13,53 @@ var handshakes = make(map[peer.ID]*pb.Hello)
 var handshakeLock sync.Mutex
 
 // AddHandshake to the local records for initial sync.
-func (p *Service) AddHandshake(pid peer.ID, hello *pb.Hello) {
+func (s *Service) AddHandshake(pid peer.ID, hello *pb.Hello) {
 	handshakeLock.Lock()
 	defer handshakeLock.Unlock()
 	handshakes[pid] = hello
 }
 
 // Handshakes has not been implemented yet and it may be moved to regular sync...
-func (p *Service) Handshakes() map[peer.ID]*pb.Hello {
+func (s *Service) Handshakes() map[peer.ID]*pb.Hello {
 	return nil
 }
 
 // AddConnectionHandler adds a callback function which handles the connection with a
 // newly added peer. It performs a handshake with that peer by sending a hello request
 // and validating the response from the peer.
-func (p *Service) AddConnectionHandler(reqFunc func(ctx context.Context, id peer.ID) error) {
-	p.host.Network().Notify(&network.NotifyBundle{
+func (s *Service) AddConnectionHandler(reqFunc func(ctx context.Context, id peer.ID) error) {
+	s.host.Network().Notify(&network.NotifyBundle{
 		ConnectedF: func(net network.Network, conn network.Conn) {
 			// Must be handled in a goroutine as this callback cannot be blocking.
 			go func() {
 				ctx := context.Background()
 				log.WithField("peer", conn.RemotePeer()).Debug(
-					"Performing handshake with to peer",
+					"Performing handshake with peer",
 				)
 				if err := reqFunc(ctx, conn.RemotePeer()); err != nil {
 					log.WithError(err).Error("Could not send successful hello rpc request")
-					if err := p.Disconnect(conn.RemotePeer()); err != nil {
+					if err := s.Disconnect(conn.RemotePeer()); err != nil {
 						log.WithError(err).Errorf("Unable to close peer %s", conn.RemotePeer())
 						return
 					}
 					log.WithField("peer", conn.RemotePeer().Pretty()).Info("New peer connected.")
 				}
+			}()
+		},
+	})
+}
+
+// addDisconnectionHandler ensures that previously disconnected peers aren't dialed again. Due
+// to either their ports being closed, nodes are no longer active,etc.
+func (s *Service) addDisconnectionHandler() {
+	s.host.Network().Notify(&network.NotifyBundle{
+		DisconnectedF: func(net network.Network, conn network.Conn) {
+			// Must be handled in a goroutine as this callback cannot be blocking.
+			go func() {
+				s.exclusionList.Set(conn.RemotePeer().String(), true, ttl)
+				log.WithField("peer", conn.RemotePeer()).Debug(
+					"Peer is added to exclusion list",
+				)
 			}()
 		},
 	})
