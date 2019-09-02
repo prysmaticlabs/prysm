6e3b2c078594bf3def937851382c952fc269fb72
Raul Jordan <raul@prysmaticlabs.com>
2019-09-02 15:42:58 -0500

    unmarshal from encoded

--- tools/genesis-state-gen/main.go
+++ tools/genesis-state-gen/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"gopkg.in/yaml.v2"
 )
 
+type keyPair struct {
+	PrivateKey []byte `yaml:"privkey"`
+	PublicKey  []byte `yaml:"pubkey"`
+}
+
+type validatorKeys struct {
+	Keys []*keyPair
+}
+
 func main() {
-	fmt.Println("Hi")
+	enc := []byte{}
+	var ks *validatorKeys
+	if err := yaml.Unmarshal(enc, ks); err != nil {
+		panic(err)
+	}
 }
