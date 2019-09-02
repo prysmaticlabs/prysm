eb6f762e2c9cd59c7eda6e6cd6c45c10bec8a552
Raul Jordan <raul@prysmaticlabs.com>
2019-09-02 17:12:59 -0500

    mock start tool

--- tools/genesis-state-gen/main.go
+++ tools/genesis-state-gen/main.go
@@ -6,11 +6,22 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/prysmaticlabs/go-ssz"
+	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/bls"
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"gopkg.in/yaml.v2"
 )
 
+const (
+	blsWithdrawalPrefixByte = byte(0)
+)
+
 var (
-	inputFile = flag.String("validator-keys-yaml", "", "Input validator keys YAML file")
+	inputFile          = flag.String("validator-keys-yaml", "", "Input validator keys YAML file")
+	domainDeposit      = [4]byte{3, 0, 0, 0}
+	genesisForkVersion = []byte{0, 0, 0, 0}
 )
 
 type keyPair struct {
@@ -32,5 +43,48 @@ func main() {
 	if err := yaml.Unmarshal(enc, &ks); err != nil {
 		panic(err)
 	}
-	fmt.Println(ks)
+	deps := make([]*ethpb.Deposit, len(ks))
+	for i, item := range ks {
+		dp, err := createDepositData([]byte(item.PrivateKey), []byte(item.PublicKey))
+		if err != nil {
+			panic(err)
+		}
+		deps[i] = &ethpb.Deposit{
+			Data: dp,
+		}
+	}
+	fmt.Println(deps)
+}
+
+func createDepositData(privKey []byte, pubKey []byte) (*ethpb.Deposit_Data, error) {
+	sk1, err := bls.SecretKeyFromBytes(privKey)
+	if err != nil {
+		return nil, err
+	}
+	di := &ethpb.Deposit_Data{
+		PublicKey:             pubKey,
+		WithdrawalCredentials: withdrawalCredentialsHash(pubKey),
+		Amount:                params.BeaconConfig().MaxEffectiveBalance,
+	}
+
+	sr, err := ssz.HashTreeRoot(di)
+	if err != nil {
+		return nil, err
+	}
+
+	domain := bls.Domain(domainDeposit[:], genesisForkVersion)
+	di.Signature = sk1.Sign(sr[:], domain).Marshal()
+	return di, nil
+}
+
+// withdrawalCredentialsHash forms a 32 byte hash of the withdrawal public
+// address.
+//
+// The specification is as follows:
+//   withdrawal_credentials[:1] == BLS_WITHDRAWAL_PREFIX_BYTE
+//   withdrawal_credentials[1:] == hash(withdrawal_pubkey)[1:]
+// where withdrawal_credentials is of type bytes32.
+func withdrawalCredentialsHash(pubKey []byte) []byte {
+	h := hashutil.HashKeccak256(pubKey)
+	return append([]byte{blsWithdrawalPrefixByte}, h[0:]...)[:32]
 }
