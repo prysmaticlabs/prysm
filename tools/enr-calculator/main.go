31d100df3308e65c85efef87c4268bc22d3f18a5
nisdas <nishdas93@gmail.com>
2019-09-06 12:04:37 +0530

    preston's requested changes

--- tools/enr-calculator/main.go
+++ tools/enr-calculator/main.go
@@ -4,101 +4,61 @@ package main
 
 import (
 	"crypto/ecdsa"
-	"encoding/json"
-	"io/ioutil"
-	"log"
+	"flag"
 	"net"
-	"net/http"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/ethereum/go-ethereum/p2p/enode"
 	"github.com/ethereum/go-ethereum/p2p/enr"
 	"github.com/libp2p/go-libp2p-core/crypto"
+	log "github.com/sirupsen/logrus"
 	_ "go.uber.org/automaxprocs"
 )
 
+var (
+	privateKey = flag.String("private", "", "Base-64 encoded Private key to use for calculation of ENR")
+	port       = flag.Int("port", 0, "Port to use for calculation of ENR")
+	ipAddr     = flag.String("ipAddress", "", "IP to use in calculation of ENR")
+)
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/enr", respondToEnrReq)
+	flag.Parse()
 
-	log.Println("Starting on port 4000")
-	if err := http.ListenAndServe(":4000", mux); err != nil {
-		log.Fatalf("Failed to start server %v", err)
+	if len(*privateKey) == 0 {
+		log.Fatal("No private key given")
 	}
-}
-
-type deserializationRequest struct {
-	PrivateKey string `json:"privateKey"`
-	Ipv4       string `json:"ipv4"`
-	UDPPort    int    `json:"udpPort"`
-}
-
-func respondToEnrReq(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	data, err := ioutil.ReadAll(r.Body)
+	decodedKey, err := crypto.ConfigDecodeKey(*privateKey)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("Failed to read request body: %v\n", err)
-		return
-	}
-
-	requestData := &deserializationRequest{}
-	if err := json.Unmarshal(data, requestData); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("Unable to unmarshal JSON: %v\n", err)
-		return
-	}
-
-	decodedKey, err := crypto.ConfigDecodeKey(requestData.PrivateKey)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("Unable to decode private key: %v\n", err)
-		return
+		log.Fatalf("Unable to decode private key: %v\n", err)
 	}
 
 	privatekey, err := crypto.UnmarshalPrivateKey(decodedKey)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("Unable to unmarshal private key: %v\n", err)
-		return
+		log.Fatalf("Unable to unmarshal private key: %v\n", err)
 	}
 
 	ecdsaPrivKey := (*ecdsa.PrivateKey)((*btcec.PrivateKey)(privatekey.(*crypto.Secp256k1PrivateKey)))
 
-	if net.ParseIP(requestData.Ipv4).To4() == nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("Invalid ipv4 address given: %v\n", err)
-		return
+	if net.ParseIP(*ipAddr).To4() == nil {
+		log.Fatalf("Invalid ipv4 address given: %v\n", err)
 	}
 
-	if requestData.UDPPort == 0 {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("Invalid udp port given: %v\n", err)
+	if *port == 0 {
+		log.Fatalf("Invalid udp port given: %v\n", err)
 		return
 	}
 
 	db, err := enode.OpenDB("")
 	defer db.Close()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf("Could not open node's peer database: %v\n", err)
+		log.Fatalf("Could not open node's peer database: %v\n", err)
 		return
 	}
 
 	localNode := enode.NewLocalNode(db, ecdsaPrivKey)
-	ipEntry := enr.IP(net.ParseIP(requestData.Ipv4))
-	udpEntry := enr.UDP(requestData.UDPPort)
+	ipEntry := enr.IP(net.ParseIP(*ipAddr))
+	udpEntry := enr.UDP(*port)
 	localNode.Set(ipEntry)
 	localNode.Set(udpEntry)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write([]byte(localNode.Node().String()))
-	if err != nil {
-		log.Printf("Failed to write data to client: %v\n", err)
-	}
+	log.Info(localNode.Node().String())
 }
