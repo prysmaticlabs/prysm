c6e56a782b8f663a94b20f47b8acd530bf09dc47
Ivan Martinez <ivanthegreat15@gmail.com>
2019-09-06 23:34:18 -0400

    Remove unneeded minimal skip

--- proto/testing/ssz_static_minimal_test.go
+++ proto/testing/ssz_static_minimal_test.go
@@ -5,6 +5,5 @@ import (
 )
 
 func TestSSZStatic_Minimal(t *testing.T) {
-	t.Skip("This test suite requires --define ssz=minimal to be provided and there isn't a great way to do that without breaking //...")
 	runSSZStaticTests(t, "minimal")
 }
