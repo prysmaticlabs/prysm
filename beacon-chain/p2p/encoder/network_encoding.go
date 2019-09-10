54634a2ceb3f54de7bec30cacb11766303e22984
Preston Van Loon <preston@prysmaticlabs.com>
2019-09-10 06:56:44 -0700

    add slightly better godoc msg

--- beacon-chain/p2p/encoder/network_encoding.go
+++ beacon-chain/p2p/encoder/network_encoding.go
@@ -12,13 +12,15 @@ const (
 
 // NetworkEncoding represents an encoder compatible with Ethereum 2.0 p2p.
 type NetworkEncoding interface {
-	// Decodes to the provided message.
+	// Decodes to the provided message. The interface must be a pointer to the decoding destination.
 	Decode([]byte, interface{}) error
-	// DecodeWithLength a bytes from a reader with a varint length prefix.
+	// DecodeWithLength a bytes from a reader with a varint length prefix. The interface must be a pointer to the
+	// decoding destination.
 	DecodeWithLength(io.Reader, interface{}) error
-	// Encode an arbitrary message to the provided writer.
+	// Encode an arbitrary message to the provided writer. The interface must be a pointer object to encode.
 	Encode(io.Writer, interface{}) (int, error)
-	// EncodeWithLength an arbitrary message to the provided writer with a varint length prefix.
+	// EncodeWithLength an arbitrary message to the provided writer with a varint length prefix. The interface must be
+	// a pointer object to encode.
 	EncodeWithLength(io.Writer, interface{}) (int, error)
 	// ProtocolSuffix returns the last part of the protocol ID to indicate the encoding scheme.
 	ProtocolSuffix() string
