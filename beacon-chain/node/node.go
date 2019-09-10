c8f2c1fb6062b524e35898f8890a23bcc61eefcc
Raul Jordan <raul@prysmaticlabs.com>
2019-09-10 14:22:08 -0500

    resolve lint

--- beacon-chain/node/node.go
+++ beacon-chain/node/node.go
@@ -82,7 +82,7 @@ func NewBeaconNode(ctx *cli.Context) (*BeaconNode, error) {
 	// Use custom config values if the --no-custom-config flag is set.
 	if !ctx.GlobalBool(flags.NoCustomConfigFlag.Name) {
 		log.Info("Using custom parameter configuration")
-		// TODO: Conditionally use demo config?
+		// TODO(3439): Conditionally use demo config?
 		// params.UseDemoBeaconConfig()
 		params.UseMinimalConfig()
 	}
