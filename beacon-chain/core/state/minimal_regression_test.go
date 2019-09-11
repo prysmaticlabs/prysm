f73cda70521ea8021613bb7f69313a5b42d7aa89
Terence Tsao <terence@prysmaticlabs.com>
2019-09-11 18:17:28 -0400

    Use min config for test

--- beacon-chain/core/state/minimal_regression_test.go
+++ beacon-chain/core/state/minimal_regression_test.go
@@ -9,6 +9,7 @@ import (
 	ob "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	eth "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-ssz"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"gopkg.in/d4l3k/messagediff.v1"
 )
 
@@ -34,6 +35,8 @@ func TestConsensusIssue0(t *testing.T) {
 	pre := &ob.BeaconState{}
 	block := &eth.BeaconBlock{}
 	post := &ob.BeaconState{}
+
+	params.UseMinimalConfig()
 
 	loadSszOrDie(t, "testdata/minimal/0_block.ssz", block)
 	loadSszOrDie(t, "testdata/minimal/0_prestate.ssz", pre)
