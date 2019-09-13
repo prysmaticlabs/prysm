cffd9cf16dfc7df5f1062e986e93232ff7365a54
Raul Jordan <raul@prysmaticlabs.com>
2019-09-13 14:20:53 -0500

    table driven tests

--- beacon-chain/core/state/minimal_regression_test.go
+++ beacon-chain/core/state/minimal_regression_test.go
@@ -7,11 +7,82 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/prysmaticlabs/go-ssz"
-	ob "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
-	eth "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"gopkg.in/d4l3k/messagediff.v1"
 )
+
+func TestConsensusBugs(t *testing.T) {
+	tests := []struct {
+		name          string
+		blockPath     string
+		preStatePath  string
+		postStatePath string
+	}{
+		{
+			// This scenario produced a consensus issue between ZCLI, and Artemis.
+			// The test expects that running a state transition with 0_block 0_prestate would
+			// output 0_poststate.
+			//
+			// Assert ExecuteStateTransition(ctx, 0_block, 0_prestate) == 0_poststate
+			//
+			// https://github.com/djrtwo/interop-test-cases/tree/master/tests/artemis_16_crosslinks_and_balances
+			name:          "ArtemisCrosslinks",
+			blockPath:     "testdata/minimal/artemis_crosslink/block.ssz",
+			preStatePath:  "testdata/minimal/artemis_crosslink/pre.ssz",
+			postStatePath: "testdata/minimal/artemis_crosslink/post.ssz",
+		},
+		{
+			// This scenario produced a consensus issue when running Prysm with Trinity.
+			// The test expects that running a state transition with 0_block 0_prestate would
+			// output 0_poststate.
+			//
+			// Assert ExecuteStateTransition(ctx, 0_block, 0_prestate) == 0_poststate
+			//
+			// https://github.com/djrtwo/interop-test-cases/tree/master/tests/prysm_16_duplicate_attestation_rewards
+			name:          "PrysmDuplicateRewards",
+			blockPath:     "testdata/minimal/duplicate_rewards/block.ssz",
+			preStatePath:  "testdata/minimal/duplicate_rewards/pre.ssz",
+			postStatePath: "testdata/minimal/duplicate_rewards/post.ssz",
+		},
+		{
+			// This scenario produced a consensus issue between Trinity, ZCLI, and Lighthouse.
+			// The test expects that running a state transition with 0_block 0_prestate would
+			// output 0_poststate.
+			//
+			// Assert ExecuteStateTransition(ctx, 0_block, 0_prestate) == 0_poststate
+			//
+			// https://github.com/djrtwo/interop-test-cases/tree/master/tests/night_one_16_crosslinks
+			name:          "TrinityLighthouseCrosslinks",
+			blockPath:     "testdata/minimal/crosslink_mismatch/block.ssz",
+			preStatePath:  "testdata/minimal/crosslink_mismatch/pre.ssz",
+			postStatePath: "testdata/minimal/crosslink_mismatch/post.ssz",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(tt *testing.T) {
+			block := &ethpb.BeaconBlock{}
+			pre := &pb.BeaconState{}
+			post := &pb.BeaconState{}
+
+			params.UseMinimalConfig()
+
+			loadSszOrDie(t, test.blockPath, block)
+			loadSszOrDie(t, test.preStatePath, pre)
+			loadSszOrDie(t, test.postStatePath, post)
+
+			result, err := ExecuteStateTransition(context.Background(), pre, block)
+			if err != nil {
+				t.Logf("Could not process state transition %v", err)
+				if !proto.Equal(result, post) {
+					diff, _ := messagediff.PrettyDiff(result, post)
+					t.Log(diff)
+				}
+			}
+		})
+	}
+}
 
 func loadSszOrDie(t *testing.T, filepath string, dst interface{}) {
 	b, err := ioutil.ReadFile(filepath)
@@ -21,90 +92,5 @@ func loadSszOrDie(t *testing.T, filepath string, dst interface{}) {
 
 	if err := ssz.Unmarshal(b, dst); err != nil {
 		t.Fatal(err)
-	}
-}
-
-// This scenario produced a consensus issue when running Prysm with Trinity.
-// The test expects that running a state transition with 0_block 0_prestate would
-// output 0_poststate.
-//
-// Assert ExecuteStateTransition(ctx, 0_block, 0_prestate) == 0_poststate
-//
-// More context/data: https://github.com/djrtwo/interop-test-cases/tree/master/tests/prysm_16_duplicate_attestation_rewards
-func TestConsensusIssueDuplicateRewards(t *testing.T) {
-	pre := &ob.BeaconState{}
-	block := &eth.BeaconBlock{}
-	post := &ob.BeaconState{}
-
-	params.UseMinimalConfig()
-
-	loadSszOrDie(t, "testdata/minimal/duplicate_rewards/block.ssz", block)
-	loadSszOrDie(t, "testdata/minimal/duplicate_rewards/pre.ssz", pre)
-	loadSszOrDie(t, "testdata/minimal/duplicate_rewards/post.ssz", post)
-
-	result, err := ExecuteStateTransition(context.Background(), pre, block)
-	if err != nil {
-		t.Fatalf("Could not process state transition %v", err)
-	}
-	if !proto.Equal(result, post) {
-		diff, _ := messagediff.PrettyDiff(result, post)
-		t.Log(diff)
-		t.Fail()
-	}
-}
-
-// This scenario produced a consensus issue between Trinity, ZCLI, and Lighthouse.
-// The test expects that running a state transition with 0_block 0_prestate would
-// output 0_poststate.
-//
-// Assert ExecuteStateTransition(ctx, 0_block, 0_prestate) == 0_poststate
-//
-// More context/data: https://github.com/djrtwo/interop-test-cases/tree/master/tests/night_one_16_crosslinks
-func TestConsensusIssueCrosslinkMismatch(t *testing.T) {
-	pre := &ob.BeaconState{}
-	block := &eth.BeaconBlock{}
-	post := &ob.BeaconState{}
-
-	params.UseMinimalConfig()
-
-	loadSszOrDie(t, "testdata/minimal/crosslink_mismatch/block.ssz", block)
-	loadSszOrDie(t, "testdata/minimal/crosslink_mismatch/pre.ssz", pre)
-	loadSszOrDie(t, "testdata/minimal/crosslink_mismatch/post.ssz", post)
-
-	result, err := ExecuteStateTransition(context.Background(), pre, block)
-	if err != nil {
-		t.Logf("Could not process state transition %v", err)
-		if !proto.Equal(result, post) {
-			diff, _ := messagediff.PrettyDiff(result, post)
-			t.Log(diff)
-		}
-	}
-}
-
-// This scenario produced a consensus issue between ZCLI, and Artemis.
-// The test expects that running a state transition with 0_block 0_prestate would
-// output 0_poststate.
-//
-// Assert ExecuteStateTransition(ctx, 0_block, 0_prestate) == 0_poststate
-//
-// More context/data: https://github.com/djrtwo/interop-test-cases/tree/master/tests/artemis_16_crosslinks_and_balances
-func TestConsensusIssueArtemisCrosslink(t *testing.T) {
-	pre := &ob.BeaconState{}
-	block := &eth.BeaconBlock{}
-	post := &ob.BeaconState{}
-
-	params.UseMinimalConfig()
-
-	loadSszOrDie(t, "testdata/minimal/artemis_crosslink/block.ssz", block)
-	loadSszOrDie(t, "testdata/minimal/artemis_crosslink/pre.ssz", pre)
-	loadSszOrDie(t, "testdata/minimal/artemis_crosslink/post.ssz", post)
-
-	result, err := ExecuteStateTransition(context.Background(), pre, block)
-	if err != nil {
-		t.Logf("Could not process state transition %v", err)
-		if !proto.Equal(result, post) {
-			diff, _ := messagediff.PrettyDiff(result, post)
-			t.Log(diff)
-		}
 	}
 }
