a6e325e634ffefc847b2461d6388be50f598c8f1
Raul Jordan <raul@prysmaticlabs.com>
2019-09-13 11:06:32 -0500

    Update beacon-chain/blockchain/forkchoice/doc.go
    Co-Authored-By: terence tsao <terence@prysmaticlabs.com>

--- beacon-chain/blockchain/forkchoice/doc.go
+++ beacon-chain/blockchain/forkchoice/doc.go
@@ -4,6 +4,6 @@ Sub-Tree) algorithm as the Ethereum Serenity beacon chain fork choice rule. This
 properly detect the canonical chain based on validator votes even in the presence of high network
 latency, network partitions, and many conflicting blocks. To read more about fork choice, read the
 official accompanying document:
-https://github.com/ethereum/eth2.0-specs/blob/1f3a5b156f7a0e7616f7c8bc31e27fa4da392139/specs/core/0_fork-choice.md
+https://github.com/ethereum/eth2.0-specs/blob/v0.8.3/specs/core/0_fork-choice.md
 */
 package forkchoice
