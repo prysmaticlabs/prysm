384f98701f65dcff6c07821807f169adc49d1297
Preston Van Loon <preston@prysmaticlabs.com>
2019-09-19 14:50:11 -0700

    lint

--- beacon-chain/sync/peerstatus/peer_statuses.go
+++ beacon-chain/sync/peerstatus/peer_statuses.go
@@ -12,30 +12,33 @@ import (
 var lock sync.RWMutex
 var peerStatuses = make(map[peer.ID]*pb.Hello)
 
-// Get most recent status from peer in cache.
+// Get most recent status from peer in cache. Threadsafe.
 func Get(pid peer.ID) *pb.Hello {
 	lock.RLock()
 	defer lock.RUnlock()
 	return peerStatuses[pid]
 }
 
-// Set most recent status from peer in cache.
+// Set most recent status from peer in cache. Threadsafe.
 func Set(pid peer.ID, status *pb.Hello) {
 	lock.Lock()
 	defer lock.Unlock()
 	peerStatuses[pid] = status
 }
 
+// Delete peer status from cache. Threadsafe.
 func Delete(pid peer.ID) {
 	lock.Lock()
 	defer lock.Unlock()
 	delete(peerStatuses, pid)
 }
 
+// Count of peer statuses in cache. Threadsafe.
 func Count() int {
 	return len(peerStatuses)
 }
 
+// Keys is the list of peer IDs which status exists. Threadsafe.
 func Keys() []peer.ID {
 	lock.RLock()
 	defer lock.RUnlock()
@@ -46,6 +49,7 @@ func Keys() []peer.ID {
 	return keys
 }
 
+// Clear the cache. This method should only be used for tests.
 func Clear() {
 	peerStatuses = make(map[peer.ID]*pb.Hello)
 }
--- beacon-chain/sync/rpc_hello_test.go
+++ beacon-chain/sync/rpc_hello_test.go
@@ -27,7 +27,7 @@ func init() {
 
 func TestHelloRPCHandler_Disconnects_OnForkVersionMismatch(t *testing.T) {
 	peerstatus.Clear()
-	
+
 	// TODO(3441): Fix ssz string length issue.
 	t.Skip("3441: SSZ is decoding a string with an unexpected length")
 	p1 := p2ptest.NewTestP2P(t)
