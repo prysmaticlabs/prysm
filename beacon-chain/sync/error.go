f0e723c5bd9a17c06cc4bdd2284d118e8353f6b9
nisdas <nishdas93@gmail.com>
2019-09-20 16:22:18 +0530

    fix error resp

--- beacon-chain/sync/error.go
+++ beacon-chain/sync/error.go
@@ -18,7 +18,7 @@ var responseCodeServerError = byte(0x02)
 
 func (r *RegularSync) generateErrorResponse(code byte, reason string) ([]byte, error) {
 	buf := bytes.NewBuffer([]byte{code})
-	if _, err := r.p2p.Encoding().EncodeWithLength(buf, reason); err != nil {
+	if _, err := r.p2p.Encoding().EncodeWithLength(buf, []byte(reason)); err != nil {
 		return nil, err
 	}
 
