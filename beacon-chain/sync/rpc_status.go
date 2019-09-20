4bc2d628b1256317b7416670143e05c69468b4fc
Nishant Das <nishdas93@gmail.com>
2019-09-20 11:57:28 +0530

    Update Naming to Latest Networking Spec (#3519)
    * update naming
    
    * replace with updated version
    
    * more changes
    
    * fixed all tests
    
    * build and lint
    
    * regen protos
    
    * fix test
    
    * remove outdated code
    
    * prestons review
    
    * preston's comments
    
    * preston's review
    
    * preston's review
    
    * lint

--- beacon-chain/sync/rpc_status.go
+++ beacon-chain/sync/rpc_status.go
@@ -13,26 +13,26 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
-// sendRPCHelloRequest for a given topic with an expected protobuf message type.
-func (r *RegularSync) sendRPCHelloRequest(ctx context.Context, id peer.ID) error {
-	log := log.WithField("rpc", "hello")
+// sendRPCStatusRequest for a given topic with an expected protobuf message type.
+func (r *RegularSync) sendRPCStatusRequest(ctx context.Context, id peer.ID) error {
+	log := log.WithField("rpc", "status")
 
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
-	// return if hello already exists
-	hello := r.helloTracker[id]
+	// return if status already exists
+	hello := r.statusTracker[id]
 	if hello != nil {
 		log.Debugf("Peer %s already exists", id)
 		return nil
 	}
 
-	resp := &pb.Hello{
-		ForkVersion:    params.BeaconConfig().GenesisForkVersion,
-		FinalizedRoot:  r.chain.FinalizedCheckpt().Root,
-		FinalizedEpoch: r.chain.FinalizedCheckpt().Epoch,
-		HeadRoot:       r.chain.HeadRoot(),
-		HeadSlot:       r.chain.HeadSlot(),
+	resp := &pb.Status{
+		HeadForkVersion: params.BeaconConfig().GenesisForkVersion,
+		FinalizedRoot:   r.chain.FinalizedCheckpt().Root,
+		FinalizedEpoch:  r.chain.FinalizedCheckpt().Epoch,
+		HeadRoot:        r.chain.HeadRoot(),
+		HeadSlot:        r.chain.HeadSlot(),
 	}
 	stream, err := r.p2p.Send(ctx, resp, id)
 	if err != nil {
@@ -48,49 +48,49 @@ func (r *RegularSync) sendRPCHelloRequest(ctx context.Context, id peer.ID) error
 		return errors.New(errMsg)
 	}
 
-	msg := &pb.Hello{}
+	msg := &pb.Status{}
 	if err := r.p2p.Encoding().DecodeWithLength(stream, msg); err != nil {
 		return err
 	}
-	r.helloTrackerLock.Lock()
-	r.helloTracker[stream.Conn().RemotePeer()] = msg
-	r.helloTrackerLock.Unlock()
+	r.statusTrackerLock.Lock()
+	r.statusTracker[stream.Conn().RemotePeer()] = msg
+	r.statusTrackerLock.Unlock()
 
-	return r.validateHelloMessage(msg, stream)
+	return r.validateStatusMessage(msg, stream)
 }
 
 func (r *RegularSync) removeDisconnectedPeerStatus(ctx context.Context, pid peer.ID) error {
-	r.helloTrackerLock.Lock()
-	delete(r.helloTracker, pid)
-	r.helloTrackerLock.Unlock()
+	r.statusTrackerLock.Lock()
+	delete(r.statusTracker, pid)
+	r.statusTrackerLock.Unlock()
 	return nil
 }
 
-// helloRPCHandler reads the incoming Hello RPC from the peer and responds with our version of a hello message.
+// statusRPCHandler reads the incoming Status RPC from the peer and responds with our version of a status message.
 // This handler will disconnect any peer that does not match our fork version.
-func (r *RegularSync) helloRPCHandler(ctx context.Context, msg interface{}, stream libp2pcore.Stream) error {
+func (r *RegularSync) statusRPCHandler(ctx context.Context, msg interface{}, stream libp2pcore.Stream) error {
 	defer stream.Close()
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 	setRPCStreamDeadlines(stream)
-	log := log.WithField("rpc", "hello")
+	log := log.WithField("handler", "status")
 
 	// return if hello already exists
-	r.helloTrackerLock.RLock()
-	hello := r.helloTracker[stream.Conn().RemotePeer()]
-	r.helloTrackerLock.RUnlock()
+	r.statusTrackerLock.RLock()
+	hello := r.statusTracker[stream.Conn().RemotePeer()]
+	r.statusTrackerLock.RUnlock()
 	if hello != nil {
 		log.Debugf("Peer %s already exists", stream.Conn().RemotePeer())
 		return nil
 	}
 
-	m := msg.(*pb.Hello)
+	m := msg.(*pb.Status)
 
-	r.helloTrackerLock.Lock()
-	r.helloTracker[stream.Conn().RemotePeer()] = m
-	r.helloTrackerLock.Unlock()
+	r.statusTrackerLock.Lock()
+	r.statusTracker[stream.Conn().RemotePeer()] = m
+	r.statusTrackerLock.Unlock()
 
-	if err := r.validateHelloMessage(m, stream); err != nil {
+	if err := r.validateStatusMessage(m, stream); err != nil {
 		originalErr := err
 		resp, err := r.generateErrorResponse(responseCodeInvalidRequest, err.Error())
 		if err != nil {
@@ -110,18 +110,18 @@ func (r *RegularSync) helloRPCHandler(ctx context.Context, msg interface{}, stre
 		return originalErr
 	}
 
-	r.helloTrackerLock.Lock()
-	r.helloTracker[stream.Conn().RemotePeer()] = m
-	r.helloTrackerLock.Unlock()
+	r.statusTrackerLock.Lock()
+	r.statusTracker[stream.Conn().RemotePeer()] = m
+	r.statusTrackerLock.Unlock()
 
 	r.p2p.AddHandshake(stream.Conn().RemotePeer(), m)
 
-	resp := &pb.Hello{
-		ForkVersion:    params.BeaconConfig().GenesisForkVersion,
-		FinalizedRoot:  r.chain.FinalizedCheckpt().Root,
-		FinalizedEpoch: r.chain.FinalizedCheckpt().Epoch,
-		HeadRoot:       r.chain.HeadRoot(),
-		HeadSlot:       r.chain.HeadSlot(),
+	resp := &pb.Status{
+		HeadForkVersion: params.BeaconConfig().GenesisForkVersion,
+		FinalizedRoot:   r.chain.FinalizedCheckpt().Root,
+		FinalizedEpoch:  r.chain.FinalizedCheckpt().Epoch,
+		HeadRoot:        r.chain.HeadRoot(),
+		HeadSlot:        r.chain.HeadSlot(),
 	}
 
 	if _, err := stream.Write([]byte{responseCodeSuccess}); err != nil {
@@ -132,8 +132,8 @@ func (r *RegularSync) helloRPCHandler(ctx context.Context, msg interface{}, stre
 	return err
 }
 
-func (r *RegularSync) validateHelloMessage(msg *pb.Hello, stream network.Stream) error {
-	if !bytes.Equal(params.BeaconConfig().GenesisForkVersion, msg.ForkVersion) {
+func (r *RegularSync) validateStatusMessage(msg *pb.Status, stream network.Stream) error {
+	if !bytes.Equal(params.BeaconConfig().GenesisForkVersion, msg.HeadForkVersion) {
 		return errWrongForkVersion
 	}
 	return nil
