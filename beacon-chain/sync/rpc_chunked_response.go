60ef7f91469f10b5cbb1a9377557c2df4b2d5dcb
nisdas <nishdas93@gmail.com>
2019-09-20 13:35:29 +0530

    lint

--- beacon-chain/sync/rpc_chunked_response.go
+++ beacon-chain/sync/rpc_chunked_response.go
@@ -21,7 +21,7 @@ func (r *RegularSync) chunkWriter(stream libp2pcore.Stream, msg interface{}) err
 	return err
 }
 
-// HandleChunkedBlocks handles each response chunk that is sent by the
+// HandleChunkedBlock handles each response chunk that is sent by the
 // peer and converts it into a beacon block.
 func HandleChunkedBlock(stream libp2pcore.Stream, p2p p2p.P2P) (*eth.BeaconBlock, error) {
 	blk := &eth.BeaconBlock{}
