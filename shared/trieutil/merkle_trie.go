24dade461c7cf3a6f57b54c4f27330b7255fb4bc
Terence Tsao <terence@prysmaticlabs.com>
2019-09-24 09:02:29 -0700

    Implemented MerkleTree

--- shared/trieutil/merkle_trie.go
+++ shared/trieutil/merkle_trie.go
@@ -35,3 +35,38 @@ func PrevPowerOf2(n int) int {
 	}
 	return 2 * PrevPowerOf2(n/2)
 }
+
+// MerkleTree returns all the nodes in a merkle tree from inputting merkle leaves.
+//
+// Spec pseudocode definition:
+//   def merkle_tree(leaves: Sequence[Hash]) -> Sequence[Hash]:
+//    padded_length = get_next_power_of_two(len(leaves))
+//    o = [Hash()] * padded_length + list(leaves) + [Hash()] * (padded_length - len(leaves))
+//    for i in range(padded_length - 1, 0, -1):
+//        o[i] = hash(o[i * 2] + o[i * 2 + 1])
+//    return o
+func MerkleTree(leaves [][]byte) [][]byte {
+	paddedLength := mathutil.NextPowerOf2(len(leaves))
+	parents := make([][]byte, paddedLength)
+	paddedLeaves := make([][]byte, paddedLength - len(leaves))
+
+	for i:=0; i<len(parents); i++ {
+		parents[i] = params.BeaconConfig().ZeroHash[:]
+	}
+	for i:=0; i<len(paddedLeaves); i++ {
+		paddedLeaves[i] = params.BeaconConfig().ZeroHash[:]
+	}
+
+	merkleTree := make([][]byte, 0, len(parents) + len(leaves) + len(paddedLeaves))
+	merkleTree = append(merkleTree, parents...)
+	merkleTree = append(merkleTree, leaves...)
+	merkleTree = append(merkleTree, paddedLeaves...)
+
+	for i:=len(paddedLeaves)-1; i > 0; i-- {
+		a := append(merkleTree[2*i], merkleTree[2*i+1]...)
+		b := hashutil.Hash(a)
+		merkleTree[i] = b[:]
+	}
+
+	return merkleTree
+}
