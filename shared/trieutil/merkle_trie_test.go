ddaa3455844278d49850e66ffa50f952a144c44a
Terence Tsao <terence@prysmaticlabs.com>
2019-09-24 09:02:51 -0700

    Test for MerkleTree

--- shared/trieutil/merkle_trie_test.go
+++ shared/trieutil/merkle_trie_test.go
@@ -43,3 +43,20 @@ func TestPrevPowerOf2(t *testing.T) {
 		}
 	}
 }
+
+func TestMerkleTreeLength(t *testing.T) {
+	tests := []struct {
+		leaves [][]byte
+		length int
+	}{
+		{[][]byte{{'A'},{'B'},{'C'}}, 8},
+		{[][]byte{{'A'},{'B'},{'C'},{'D'}}, 8},
+		{[][]byte{{'A'},{'B'},{'C'},{'D'},{'E'}}, 16},
+
+	}
+	for _, tt := range tests {
+		if got := MerkleTree(tt.leaves); len(got) != tt.length {
+			t.Errorf("len(MerkleTree()) = %v, want %v", got, tt.length)
+		}
+	}
+}
