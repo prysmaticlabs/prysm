d532b8b77626521a2996928ccea71c28d709779e
Terence Tsao <terence@prysmaticlabs.com>
2019-09-24 08:59:35 -0700

    Fmt

--- shared/trieutil/merkle_trie_test.go
+++ shared/trieutil/merkle_trie_test.go
@@ -1,21 +1,21 @@
 package trieutil
 
 import (
-"testing"
+	"testing"
 )
 
 func TestNextPowerOf2(t *testing.T) {
 	tests := []struct {
 		input int
-		want int
+		want  int
 	}{
-		{input:0, want:0},
-		{input:1, want:1},
-		{input:2, want:2},
-		{input:3, want:4},
-		{input:5, want:8},
-		{input:9, want:16},
-		{input:20, want:32},
+		{input: 0, want: 0},
+		{input: 1, want: 1},
+		{input: 2, want: 2},
+		{input: 3, want: 4},
+		{input: 5, want: 8},
+		{input: 9, want: 16},
+		{input: 20, want: 32},
 	}
 	for _, tt := range tests {
 		if got := NextPowerOf2(tt.input); got != tt.want {
@@ -27,15 +27,15 @@ func TestNextPowerOf2(t *testing.T) {
 func TestPrevPowerOf2(t *testing.T) {
 	tests := []struct {
 		input int
-		want int
+		want  int
 	}{
-		{input:0, want:0},
-		{input:1, want:1},
-		{input:2, want:2},
-		{input:3, want:2},
-		{input:5, want:4},
-		{input:9, want:8},
-		{input:20, want:16},
+		{input: 0, want: 0},
+		{input: 1, want: 1},
+		{input: 2, want: 2},
+		{input: 3, want: 2},
+		{input: 5, want: 4},
+		{input: 9, want: 8},
+		{input: 20, want: 16},
 	}
 	for _, tt := range tests {
 		if got := PrevPowerOf2(tt.input); got != tt.want {
