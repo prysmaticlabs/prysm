4bb14fb644a27457530b63f756f6a7525eb45b18
Terence Tsao <terence@prysmaticlabs.com>
2019-09-24 13:43:22 -0700

    Tests for the above

--- shared/trieutil/merkle_trie_test.go
+++ shared/trieutil/merkle_trie_test.go
@@ -19,7 +19,7 @@ func TestNextPowerOf2(t *testing.T) {
 	}
 	for _, tt := range tests {
 		if got := NextPowerOf2(tt.input); got != tt.want {
-			t.Errorf("NextPowerOf2() = %v, want %v", got, tt.want)
+			t.Errorf("NextPowerOf2() = %d, want %d", got, tt.want)
 		}
 	}
 }
@@ -39,7 +39,7 @@ func TestPrevPowerOf2(t *testing.T) {
 	}
 	for _, tt := range tests {
 		if got := PrevPowerOf2(tt.input); got != tt.want {
-			t.Errorf("PrevPowerOf2() = %v, want %v", got, tt.want)
+			t.Errorf("PrevPowerOf2() = %d, want %d", got, tt.want)
 		}
 	}
 }
@@ -55,7 +55,59 @@ func TestMerkleTreeLength(t *testing.T) {
 	}
 	for _, tt := range tests {
 		if got := MerkleTree(tt.leaves); len(got) != tt.length {
-			t.Errorf("len(MerkleTree()) = %v, want %v", got, tt.length)
+			t.Errorf("len(MerkleTree()) = %d, want %d", got, tt.length)
+		}
+	}
+}
+
+func TestConcatGeneralizedIndices(t *testing.T) {
+	tests := []struct {
+		indices []int
+		result int
+	}{
+		{[]int{1,2}, 2},
+		{[]int{1,3,6}, 14},
+		{[]int{1,2,4,8}, 64},
+		{[]int{1,2,5,10}, 74},
+	}
+	for _, tt := range tests {
+		if got := ConcatGeneralizedIndices(tt.indices); got != tt.result {
+			t.Errorf("ConcatGeneralizedIndices() = %d, want %d", got, tt.result)
+		}
+	}
+}
+
+func TestIntegerSquareRoot(t *testing.T) {
+	tests := []struct {
+		number int
+		root   int
+	}{
+		{
+			number: 20,
+			root:   4,
+		},
+		{
+			number: 200,
+			root:   7,
+		},
+		{
+			number: 1987,
+			root:   10,
+		},
+		{
+			number: 34989843,
+			root:   25,
+		},
+		{
+			number: 97282,
+			root:   16,
+		},
+	}
+
+	for _, tt := range tests {
+		root := GeneralizedIndexLength(tt.number)
+		if tt.root != root {
+			t.Errorf("GeneralizedIndexLength() = %d, want %d", tt.root, root)
 		}
 	}
 }
