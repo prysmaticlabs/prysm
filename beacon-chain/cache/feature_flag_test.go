c0ecc0b55a067e2ad507404da15721dd4e9d9845
Terence Tsao <terence@prysmaticlabs.com>
2019-09-25 14:03:10 -0700

    Removed active balance and ancestor block caches

--- beacon-chain/cache/feature_flag_test.go
+++ beacon-chain/cache/feature_flag_test.go
@@ -5,8 +5,6 @@ import "github.com/prysmaticlabs/prysm/shared/featureconfig"
 func init() {
 	featureconfig.InitFeatureConfig(&featureconfig.FeatureFlagConfig{
 		EnableAttestationCache:   true,
-		EnableActiveBalanceCache: true,
-		EnableAncestorBlockCache: true,
 		EnableEth1DataVoteCache:  true,
 	})
 }
