957ea65158486f16c74d6f7af20fd4236606e5bf
Preston Van Loon <preston@prysmaticlabs.com>
2019-09-26 22:42:40 -0700

    change pk bytes maps to 48 bytes

--- beacon-chain/core/state/stateutils/validator_index_map.go
+++ beacon-chain/core/state/stateutils/validator_index_map.go
@@ -7,10 +7,10 @@ import (
 
 // ValidatorIndexMap builds a lookup map for quickly determining the index of
 // a validator by their public key.
-func ValidatorIndexMap(state *pb.BeaconState) map[[32]byte]int {
-	m := make(map[[32]byte]int)
+func ValidatorIndexMap(state *pb.BeaconState) map[[48]byte]int {
+	m := make(map[[48]byte]int)
 	for idx, record := range state.Validators {
-		key := bytesutil.ToBytes32(record.PublicKey)
+		key := bytesutil.ToBytes48(record.PublicKey)
 		m[key] = idx
 	}
 	return m
