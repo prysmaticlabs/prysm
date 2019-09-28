d46006f11b6df62f7e4009057e518c770ad6bed4
Terence Tsao <terence@prysmaticlabs.com>
2019-09-28 15:29:15 -0700

    Updated committee helper functions to d4f48117d3463cddb18f0f65d66798933d679beb

--- shard-chains/core/helpers/committee.go
+++ shard-chains/core/helpers/committee.go
@@ -45,8 +45,8 @@ func PeriodCommittee(state *pb.BeaconState, shard uint64, epoch uint64) ([]uint6
 //    newer_subcommittee = [i for i in newer_committee if i % EPOCHS_PER_SHARD_PERIOD <= epoch % EPOCHS_PER_SHARD_PERIOD]
 //    return older_subcommittee + newer_subcommittee
 func ShardCommittee(state *pb.BeaconState, shard uint64, epoch uint64) ([]uint64, error) {
-	olderEpoch := ShardPeriodStartEpoch(epoch, 2)
-	newerEpoch := ShardPeriodStartEpoch(epoch, 1)
+	olderEpoch := ComputeShardPeriodStartEpoch(epoch, 2)
+	newerEpoch := ComputeShardPeriodStartEpoch(epoch, 1)
 	olderCommittee, err := PeriodCommittee(state, shard, olderEpoch)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not get older committee")
@@ -77,11 +77,16 @@ func ShardCommittee(state *pb.BeaconState, shard uint64, epoch uint64) ([]uint64
 // ShardProposerIndex returns the shard proposer index of a given shard slot.
 //
 // Spec pseudocode definition:
-//  def get_shard_proposer_index(state: BeaconState, shard: Shard, slot: ShardSlot) -> ValidatorIndex:
-//    epoch = get_current_epoch(state)
-//    active_indices = [i for i in get_shard_committee(state, shard, epoch) if is_active_validator(state.validators[i], epoch)]
-//    seed = hash(get_seed(state, epoch) + int_to_bytes(slot, length=8) + int_to_bytes(shard, length=8))
-//    compute_proposer_index(state, active_indices, seed)
+//  def get_shard_proposer_index(beacon_state: BeaconState, shard: Shard, slot: ShardSlot) -> Optional[ValidatorIndex]:
+//    epoch = get_current_epoch(beacon_state)
+//    shard_committee = get_shard_committee(beacon_state, shard, epoch)
+//    active_indices = [i for i in shard_committee if is_active_validator(beacon_state.validators[i], epoch)]
+//    if not any(active_indices):
+//        return None
+//
+//    epoch_seed = get_seed(beacon_state, epoch, DOMAIN_SHARD_PROPOSER)
+//    seed = hash(epoch_seed + int_to_bytes(slot, length=8) + int_to_bytes(shard, length=8))
+//    return compute_proposer_index(beacon_state, active_indices, seed)
 func ShardProposerIndex(state *pb.BeaconState, shard uint64, slot uint64) (uint64, error) {
 	epoch := helpers.CurrentEpoch(state)
 	shardCommittee, err := ShardCommittee(state, shard, epoch)
@@ -94,6 +99,10 @@ func ShardProposerIndex(state *pb.BeaconState, shard uint64, slot uint64) (uint6
 			activeIndices = append(activeIndices, index)
 		}
 	}
+	if len(activeIndices) == 0 {
+		return 0, errors.New("no active proposer available")
+	}
+	// TODO(9999): Update get_seed to take in domain
 	s, err := helpers.Seed(state, epoch)
 	if err != nil {
 		return 0, errors.Wrap(err, "could not get seed")
@@ -104,7 +113,7 @@ func ShardProposerIndex(state *pb.BeaconState, shard uint64, slot uint64) (uint6
 	seed = append(seed, slotBytes...)
 	seed = append(seed, shardBytes...)
 
-	// TODO(1371): https://github.com/ethereum/eth2.0-specs/pull/1371/files
+	// TODO(9999): https://github.com/ethereum/eth2.0-specs/pull/1371/files
 	// compute_proposer_index is getting implemented in #1371 eth2 repo.
 	return 0, fmt.Errorf("not implemented")
 }
