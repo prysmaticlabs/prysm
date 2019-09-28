f0de990bb5869dfb5d7d8aff5fca93942cee5327
Terence Tsao <terence@prysmaticlabs.com>
2019-09-28 15:29:30 -0700

    Updated rewards helper functions to d4f48117d3463cddb18f0f65d66798933d679beb

--- shard-chains/core/helpers/reward_fee.go
+++ shard-chains/core/helpers/reward_fee.go
@@ -6,21 +6,37 @@ import (
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 )
 
-// AddReward adds reward to input validator index.
+// ProcessDelta adds fee to input validator index.
 //
 // Spec pseudocode definition:
-//  def add_reward(state: BeaconState, shard_state: ShardState, index: ValidatorIndex, delta: Gwei) -> None:
-//    epoch = compute_epoch_of_shard_slot(state.slot)
-//    older_committee = get_period_committee(state, shard_state.shard, compute_shard_period_start_epoch(epoch, 2))
-//    newer_committee = get_period_committee(state, shard_state.shard, compute_shard_period_start_epoch(epoch, 1))
+//  def process_delta(beacon_state: BeaconState,
+//                  shard_state: ShardState,
+//                  index: ValidatorIndex,
+//                  delta: Gwei,
+//                  positive: bool=True) -> None:
+//    epoch = compute_epoch_of_shard_slot(beacon_state.slot)
+//    older_committee = get_period_committee(beacon_state, shard_state.shard, compute_shard_period_start_epoch(epoch, 2))
+//    newer_committee = get_period_committee(beacon_state, shard_state.shard, compute_shard_period_start_epoch(epoch, 1))
 //    if index in older_committee:
-//        shard_state.older_committee_rewards[older_committee.index(index)] += delta
+//        if positive:
+//            shard_state.older_committee_positive_deltas[older_committee.index(index)] += delta
+//        else:
+//            shard_state.older_committee_negative_deltas[older_committee.index(index)] += delta
 //    elif index in newer_committee:
-//        shard_state.newer_committee_rewards[newer_committee.index(index)] += delta
-func AddReward(beaconState *pb.BeaconState, shardState *ethpb.ShardState, index uint64, delta uint64) (*ethpb.ShardState, error) {
-	epoch := ShardSlotToEpoch(beaconState.Slot)
-	olderEpoch := ShardPeriodStartEpoch(epoch, 2)
-	newerEpoch := ShardPeriodStartEpoch(epoch, 1)
+//        if positive:
+//            shard_state.newer_committee_positive_deltas[newer_committee.index(index)] += delta
+//        else:
+//            shard_state.newer_committee_negative_deltas[newer_committee.index(index)] += delta
+func ProcessDelta(
+	beaconState *pb.BeaconState,
+	shardState *ethpb.ShardState,
+	index uint64,
+	delta uint64,
+	positive bool,
+) (*ethpb.ShardState, error) {
+	epoch := ComputeEpochOfShardSlot(beaconState.Slot)
+	olderEpoch := ComputeShardPeriodStartEpoch(epoch, 2)
+	newerEpoch := ComputeShardPeriodStartEpoch(epoch, 1)
 	olderCommittee, err := PeriodCommittee(beaconState, shardState.Shard, olderEpoch)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not get older committee")
@@ -32,55 +48,22 @@ func AddReward(beaconState *pb.BeaconState, shardState *ethpb.ShardState, index 
 
 	for _, i := range olderCommittee {
 		if i == index {
-			shardState.OlderCommitteeRewards[i] += delta
+			if positive {
+				shardState.OlderCommitteePositiveDeltas[olderCommittee[index]] += delta
+			} else {
+				shardState.OlderCommitteeNegativeDeltas[olderCommittee[index]] += delta
+			}
 			return shardState, nil
 		}
 	}
 
 	for _, i := range newerCommittee {
 		if i == index {
-			shardState.NewerCommitteeRewards[i] += delta
-			return shardState, nil
-		}
-	}
-
-	return shardState, nil
-}
-
-// AddFee adds fee to input validator index.
-//
-// Spec pseudocode definition:
-//  def add_fee(state: BeaconState, shard_state: ShardState, index: ValidatorIndex, delta: Gwei) -> None:
-//	    epoch = compute_epoch_of_shard_slot(state.slot)
-//	    older_committee = get_period_committee(state, shard_state.shard, compute_shard_period_start_epoch(epoch, 2))
-//	    newer_committee = get_period_committee(state, shard_state.shard, compute_shard_period_start_epoch(epoch, 1))
-//	    if index in older_committee:
-//	        shard_state.older_committee_fees[older_committee.index(index)] += delta
-//	    elif index in newer_committee:
-//	        shard_state.newer_committee_fees[newer_committee.index(index)] += delta
-func AddFee(beaconState *pb.BeaconState, shardState *ethpb.ShardState, index uint64, delta uint64) (*ethpb.ShardState, error) {
-	epoch := ShardSlotToEpoch(beaconState.Slot)
-	olderEpoch := ShardPeriodStartEpoch(epoch, 2)
-	newerEpoch := ShardPeriodStartEpoch(epoch, 1)
-	olderCommittee, err := PeriodCommittee(beaconState, shardState.Shard, olderEpoch)
-	if err != nil {
-		return nil, errors.Wrap(err, "could not get older committee")
-	}
-	newerCommittee, err := PeriodCommittee(beaconState, shardState.Shard, newerEpoch)
-	if err != nil {
-		return nil, errors.Wrap(err, "could not get newer committee")
-	}
-
-	for _, i := range olderCommittee {
-		if i == index {
-			shardState.OlderCommitteeFees[i] += delta
-			return shardState, nil
-		}
-	}
-
-	for _, i := range newerCommittee {
-		if i == index {
-			shardState.NewerCommitteeFees[i] += delta
+			if positive {
+				shardState.NewerCommitteePositiveDeltas[newerCommittee[index]] += delta
+			} else {
+				shardState.NewerCommitteeNegativeDeltas[newerCommittee[index]] += delta
+			}
 			return shardState, nil
 		}
 	}
