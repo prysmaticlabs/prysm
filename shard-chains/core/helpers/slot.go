d86a4f78423e3f1d9ee719ea4120abd84a309dfc
Terence Tsao <terence@prysmaticlabs.com>
2019-09-28 15:29:42 -0700

    Updated slot helper functions to d4f48117d3463cddb18f0f65d66798933d679beb

--- shard-chains/core/helpers/slot.go
+++ shard-chains/core/helpers/slot.go
@@ -4,22 +4,22 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
-// ShardSlotToEpoch returns the epoch number of the input shard slot.
+// ComputeEpochOfShardSlot returns the epoch number of the input shard slot.
 //
 // Spec pseudocode definition:
 //  def compute_epoch_of_shard_slot(slot: ShardSlot) -> Epoch:
-//    return compute_epoch_of_slot(slot // SHARD_SLOTS_PER_EPOCH)
-func ShardSlotToEpoch(slot uint64) uint64 {
+//    return Epoch(slot // SHARD_SLOTS_PER_EPOCH)
+func ComputeEpochOfShardSlot(slot uint64) uint64 {
 	return slot / params.ShardConfig().ShardSlotsPerEpoch
 }
 
-// ShardPeriodStartEpoch returns the start epoch number of the a
+// ComputeShardPeriodStartEpoch returns the start epoch number of the a
 // given shard period.
 //
 // Spec pseudocode definition:
 //  def compute_shard_period_start_epoch(epoch: Epoch, lookback: uint64) -> Epoch:
 //    return Epoch(epoch - (epoch % EPOCHS_PER_SHARD_PERIOD) - lookback * EPOCHS_PER_SHARD_PERIOD)
-func ShardPeriodStartEpoch(epoch uint64, lookback uint64) uint64 {
+func ComputeShardPeriodStartEpoch(epoch uint64, lookback uint64) uint64 {
 	epochsPerShardPeriod := params.ShardConfig().EpochsPerShardPeriod
 	return epoch - (epoch % epochsPerShardPeriod) - lookback*epochsPerShardPeriod
 }
