9cd18488bf81aad06e8e84a5b1e68cf9fa63c78b
Preston Van Loon <preston@prysmaticlabs.com>
2019-09-29 22:15:19 -0700

    Add metrics for p2p failures

--- beacon-chain/sync/metrics.go
+++ beacon-chain/sync/metrics.go
@@ -5,8 +5,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-// TODO(3147): Add metrics for RPC & subscription success/error.
-
 var (
 	messageReceivedCounter = promauto.NewCounterVec(
 		prometheus.CounterOpts{
@@ -19,6 +17,20 @@ var (
 		prometheus.CounterOpts{
 			Name: "p2p_message_received_before_chain_start",
 			Help: "Count of messages received before chain started.",
+		},
+		[]string{"topic"},
+	)
+	messageFailedValidationCounter = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "p2p_message_failed_validation_total",
+			Help: "Count of messages that failed validation.",
+		},
+		[]string{"topic"},
+	)
+	messageFailedProcessingCounter = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "p2p_message_failed_processing_total",
+			Help: "Count of messages that passed validation but failed processing.",
 		},
 		[]string{"topic"},
 	)
