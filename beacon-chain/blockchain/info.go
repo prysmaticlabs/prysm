ad735869158b94cce169c6a568179e08cdbe67d3
Terence Tsao <terence@prysmaticlabs.com>
2019-10-02 11:40:54 -0700

    Starting

--- beacon-chain/blockchain/info.go
+++ beacon-chain/blockchain/info.go
@@ -3,15 +3,64 @@ package blockchain
 import (
 	"bytes"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"sort"
+	"time"
 
+	"github.com/awalterschulze/gographviz"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/sirupsen/logrus"
 )
 
 const latestSlotCount = 10
+
+// For treehandler, each node is a representation of a node in the graph
+type node struct {
+	parentRoot string
+	selfRoot string
+}
+
+// TreeHandler is a handler to serve /tree page in metrics.
+func (s *Service) TreeHandler(w http.ResponseWriter, _ *http.Request) {
+	graphAst, _ := gographviz.Parse([]byte(`digraph G{}`))
+	graph := gographviz.NewGraph()
+	gographviz.Analyse(graphAst, graph)
+
+	currentSlot := s.currentSlot()
+	filter := filters.NewFilter().SetStartSlot(1).SetEndSlot(currentSlot)
+	blks, err := s.beaconDB.Blocks(context.Background(), filter)
+	nodes := make([]node, len(blks))
+	for i:=0; i<len(nodes) ; i++  {
+		r, _ := ssz.SigningRoot(blks[i])
+		nodes[i].selfRoot = hex.EncodeToString(r[:1])
+		nodes[i].parentRoot = hex.EncodeToString(blks[i].ParentRoot[:1])
+	}
+	for i := 0; i<len(nodes); i++ {
+		n := nodes[i]
+		if err := graph.AddNode("G", n.selfRoot, nil); err != nil {
+			log.WithError(err).Error("Could not add node to graph")
+		}
+		if i != 0 {
+			if err := graph.AddEdge(n.parentRoot, n.selfRoot, true, nil); err != nil {
+				log.WithError(err).Error("Could not add node to graph")
+			}
+		}
+	}
+
+	img, err := dotToSvg([]byte(graph.String()))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.ServeFile(w, r, img)
+}
 
 // HeadsHandler is a handler to serve /heads page in metrics.
 func (s *Service) HeadsHandler(w http.ResponseWriter, _ *http.Request) {
@@ -54,4 +103,28 @@ func (s *Service) latestHeadSlots() []int {
 		return slots[len(slots)-latestSlotCount:]
 	}
 	return slots
+}
+
+// This returns the current slot of the chain
+func (s *Service) currentSlot() uint64 {
+	diff := time.Now().Unix() - s.GenesisTime().Unix()
+	return uint64(diff) / params.BeaconConfig().SecondsPerSlot
+}
+
+// This converts a dot raw data to svg
+func dotToSvg(dot []byte) (string, error) {
+	format := "svg"
+	dotExe, err := exec.LookPath("dot")
+	if err != nil {
+		return "", errors.New("unable to find program 'dot', please install it or check your PATH")
+	}
+
+	img := filepath.Join(os.TempDir(), fmt.Sprintf("tree-vis.%s", format))
+
+	cmd := exec.Command(dotExe, fmt.Sprintf("-T%s", format), "-o", img)
+	cmd.Stdin = bytes.NewReader(dot)
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+	return img, nil
 }
