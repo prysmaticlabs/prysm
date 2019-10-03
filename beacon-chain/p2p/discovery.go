b006b18d2f66e11ee21c0a08e2c128ffd1e2db5b
nisdas <nishdas93@gmail.com>
2019-10-03 21:40:39 +0530

    check if IP is nil

--- beacon-chain/p2p/discovery.go
+++ beacon-chain/p2p/discovery.go
@@ -125,9 +125,13 @@ func parseBootStrapAddrs(addrs []string) (discv5Nodes []string, kadDHTNodes []st
 func convertToMultiAddr(nodes []*enode.Node) []ma.Multiaddr {
 	var multiAddrs []ma.Multiaddr
 	for _, node := range nodes {
+		// ignore nodes with no ip address stored
+		if node.IP() == nil {
+			continue
+		}
 		multiAddr, err := convertToSingleMultiAddr(node)
 		if err != nil {
-			log.WithError(err).Debug("Could not convert to multiAddr")
+			log.WithError(err).Error("Could not convert to multiAddr")
 			continue
 		}
 		multiAddrs = append(multiAddrs, multiAddr)
