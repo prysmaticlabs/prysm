2ca6a6a21438b6ed5776b240fe7d784ca36f366f
nisdas <nishdas93@gmail.com>
2019-10-03 14:59:10 +0530

    remove space

--- beacon-chain/sync/rpc_status_test.go
+++ beacon-chain/sync/rpc_status_test.go
@@ -41,7 +41,6 @@ func TestHelloRPCHandler_Disconnects_OnForkVersionMismatch(t *testing.T) {
 	pcl := protocol.ID("/testing")
 
 	var wg sync.WaitGroup
-
 	wg.Add(1)
 	p2.Host.SetStreamHandler(pcl, func(stream network.Stream) {
 		defer wg.Done()
