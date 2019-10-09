2a2b27a00d4e6bdee38c1930c1e70be18f44ab6f
Raul Jordan <raul@prysmaticlabs.com>
2019-10-09 12:23:36 +0900

    added the required shard states

--- wasm/main.go
+++ wasm/main.go
@@ -6,6 +6,18 @@ import (
 	wasm "github.com/wasmerio/go-ext-wasm/wasmer"
 )
 
+type shardBlockData struct {
+}
+
+type shardBlockHeader struct {
+}
+
+type shardState struct {
+	Slot              uint64
+	ParentBlock       *shardBlockHeader
+	ExecEnvStateRoots [][32]byte
+}
+
 func main() {
 	// Reads the WebAssembly module as bytes.
 	bytes, _ := wasm.ReadBytes("scripts/simple.wasm")
@@ -20,4 +32,12 @@ func main() {
 	// types are inferred and values are casted automatically.
 	result, _ := sum(492, 3222)
 	fmt.Println(result)
+
+	// TODO: Get the code from the beacon state exec env index.
+	// TODO: Get the EE pre-state root from the shard state exec env index.
+	// TODO: Run code to get the post-state, update the state root at env index.
+}
+
+func ExecuteCode(code []byte, preStateRoot [32]byte, blockData *shardBlockData) ([32]byte, error) {
+	return [32]byte{}, nil
 }
