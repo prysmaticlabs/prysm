639656387942b6ec2bef806359b5c0f9011b85d9
Ivan Martinez <ivanthegreatdev@gmail.com>
2019-10-09 09:47:03 +0900

    Run gazelle

--- beacon-chain/db/kv/state.go
+++ beacon-chain/db/kv/state.go
@@ -2,7 +2,6 @@ package kv
 
 import (
 	"context"
-	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 
 	"github.com/boltdb/bolt"
 	"github.com/gogo/protobuf/proto"
@@ -13,6 +12,7 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/db/filters"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"go.opencensus.io/trace"
 )
