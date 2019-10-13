c358fbb8159f181f64fdb7c4c90330c2fede077a
Terence Tsao <terence@prysmaticlabs.com>
2019-10-13 10:12:09 -0700

    Fix protobuf defs

--- validator/client/validator_attest.go
+++ validator/client/validator_attest.go
@@ -54,7 +54,7 @@ func (v *validator) AttestToBlockHead(ctx context.Context, slot uint64, pubKey [
 
 	req := &pb.AttestationRequest{
 		Slot:  slot,
-		Shard: assignment.Shard,
+		Index: assignment.Index,
 	}
 	data, err := v.attesterClient.RequestAttestation(ctx, req)
 	if err != nil {
@@ -115,7 +115,7 @@ func (v *validator) AttestToBlockHead(ctx context.Context, slot uint64, pubKey [
 	span.AddAttributes(
 		trace.Int64Attribute("slot", int64(slot)),
 		trace.StringAttribute("attestationHash", fmt.Sprintf("%#x", attResp.Root)),
-		trace.Int64Attribute("shard", int64(data.Crosslink.Shard)),
+		trace.Int64Attribute("index", int64(data.Index)),
 		trace.StringAttribute("blockRoot", fmt.Sprintf("%#x", data.BeaconBlockRoot)),
 		trace.Int64Attribute("justifiedEpoch", int64(data.Source.Epoch)),
 		trace.Int64Attribute("targetEpoch", int64(data.Target.Epoch)),
