515aa60dd0a1350a4a52bfa6d624cddceebf0d74
Terence Tsao <terence@prysmaticlabs.com>
2019-10-13 19:15:03 -0700

    Implemented PrecomputeAttestations

--- beacon-chain/core/epoch/precompute.go
+++ beacon-chain/core/epoch/precompute.go
@@ -1,14 +1,17 @@
 package epoch
 
 import (
+	"bytes"
+	"reflect"
+
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
-// ValidatorInfo track the pre-compute of individual validator info such
+// ValidatorPrecompute track the pre-compute of individual validator info such
 // whether validator has performed certain action. (i.e. votes, block inclusion,
 // winning root participation)
-type ValidatorInfo struct {
+type ValidatorPrecompute struct {
 	// IsSlashed is true if the validator has been slashed.
 	IsSlashed bool
 	// IsWithdrawableCurrentEpoch is true if the validator can withdraw current epoch.
@@ -45,8 +48,8 @@ type ValidatorInfo struct {
 	WinningRootAttestingBalance uint64
 }
 
-// BalanceInfo track the different sets of balances during prev and current epochs.
-type BalanceInfo struct {
+// BalancePrecompute track the different sets of balances during prev and current epochs.
+type BalancePrecompute struct {
 	// CurrentEpoch is the total effective balance of all active validators during current epoch.
 	CurrentEpoch uint64
 	// PrevEpoch is the total effective balance of all active validators during prev epoch.
@@ -66,29 +69,124 @@ type BalanceInfo struct {
 	PrevEpochHeadAttesters uint64
 }
 
-// NewPrecompute returns new instances of ValidatorInfo and BalanceInfo.
-func NewPrecompute(state *pb.BeaconState) ([]*ValidatorInfo, *BalanceInfo) {
-	vInfo := make([]*ValidatorInfo, len(state.Validators))
-	bInfo := &BalanceInfo{}
+// NewPrecompute returns new instances of ValidatorPrecompute and BalancePrecompute.
+func NewPrecompute(state *pb.BeaconState) ([]*ValidatorPrecompute, *BalancePrecompute) {
+	vPrecompute := make([]*ValidatorPrecompute, len(state.Validators))
+	bPrecompute := &BalancePrecompute{}
 
 	for i, v := range state.Validators {
 		currentEpoch := helpers.CurrentEpoch(state)
 		prevEpoch := helpers.PrevEpoch(state)
 		withdrawable := currentEpoch > v.WithdrawableEpoch
-		info := &ValidatorInfo{
+		precomp := &ValidatorPrecompute{
 			IsSlashed:                    v.Slashed,
 			IsWithdrawableCurrentEpoch:   withdrawable,
 			CurrentEpochEffectiveBalance: v.EffectiveBalance,
 		}
 		if helpers.IsActiveValidator(v, currentEpoch) {
-			info.IsActiveCurrentEpoch = true
-			bInfo.CurrentEpoch += v.EffectiveBalance
+			precomp.IsActiveCurrentEpoch = true
+			bPrecompute.CurrentEpoch += v.EffectiveBalance
 		}
 		if helpers.IsActiveValidator(v, prevEpoch) {
-			info.IsActivePrevEpoch = true
-			bInfo.PrevEpoch += v.EffectiveBalance
+			precomp.IsActivePrevEpoch = true
+			bPrecompute.PrevEpoch += v.EffectiveBalance
 		}
-		vInfo[i] = info
+		vPrecompute[i] = precomp
 	}
-	return vInfo, bInfo
+	return vPrecompute, bPrecompute
+}
+
+func PrecomputeAttestations(state *pb.BeaconState, v []*ValidatorPrecompute) error {
+	currentEpoch := helpers.CurrentEpoch(state)
+	prevEpoch := helpers.PrevEpoch(state)
+	for _, a := range append(state.PreviousEpochAttestations, state.CurrentEpochAttestations...) {
+		p := &ValidatorPrecompute{}
+		if a.Data.Target.Epoch == currentEpoch {
+			// Check how validator voted in current epoch.
+			p.IsCurrentEpochAttester = true
+			votedTarget, err := sameTargetBlockRoot(state, a, currentEpoch)
+			if err != nil {
+				return err
+			}
+			if votedTarget {
+				p.IsCurrentEpochTargetAttester = true
+			}
+		} else if a.Data.Target.Epoch == prevEpoch {
+			// Check how validator voted in previous epoch.
+			p.IsPrevEpochAttester = true
+			votedTarget, err := sameTargetBlockRoot(state, a, prevEpoch)
+			if err != nil {
+				return err
+			}
+			if votedTarget {
+				p.IsPrevEpochAttester = true
+			}
+
+			// Inclusion slot and distance are only required for prev epoch attesters.
+			aSlot, err := helpers.AttestationDataSlot(state, a.Data)
+			if err != nil {
+				return err
+			}
+			p.InclusionSlot = aSlot + a.InclusionDelay
+			p.InclusionDistance = a.InclusionDelay
+			p.ProposerIndex = a.ProposerIndex
+
+			// Check if validator voted for canonical blocks.
+			votedHead, err := sameHeadBlockRoot(state, a)
+			if err != nil {
+				return err
+			}
+			if votedHead {
+				p.IsHeadAttester = true
+			}
+		}
+
+		// Update the precompute fields for each attested validators.
+		indices, err := helpers.AttestingIndices(state, a.Data, a.AggregationBits)
+		if err != nil {
+			return err
+		}
+		for _, i := range indices {
+			update(v[i], p)
+		}
+
+	}
+	return nil
+}
+
+func sameTargetBlockRoot(state *pb.BeaconState, a *pb.PendingAttestation, e uint64) (bool, error) {
+	r, err := helpers.BlockRoot(state, e)
+	if err != nil {
+		return false, err
+	}
+	if bytes.Equal(a.Data.Target.Root, r) {
+		return true, nil
+	}
+	return false, nil
+}
+
+func sameHeadBlockRoot(state *pb.BeaconState, a *pb.PendingAttestation) (bool, error) {
+	aSlot, err := helpers.AttestationDataSlot(state, a.Data)
+	if err != nil {
+		return false, err
+	}
+	r, err := helpers.BlockRootAtSlot(state, aSlot)
+	if err != nil {
+		return false, err
+	}
+	if bytes.Equal(a.Data.BeaconBlockRoot, r) {
+		return true, nil
+	}
+	return false, nil
+}
+
+func update(p1 *ValidatorPrecompute, p2 *ValidatorPrecompute) *ValidatorPrecompute {
+	entityType := reflect.TypeOf(p1).Elem()
+	for i := 0; i < entityType.NumField(); i++ {
+		value := entityType.Field(i)
+		oldField := reflect.ValueOf(p1).Elem().Field(i)
+		newField := reflect.ValueOf(p2).Elem().FieldByName(value.Name)
+		oldField.Set(newField)
+	}
+	return p1
 }
