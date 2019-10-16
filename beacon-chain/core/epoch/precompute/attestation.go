240c4cd4bb9ce207d207062f7381130bfac12c66
Terence Tsao <terence@prysmaticlabs.com>
2019-10-16 14:30:10 -0700

    Better file names

--- beacon-chain/core/epoch/precompute/attestation.go
+++ beacon-chain/core/epoch/precompute/attestation.go
@@ -11,42 +11,6 @@ import (
 	"go.opencensus.io/trace"
 )
 
-// New gets called at the beginning of process epoch cycle to return
-// pre computed instances of validators attesting records and total
-// balances attested in an epoch.
-func New(ctx context.Context, state *pb.BeaconState) ([]*Validator, *Balance) {
-	ctx, span := trace.StartSpan(ctx, "precomputeEpoch.New")
-	defer span.End()
-
-	vp := make([]*Validator, len(state.Validators))
-	bp := &Balance{}
-
-	currentEpoch := helpers.CurrentEpoch(state)
-	prevEpoch := helpers.PrevEpoch(state)
-
-	for i, v := range state.Validators {
-		// Was validator withdrawable or slashed
-		withdrawable := currentEpoch >= v.WithdrawableEpoch
-		p := &Validator{
-			IsSlashed:                    v.Slashed,
-			IsWithdrawableCurrentEpoch:   withdrawable,
-			CurrentEpochEffectiveBalance: v.EffectiveBalance,
-		}
-		// Was validator active current epoch
-		if helpers.IsActiveValidator(v, currentEpoch) {
-			p.IsActiveCurrentEpoch = true
-			bp.CurrentEpoch += v.EffectiveBalance
-		}
-		// Was validator active previous epoch
-		if helpers.IsActiveValidator(v, prevEpoch) {
-			p.IsActivePrevEpoch = true
-			bp.PrevEpoch += v.EffectiveBalance
-		}
-		vp[i] = p
-	}
-	return vp, bp
-}
-
 // ProcessAttestations process the attestations in state and update individual validator's pre computes,
 // it also tracks and updates epoch attesting balances.
 func ProcessAttestations(
--- beacon-chain/core/epoch/precompute/attestation_test.go
+++ beacon-chain/core/epoch/precompute/attestation_test.go
@@ -13,44 +13,6 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 )
 
-func TestNew(t *testing.T) {
-	ffe := params.BeaconConfig().FarFutureEpoch
-	s := &pb.BeaconState{
-		Slot: params.BeaconConfig().SlotsPerEpoch,
-		// Validator 0 is slashed
-		// Validator 1 is withdrawable
-		// Validator 2 is active prev epoch and current epoch
-		// Validator 3 is active prev epoch
-		Validators: []*ethpb.Validator{
-			{Slashed: true, WithdrawableEpoch: ffe, EffectiveBalance: 100},
-			{EffectiveBalance: 100},
-			{WithdrawableEpoch: ffe, ExitEpoch: ffe, EffectiveBalance: 100},
-			{WithdrawableEpoch: ffe, ExitEpoch: 1, EffectiveBalance: 100},
-		},
-	}
-	v, b := New(context.Background(), s)
-	if !reflect.DeepEqual(v[0], &Validator{IsSlashed: true, CurrentEpochEffectiveBalance: 100}) {
-		t.Error("Incorrect validator 0 status")
-	}
-	if !reflect.DeepEqual(v[1], &Validator{IsWithdrawableCurrentEpoch: true, CurrentEpochEffectiveBalance: 100}) {
-		t.Error("Incorrect validator 1 status")
-	}
-	if !reflect.DeepEqual(v[2], &Validator{IsActiveCurrentEpoch: true, IsActivePrevEpoch: true, CurrentEpochEffectiveBalance: 100}) {
-		t.Error("Incorrect validator 2 status")
-	}
-	if !reflect.DeepEqual(v[3], &Validator{IsActivePrevEpoch: true, CurrentEpochEffectiveBalance: 100}) {
-		t.Error("Incorrect validator 3 status")
-	}
-
-	wantedBalances := &Balance{
-		CurrentEpoch: 100,
-		PrevEpoch:    200,
-	}
-	if !reflect.DeepEqual(b, wantedBalances) {
-		t.Error("Incorrect wanted balance")
-	}
-}
-
 func TestUpdateValidator(t *testing.T) {
 	vp := []*Validator{{}, {}, {}, {}, {}, {}}
 	record := &Validator{IsCurrentEpochAttester: true, IsCurrentEpochTargetAttester: true,
