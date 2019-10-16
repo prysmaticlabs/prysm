e8d173d6fd5cf4888cf0e2b3fd6e993f920aee71
Preston Van Loon <preston@prysmaticlabs.com>
2019-10-16 11:11:37 -0700

    database changes

--- beacon-chain/blockchain/forkchoice/process_block_test.go
+++ beacon-chain/blockchain/forkchoice/process_block_test.go
@@ -226,47 +226,49 @@ func TestStore_SavesNewBlockAttestations(t *testing.T) {
 	defer testDB.TeardownDB(t, db)
 
 	store := NewForkChoiceService(ctx, db)
-	a1 := &ethpb.Attestation{Data: &ethpb.AttestationData{}, AggregationBits: bitfield.Bitlist{0x02}}
-	a2 := &ethpb.Attestation{Data: &ethpb.AttestationData{BeaconBlockRoot: []byte{'A'}}, AggregationBits: bitfield.Bitlist{0x02}}
+	a1 := &ethpb.Attestation{Data: &ethpb.AttestationData{}, AggregationBits: bitfield.NewBitlist(1), CustodyBits: bitfield.NewBitlist(1)}
+	a2 := &ethpb.Attestation{Data: &ethpb.AttestationData{BeaconBlockRoot: []byte{'A'}}, AggregationBits: bitfield.NewBitlist(1), CustodyBits: bitfield.NewBitlist(1)}
 	r1, _ := ssz.HashTreeRoot(a1.Data)
 	r2, _ := ssz.HashTreeRoot(a2.Data)
 
 	store.saveNewBlockAttestations(ctx, []*ethpb.Attestation{a1, a2})
 
-	saved, err := store.db.Attestation(ctx, r1)
+	saved, err := store.db.AttestationsByDataRoot(ctx, r1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !reflect.DeepEqual(a1, saved) {
+	if !reflect.DeepEqual([]*ethpb.Attestation{a1}, saved) {
 		t.Error("did not retrieve saved attestation")
 	}
 
-	saved, err = store.db.Attestation(ctx, r2)
+	saved, err = store.db.AttestationsByDataRoot(ctx, r2)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !reflect.DeepEqual(a2, saved) {
+	if !reflect.DeepEqual([]*ethpb.Attestation{a2}, saved) {
 		t.Error("did not retrieve saved attestation")
 	}
 
-	a1 = &ethpb.Attestation{Data: &ethpb.AttestationData{}, AggregationBits: bitfield.Bitlist{0x03}}
-	a2 = &ethpb.Attestation{Data: &ethpb.AttestationData{BeaconBlockRoot: []byte{'A'}}, AggregationBits: bitfield.Bitlist{0x03}}
+	a1 = &ethpb.Attestation{Data: &ethpb.AttestationData{}, AggregationBits: bitfield.Bitlist{0x03}, CustodyBits: bitfield.NewBitlist(1)}
+	a2 = &ethpb.Attestation{Data: &ethpb.AttestationData{BeaconBlockRoot: []byte{'A'}}, AggregationBits: bitfield.Bitlist{0x03}, CustodyBits: bitfield.NewBitlist(1)}
 
-	store.saveNewBlockAttestations(ctx, []*ethpb.Attestation{a1, a2})
+	if err := store.saveNewBlockAttestations(ctx, []*ethpb.Attestation{a1, a2}); err != nil {
+		t.Fatal(err)
+	}
 
-	saved, err = store.db.Attestation(ctx, r1)
+	saved, err = store.db.AttestationsByDataRoot(ctx, r1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !reflect.DeepEqual(a1, saved) {
+	if !reflect.DeepEqual([]*ethpb.Attestation{a1}, saved) {
 		t.Error("did not retrieve saved attestation")
 	}
 
-	saved, err = store.db.Attestation(ctx, r2)
+	saved, err = store.db.AttestationsByDataRoot(ctx, r2)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !reflect.DeepEqual(a2, saved) {
+	if !reflect.DeepEqual([]*ethpb.Attestation{a2}, saved) {
 		t.Error("did not retrieve saved attestation")
 	}
 }
---
+++ proto/beacon/db/attestation_container_helper.go
@@ -0,0 +1,70 @@
+package db
+
+import (
+	"github.com/prysmaticlabs/go-bitfield"
+	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
+)
+
+func NewContainerFromAttestations(atts []*ethpb.Attestation) *AttestationContainer {
+	if len(atts) == 0 {
+		panic("no attestations provided")
+	}
+	var sp []*AttestationContainer_SignaturePair
+	for _, att := range atts {
+		sp = append(sp, &AttestationContainer_SignaturePair{
+			AggregationBits: att.AggregationBits,
+			Signature:       att.Signature,
+		})
+	}
+	return &AttestationContainer{
+		Data:           atts[0].Data,
+		SignaturePairs: sp,
+	}
+}
+
+func (ac *AttestationContainer) Contains(att *ethpb.Attestation) bool {
+	for _, sp := range ac.SignaturePairs {
+		if sp.AggregationBits.Contains(att.AggregationBits) {
+			return true
+		}
+	}
+	return false
+}
+
+func (ac *AttestationContainer) ToAttestations() []*ethpb.Attestation {
+	if ac == nil {
+		return nil
+	}
+
+	atts := make([]*ethpb.Attestation, len(ac.SignaturePairs))
+	for i, sp := range ac.SignaturePairs {
+		atts[i] = &ethpb.Attestation{
+			Data:            ac.Data,
+			AggregationBits: sp.AggregationBits,
+			Signature:       sp.Signature,
+			// TODO: Add custody bits in phase 1.
+			// Stub: CustodyBits must be same length as aggregation bits; committee size.
+			CustodyBits: bitfield.NewBitlist(sp.AggregationBits.Len()),
+		}
+	}
+	return atts
+}
+
+func (ac *AttestationContainer) InsertAttestation(att *ethpb.Attestation) {
+	sigPairsNotEclipsed := make([]*AttestationContainer_SignaturePair, 0, len(ac.SignaturePairs))
+	for _, sp := range ac.SignaturePairs {
+		// if att is fully contained in some existing bitfield, do nothing.
+		if sp.AggregationBits.Contains(att.AggregationBits) {
+			return
+		}
+		// filter any existing signature pairs that are fully contained within
+		// the new attestation.
+		if !att.AggregationBits.Contains(sp.AggregationBits) {
+			sigPairsNotEclipsed = append(sigPairsNotEclipsed, sp)
+		}
+	}
+	ac.SignaturePairs = append(sigPairsNotEclipsed, &AttestationContainer_SignaturePair{
+		AggregationBits: att.AggregationBits,
+		Signature:       att.Signature,
+	})
+}
