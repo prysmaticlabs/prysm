1c5932da128e497c039b86ba3c6e6da504fadf33
Terence Tsao <terence@prysmaticlabs.com>
2019-10-15 17:27:06 -0700

    Tests for update validator and balance

--- beacon-chain/core/epoch/precompute/processing_test.go
+++ beacon-chain/core/epoch/precompute/processing_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
-func TestNew_CanSetup(t *testing.T) {
+func TestNew(t *testing.T) {
 	ffe := params.BeaconConfig().FarFutureEpoch
 	s := &pb.BeaconState{
 		Slot: params.BeaconConfig().SlotsPerEpoch,
@@ -44,5 +44,46 @@ func TestNew_CanSetup(t *testing.T) {
 	}
 	if !reflect.DeepEqual(b, wantedBalances) {
 		t.Error("Incorrect wanted balance")
+	}
+}
+
+func TestUpdateValidator(t *testing.T) {
+	vp := []*Validator{{}, {}, {}, {}, {}, {}}
+	record := &Validator{IsCurrentEpochAttester: true, IsCurrentEpochTargetAttester: true,
+		IsPrevEpochAttester: true, IsPrevEpochTargetAttester: true, IsPrevEpochHeadAttester: true}
+	a := &pb.PendingAttestation{InclusionDelay: 1, ProposerIndex: 2}
+
+	// Indices 1 3 and 5 attested
+	vp = updateValidator(vp, record, []uint64{1, 3, 5}, a)
+
+	wanted := &Validator{IsCurrentEpochAttester: true, IsCurrentEpochTargetAttester: true,
+		IsPrevEpochAttester: true, IsPrevEpochTargetAttester: true, IsPrevEpochHeadAttester: true, ProposerIndex: 2, InclusionDistance: 1}
+	wantedVp := []*Validator{{}, wanted, {}, wanted, {}, wanted}
+	if !reflect.DeepEqual(vp, wantedVp) {
+		t.Error("Incorrect attesting validator calculations")
+	}
+}
+
+func TestUpdateBalance(t *testing.T) {
+	vp := []*Validator{
+		{IsCurrentEpochAttester: true, CurrentEpochEffectiveBalance: 100},
+		{IsCurrentEpochTargetAttester: true, IsCurrentEpochAttester: true, CurrentEpochEffectiveBalance: 100},
+		{IsCurrentEpochTargetAttester: true, CurrentEpochEffectiveBalance: 100},
+		{IsPrevEpochAttester: true, CurrentEpochEffectiveBalance: 100},
+		{IsPrevEpochAttester: true, IsPrevEpochTargetAttester: true, CurrentEpochEffectiveBalance: 100},
+		{IsPrevEpochHeadAttester: true, CurrentEpochEffectiveBalance: 100},
+		{IsPrevEpochAttester: true, IsPrevEpochHeadAttester: true, CurrentEpochEffectiveBalance: 100},
+		{IsSlashed: true, IsCurrentEpochAttester: true, CurrentEpochEffectiveBalance: 100},
+	}
+	wantedBp := &Balance{
+		CurrentEpochAttesters:       200,
+		CurrentEpochTargetAttesters: 200,
+		PrevEpochAttesters:          300,
+		PrevEpochTargetAttesters:    100,
+		PrevEpochHeadAttesters:      200,
+	}
+	bp := updateBalance(vp, &Balance{})
+	if !reflect.DeepEqual(bp, wantedBp) {
+		t.Error("Incorrect balance calculations")
 	}
 }
