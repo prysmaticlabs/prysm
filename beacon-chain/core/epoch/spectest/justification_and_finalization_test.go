902dd56afbe7b4d010ee0f2f6a96f23ad8253c43
Terence Tsao <terence@prysmaticlabs.com>
2019-10-18 08:47:11 -0700

    Add precompute spec tests

--- beacon-chain/core/epoch/spectest/justification_and_finalization_test.go
+++ beacon-chain/core/epoch/spectest/justification_and_finalization_test.go
@@ -1,10 +1,12 @@
 package spectest
 
 import (
+	"context"
 	"path"
 	"testing"
 
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/epoch"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/epoch/precompute"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/params/spectest"
@@ -22,6 +24,7 @@ func runJustificationAndFinalizationTests(t *testing.T, config string) {
 		t.Run(folder.Name(), func(t *testing.T) {
 			folderPath := path.Join(testsFolderPath, folder.Name())
 			testutil.RunEpochOperationTest(t, folderPath, processJustificationAndFinalizationWrapper)
+			testutil.RunEpochOperationTest(t, folderPath, processJustificationAndFinalizationPrecomputeWrapper)
 		})
 	}
 }
@@ -47,6 +50,22 @@ func processJustificationAndFinalizationWrapper(t *testing.T, state *pb.BeaconSt
 	}
 
 	state, err = epoch.ProcessJustificationAndFinalization(state, prevEpochAttestedBalance, currentEpochAttestedBalance)
+	if err != nil {
+		t.Fatalf("could not process justification: %v", err)
+	}
+
+	return state, nil
+}
+
+func processJustificationAndFinalizationPrecomputeWrapper(t *testing.T, state *pb.BeaconState) (*pb.BeaconState, error) {
+	ctx := context.Background()
+	vp, bp := precompute.New(ctx, state)
+	_, bp, err := precompute.ProcessAttestations(ctx, state, vp, bp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	state, err = precompute.ProcessJustificationAndFinalizationPreCompute(state, bp)
 	if err != nil {
 		t.Fatalf("could not process justification: %v", err)
 	}
