297da4718b7874bd91d4577f7aab4f8cbef9074b
Terence Tsao <terence@prysmaticlabs.com>
2019-10-28 14:48:41 -0700

    Starting but need new seed function

--- beacon-chain/core/helpers/shuffle.go
+++ beacon-chain/core/helpers/shuffle.go
@@ -35,13 +35,13 @@ func SplitIndices(l []uint64, n uint64) [][]uint64 {
 // constant between iterations instead of reallocating it each iteration as in the spec. This implementation is based
 // on the original implementation from protolambda, https://github.com/protolambda/eth2-shuffle
 func ShuffledIndex(index uint64, indexCount uint64, seed [32]byte) (uint64, error) {
-	return innerShuffledIndex(index, indexCount, seed, true /* shuffle */)
+	return ComputeShuffledIndex(index, indexCount, seed, true /* shuffle */)
 }
 
 // UnShuffledIndex returns the inverse of ShuffledIndex. This implementation is based
 // on the original implementation from protolambda, https://github.com/protolambda/eth2-shuffle
 func UnShuffledIndex(index uint64, indexCount uint64, seed [32]byte) (uint64, error) {
-	return innerShuffledIndex(index, indexCount, seed, false /* un-shuffle */)
+	return ComputeShuffledIndex(index, indexCount, seed, false /* un-shuffle */)
 }
 
 // Spec pseudocode definition:
@@ -63,7 +63,7 @@ func UnShuffledIndex(index uint64, indexCount uint64, seed [32]byte) (uint64, er
 //        index = flip if bit else index
 //
 //    return ValidatorIndex(index)
-func innerShuffledIndex(index uint64, indexCount uint64, seed [32]byte, shuffle bool) (uint64, error) {
+func ComputeShuffledIndex(index uint64, indexCount uint64, seed [32]byte, shuffle bool) (uint64, error) {
 	if params.BeaconConfig().ShuffleRoundCount == 0 {
 		return index, nil
 	}
