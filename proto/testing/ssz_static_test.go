c80c8227ee8f73945a397d0a29dc571b1d324a9d
Terence Tsao <terence@prysmaticlabs.com>
2019-10-28 10:49:26 -0700

    Fixed static test

--- proto/testing/ssz_static_test.go
+++ proto/testing/ssz_static_test.go
@@ -110,8 +110,6 @@ func UnmarshalledSSZ(serializedBytes []byte, folderName string) (interface{}, er
 		obj = &pb.BeaconState{}
 	case "Checkpoint":
 		obj = &ethpb.Checkpoint{}
-	case "CompactCommittee":
-		obj = &pb.CompactCommittee{}
 	case "Crosslink":
 		obj = &ethpb.Crosslink{}
 	case "Deposit":
