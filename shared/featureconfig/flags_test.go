a85c4f0115a97c627092f319a0e6e36de4bd87b2
Preston Van Loon <preston@prysmaticlabs.com>
2019-10-29 08:41:17 -0700

    Deprecated do not use

--- shared/featureconfig/flags_test.go
+++ shared/featureconfig/flags_test.go
@@ -2,6 +2,7 @@ package featureconfig
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -10,6 +11,9 @@ func TestDeprecatedFlags(t *testing.T) {
 		fv := reflect.ValueOf(f)
 		if !fv.FieldByName("Hidden").Bool() {
 			t.Errorf("%s must be hidden when deprecated.", f.GetName())
+		}
+		if !strings.Contains(fv.FieldByName("Usage").String(), "DEPRECATED. DO NOT USE.") {
+			t.Errorf("Usage for %s must contain \"DEPRECATED. DO NOT USE.\"", f.GetName())
 		}
 	}
 }
