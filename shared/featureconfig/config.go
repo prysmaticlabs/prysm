f1627c0b67ce9373f62e2ba096993e3fc661ae0f
Nishant Das <nishdas93@gmail.com>
2019-10-29 22:31:10 +0800

    Make No Genesis Delay the Default (#3878)

--- shared/featureconfig/config.go
+++ shared/featureconfig/config.go
@@ -25,7 +25,7 @@ var log = logrus.WithField("prefix", "flags")
 
 // Flag is a struct to represent what features the client will perform on runtime.
 type Flag struct {
-	NoGenesisDelay           bool // NoGenesisDelay when processing a chain start genesis event.
+	GenesisDelay             bool // GenesisDelay when processing a chain start genesis event.
 	MinimalConfig            bool // MinimalConfig as defined in the spec.
 	WriteSSZStateTransitions bool // WriteSSZStateTransitions to tmp directory.
 	InitSyncNoVerify         bool // InitSyncNoVerify when initial syncing w/o verifying block's contents.
@@ -64,9 +64,9 @@ func ConfigureBeaconChain(ctx *cli.Context) {
 		log.Warn("Using minimal config")
 		cfg.MinimalConfig = true
 	}
-	if ctx.GlobalBool(NoGenesisDelayFlag.Name) {
+	if ctx.GlobalBool(GenesisDelayFlag.Name) {
 		log.Warn("Using non standard genesis delay. This may cause problems in a multi-node environment.")
-		cfg.NoGenesisDelay = true
+		cfg.GenesisDelay = true
 	}
 	if ctx.GlobalBool(writeSSZStateTransitionsFlag.Name) {
 		log.Warn("Writing SSZ states and blocks after state transitions")
