1864d948d55d19ca1d97b3022f0781ab4ed8f18e
shayzluf <thezluf@gmail.com>
2019-10-29 14:32:09 +0530

    remove unneeded code

--- slasher/flags/flags.go
+++ slasher/flags/flags.go
@@ -1,11 +1,6 @@
 package flags
 
 import (
-	"os"
-	"os/user"
-	"path/filepath"
-	"runtime"
-
 	"github.com/urfave/cli"
 )
 
@@ -27,29 +22,3 @@ var (
 		Usage: "Key for secure gRPC. Pass this and the tls-cert flag in order to use gRPC securely.",
 	}
 )
-
-func homeDir() string {
-	if home := os.Getenv("HOME"); home != "" {
-		return home
-	}
-	if usr, err := user.Current(); err == nil {
-		return usr.HomeDir
-	}
-	return ""
-}
-
-func defaultValidatorDir() string {
-	// Try to place the data folder in the user's home dir
-	home := homeDir()
-	if home != "" {
-		if runtime.GOOS == "darwin" {
-			return filepath.Join(home, "Library", "Eth2Validators")
-		} else if runtime.GOOS == "windows" {
-			return filepath.Join(home, "AppData", "Roaming", "Eth2Validators")
-		} else {
-			return filepath.Join(home, ".eth2validators")
-		}
-	}
-	// As we cannot guess a stable location, return empty and handle later
-	return ""
-}
