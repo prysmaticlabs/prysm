7379c2cb04e52a285c6c4e5b03728d4e91dfa582
shayzluf <thezluf@gmail.com>
2019-11-07 14:07:54 +0530

    min max span update logic

---
+++ slasher/rpc/update_min_max_span.go
@@ -0,0 +1,57 @@
+package rpc
+
+import (
+	"github.com/pkg/errors"
+
+	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
+)
+
+func (ss *Server) UpdateMaxSpan(source uint64, target uint64, validatorIdx uint64) error {
+	spanMap, err := ss.SlasherDb.ValidatorSpansMap(validatorIdx)
+	if err != nil {
+		return errors.Wrapf(err, "couldnt retrieve span map for validatorIdx: %v", validatorIdx)
+	}
+	if spanMap.EpochSpanMap == nil {
+		spanMap.EpochSpanMap = make(map[uint64]*ethpb.MinMaxSpan)
+	}
+	for i := uint64(1); i < target-source; i++ {
+		val := uint32(target - source - i)
+		if spanMap.EpochSpanMap[source+i] == nil {
+			spanMap.EpochSpanMap[source+i] = &ethpb.MinMaxSpan{MinSpan: 0, MaxSpan: 0}
+		}
+		if spanMap.EpochSpanMap[source+i].MaxSpan < val {
+			spanMap.EpochSpanMap[source+i].MaxSpan = val
+		} else {
+			break
+		}
+	}
+	if err := ss.SlasherDb.SaveValidatorSpansMap(validatorIdx, spanMap); err != nil {
+		errors.Wrap(err, "Got error while trying to save validator spans")
+	}
+	return nil
+}
+
+func (ss *Server) UpdateMinSpan(source uint64, target uint64, validatorIdx uint64) error {
+	spanMap, err := ss.SlasherDb.ValidatorSpansMap(validatorIdx)
+	if err != nil {
+		return errors.Wrapf(err, "couldn't retrieve span map for validatorIdx: %v", validatorIdx)
+	}
+	if spanMap.EpochSpanMap == nil {
+		spanMap.EpochSpanMap = make(map[uint64]*ethpb.MinMaxSpan)
+	}
+	for i := source - 1; i > 0; i-- {
+		val := uint32(target - (i))
+		if spanMap.EpochSpanMap[i] == nil {
+			spanMap.EpochSpanMap[i] = &ethpb.MinMaxSpan{MinSpan: 0, MaxSpan: 0}
+		}
+		if spanMap.EpochSpanMap[i].MinSpan == 0 || spanMap.EpochSpanMap[i].MinSpan > val {
+			spanMap.EpochSpanMap[i].MinSpan = val
+		} else {
+			break
+		}
+	}
+	if err := ss.SlasherDb.SaveValidatorSpansMap(validatorIdx, spanMap); err != nil {
+		errors.Wrap(err, "Got error while trying to save validator spans")
+	}
+	return nil
+}
