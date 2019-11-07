da141130ee7841d8c1400f549c262a0497bedab4
Preston Van Loon <preston@prysmaticlabs.com>
2019-11-07 10:02:19 -0800

    regression test

--- beacon-chain/db/kv/finalized_block_roots_test.go
+++ beacon-chain/db/kv/finalized_block_roots_test.go
@@ -8,6 +8,7 @@ import (
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
 var genesisBlockRoot = bytesutil.ToBytes32([]byte{'G', 'E', 'N', 'E', 'S', 'I', 'S'})
@@ -21,7 +22,7 @@ func TestStore_IsFinalizedBlock(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	blks := makeBlocks(t, 128)
+	blks := makeBlocks(t, 0, 128, genesisBlockRoot)
 	if err := db.SaveBlocks(ctx, blks); err != nil {
 		t.Fatal(err)
 	}
@@ -66,18 +67,110 @@ func TestStore_IsFinalizedBlock(t *testing.T) {
 	}
 }
 
-func makeBlocks(t *testing.T, n int) []*ethpb.BeaconBlock {
-	previousRoot := genesisBlockRoot
+// This test scenario is to test a specific edge case where the finalized block root is not part of
+// the finalized and canonical chain.
+//
+// Example:
+// 0    1  2  3   4     5   6     slot
+// a <- b <-- d <- e <- f <- g    roots
+//      ^- c
+// Imagine that epochs are 2 slots and that epoch 1, 2, and 3 are finalized. Checkpoint roots would
+// be c, e, and g. In this scenario, c was a finalized checkpoint root but no block built upon it so
+// it should not be considered "final and canonical" in the view at slot 6.
+func TestStore_IsFinalized_ForkEdgeCase(t *testing.T) {
+	slotsPerEpoch := int(params.BeaconConfig().SlotsPerEpoch)
+	blocks0 := makeBlocks(t, slotsPerEpoch*0, slotsPerEpoch, genesisBlockRoot)
+	blocks1 := append(
+		makeBlocks(t, slotsPerEpoch*1, 1, bytesutil.ToBytes32(sszRootOrDie(t, blocks0[len(blocks0)-1]))), // No block builds off of the first block in epoch.
+		makeBlocks(t, slotsPerEpoch*1+1, slotsPerEpoch-1, bytesutil.ToBytes32(sszRootOrDie(t, blocks0[len(blocks0)-1])))...
+	)
+	blocks2 := makeBlocks(t, slotsPerEpoch*2, slotsPerEpoch, bytesutil.ToBytes32(sszRootOrDie(t, blocks1[len(blocks1)-1])))
+
+
+	db := setupDB(t)
+	defer teardownDB(t, db)
+	ctx := context.Background()
+
+	if err := db.SaveGenesisBlockRoot(ctx, genesisBlockRoot); err != nil {
+		t.Fatal(err)
+	}
+	if err := db.SaveBlocks(ctx, blocks0); err != nil {
+		t.Fatal(err)
+	}
+	if err := db.SaveBlocks(ctx, blocks1); err != nil {
+		t.Fatal(err)
+	}
+	if err := db.SaveBlocks(ctx, blocks2); err != nil {
+		t.Fatal(err)
+	}
+
+	// First checkpoint
+	checkpoint1 := &ethpb.Checkpoint{
+		Root:  sszRootOrDie(t, blocks1[0]),
+		Epoch: 1,
+	}
+	// A state is required to save checkpoint
+	if err := db.SaveState(ctx, &pb.BeaconState{}, bytesutil.ToBytes32(checkpoint1.Root)); err != nil {
+		t.Fatal(err)
+	}
+	if err := db.SaveFinalizedCheckpoint(ctx, checkpoint1); err != nil {
+		t.Fatal(err)
+	}
+	// All blocks in blocks0 and blocks1 should be finalized and canonical.
+	for i, block := range append(blocks0, blocks1...) {
+		root := sszRootOrDie(t, block)
+		if !db.IsFinalizedBlock(ctx, bytesutil.ToBytes32(root)) {
+			t.Errorf("%d - Expected block %#x to be finalized", i, root)
+		}
+	}
+
+	// Second checkpoint
+	checkpoint2 := &ethpb.Checkpoint{
+		Root:  sszRootOrDie(t, blocks2[0]),
+		Epoch: 2,
+	}
+	// A state is required to save checkpoint
+	if err := db.SaveState(ctx, &pb.BeaconState{}, bytesutil.ToBytes32(checkpoint2.Root)); err != nil {
+		t.Fatal(err)
+	}
+	if err := db.SaveFinalizedCheckpoint(ctx, checkpoint2); err != nil {
+		t.Error(err)
+	}
+	// All blocks in blocks0 and blocks2 should be finalized and canonical.
+	for i, block := range append(blocks0, blocks2...) {
+		root := sszRootOrDie(t, block)
+		if !db.IsFinalizedBlock(ctx, bytesutil.ToBytes32(root)) {
+			t.Errorf("%d - Expected block %#x to be finalized", i, root)
+		}
+	}
+	// All blocks in blocks1 should be finalized and canonical, except blocks1[0].
+	for i, block := range blocks1 {
+		root := sszRootOrDie(t, block)
+		if db.IsFinalizedBlock(ctx, bytesutil.ToBytes32(root)) == (i == 0) {
+			t.Errorf("Expected db.IsFinalizedBlock(ctx, blocks1[%d]) to be %v", i, i != 0)
+		}
+	}
+}
+
+func sszRootOrDie(t *testing.T, block *ethpb.BeaconBlock) []byte {
+	root, err := ssz.SigningRoot(block)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return root[:]
+}
+
+func makeBlocks(t *testing.T, i, n int, previousRoot [32]byte) []*ethpb.BeaconBlock {
 	blocks := make([]*ethpb.BeaconBlock, n)
-	for i := 0; i < n; i++ {
+	for j := i; j < n+i; j++ {
 		parentRoot := make([]byte, 32)
 		copy(parentRoot, previousRoot[:])
-		blocks[i] = &ethpb.BeaconBlock{
-			Slot:       uint64(i + 1),
+		blocks[j-i] = &ethpb.BeaconBlock{
+			Slot:       uint64(j + 1),
 			ParentRoot: parentRoot,
 		}
 		var err error
-		previousRoot, err = ssz.SigningRoot(blocks[i])
+		previousRoot, err = ssz.SigningRoot(blocks[j-i])
 		if err != nil {
 			t.Fatal(err)
 		}
