2de8a1db8d7444b230f8f4576285d1645fcb9f83
shayzluf <thezluf@gmail.com>
2019-11-11 20:34:25 +0530

    added benchmark

---
+++ slasher/rpc/detect_update_min_max_span_bench_test.go
@@ -0,0 +1,27 @@
+package rpc
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/slasher/db"
+)
+
+func BenchmarkMinSpan(b *testing.B) {
+	diffs := []uint64{2, 10, 100, 1000, 10000, 54000}
+	dbs := db.SetupSlasherDB(b)
+	defer db.TeardownSlasherDB(b, dbs)
+	ctx := context.Background()
+	slasherServer := &Server{
+		SlasherDB: dbs,
+	}
+	for _, diff := range diffs {
+		b.Run(fmt.Sprintf("MinSpan with diff: %d", diff), func(ib *testing.B) {
+			for i := uint64(ib.N) - 10; i < uint64(ib.N); i++ {
+				_, _ = slasherServer.DetectAndUpdateMinSpan(ctx, i, i+diff, 1)
+
+			}
+		})
+	}
+}
