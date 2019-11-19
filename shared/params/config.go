89a25c965154b79853c35b62fd3f15ec26f54e0d
Terence Tsao <terence@prysmaticlabs.com>
2019-11-19 15:49:59 -0800

    Config

--- shared/params/config.go
+++ shared/params/config.go
@@ -26,6 +26,7 @@ type BeaconChainConfig struct {
 	ShuffleRoundCount              uint64 `yaml:"SHUFFLE_ROUND_COUNT"`                // ShuffleRoundCount is used for retrieving the permuted index.
 	MinGenesisActiveValidatorCount uint64 `yaml:"MIN_GENESIS_ACTIVE_VALIDATOR_COUNT"` // MinGenesisActiveValidatorCount defines how many validator deposits needed to kick off beacon chain.
 	MinGenesisTime                 uint64 `yaml:"MIN_GENESIS_TIME"`                   // MinGenesisTime is the time that needed to pass before kicking off beacon chain. Currently set to Jan/3/2020.
+	TargetAggregatorsPerCommittee  uint64 // TargetAggregatorsPerCommittee defines the number of aggregators inside one committee.
 
 	// Gwei value constants.
 	MinDepositAmount          uint64 `yaml:"MIN_DEPOSIT_AMOUNT"`          // MinDepositAmount is the maximal amount of Gwei a validator can send to the deposit contract at once.
@@ -120,6 +121,7 @@ var defaultBeaconConfig = &BeaconChainConfig{
 	ShuffleRoundCount:              90,
 	MinGenesisActiveValidatorCount: 65536,
 	MinGenesisTime:                 1578009600,
+	TargetAggregatorsPerCommittee:  16,
 
 	// Gwei value constants.
 	MinDepositAmount:          1 * 1e9,
