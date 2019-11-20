fcb663acdeb89e33df28998374deab731ede160a
terence tsao <terence@prysmaticlabs.com>
2019-11-19 18:24:39 -0800

    Implement aggregation helpers (#4062)
    * Aggregation helpers
    
    * Tests
    
    * Config
    
    * Faulty test cases
    
    * Err

--- beacon-chain/core/helpers/attestation.go
+++ beacon-chain/core/helpers/attestation.go
@@ -1,10 +1,17 @@
 package helpers
 
 import (
+	"encoding/binary"
+	"fmt"
+
 	"github.com/gogo/protobuf/proto"
 	"github.com/pkg/errors"
+	"github.com/prysmaticlabs/go-ssz"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/bls"
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
 var (
@@ -104,4 +111,58 @@ func AggregateAttestation(a1 *ethpb.Attestation, a2 *ethpb.Attestation) (*ethpb.
 	baseAtt.AggregationBits = newBits
 
 	return baseAtt, nil
+}
+
+// SlotSignature returns the signed signature of the hash tree root of input slot.
+//
+// Spec pseudocode definition:
+//   def slot_signature(state: BeaconState, slot: Slot, privkey: int) -> BLSSignature:
+//    domain = get_domain(state, DOMAIN_BEACON_ATTESTER, compute_epoch_at_slot(slot))
+//    return bls_sign(privkey, hash_tree_root(slot), domain)
+func SlotSignature(state *pb.BeaconState, slot uint64, privKey *bls.SecretKey) (*bls.Signature, error) {
+	d := Domain(state.Fork, CurrentEpoch(state), params.BeaconConfig().DomainBeaconAttester)
+	s, err := ssz.HashTreeRoot(slot)
+	if err != nil {
+		return nil, err
+	}
+	return privKey.Sign(s[:], d), nil
+}
+
+// IsAggregator returns true if the signature is from the input validator.
+//
+// Spec pseudocode definition:
+//   def is_aggregator(state: BeaconState, slot: Slot, index: CommitteeIndex, slot_signature: BLSSignature) -> bool:
+//    committee = get_beacon_committee(state, slot, index)
+//    modulo = max(1, len(committee) // TARGET_AGGREGATORS_PER_COMMITTEE)
+//    return bytes_to_int(hash(slot_signature)[0:8]) % modulo == 0
+func IsAggregator(state *pb.BeaconState, slot uint64, index uint64, slotSig *bls.Signature) (bool, error) {
+	committee, err := BeaconCommittee(state, slot, index)
+	if err != nil {
+		return false, err
+	}
+	modulo := uint64(1)
+	if len(committee)/int(params.BeaconConfig().TargetAggregatorsPerCommittee) > 1 {
+		modulo = uint64(len(committee)) / params.BeaconConfig().TargetAggregatorsPerCommittee
+	}
+	fmt.Println(modulo)
+	b := hashutil.Hash(slotSig.Marshal())
+	return binary.LittleEndian.Uint64(b[:8])%modulo == 0, nil
+}
+
+// AggregateSignature returns the aggregated signature of the input attestations.
+//
+// Spec pseudocode definition:
+//   def get_aggregate_signature(attestations: Sequence[Attestation]) -> BLSSignature:
+//    signatures = [attestation.signature for attestation in attestations]
+//    return bls_aggregate_signatures(signatures)
+func AggregateSignature(attestations []*ethpb.Attestation) (*bls.Signature, error) {
+	sigs := make([]*bls.Signature, len(attestations))
+	var err error
+	for i := 0; i < len(sigs); i++ {
+		sigs[i], err = signatureFromBytes(attestations[i].Signature)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return aggregateSignatures(sigs), nil
 }
