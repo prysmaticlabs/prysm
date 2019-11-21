e54a88ed0f96ff5cb58c11184485618063775d49
Terence Tsao <terence@prysmaticlabs.com>
2019-11-20 18:20:26 -0800

    Fix slot

--- beacon-chain/core/helpers/committee.go
+++ beacon-chain/core/helpers/committee.go
@@ -12,6 +12,7 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/sliceutil"
+	"github.com/terencechain/prysm-phase2/beacon-chain/core/helpers"
 )
 
 var committeeCache = cache.NewCommitteeCache()
@@ -296,7 +297,7 @@ func UpdateCommitteeCache(state *pb.BeaconState) error {
 		if err != nil {
 			return err
 		}
-		count, err := CommitteeCountAtSlot(state, epoch*params.BeaconConfig().SlotsPerEpoch)
+		count, err := CommitteeCountAtSlot(state, helpers.StartSlot(epoch))
 		if err != nil {
 			return err
 		}
