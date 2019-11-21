4c3b7c87addce0275fc7fb4e655e4a4619ed3eec
Raul Jordan <raul@prysmaticlabs.com>
2019-11-21 11:37:43 -0600

    added bench info

--- shared/stateutil/state_root_test.go
+++ shared/stateutil/state_root_test.go
@@ -3,12 +3,13 @@ package stateutil
 import (
 	"testing"
 
+	"github.com/prysmaticlabs/go-ssz"
 	"github.com/prysmaticlabs/prysm/shared/interop"
 )
 
-func BenchmarkHashTreeRootState(b *testing.B) {
+func BenchmarkHashTreeRootState_Custom(b *testing.B) {
 	b.StopTimer()
-	count := 2048
+	count := 512
 	genesisState, _, err := interop.GenerateGenesisState(0, uint64(count))
 	if err != nil {
 		b.Fatalf("Could not generate genesis beacon state: %v", err)
@@ -16,6 +17,21 @@ func BenchmarkHashTreeRootState(b *testing.B) {
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		if _, err := HashTreeRootState(genesisState); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkHashTreeRootState_Generic(b *testing.B) {
+	b.StopTimer()
+	count := 512
+	genesisState, _, err := interop.GenerateGenesisState(0, uint64(count))
+	if err != nil {
+		b.Fatalf("Could not generate genesis beacon state: %v", err)
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := ssz.HashTreeRoot(genesisState); err != nil {
 			b.Fatal(err)
 		}
 	}
