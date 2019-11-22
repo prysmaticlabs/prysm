f9fe6b96ae1946f4a9481fad391cccc3a12a70c4
nisdas <nishdas93@gmail.com>
2019-11-22 12:52:12 +0800

    fix data races

--- proto/beacon/rpc/v1/services.pb.go
+++ proto/beacon/rpc/v1/services.pb.go
@@ -7,12 +7,13 @@ import (
 	context "context"
 	encoding_binary "encoding/binary"
 	fmt "fmt"
+	io "io"
+	math "math"
+
 	proto "github.com/gogo/protobuf/proto"
 	types "github.com/gogo/protobuf/types"
 	v1alpha1 "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	grpc "google.golang.org/grpc"
-	io "io"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
