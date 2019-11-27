3f13dcbbd854dceac3160908c794b48753c96ff4
Preston Van Loon <preston@prysmaticlabs.com>
2019-11-26 19:14:45 -0800

    batch is probably fine...

--- beacon-chain/db/kv/attestations.go
+++ beacon-chain/db/kv/attestations.go
@@ -149,7 +149,7 @@ func (k *Store) SaveAttestation(ctx context.Context, att *ethpb.Attestation) err
 		return err
 	}
 
-	err := k.db.Update(func(tx *bolt.Tx) error {
+	err := k.db.Batch(func(tx *bolt.Tx) error {
 		attDataRoot, err := ssz.HashTreeRoot(att.Data)
 		if err != nil {
 			return err
