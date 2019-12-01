3be4894b8adbb0a19d7baa4377a270f1b236ae43
Raul Jordan <raul@prysmaticlabs.com>
2019-11-30 22:30:48 -0600

    Update Ethereum APIs, Allow Genesis Data Retrieval for Blocks + Attestations (#4150)
    * update apis
    
    * include block filter genesis
    
    * genesis atts
    
    * add in workspace file
    
    * include proper diff targeting master of ethereum apis
    
    * genesis block fetching fixes
    
    * remove fmt
    
    * tests for genesis list blocks passing
    
    * fixed up container tests
    
    * tests now passing
    
    * fix up tests

--- beacon-chain/rpc/beacon/blocks_test.go
+++ beacon-chain/rpc/beacon/blocks_test.go
@@ -65,6 +65,73 @@ func TestServer_ListBlocks_NoResults(t *testing.T) {
 	}
 }
 
+func TestServer_ListBlocks_Genesis(t *testing.T) {
+	db := dbTest.SetupDB(t)
+	defer dbTest.TeardownDB(t, db)
+
+	ctx := context.Background()
+	bs := &Server{
+		BeaconDB: db,
+	}
+
+	// Should throw an error if no genesis block is found.
+	if _, err := bs.ListBlocks(ctx, &ethpb.ListBlocksRequest{
+		QueryFilter: &ethpb.ListBlocksRequest_Genesis{
+			Genesis: true,
+		},
+	}); err != nil && !strings.Contains(err.Error(), "Could not find genesis") {
+		t.Fatal(err)
+	}
+
+	// Should return the proper genesis block if it exists.
+	parentRoot := [32]byte{1, 2, 3}
+	blk := &ethpb.BeaconBlock{
+		Slot:       0,
+		ParentRoot: parentRoot[:],
+	}
+	root, err := ssz.SigningRoot(blk)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := db.SaveBlock(ctx, blk); err != nil {
+		t.Fatal(err)
+	}
+	wanted := &ethpb.ListBlocksResponse{
+		BlockContainers: []*ethpb.BeaconBlockContainer{
+			{
+				Block:     blk,
+				BlockRoot: root[:],
+			},
+		},
+		NextPageToken: "0",
+		TotalSize:     1,
+	}
+	res, err := bs.ListBlocks(ctx, &ethpb.ListBlocksRequest{
+		QueryFilter: &ethpb.ListBlocksRequest_Genesis{
+			Genesis: true,
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !proto.Equal(wanted, res) {
+		t.Errorf("Wanted %v, received %v", wanted, res)
+	}
+
+	// Should throw an error if there is more than 1 block
+	// for the genesis slot.
+	if err := db.SaveBlock(ctx, blk); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := bs.ListBlocks(ctx, &ethpb.ListBlocksRequest{
+		QueryFilter: &ethpb.ListBlocksRequest_Genesis{
+			Genesis: true,
+		},
+	}); err != nil && !strings.Contains(err.Error(), "Found more than 1") {
+		t.Fatal(err)
+	}
+}
+
 func TestServer_ListBlocks_Pagination(t *testing.T) {
 	db := dbTest.SetupDB(t)
 	defer dbTest.TeardownDB(t, db)
