acf5baf3f07e58a18b10ba64837ff8c46f1cf7e0
Terence Tsao <terence@prysmaticlabs.com>
2019-12-03 15:36:51 -0800

    Move kv init methods

---
+++ beacon-chain/operations/attestations/kv/kv.go
@@ -0,0 +1,34 @@
+package kv
+
+import (
+	"time"
+
+	"github.com/patrickmn/go-cache"
+	"github.com/prysmaticlabs/prysm/shared/params"
+)
+
+// AttCaches defines the caches used to satisfy attestation pool interface.
+// These caches are KV store for various attestations
+// such are unaggregated, aggregated or attestations within a block.
+type AttCaches struct {
+	aggregatedAtt   *cache.Cache
+	unAggregatedAtt *cache.Cache
+	attInBlock      *cache.Cache
+}
+
+// NewAttCaches initializes a new attestation pool consists of multiple KV store in cache for
+// various kind of attestations.
+func NewAttCaches() *AttCaches {
+
+	secsInEpoch := time.Duration(params.BeaconConfig().SlotsPerEpoch * params.BeaconConfig().SecondsPerSlot)
+
+	// Create caches with default expiration time of one epoch and which
+	// purges expired items every other epoch.
+	pool := &AttCaches{
+		unAggregatedAtt: cache.New(secsInEpoch/time.Minute, 2*secsInEpoch/time.Minute),
+		aggregatedAtt:   cache.New(secsInEpoch/time.Minute, 2*secsInEpoch/time.Minute),
+		attInBlock:      cache.New(secsInEpoch/time.Minute, 2*secsInEpoch/time.Minute),
+	}
+
+	return pool
+}
