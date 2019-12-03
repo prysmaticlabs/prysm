4f889bb563f10f71320c8683b26c8ac1ae5166b2
Terence Tsao <terence@prysmaticlabs.com>
2019-12-03 15:37:10 -0800

    Curd for unaggregated

---
+++ beacon-chain/operations/attestations/kv/unaggregated.go
@@ -0,0 +1,46 @@
+package kv
+
+import (
+	"github.com/patrickmn/go-cache"
+	"github.com/pkg/errors"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/go-ssz"
+)
+
+// SaveUnaggregatedAttestation saves an unaggregated attestation in cache.
+func (p *AttCaches) SaveUnaggregatedAttestation(att *ethpb.Attestation) error {
+	if att.AggregationBits.Count() > 1 {
+		return errors.New("attestation is aggregated")
+	}
+
+	r, err := ssz.HashTreeRoot(att)
+	if err != nil {
+		return errors.Wrap(err, "could not tree hash attestation")
+	}
+
+	// DefaultExpiration is set to what was given to New(). In this case
+	// it's one epoch.
+	p.unAggregatedAtt.Set(string(r[:]), att, cache.DefaultExpiration)
+
+	return nil
+}
+
+// UnaggregatedAttestation returns the aggregated attestations in cache,
+// filtered by committee index and slot.
+func (p *AttCaches) UnaggregatedAttestation(slot uint64, committeeIndex uint64) []*ethpb.Attestation {
+	atts := make([]*ethpb.Attestation, 0, p.unAggregatedAtt.ItemCount())
+	for s, i := range p.unAggregatedAtt.Items() {
+
+		// Type assertion for the worst case. This shouldn't happen.
+		att, ok := i.Object.(*ethpb.Attestation)
+		if !ok {
+			p.unAggregatedAtt.Delete(s)
+		}
+
+		if slot == att.Data.Slot && committeeIndex == att.Data.CommitteeIndex {
+			atts = append(atts, att)
+		}
+	}
+
+	return atts
+}
