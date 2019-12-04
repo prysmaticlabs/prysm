3e8ae88abc7177a6b6251f749ff749a18eeb0d96
Raul Jordan <raul@prysmaticlabs.com>
2019-12-04 14:18:16 -0800

    do cache initialization

--- shared/stateutil/arrays.go
+++ shared/stateutil/arrays.go
@@ -4,23 +4,25 @@ import (
 	"bytes"
 
 	"github.com/minio/highwayhash"
+	"github.com/protolambda/zssz/merkle"
+
 	//"github.com/protolambda/zssz/merkle"
 
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
 )
 
 var (
 	cachedLeaves   = make(map[string][][]byte)
+	cachedLayers   = make(map[string][][][]byte)
 	fastSumHashKey = bytesutil.ToBytes32([]byte("hash_fast_sum64_key"))
 )
 
 func arraysRoot(roots [][]byte, fieldName string) ([32]byte, error) {
-
-	// TODO: Do some init.
-	//if _, ok := a.layers[fieldName]; !ok {
-	//	depth := merkle.GetDepth(uint64(numItems))
-	//	a.layers[fieldName] = make([][][]byte, depth+1)
-	//}
+	if _, ok := cachedLayers[fieldName]; !ok {
+		depth := merkle.GetDepth(uint64(len(roots)))
+		cachedLayers[fieldName] = make([][][]byte, depth+1)
+	}
 
 	hashKeyElements := make([]byte, len(roots)*32)
 	emptyKey := highwayhash.Sum(hashKeyElements, fastSumHashKey[:])
@@ -39,7 +41,7 @@ func arraysRoot(roots [][]byte, fieldName string) ([32]byte, error) {
 		// If indices did change since last computation, we only recompute
 		// the modified branches in the cached Merkle tree for this state field.
 		for i := 0; i < len(changedIndices); i++ {
-			//rt = recomputeRoot(changedIndices[i], chunks, fieldName)
+			rt = recomputeRoot(changedIndices[i], roots, fieldName)
 		}
 		return rt, nil
 	}
@@ -114,4 +116,24 @@ func randaoRoots(roots [][]byte) ([32]byte, error) {
 	}
 	cache.Set(key, res, 32)
 	return res, nil
+}
+
+func recomputeRoot(idx int, chunks [][]byte, fieldName string) [32]byte {
+	root := chunks[idx]
+	for i := 0; i < len(cachedLayers[fieldName])-1; i++ {
+		subIndex := (uint64(idx) / (1 << uint64(i))) ^ 1
+		isLeft := uint64(idx) / (1 << uint64(i))
+		parentIdx := uint64(idx) / (1 << uint64(i+1))
+		item := cachedLayers[fieldName][i][subIndex]
+		if isLeft%2 != 0 {
+			parentHash := hashutil.Hash(append(item, root...))
+			root = parentHash[:]
+		} else {
+			parentHash := hashutil.Hash(append(root, item...))
+			root = parentHash[:]
+		}
+		// Update the cached layers at the parent index.
+		cachedLayers[fieldName][i+1][parentIdx] = root
+	}
+	return bytesutil.ToBytes32(root)
 }
