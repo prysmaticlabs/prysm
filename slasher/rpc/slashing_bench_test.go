49d985e8ee55202fd799b41626f1632e0b3aee21
shayzluf <thezluf@gmail.com>
2019-12-04 11:03:30 +0530

    add benchmark

--- slasher/rpc/slashing_bench_test.go
+++ slasher/rpc/slashing_bench_test.go
@@ -3,8 +3,11 @@ package rpc
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"testing"
 
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/slasher/db"
 )
 
@@ -94,4 +97,47 @@ func BenchmarkDetectSpan(b *testing.B) {
 			}
 		})
 	}
+}
+
+func BenchmarkCheckAttestations(b *testing.B) {
+	dbs := db.SetupSlasherDB(b)
+	defer db.TeardownSlasherDB(b, dbs)
+	ctx := context.Background()
+	slasherServer := &Server{
+		ctx:       ctx,
+		SlasherDB: dbs,
+	}
+	var cb []uint64
+	for i := uint64(0); i < 100; i++ {
+		cb = append(cb, i)
+	}
+	ia1 := &ethpb.IndexedAttestation{
+		CustodyBit_0Indices: cb,
+		CustodyBit_1Indices: []uint64{},
+		Signature:           make([]byte, 96),
+		Data: &ethpb.AttestationData{
+			CommitteeIndex:  0,
+			BeaconBlockRoot: make([]byte, 32),
+			Source:          &ethpb.Checkpoint{Epoch: 2},
+			Target:          &ethpb.Checkpoint{Epoch: 4},
+		},
+	}
+	for i := uint64(0); i < uint64(b.N); i++ {
+		ia1.Data.Target.Epoch = i + 1
+		ia1.Data.Source.Epoch = i
+		b.Logf("In Loop: %d", i)
+		ia1.Data.Slot = (i + 1) * params.BeaconConfig().SlotsPerEpoch
+		root := []byte(strconv.Itoa(int(i)))
+		ia1.Data.BeaconBlockRoot = append(root, ia1.Data.BeaconBlockRoot[len(root):]...)
+		if _, err := slasherServer.IsSlashableAttestation(ctx, ia1); err != nil {
+			b.Errorf("Could not call RPC method: %v", err)
+		}
+	}
+
+	s, err := dbs.Size()
+	if err != nil {
+		b.Error(err)
+	}
+	b.Logf("DB size is: %d", s)
+
 }
