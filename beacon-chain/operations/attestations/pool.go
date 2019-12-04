5d6ba2a7c0a79bfbe8bf5d09a4acdc3dd93bbe83
Terence Tsao <terence@prysmaticlabs.com>
2019-12-03 17:23:25 -0800

    Minor fixes

--- beacon-chain/operations/attestations/pool.go
+++ beacon-chain/operations/attestations/pool.go
@@ -5,18 +5,18 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/operations/attestations/kv"
 )
 
-// Pool defines the necessary methods for Prysm attestations pool to serve
+// AttestationPool defines the necessary methods for Prysm attestations pool to serve
 // fork choice and validators. In the current design, aggregated attestations
 // are used by proposer actor. Unaggregated attestations are used by
 // for aggregator actor.
-type Pool interface {
+type AttestationPool interface {
 	SaveAggregatedAttestation(att *ethpb.Attestation) error
 	AggregatedAttestation() []*ethpb.Attestation
 	SaveUnaggregatedAttestation(att *ethpb.Attestation) error
 	UnaggregatedAttestation() []*ethpb.Attestation
 }
 
-// NewPool initializes a new attestation pool.
-func NewPool(dirPath string) *kv.AttCaches {
+// NewAttestationPool initializes a new attestation pool.
+func NewAttestationPool(dirPath string) *kv.AttCaches {
 	return kv.NewAttCaches()
 }
