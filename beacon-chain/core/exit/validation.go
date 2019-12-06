cb9aa8599b6de76a089ed8347728ffb43fb22835
Jim McDonald <Jim@mcdee.net>
2019-12-06 21:09:36 +0000

    Break out validation logic for voluntary exits to core module

---
+++ beacon-chain/core/exit/validation.go
@@ -0,0 +1,62 @@
+package exit
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/go-ssz"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/bls"
+	"github.com/prysmaticlabs/prysm/shared/mathutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
+	"github.com/prysmaticlabs/prysm/shared/roughtime"
+)
+
+// ValidateVoluntaryExit validates the voluntary exit.
+// If it is invalid for some reason an error, if valid it will return no error.
+func ValidateVoluntaryExit(state *pb.BeaconState, genesisTime time.Time, ve *ethpb.VoluntaryExit) error {
+	if ve.ValidatorIndex >= uint64(len(state.Validators)) {
+		return fmt.Errorf("unknown validator index %d", ve.ValidatorIndex)
+	}
+	validator := state.Validators[ve.ValidatorIndex]
+
+	if !helpers.IsActiveValidator(validator, ve.Epoch) {
+		return fmt.Errorf("validator %d not active at epoch %d", ve.ValidatorIndex, ve.Epoch)
+	}
+	if validator.ExitEpoch != params.BeaconConfig().FarFutureEpoch {
+		return fmt.Errorf("validator %d already exiting or exited", ve.ValidatorIndex)
+	}
+
+	secondsPerEpoch := params.BeaconConfig().SecondsPerSlot * params.BeaconConfig().SlotsPerEpoch
+	currentEpoch := uint64(roughtime.Now().Unix()-genesisTime.Unix()) / secondsPerEpoch
+	earliestRequestedExitEpoch := mathutil.Max(ve.Epoch, currentEpoch)
+	earliestExitEpoch := validator.ActivationEpoch + params.BeaconConfig().PersistentCommitteePeriod
+	if earliestRequestedExitEpoch < earliestExitEpoch {
+		return fmt.Errorf("validator %d cannot exit before epoch %d", ve.ValidatorIndex, earliestExitEpoch)
+	}
+
+	// Confirm signature is valid
+	root, err := ssz.SigningRoot(ve)
+	if err != nil {
+		return errors.New("cannot confirm signature")
+	}
+	sig, err := bls.SignatureFromBytes(ve.Signature)
+	if err != nil {
+		return errors.New("malformed signature")
+	}
+	validatorPubKey, err := bls.PublicKeyFromBytes(validator.PublicKey)
+	if err != nil {
+		return errors.New("invalid validator public key")
+	}
+	domain := bls.ComputeDomain(params.BeaconConfig().DomainVoluntaryExit)
+	verified := sig.Verify(root[:], validatorPubKey, domain)
+	if !verified {
+		return errors.New("incorrect signature")
+	}
+
+	// Parameters are valid.
+	return nil
+}
--- beacon-chain/rpc/validator/exit.go
+++ beacon-chain/rpc/validator/exit.go
@@ -5,13 +5,8 @@ import (
 
 	ptypes "github.com/gogo/protobuf/types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	"github.com/prysmaticlabs/go-ssz"
-	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/exit"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/statefeed"
-	"github.com/prysmaticlabs/prysm/shared/bls"
-	"github.com/prysmaticlabs/prysm/shared/mathutil"
-	"github.com/prysmaticlabs/prysm/shared/params"
-	"github.com/prysmaticlabs/prysm/shared/roughtime"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -24,43 +19,9 @@ func (vs *Server) RequestExit(ctx context.Context, req *ethpb.VoluntaryExit) (*p
 	}
 
 	// Confirm the validator is eligible to exit with the parameters provided
-	if req.ValidatorIndex >= uint64(len(s.Validators)) {
-		return nil, status.Errorf(codes.InvalidArgument, "Unknown validator index %d", req.ValidatorIndex)
-	}
-	validator := s.Validators[req.ValidatorIndex]
-	if !helpers.IsActiveValidator(validator, req.Epoch) {
-		return nil, status.Errorf(codes.InvalidArgument, "Validator %d not active at epoch %d", req.ValidatorIndex, req.Epoch)
-	}
-	if validator.ExitEpoch != params.BeaconConfig().FarFutureEpoch {
-		return nil, status.Errorf(codes.InvalidArgument, "Validator %d already exiting", req.ValidatorIndex)
-	}
-
-	secondsPerEpoch := params.BeaconConfig().SecondsPerSlot * params.BeaconConfig().SlotsPerEpoch
-	genesisTime := vs.GenesisTimeFetcher.GenesisTime().Unix()
-	currentEpoch := uint64(roughtime.Now().Unix()-genesisTime) / secondsPerEpoch
-	earliestRequestedExitEpoch := mathutil.Max(req.Epoch, currentEpoch)
-	earliestExitEpoch := validator.ActivationEpoch + params.BeaconConfig().PersistentCommitteePeriod
-	if earliestRequestedExitEpoch < earliestExitEpoch {
-		return nil, status.Errorf(codes.InvalidArgument, "Validator %d cannot exit before epoch %d", req.ValidatorIndex, earliestExitEpoch)
-	}
-
-	// Confirm signature is valid
-	root, err := ssz.SigningRoot(req)
+	err = exit.ValidateVoluntaryExit(s, vs.GenesisTimeFetcher.GenesisTime(), req)
 	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, "Malformed request")
-	}
-	sig, err := bls.SignatureFromBytes(req.Signature)
-	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "Malformed signature: %v", err)
-	}
-	validatorPubKey, err := bls.PublicKeyFromBytes(validator.PublicKey)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Invalid validator public key: %v", err)
-	}
-	domain := bls.ComputeDomain(params.BeaconConfig().DomainVoluntaryExit)
-	verified := sig.Verify(root[:], validatorPubKey, domain)
-	if !verified {
-		return nil, status.Error(codes.InvalidArgument, "Incorrect signature")
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	// Send the voluntary exit to the state feed.
