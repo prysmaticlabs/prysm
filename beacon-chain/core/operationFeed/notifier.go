6e446887dfa49b85528e55eea51d3fc3645c49e2
Terence Tsao <terence@prysmaticlabs.com>
2019-12-06 17:01:53 -0800

    Notifiers

--- beacon-chain/core/operationFeed/notifier.go
+++ beacon-chain/core/operationFeed/notifier.go
@@ -1,1 +1,8 @@
 package operationFeed
+
+import "github.com/prysmaticlabs/prysm/shared/event"
+
+// Notifier interface defines the methods of the service that provides beacon block operation updates to consumers.
+type Notifier interface {
+	OperationFeed() *event.Feed
+}
