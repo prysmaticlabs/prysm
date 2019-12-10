960a1796f40047d195323d2ffeabe85d8417fb63
Terence Tsao <terence@prysmaticlabs.com>
2019-12-10 14:50:21 -0800

    Add b.N back

--- beacon-chain/core/state/benchmarks/benchmarks_test.go
+++ beacon-chain/core/state/benchmarks/benchmarks_test.go
@@ -119,6 +119,8 @@ func BenchmarkProcessEpoch_2FullEpochs(b *testing.B) {
 	}
 	beaconState.Slot = currentSlot
 
+	b.N = 5
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		// ProcessEpochPrecompute is the optimized version of process epoch. It's enabled by default
 		// at run time.
