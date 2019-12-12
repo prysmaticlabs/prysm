e38d56c7d6c963646d06a40eaf9f4d5e80bada0a
Raul Jordan <raul@prysmaticlabs.com>
2019-12-12 15:45:20 -0600

    test passing

--- beacon-chain/db/kafka/passthrough.go
+++ beacon-chain/db/kafka/passthrough.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	eth "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db/filters"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1_gateway"
 	ethereum_beacon_p2p_v1 "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
@@ -197,6 +198,11 @@ func (e Exporter) DepositContractAddress(ctx context.Context) ([]byte, error) {
 // SaveHeadBlockRoot -- passthrough.
 func (e Exporter) SaveHeadBlockRoot(ctx context.Context, blockRoot [32]byte) error {
 	return e.db.SaveHeadBlockRoot(ctx, blockRoot)
+}
+
+// GenesisBlock -- passthrough.
+func (e Exporter) GenesisBlock(ctx context.Context) (*ethpb.BeaconBlock, error) {
+	return e.db.GenesisBlock(ctx)
 }
 
 // SaveGenesisBlockRoot -- passthrough.
