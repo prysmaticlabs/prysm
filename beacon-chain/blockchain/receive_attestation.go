a51e84df0fd355a78bf3d649908f6c444db11578
Jim McDonald <Jim@mcdee.net>
2019-12-13 21:56:03 +0000

    Add ticker

--- beacon-chain/blockchain/receive_attestation.go
+++ beacon-chain/blockchain/receive_attestation.go
@@ -10,6 +10,7 @@ import (
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
+	"github.com/prysmaticlabs/prysm/shared/runutil"
 	"github.com/sirupsen/logrus"
 	"go.opencensus.io/trace"
 )
@@ -56,26 +57,20 @@ func (s *Service) ReceiveAttestationNoPubsub(ctx context.Context, att *ethpb.Att
 // This processes attestations from the attestation pool to account for validator votes and fork choice.
 func (s *Service) processAttestation() {
 	period := time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Second
-	ticker := time.NewTicker(period)
-	for {
-		ctx := context.Background()
-		select {
-		case <-ticker.C:
-			atts, err := s.opsPoolService.AttestationPoolForForkchoice(ctx)
-			if err != nil {
-				log.WithError(err).Error("Could not retrieve attestation from pool")
-			}
-
-			for _, a := range atts {
-				if err := s.ReceiveAttestationNoPubsub(ctx, a); err != nil {
-					log.WithFields(logrus.Fields{
-						"targetRoot": fmt.Sprintf("%#x", a.Data.Target.Root),
-					}).WithError(err).Error("Could not receive attestation in chain service")
-				}
-			}
-		case <-s.ctx.Done():
-			log.Debug("Context closed, exiting routine")
+	ctx := context.Background()
+	runutil.RunEvery(s.ctx, period, func() {
+		atts, err := s.opsPoolService.AttestationPoolForForkchoice(ctx)
+		if err != nil {
+			log.WithError(err).Error("Could not retrieve attestation from pool")
 			return
 		}
-	}
+
+		for _, a := range atts {
+			if err := s.ReceiveAttestationNoPubsub(ctx, a); err != nil {
+				log.WithFields(logrus.Fields{
+					"targetRoot": fmt.Sprintf("%#x", a.Data.Target.Root),
+				}).WithError(err).Error("Could not receive attestation in chain service")
+			}
+		}
+	})
 }
