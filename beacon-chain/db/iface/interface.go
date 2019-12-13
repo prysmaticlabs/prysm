a5881f924fee8a58f32c0b2ed690fc7aeab1a784
terence tsao <terence@prysmaticlabs.com>
2019-12-13 06:00:29 -0800

    Deprecate active count and committee cache flags (#4276)
    * Deprecate active count and committee cache flags
    * Merge branch 'master' into deprecate-flags

--- beacon-chain/db/iface/interface.go
+++ beacon-chain/db/iface/interface.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	eth "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	"github.com/prysmaticlabs/prysm/beacon-chain/db/filters"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/beacon-chain/db/filters"
 	ethereum_beacon_p2p_v1 "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
