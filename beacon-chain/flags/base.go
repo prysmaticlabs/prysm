c5dcf49ded22ef8f5b66b09bde5d9417f679192e
Nishant Das <nishdas93@gmail.com>
2019-12-13 23:12:49 +0800

    Add Flag For Minimum Handshakes (#4280)
    * add flag
    * jim and preston's review
    * check max peers
    * gaz
    * Merge branch 'master' of https://github.com/prysmaticlabs/geth-sharding into minStatusCount
    * remove space
    * add references
    * add warning log
    * change log
    * gaz

--- beacon-chain/flags/base.go
+++ beacon-chain/flags/base.go
@@ -48,4 +48,12 @@ var (
 		Name:  "grpc-gateway-port",
 		Usage: "Enable gRPC gateway for JSON requests",
 	}
+
+	// MinSyncPeers specifies the required number of successful peer handshakes in order
+	// to start syncing with external peers.
+	MinSyncPeers = cli.IntFlag{
+		Name:  "min-sync-peers",
+		Usage: "The required number of valid peers to connect with before syncing.",
+		Value: 3,
+	}
 )
