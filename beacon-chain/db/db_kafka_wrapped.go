62d0f9d37fec309242d84ceef49cb3ff08e030a1
Preston Van Loon <preston@prysmaticlabs.com>
2019-12-17 14:17:00 -0800

    add flag to exclude kafka

---
+++ beacon-chain/db/db_kafka_wrapped.go
@@ -0,0 +1,18 @@
+// +build kafka_enabled
+
+package db
+
+import (
+	"github.com/prysmaticlabs/prysm/beacon-chain/db/kafka"
+	"github.com/prysmaticlabs/prysm/beacon-chain/db/kv"
+)
+
+// NewDB initializes a new DB with kafka wrapper.
+func NewDB(dirPath string) (Database, error) {
+	db, err := kv.NewKVStore(dirPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return kafka.Wrap(db)
+}
