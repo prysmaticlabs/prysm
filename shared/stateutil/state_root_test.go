5c91ebd9f7b388c293cf832b82636b10738b8880
Raul Jordan <raul@prysmaticlabs.com>
2019-12-17 20:31:38 -0600

    bench ssz tree root

--- shared/stateutil/state_root_test.go
+++ shared/stateutil/state_root_test.go
@@ -2,12 +2,15 @@ package stateutil_test
 
 import (
 	"reflect"
+	"strconv"
 	"strings"
 	"testing"
 
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-ssz"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/interop"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/stateutil"
 )
 
@@ -41,9 +44,31 @@ func TestHashTreeRootEquality(t *testing.T) {
 	}
 }
 
-func BenchmarkHashTreeRootState_Custom(b *testing.B) {
+func BenchmarkHashTreeRootState_Custom_512(b *testing.B) {
 	b.StopTimer()
 	genesisState := setupGenesisState(b, 512)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := stateutil.HashTreeRootState(genesisState); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkHashTreeRootState_Custom_16384(b *testing.B) {
+	b.StopTimer()
+	genesisState := setupGenesisState(b, 16384)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := stateutil.HashTreeRootState(genesisState); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkHashTreeRootState_Custom_300000(b *testing.B) {
+	b.StopTimer()
+	genesisState := setupGenesisState(b, 300000)
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		if _, err := stateutil.HashTreeRootState(genesisState); err != nil {
@@ -64,9 +89,26 @@ func BenchmarkHashTreeRootState_Generic(b *testing.B) {
 }
 
 func setupGenesisState(tb testing.TB, count uint64) *pb.BeaconState {
-	genesisState, _, err := interop.GenerateGenesisState(0, count)
+	genesisState, _, err := interop.GenerateGenesisState(0, 1)
 	if err != nil {
 		tb.Fatalf("Could not generate genesis beacon state: %v", err)
+	}
+	for i := uint64(1); i < count; i++ {
+		someRoot := [32]byte{}
+		someKey := [48]byte{}
+		copy(someRoot[:], strconv.Itoa(int(i)))
+		copy(someKey[:], strconv.Itoa(int(i)))
+		genesisState.Validators = append(genesisState.Validators, &ethpb.Validator{
+			PublicKey:                  someKey[:],
+			WithdrawalCredentials:      someRoot[:],
+			EffectiveBalance:           params.BeaconConfig().MaxEffectiveBalance,
+			Slashed:                    false,
+			ActivationEligibilityEpoch: 1,
+			ActivationEpoch:            1,
+			ExitEpoch:                  1,
+			WithdrawableEpoch:          1,
+		})
+		genesisState.Balances = append(genesisState.Balances, params.BeaconConfig().MaxEffectiveBalance)
 	}
 	return genesisState
 }
