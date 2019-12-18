33d2f941af60247f28e6a4458fc55a73043c8a30
Preston Van Loon <preston@prysmaticlabs.com>
2019-12-17 23:05:28 -0800

    update subscriber to call pool

--- beacon-chain/sync/subscriber_committee_index_beacon_attestation.go
+++ beacon-chain/sync/subscriber_committee_index_beacon_attestation.go
@@ -2,14 +2,19 @@ package sync
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gogo/protobuf/proto"
+	eth "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 )
 
 func (r *Service) committeeIndexBeaconAttestationSubscriber(ctx context.Context, msg proto.Message) error {
-
-	return nil
+	a, ok := msg.(*eth.Attestation)
+	if !ok {
+		return fmt.Errorf("message was not type *eth.Attestation, type=%T", msg)
+	}
+	return r.attPool.SaveUnaggregatedAttestation(a)
 }
 
 func (r *Service) currentCommitteeIndex() int {
