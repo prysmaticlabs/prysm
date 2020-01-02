56b2f0774afceded8aa45862818fd9eecaa1b2d2
shayzluf <thezluf@gmail.com>
2020-01-02 13:35:22 +0530

    typo fix

--- slasher/db/proposer_slashings.go
+++ slasher/db/proposer_slashings.go
@@ -45,7 +45,7 @@ func (status SlashingStatus) String() string {
 type SlashingType uint8
 
 const (
-	// Proposal enun value.
+	// Proposal enum value.
 	Proposal = iota
 	// Attestation enum value.
 	Attestation
