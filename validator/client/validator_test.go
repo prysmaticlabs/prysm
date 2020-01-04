dbf9e170b860df1a6ae28624871550dd67c01a90
Jim McDonald <Jim@mcdee.net>
2020-01-04 01:26:58 +0000

    And again

--- validator/client/validator_test.go
+++ validator/client/validator_test.go
@@ -351,7 +351,6 @@ func TestWaitActivation_NotAllValidatorsActivatedOK(t *testing.T) {
 	v := validator{
 		keyManager:      testKeyManagerThreeValidators,
 		validatorClient: client,
-		pubkeys:         publicKeys(keyMapThreeValidators),
 		genesisTime:     1,
 	}
 	resp := generateMockStatusResponse(publicKeys(v.keyManager))
