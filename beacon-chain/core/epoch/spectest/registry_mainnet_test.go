4eeefbe4cb637e7911ff45cc7dfe402bc493a921
terence tsao <terence@prysmaticlabs.com>
2020-01-04 20:12:31 -0800

    Fix activation queue sorting (#4409)
    * Removed old save/process block atts
    
    * Fixed tests
    
    * Proper sorting by eligibility epoch then by indices
    
    * Deleted old colde

--- beacon-chain/core/epoch/spectest/registry_mainnet_test.go
+++ beacon-chain/core/epoch/spectest/registry_mainnet_test.go
@@ -5,6 +5,5 @@ import (
 )
 
 func TestRegistryUpdatesMainnet(t *testing.T) {
-	t.Skip("Skip until 4272 merged")
 	runRegistryUpdatesTests(t, "mainnet")
 }
--- beacon-chain/core/epoch/spectest/registry_minimal_test.go
+++ beacon-chain/core/epoch/spectest/registry_minimal_test.go
@@ -5,6 +5,5 @@ import (
 )
 
 func TestRegistryUpdatesMinimal(t *testing.T) {
-	t.Skip("Skip until 4272 merged")
 	runRegistryUpdatesTests(t, "minimal")
 }
