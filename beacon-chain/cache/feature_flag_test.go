1cb0edac0088f8d14a243d37bd1249a0925d6fdd
Preston Van Loon <preston@prysmaticlabs.com>
2020-01-12 15:46:14 -0800

    PR #4502 take two (#4522)
    * PR #4502 take two

--- beacon-chain/cache/feature_flag_test.go
+++ beacon-chain/cache/feature_flag_test.go
@@ -4,8 +4,7 @@ import "github.com/prysmaticlabs/prysm/shared/featureconfig"
 
 func init() {
 	featureconfig.Init(&featureconfig.Flags{
-		EnableAttestationCache:   true,
-		EnableEth1DataVoteCache:  true,
-		EnableShuffledIndexCache: true,
+		EnableAttestationCache:  true,
+		EnableEth1DataVoteCache: true,
 	})
 }
