5b338f1c07ed1b2552e1461f3c58e7b608971e69
Jim McDonald <Jim@mcdee.net>
2020-01-12 21:09:41 +0000

    Separate out fallen behind/resync check

--- beacon-chain/sync/service.go
+++ beacon-chain/sync/service.go
@@ -82,6 +82,7 @@ func (r *Service) Start() {
 	r.p2p.AddDisconnectionHandler(r.removeDisconnectedPeerStatus)
 	r.processPendingBlocksQueue()
 	r.maintainPeerStatuses()
+	r.resyncIfBehind()
 }
 
 // Stop the regular sync service.
