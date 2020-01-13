c803220da3243e330342e2e5de6ba6bbbd097e74
Preston Van Loon <preston@prysmaticlabs.com>
2020-01-13 08:45:20 -0800

    Clone head block to avoid mutation

--- beacon-chain/blockchain/service.go
+++ beacon-chain/blockchain/service.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/gogo/protobuf/proto"
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-ssz"
@@ -248,7 +249,7 @@ func (s *Service) saveHead(ctx context.Context, signed *ethpb.SignedBeaconBlock,
 	if err := s.beaconDB.SaveHeadBlockRoot(ctx, r); err != nil {
 		return errors.Wrap(err, "could not save head root in DB")
 	}
-	s.headBlock = signed
+	s.headBlock = proto.Clone(signed).(*ethpb.SignedBeaconBlock)
 
 	headState, err := s.beaconDB.State(ctx, r)
 	if err != nil {
@@ -274,7 +275,7 @@ func (s *Service) saveHeadNoDB(ctx context.Context, b *ethpb.SignedBeaconBlock, 
 
 	s.canonicalRoots[b.Block.Slot] = r[:]
 
-	s.headBlock = b
+	s.headBlock = proto.Clone(b).(*ethpb.SignedBeaconBlock)
 
 	headState, err := s.beaconDB.State(ctx, r)
 	if err != nil {
