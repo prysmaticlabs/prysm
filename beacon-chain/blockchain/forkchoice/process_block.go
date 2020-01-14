6ef1a712c27ed4a4f7befabeb80f3400714e5809
terence tsao <terence@prysmaticlabs.com>
2020-01-14 08:05:22 -0800

    OnBlockCacheFilteredTree (#4541)

--- beacon-chain/blockchain/forkchoice/process_block.go
+++ beacon-chain/blockchain/forkchoice/process_block.go
@@ -93,16 +93,6 @@ func (s *Store) OnBlock(ctx context.Context, signed *ethpb.SignedBeaconBlock) er
 		return errors.Wrap(err, "could not save state")
 	}
 
-	if featureconfig.Get().EnableBlockTreeCache {
-		tree, err := s.getFilterBlockTree(ctx)
-		if err != nil {
-			return errors.Wrap(err, "could not calculate filtered block tree")
-		}
-		s.filteredBlockTreeLock.Lock()
-		s.filteredBlockTree = tree
-		s.filteredBlockTreeLock.Unlock()
-	}
-
 	// Update justified check point.
 	if postState.CurrentJustifiedCheckpoint.Epoch > s.justifiedCheckpt.Epoch {
 		if err := s.updateJustified(ctx, postState); err != nil {
@@ -150,6 +140,26 @@ func (s *Store) OnBlock(ctx context.Context, signed *ethpb.SignedBeaconBlock) er
 		}
 
 		s.nextEpochBoundarySlot = helpers.StartSlot(helpers.NextEpoch(postState))
+	}
+
+	return nil
+}
+
+// OnBlockCacheFilteredTree calls OnBlock with additional of caching of filtered block tree
+// for efficient fork choice processing.
+func (s *Store) OnBlockCacheFilteredTree(ctx context.Context, signed *ethpb.SignedBeaconBlock) error {
+	if err := s.OnBlock(ctx, signed); err != nil {
+		return err
+	}
+
+	if featureconfig.Get().EnableBlockTreeCache {
+		tree, err := s.getFilterBlockTree(ctx)
+		if err != nil {
+			return errors.Wrap(err, "could not calculate filtered block tree")
+		}
+		s.filteredBlockTreeLock.Lock()
+		s.filteredBlockTree = tree
+		s.filteredBlockTreeLock.Unlock()
 	}
 
 	return nil
