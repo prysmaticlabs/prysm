9231f84b928f4807ade6cd3d56dae58ba4944f38
Raul Jordan <raul@prysmaticlabs.com>
2020-01-14 15:14:54 -0600

    wrapped up proposer slashings and variable offset-based methods

--- shared/ssz/block.go
+++ shared/ssz/block.go
@@ -46,14 +46,104 @@ func marshalBlockBody(body *ethpb.BeaconBlockBody, buf []byte, startOffset int) 
 	// Graffiti..
 	copy(buf[fixedIndex:fixedIndex+32], body.RandaoReveal)
 	fixedIndex += 32
+
+	// Randao + Eth1Data + Graffiti + 5 variable-sized fields.
+	fixedLength := 96 + 72 + 32 + (BytesPerLengthOffset * 5)
+	currentOffsetIndex := startOffset + fixedLength
+	nextOffsetIndex := currentOffsetIndex
+
+	// ProposerSlashings.
+	nextOffsetIndex = marshalProposerSlashings(body.ProposerSlashings, buf, currentOffsetIndex)
+	offsetBuf := make([]byte, BytesPerLengthOffset)
+	binary.LittleEndian.PutUint32(offsetBuf, uint32(currentOffsetIndex-startOffset))
+	copy(buf[fixedIndex:fixedIndex+BytesPerLengthOffset], offsetBuf)
+	currentOffsetIndex = nextOffsetIndex
+	fixedIndex += BytesPerLengthOffset
+
+	// AttesterSlashings.
+	nextOffsetIndex = marshalAttesterSlashings(body.ProposerSlashings, buf, currentOffsetIndex)
+	binary.LittleEndian.PutUint32(offsetBuf, uint32(currentOffsetIndex-startOffset))
+	copy(buf[fixedIndex:fixedIndex+BytesPerLengthOffset], offsetBuf)
+	currentOffsetIndex = nextOffsetIndex
+	fixedIndex += BytesPerLengthOffset
+
+	binary.LittleEndian.PutUint32(offsetBuf, uint32(currentOffsetIndex-startOffset))
+	copy(buf[fixedIndex:fixedIndex+BytesPerLengthOffset], offsetBuf)
+	currentOffsetIndex = nextOffsetIndex
+	fixedIndex += BytesPerLengthOffset
 }
 
 func marshalEth1Data(data *ethpb.Eth1Data, buf []byte, startOffset int) int {
 	fixedIndex := startOffset
+	// DepositRoot.
 	copy(buf[fixedIndex:fixedIndex+32], data.DepositRoot)
 	fixedIndex += 32
+
+	// DepositCount.
 	binary.LittleEndian.PutUint64(buf[fixedIndex:fixedIndex+8], data.DepositCount)
+	fixedIndex += 8
+
+	// BlockHash.
 	copy(buf[fixedIndex:fixedIndex+32], data.BlockHash)
+	fixedIndex += 32
+	return fixedIndex
+}
+
+// TODO: Implement.
+func marshalAttesterSlashings(slashings []*ethpb.AttesterSlashing, buf []byte, startOffset int) int {
+	fixedIndex := startOffset
+	for i := 0; i < len(slashings); i++ {
+		fixedIndex = marshalAttesterSlashing(slashings[i], buf, fixedIndex)
+	}
+	return fixedIndex
+}
+
+func marshalAttesterSlashing(slashing *ethpb.AttesterSlashing, buf []byte, startOffset int) int {
+	return 0
+}
+
+func marshalProposerSlashings(slashings []*ethpb.ProposerSlashing, buf []byte, startOffset int) int {
+	fixedIndex := startOffset
+	for i := 0; i < len(slashings); i++ {
+		fixedIndex = marshalProposerSlashing(slashings[i], buf, fixedIndex)
+	}
+	return fixedIndex
+}
+
+func marshalProposerSlashing(slashing *ethpb.ProposerSlashing, buf []byte, startOffset int) int {
+	fixedIndex := startOffset
+
+	binary.LittleEndian.PutUint64(buf[fixedIndex:fixedIndex+8], slashing.ProposerIndex)
+	fixedIndex += 8
+
+	fixedIndex = marshalBlockHeader(slashing.Header_1, buf, fixedIndex)
+	fixedIndex = marshalBlockHeader(slashing.Header_2, buf, fixedIndex)
+	return fixedIndex
+}
+
+func marshalBlockHeader(sheader *ethpb.SignedBeaconBlockHeader, buf []byte, startOffset int) int {
+	fixedIndex := startOffset
+
+	// Slot.
+	binary.LittleEndian.PutUint64(buf[fixedIndex:fixedIndex+8], sheader.Header.Slot)
+	fixedIndex += 8
+
+	// ParentRoot.
+	copy(buf[fixedIndex:fixedIndex+32], sheader.Header.ParentRoot)
+	fixedIndex += 32
+
+	// StateRoot.
+	copy(buf[fixedIndex:fixedIndex+32], sheader.Header.StateRoot)
+	fixedIndex += 32
+
+	// BodyRoot.
+	copy(buf[fixedIndex:fixedIndex+32], sheader.Header.BodyRoot)
+	fixedIndex += 32
+
+	// Signature.
+	copy(buf[fixedIndex:fixedIndex+96], sheader.Signature)
+	fixedIndex += 96
+
 	return fixedIndex
 }
 
