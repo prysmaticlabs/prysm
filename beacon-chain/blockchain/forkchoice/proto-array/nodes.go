c20634ee27eaa5de6cb3c624bfe551a4cb1f5b41
TerenceTsao <terence@prysmaticlabs.com>
2020-01-16 09:10:41 -0800

    Prune store and minor touchups

--- beacon-chain/blockchain/forkchoice/proto-array/nodes.go
+++ beacon-chain/blockchain/forkchoice/proto-array/nodes.go
@@ -3,11 +3,15 @@ package protoarray
 import (
 	"bytes"
 	"errors"
+	"fmt"
+	"math"
+
+	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
 // Insert registers a new block node to the fork choice store.
 // It updates the new node's parent with best child and descendant node.
-func (s *Store) Insert(slot uint64, root [32]byte, parent [32]byte, justifiedEpoch uint64, finalizedEpoch uint64) {
+func (s *Store) Insert(slot uint64, root [32]byte, parent [32]byte, justifiedEpoch uint64, finalizedEpoch uint64) error {
 	s.nodeIndicesLock.Lock()
 	defer s.nodeIndicesLock.Unlock()
 
@@ -32,9 +36,14 @@ func (s *Store) Insert(slot uint64, root [32]byte, parent [32]byte, justifiedEpo
 	s.nodeIndices[root] = uint64(index)
 	s.nodes = append(s.nodes, n)
 
-	if n.parent != nonExistentNode {
-		s.UpdateBestChildAndDescendant(parentIndex, uint64(index))
+	if n.parent == nonExistentNode {
+		return errors.New("invalid parent index")
 	}
+	if err := s.UpdateBestChildAndDescendant(parentIndex, uint64(index)); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Head starts from justifiedRoot and then follows the best descendant links to find the best block for head.
@@ -69,17 +78,24 @@ func (s *Store) Head(justifiedRoot [32]byte) ([32]byte, error) {
 // - The child is already the best child and the parent is updated with the new best descendant.
 // - The child is not the best child but becomes the best child.
 // - The child is not the best child and does not become best child.
-func (s *Store) UpdateBestChildAndDescendant(parentIndex uint64, childIndex uint64) {
+func (s *Store) UpdateBestChildAndDescendant(parentIndex uint64, childIndex uint64) error {
 	parent := s.nodes[parentIndex]
 	child := s.nodes[childIndex]
 
-	childLeadsToViableHead := s.LeadsToViableHead(&child)
+	childLeadsToViableHead, err := s.LeadsToViableHead(&child)
+	if err != nil {
+		return err
+	}
 
 	// Define 3 variables for the 3 options mentioned above. This is to
 	// set `parent.bestChild` and `parent.bestDescendent` to. These
 	// aliases are to assist readability.
 	changeToNone := []uint64{nonExistentNode, nonExistentNode}
-	changeToChild := []uint64{childIndex, child.bestDescendant}
+	bestDescendant := child.bestDescendant
+	if bestDescendant == nonExistentNode {
+		bestDescendant = childIndex
+	}
+	changeToChild := []uint64{childIndex, bestDescendant}
 	noChange := []uint64{parent.bestChild, parent.bestDescendant}
 	newParentChild := make([]uint64, 0)
 
@@ -94,7 +110,10 @@ func (s *Store) UpdateBestChildAndDescendant(parentIndex uint64, childIndex uint
 			newParentChild = changeToChild
 		} else {
 			bestChild := &s.nodes[parent.bestChild]
-			bestChildLeadsToViableHead := s.LeadsToViableHead(bestChild)
+			bestChildLeadsToViableHead, err := s.LeadsToViableHead(bestChild)
+			if err != nil {
+				return err
+			}
 
 			if childLeadsToViableHead && !bestChildLeadsToViableHead {
 				// The child leads to a viable head, but the current best child doesnt.
@@ -131,15 +150,157 @@ func (s *Store) UpdateBestChildAndDescendant(parentIndex uint64, childIndex uint
 	parent.bestChild = newParentChild[0]
 	parent.bestDescendant = newParentChild[1]
 	s.nodes[parentIndex] = parent
+
+	return nil
+}
+
+// ApplyScoreChanges
+
+/// Iterate backwards through the array, touching all nodes and their parents and potentially
+/// the best-child of each parent.
+///
+/// The structure of the `self.nodes` array ensures that the child of each node is always
+/// touched before it's parent.
+///
+/// For each node, the following is done:
+///
+/// - Update the nodes weight with the corresponding delta.
+/// - Back-propgrate each nodes delta to its parents delta.
+/// - Compare the current node with the parents best-child, updating it if the current node
+/// should become the best child.
+/// - Update the parents best-descendant with the current node or its best-descendant, if
+/// required.
+func (s *Store) ApplyScoreChanges(justifiedEpoch uint64, finalizedEpoch uint64, delta []int) error {
+	if len(s.nodeIndices) != len(delta) {
+		return fmt.Errorf("node indices length diff than delta length, %d != %d", len(s.nodeIndices), len(delta))
+	}
+	if len(s.nodes) != len(delta) {
+		return fmt.Errorf("nodes length diff than delta length, %d != %d", len(s.nodes), len(delta))
+	}
+
+	if s.justifiedEpoch != justifiedEpoch {
+		s.justifiedEpoch = justifiedEpoch
+	}
+	if s.finalizedEpoch != finalizedEpoch {
+		s.finalizedEpoch = finalizedEpoch
+	}
+
+	// Iterate backwards through all indices store nodes.
+	for i := len(s.nodes) - 1; i >= 0; i-- {
+		n := s.nodes[i]
+
+		// There is no need to adjust the balances or manage parent of the zero hash since it
+		// is an alias to the genesis block.
+		if n.root == params.BeaconConfig().ZeroHash {
+			continue
+		}
+
+		if i >= len(delta) {
+		}
+		nodeDelta := delta[i]
+
+		if nodeDelta < 0 {
+			if int(n.weight)+nodeDelta < 0 {
+				n.weight = 0
+			} else {
+				n.weight -= uint64(math.Abs(float64(nodeDelta)))
+			}
+		} else {
+			n.weight += uint64(nodeDelta)
+		}
+
+		// Update parent's best child and descendent if the node has a known parent.
+		if n.parent != nonExistentNode {
+			// Back propagate the nodes delta to its parent.
+			if int(n.parent) >= len(delta) {
+				return errors.New("invalid parent index")
+			}
+			delta[n.parent] += nodeDelta
+			s.UpdateBestChildAndDescendant(n.parent, uint64(i))
+		}
+	}
+
+	return nil
+}
+
+// PruneBeforeFinalized prunes the store with the new finalization information. The tree is only
+// pruned if the supplied finalized epoch and root are different than current store value and
+// the number of the nodes in store has met prune threshold.
+func (s *Store) PruneBeforeFinalized(finalizedRoot [32]byte, finalizedEpoch uint64) error {
+	s.nodeIndicesLock.Lock()
+	defer s.nodeIndicesLock.Unlock()
+
+	if finalizedEpoch < s.finalizedEpoch {
+		return fmt.Errorf("reverted finalized epoch %d <= %d", finalizedEpoch, s.finalizedEpoch)
+	} else if finalizedEpoch != s.finalizedEpoch {
+		s.finalizedEpoch = finalizedEpoch
+	}
+
+	finalizedIndex, ok := s.nodeIndices[finalizedRoot]
+	if !ok {
+		return errors.New("finalized node unknown")
+	}
+
+	// The number of the nodes has not met the prune threshold.
+	// Pruning at small numbers incurs more cost than benefit.
+	if finalizedIndex < s.pruneThreshold {
+		return nil
+	}
+
+	// Remove the key/values from indices mapping on to be deleted nodes.
+	for i := uint64(0); i < finalizedIndex; i++ {
+		if int(i) >= len(s.nodes) {
+			return errors.New("invalid node index")
+		}
+		delete(s.nodeIndices, s.nodes[i].root)
+	}
+
+	// Discard all the nodes before finalization.
+	if int(finalizedIndex) >= len(s.nodes) {
+		return errors.New("invalid finalized index")
+	}
+	s.nodes = s.nodes[:finalizedIndex]
+
+	// Adjust indices to node mapping.
+	for k, v := range s.nodeIndices {
+		s.nodeIndices[k] = v - finalizedIndex
+	}
+
+	// Iterate through existing nodes and adjust its parent/child indices with the new layout.
+	for _, node := range s.nodes {
+		if node.parent != nonExistentNode {
+			// If the node's parent is less than finalized index, set it to non existent.
+			node.parent = nonExistentNode
+			if node.parent > finalizedIndex {
+				node.parent -= finalizedIndex
+			}
+		}
+		if node.bestDescendant != nonExistentNode {
+			if node.bestDescendant < finalizedIndex {
+				return errors.New("invalid best descendant index")
+			}
+			node.bestDescendant -= finalizedIndex
+		}
+		if node.bestChild != nonExistentNode {
+			if node.bestDescendant < finalizedIndex {
+				return errors.New("invalid best child index")
+			}
+			node.bestChild -= finalizedIndex
+		}
+	}
+	return nil
 }
 
 // LeadsToViableHead returns true if the node or the best descendent of the node is viable for head.
 // Any node with diff finalized or justified epoch than the ones in fork choice store
 // should not be viable to head.
-func (s *Store) LeadsToViableHead(node *Node) bool {
+func (s *Store) LeadsToViableHead(node *Node) (bool, error) {
 	bestDescendentIndex := node.bestDescendant
+	if bestDescendentIndex == nonExistentNode {
+		return false, errors.New("invalid best descendant index")
+	}
 	bestDescendentNode := &s.nodes[bestDescendentIndex]
-	return s.ViableForHead(bestDescendentNode) || s.ViableForHead(node)
+	return s.ViableForHead(bestDescendentNode) || s.ViableForHead(node), nil
 }
 
 // ViableForHead returns true if the node is viable to head.
