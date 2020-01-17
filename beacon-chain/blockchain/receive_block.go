d8d6449ab6044882241e8eab8f86a69834885fcd
Raul Jordan <raul@prysmaticlabs.com>
2020-01-17 17:18:30 -0600

    no fork choice

--- beacon-chain/blockchain/receive_block.go
+++ beacon-chain/blockchain/receive_block.go
@@ -9,15 +9,15 @@ import (
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-ssz"
+	"github.com/sirupsen/logrus"
+	"go.opencensus.io/trace"
+
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/epoch/precompute"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/feed"
 	statefeed "github.com/prysmaticlabs/prysm/beacon-chain/core/feed/state"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
-	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 	"github.com/prysmaticlabs/prysm/shared/traceutil"
-	"github.com/sirupsen/logrus"
-	"go.opencensus.io/trace"
 )
 
 // BlockReceiver interface defines the methods of chain service receive and processing new blocks.
@@ -81,31 +81,31 @@ func (s *Service) ReceiveBlockNoPubsub(ctx context.Context, block *ethpb.SignedB
 	}
 
 	// Run fork choice after applying state transition on the new block.
-	headRoot, err := s.forkChoiceStore.Head(ctx)
-	if err != nil {
-		return errors.Wrap(err, "could not get head from fork choice service")
+	//headRoot, err := s.forkChoiceStore.Head(ctx)
+	//if err != nil {
+	//	return errors.Wrap(err, "could not get head from fork choice service")
+	//}
+	//
+	//// Only save head if it's different than the current head.
+	//cachedHeadRoot, err := s.HeadRoot(ctx)
+	//if err != nil {
+	//	return errors.Wrap(err, "could not get head root from cache")
+	//}
+	//if !bytes.Equal(headRoot, cachedHeadRoot) {
+	//	signedHeadBlock, err := s.beaconDB.Block(ctx, bytesutil.ToBytes32(headRoot))
+	//	if err != nil {
+	//		return errors.Wrap(err, "could not compute state from block head")
+	//	}
+	//	if signedHeadBlock == nil || signedHeadBlock.Block == nil {
+	//		return errors.New("nil head block")
+	//	}
+	//
+	if err := s.saveHead(ctx, blockCopy, root); err != nil {
+		return errors.Wrap(err, "could not save head")
 	}
 
-	// Only save head if it's different than the current head.
-	cachedHeadRoot, err := s.HeadRoot(ctx)
-	if err != nil {
-		return errors.Wrap(err, "could not get head root from cache")
-	}
-	if !bytes.Equal(headRoot, cachedHeadRoot) {
-		signedHeadBlock, err := s.beaconDB.Block(ctx, bytesutil.ToBytes32(headRoot))
-		if err != nil {
-			return errors.Wrap(err, "could not compute state from block head")
-		}
-		if signedHeadBlock == nil || signedHeadBlock.Block == nil {
-			return errors.New("nil head block")
-		}
-
-		if err := s.saveHead(ctx, signedHeadBlock, bytesutil.ToBytes32(headRoot)); err != nil {
-			return errors.Wrap(err, "could not save head")
-		}
-
-		isCompetingBlock(root[:], blockCopy.Block.Slot, headRoot, signedHeadBlock.Block.Slot)
-	}
+	//	isCompetingBlock(root[:], blockCopy.Block.Slot, headRoot, signedHeadBlock.Block.Slot)
+	//}
 
 	// Send notification of the processed block to the state feed.
 	s.stateNotifier.StateFeed().Send(&feed.Event{
