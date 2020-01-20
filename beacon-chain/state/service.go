d4cd5af4ebf35a00b68361b77dc415f64af9b483
Raul Jordan <raul@prysmaticlabs.com>
2020-01-20 12:09:46 -0600

    begin state service

---
+++ beacon-chain/state/service.go
@@ -0,0 +1,31 @@
+package state
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.WithField("prefix", "state")
+
+// Service defining a beacon state cache that manages the entire lifecycle of
+// the beacon state in the eth2 runtime.
+type Service struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+// Start the service event loop.
+func (s *Service) Start() {
+}
+
+// Stop the service event loop.
+func (s *Service) Stop() error {
+	defer s.cancel()
+	return nil
+}
+
+// Status reports the healthy status of the service. Returning nil means service
+// is correctly running without error.
+func (s *Service) Status() error {
+	return nil
+}
