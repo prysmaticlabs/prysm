a2551dfb31dfd6d895630d4b5b55eade05a15603
Preston Van Loon <preston@prysmaticlabs.com>
2020-01-21 14:43:03 -0800

    lint

--- beacon-chain/operations/voluntaryexits/doc.go
+++ beacon-chain/operations/voluntaryexits/doc.go
@@ -1,2 +1,2 @@
-// package voluntaryexits defines the operations management of voluntary exits.
+// Package voluntaryexits defines the operations management of voluntary exits.
 package voluntaryexits
