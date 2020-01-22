af8e4f107c9cec790c3e0ec8949ecff3112cb051
TerenceTsao <terence@prysmaticlabs.com>
2020-01-22 06:20:29 -0800

    Docs

---
+++ beacon-chain/forkchoice/doc.go
@@ -0,0 +1,7 @@
+/*
+Package forkchoice implements the service to support fork choice for the eth2 beacon chain. This contains the
+necessary components to track latest validators votes, and balances. Then a store object to be used
+to calculate head. High level fork choice summary:
+https://notes.ethereum.org/@vbuterin/rkhCgQteN?type=view#LMD-GHOST-fork-choice-rule
+*/
+package forkchoice
