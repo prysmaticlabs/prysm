8d889f169ea9518e8974f2f9db4da9eca77cb7c6
terence tsao <terence@prysmaticlabs.com>
2020-01-22 08:50:16 -0800

    Part 1 of proto array fork choice - docs and interfaces (#4615)
    * Docs
    
    * Interface definitions
    
    * Fmt and gazelle
    
    * Rename interface to interfaces

---
+++ beacon-chain/forkchoice/protoarray/doc.go
@@ -0,0 +1,7 @@
+/*
+Package protoarray implements proto array fork choice as outlined:
+https://github.com/protolambda/lmd-ghost#array-based-stateful-dag-proto_array
+This was motivated by the following light house implementation:
+https://github.com/sigp/lighthouse/pull/804
+*/
+package protoarray
