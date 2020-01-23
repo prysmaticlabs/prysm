043933c5876eec1b638f86b4f7ebfa11889fd3c4
Raul Jordan <raul@prysmaticlabs.com>
2020-01-22 18:23:15 -0600

    fix build in fork choice

--- beacon-chain/blockchain/forkchoice/process_block.go
+++ beacon-chain/blockchain/forkchoice/process_block.go
@@ -66,11 +66,11 @@ func (s *Store) OnBlock(ctx context.Context, signed *ethpb.SignedBeaconBlock) er
 	b := signed.Block
 
 	// Retrieve incoming block's pre state.
-	preState, err := s.getBlockPreState(ctx, b)
+	postState, err := s.getBlockPreState(ctx, b)
 	if err != nil {
 		return err
 	}
-	preStateValidatorCount := preState.NumofValidators()
+	preStateValidatorCount := postState.NumofValidators()
 
 	root, err := ssz.HashTreeRoot(b)
 	if err != nil {
@@ -80,8 +80,7 @@ func (s *Store) OnBlock(ctx context.Context, signed *ethpb.SignedBeaconBlock) er
 		"slot": b.Slot,
 		"root": fmt.Sprintf("0x%s...", hex.EncodeToString(root[:])[:8]),
 	}).Info("Executing state transition on block")
-	postState, err := state.ExecuteStateTransition(ctx, preState, signed)
-	if err != nil {
+	if err := state.ExecuteStateTransition(ctx, postState, signed); err != nil {
 		return errors.Wrap(err, "could not execute state transition")
 	}
 
@@ -137,7 +136,7 @@ func (s *Store) OnBlock(ctx context.Context, signed *ethpb.SignedBeaconBlock) er
 		if err := helpers.UpdateCommitteeCache(postState, helpers.CurrentEpoch(postState)); err != nil {
 			return err
 		}
-		if err := helpers.UpdateProposerIndicesInCache(postState, helpers.CurrentEpoch(postState)); err != nil {
+		if err := helpers.UpdateProposerIndicesInCache(postState, postState.Validators(), helpers.CurrentEpoch(postState)); err != nil {
 			return err
 		}
 
@@ -185,16 +184,15 @@ func (s *Store) OnBlockInitialSyncStateTransition(ctx context.Context, signed *e
 	defer s.initSyncStateLock.Unlock()
 
 	// Retrieve incoming block's pre state.
-	preState, err := s.cachedPreState(ctx, b)
+	postState, err := s.cachedPreState(ctx, b)
 	if err != nil {
 		return err
 	}
-	preStateValidatorCount := preState.NumofValidators()
+	postStateValidatorCount := postState.NumofValidators()
 
 	log.WithField("slot", b.Slot).Debug("Executing state transition on block")
 
-	postState, err := state.ExecuteStateTransitionNoVerifyAttSigs(ctx, preState, signed)
-	if err != nil {
+	if err := state.ExecuteStateTransitionNoVerifyAttSigs(ctx, postState, signed); err != nil {
 		return errors.Wrap(err, "could not execute state transition")
 	}
 
@@ -246,7 +244,7 @@ func (s *Store) OnBlockInitialSyncStateTransition(ctx context.Context, signed *e
 	}
 
 	// Update validator indices in database as needed.
-	if err := s.saveNewValidators(ctx, preStateValidatorCount, postState); err != nil {
+	if err := s.saveNewValidators(ctx, postStateValidatorCount, postState); err != nil {
 		return errors.Wrap(err, "could not save finalized checkpoint")
 	}
 
