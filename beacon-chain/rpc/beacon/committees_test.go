119ec5bcd43e9cbc12578b1f3a075cc5a4242910
Raul Jordan <raul@prysmaticlabs.com>
2020-01-24 17:15:06 -0600

    resolve tests

--- beacon-chain/rpc/beacon/committees_test.go
+++ beacon-chain/rpc/beacon/committees_test.go
@@ -14,8 +14,6 @@ import (
 	dbTest "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
 	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
 	pbp2p "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
-	"gopkg.in/d4l3k/messagediff.v1"
-
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
@@ -190,7 +188,8 @@ func TestServer_ListBeaconCommittees_FromArchive(t *testing.T) {
 		ActiveValidatorCount: uint64(numValidators),
 	}
 	if !reflect.DeepEqual(wantedRes, res1) {
-		t.Error(messagediff.PrettyDiff(wantedRes, res1))
+		t.Errorf("Wanted %v", wantedRes)
+		t.Errorf("Received %v", res1)
 	}
 }
 
