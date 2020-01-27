127f05d531365c644160d785c2a9f5d3d5b4e170
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-01-26 21:42:10 -0500

    Allow easy plugin of featureflags into E2E (#4659)
    * Enable easy plugin of featureflags into E2E
    
    * Gazelle
    
    * Fix text
    
    * Fix whitespace

--- endtoend/demo_e2e_test.go
+++ endtoend/demo_e2e_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	ev "github.com/prysmaticlabs/prysm/endtoend/evaluators"
+	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 )
@@ -14,13 +15,8 @@ func TestEndToEnd_DemoConfig(t *testing.T) {
 	params.UseDemoBeaconConfig()
 
 	demoConfig := &end2EndConfig{
-		beaconFlags: []string{
-			"--enable-ssz-cache",
-			"--cache-proposer-indices",
-			"--cache-filtered-block-tree",
-			"--enable-skip-slots-cache",
-			"--enable-attestation-cache",
-		},
+		beaconFlags: featureconfig.E2EBeaconChainFlags,
+		validatorFlags: featureconfig.E2EValidatorFlags,
 		epochsToRun:    5,
 		numBeaconNodes: 2,
 		numValidators:  params.BeaconConfig().MinGenesisActiveValidatorCount,
