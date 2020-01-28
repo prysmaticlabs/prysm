4e5e64e8e316e898bc46bc766bbb8d6e623917a5
Raul Jordan <raul@prysmaticlabs.com>
2020-01-28 11:16:01 -0600

    Update beacon-chain/core/blocks/block_operations.go

--- beacon-chain/core/blocks/block_operations.go
+++ beacon-chain/core/blocks/block_operations.go
@@ -9,7 +9,6 @@ import (
 	"sort"
 
 	"github.com/prysmaticlabs/prysm/shared/attestationutil"
-
 	"github.com/gogo/protobuf/proto"
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
