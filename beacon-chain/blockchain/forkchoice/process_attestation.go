900b541bd0d5411bdf0292603a1e19cf504f6cc7
shayzluf <thezluf@gmail.com>
2020-01-28 23:23:40 +0530

    Update beacon-chain/blockchain/forkchoice/process_attestation.go
    Co-Authored-By: terence tsao <terence@prysmaticlabs.com>

--- beacon-chain/blockchain/forkchoice/process_attestation.go
+++ beacon-chain/blockchain/forkchoice/process_attestation.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/prysmaticlabs/prysm/shared/attestationutil"
-
 	"github.com/gogo/protobuf/proto"
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
