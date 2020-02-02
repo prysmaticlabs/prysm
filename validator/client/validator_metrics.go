69135f41383e5891aa73a9476a42df684165530e
Gary Schulte <gary.schulte@opengov.com>
2020-02-02 12:17:49 -0800

    add prometheus metrics for validator keys

--- validator/client/validator_metrics.go
+++ validator/client/validator_metrics.go
@@ -4,10 +4,26 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/sirupsen/logrus"
+)
+
+var validatorBalancesGaugeVec = promauto.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Namespace: "validator",
+		Name:      "balance",
+		Help:      "current validator balance.",
+	},
+	[]string{
+		// validator pubkey
+		"pkey",
+		// slot for this metric
+		"slot",
+	},
 )
 
 // LogValidatorGainsAndLosses logs important metrics related to this validator client's
@@ -15,6 +31,7 @@ import (
 // and penalties over time, percentage gain/loss, and gives the end user a better idea
 // of how the validator performs with respect to the rest.
 func (v *validator) LogValidatorGainsAndLosses(ctx context.Context, slot uint64) error {
+
 	if slot%params.BeaconConfig().SlotsPerEpoch != 0 || slot < params.BeaconConfig().SlotsPerEpoch {
 		// Do nothing if we are not at the start of a new epoch and before the first epoch.
 		return nil
@@ -47,6 +64,7 @@ func (v *validator) LogValidatorGainsAndLosses(ctx context.Context, slot uint64)
 		log := log.WithField("pubKey", pubKey)
 		if missingValidators[bytesutil.ToBytes48(pkey)] {
 			log.Info("Validator not in beacon chain")
+			validatorBalancesGaugeVec.WithLabelValues(pubKey, slot).Set(0)
 			continue
 		}
 		if slot < params.BeaconConfig().SlotsPerEpoch {
@@ -63,6 +81,8 @@ func (v *validator) LogValidatorGainsAndLosses(ctx context.Context, slot uint64)
 				"newBalance":    newBalance,
 				"percentChange": fmt.Sprintf("%.5f%%", percentNet*100),
 			}).Info("New Balance")
+			validatorBalancesGaugeVec.WithLabelValues(pubKey, slot).Set(newBalance)
+
 		}
 		v.prevBalance[bytesutil.ToBytes48(pkey)] = resp.Balances[i]
 	}
