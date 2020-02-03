6108d51cbd6b12e440bda21c491d6a08dfcb334d
Raul Jordan <raul@prysmaticlabs.com>
2020-02-03 15:13:44 -0600

    passing tests

--- beacon-chain/rpc/beacon/config_test.go
+++ beacon-chain/rpc/beacon/config_test.go
@@ -2,10 +2,13 @@ package beacon
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 	"testing"
 
 	ptypes "github.com/gogo/protobuf/types"
 	dbTest "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
+	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
 func TestServer_GetBeaconConfig(t *testing.T) {
@@ -18,5 +21,17 @@ func TestServer_GetBeaconConfig(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log(res)
+	conf := params.BeaconConfig()
+	numFields := reflect.TypeOf(conf).Elem().NumField()
+
+	// Check if the result has the same number of items as our config struct.
+	if len(res.Config) != numFields {
+		t.Errorf("Expected %d items in config result, got %d", numFields, len(res.Config))
+	}
+	want := fmt.Sprintf("%d", conf.Eth1FollowDistance)
+
+	// Check that an element is properly populated from the config.
+	if res.Config["Eth1FollowDistance"] != want {
+		t.Errorf("Wanted %s for eth1 follow distance, received %s", want, res.Config["Eth1FollowDistance"])
+	}
 }
