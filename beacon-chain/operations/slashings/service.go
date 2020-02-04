5afc455e68a1cd4b3a59f5b0b3c9b91e03268718
shayzluf <thezluf@gmail.com>
2020-02-04 07:12:49 +0530

    fix lint

--- beacon-chain/operations/slashings/service.go
+++ beacon-chain/operations/slashings/service.go
@@ -53,8 +53,8 @@ func (p *Pool) PendingAttesterSlashings() []*ethpb.AttesterSlashing {
 	return pending
 }
 
-// PendingAttesterSlashings returns exits that are ready for inclusion at the given slot. This method will not
-// return more than the block enforced MaxProposerSlashings.
+// PendingProposerSlashings returns proposer slashings that are ready for inclusion at the given slot.
+// This method will not return more than the block enforced MaxProposerSlashings.
 func (p *Pool) PendingProposerSlashings() []*ethpb.ProposerSlashing {
 	p.lock.RLock()
 	defer p.lock.RUnlock()
@@ -119,8 +119,8 @@ func (p *Pool) InsertAttesterSlashing(ctx context.Context, state *beaconstate.Be
 	}
 }
 
-// InsertAttesterSlashing into the pool. This method is a no-op if the pending exit already exists,
-// has been included recently, or the validator is already exited.
+// InsertProposerSlashing into the pool. This method is a no-op if the pending slashing already exists,
+// has been included recently, the validator is already exited, or the validator was already slashed.
 func (p *Pool) InsertProposerSlashing(ctx context.Context, state *beaconstate.BeaconState, slashing *ethpb.ProposerSlashing) {
 	p.lock.Lock()
 	defer p.lock.Unlock()
@@ -173,7 +173,7 @@ func (p *Pool) MarkIncludedProposerSlashing(ps *ethpb.ProposerSlashing) {
 	p.included[ps.ProposerIndex] = true
 }
 
-// MarkIncludedProposerSlashing is used when an proposer slashing has been included in a beacon block.
+// MarkIncludedAttesterSlashing is used when an attester slashing has been included in a beacon block.
 // Every block seen by this node that contains proposer slashings should call this method to include
 // the proposer slashings.
 func (p *Pool) MarkIncludedAttesterSlashing(as *ethpb.AttesterSlashing) {
