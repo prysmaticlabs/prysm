16a44e6084744b84d53cce630ce1b1f33af42a9c
TerenceTsao <terence@prysmaticlabs.com>
2020-02-05 15:08:39 -0800

    Starting testing

--- beacon-chain/blockchain/info.go
+++ beacon-chain/blockchain/info.go
@@ -80,7 +80,12 @@ func (s *Service) TreeHandler(w http.ResponseWriter, _ *http.Request) {
 
 	dotNodes := make([]*dot.Node, len(nodes))
 	headSlot := s.headSlot
-	avgBalance := uint64(averageBalance(s.headState.Balances()))
+	headParentIndex := uint64(0)
+	avgBalance := uint64(1)
+	if s.headState != nil {
+		avgBalance = uint64(averageBalance(s.headState.Balances()))
+
+	}
 
 	for i := len(nodes) - 1; i >= 0; i-- {
 		// Construct label for each node.
@@ -97,6 +102,10 @@ func (s *Service) TreeHandler(w http.ResponseWriter, _ *http.Request) {
 
 		if nodes[i].Slot == headSlot {
 			dotN = dotN.Attr("color", "green")
+		}
+		if uint64(i) == headParentIndex {
+			dotN = dotN.Attr("color", "green")
+			headParentIndex = nodes[i].Parent
 		}
 
 		dotNodes[i] = &dotN
