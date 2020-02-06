d4fd051628621d695b443537c302b1c42fa60810
TerenceTsao <terence@prysmaticlabs.com>
2020-02-06 09:23:10 -0800

    Init fork choice store for tests

--- beacon-chain/blockchain/receive_attestation_test.go
+++ beacon-chain/blockchain/receive_attestation_test.go
@@ -5,48 +5,8 @@ import (
 	"time"
 
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	"github.com/prysmaticlabs/go-ssz"
-	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	testDB "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
-	beaconstate "github.com/prysmaticlabs/prysm/beacon-chain/state"
-	"github.com/prysmaticlabs/prysm/shared/params"
-	"github.com/prysmaticlabs/prysm/shared/testutil"
-	logTest "github.com/sirupsen/logrus/hooks/test"
-	"golang.org/x/net/context"
 )
-
-func TestReceiveAttestationNoPubsub_ProcessCorrectly(t *testing.T) {
-	hook := logTest.NewGlobal()
-	db := testDB.SetupDB(t)
-	defer testDB.TeardownDB(t, db)
-	ctx := context.Background()
-
-	chainService := setupBeaconChain(t, db)
-
-	b := &ethpb.SignedBeaconBlock{Block: &ethpb.BeaconBlock{}}
-	if err := chainService.beaconDB.SaveBlock(ctx, b); err != nil {
-		t.Fatal(err)
-	}
-	root, err := ssz.HashTreeRoot(b.Block)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := chainService.beaconDB.SaveState(ctx, &beaconstate.BeaconState{}, root); err != nil {
-		t.Fatal(err)
-	}
-
-	slot := uint64(time.Now().Unix()) / params.BeaconConfig().SlotsPerEpoch
-	epoch := helpers.SlotToEpoch(slot)
-	a := &ethpb.Attestation{Data: &ethpb.AttestationData{
-		Slot: slot, Target: &ethpb.Checkpoint{Root: root[:], Epoch: epoch},
-	}}
-	if err := chainService.ReceiveAttestationNoPubsub(ctx, a); err != nil {
-		t.Fatal(err)
-	}
-
-	testutil.AssertLogsContain(t, hook, "Saved new head info")
-	testutil.AssertLogsDoNotContain(t, hook, "Broadcasting attestation")
-}
 
 func TestVerifyCheckpointEpoch_Ok(t *testing.T) {
 	db := testDB.SetupDB(t)
