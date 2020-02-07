f16a71f1787b36fdaca73b5a8dd21afedf0ab723
Nishant Das <nishdas93@gmail.com>
2020-02-08 00:58:36 +0800

    Revert "Update Slices More Efficiently" (#4790)
    * Revert "Update Slices More Efficiently (#4789)"
    
    This reverts commit 669e1ea787cf0aee7c60b389e8228c194a2e65ff.

--- beacon-chain/state/setters.go
+++ beacon-chain/state/setters.go
@@ -106,9 +106,7 @@ func (b *BeaconState) UpdateBlockRootAtIndex(idx uint64, blockRoot [32]byte) err
 	r := b.state.BlockRoots
 	if ref := b.sharedFieldReferences[blockRoots]; ref.refs > 1 {
 		// Copy on write since this is a shared array.
-		newRoots := make([][]byte, len(r))
-		copy(newRoots, r)
-		r = newRoots
+		r = b.BlockRoots()
 
 		ref.refs--
 		b.sharedFieldReferences[blockRoots] = &reference{refs: 1}
@@ -152,9 +150,7 @@ func (b *BeaconState) UpdateStateRootAtIndex(idx uint64, stateRoot [32]byte) err
 	r := b.state.StateRoots
 	if ref := b.sharedFieldReferences[stateRoots]; ref.refs > 1 {
 		// Perform a copy since this is a shared reference and we don't want to mutate others.
-		newRoots := make([][]byte, len(r))
-		copy(newRoots, r)
-		r = newRoots
+		r = b.StateRoots()
 
 		ref.refs--
 		b.sharedFieldReferences[stateRoots] = &reference{refs: 1}
@@ -293,9 +289,8 @@ func (b *BeaconState) UpdateValidatorAtIndex(idx uint64, val *ethpb.Validator) e
 	v := b.state.Validators
 	if ref := b.sharedFieldReferences[validators]; ref.refs > 1 {
 		// Perform a copy since this is a shared reference and we don't want to mutate others.
-		newVals := make([]*ethpb.Validator, len(v))
-		copy(newVals, v)
-		v = newVals
+		v = b.Validators()
+
 		ref.refs--
 		b.sharedFieldReferences[validators] = &reference{refs: 1}
 	}
@@ -386,9 +381,7 @@ func (b *BeaconState) UpdateRandaoMixesAtIndex(val []byte, idx uint64) error {
 	b.lock.RLock()
 	mixes := b.state.RandaoMixes
 	if refs := b.sharedFieldReferences[randaoMixes].refs; refs > 1 {
-		newSlice := make([][]byte, len(mixes))
-		copy(newSlice, mixes)
-		mixes = newSlice
+		mixes = b.RandaoMixes()
 		b.sharedFieldReferences[randaoMixes].refs--
 		b.sharedFieldReferences[randaoMixes] = &reference{refs: 1}
 	}
