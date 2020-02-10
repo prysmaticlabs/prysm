cc9cce32f0beb876909b21c229b3d0b11c874bea
shayzluf <thezluf@gmail.com>
2020-02-10 17:49:23 +0530

    add tests

--- beacon-chain/rpc/slasher/client_test.go
+++ beacon-chain/rpc/slasher/client_test.go
@@ -2,13 +2,17 @@ package slasher
 
 import (
 	"context"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
 
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	mock "github.com/prysmaticlabs/prysm/beacon-chain/blockchain/testing"
 	"github.com/prysmaticlabs/prysm/beacon-chain/operations/slashings"
+	"github.com/prysmaticlabs/prysm/beacon-chain/state"
 	beaconstate "github.com/prysmaticlabs/prysm/beacon-chain/state"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
@@ -60,9 +64,34 @@ func TestClient_SlashingPoolFeeder_NoSlasher(t *testing.T) {
 
 func TestClient_UpdatePool_OK(t *testing.T) {
 	ctx := context.Background()
-	s := &beaconstate.BeaconState{}
-
+	// Generate validators and state for the 2 attestations.
+	validatorCount := 10
+	validators := make([]*ethpb.Validator, validatorCount)
+	for i := 0; i < len(validators); i++ {
+		validators[i] = &ethpb.Validator{
+			ExitEpoch: params.BeaconConfig().FarFutureEpoch,
+		}
+	}
+	base := &pb.BeaconState{
+		Validators:  validators,
+		RandaoMixes: make([][]byte, params.BeaconConfig().EpochsPerHistoricalVector),
+	}
+	s, err := state.InitializeFromProto(base)
+	if err != nil {
+		t.Fatal(err)
+	}
 	slasherClient := &fakeSlasher{}
+	wantAtt := make([]*ethpb.AttesterSlashing, 1)
+	at1 := &ethpb.AttesterSlashing{
+		Attestation_1: &ethpb.IndexedAttestation{AttestingIndices: []uint64{1}},
+		Attestation_2: &ethpb.IndexedAttestation{AttestingIndices: []uint64{1}}}
+	wantAtt[0] = at1
+	slasherClient.attesterSlashing = wantAtt
+
+	wantProSlash := make([]*ethpb.ProposerSlashing, 1)
+	ps := &ethpb.ProposerSlashing{ProposerIndex: 1}
+	wantProSlash[0] = ps
+	slasherClient.proposerSlashings = wantProSlash
 
 	client := &Client{
 		HeadFetcher:     &mock.ChainService{State: s},
@@ -74,11 +103,18 @@ func TestClient_UpdatePool_OK(t *testing.T) {
 	if err := client.updatePool(ctx); err != nil {
 		t.Fatal(err)
 	}
-
 	if !slasherClient.ProposerSlashingsCalled {
 		t.Fatal("Expected ProposerSlashings() to be called")
 	}
 	if !slasherClient.AttesterSlashingsCalled {
 		t.Fatal("Expected AttesterSlashings() to be called")
+	}
+	pendAttSlash := client.SlashingPool.PendingAttesterSlashings()
+	if !reflect.DeepEqual(pendAttSlash, wantAtt) {
+		t.Fatalf("expected pool to be filled with: %v got: %v", wantAtt, pendAttSlash)
+	}
+	pendProSlash := client.SlashingPool.PendingProposerSlashings()
+	if !reflect.DeepEqual(pendProSlash, wantProSlash) {
+		t.Fatalf("expected pool to be filled with: %v got: %v", wantAtt, pendAttSlash)
 	}
 }
--- beacon-chain/rpc/slasher/fake_slasher_test.go
+++ beacon-chain/rpc/slasher/fake_slasher_test.go
@@ -17,24 +17,27 @@ type fakeSlasher struct {
 	IsSlashableAttestationCalled bool
 	ProposerSlashingsCalled      bool
 	AttesterSlashingsCalled      bool
+	attesterSlashing             []*ethpb.AttesterSlashing
+	proposerSlashings            []*ethpb.ProposerSlashing
 }
 
 func (fs *fakeSlasher) Done() {
 	fs.DoneCalled = true
 }
+
 func (fs *fakeSlasher) IsSlashableAttestation(ctx context.Context, in *ethpb.IndexedAttestation, arg2 ...grpc.CallOption) (*slashpb.AttesterSlashingResponse, error) {
 	fs.IsSlashableAttestationCalled = true
-	return &slashpb.AttesterSlashingResponse{}, nil
+	return &slashpb.AttesterSlashingResponse{AttesterSlashing: fs.attesterSlashing}, nil
 }
 func (fs *fakeSlasher) IsSlashableBlock(ctx context.Context, in *slashpb.ProposerSlashingRequest, arg2 ...grpc.CallOption) (*slashpb.ProposerSlashingResponse, error) {
 	fs.IsSlashableBlockCalled = true
-	return &slashpb.ProposerSlashingResponse{}, nil
+	return &slashpb.ProposerSlashingResponse{ProposerSlashing: fs.proposerSlashings}, nil
 }
 func (fs *fakeSlasher) ProposerSlashings(ctx context.Context, in *slashpb.SlashingStatusRequest, arg2 ...grpc.CallOption) (*slashpb.ProposerSlashingResponse, error) {
 	fs.ProposerSlashingsCalled = true
-	return &slashpb.ProposerSlashingResponse{}, nil
+	return &slashpb.ProposerSlashingResponse{ProposerSlashing: fs.proposerSlashings}, nil
 }
 func (fs *fakeSlasher) AttesterSlashings(ctx context.Context, in *slashpb.SlashingStatusRequest, arg2 ...grpc.CallOption) (*slashpb.AttesterSlashingResponse, error) {
 	fs.AttesterSlashingsCalled = true
-	return &slashpb.AttesterSlashingResponse{}, nil
+	return &slashpb.AttesterSlashingResponse{AttesterSlashing: fs.attesterSlashing}, nil
 }
