f7806ac0d1ff766789a56b5c467b126ea34ed30f
nisdas <nishdas93@gmail.com>
2020-02-10 18:39:11 +0800

    add test

--- shared/memorypool/memorypool.go
+++ shared/memorypool/memorypool.go
@@ -2,8 +2,12 @@ package memorypool
 
 import "sync"
 
+// DoubleByteSlicePool represents the memory pool
+// for 2d byte slices
 var DoubleByteSlicePool = new(sync.Pool)
 
+// GetDoubleByteSlice retrieves the 2d byte slice of
+// the desired size from the memory pool.
 func GetDoubleByteSlice(size int) [][]byte {
 	rawObj := DoubleByteSlicePool.Get()
 	if rawObj == nil {
@@ -16,6 +20,8 @@ func GetDoubleByteSlice(size int) [][]byte {
 	return append(byteSlice, make([][]byte, size-len(byteSlice))...)
 }
 
+// PutDoubleByteSlice places the provided 2d byte slice
+// in the memory pool
 func PutDoubleByteSlice(data [][]byte) {
 	DoubleByteSlicePool.Put(data)
 }
