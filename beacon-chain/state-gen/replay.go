f4a40088c163ffb2a531d9e3f64b7c740a5b474e
TerenceTsao <terence@prysmaticlabs.com>
2020-02-11 10:16:33 -0800

    Added load blocks

--- beacon-chain/state-gen/replay.go
+++ beacon-chain/state-gen/replay.go
@@ -2,12 +2,51 @@ package state_gen
 
 import (
 	"context"
+	"errors"
 
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/beacon-chain/db/filters"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 )
 
 // This replays the input blocks on the input state until the target slot is reached.
 func replayBlocks(ctx context.Context, state *state.BeaconState, blocks []*ethpb.BeaconBlock, targetSlot uint64) (*state.BeaconState, error) {
 	return nil, nil
+}
+
+// This loads the blocks between start slot and end slot by recursively fetching from end block root.
+// Blocks are returned in slot-descending order.
+func (s *Service)loadBlocks(ctx context.Context, startSlot uint64, endSlot uint64, endBlockRoot [32]byte) ([]*ethpb.SignedBeaconBlock, error) {
+	filter := filters.NewFilter().SetStartSlot(startSlot).SetEndSlot(endSlot)
+	blocks, err := s.beaconDB.Blocks(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	blockRoots, err := s.beaconDB.BlockRoots(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	// The retrieved blocks and block roots have to be in the same length given same filter.
+	if len(blocks) != len(blockRoots) {
+		return nil, errors.New("length of blocks and roots don't match")
+	}
+
+	// The last retrieved block root has to match input end block root.
+	length := len(blocks)
+	if blockRoots[length - 1] != endBlockRoot {
+		return nil, errors.New("end block roots don't match")
+	}
+
+	filteredBlocks :=[]*ethpb.SignedBeaconBlock{blocks[length-1]}
+	// Starting from second to last index because the last block is already in the filtered block list.
+	for i:=length-2; i >= 0; i--  {
+		b := filteredBlocks[len(filteredBlocks) - 1]
+		if bytesutil.ToBytes32(b.Block.ParentRoot) != blockRoots[i] {
+			continue
+		}
+		filteredBlocks = append(filteredBlocks, blocks[i])
+	}
+
+	return filteredBlocks, nil
 }
