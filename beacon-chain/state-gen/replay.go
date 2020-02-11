1956caebeb1d4534f1a8c8dcd5d22ddfb82498d4
TerenceTsao <terence@prysmaticlabs.com>
2020-02-11 15:04:34 -0800

    Added replay blocks

--- beacon-chain/state-gen/replay.go
+++ beacon-chain/state-gen/replay.go
@@ -5,18 +5,43 @@ import (
 	"errors"
 
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	transition "github.com/prysmaticlabs/prysm/beacon-chain/core/state"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db/filters"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 )
 
 // This replays the input blocks on the input state until the target slot is reached.
-func replayBlocks(ctx context.Context, state *state.BeaconState, blocks []*ethpb.BeaconBlock, targetSlot uint64) (*state.BeaconState, error) {
-	return nil, nil
+func (s *Service)replayBlocks(ctx context.Context, state *state.BeaconState, signed []*ethpb.SignedBeaconBlock, targetSlot uint64) (*state.BeaconState, error) {
+	var err error
+	// The input block list is sorted in decreasing slots order.
+	for i:=len(signed) - 1; i >= 0 ; i--  {
+		// If there is skip slot.
+		for state.Slot() < signed[i].Block.Slot {
+			state, err = transition.ProcessSlot(ctx, state)
+			if err != nil {
+				return nil, err
+			}
+		}
+		state, err = transition.ProcessBlock(ctx, state, signed[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// If there is skip slots at the end.
+	for state.Slot() < targetSlot {
+		state, err = transition.ProcessSlot(ctx, state)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return state, nil
 }
 
 // This loads the blocks between start slot and end slot by recursively fetching from end block root.
-// Blocks are returned in slot-descending order.
+// The Blocks are returned in slot-descending order.
 func (s *Service)loadBlocks(ctx context.Context, startSlot uint64, endSlot uint64, endBlockRoot [32]byte) ([]*ethpb.SignedBeaconBlock, error) {
 	filter := filters.NewFilter().SetStartSlot(startSlot).SetEndSlot(endSlot)
 	blocks, err := s.beaconDB.Blocks(ctx, filter)
