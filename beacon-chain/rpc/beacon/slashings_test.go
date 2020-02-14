4fb7b8f524842e83219964d41a32a98d41fdf2cd
Raul Jordan <raul@prysmaticlabs.com>
2020-02-13 19:30:48 -0600

    we check for insertion into the pool based on attester slashings

--- beacon-chain/rpc/beacon/slashings_test.go
+++ beacon-chain/rpc/beacon/slashings_test.go
@@ -90,17 +90,72 @@ func TestServer_SubmitProposerSlashing(t *testing.T) {
 
 func TestServer_SubmitAttesterSlashing(t *testing.T) {
 	ctx := context.Background()
+	vals := make([]*ethpb.Validator, 10)
+	for i := 0; i < len(vals); i++ {
+		key := make([]byte, 48)
+		copy(key, strconv.Itoa(i))
+		vals[i] = &ethpb.Validator{
+			PublicKey:             key[:],
+			WithdrawalCredentials: make([]byte, 32),
+			EffectiveBalance:      params.BeaconConfig().MaxEffectiveBalance,
+			Slashed:               false,
+		}
+	}
 	bs := &Server{
 		SlashingsPool: slashings.NewPool(),
 	}
-	wanted := &ethpb.SubmitSlashingResponse{
-		SlashedIndices: []uint64{0, 1, 2},
+	// We mark the validators at index 5, 6, 7 as already slashed.
+	vals[5].Slashed = true
+	vals[6].Slashed = true
+	vals[7].Slashed = true
+
+	st, err := stateTrie.InitializeFromProto(&pbp2p.BeaconState{
+		Slot:       0,
+		Validators: vals,
+	})
+	if err != nil {
+		t.Fatal(err)
 	}
-	res, err := bs.SubmitAttesterSlashing(ctx, &ethpb.AttesterSlashing{})
+	bs := &Server{
+		HeadFetcher: &mock.ChainService{
+			State: st,
+		},
+		SlashingsPool: slashings.NewPool(),
+	}
+
+	slashing := &ethpb.AttesterSlashing{
+		Attestation_1: &ethpb.IndexedAttestation{
+			AttestingIndices: []uint64{1, 2, 3},
+		},
+		Attestation_2: &ethpb.IndexedAttestation{
+			AttestingIndices: []uint64{2, 3, 4},
+		},
+	}
+	// We want the intersection of the slashing attesting indices
+	// to be slashed, so we expect validators 2 and 3 to be in the response
+	// slashed indices.
+	wanted := &ethpb.SubmitSlashingResponse{
+		SlashedIndices: []uint64{2, 3},
+	}
+	res, err := bs.SubmitAttesterSlashing(ctx, slashing)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if !proto.Equal(wanted, res) {
 		t.Errorf("Wanted %v, received %v", wanted, res)
+	}
+
+	// If any of the attesting indices in the slashing object have already
+	// been slashed, we should fail to insert properly into the attester slashing pool.
+	slashing = &ethpb.AttesterSlashing{
+		Attestation_1: &ethpb.IndexedAttestation{
+			AttestingIndices: []uint64{5, 6, 7},
+		},
+		Attestation_2: &ethpb.IndexedAttestation{
+			AttestingIndices: []uint64{6, 7, 8},
+		},
+	}
+	if _, err := bs.SubmitAttesterSlashing(ctx, slashing); err == nil {
+		t.Error("Expected including a attester slashing for an already slashed validator to fail")
 	}
 }
