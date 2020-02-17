5b51607c72da0431cd530c80ce22cdbe3fe40de0
nisdas <nishdas93@gmail.com>
2020-02-17 15:17:05 +0800

    add test

--- beacon-chain/stategen/service.go
+++ beacon-chain/stategen/service.go
@@ -3,6 +3,8 @@ package stategen
 import (
 	"context"
 
+	"github.com/pkg/errors"
+
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-ssz"
 	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
@@ -23,19 +25,20 @@ func New(db db.NoHeadAccessDatabase) *State {
 	}
 }
 
+// GenerateState generates a state from the provided pre-state till the provided block.
 func (s *State) GenerateState(ctx context.Context, preState *stateTrie.BeaconState, endBlock *ethpb.SignedBeaconBlock) (*stateTrie.BeaconState, error) {
 	preState = preState.Copy()
 	root, err := ssz.HashTreeRoot(endBlock.Block)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "could not get the root of the block")
 	}
-	blocks, err := s.LoadBlocks(ctx, preState.Slot()+1, endBlock.Block.Slot, root)
+	blocks, err := s.loadBlocks(ctx, preState.Slot()+1, endBlock.Block.Slot, root)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "could not load the required blocks")
 	}
-	postState, err := s.ReplayBlocks(ctx, preState, blocks, endBlock.Block.Slot)
+	postState, err := s.replayBlocks(ctx, preState, blocks, endBlock.Block.Slot)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "could not replay the blocks to generate the resultant state")
 	}
 	return postState, nil
 }
