1940c9ad2a6c0d9a7992ac6deed3134d1c1c449e
rauljordan <raul@prysmaticlabs.com>
2020-02-18 14:28:52 -0600

    tests pass

--- slasher/detection/listeners_test.go
+++ slasher/detection/listeners_test.go
@@ -2,13 +2,20 @@ package detection
 
 import (
 	"context"
+	"io/ioutil"
 	"testing"
 
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/event"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
+	"github.com/sirupsen/logrus"
 	logTest "github.com/sirupsen/logrus/hooks/test"
 )
+
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetOutput(ioutil.Discard)
+}
 
 type mockNotifier struct{}
 
