330138f0371a123348176bca419599368cadeece
Nishant Das <nishdas93@gmail.com>
2020-02-18 08:04:53 +0800

    Update beacon-chain/stategen/service.go
    Co-Authored-By: Raul Jordan <raul@prysmaticlabs.com>

--- beacon-chain/stategen/service.go
+++ beacon-chain/stategen/service.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/pkg/errors"
-
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-ssz"
 	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
