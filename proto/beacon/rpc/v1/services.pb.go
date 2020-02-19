6b4c9e888cf3571e0e9808a69e4f8ef71eba6f6b
Raul Jordan <raul@prysmaticlabs.com>
2020-02-19 16:40:04 -0600

    add services

--- proto/beacon/rpc/v1/services.pb.go
+++ proto/beacon/rpc/v1/services.pb.go
@@ -144,33 +144,134 @@ func (m *AggregationResponse) GetRoot() []byte {
 	return nil
 }
 
+type ExitedValidatorsRequest struct {
+	PublicKeys           [][]byte `protobuf:"bytes,1,rep,name=public_keys,json=publicKeys,proto3" json:"public_keys,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ExitedValidatorsRequest) Reset()         { *m = ExitedValidatorsRequest{} }
+func (m *ExitedValidatorsRequest) String() string { return proto.CompactTextString(m) }
+func (*ExitedValidatorsRequest) ProtoMessage()    {}
+func (*ExitedValidatorsRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9eb4e94b85965285, []int{2}
+}
+func (m *ExitedValidatorsRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *ExitedValidatorsRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_ExitedValidatorsRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *ExitedValidatorsRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ExitedValidatorsRequest.Merge(m, src)
+}
+func (m *ExitedValidatorsRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *ExitedValidatorsRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_ExitedValidatorsRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ExitedValidatorsRequest proto.InternalMessageInfo
+
+func (m *ExitedValidatorsRequest) GetPublicKeys() [][]byte {
+	if m != nil {
+		return m.PublicKeys
+	}
+	return nil
+}
+
+type ExitedValidatorsResponse struct {
+	PublicKeys           [][]byte `protobuf:"bytes,1,rep,name=public_keys,json=publicKeys,proto3" json:"public_keys,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ExitedValidatorsResponse) Reset()         { *m = ExitedValidatorsResponse{} }
+func (m *ExitedValidatorsResponse) String() string { return proto.CompactTextString(m) }
+func (*ExitedValidatorsResponse) ProtoMessage()    {}
+func (*ExitedValidatorsResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9eb4e94b85965285, []int{3}
+}
+func (m *ExitedValidatorsResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *ExitedValidatorsResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_ExitedValidatorsResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *ExitedValidatorsResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ExitedValidatorsResponse.Merge(m, src)
+}
+func (m *ExitedValidatorsResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *ExitedValidatorsResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_ExitedValidatorsResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ExitedValidatorsResponse proto.InternalMessageInfo
+
+func (m *ExitedValidatorsResponse) GetPublicKeys() [][]byte {
+	if m != nil {
+		return m.PublicKeys
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*AggregationRequest)(nil), "ethereum.beacon.rpc.v1.AggregationRequest")
 	proto.RegisterType((*AggregationResponse)(nil), "ethereum.beacon.rpc.v1.AggregationResponse")
+	proto.RegisterType((*ExitedValidatorsRequest)(nil), "ethereum.beacon.rpc.v1.ExitedValidatorsRequest")
+	proto.RegisterType((*ExitedValidatorsResponse)(nil), "ethereum.beacon.rpc.v1.ExitedValidatorsResponse")
 }
 
 func init() { proto.RegisterFile("proto/beacon/rpc/v1/services.proto", fileDescriptor_9eb4e94b85965285) }
 
 var fileDescriptor_9eb4e94b85965285 = []byte{
-	// 282 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x90, 0x41, 0x4a, 0xc4, 0x30,
-	0x14, 0x86, 0x89, 0x16, 0xc1, 0x50, 0x47, 0x8c, 0xa0, 0x45, 0xb0, 0x0c, 0x05, 0x71, 0x54, 0x48,
-	0x19, 0x3d, 0xc1, 0xb8, 0x13, 0x37, 0xd2, 0x1e, 0xa0, 0xb4, 0x99, 0x67, 0x0d, 0x4e, 0x93, 0xfa,
-	0x92, 0x16, 0xe7, 0x04, 0x1e, 0xc1, 0x2b, 0xb9, 0xf4, 0x08, 0xd2, 0x93, 0x48, 0x53, 0x2b, 0x88,
-	0x2e, 0xdc, 0x3d, 0xbe, 0xff, 0x27, 0x7c, 0xf9, 0x69, 0x54, 0xa3, 0xb6, 0x3a, 0x2e, 0x20, 0x17,
-	0x5a, 0xc5, 0x58, 0x8b, 0xb8, 0x9d, 0xc7, 0x06, 0xb0, 0x95, 0x02, 0x0c, 0x77, 0x21, 0x3b, 0x00,
-	0xfb, 0x00, 0x08, 0x4d, 0xc5, 0x87, 0x1a, 0xc7, 0x5a, 0xf0, 0x76, 0x1e, 0xbd, 0x12, 0xca, 0x16,
-	0x65, 0x89, 0x50, 0xe6, 0x56, 0x6a, 0x95, 0xc0, 0x53, 0x03, 0xc6, 0x32, 0x46, 0x3d, 0xb3, 0xd2,
-	0x36, 0x20, 0x53, 0x32, 0xf3, 0x12, 0x77, 0xb3, 0x53, 0xba, 0x2b, 0x74, 0x55, 0x49, 0x6b, 0x01,
-	0x32, 0xa9, 0x96, 0xf0, 0x1c, 0x6c, 0xb8, 0x78, 0xf2, 0x8d, 0x6f, 0x7a, 0xca, 0x8e, 0x29, 0xad,
-	0x9b, 0x62, 0x25, 0x45, 0xf6, 0x08, 0xeb, 0x60, 0x73, 0x4a, 0x66, 0x7e, 0xb2, 0x3d, 0x90, 0x5b,
-	0x58, 0xb3, 0x13, 0x3a, 0xe9, 0xdf, 0xcb, 0x8c, 0x2c, 0x55, 0x6e, 0x1b, 0x84, 0xc0, 0x73, 0x95,
-	0x9d, 0x9e, 0xa6, 0x23, 0x8c, 0xce, 0xe8, 0xfe, 0x0f, 0x31, 0x53, 0x6b, 0x65, 0xa0, 0x37, 0x43,
-	0xfd, 0x65, 0xe6, 0x27, 0xee, 0xbe, 0x7c, 0x21, 0x74, 0x6f, 0xec, 0x6a, 0x4c, 0x87, 0x9f, 0x33,
-	0xa4, 0x87, 0x69, 0x53, 0x54, 0xd2, 0x8e, 0x11, 0x2c, 0xd4, 0xf2, 0x0e, 0xb5, 0xbe, 0x67, 0xe7,
-	0xfc, 0xef, 0x39, 0xf8, 0xef, 0x29, 0x8e, 0x2e, 0xfe, 0xd5, 0x1d, 0xec, 0xae, 0xfd, 0xb7, 0x2e,
-	0x24, 0xef, 0x5d, 0x48, 0x3e, 0xba, 0x90, 0x14, 0x5b, 0x6e, 0xfb, 0xab, 0xcf, 0x00, 0x00, 0x00,
-	0xff, 0xff, 0x73, 0x23, 0x52, 0xe6, 0xa1, 0x01, 0x00, 0x00,
+	// 356 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0xcd, 0x4a, 0xc3, 0x40,
+	0x10, 0xc7, 0x59, 0x5b, 0x04, 0xc7, 0x58, 0xeb, 0x0a, 0x36, 0x14, 0xac, 0x25, 0x20, 0x56, 0x85,
+	0xc4, 0xd6, 0x9b, 0x9e, 0x2a, 0x78, 0x10, 0x2f, 0x92, 0x82, 0xd7, 0x92, 0x8f, 0x31, 0x2e, 0x36,
+	0xd9, 0xb8, 0xbb, 0x29, 0xed, 0x13, 0x88, 0x4f, 0xe0, 0x2b, 0x79, 0xf4, 0x11, 0xa4, 0x4f, 0x22,
+	0x49, 0x9a, 0x88, 0xd6, 0x62, 0x6f, 0xc3, 0x7f, 0x3e, 0xf6, 0xf7, 0x9f, 0x59, 0x30, 0x62, 0xc1,
+	0x15, 0xb7, 0x5c, 0x74, 0x3c, 0x1e, 0x59, 0x22, 0xf6, 0xac, 0x71, 0xd7, 0x92, 0x28, 0xc6, 0xcc,
+	0x43, 0x69, 0x66, 0x49, 0xba, 0x87, 0xea, 0x11, 0x05, 0x26, 0xa1, 0x99, 0x97, 0x99, 0x22, 0xf6,
+	0xcc, 0x71, 0xd7, 0x78, 0x23, 0x40, 0xfb, 0x41, 0x20, 0x30, 0x70, 0x14, 0xe3, 0x91, 0x8d, 0xcf,
+	0x09, 0x4a, 0x45, 0x29, 0x54, 0xe5, 0x88, 0x2b, 0x9d, 0xb4, 0x49, 0xa7, 0x6a, 0x67, 0x31, 0x3d,
+	0x82, 0x6d, 0x8f, 0x87, 0x21, 0x53, 0x0a, 0x71, 0xc8, 0x22, 0x1f, 0x27, 0xfa, 0x5a, 0x96, 0xae,
+	0x95, 0xf2, 0x4d, 0xaa, 0xd2, 0x7d, 0x80, 0x38, 0x71, 0x47, 0xcc, 0x1b, 0x3e, 0xe1, 0x54, 0xaf,
+	0xb4, 0x49, 0x47, 0xb3, 0x37, 0x72, 0xe5, 0x16, 0xa7, 0xf4, 0x10, 0x6a, 0xe9, 0xbc, 0xa1, 0x64,
+	0x41, 0xe4, 0xa8, 0x44, 0xa0, 0x5e, 0xcd, 0x4a, 0xb6, 0x52, 0x75, 0x50, 0x88, 0xc6, 0x31, 0xec,
+	0xfe, 0x00, 0x93, 0x31, 0x8f, 0x24, 0xa6, 0x64, 0x82, 0xcf, 0xc9, 0x34, 0x3b, 0x8b, 0x8d, 0x0b,
+	0x68, 0x5c, 0x4f, 0x98, 0x42, 0xff, 0xde, 0x19, 0x31, 0xdf, 0x51, 0x5c, 0xc8, 0xc2, 0xc8, 0x01,
+	0x6c, 0x7e, 0xb3, 0x48, 0x9d, 0xb4, 0x2b, 0x1d, 0xcd, 0x86, 0x12, 0x46, 0x1a, 0x97, 0xa0, 0x2f,
+	0xf6, 0xce, 0xdf, 0xfa, 0xaf, 0xb9, 0xf7, 0x42, 0x60, 0xa7, 0x80, 0xe4, 0x62, 0x90, 0xaf, 0x9c,
+	0x0a, 0x68, 0x0c, 0x12, 0x37, 0x64, 0xaa, 0x48, 0x61, 0x3f, 0xf2, 0xef, 0x04, 0xe7, 0x0f, 0xf4,
+	0xc4, 0xfc, 0xfb, 0x0e, 0xe6, 0xe2, 0x0d, 0x9a, 0xa7, 0x2b, 0xd5, 0xe6, 0xa8, 0xbd, 0x57, 0x02,
+	0xf5, 0xd2, 0x41, 0x01, 0x92, 0x40, 0xfd, 0xb7, 0x37, 0x6a, 0x2d, 0x9b, 0xba, 0x64, 0x83, 0xcd,
+	0xb3, 0xd5, 0x1b, 0x72, 0x96, 0x2b, 0xed, 0x7d, 0xd6, 0x22, 0x1f, 0xb3, 0x16, 0xf9, 0x9c, 0xb5,
+	0x88, 0xbb, 0x9e, 0x7d, 0xc0, 0xf3, 0xaf, 0x00, 0x00, 0x00, 0xff, 0xff, 0xbc, 0x07, 0xb5, 0x29,
+	0xa6, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -247,6 +348,78 @@ var _AggregatorService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SubmitAggregateAndProof",
 			Handler:    _AggregatorService_SubmitAggregateAndProof_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "proto/beacon/rpc/v1/services.proto",
+}
+
+// ValidatorServiceClient is the client API for ValidatorService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type ValidatorServiceClient interface {
+	ExitedValidators(ctx context.Context, in *ExitedValidatorsRequest, opts ...grpc.CallOption) (*ExitedValidatorsResponse, error)
+}
+
+type validatorServiceClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewValidatorServiceClient(cc *grpc.ClientConn) ValidatorServiceClient {
+	return &validatorServiceClient{cc}
+}
+
+func (c *validatorServiceClient) ExitedValidators(ctx context.Context, in *ExitedValidatorsRequest, opts ...grpc.CallOption) (*ExitedValidatorsResponse, error) {
+	out := new(ExitedValidatorsResponse)
+	err := c.cc.Invoke(ctx, "/ethereum.beacon.rpc.v1.ValidatorService/ExitedValidators", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// ValidatorServiceServer is the server API for ValidatorService service.
+type ValidatorServiceServer interface {
+	ExitedValidators(context.Context, *ExitedValidatorsRequest) (*ExitedValidatorsResponse, error)
+}
+
+// UnimplementedValidatorServiceServer can be embedded to have forward compatible implementations.
+type UnimplementedValidatorServiceServer struct {
+}
+
+func (*UnimplementedValidatorServiceServer) ExitedValidators(ctx context.Context, req *ExitedValidatorsRequest) (*ExitedValidatorsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ExitedValidators not implemented")
+}
+
+func RegisterValidatorServiceServer(s *grpc.Server, srv ValidatorServiceServer) {
+	s.RegisterService(&_ValidatorService_serviceDesc, srv)
+}
+
+func _ValidatorService_ExitedValidators_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ExitedValidatorsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ValidatorServiceServer).ExitedValidators(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/ethereum.beacon.rpc.v1.ValidatorService/ExitedValidators",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ValidatorServiceServer).ExitedValidators(ctx, req.(*ExitedValidatorsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _ValidatorService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "ethereum.beacon.rpc.v1.ValidatorService",
+	HandlerType: (*ValidatorServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "ExitedValidators",
+			Handler:    _ValidatorService_ExitedValidators_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -338,6 +511,78 @@ func (m *AggregationResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *ExitedValidatorsRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ExitedValidatorsRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *ExitedValidatorsRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	if len(m.PublicKeys) > 0 {
+		for iNdEx := len(m.PublicKeys) - 1; iNdEx >= 0; iNdEx-- {
+			i -= len(m.PublicKeys[iNdEx])
+			copy(dAtA[i:], m.PublicKeys[iNdEx])
+			i = encodeVarintServices(dAtA, i, uint64(len(m.PublicKeys[iNdEx])))
+			i--
+			dAtA[i] = 0xa
+		}
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *ExitedValidatorsResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ExitedValidatorsResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *ExitedValidatorsResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	if len(m.PublicKeys) > 0 {
+		for iNdEx := len(m.PublicKeys) - 1; iNdEx >= 0; iNdEx-- {
+			i -= len(m.PublicKeys[iNdEx])
+			copy(dAtA[i:], m.PublicKeys[iNdEx])
+			i = encodeVarintServices(dAtA, i, uint64(len(m.PublicKeys[iNdEx])))
+			i--
+			dAtA[i] = 0xa
+		}
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintServices(dAtA []byte, offset int, v uint64) int {
 	offset -= sovServices(v)
 	base := offset
@@ -384,6 +629,42 @@ func (m *AggregationResponse) Size() (n int) {
 	l = len(m.Root)
 	if l > 0 {
 		n += 1 + l + sovServices(uint64(l))
+	}
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *ExitedValidatorsRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if len(m.PublicKeys) > 0 {
+		for _, b := range m.PublicKeys {
+			l = len(b)
+			n += 1 + l + sovServices(uint64(l))
+		}
+	}
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *ExitedValidatorsResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if len(m.PublicKeys) > 0 {
+		for _, b := range m.PublicKeys {
+			l = len(b)
+			n += 1 + l + sovServices(uint64(l))
+		}
 	}
 	if m.XXX_unrecognized != nil {
 		n += len(m.XXX_unrecognized)
@@ -619,6 +900,178 @@ func (m *AggregationResponse) Unmarshal(dAtA []byte) error {
 			if m.Root == nil {
 				m.Root = []byte{}
 			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipServices(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthServices
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthServices
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *ExitedValidatorsRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowServices
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ExitedValidatorsRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ExitedValidatorsRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PublicKeys", wireType)
+			}
+			var byteLen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowServices
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				byteLen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if byteLen < 0 {
+				return ErrInvalidLengthServices
+			}
+			postIndex := iNdEx + byteLen
+			if postIndex < 0 {
+				return ErrInvalidLengthServices
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PublicKeys = append(m.PublicKeys, make([]byte, postIndex-iNdEx))
+			copy(m.PublicKeys[len(m.PublicKeys)-1], dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipServices(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthServices
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthServices
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *ExitedValidatorsResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowServices
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ExitedValidatorsResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ExitedValidatorsResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PublicKeys", wireType)
+			}
+			var byteLen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowServices
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				byteLen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if byteLen < 0 {
+				return ErrInvalidLengthServices
+			}
+			postIndex := iNdEx + byteLen
+			if postIndex < 0 {
+				return ErrInvalidLengthServices
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PublicKeys = append(m.PublicKeys, make([]byte, postIndex-iNdEx))
+			copy(m.PublicKeys[len(m.PublicKeys)-1], dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
