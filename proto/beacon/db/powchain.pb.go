921a44d9fdc84d68c7d44347de782a30ac542703
Preston Van Loon <preston@prysmaticlabs.com>
2020-02-26 13:15:36 -0800

    Clean up unused / deprecated protobuf definitions (#4949)
    * Remove unused services, mark everything as deprecated, regen pb.go
    * remove some code from cluster pk manager, gazelle
    * goimports
    * remove mocks
    * Merge branch 'master' into remove-deprecated-proto-defs

--- proto/beacon/db/powchain.pb.go
+++ proto/beacon/db/powchain.pb.go
@@ -1977,6 +1977,7 @@ func (m *DepositContainer) Unmarshal(dAtA []byte) error {
 func skipPowchain(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
+	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -2008,10 +2009,8 @@ func skipPowchain(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
-			return iNdEx, nil
 		case 1:
 			iNdEx += 8
-			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -2032,55 +2031,30 @@ func skipPowchain(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthPowchain
 			}
 			iNdEx += length
-			if iNdEx < 0 {
-				return 0, ErrInvalidLengthPowchain
-			}
-			return iNdEx, nil
 		case 3:
-			for {
-				var innerWire uint64
-				var start int = iNdEx
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return 0, ErrIntOverflowPowchain
-					}
-					if iNdEx >= l {
-						return 0, io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					innerWire |= (uint64(b) & 0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				innerWireType := int(innerWire & 0x7)
-				if innerWireType == 4 {
-					break
-				}
-				next, err := skipPowchain(dAtA[start:])
-				if err != nil {
-					return 0, err
-				}
-				iNdEx = start + next
-				if iNdEx < 0 {
-					return 0, ErrInvalidLengthPowchain
-				}
-			}
-			return iNdEx, nil
+			depth++
 		case 4:
-			return iNdEx, nil
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupPowchain
+			}
+			depth--
 		case 5:
 			iNdEx += 4
-			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthPowchain
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
 	}
-	panic("unreachable")
+	return 0, io.ErrUnexpectedEOF
 }
 
 var (
-	ErrInvalidLengthPowchain = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowPowchain   = fmt.Errorf("proto: integer overflow")
+	ErrInvalidLengthPowchain        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowPowchain          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupPowchain = fmt.Errorf("proto: unexpected end of group")
 )
