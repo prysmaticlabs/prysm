21a49419a53816b55d69dfdadc412283a3067665
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-02-26 19:27:45 -0500

    Fix comments and organize test

--- slasher/detection/filter/filter.go
+++ slasher/detection/filter/filter.go
@@ -8,25 +8,30 @@ import (
 	"github.com/spaolacci/murmur3"
 )
 
-// Filter is an encoded set of a []byte key.
-type Filter []byte
+// BloomFilter is an encoded set of a []byte key.
+type BloomFilter []byte
 
-// NewFilter returns a new Bloom filter that encodes the given key with 16 bits allotted for it.
-func NewFilter(key []byte) (Filter, error) {
+// NewBloomFilter returns a new bloom filter that encodes the given key with 16 bits allotted for it.
+// This bloom filter has a set length of 16 bits, and uses 5 hash functions in order to provide less than
+// 0.1% chance of false positives.
+func NewBloomFilter(key []byte) (BloomFilter, error) {
 	nBits := 16
 	filter := make([]byte, 2)
 	for i := 0; i < len(filter); i++ {
 		filter[i] = 0
 	}
 
+	// Getting the highway hash and setting its bit in the filter.
 	hash1 := hashutil.FastSum64(key)
 	bitPos := hash1 % uint64(nBits)
 	filter[bitPos/8] |= 1 << (bitPos % 8)
 
+	// Getting the murmur3 hash and setting its bit in the filter.
 	hash2 := murmur3.Sum64(key)
 	bitPos = hash2 % uint64(nBits)
 	filter[bitPos/8] |= 1 << (bitPos % 8)
 
+	// Getting the fnv hash and setting its bit in the filter.
 	hash3 := fnv.New64()
 	if _, err := hash3.Write(key); err != nil {
 		return nil, err
@@ -34,11 +39,13 @@ func NewFilter(key []byte) (Filter, error) {
 	bitPos = hash3.Sum64() % uint64(nBits)
 	filter[bitPos/8] |= 1 << (bitPos % 8)
 
+	// Getting the sha256 hash and setting its bit in the filter.
 	hash4 := hashutil.Hash(key)
 	hash64 := binary.LittleEndian.Uint64(hash4[:])
 	bitPos = hash64 % uint64(nBits)
 	filter[bitPos/8] |= 1 << (bitPos % 8)
 
+	// Getting the keccak256 hash and setting its bit in the filter.
 	hash5 := hashutil.HashKeccak256(key)
 	hash64 = binary.LittleEndian.Uint64(hash5[:])
 	bitPos = hash64 % uint64(nBits)
@@ -47,9 +54,11 @@ func NewFilter(key []byte) (Filter, error) {
 	return filter, nil
 }
 
-// Contains returns whether the filter contains given key. False positives
+// Contains returns whether the bloom filter contains given key. False positives
 // are possible, where it could return true for a key not in the original set.
-func (f Filter) Contains(key []byte) (bool, error) {
+// This function generates the proper hashes for the key and checks if the
+// corresponding bit is marked in the bloom filter.
+func (f BloomFilter) Contains(key []byte) (bool, error) {
 	if len(f) < 2 {
 		return false, nil
 	}
@@ -88,7 +97,7 @@ func (f Filter) Contains(key []byte) (bool, error) {
 	return true, nil
 }
 
-func (f Filter) bitAt(bitPos uint64) bool {
+func (f BloomFilter) bitAt(bitPos uint64) bool {
 	i := uint8(1 << (bitPos % 8))
 	return f[bitPos/8]&i == i
 }
--- slasher/detection/filter/filter_test.go
+++ slasher/detection/filter/filter_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/prysmaticlabs/go-ssz"
 )
 
-func (f Filter) String() string {
+func (f BloomFilter) String() string {
 	s := make([]byte, 8*len(f))
 	for i, x := range f {
 		for j := 0; j < 8; j++ {
@@ -39,7 +39,7 @@ func TestFilter_OK(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	f, err := NewFilter(dataRoot[:])
+	f, err := NewBloomFilter(dataRoot[:])
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -126,7 +126,7 @@ func TestFilter_NoCollisions(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	f, err := NewFilter(dataRoot[:])
+	f, err := NewBloomFilter(dataRoot[:])
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -138,45 +138,60 @@ func TestFilter_NoCollisions(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		filter, err := NewFilter(dataRoot[:])
+		filter, err := NewBloomFilter(dataRoot[:])
 		if err != nil {
 			t.Fatal(err)
 		}
 		if filter.String() == got {
-			t.Fatalf("coliision at %d", i)
+			t.Fatalf("Unexpected coliision at %d", i)
 		}
 	}
 }
 
 func TestFilter_Output(t *testing.T) {
 	testCases := []struct {
+		name string
 		key  string
 		want string
 	}{
-		{key: "eth2.0", want: "..111....11....."},
-		{key: "is very", want: "...1..11........"},
-		{key: "awesome", want: "111......1......"},
-		{key: "and you", want: ".1..1...1.....11"},
-		{key: "should join", want: "11.........11..."},
-		{key: "and be part", want: "1....1....1...1."},
-		{key: "of the vision", want: ".1.......11.1..1"},
-		{key: ":woke:", want: "...1..11.11....."},
+		{
+			name: "large key",
+			key:  "A very very large key, almost too large. This key is wayyyy too large.",
+			want: "....11....11.1..",
+		},
+		{
+			name: "small key",
+			key:  "A small key",
+			want: "..........1..111",
+		},
+		{
+			name: "tiny key",
+			key:  "Tiny",
+			want: ".......1...111..",
+		},
+		{
+			name: "empty key",
+			key:  "",
+			want: "11.1.1..........",
+		},
 	}
 	for _, tc := range testCases {
-		filter, err := NewFilter([]byte(tc.key))
-		if err != nil {
-			t.Fatal(err)
-		}
-		if filter.String() != tc.want {
-			t.Errorf("Unexpected filter result, received %s, expected %s", filter.String(), tc.want)
-		}
-		found, err := filter.Contains([]byte(tc.key))
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !found {
-			t.Fatal("Unexpected failure of contain")
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			filter, err := NewBloomFilter([]byte(tc.key))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if filter.String() != tc.want {
+				t.Errorf("Unexpected filter result, received %s, expected %s", filter.String(), tc.want)
+			}
+			found, err := filter.Contains([]byte(tc.key))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !found {
+				t.Fatal("Unexpected failure of contain")
+			}
+		})
 	}
 }
 
@@ -200,7 +215,7 @@ func BenchmarkNewFilter(b *testing.B) {
 	}
 
 	for i := 0; i < b.N; i++ {
-		_, err := NewFilter(dataRoot[:])
+		_, err := NewBloomFilter(dataRoot[:])
 		if err != nil {
 			b.Fatal(err)
 		}
