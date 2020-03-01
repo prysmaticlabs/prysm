006da5783a9fa238e2706ee5a0e88f44fef336eb
Terence Tsao <terence@prysmaticlabs.com>
2020-03-01 07:31:07 +0000

    Add state_summary.go

---
+++ beacon-chain/db/kv/state_summary.go
@@ -0,0 +1,58 @@
+package kv
+
+import (
+	"context"
+
+	"github.com/boltdb/bolt"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"go.opencensus.io/trace"
+)
+
+// SaveStateSummary saves a state summary object to the DB.
+func (k *Store) SaveStateSummary(ctx context.Context, summary *pb.StateSummary) error {
+	ctx, span := trace.StartSpan(ctx, "BeaconDB.SaveStateSummary")
+	defer span.End()
+
+	enc, err := encode(summary)
+	if err != nil {
+		return err
+	}
+	return k.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(stateSummaryBucket)
+		return bucket.Put(summary.Root, enc)
+	})
+}
+
+// StateSummary returns the state summary object from the db using input block root.
+func (k *Store) StateSummary(ctx context.Context, blockRoot [32]byte) (*pb.StateSummary, error) {
+	ctx, span := trace.StartSpan(ctx, "BeaconDB.StateSummary")
+	defer span.End()
+
+	var summary *pb.StateSummary
+	err := k.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(stateSummaryBucket)
+		enc := bucket.Get(blockRoot[:])
+		if enc == nil {
+			return nil
+		}
+		summary = &pb.StateSummary{}
+		return decode(enc, summary)
+	})
+
+	return summary, err
+}
+
+// HasStateSummary returns true if a state summary exists in DB.
+func (k *Store) HasStateSummary(ctx context.Context, blockRoot [32]byte) bool {
+	ctx, span := trace.StartSpan(ctx, "BeaconDB.HasStateSummary")
+	defer span.End()
+	var exists bool
+	// #nosec G104. Always returns nil.
+	k.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(stateSummaryBucket)
+		exists = bucket.Get(blockRoot[:]) != nil
+		return nil
+	})
+	return exists
+}
+
