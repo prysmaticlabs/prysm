a6a1ffbc16afec1b6064fe69538a8af6bd734967
nisdas <nishdas93@gmail.com>
2020-03-02 15:16:26 +0800

    make it the same

--- beacon-chain/core/helpers/slot_epoch.go
+++ beacon-chain/core/helpers/slot_epoch.go
@@ -90,7 +90,7 @@ func SlotsSinceEpochStarts(slot uint64) uint64 {
 }
 
 // Allow for slots "from the future" within a certain tolerance.
-const TimeShiftTolerance = 10 * time.Millisecond // ms
+const TimeShiftTolerance = 500 * time.Millisecond // ms
 
 // VerifySlotTime validates the input slot is not from the future.
 func VerifySlotTime(genesisTime uint64, slot uint64, timeTolerance time.Duration) error {
