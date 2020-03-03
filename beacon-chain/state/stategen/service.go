699e7efc61e0435621e3f4c85bddabe5e7711cf0
terence tsao <terence@prysmaticlabs.com>
2020-03-03 20:07:34 +0100

    Add epoch boundary root map (#4993)
    * Add to struct
    
    * Add implementations
    
    * Tests

--- beacon-chain/state/stategen/service.go
+++ beacon-chain/state/stategen/service.go
@@ -1,18 +1,23 @@
 package stategen
 
 import (
+	"sync"
+
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 )
 
 // State represents a management object that handles the internal
 // logic of maintaining both hot and cold states in DB.
 type State struct {
-	beaconDB db.NoHeadAccessDatabase
+	beaconDB                db.NoHeadAccessDatabase
+	epochBoundarySlotToRoot map[uint64][32]byte
+	epochBoundaryLock       sync.RWMutex
 }
 
 // New returns a new state management object.
 func New(db db.NoHeadAccessDatabase) *State {
 	return &State{
-		beaconDB: db,
+		beaconDB:                db,
+		epochBoundarySlotToRoot: make(map[uint64][32]byte),
 	}
 }
