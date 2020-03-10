78da3c2c701202ab6287b446b293435cf8ed97e6
Raul Jordan <raul@prysmaticlabs.com>
2020-03-10 14:14:18 -0500

    include all metrics

---
+++ beacon-chain/operations/slashings/metrics.go
@@ -0,0 +1,45 @@
+package slashings
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	numPendingAttesterSlashings = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "num_pending_attester_slashings",
+			Help: "Number of pending attester slashings in the pool",
+		},
+	)
+	numAttesterSlashingsIncluded = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "num_attester_slashings_included",
+			Help: "Number of attester slashings included in blocks",
+		},
+	)
+	attesterSlashingReattempts = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "attester_slashing_reattempts",
+			Help: "Times an attester slashing for an already slashed validator is received",
+		},
+	)
+	numPendingProposerSlashings = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "num_pending_proposer_slashings",
+			Help: "Number of pending proposer slashings in the pool",
+		},
+	)
+	numProposerSlashingsIncluded = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "num_proposer_slashings_included",
+			Help: "Number of proposer slashings included in blocks",
+		},
+	)
+	proposerSlashingReattempts = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "proposer_slashing_reattempts",
+			Help: "Times a proposer slashing for an already slashed validator is received",
+		},
+	)
+)
