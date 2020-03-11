8d36bef714dae88fe2ad3852e9e915afec8548aa
Terence Tsao <terence@prysmaticlabs.com>
2020-03-11 07:08:57 +0100

    minor refactors

--- beacon-chain/state/stategen/cold.go
+++ beacon-chain/state/stategen/cold.go
@@ -8,64 +8,47 @@ import (
 	"go.opencensus.io/trace"
 )
 
-// This loads the cold state by block root, it chooses whether to load from archive point (faster) or
-// somewhere between archive points (slower) since it requires replaying blocks. This is more efficient
-// than load cold state by slot.
-func (s *State) loadColdStateByRoot(ctx context.Context, blockRoot [32]byte) (*state.BeaconState, error) {
-	ctx, span := trace.StartSpan(ctx, "stateGen.loadColdStateByRoot")
+// Given the archive index, this returns the archived cold state in the DB.
+// If the archived state does not exist in the state, it'll compute it and save it.
+func (s *State) archivedPointByIndex(ctx context.Context, archiveIndex uint64) (*state.BeaconState, error) {
+	ctx, span := trace.StartSpan(ctx, "stateGen.loadArchivedPointByIndex")
 	defer span.End()
-
-	summary, err := s.beaconDB.StateSummary(ctx, blockRoot)
-	if err != nil {
-		return nil, err
-	}
-	if summary == nil {
-		return nil, errUnknownStateSummary
+	if s.beaconDB.HasArchivedPoint(ctx, archiveIndex) {
+		return s.beaconDB.ArchivedPointState(ctx, archiveIndex)
 	}
 
-	// Use the archived point state if the slot lies on top of the archived point.
-	if summary.Slot%s.slotsPerArchivePoint == 0 {
-		archivedPoint := summary.Slot / s.slotsPerArchivePoint
-		s, err := s.loadColdStateByArchivedPoint(ctx, archivedPoint)
-		if err != nil {
-			return nil, errors.Wrap(err, "could not get cold state using archived index")
-		}
-		if s == nil {
-			return nil, errUnknownArchivedState
-		}
-		return s, nil
-	}
-
-	return s.loadColdIntermediateStateWithRoot(ctx, summary.Slot, blockRoot)
+	// If for certain reasons, archived point does not exist in DB,
+	// a node should regenerate it and save it.
+	return s.recoverArchivedPointByIndex(ctx, archiveIndex)
 }
 
-// This loads the cold state for the input archived point.
-func (s *State) loadColdStateByArchivedPoint(ctx context.Context, archivedPoint uint64) (*state.BeaconState, error) {
-	return s.beaconDB.ArchivedPointState(ctx, archivedPoint)
-}
-
-// This loads a cold state by slot and block root which lies between the archive point.
-// This is a faster implementation given the block root is provided.
-func (s *State) loadColdIntermediateStateWithRoot(ctx context.Context, slot uint64, blockRoot [32]byte) (*state.BeaconState, error) {
-	ctx, span := trace.StartSpan(ctx, "stateGen.loadColdIntermediateStateWithRoot")
+// This recovers an archived point by index. For certain reasons (ex. user toggles feature flag),
+// an archived point may not be present in the DB. This regenerates the archived point state via
+// playback and saves the archived root/state to the DB.
+func (s *State) recoverArchivedPointByIndex(ctx context.Context, archiveIndex uint64) (*state.BeaconState, error) {
+	ctx, span := trace.StartSpan(ctx, "stateGen.recoverArchivedPointByIndex")
 	defer span.End()
 
-	// Load the archive point for lower side of the intermediate state.
-	lowArchivePointIdx := slot / s.slotsPerArchivePoint
-
-	// Acquire the read lock so the split can't change while this is happening.
-	lowArchivePointState, err := s.loadArchivedPointByIndex(ctx, lowArchivePointIdx)
+	archivedSlot := archiveIndex * s.slotsPerArchivedPoint
+	archivedState, err := s.ComputeStateUpToSlot(ctx, archivedSlot)
 	if err != nil {
-		return nil, errors.Wrap(err, "could not get lower bound archived state using index")
+		return nil, errors.Wrap(err, "could not compute state up to archived index slot")
 	}
-	if lowArchivePointState == nil {
+	if archivedState == nil {
 		return nil, errUnknownArchivedState
 	}
-
-	replayBlks, err := s.LoadBlocks(ctx, lowArchivePointState.Slot()+1, slot, blockRoot)
+	lastRoot, _, err := s.lastSavedBlock(ctx, archivedSlot)
 	if err != nil {
-		return nil, errors.Wrap(err, "could not get load blocks for cold state using slot")
+		return nil, errors.Wrap(err, "could not get last valid block up to archived index slot")
 	}
 
-	return s.ReplayBlocks(ctx, lowArchivePointState, replayBlks, slot)
+	if err := s.beaconDB.SaveArchivedPointRoot(ctx, lastRoot, archiveIndex); err != nil {
+		return nil, err
+	}
+	if err := s.beaconDB.SaveArchivedPointState(ctx, archivedState, archiveIndex); err != nil {
+		return nil, err
+	}
+
+	return archivedState, nil
 }
+
