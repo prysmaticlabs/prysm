78c35028387bf728f11dddd07e0bf265bdf200f4
rauljordan <raul@prysmaticlabs.com>
2020-03-11 17:12:24 -0500

    imports

--- beacon-chain/operations/slashings/service_proposer_test.go
+++ beacon-chain/operations/slashings/service_proposer_test.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
