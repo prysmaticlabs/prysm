f62415757b5d0e6fe8206bb36e8a7ee2f8cae6b8
Terence Tsao <terence@prysmaticlabs.com>
2020-03-12 10:21:51 -0700

    Starting test

--- beacon-chain/state/stategen/hot_test.go
+++ beacon-chain/state/stategen/hot_test.go
@@ -11,7 +11,29 @@ import (
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
+	logTest "github.com/sirupsen/logrus/hooks/test"
 )
+
+func TestSaveHotState_AlreadyHas(t *testing.T) {
+	hook := logTest.NewGlobal()
+	ctx := context.Background()
+	db := testDB.SetupDB(t)
+	defer testDB.TeardownDB(t, db)
+	service := New(db)
+
+	beaconState, _ := testutil.DeterministicGenesisState(t, 32)
+	beaconState.SetSlot(params.BeaconConfig().SlotsPerEpoch)
+	r := [32]byte{'A'}
+
+	// Pre cache the hot state.
+	service.hotStateCache.Put(r, beaconState)
+	if err := service.saveHotState(ctx, r, beaconState); err != nil {
+		t.Fatal(err)
+	}
+
+	// Should not save the state.
+	testutil.AssertLogsDoNotContain(t, hook, "Saved full state on epoch boundary")
+}
 
 func TestLoadHoteStateByRoot_Cached(t *testing.T) {
 	ctx := context.Background()
