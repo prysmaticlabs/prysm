bd056829a1e07d953b559ec2176fe77b29e74925
Terence Tsao <terence@prysmaticlabs.com>
2020-03-12 13:56:34 -0700

    loadColdStateByRoot

--- beacon-chain/state/stategen/cold.go
+++ beacon-chain/state/stategen/cold.go
@@ -36,6 +36,66 @@ func (s *State) saveColdState(ctx context.Context, blockRoot [32]byte, state *st
 	return nil
 }
 
+// This loads the cold state by block root, it decides whether to load from archived point (faster) or
+// somewhere between archived points (slower) because it requires replaying blocks.
+// This method is more efficient than load cold state by slot.
+func (s *State) loadColdStateByRoot(ctx context.Context, blockRoot [32]byte) (*state.BeaconState, error) {
+	ctx, span := trace.StartSpan(ctx, "stateGen.loadColdStateByRoot")
+	defer span.End()
+
+	summary, err := s.beaconDB.StateSummary(ctx, blockRoot)
+	if err != nil {
+		return nil, err
+	}
+	if summary == nil {
+		return nil, errUnknownStateSummary
+	}
+
+	// Use the archived point state if the summary slot lies on top of the archived point.
+	if summary.Slot%s.slotsPerArchivedPoint == 0 {
+		archivedPoint := summary.Slot / s.slotsPerArchivedPoint
+		s, err := s.loadColdStateByArchivedPoint(ctx, archivedPoint)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not get cold state using archived index")
+		}
+		if s == nil {
+			return nil, errUnknownArchivedState
+		}
+		return s, nil
+	}
+
+	return s.loadColdIntermediateStateByRoot(ctx, summary.Slot, blockRoot)
+}
+
+// This loads the cold state for the input archived point.
+func (s *State) loadColdStateByArchivedPoint(ctx context.Context, archivedPoint uint64) (*state.BeaconState, error) {
+	return s.beaconDB.ArchivedPointState(ctx, archivedPoint)
+}
+
+// This loads a cold state by slot and block root which lies between the archive point.
+// This is a faster implementation given the block root is provided.
+func (s *State) loadColdIntermediateStateByRoot(ctx context.Context, slot uint64, blockRoot [32]byte) (*state.BeaconState, error) {
+	ctx, span := trace.StartSpan(ctx, "stateGen.loadColdIntermediateStateByRoot")
+	defer span.End()
+
+	// Load the archive point for lower side of the intermediate state.
+	lowArchivedPointIdx := slot / s.slotsPerArchivedPoint
+	lowArchivedPointState, err := s.archivedPointByIndex(ctx, lowArchivedPointIdx)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get lower archived state using index")
+	}
+	if lowArchivedPointState == nil {
+		return nil, errUnknownArchivedState
+	}
+
+	replayBlks, err := s.LoadBlocks(ctx, lowArchivedPointState.Slot()+1, slot, blockRoot)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get load blocks for cold state using slot")
+	}
+
+	return s.ReplayBlocks(ctx, lowArchivedPointState, replayBlks, slot)
+}
+
 // Given the archive index, this returns the archived cold state in the DB.
 // If the archived state does not exist in the state, it'll compute it and save it.
 func (s *State) archivedPointByIndex(ctx context.Context, archiveIndex uint64) (*state.BeaconState, error) {
