c20d9ccbb346ca618cee376a4458c2ed1a4a76b0
terence tsao <terence@prysmaticlabs.com>
2020-03-13 10:35:28 -0700

    Better attestation pool with map instead of expiration cache (#5087)
    * update aggregated
    
    * update block att
    
    * update forkchoice att
    
    * update unaggregated att
    
    * gazelle
    
    * Use copy
    
    * Locks
    
    * Genesis time
    
    * Fixed tests
    
    * Comments
    
    * Fixed tests

--- beacon-chain/operations/attestations/kv/block_test.go
+++ beacon-chain/operations/attestations/kv/block_test.go
@@ -1,17 +1,12 @@
 package kv
 
 import (
-	"math"
 	"reflect"
 	"sort"
 	"testing"
-	"time"
 
-	"github.com/gogo/protobuf/proto"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-bitfield"
-	"github.com/prysmaticlabs/go-ssz"
-	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
 func TestKV_BlockAttestation_CanSaveRetrieve(t *testing.T) {
@@ -65,32 +60,5 @@ func TestKV_BlockAttestation_CanDelete(t *testing.T) {
 
 	if !reflect.DeepEqual(wanted, returned) {
 		t.Error("Did not receive correct aggregated atts")
-	}
-}
-
-func TestKV_BlockAttestation_CheckExpTime(t *testing.T) {
-	cache := NewAttCaches()
-
-	att := &ethpb.Attestation{AggregationBits: bitfield.Bitlist{0b111}}
-	r, _ := ssz.HashTreeRoot(att.Data)
-
-	if err := cache.SaveBlockAttestation(att); err != nil {
-		t.Fatal(err)
-	}
-
-	item, exp, exists := cache.blockAtt.GetWithExpiration(string(r[:]))
-	if !exists {
-		t.Error("Saved att does not exist")
-	}
-
-	receivedAtt := item.([]*ethpb.Attestation)[0]
-	if !proto.Equal(att, receivedAtt) {
-		t.Error("Did not receive correct aggregated att")
-	}
-
-	wanted := float64(params.BeaconConfig().SlotsPerEpoch * params.BeaconConfig().SecondsPerSlot)
-	if math.RoundToEven(exp.Sub(time.Now()).Seconds()) != wanted {
-		t.Errorf("Did not receive correct exp time. Wanted: %f, got: %f", wanted,
-			math.RoundToEven(exp.Sub(time.Now()).Seconds()))
 	}
 }
--- beacon-chain/operations/attestations/kv/forkchoice.go
+++ beacon-chain/operations/attestations/kv/forkchoice.go
@@ -1,10 +1,10 @@
 package kv
 
 import (
-	"github.com/patrickmn/go-cache"
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-ssz"
+	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
 )
 
 // SaveForkchoiceAttestation saves an forkchoice attestation in cache.
@@ -14,9 +14,9 @@ func (p *AttCaches) SaveForkchoiceAttestation(att *ethpb.Attestation) error {
 		return errors.Wrap(err, "could not tree hash attestation")
 	}
 
-	// DefaultExpiration is set to what was given to New(). In this case
-	// it's one epoch.
-	p.forkchoiceAtt.Set(string(r[:]), att, cache.DefaultExpiration)
+	p.forkchoiceAttLock.Lock()
+	defer p.forkchoiceAttLock.Unlock()
+	p.forkchoiceAtt[r] = stateTrie.CopyAttestation(att) // Copied.
 
 	return nil
 }
@@ -34,15 +34,12 @@ func (p *AttCaches) SaveForkchoiceAttestations(atts []*ethpb.Attestation) error 
 
 // ForkchoiceAttestations returns the forkchoice attestations in cache.
 func (p *AttCaches) ForkchoiceAttestations() []*ethpb.Attestation {
-	atts := make([]*ethpb.Attestation, 0, p.forkchoiceAtt.ItemCount())
-	for s, i := range p.forkchoiceAtt.Items() {
-		// Type assertion for the worst case. This shouldn't happen.
-		att, ok := i.Object.(*ethpb.Attestation)
-		if !ok {
-			p.forkchoiceAtt.Delete(s)
-			continue
-		}
-		atts = append(atts, att)
+	atts := make([]*ethpb.Attestation, 0)
+
+	p.forkchoiceAttLock.RLock()
+	defer p.forkchoiceAttLock.RUnlock()
+	for _, att := range p.forkchoiceAtt {
+		atts = append(atts, stateTrie.CopyAttestation(att) /* Copied */)
 	}
 
 	return atts
@@ -55,7 +52,9 @@ func (p *AttCaches) DeleteForkchoiceAttestation(att *ethpb.Attestation) error {
 		return errors.Wrap(err, "could not tree hash attestation")
 	}
 
-	p.forkchoiceAtt.Delete(string(r[:]))
+	p.forkchoiceAttLock.Lock()
+	defer p.forkchoiceAttLock.Unlock()
+	delete(p.forkchoiceAtt, r)
 
 	return nil
 }
