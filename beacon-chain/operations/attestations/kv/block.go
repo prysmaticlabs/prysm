08bd8d3cce09064ba9e6889919d5f44af9d911b4
Terence Tsao <terence@prysmaticlabs.com>
2020-03-12 17:46:43 -0700

    update block att

--- beacon-chain/operations/attestations/kv/block.go
+++ beacon-chain/operations/attestations/kv/block.go
@@ -1,7 +1,6 @@
 package kv
 
 import (
-	"github.com/patrickmn/go-cache"
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-ssz"
@@ -14,15 +13,9 @@ func (p *AttCaches) SaveBlockAttestation(att *ethpb.Attestation) error {
 		return errors.Wrap(err, "could not tree hash attestation")
 	}
 
-	var atts []*ethpb.Attestation
-	d, ok := p.blockAtt.Get(string(r[:]))
+	atts, ok := p.blockAtt[r]
 	if !ok {
 		atts = make([]*ethpb.Attestation, 0)
-	} else {
-		atts, ok = d.([]*ethpb.Attestation)
-		if !ok {
-			return errors.New("cached value is not of type []*ethpb.Attestation")
-		}
 	}
 
 	// Ensure that this attestation is not already fully contained in an existing attestation.
@@ -33,9 +26,7 @@ func (p *AttCaches) SaveBlockAttestation(att *ethpb.Attestation) error {
 	}
 	atts = append(atts, att)
 
-	// DefaultExpiration is set to what was given to New(). In this case
-	// it's one epoch.
-	p.blockAtt.Set(string(r[:]), atts, cache.DefaultExpiration)
+	p.blockAtt[r] = atts
 
 	return nil
 }
@@ -53,14 +44,8 @@ func (p *AttCaches) SaveBlockAttestations(atts []*ethpb.Attestation) error {
 
 // BlockAttestations returns the block attestations in cache.
 func (p *AttCaches) BlockAttestations() []*ethpb.Attestation {
-	atts := make([]*ethpb.Attestation, 0, p.blockAtt.ItemCount())
-	for s, i := range p.blockAtt.Items() {
-		// Type assertion for the worst case. This shouldn't happen.
-		att, ok := i.Object.([]*ethpb.Attestation)
-		if !ok {
-			p.blockAtt.Delete(s)
-			continue
-		}
+	atts := make([]*ethpb.Attestation, 0)
+	for _, att := range p.blockAtt {
 		atts = append(atts, att...)
 	}
 
@@ -74,7 +59,7 @@ func (p *AttCaches) DeleteBlockAttestation(att *ethpb.Attestation) error {
 		return errors.Wrap(err, "could not tree hash attestation")
 	}
 
-	p.blockAtt.Delete(string(r[:]))
+	delete(p.blockAtt, r)
 
 	return nil
 }
