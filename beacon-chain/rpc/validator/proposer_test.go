ac27882e325c75041d9769a282939945d3fc991f
Terence Tsao <terence@prysmaticlabs.com>
2020-03-17 16:06:58 -0700

    updated rpc tests

--- beacon-chain/rpc/validator/proposer_test.go
+++ beacon-chain/rpc/validator/proposer_test.go
@@ -349,6 +349,7 @@ func TestComputeStateRoot_OK(t *testing.T) {
 
 	req := &ethpb.SignedBeaconBlock{
 		Block: &ethpb.BeaconBlock{
+			ProposerIndex: 51,
 			ParentRoot: parentRoot[:],
 			Slot:       1,
 			Body: &ethpb.BeaconBlockBody{
