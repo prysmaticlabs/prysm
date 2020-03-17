fbd9a0e35350c5f1a70e4bb3c3e8d8913cee9191
rauljordan <raul@prysmaticlabs.com>
2020-03-17 15:58:46 -0500

    proper comments

--- beacon-chain/cache/tracked_committee_indices.go
+++ beacon-chain/cache/tracked_committee_indices.go
@@ -9,19 +9,21 @@ import (
 
 type trackedCommitteeIndices struct {
 	cache *lru.Cache
-	lock sync.RWMutex
+	lock  sync.RWMutex
 }
 
-var TrackedCommitteeIndices = newTrackedCommitteeIndicies()
+// TrackedCommitteeIndices for attestations.
+var TrackedCommitteeIndices = newTrackedCommitteeIndices()
 
-func newTrackedCommitteeIndicies() *trackedCommitteeIndices {
+func newTrackedCommitteeIndices() *trackedCommitteeIndices {
 	cache, err := lru.New(8)
 	if err != nil {
 		panic(err)
 	}
-	return &trackedCommitteeIndices{cache:cache}
+	return &trackedCommitteeIndices{cache: cache}
 }
 
+// AddIndices to the cache for attestation committees by epoch.
 func (t *trackedCommitteeIndices) AddIndices(indices []uint64, epoch uint64) {
 	t.lock.Lock()
 	defer t.lock.Unlock()
@@ -32,6 +34,7 @@ func (t *trackedCommitteeIndices) AddIndices(indices []uint64, epoch uint64) {
 	t.cache.Add(epoch, indices)
 }
 
+// GetIndices from the cache for attestation committees by epoch.
 func (t *trackedCommitteeIndices) GetIndices(epoch uint64) []uint64 {
 	val, exists := t.cache.Get(epoch)
 	if !exists {
