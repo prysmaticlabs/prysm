cc19d2a8d9330a5b787dc7dba1ac842b407df113
Terence Tsao <terence@prysmaticlabs.com>
2020-03-22 13:05:30 -0700

    Update test

--- beacon-chain/sync/validate_beacon_blocks_test.go
+++ beacon-chain/sync/validate_beacon_blocks_test.go
@@ -3,6 +3,7 @@ package sync
 import (
 	"bytes"
 	"context"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"reflect"
 	"testing"
 	"time"
@@ -123,6 +124,7 @@ func TestValidateBeaconBlockPubSub_ValidSignature(t *testing.T) {
 	}
 	msg := &ethpb.SignedBeaconBlock{
 		Block: &ethpb.BeaconBlock{
+			Slot: 1,
 			ParentRoot: testutil.Random32Bytes(t),
 		},
 		Signature: sk.Sign([]byte("data")).Marshal(),
@@ -132,7 +134,7 @@ func TestValidateBeaconBlockPubSub_ValidSignature(t *testing.T) {
 		db:          db,
 		p2p:         p,
 		initialSync: &mockSync.Sync{IsSyncing: false},
-		chain: &mock.ChainService{Genesis: time.Now(),
+		chain: &mock.ChainService{Genesis: time.Unix(time.Now().Unix() - int64(params.BeaconConfig().SecondsPerSlot),0),
 			FinalizedCheckPoint: &ethpb.Checkpoint{
 				Epoch: 0,
 			}},
