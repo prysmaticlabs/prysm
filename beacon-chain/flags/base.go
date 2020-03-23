d59b609c56b858bffe1277d0fcc4905c514d71a1
Preston Van Loon <preston@prysmaticlabs.com>
2020-03-23 11:03:18 -0700

    clarify description

--- beacon-chain/flags/base.go
+++ beacon-chain/flags/base.go
@@ -61,7 +61,7 @@ var (
 	GPRCGatewayCorsDomain = &cli.StringFlag{
 		Name: "grpc-gateway-corsdomain",
 		Usage: "Comma separated list of domains from which to accept cross origin requests " +
-			"(browser enforced). This flag must be used with --grpc-gateway-port.",
+			"(browser enforced). This flag has no effect if not used with --grpc-gateway-port.",
 	}
 	// MinSyncPeers specifies the required number of successful peer handshakes in order
 	// to start syncing with external peers.
