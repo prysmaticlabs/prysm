55c42b59cf8e5cf992aabaa692ef17f441a307a7
Terence Tsao <terence@prysmaticlabs.com>
2020-03-23 09:45:55 -0700

    Fixed TestValidateBeaconBlockPubSub_SeenProposerSlot

--- beacon-chain/sync/validate_beacon_blocks_test.go
+++ beacon-chain/sync/validate_beacon_blocks_test.go
@@ -327,6 +327,7 @@ func TestValidateBeaconBlockPubSub_SeenProposerSlot(t *testing.T) {
 	}
 	msg := &ethpb.SignedBeaconBlock{
 		Block: &ethpb.BeaconBlock{
+			Slot: 1,
 			ParentRoot: testutil.Random32Bytes(t),
 		},
 		Signature: sk.Sign([]byte("data")).Marshal(),
@@ -337,7 +338,7 @@ func TestValidateBeaconBlockPubSub_SeenProposerSlot(t *testing.T) {
 		db:          db,
 		p2p:         p,
 		initialSync: &mockSync.Sync{IsSyncing: false},
-		chain: &mock.ChainService{Genesis: time.Now(),
+		chain: &mock.ChainService{Genesis: time.Unix(time.Now().Unix() - int64(params.BeaconConfig().SecondsPerSlot), 0) ,
 			FinalizedCheckPoint: &ethpb.Checkpoint{
 				Epoch: 0,
 			}},
