e1dbea466d4672d5cdc1a6f5e83d538540c59d40
Victor Farazdagi <simple.square@gmail.com>
2020-03-23 09:50:44 +0300

    adds docs to fsm

--- beacon-chain/sync/initial-sync/fsm/fsm.go
+++ beacon-chain/sync/initial-sync/fsm/fsm.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+// StateMachine is a FSM that allows easy state transitions:
+// State(S) x Event(E) -> Actions (A), State(S').
 type StateMachine struct {
 	currentState StateID
 	updateTime   time.Time
@@ -12,27 +14,33 @@ type StateMachine struct {
 	events       map[EventID]*Event
 }
 
+// HandlerFn is an event handler signature.
 type HandlerFn func(*StateMachine, interface{}) (StateID, error)
 
+// StateID is unique handle for a state.
 type StateID string
 
-type State struct {
-	name StateID
-	//handlerFns []HandlerFn
-}
-
+// EventID is unique handle for an event.
 type EventID string
 
+// State is a container for data.
+type State struct {
+	name StateID
+}
+
+// Event is a container for event data.
 type Event struct {
 	name    EventID
 	actions []*Action
 }
 
+// Action represents event actions that can be attached to an event.
 type Action struct {
 	state     StateID
 	handlerFn HandlerFn
 }
 
+// NewStateMachine returns fully initialized state machine.
 func NewStateMachine() *StateMachine {
 	return &StateMachine{
 		states: map[StateID]*State{},
@@ -40,33 +48,31 @@ func NewStateMachine() *StateMachine {
 	}
 }
 
+// String returns human readable representation of a state.
 func (sm *StateMachine) String() string {
 	return fmt.Sprintf("%v", sm.currentState)
 }
 
+// StateAge returns time since the current state has been updated.
 func (sm *StateMachine) StateAge() time.Duration {
 	return time.Since(sm.updateTime)
 }
 
+// SetCurrentState updates the current state.
 func (sm *StateMachine) SetCurrentState(name StateID) {
 	if sm.currentState == name {
 		return
 	}
 	sm.updateTime = time.Now()
 	sm.currentState = name
-	//if state, ok := sm.states[name]; ok {
-	//	for _, fn := range state.handlerFns {
-	//		if err := fn(sm); err != nil {
-	//			return err
-	//		}
-	//	}
-	//}
 }
 
+// CurrentState returns the current state.
 func (sm *StateMachine) CurrentState() StateID {
 	return sm.currentState
 }
 
+// Trigger is used to call events on a state machine.
 func (sm *StateMachine) Trigger(name EventID, data interface{}) error {
 	event, ok := sm.events[name]
 	if !ok {
@@ -91,6 +97,7 @@ func (sm *StateMachine) Trigger(name EventID, data interface{}) error {
 	return nil
 }
 
+// OnEvent is a helper method to attach event to the state machine.
 func (sm *StateMachine) OnEvent(name EventID) *Event {
 	event, ok := sm.events[name]
 	if !ok {
@@ -102,18 +109,7 @@ func (sm *StateMachine) OnEvent(name EventID) *Event {
 	return event
 }
 
-//func (sm *StateMachine) AddHandler(name StateID, fn func(interface{}) error) *StateMachine {
-//	state, ok := sm.states[name]
-//	if !ok {
-//		state = &State{
-//			name: name,
-//		}
-//		sm.states[name] = state
-//	}
-//	sm.states[name].handlerFns = append(sm.states[name].handlerFns, fn)
-//	return sm
-//}
-
+// AddHandler is a helper method to attach event handlers to events.
 func (e *Event) AddHandler(state StateID, fn HandlerFn) *Event {
 	action := &Action{
 		state:     state,
