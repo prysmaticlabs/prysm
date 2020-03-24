486cc650d35ff5a6df18d27ad08f0076751fe17f
shayzluf <thezluf@gmail.com>
2020-03-24 15:20:10 +0530

    remove server implementation from pr

--- slasher/rpc/server.go
+++ slasher/rpc/server.go
@@ -7,9 +7,6 @@ import (
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	slashpb "github.com/prysmaticlabs/prysm/proto/slashing"
 	"github.com/prysmaticlabs/prysm/slasher/detection"
-	"go.opencensus.io/trace"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 // Server defines a server implementation of the gRPC Slasher service,
@@ -22,15 +19,7 @@ type Server struct {
 // IsSlashableAttestation returns an attester slashing if the attestation submitted
 // is a slashable vote.
 func (ss *Server) IsSlashableAttestation(ctx context.Context, req *ethpb.IndexedAttestation) (*slashpb.AttesterSlashingResponse, error) {
-	ctx, span := trace.StartSpan(ctx, "detection.detectAttesterSlashings")
-	defer span.End()
-	slashings, err := ss.detector.DetectAttesterSlashings(ctx, req)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Could not detect attester slashings for attestation: %v: %v", req, err)
-	}
-	return &slashpb.AttesterSlashingResponse{
-		AttesterSlashing: slashings,
-	}, nil
+	return nil, errors.New("unimplemented")
 }
 
 // IsSlashableBlock returns an proposer slashing if the block submitted
