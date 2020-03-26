07fac4d0b009181ba473aa585cfbd19950569d55
Raul Jordan <raul@prysmaticlabs.com>
2020-03-26 13:13:59 -0500

    Update slasher/rpc/server.go

--- slasher/rpc/server.go
+++ slasher/rpc/server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/prysmaticlabs/prysm/slasher/db"
-
 	log "github.com/sirupsen/logrus"
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
