09729aaa70045704f2accfe0b15378b4d5ee1892
terence tsao <terence@prysmaticlabs.com>
2020-03-28 09:45:44 -0700

    E2e test

--- shared/featureconfig/flags.go
+++ shared/featureconfig/flags.go
@@ -342,6 +342,7 @@ var E2EBeaconChainFlags = []string{
 	"--check-head-state",
 	"--enable-initial-sync-queue",
 	"--enable-state-field-trie",
+	"--init-sync-batch-save-blocks",
 	// TODO(5123): This flag currently fails E2E. Commenting until it's resolved.
 	//"--enable-dynamic-committee-subnets",
 }
