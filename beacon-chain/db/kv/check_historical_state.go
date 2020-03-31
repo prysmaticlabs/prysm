d991e78872ecab0e326306d2662b9a67cff9d5f6
terence tsao <terence@prysmaticlabs.com>
2020-03-31 14:59:06 -0700

    Hook it up to start routine

--- beacon-chain/db/kv/check_historical_state.go
+++ beacon-chain/db/kv/check_historical_state.go
@@ -1,15 +1,17 @@
 package kv
 
 import (
-	"errors"
+	"context"
 
+	"github.com/pkg/errors"
+	"github.com/prysmaticlabs/prysm/shared/cmd"
 	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 	bolt "go.etcd.io/bbolt"
 )
 
 var historicalStateDeletedKey = []byte("historical-states-deleted")
 
-func (kv *Store) ensureNewStateServiceCompatible() error {
+func (kv *Store) ensureNewStateServiceCompatible(ctx context.Context) error {
 	if !featureconfig.Get().NewStateMgmt {
 		return kv.db.Update(func(tx *bolt.Tx) error {
 			bkt := tx.Bucket(newStateServiceCompatibleBucket)
@@ -25,9 +27,29 @@ func (kv *Store) ensureNewStateServiceCompatible() error {
 		return nil
 	})
 
+	regenHistoricalStatesConfirmed := false
+	var err error
 	if historicalStateDeleted {
-		return errors.New("historical states were pruned in db, do not run with flag --new-state-mgmt")
+		actionText := "Looks like you stopped using --new-state-mgmt. To reuse it, the node will need " +
+			"to generate and save historical states. The process may take a while, - do you want to proceed? (Y/N)"
+		deniedText := "Historical states will not be generated. Please remove usage --new-state-mgmt"
+
+		regenHistoricalStatesConfirmed, err = cmd.ConfirmAction(actionText, deniedText)
+		if err != nil {
+			return err
+		}
+
+		if !regenHistoricalStatesConfirmed {
+			return errors.New("exiting... please do not run with flag --new-state-mgmt")
+		}
+
+		if err := kv.regenHistoricalStates(ctx); err != nil {
+			return errors.Wrap(err, "could not regenerate historical states, please retry")
+		}
 	}
 
-	return nil
+	return kv.db.Update(func(tx *bolt.Tx) error {
+		bkt := tx.Bucket(newStateServiceCompatibleBucket)
+		return bkt.Put(historicalStateDeletedKey, []byte{0x00})
+	})
 }
