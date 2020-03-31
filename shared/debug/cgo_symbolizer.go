53b9ca5768350f10e74c6f87a2a0bfdd31c8cba0
Preston Van Loon <preston@prysmaticlabs.com>
2020-03-30 19:34:03 -0700

    Add comment

--- shared/debug/cgo_symbolizer.go
+++ shared/debug/cgo_symbolizer.go
@@ -1,3 +1,7 @@
 package debug
 
+// Using this file with this imported library configures the process to
+// expose cgo symbols when possible. Use --config=cgo_symbolizer to make use of
+// this feature.
+
 import _ "github.com/ianlancetaylor/cgosymbolizer"
