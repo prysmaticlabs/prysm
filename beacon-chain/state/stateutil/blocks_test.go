7127b0bc2d0c9a6e96c167d77fbdd73f14789232
nisdas <nishdas93@gmail.com>
2020-04-01 15:54:06 +0800

    fix test

--- beacon-chain/state/stateutil/blocks_test.go
+++ beacon-chain/state/stateutil/blocks_test.go
@@ -3,12 +3,17 @@ package stateutil_test
 import (
 	"testing"
 
+	"github.com/prysmaticlabs/prysm/shared/featureconfig"
+
 	"github.com/prysmaticlabs/go-ssz"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stateutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 )
 
 func TestBlockRoot(t *testing.T) {
+	f := featureconfig.Get()
+	f.EnableBlockHTR = true
+	featureconfig.Init(f)
 	genState, keys := testutil.DeterministicGenesisState(t, 100)
 	blk, err := testutil.GenerateFullBlock(genState, keys, testutil.DefaultBlockGenConfig(), 10)
 	if err != nil {
