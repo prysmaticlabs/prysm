602071590e80f9a73f1e60c8335174d312ade179
Raul Jordan <raul@prysmaticlabs.com>
2020-04-02 13:20:25 -0500

    Update slasher/detection/service.go
    Co-Authored-By: Ivan Martinez <ivanthegreatdev@gmail.com>

--- slasher/detection/service.go
+++ slasher/detection/service.go
@@ -171,7 +171,7 @@ func (ds *Service) submitProposerSlashing(ctx context.Context, slash *ethpb.Prop
 			"header2Slot":         slash.Header_2.Header.Slot,
 			"proposerIdxHeader1": slash.Header_1.Header.ProposerIndex,
 			"proposerIdxHeader2": slash.Header_2.Header.ProposerIndex,
-		}).Info("Found an proposer slashing! Submitting to beacon node")
+		}).Info("Found a proposer slashing! Submitting to beacon node")
 		ds.proposerSlashingsFeed.Send(slash)
 	}
 }
