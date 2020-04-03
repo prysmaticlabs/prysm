86524340b22838ad102419460668fa57f5a6e356
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-04-03 13:18:06 -0400

    Fix

--- validator/client/validator_propose_test.go
+++ validator/client/validator_propose_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/golang/mock/gomock"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
