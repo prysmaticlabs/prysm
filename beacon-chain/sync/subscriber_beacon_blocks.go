899175d250fcfc416e274b4e661223813d6118c1
terence tsao <terence@prysmaticlabs.com>
2020-04-06 18:01:28 -0700

    V0.11 run time fixes to use interop config (#5324)
    * Started testing
    * Bunch of fixes
    * use-interop
    * Sync with v0.11
    * Conflict
    * Uncomment wait for activation
    * Move pending block queue from subscriber to validator pipeline
    * Merge branch 'v0.11' into use-interop-config
    * passing tests
    * Merge refs/heads/v0.11 into use-interop-config
    * Merge refs/heads/v0.11 into use-interop-config

--- beacon-chain/sync/subscriber_beacon_blocks.go
+++ beacon-chain/sync/subscriber_beacon_blocks.go
@@ -6,12 +6,10 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	"github.com/prysmaticlabs/go-ssz"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/feed"
 	blockfeed "github.com/prysmaticlabs/prysm/beacon-chain/core/feed/block"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state/interop"
-	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 )
 
 func (r *Service) beaconBlockSubscriber(ctx context.Context, msg proto.Message) error {
@@ -25,21 +23,6 @@ func (r *Service) beaconBlockSubscriber(ctx context.Context, msg proto.Message) 
 
 	block := signed.Block
 
-	blockRoot, err := ssz.HashTreeRoot(block)
-	if err != nil {
-		log.Errorf("Could not sign root block: %v", err)
-		return nil
-	}
-
-	// Handle block when the parent is unknown.
-	if !r.db.HasBlock(ctx, bytesutil.ToBytes32(block.ParentRoot)) {
-		r.pendingQueueLock.Lock()
-		r.slotToPendingBlocks[block.Slot] = signed
-		r.seenPendingBlocks[blockRoot] = true
-		r.pendingQueueLock.Unlock()
-		return nil
-	}
-
 	// Broadcast the block on a feed to notify other services in the beacon node
 	// of a received block (even if it does not process correctly through a state transition).
 	r.blockNotifier.BlockFeed().Send(&feed.Event{
@@ -49,7 +32,7 @@ func (r *Service) beaconBlockSubscriber(ctx context.Context, msg proto.Message) 
 		},
 	})
 
-	err = r.chain.ReceiveBlockNoPubsub(ctx, signed)
+	err := r.chain.ReceiveBlockNoPubsub(ctx, signed)
 	if err != nil {
 		interop.WriteBlockToDisk(signed, true /*failed*/)
 	}
