ffa3d660d0f094004225ce79c9907fedf2cebab0
rauljordan <raul@prysmaticlabs.com>
2020-04-08 09:33:59 -0500

    use marshaler structure for db instead of proto

--- beacon-chain/db/kv/encoding.go
+++ beacon-chain/db/kv/encoding.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"reflect"
 
+	fastssz "github.com/ferranbt/fastssz"
 	"github.com/gogo/protobuf/proto"
 	"github.com/golang/snappy"
 )
@@ -13,20 +14,28 @@ func decode(data []byte, dst proto.Message) error {
 	if err != nil {
 		return err
 	}
-	if err := proto.Unmarshal(data, dst); err != nil {
-		return err
+	if mm, ok := dst.(fastssz.Unmarshaler); ok {
+		return mm.UnmarshalSSZ(data)
 	}
-	return nil
+	return proto.Unmarshal(data, dst)
 }
 
 func encode(msg proto.Message) ([]byte, error) {
 	if msg == nil || reflect.ValueOf(msg).IsNil() {
 		return nil, errors.New("cannot encode nil message")
 	}
-	enc, err := proto.Marshal(msg)
-	if err != nil {
-		return nil, err
+	var enc []byte
+	var err error
+	if mm, ok := msg.(fastssz.Marshaler); ok {
+		enc, err = mm.MarshalSSZ()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		enc, err = proto.Marshal(msg)
+		if err != nil {
+			return nil, err
+		}
 	}
-
 	return snappy.Encode(nil, enc), nil
 }
