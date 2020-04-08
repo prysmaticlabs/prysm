5af65d2d7fdb23f600d7614bc9b6f8365ce01962
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-04-07 22:00:36 -0400

    ADd cache to other function and add comment

--- beacon-chain/powchain/block_reader.go
+++ beacon-chain/powchain/block_reader.go
@@ -61,6 +61,14 @@ func (s *Service) BlockHashByHeight(ctx context.Context, height *big.Int) (commo
 func (s *Service) BlockTimeByHeight(ctx context.Context, height *big.Int) (uint64, error) {
 	ctx, span := trace.StartSpan(ctx, "beacon-chain.web3service.BlockTimeByHeight")
 	defer span.End()
+	if exists, blkInfo, err := s.blockCache.BlockInfoByHeight(height); exists || err != nil {
+		if err != nil {
+			return 0, err
+		}
+		span.AddAttributes(trace.BoolAttribute("blockCacheHit", true))
+		return blkInfo.Number.Uint64(), nil
+	}
+	span.AddAttributes(trace.BoolAttribute("blockCacheHit", false))
 	block, err := s.blockFetcher.BlockByNumber(ctx, height)
 	if err != nil {
 		return 0, errors.Wrap(err, "could not query block with given height")
@@ -72,8 +80,9 @@ func (s *Service) BlockTimeByHeight(ctx context.Context, height *big.Int) (uint6
 // This is a naive implementation that will use O(ETH1_FOLLOW_DISTANCE) calls to cache
 // or ETH1. This is called for multiple times but only changes every
 // SlotsPerEth1VotingPeriod (1024 slots) so the whole method should be cached.
+// How should caching the whole method be done?
 func (s *Service) BlockNumberByTimestamp(ctx context.Context, time uint64) (*big.Int, error) {
-	ctx, span := trace.StartSpan(ctx, "beacon-chain.web3service.BlockByTimestamp")
+	ctx, span := trace.StartSpan(ctx, "beacon-chain.web3service.BlockNumberByTimestamp")
 	defer span.End()
 
 	head, err := s.blockFetcher.BlockByNumber(ctx, nil)
