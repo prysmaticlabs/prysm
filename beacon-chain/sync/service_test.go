d6d6fbab05634069bc1bc0ef74dff8ad9f4c7e0f
nisdas <nishdas93@gmail.com>
2020-04-10 01:43:31 +0800

    fix overflow

---
+++ beacon-chain/sync/service_test.go
@@ -0,0 +1,35 @@
+package sync
+
+import (
+	"testing"
+	"time"
+
+	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+
+	mockChain "github.com/prysmaticlabs/prysm/beacon-chain/blockchain/testing"
+
+	p2ptest "github.com/prysmaticlabs/prysm/beacon-chain/p2p/testing"
+	mockSync "github.com/prysmaticlabs/prysm/beacon-chain/sync/initial-sync/testing"
+)
+
+func TestService_StatusZeroEpoch(t *testing.T) {
+	bState, err := stateTrie.InitializeFromProto(&pb.BeaconState{Slot: 0})
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := &Service{
+		p2p:         p2ptest.NewTestP2P(t),
+		initialSync: new(mockSync.Sync),
+		chain: &mockChain.ChainService{
+			Genesis: time.Now(),
+			State:   bState,
+		},
+	}
+	r.chainStarted = true
+
+	err = r.Status()
+	if err != nil {
+		t.Errorf("Wanted non failing status but got: %v", err)
+	}
+}
