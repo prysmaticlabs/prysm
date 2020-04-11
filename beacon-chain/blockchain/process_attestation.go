b69c76c879c1c1a827ba8b475bb59f4bdefd703e
Preston Van Loon <preston@prysmaticlabs.com>
2020-04-10 17:12:47 -0700

    Remove next slot requirement for processing attestations (#5384)
    * Remove next slot requirement for processing attestations

--- beacon-chain/blockchain/process_attestation.go
+++ beacon-chain/blockchain/process_attestation.go
@@ -108,7 +108,7 @@ func (s *Service) onAttestation(ctx context.Context, a *ethpb.Attestation) ([]ui
 	}
 
 	// Verify attestations can only affect the fork choice of subsequent slots.
-	if err := helpers.VerifySlotTime(genesisTime, a.Data.Slot+1); err != nil {
+	if err := helpers.VerifySlotTime(genesisTime, a.Data.Slot); err != nil {
 		return nil, err
 	}
 
