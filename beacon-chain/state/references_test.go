b28e0e69ffb719d70695000f34f311a93190c053
Victor Farazdagi <simple.square@gmail.com>
2020-04-13 22:46:00 +0300

    avoid unexpected mutation in block roots on refcopy

--- beacon-chain/state/references_test.go
+++ beacon-chain/state/references_test.go
@@ -7,6 +7,7 @@ import (
 
 	eth "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	p2ppb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 )
 
@@ -96,6 +97,9 @@ func TestStateReferenceCopy_NoUnexpectedValidatorMutation(t *testing.T) {
 	err = a.AppendValidator(&eth.Validator{
 		PublicKey: pubKey2[:],
 	})
+	if err != nil {
+		t.Error(err)
+	}
 
 	// Copy on write happened, reference counters are reset.
 	if refsCount := a.sharedFieldReferences[validators].refs; refsCount != 1 {
@@ -159,5 +163,101 @@ func TestStateReferenceCopy_NoUnexpectedValidatorMutation(t *testing.T) {
 		if val.EffectiveBalance == changedBalance {
 			t.Errorf("Unexpected mutation of effective balance, want: %v, got: %v", 0, valsB[i].EffectiveBalance)
 		}
+	}
+}
+
+func TestStateReferenceCopy_NoUnexpectedBlockRootMutation(t *testing.T) {
+	// Assert that feature is enabled.
+	if cfg := featureconfig.Get(); !cfg.EnableStateRefCopy {
+		cfg.EnableStateRefCopy = true
+		featureconfig.Init(cfg)
+		defer func() {
+			cfg := featureconfig.Get()
+			cfg.EnableStateRefCopy = false
+			featureconfig.Init(cfg)
+		}()
+	}
+
+	root1, root2 := bytesutil.ToBytes32([]byte("foo")), bytesutil.ToBytes32([]byte("bar"))
+	a, err := InitializeFromProtoUnsafe(&p2ppb.BeaconState{
+		BlockRoots: [][]byte{
+			root1[:],
+		},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if refsCount := a.sharedFieldReferences[blockRoots].refs; refsCount != 1 {
+		t.Errorf("Unexpected count of references for block roots field, want: %v, got: %v", 1, refsCount)
+	}
+
+	// Copy, increases reference count.
+	b := a.Copy()
+	if refsCount := a.sharedFieldReferences[blockRoots].refs; refsCount != 2 {
+		t.Errorf("Unexpected count of references for block roots field, want: %v, got: %v", 2, refsCount)
+	}
+	if refsCount := b.sharedFieldReferences[blockRoots].refs; refsCount != 2 {
+		t.Errorf("Unexpected count of references for validators field, want: %v, got: %v", 2, refsCount)
+	}
+	if len(b.state.GetBlockRoots()) != 1 {
+		t.Error("No block roots found")
+	}
+
+	hasBlockRootWithKey := func(state *p2ppb.BeaconState, key [32]byte) bool {
+		for _, root := range state.GetBlockRoots() {
+			if reflect.DeepEqual(root, key[:]) {
+				return true
+			}
+		}
+		return false
+	}
+	// Assert shared state.
+	rootsA := a.state.GetBlockRoots()
+	rootsB := b.state.GetBlockRoots()
+	if len(rootsA) != len(rootsB) || len(rootsA) < 0 {
+		t.Errorf("Unexpected number of roots, want: %v", 1)
+	}
+	if !hasBlockRootWithKey(a.state, root1) {
+		t.Errorf("Expected block root not found, want: %v", root1)
+	}
+	if !hasBlockRootWithKey(b.state, root1) {
+		t.Errorf("Expected block root not found, want: %v", root1)
+	}
+
+	// Mutator method, should only affect called state a.
+	err = a.UpdateBlockRootAtIndex(0, root2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Assert no shared state mutation occurred only on state a (copy on write).
+	if hasBlockRootWithKey(a.state, root1) {
+		t.Errorf("Unexpected block root found, want: %v", root1)
+	}
+	if !hasBlockRootWithKey(a.state, root2) {
+		t.Errorf("Expected block root not found, want: %v", root2)
+	}
+	if !hasBlockRootWithKey(b.state, root1) {
+		t.Errorf("Expected block root not found, want: %v", root1)
+	}
+	// Get updated pointers to data.
+	rootsA = a.state.GetBlockRoots()
+	rootsB = b.state.GetBlockRoots()
+	if len(rootsA) != len(rootsB) || len(rootsA) < 1 {
+		t.Errorf("Unexpected number of roots, want: %v", 1)
+	}
+	if !reflect.DeepEqual(rootsA[0], root2[:]) {
+		t.Errorf("Expected mutation not found")
+	}
+	if !reflect.DeepEqual(rootsB[0], root1[:]) {
+		t.Errorf("Unexpected mutation found")
+	}
+
+	// Copy on write happened, reference counters are reset.
+	if refsCount := a.sharedFieldReferences[blockRoots].refs; refsCount != 1 {
+		t.Errorf("Unexpected count of references for block roots field, want: %v, got: %v", 1, refsCount)
+	}
+	if refsCount := b.sharedFieldReferences[blockRoots].refs; refsCount != 1 {
+		t.Errorf("Unexpected count of references for block roots field, want: %v, got: %v", 1, refsCount)
 	}
 }
