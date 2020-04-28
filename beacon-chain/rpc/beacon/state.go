39cbb9b1f215d4b564182044417a2236c561bb34
rauljordan <raul@prysmaticlabs.com>
2020-04-28 17:11:43 -0500

    passing tests

--- beacon-chain/rpc/beacon/state.go
+++ beacon-chain/rpc/beacon/state.go
@@ -5,25 +5,31 @@ import (
 
 	pbp2p "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	pbrpc "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
-	//"google.golang.org/grpc/codes"
-	//"google.golang.org/grpc/status"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-// GetBeaconState --
+// GetBeaconState retrieves a beacon state
+// from the beacon node by either a slot or block root.
 func (bs *Server) GetBeaconState(
 	ctx context.Context,
 	req *pbrpc.BeaconStateRequest,
 ) (*pbp2p.BeaconState, error) {
-
-	//currentSlot := bs.GenesisTimeFetcher.CurrentSlot()
-	//var requestedSlot uint64
-	//switch q := req.QueryFilter.(type) {
-	//case *ethpb.ListCommitteesRequest_Epoch:
-	//	requestedSlot = helpers.StartSlot(q.Epoch)
-	//case *ethpb.ListCommitteesRequest_Genesis:
-	//	requestedSlot = 0
-	//default:
-	//	requestedSlot = currentSlot
-	//}
-	return nil, nil
+	switch q := req.QueryFilter.(type) {
+	case *pbrpc.BeaconStateRequest_Slot:
+		st, err := bs.StateGen.StateBySlot(ctx, q.Slot)
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "could not compute state by slot: %v", err)
+		}
+		return st.CloneInnerState(), nil
+	case *pbrpc.BeaconStateRequest_BlockRoot:
+		st, err := bs.StateGen.StateByRoot(ctx, bytesutil.ToBytes32(q.BlockRoot))
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "could not compute state by block root: %v", err)
+		}
+		return st.CloneInnerState(), nil
+	default:
+		return nil, status.Error(codes.InvalidArgument, "need to specify either a block root or slot to request state")
+	}
 }
---
+++ beacon-chain/rpc/beacon/state_test.go
@@ -0,0 +1,80 @@
+package beacon
+
+import (
+	"context"
+	"testing"
+
+	"github.com/gogo/protobuf/proto"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/go-ssz"
+	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
+	dbTest "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
+	"github.com/prysmaticlabs/prysm/beacon-chain/state/stategen"
+	pbrpc "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
+	"github.com/prysmaticlabs/prysm/shared/featureconfig"
+	"github.com/prysmaticlabs/prysm/shared/testutil"
+)
+
+func TestServer_GetBeaconState(t *testing.T) {
+	resetCfg := featureconfig.InitWithReset(&featureconfig.Flags{NewStateMgmt: true})
+	defer resetCfg()
+
+	db := dbTest.SetupDB(t)
+	defer dbTest.TeardownDB(t, db)
+
+	ctx := context.Background()
+	st := testutil.NewBeaconState()
+	slot := uint64(100)
+	if err := st.SetSlot(slot); err != nil {
+		t.Fatal(err)
+	}
+	b := &ethpb.SignedBeaconBlock{Block: &ethpb.BeaconBlock{
+		Slot: slot,
+	}}
+	if err := db.SaveBlock(ctx, b); err != nil {
+		t.Fatal(err)
+	}
+	gRoot, err := ssz.HashTreeRoot(b.Block)
+	if err != nil {
+		t.Fatal(err)
+	}
+	gen := stategen.New(db, cache.NewStateSummaryCache())
+	if err := gen.SaveState(ctx, gRoot, st); err != nil {
+		t.Fatal(err)
+	}
+	if err := db.SaveState(ctx, st, gRoot); err != nil {
+		t.Fatal(err)
+	}
+	bs := &Server{
+		BeaconDB: db,
+		StateGen: gen,
+	}
+	if _, err := bs.GetBeaconState(ctx, &pbrpc.BeaconStateRequest{}); err == nil {
+		t.Errorf("Expected error without a query filter, received nil")
+	}
+	req := &pbrpc.BeaconStateRequest{
+		QueryFilter: &pbrpc.BeaconStateRequest_BlockRoot{
+			BlockRoot: gRoot[:],
+		},
+	}
+	res, err := bs.GetBeaconState(ctx, req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	wanted := st.CloneInnerState()
+	if !proto.Equal(wanted, res) {
+		t.Errorf("Wanted %v, received %v", wanted, res)
+	}
+	req = &pbrpc.BeaconStateRequest{
+		QueryFilter: &pbrpc.BeaconStateRequest_Slot{
+			Slot: slot,
+		},
+	}
+	res, err = bs.GetBeaconState(ctx, req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !proto.Equal(wanted, res) {
+		t.Errorf("Wanted %v, received %v", wanted, res)
+	}
+}
