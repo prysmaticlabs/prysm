85ff8aa16ebcbb44c9d02d6aa0607e009a55024b
terence tsao <terence@prysmaticlabs.com>
2020-04-28 10:37:20 -0700

    Minor touchups

--- tools/update-genesis-time/main.go
+++ tools/update-genesis-time/main.go
@@ -11,18 +11,18 @@ import (
 )
 
 var (
-	genesisTime = flag.Uint64("genesis-time", 0, "Unix timestamp used as the genesis time in the generated genesis state (defaults to now)")
-	inputSSZ    = flag.String("input-ssz", "", "Output filename of the SSZ marshaling of the generated genesis state")
+	genesisTime   = flag.Uint64("genesis-time", 0, "Unix timestamp used as the genesis time in the generated genesis state (defaults to now)")
+	inputSSZState = flag.String("input-ssz-state", "", "Input filename of the SSZ marshaling of the genesis state")
 )
 
 func main() {
 	flag.Parse()
-	if *inputSSZ == "" {
-		log.Fatal("Expected --input-ssz")
+	if *inputSSZState == "" {
+		log.Fatal("Expected --input-ssz-state")
 	}
 
 	beaconState := &pb.BeaconState{}
-	if err := unmarshalFile(*inputSSZ, beaconState); err != nil {
+	if err := unmarshalFile(*inputSSZState, beaconState); err != nil {
 		log.Fatal(err)
 	}
 	if *genesisTime == 0 {
@@ -36,10 +36,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not ssz marshal the beacon state: %v", err)
 	}
-	if err := ioutil.WriteFile(*inputSSZ, encodedState, 0644); err != nil {
+	if err := ioutil.WriteFile(*inputSSZState, encodedState, 0644); err != nil {
 		log.Fatalf("Could not write encoded beacon state to file: %v", err)
 	}
-	log.Printf("Done writing to %s", *inputSSZ)
+	log.Printf("Done writing to %s", *inputSSZState)
 }
 
 func unmarshalFile(fPath string, data interface{}) error {
