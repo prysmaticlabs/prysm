a9638e60222f013589b79b5fbaf81be327eff2f5
Victor Farazdagi <simple.square@gmail.com>
2020-04-29 21:10:59 +0300

    better teardown of resources

--- shared/keystore/keystore_test.go
+++ shared/keystore/keystore_test.go
@@ -124,10 +124,12 @@ func TestEncryptDecryptKey(t *testing.T) {
 }
 
 func TestGetSymlinkedKeys(t *testing.T) {
-	tmpdir := testutil.TempDir() + "/keystore"
-	if err := os.RemoveAll(tmpdir); err != nil {
-		t.Errorf("unable to remove temporary files %v", err)
-	}
+	tmpdir := testutil.TempDir() + "/symlinked-keystore"
+	defer func() {
+		if err := os.RemoveAll(tmpdir); err != nil {
+			t.Logf("unable to remove temporary files: %v", err)
+		}
+	}()
 	ks := &Store{
 		scryptN: LightScryptN,
 		scryptP: LightScryptP,
@@ -157,9 +159,5 @@ func TestGetSymlinkedKeys(t *testing.T) {
 		if !bytes.Equal(s.SecretKey.Marshal(), key.SecretKey.Marshal()) {
 			t.Fatalf("retrieved secret keys are not equal %v ", s.SecretKey.Marshal())
 		}
-	}
-
-	if err := os.RemoveAll(tmpdir); err != nil {
-		t.Errorf("unable to remove temporary files %v", err)
 	}
 }
