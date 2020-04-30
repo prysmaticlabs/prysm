cebfc1b64114b483dc2c8513b718018ff2594d50
Preston Van Loon <preston@prysmaticlabs.com>
2020-04-30 15:36:27 -0700

    allow environment variable

--- fuzz/testing/beacon_fuzz_states.go
+++ fuzz/testing/beacon_fuzz_states.go
@@ -4,14 +4,22 @@ import (
 
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
+	"os"
 	"strconv"
 )
 
 const fileBase = "0-11-0/mainnet/beaconstate"
+const fileBaseENV = "BEACONSTATES_PATH"
 
 // GetBeaconFuzzState returns a beacon state by ID using the beacon-fuzz corpora.
 func GetBeaconFuzzState(ID uint16) (*pb.BeaconState, error) {
-	b, err := testutil.BazelFileBytes(fileBase, strconv.Itoa(int(ID)))
+	base := fileBase
+	// Using an environment variable allows a host image to specify the path when only the binary
+	// executable was uploaded (without the runfiles). i.e. fuzzit's platform.
+	if p, ok := os.LookupEnv(fileBaseENV); ok {
+		base = p
+	}
+	b, err := testutil.BazelFileBytes(base, strconv.Itoa(int(ID)))
 	if err != nil {
 		return nil, err
 	}
