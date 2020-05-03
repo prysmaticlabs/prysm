725a6f0388ef8a3798f868f03095896f850d90db
Victor Farazdagi <simple.square@gmail.com>
2020-05-04 00:50:44 +0300

    rest of the tests

--- slasher/db/testing/setup_db_test.go
+++ slasher/db/testing/setup_db_test.go
@@ -1,17 +1,38 @@
 package testing
 
 import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
 	"os"
+	"path"
 	"testing"
+
+	"github.com/prysmaticlabs/prysm/shared/testutil"
+	slasherDB "github.com/prysmaticlabs/prysm/slasher/db"
+	"github.com/prysmaticlabs/prysm/slasher/db/kv"
 )
 
 func TestClearDB(t *testing.T) {
-	slasherDB := SetupSlasherDB(t, false)
-	if err := slasherDB.ClearDB(); err != nil {
+	randPath, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	if err != nil {
+		t.Fatalf("Could not generate random file path: %v", err)
+	}
+	p := path.Join(testutil.TempDir(), fmt.Sprintf("/%d", randPath))
+	if err := os.RemoveAll(p); err != nil {
+		t.Fatalf("Failed to remove directory: %v", err)
+	}
+	cfg := &kv.Config{}
+	db, err := slasherDB.NewDB(p, cfg)
+	db.EnableSpanCache(false)
+	if err != nil {
+		t.Fatalf("Failed to instantiate DB: %v", err)
+	}
+	if err := db.ClearDB(); err != nil {
 		t.Fatal(err)
 	}
 
-	if _, err := os.Stat(slasherDB.DatabasePath()); !os.IsNotExist(err) {
+	if _, err := os.Stat(db.DatabasePath()); !os.IsNotExist(err) {
 		t.Fatalf("db wasnt cleared %v", err)
 	}
 }
