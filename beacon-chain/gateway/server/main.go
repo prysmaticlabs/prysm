36e2a49cbe46f3fefe100c51119d7cf7d2b0e671
terence tsao <terence@prysmaticlabs.com>
2020-05-06 13:10:16 -0700

    Update beacon-chain/gateway/server/main.go
    Co-authored-by: Preston Van Loon <preston@prysmaticlabs.com>

--- beacon-chain/gateway/server/main.go
+++ beacon-chain/gateway/server/main.go
@@ -21,7 +21,7 @@ var (
 	debug                   = flag.Bool("debug", false, "Enable debug logging")
 	allowedOrigins          = flag.String("corsdomain", "", "A comma separated list of CORS domains to allow")
 	enableDebugRPCEndpoints = flag.Bool("enable-debug-rpc-endpoints", false, "Enable debug rpc endpoints such as /eth/v1alpha1/beacon/state")
-	grpcMaxMsgSize          = flag.Int("grpc-max-msg-size", 1<<22, "Integer to define max recieve message call size (default: 4194304 (for 40MB))")
+	grpcMaxMsgSize          = flag.Int("grpc-max-msg-size", 1<<22, "Integer to define max recieve message call size")
 )
 
 func init() {
