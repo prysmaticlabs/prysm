e8123b4aa77cd0e36cc62cba3c06e3a504faa444
michaelhly <michaelhly@gmail.com>
2020-05-09 18:21:28 -0500

    Expect ValidatorStatus to be called

--- validator/accounts/status_test.go
+++ validator/accounts/status_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/keystore"
 	"github.com/prysmaticlabs/prysm/validator/internal"
 )
@@ -14,8 +15,18 @@ func TestFetchAccountStatuses_OK(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockClient := internal.NewMockBeaconNodeValidatorClient(ctrl)
-	keyPairs := make(map[string]*keystore.Key)
-	_, err := FetchAccountStatuses(ctx, mockClient, keyPairs)
+	key, err := keystore.NewKey()
+	if err != nil {
+		t.Fatal("Failed to generate new key.")
+	}
+	keyPairs := map[string]*keystore.Key{"": key}
+	mockClient.EXPECT().ValidatorStatus(
+		gomock.Any(),
+		&ethpb.ValidatorStatusRequest{
+			PublicKey: key.PublicKey.Marshal(),
+		},
+	)
+	_, err = FetchAccountStatuses(ctx, mockClient, keyPairs)
 	if err != nil {
 		t.Fatalf("FetchAccountStatuses failed with error: %v", err)
 	}
