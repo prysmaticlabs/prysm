15132b75f3ce1ba2c14452d9e4ab446e1dec483e
michaelhly <michaelhly@gmail.com>
2020-05-09 17:36:22 -0500

    Remove unnecessary casting

--- validator/accounts/status_test.go
+++ validator/accounts/status_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/keystore"
 	"github.com/prysmaticlabs/prysm/validator/internal"
 )
@@ -15,10 +14,8 @@ func TestFetchAccountStatuses_OK(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockClient := internal.NewMockBeaconNodeValidatorClient(ctrl)
-
 	keyPairs := make(map[string]*keystore.Key)
-	_, err := FetchAccountStatuses(
-		ctx, ethpb.BeaconNodeValidatorClient(mockClient), keyPairs)
+	_, err := FetchAccountStatuses(ctx, mockClient, keyPairs)
 	if err != nil {
 		t.Fatalf("FetchAccountStatuses failed with error: %v", err)
 	}
