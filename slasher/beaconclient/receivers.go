cd1a4f18f47876c0f9e6909cd04d048b28831a0e
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-05-12 14:32:42 -0400

    Fix historical detection for the slasher (#5831)
    * Fix historical detection for the slasher
    * More fixes
    * Fix
    * Run historical detection sequentially
    * Add context err handler
    * Add return
    * Fixes
    * Merge branch 'master' into slasher-fixes

--- slasher/beaconclient/receivers.go
+++ slasher/beaconclient/receivers.go
@@ -104,7 +104,7 @@ func (bs *Service) receiveAttestations(ctx context.Context) {
 					break
 				default:
 					log.WithError(err).Errorf("Could not receive attestations from beacon node. rpc status: %v", e.Code())
-					return
+					break
 				}
 			} else {
 				log.WithError(err).Error("Could not receive attestations from beacon node")
