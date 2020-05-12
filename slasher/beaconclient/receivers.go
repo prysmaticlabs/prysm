6e80e1653b9270016f1d6d8aa2391bbde1566745
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-05-12 13:29:56 -0400

    Fix

--- slasher/beaconclient/receivers.go
+++ slasher/beaconclient/receivers.go
@@ -104,11 +104,11 @@ func (bs *Service) receiveAttestations(ctx context.Context) {
 					break
 				default:
 					log.WithError(err).Errorf("Could not receive attestations from beacon node. rpc status: %v", e.Code())
-					continue
+					break
 				}
 			} else {
 				log.WithError(err).Error("Could not receive attestations from beacon node")
-				continue
+				break
 			}
 		}
 		if res == nil {
