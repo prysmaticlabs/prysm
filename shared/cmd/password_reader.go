c67fe4a0201f782866f70b88259af297fec9b8c4
connorwstein <connor.stein@mail.mcgill.ca>
2020-05-11 23:08:54 -0400

    more lint

--- shared/cmd/password_reader.go
+++ shared/cmd/password_reader.go
@@ -5,15 +5,16 @@ import (
 	"os"
 )
 
-// Reads a password from a mock or stdin.
+// PasswordReader reads a password from a mock or stdin.
 type PasswordReader interface {
 	ReadPassword() (string, error)
 }
 
-// Reads a password from stdin.
+// StdInPasswordReader reads a password from stdin.
 type StdInPasswordReader struct {
 }
 
+// ReadPassword reads a password from stdin.
 func (pr StdInPasswordReader) ReadPassword() (string, error) {
 	pwd, error := terminal.ReadPassword(int(os.Stdin.Fd()))
 	return string(pwd), error
