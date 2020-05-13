9564ad4a9e277ed4c203f052d3e175cbe8e67ff5
michaelhly <michaelhly@gmail.com>
2020-05-13 09:39:00 -0500

    Capitalize constants

--- validator/accounts/status_test.go
+++ validator/accounts/status_test.go
@@ -15,9 +15,9 @@ func TestFetchAccountStatuses_OK(t *testing.T) {
 	ctx := context.Background()
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	const numBatches = 5
-	pubkeys := make([][]byte, MaxRequestKeys*numBatches)
-	for i := 0; i < MaxRequestKeys*numBatches; i++ {
+	const NumBatches = 5
+	pubkeys := make([][]byte, MaxRequestKeys*NumBatches)
+	for i := 0; i < MaxRequestKeys*NumBatches; i++ {
 		pubkeyBytes := bytesutil.ToBytes48([]byte{byte(i)})
 		pubkeys[i] = pubkeyBytes[:]
 	}
@@ -34,21 +34,21 @@ func TestFetchAccountStatuses_OK(t *testing.T) {
 	if err != nil {
 		t.Fatalf("FetchAccountStatuses failed with error: %v.", err)
 	}
-	if len(allStatuses) != numBatches {
-		t.Fatalf("Want %d status arrays. Recieved %d", numBatches, len(allStatuses))
+	if len(allStatuses) != NumBatches {
+		t.Fatalf("Want %d status arrays. Recieved %d", NumBatches, len(allStatuses))
 	}
 }
 
 func TestMergeStatuses_OK(t *testing.T) {
-	const numBatches = 3
-	const numStatusTypes = int(ethpb.ValidatorStatus_EXITED) + 1
-	allStatuses := make([][]ValidatorStatusMetadata, numBatches)
-	for i := 0; i < numBatches; i++ {
+	const NumBatches = 5
+	const NumStatusTypes = int(ethpb.ValidatorStatus_EXITED) + 1
+	allStatuses := make([][]ValidatorStatusMetadata, NumBatches)
+	for i := 0; i < NumBatches; i++ {
 		statuses := make([]ValidatorStatusMetadata, MaxRequestKeys)
 		for j := 0; j < MaxRequestKeys; j++ {
 			statuses[j] = ValidatorStatusMetadata{
 				Metadata: &ethpb.ValidatorStatusResponse{
-					Status: ethpb.ValidatorStatus(j % numStatusTypes),
+					Status: ethpb.ValidatorStatus(j % NumStatusTypes),
 				},
 			}
 		}
