82760ed932bede122a0c709f468cabd3bbcbb169
rauljordan <raul@prysmaticlabs.com>
2020-05-14 11:38:04 -0500

    back to 12

--- shared/params/config.go
+++ shared/params/config.go
@@ -256,7 +256,7 @@ func MinimalSpecConfig() *BeaconChainConfig {
 	minimalConfig.BLSWithdrawalPrefixByte = byte(0)
 
 	// Time parameters
-	minimalConfig.SecondsPerSlot = 1
+	minimalConfig.SecondsPerSlot = 12
 	minimalConfig.MinAttestationInclusionDelay = 1
 	minimalConfig.SlotsPerEpoch = 8
 	minimalConfig.MinSeedLookahead = 1
