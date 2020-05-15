59dd82847b62e0f40c7c2bd77322e9a7ba638f62
rauljordan <raul@prysmaticlabs.com>
2020-05-15 16:15:24 -0500

    gen

--- beacon-chain/rpc/node/server.go
+++ beacon-chain/rpc/node/server.go
@@ -7,7 +7,6 @@ import (
 	"context"
 	"fmt"
 	"sort"
-	"time"
 
 	ptypes "github.com/gogo/protobuf/types"
 	"github.com/libp2p/go-libp2p-core/network"
@@ -48,9 +47,6 @@ func (ns *Server) GetGenesis(ctx context.Context, _ *ptypes.Empty) (*ethpb.Genes
 		return nil, status.Errorf(codes.Internal, "Could not retrieve contract address from db: %v", err)
 	}
 	genesisTime := ns.GenesisTimeFetcher.GenesisTime()
-	if genesisTime == time.Unix(0, 0) {
-		return nil, status.Error(codes.FailedPrecondition, "Genesis time yet to be determined")
-	}
 	gt, err := ptypes.TimestampProto(genesisTime)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "Could not convert genesis time to proto: %v", err)
