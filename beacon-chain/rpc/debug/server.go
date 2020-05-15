3d742720207d55046444cb23a55594eb0278b00b
rauljordan <raul@prysmaticlabs.com>
2020-05-15 16:34:20 -0500

    comment

--- beacon-chain/rpc/debug/server.go
+++ beacon-chain/rpc/debug/server.go
@@ -1,5 +1,6 @@
 // Package debug defines a gRPC server implementation of a debugging service
-// which allows for helpful endpoints to debug a beacon node at runtime.
+// which allows for helpful endpoints to debug a beacon node at runtime, this server is
+// gated behind the feature flag --enable-debug-rpc-endpoints.
 package debug
 
 import (
@@ -19,7 +20,8 @@ import (
 )
 
 // Server defines a server implementation of the gRPC Debug service,
-// providing RPC endpoints for runtime debugging of a node.
+// providing RPC endpoints for runtime debugging of a node, this server is
+// gated behind the feature flag --enable-debug-rpc-endpoints.
 type Server struct {
 	GenesisTimeFetcher blockchain.TimeFetcher
 	StateGen           *stategen.State
