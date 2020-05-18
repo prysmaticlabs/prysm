5fd398e477a63533afe9dabd3b54a647f220dde6
nisdas <nishdas93@gmail.com>
2020-05-18 18:51:38 +0800

    lint

--- beacon-chain/sync/utils.go
+++ beacon-chain/sync/utils.go
@@ -6,6 +6,9 @@ import (
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 )
 
+// A type to represent beacon blocks and roots which have methods
+// which satisfy the Interface in `Sort` so that this type can
+// be sorted in ascending order.
 type sortedObj struct {
 	blks  []*ethpb.SignedBeaconBlock
 	roots [][32]byte
@@ -24,6 +27,8 @@ func (s sortedObj) Len() int {
 	return len(s.blks)
 }
 
+// sort the provided blocks and roots in ascending order. This method assumes that the size of
+// block slice and root slice is equal.
 func (r *Service) sortBlocksAndRoots(blks []*ethpb.SignedBeaconBlock, roots [][32]byte) ([]*ethpb.SignedBeaconBlock, [][32]byte) {
 	obj := sortedObj{
 		blks:  blks,
