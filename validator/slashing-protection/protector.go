b7330c494d6c84406b64b06e6f5526543b51547d
shayzluf <thezluf@gmail.com>
2020-05-18 16:52:23 +0300

    fix comment

--- validator/slashing-protection/protector.go
+++ validator/slashing-protection/protector.go
@@ -6,7 +6,7 @@ import (
 	eth "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 )
 
-// Notifier interface defines the methods of the service that provides block updates to consumers.
+// Protector interface defines the methods of the service that provides slashing protection.
 type Protector interface {
 	VerifyAttestation(ctx context.Context, attestation *eth.IndexedAttestation) bool
 	VerifyBlock(ctx context.Context, blockHeader *eth.SignedBeaconBlockHeader) bool
