d4c1349da2142291b7cf4239f85a3479144443c0
terence tsao <terence@prysmaticlabs.com>
2020-05-19 09:17:11 -0700

    Filter inactive penalties processing by slashed (#5903)
    * Fixed existing tests
    
    * Filter finality delay penalty by slashed
    
    * Regression test
    
    * Log
    
    * Feedback
    
    * Comment
    
    * Update reward_penalty.go
    
    * Update reward_penalty.go
    
    * Added a slashed inactive penalty test

--- beacon-chain/core/epoch/precompute/reward_penalty_test.go
+++ beacon-chain/core/epoch/precompute/reward_penalty_test.go
@@ -90,6 +90,14 @@ func TestAttestationDeltaPrecompute(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	slashedAttestedIndices := []uint64{1413}
+	for _, i := range slashedAttestedIndices {
+		vs := state.Validators()
+		vs[i].Slashed = true
+		if state.SetValidators(vs) != nil {
+			t.Fatal(err)
+		}
+	}
 
 	vp, bp, err := New(context.Background(), state)
 	if err != nil {
@@ -102,8 +110,8 @@ func TestAttestationDeltaPrecompute(t *testing.T) {
 
 	// Add some variances to target and head balances.
 	// See: https://github.com/prysmaticlabs/prysm/issues/5593
-	bp.PrevEpochTargetAttesters = bp.PrevEpochTargetAttesters / 2
-	bp.PrevEpochHeadAttesters = bp.PrevEpochHeadAttesters * 2 / 3
+	bp.PrevEpochTargetAttested = bp.PrevEpochTargetAttested / 2
+	bp.PrevEpochHeadAttested = bp.PrevEpochHeadAttested * 2 / 3
 	rewards, penalties, err := attestationDeltas(state, bp, vp)
 	if err != nil {
 		t.Fatal(err)
@@ -117,26 +125,40 @@ func TestAttestationDeltaPrecompute(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	attestedIndices := []uint64{55, 1339, 1746, 1811, 1569, 1413}
+	attestedIndices := []uint64{55, 1339, 1746, 1811, 1569}
 	for _, i := range attestedIndices {
 		base, err := epoch.BaseReward(state, i)
 		if err != nil {
 			t.Errorf("Could not get base reward: %v", err)
 		}
+
 		// Base rewards for getting source right
 		wanted := attestedBalance*base/totalBalance +
-			bp.PrevEpochTargetAttesters*base/totalBalance +
-			bp.PrevEpochHeadAttesters*base/totalBalance
+			bp.PrevEpochTargetAttested*base/totalBalance +
+			bp.PrevEpochHeadAttested*base/totalBalance
 		// Base rewards for proposer and attesters working together getting attestation
 		// on chain in the fatest manner
 		proposerReward := base / params.BeaconConfig().ProposerRewardQuotient
-		wanted += (base - proposerReward) * params.BeaconConfig().MinAttestationInclusionDelay
+		wanted += (base-proposerReward)*params.BeaconConfig().MinAttestationInclusionDelay - 1
 		if rewards[i] != wanted {
 			t.Errorf("Wanted reward balance %d, got %d for validator with index %d", wanted, rewards[i], i)
 		}
 		// Since all these validators attested, they shouldn't get penalized.
 		if penalties[i] != 0 {
 			t.Errorf("Wanted penalty balance 0, got %d", penalties[i])
+		}
+	}
+
+	for _, i := range slashedAttestedIndices {
+		base, err := epoch.BaseReward(state, i)
+		if err != nil {
+			t.Errorf("Could not get base reward: %v", err)
+		}
+		if rewards[i] != 0 {
+			t.Errorf("Wanted slashed indices reward balance 0, got %d", penalties[i])
+		}
+		if penalties[i] != 3*base {
+			t.Errorf("Wanted slashed indices penalty balance %d, got %d", 3*base, penalties[i])
 		}
 	}
 
@@ -199,6 +221,72 @@ func TestAttestationDeltas_ZeroEpoch(t *testing.T) {
 	_, _, err = attestationDeltas(state, pBal, pVals)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestProcessRewardsAndPenaltiesPrecompute_SlashedInactivePenalty(t *testing.T) {
+	e := params.BeaconConfig().SlotsPerEpoch
+	validatorCount := uint64(2048)
+	base := buildState(e+3, validatorCount)
+	atts := make([]*pb.PendingAttestation, 3)
+	for i := 0; i < len(atts); i++ {
+		atts[i] = &pb.PendingAttestation{
+			Data: &ethpb.AttestationData{
+				Target: &ethpb.Checkpoint{},
+				Source: &ethpb.Checkpoint{},
+			},
+			AggregationBits: bitfield.Bitlist{0xC0, 0xC0, 0xC0, 0xC0, 0x01},
+			InclusionDelay:  1,
+		}
+	}
+	base.PreviousEpochAttestations = atts
+
+	state, err := state.InitializeFromProto(base)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := state.SetSlot(params.BeaconConfig().SlotsPerEpoch * 10); err != nil {
+		t.Fatal(err)
+	}
+
+	slashedAttestedIndices := []uint64{14, 37, 68, 77, 139}
+	for _, i := range slashedAttestedIndices {
+		vs := state.Validators()
+		vs[i].Slashed = true
+		if state.SetValidators(vs) != nil {
+			t.Fatal(err)
+		}
+	}
+
+	vp, bp, err := New(context.Background(), state)
+	if err != nil {
+		t.Error(err)
+	}
+	vp, bp, err = ProcessAttestations(context.Background(), state, vp, bp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rewards, penalties, err := attestationDeltas(state, bp, vp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	finalityDelay := helpers.PrevEpoch(state) - state.FinalizedCheckpointEpoch()
+	for _, i := range slashedAttestedIndices {
+		base, err := epoch.BaseReward(state, i)
+		if err != nil {
+			t.Errorf("Could not get base reward: %v", err)
+		}
+		penalty := 3 * base
+		penalty += params.BeaconConfig().BaseRewardsPerEpoch * base
+		penalty += vp[i].CurrentEpochEffectiveBalance * finalityDelay / params.BeaconConfig().InactivityPenaltyQuotient
+		if penalties[i] != penalty {
+			t.Errorf("Wanted slashed indices penalty balance %d, got %d", penalty, penalties[i])
+		}
+
+		if rewards[i] != 0 {
+			t.Errorf("Wanted slashed indices reward balance 0, got %d", penalties[i])
+		}
 	}
 }
 
