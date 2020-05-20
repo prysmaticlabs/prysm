28753cda6586fcf424b702529f71e17b470ad468
rauljordan <raul@prysmaticlabs.com>
2020-05-20 09:44:25 -0500

    add block endpoint

---
+++ beacon-chain/rpc/debug/block.go
@@ -0,0 +1,30 @@
+package debug
+
+import (
+	"context"
+
+	"github.com/prysmaticlabs/go-ssz"
+	pbrpc "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// GetBlock in an ssz-encoded format by block root.
+func (ds *Server) GetBlock(
+	ctx context.Context,
+	req *pbrpc.BlockRequest,
+) (*pbrpc.SSZResponse, error) {
+	root := bytesutil.ToBytes32(req.BlockRoot)
+	signedBlock, err := ds.BeaconDB.Block(ctx, root)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not retrieve block by root: %v", err)
+	}
+	encoded, err := ssz.Marshal(signedBlock.Block)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not marshal block: %v", err)
+	}
+	return &pbrpc.SSZResponse{
+		Encoded: encoded,
+	}, nil
+}
