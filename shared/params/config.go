52c1cca8f62a6222a17cce1a51d4791a349416f5
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-05-21 11:25:26 -0400

    Change minimal and fix test

--- shared/params/config.go
+++ shared/params/config.go
@@ -297,7 +297,7 @@ func MinimalSpecConfig() *BeaconChainConfig {
 
 	// Max operations per block
 	minimalConfig.MaxProposerSlashings = 16
-	minimalConfig.MaxAttesterSlashings = 1
+	minimalConfig.MaxAttesterSlashings = 2
 	minimalConfig.MaxAttestations = 128
 	minimalConfig.MaxDeposits = 16
 	minimalConfig.MaxVoluntaryExits = 16
