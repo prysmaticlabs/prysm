37ba8b7c6973f3d5cec1dbcc8d0efc6b301f71b6
terence tsao <terence@prysmaticlabs.com>
2020-05-22 22:50:09 -0700

    Unskip ssz tests

--- proto/testing/ssz_static_minimal_test.go
+++ proto/testing/ssz_static_minimal_test.go
@@ -5,7 +5,5 @@ import (
 )
 
 func TestSSZStatic_Minimal(t *testing.T) {
-	t.Skip("Skipping until 5935 is resolved, this requires pointing spec test to the latest version")
-
 	runSSZStaticTests(t, "minimal")
 }
--- proto/testing/ssz_static_test.go
+++ proto/testing/ssz_static_test.go
@@ -23,7 +23,6 @@ type SSZRoots struct {
 }
 
 func runSSZStaticTests(t *testing.T, config string) {
-	t.Skip("Skipping until #5935 is complete")
 	if err := spectest.SetConfig(t, config); err != nil {
 		t.Fatal(err)
 	}
