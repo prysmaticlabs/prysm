4ef888d9d61406d6a72a4ad3554ab19c3ab650ce
terence tsao <terence@prysmaticlabs.com>
2020-05-26 11:21:17 -0700

    Test

--- beacon-chain/rpc/debug/state.go
+++ beacon-chain/rpc/debug/state.go
@@ -3,6 +3,7 @@ package debug
 import (
 	"context"
 
+	ptypes "github.com/gogo/protobuf/types"
 	"github.com/prysmaticlabs/go-ssz"
 	pbrpc "github.com/prysmaticlabs/prysm/proto/beacon/rpc/v1"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
@@ -60,4 +61,14 @@ func (ds *Server) GetBeaconState(
 	default:
 		return nil, status.Error(codes.InvalidArgument, "Need to specify either a block root or slot to request state")
 	}
+}
+
+// GetProtoArrayForkChoice returns proto array fork choice store.
+func (ds *Server) GetProtoArrayForkChoice(ctx context.Context, _ *ptypes.Empty) (*pbrpc.ProtoArrayForkChoiceResponse, error) {
+	store := ds.HeadFetcher.ProtoArrayStore()
+	return &pbrpc.ProtoArrayForkChoiceResponse{
+		PruneThreshold: store.PruneThreshold,
+		JustifiedEpoch: store.JustifiedEpoch,
+		FinalizedEpoch: store.FinalizedEpoch,
+	}, nil
 }
