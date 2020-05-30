fce6822cd48a6ce6fa9c5f735232e27ff63d2b05
Preston Van Loon <preston@prysmaticlabs.com>
2020-05-30 14:19:36 -0700

    lint

--- endtoend/geth_deps.go
+++ endtoend/geth_deps.go
@@ -4,16 +4,16 @@ package endtoend
 // Having these dependencies listed here helps go mod understand that these dependencies are
 // necessary for end to end tests since we build go-ethereum binary for this test.
 import (
-	_ "github.com/ethereum/go-ethereum/accounts"
-	_ "github.com/ethereum/go-ethereum/accounts/keystore"
-	_ "github.com/ethereum/go-ethereum/cmd/utils"
-	_ "github.com/ethereum/go-ethereum/common"
-	_ "github.com/ethereum/go-ethereum/console"
-	_ "github.com/ethereum/go-ethereum/eth"
-	_ "github.com/ethereum/go-ethereum/eth/downloader"
-	_ "github.com/ethereum/go-ethereum/ethclient"
-	_ "github.com/ethereum/go-ethereum/les"
-	_ "github.com/ethereum/go-ethereum/log"
-	_ "github.com/ethereum/go-ethereum/metrics"
-	_ "github.com/ethereum/go-ethereum/node"
+	_ "github.com/ethereum/go-ethereum/accounts"          // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/accounts/keystore" // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/cmd/utils"         // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/common"            // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/console"           // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/eth"               // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/eth/downloader"    // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/ethclient"         // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/les"               // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/log"               // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/metrics"           // Required for go-ethereum e2e.
+	_ "github.com/ethereum/go-ethereum/node"              // Required for go-ethereum e2e.
 )
