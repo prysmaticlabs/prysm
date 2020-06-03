4b669d91118ed0f347789155082a2dc193dc7364
Victor Farazdagi <simple.square@gmail.com>
2020-06-03 23:39:22 +0300

    Assert gfind is available on OSX, for protobufs updates

--- scripts/update-go-ssz.sh
+++ scripts/update-go-ssz.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. $(dirname "$0")/common.sh
 
 # Script to copy ssz.go files from bazel build folder to appropriate location.
 # Bazel builds to bazel-bin/... folder, script copies them back to original folder where target is.
@@ -6,21 +7,18 @@
 bazel query 'kind(ssz_gen_marshal, //proto/...) union kind(ssz_gen_marshal, //fuzz/...)' | xargs bazel build
 
 # Get locations of proto ssz.go files.
-
 file_list=()
-while IFS= read -d $'\0' -r file ; do
+while IFS= read -d $'\0' -r file; do
     file_list=("${file_list[@]}" "$file")
-done < <(find -L $(bazel info bazel-bin)/ -type f -regextype sed -regex ".*ssz\.go$" -print0)
+done < <($findutil -L $(bazel info bazel-bin)/ -type f -regextype sed -regex ".*ssz\.go$" -print0)
 
 arraylength=${#file_list[@]}
 searchstring="/bin/"
 
 # Copy ssz.go files from bazel-bin to original folder where the target is located.
-
-for (( i=0; i<${arraylength}; i++ ));
-do
-  destination=${file_list[i]#*$searchstring}
-  chmod 755 "$destination"
-  cp -R -L "${file_list[i]}" "$destination"
+for ((i = 0; i < ${arraylength}; i++)); do
+    destination=${file_list[i]#*$searchstring}
+    color "34" $destination
+    chmod 755 "$destination"
+    cp -R -L "${file_list[i]}" "$destination"
 done
-
