4cc20ebe529076c85eb16c3b887a8113044b4b0a
Preston Van Loon <preston@prysmaticlabs.com>
2020-06-08 15:38:58 -0700

    Goimports

--- beacon-chain/blockchain/process_attestation_test.go
+++ beacon-chain/blockchain/process_attestation_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	"github.com/prysmaticlabs/go-ssz"
 	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
