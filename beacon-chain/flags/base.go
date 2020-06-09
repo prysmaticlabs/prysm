145019a4efafb2ac030eecbd2d51c4f8152ea5f2
Preston Van Loon <preston@prysmaticlabs.com>
2020-06-09 11:37:28 -0700

    Add deployed Onyx contract

--- beacon-chain/flags/base.go
+++ beacon-chain/flags/base.go
@@ -17,7 +17,7 @@ var (
 	DepositContractFlag = &cli.StringFlag{
 		Name:  "deposit-contract",
 		Usage: "Deposit contract address. Beacon chain node will listen logs coming from the deposit contract to determine when validator is eligible to participate.",
-		Value: "0x5cA1e00004366Ac85f492887AAab12d0e6418876",
+		Value: "0x0F0F0fc0530007361933EaB5DB97d09aCDD6C1c8",
 	}
 	// RPCHost defines the host on which the RPC server should listen.
 	RPCHost = &cli.StringFlag{
@@ -82,7 +82,7 @@ var (
 	ContractDeploymentBlock = &cli.IntFlag{
 		Name:  "contract-deployment-block",
 		Usage: "The eth1 block in which the deposit contract was deployed.",
-		Value: 2523557,
+		Value: 2844925,
 	}
 	// SetGCPercent is the percentage of current live allocations at which the garbage collector is to run.
 	SetGCPercent = &cli.IntFlag{
