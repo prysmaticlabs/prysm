e2a3003632e3ea0785cf408aad96848c4fcdcf66
nisdas <nishdas93@gmail.com>
2020-06-11 13:55:23 +0800

    add user agent

--- shared/version/version.go
+++ shared/version/version.go
@@ -17,6 +17,15 @@ var gitTag = "Unknown"
 
 // GetVersion returns the version string of this build.
 func GetVersion() string {
+	if buildDate == "{DATE}" {
+		now := time.Now().Format(time.RFC3339)
+		buildDate = now
+	}
+	return fmt.Sprintf("%s. Built at: %s", GetBuildData(), buildDate)
+}
+
+// GetBuildData returns the git tag and commit of the current build.
+func GetBuildData() string {
 	// if doing a local build, these values are not interpolated
 	if gitCommit == "{STABLE_GIT_COMMIT}" {
 		commit, err := exec.Command("git", "rev-parse", "HEAD").Output()
@@ -26,9 +35,5 @@ func GetVersion() string {
 			gitCommit = strings.TrimRight(string(commit), "\r\n")
 		}
 	}
-	if buildDate == "{DATE}" {
-		now := time.Now().Format(time.RFC3339)
-		buildDate = now
-	}
-	return fmt.Sprintf("Prysm/%s/%s. Built at: %s", gitTag, gitCommit, buildDate)
+	return fmt.Sprintf("Prysm/%s/%s", gitTag, gitCommit)
 }
