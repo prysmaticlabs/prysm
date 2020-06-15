82dd3e4723fd4e307fa5d0e1a6390b9c16e22433
Victor Farazdagi <simple.square@gmail.com>
2020-06-15 17:34:06 +0300

    go mod tidy checker

---
+++ .github/actions/gomodtidy/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+set -e
+
+cd $GITHUB_WORKSPACE
+
+go mod tidy
+
+if [ "$(git status | grep -c 'nothing to commit, working tree clean')" = "1" ]; then
+  echo "go.sum is up to date"
+  exit 0
+fi
+
+# Notify of any issues.
+echo "The go.sum is not up to date: run 'go mod tidy' to update"
+exit 1
