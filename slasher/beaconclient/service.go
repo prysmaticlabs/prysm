89216427854c1a3aeb8647cf44ee13161695b286
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-06-17 16:26:10 -0400

    Uncomment

--- slasher/beaconclient/service.go
+++ slasher/beaconclient/service.go
@@ -192,6 +192,6 @@ func (bs *Service) Start() {
 	go bs.subscribeDetectedAttesterSlashings(bs.ctx, bs.attesterSlashingsChan)
 
 	// We listen to a stream of blocks and attestations from the beacon node.
-	//go bs.receiveBlocks(bs.ctx)
-	//go bs.receiveAttestations(bs.ctx)
+	go bs.receiveBlocks(bs.ctx)
+	go bs.receiveAttestations(bs.ctx)
 }
