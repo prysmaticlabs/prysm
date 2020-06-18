fce31573b914608df45514502c335c62ad5eddd3
Victor Farazdagi <simple.square@gmail.com>
2020-06-18 19:51:50 +0300

    update base aggregate

--- beacon-chain/core/helpers/attaggregation/aggregate.go
+++ beacon-chain/core/helpers/attaggregation/aggregate.go
@@ -1,10 +1,23 @@
+// Package attaggregation contains implementations of attestation aggregation
+// algorithms and heuristics.
 package attaggregation
 
-import "errors"
+import (
+	"errors"
 
-// AttestationAggregationStrategy defines attestation aggregation strategy.
-type AttestationAggregationStrategy string
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
+	"github.com/prysmaticlabs/prysm/shared/bls"
+	"github.com/sirupsen/logrus"
+)
 
+// BLS aggregate signature aliases for testing / benchmark substitution. These methods are
+// significantly more expensive than the inner logic of AggregateAttestations so they must be
+// substituted for benchmarks which analyze AggregateAttestations.
+var aggregateSignatures = bls.AggregateSignatures
+var signatureFromBytes = bls.SignatureFromBytes
+
+var log = logrus.WithField("prefix", "attaggregation")
 
 var (
 	// ErrBitsOverlap is returned when two attestations aggregation bits overlap with each other.
@@ -16,3 +29,42 @@ var (
 	// ErrInvalidStrategy is returned when invalid aggregation strategy is selected.
 	ErrInvalidStrategy = errors.New("invalid aggregation strategy")
 )
+
+// AttestationAggregationStrategy defines attestation aggregation strategy.
+type AttestationAggregationStrategy string
+
+// AggregatePair aggregates pair of attestations a1 and a2 together.
+func AggregatePair(a1 *ethpb.Attestation, a2 *ethpb.Attestation) (*ethpb.Attestation, error) {
+	if a1.AggregationBits.Len() != a2.AggregationBits.Len() {
+		return nil, ErrBitsDifferentLen
+	}
+	if a1.AggregationBits.Overlaps(a2.AggregationBits) {
+		return nil, ErrBitsOverlap
+	}
+
+	baseAtt := stateTrie.CopyAttestation(a1)
+	newAtt := stateTrie.CopyAttestation(a2)
+	if newAtt.AggregationBits.Count() > baseAtt.AggregationBits.Count() {
+		baseAtt, newAtt = newAtt, baseAtt
+	}
+
+	if baseAtt.AggregationBits.Contains(newAtt.AggregationBits) {
+		return baseAtt, nil
+	}
+
+	newBits := baseAtt.AggregationBits.Or(newAtt.AggregationBits)
+	newSig, err := signatureFromBytes(newAtt.Signature)
+	if err != nil {
+		return nil, err
+	}
+	baseSig, err := signatureFromBytes(baseAtt.Signature)
+	if err != nil {
+		return nil, err
+	}
+
+	aggregatedSig := aggregateSignatures([]*bls.Signature{baseSig, newSig})
+	baseAtt.Signature = aggregatedSig.Marshal()
+	baseAtt.AggregationBits = newBits
+
+	return baseAtt, nil
+}
