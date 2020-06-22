f58170ca3f0d0440c04df4d5bb3ab1221bed0484
Victor Farazdagi <simple.square@gmail.com>
2020-06-22 18:02:35 +0300

    adds feature flag

--- shared/featureconfig/flags.go
+++ shared/featureconfig/flags.go
@@ -173,6 +173,11 @@ var (
 		Name:  "disable-grpc-connection-logging",
 		Usage: "Disables displaying logs for newly connected grpc clients",
 	}
+	attestationAggregationStrategy = &cli.StringFlag{
+		Name:  "attestation-aggregation-strategy",
+		Usage: "Which strategy to use when aggregating attestations, one of: naive, max_cover.",
+		Value: "naive",
+	}
 )
 
 // devModeFlags holds list of flags that are set when development mode is on.
@@ -544,6 +549,7 @@ var BeaconChainFlags = append(deprecatedFlags, []cli.Flag{
 	enableKadDht,
 	disableReduceAttesterStateCopy,
 	disableGRPCConnectionLogging,
+	attestationAggregationStrategy,
 }...)
 
 // E2EBeaconChainFlags contains a list of the beacon chain feature flags to be tested in E2E.
@@ -551,5 +557,6 @@ var E2EBeaconChainFlags = []string{
 	"--cache-filtered-block-tree",
 	"--enable-state-gen-sig-verify",
 	"--check-head-state",
+	"--attestation-aggregation-strategy=max_cover",
 	"--dev",
 }
