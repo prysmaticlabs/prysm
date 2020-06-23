5c90038007ec58980f057b8b5439a6448988ceda
Victor Farazdagi <simple.square@gmail.com>
2020-06-23 07:00:38 +0300

    Adds aggregation/attestations package (#6343)
    * adds feature flag
    
    * aggregations/attestation package
    
    * better tests
    
    * bazel visibility issues
    
    * removes redundant code
    
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- shared/aggregation/attestations/maxcover_test.go
+++ shared/aggregation/attestations/maxcover_test.go
@@ -1,4 +1,4 @@
-package aggregation
+package attestations
 
 import (
 	"errors"
@@ -7,16 +7,17 @@ import (
 
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-bitfield"
+	"github.com/prysmaticlabs/prysm/shared/aggregation"
 )
 
-func TestAttestations_NewAttestationsMaxCover(t *testing.T) {
+func TestAggregateAttestations_MaxCover_NewMaxCover(t *testing.T) {
 	type args struct {
 		atts []*ethpb.Attestation
 	}
 	tests := []struct {
 		name        string
 		args        args
-		want        *MaxCoverProblem
+		want        *aggregation.MaxCoverProblem
 		wantErr     bool
 		expectedErr error
 	}{
@@ -48,7 +49,7 @@ func TestAttestations_NewAttestationsMaxCover(t *testing.T) {
 			},
 			want:        nil,
 			wantErr:     true,
-			expectedErr: ErrBitsDifferentLen,
+			expectedErr: aggregation.ErrBitsDifferentLen,
 		},
 		{
 			name: "single attestation",
@@ -57,9 +58,9 @@ func TestAttestations_NewAttestationsMaxCover(t *testing.T) {
 					{AggregationBits: bitfield.Bitlist{0b00001010, 0b1}},
 				},
 			},
-			want: &MaxCoverProblem{
-				Candidates: MaxCoverCandidates{
-					{0, &bitfield.Bitlist{0b00001010, 0b1}, 0, false},
+			want: &aggregation.MaxCoverProblem{
+				Candidates: aggregation.MaxCoverCandidates{
+					aggregation.NewMaxCoverCandidate(0, &bitfield.Bitlist{0b00001010, 0b1}),
 				},
 			},
 			wantErr: false,
@@ -75,13 +76,13 @@ func TestAttestations_NewAttestationsMaxCover(t *testing.T) {
 					{AggregationBits: bitfield.Bitlist{0b00000001, 0b1}},
 				},
 			},
-			want: &MaxCoverProblem{
-				Candidates: MaxCoverCandidates{
-					{0, &bitfield.Bitlist{0b00001010, 0b1}, 0, false},
-					{1, &bitfield.Bitlist{0b00101010, 0b1}, 0, false},
-					{2, &bitfield.Bitlist{0b11111010, 0b1}, 0, false},
-					{3, &bitfield.Bitlist{0b00000010, 0b1}, 0, false},
-					{4, &bitfield.Bitlist{0b00000001, 0b1}, 0, false},
+			want: &aggregation.MaxCoverProblem{
+				Candidates: aggregation.MaxCoverCandidates{
+					aggregation.NewMaxCoverCandidate(0, &bitfield.Bitlist{0b00001010, 0b1}),
+					aggregation.NewMaxCoverCandidate(1, &bitfield.Bitlist{0b00101010, 0b1}),
+					aggregation.NewMaxCoverCandidate(2, &bitfield.Bitlist{0b11111010, 0b1}),
+					aggregation.NewMaxCoverCandidate(3, &bitfield.Bitlist{0b00000010, 0b1}),
+					aggregation.NewMaxCoverCandidate(4, &bitfield.Bitlist{0b00000001, 0b1}),
 				},
 			},
 			wantErr: false,
@@ -89,7 +90,7 @@ func TestAttestations_NewAttestationsMaxCover(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewAttestationsMaxCover(tt.args.atts)
+			got, err := NewMaxCover(tt.args.atts)
 			if (err != nil) != tt.wantErr || !errors.Is(err, tt.expectedErr) {
 				t.Errorf("NewMaxCoverProblem() unexpected error, got: %v, want: %v", err, tt.expectedErr)
 				return
