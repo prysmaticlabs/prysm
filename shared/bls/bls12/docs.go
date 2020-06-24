a83f4346fc73dfe20dec539e0cc7f196018fb8fb
Preston Van Loon <preston@prysmaticlabs.com>
2020-06-24 16:43:43 -0700

    Split up tests, gofmt, goimports, docs

--- shared/bls/bls12/docs.go
+++ shared/bls/bls12/docs.go
@@ -1,4 +1,6 @@
-// Package bls implements a go-wrapper around a library implementing the
+// Package bls12 implements a go-wrapper around a library implementing the
 // the BLS12-381 curve and signature scheme. This package exposes a public API for
 // verifying and aggregating BLS signatures used by Ethereum 2.0.
+//
+// This implementation uses the library written by Herumi.
 package bls12
--- shared/bls/iface/interface.go
+++ shared/bls/iface/interface.go
@@ -1,17 +1,20 @@
 package iface
 
+// SecretKey represents a BLS secret or private key.
 type SecretKey interface {
 	PublicKey() PublicKey
 	Sign(msg []byte) Signature
 	Marshal() []byte
 }
 
+// PublicKey represents a BLS public key.
 type PublicKey interface {
 	Marshal() []byte
 	Copy() PublicKey
 	Aggregate(p2 PublicKey) PublicKey
 }
 
+// Signature represents a BLS signature.
 type Signature interface {
 	Verify(pubKey PublicKey, msg []byte) bool
 	AggregateVerify(pubKeys []PublicKey, msgs [][32]byte) bool
