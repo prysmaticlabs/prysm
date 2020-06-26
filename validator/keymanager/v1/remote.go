537b43f3f4958571211926929e63da46d969e2af
Raul Jordan <raul@prysmaticlabs.com>
2020-06-25 22:58:53 -0500

    fmt

--- validator/keymanager/v1/remote.go
+++ validator/keymanager/v1/remote.go
@@ -12,11 +12,11 @@ import (
 
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/bls"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	pb "github.com/wealdtech/eth2-signer-api/pb/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"github.com/prysmaticlabs/prysm/shared/bls"
-	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 )
 
 const (
