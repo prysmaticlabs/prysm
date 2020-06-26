c522fa5a93f4efe39d9f9c5c87d2e961eb51946d
Victor Farazdagi <simple.square@gmail.com>
2020-06-26 12:32:31 +0300

    adds simple no-panic test

--- shared/rand/rand.go
+++ shared/rand/rand.go
@@ -12,10 +12,10 @@ There are two modes, one for deterministic and another non-deterministic randomn
 	randGen.Intn(32) // or any other func defined in math.rand API
 
    In this mode, only seed is generated using cryptographically secure source (crypto/rand). So,
-   once seed is obtained, and generator is seeded, all the rest usage is deterministic, and fast.
+   once seed is obtained, and generator is seeded, the next generations are deterministic, thus fast.
    This method is still better than using unix time for source of randomness - since time is not a
-   good source of seed randomness, when you have many concurrent servers using it (and can coincide
-   in start times).
+   good source of seed randomness, when you have many concurrent servers using it (and they have
+   coinciding random generators' start times).
 
 2. For cryptographically secure non-deterministic mode (CSPRNG), use:
 
