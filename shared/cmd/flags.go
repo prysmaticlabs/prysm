2b4136640ca5e2b50dd84e6ca6f590f7ed4411d5
dv8silencer <dv8silencer+github@gmail.com>
2020-06-25 20:54:30 -0500

    Changed wording in the cli help for --bootstrap-node

--- shared/cmd/flags.go
+++ shared/cmd/flags.go
@@ -82,8 +82,8 @@ var (
 	// BootstrapNode tells the beacon node which bootstrap node to connect to
 	BootstrapNode = &cli.StringSliceFlag{
 		Name: "bootstrap-node",
-		Usage: "The address of bootstrap node. Beacon node will connect for peer discovery via DHT.  Multiple nodes can be passed by using the flag multiple times instead of comma-separated." +
-			" You can also pass YAML files ending with .enr",
+		Usage: "The address of bootstrap node. Beacon node will connect for peer discovery via DHT.  Multiple nodes can be passed by using the flag multiple times but not comma-separated." +
+			" You can also pass YAML files ending with .enr containing multiple nodes.",
 		Value: cli.NewStringSlice("enr:-Ku4QMKVC_MowDsmEa20d5uGjrChI0h8_KsKXDmgVQbIbngZV0idV6_RL7fEtZGo-kTNZ5o7_EJI_vCPJ6scrhwX0Z4Bh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhBLf22SJc2VjcDI1NmsxoQJxCnE6v_x2ekgY_uoE1rtwzvGy40mq9eD66XfHPBWgIIN1ZHCCD6A"),
 	}
 	// RelayNode tells the beacon node which relay node to connect to.
