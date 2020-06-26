08663b5eec3d4dfab1a3ba014ca808ea0cc06ddd
Raul Jordan <raul@prysmaticlabs.com>
2020-06-25 21:59:42 -0500

    Update validator/client/polling/propose.go

--- validator/client/polling/propose.go
+++ validator/client/polling/propose.go
@@ -8,7 +8,6 @@ import (
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-bitfield"
-
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stateutil"
 	"github.com/prysmaticlabs/prysm/shared/blockutil"
