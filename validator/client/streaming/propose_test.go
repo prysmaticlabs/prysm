69cb0807ee1e7cd9daa34e253fbb50d5ccbe0ab3
terence tsao <terence@prysmaticlabs.com>
2020-06-26 13:41:06 -0700

    Lint

--- validator/client/streaming/propose_test.go
+++ validator/client/streaming/propose_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/golang/mock/gomock"
 	lru "github.com/hashicorp/golang-lru"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	testing2 "github.com/prysmaticlabs/prysm/validator/db/testing"
+	dbTest "github.com/prysmaticlabs/prysm/validator/db/testing"
 	logTest "github.com/sirupsen/logrus/hooks/test"
 
 	slashpb "github.com/prysmaticlabs/prysm/proto/slashing"
@@ -23,7 +23,7 @@ type mocks struct {
 }
 
 func setup(t *testing.T) (*validator, *mocks, func()) {
-	valDB := testing2.SetupDB(t, [][48]byte{validatorPubKey})
+	valDB := dbTest.SetupDB(t, [][48]byte{validatorPubKey})
 	ctrl := gomock.NewController(t)
 	m := &mocks{
 		validatorClient: mock.NewMockBeaconNodeValidatorClient(ctrl),
