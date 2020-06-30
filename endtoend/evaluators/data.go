45aacc62851d015a3977b5f0e5f8835d177e0d4d
Preston Van Loon <preston@prysmaticlabs.com>
2020-06-30 09:22:36 -0700

    PR feedback

--- endtoend/evaluators/data.go
+++ endtoend/evaluators/data.go
@@ -9,28 +9,35 @@ import (
 	"google.golang.org/grpc"
 )
 
+const epochToCheck = 50 // must be more than 46 (32 hot states + 16 chkpt interval)
+
 // ColdStateCheckpoint checks data from the database using cold state storage.
 var ColdStateCheckpoint = types.Evaluator{
-	Name: "cold_state_assignments_for_epoch_5_from_epoch_%d",
+	Name: "cold_state_assignments_from_epoch_%d",
 	Policy: func(currentEpoch uint64) bool {
-		return currentEpoch == 50 // must be more than 46 (32 hot states + 16 chkpt interval)
+		return currentEpoch == epochToCheck
 	},
 	Evaluation: checkColdStateCheckpoint,
 }
 
 // Checks the first node for an old checkpoint using cold state storage.
 func checkColdStateCheckpoint(conns ...*grpc.ClientConn) error {
+	ctx := context.Background()
 	client := eth.NewBeaconChainClient(conns[0])
-	// This request for epoch 24 should use the cold state checkpoint from epoch 16.
-	res, err := client.ListValidatorAssignments(context.Background(), &eth.ListValidatorAssignmentsRequest{
-		QueryFilter: &eth.ListValidatorAssignmentsRequest_Epoch{Epoch: 24},
-	})
-	if err != nil {
-		return err
-	}
-	if res.Epoch != 24 {
-		return errors.New("failed to return a validator assignments response for an old epoch " +
-			"using cold state storage from the database")
+
+	for i := uint64(0); i < epochToCheck; i++ {
+		// This request for epoch 24 should use the cold state checkpoint from epoch 16.
+		res, err := client.ListValidatorAssignments(ctx, &eth.ListValidatorAssignmentsRequest{
+			QueryFilter: &eth.ListValidatorAssignmentsRequest_Epoch{Epoch: i},
+		})
+		if err != nil {
+			return err
+		}
+		// A simple check to ensure we received some data.
+		if res == nil || res.Epoch != i {
+			return errors.New("failed to return a validator assignments response for an old epoch " +
+				"using cold state storage from the database")
+		}
 	}
 
 	return nil
