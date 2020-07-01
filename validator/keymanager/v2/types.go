2d6f4ebf18792f4a1ea5e5d1fcc472de962dc0a9
Raul Jordan <raul@prysmaticlabs.com>
2020-07-01 16:30:01 -0500

    Part 1: Implement Accounts-V2 New, Wallet Creation (#6451)
    * begin accounts-v2 new
    
    * password validation
    
    * validator accounts new with eip-2335 keystore
    
    * select different wallet type based on enum
    
    * clean up code significantly
    
    * more robust code structure
    
    * check if wallet exists
    
    * define read and create wallet methods
    
    * fmt
    
    * go mod and comment
    
    * comment
    
    * redundant name
    
    * satify gofmt
    
    * add instructions with keymanager opts
    
    * wrap up create and read wallet functionality
    
    * prep for readiness
    
    * doc improvements
    
    * tests for create and read wallet
    
    * update deps
    
    * tidy
    
    * visibility
    
    * gaz
    
    * fix up
    
    * refactor for proper usage, with wallet and keymanager ifaces
    
    * Update validator/flags/flags.go
    
    Co-authored-by: Ivan Martinez <ivanthegreatdev@gmail.com>
    
    * import
    
    * improve structure
    
    * wrap up all comments
    
    * simplify
    
    * lint
    
    * Update validator/accounts/v2/cmd.go
    
    * viz check
    
    * add interface methods as needed
    
    * fix build
    
    * lint
    
    * nishant feedback
    
    * simplify structure
    
    * add tests for strong password check
    
    * all feedback done
    
    * ivan feedback
    
    * ivan feedback
    
    Co-authored-by: Ivan Martinez <ivanthegreatdev@gmail.com>
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

---
+++ validator/keymanager/v2/types.go
@@ -0,0 +1,47 @@
+package v2
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/prysmaticlabs/prysm/shared/bls"
+)
+
+// IKeymanager defines a general keymanager-v2 interface for Prysm wallets.
+type IKeymanager interface {
+	// CreateAccount based on the keymanager's logic.
+	CreateAccount(ctx context.Context, password string) error
+	// MarshalConfigFile for the keymanager's options.
+	MarshalConfigFile(ctx context.Context) ([]byte, error)
+	// FetchValidatingKeys fetches the list of public keys that should be used to validate with.
+	FetchValidatingPublicKeys() ([][48]byte, error)
+	// Sign signs a message using a validator key.
+	Sign(context.Context, interface{}) (bls.Signature, error)
+}
+
+// Kind defines an enum for either direct, derived, or remote-signing
+// keystores for Prysm wallets.
+type Kind int
+
+const (
+	// Direct keymanager defines an on-disk, encrypted keystore-capable store.
+	Direct Kind = iota
+	// Derived keymanager using a hierarchical-deterministic algorithm.
+	Derived
+	// Remote keymanager capable of remote-signing data.
+	Remote
+)
+
+// String marshals a keymanager kind to a string value.
+func (k Kind) String() string {
+	switch k {
+	case Direct:
+		return "direct"
+	case Derived:
+		return "derived"
+	case Remote:
+		return "remote"
+	default:
+		return fmt.Sprintf("%d", int(k))
+	}
+}
