a1a720fe588616c1eec32efa48a954f29e9331c7
terence tsao <terence@prysmaticlabs.com>
2020-07-01 12:31:31 -0700

    More exp

--- beacon-chain/state/stategen/migrate.go
+++ beacon-chain/state/stategen/migrate.go
@@ -5,7 +5,6 @@ import (
 	"encoding/hex"
 
 	"github.com/prysmaticlabs/go-ssz"
-	"github.com/prysmaticlabs/prysm/beacon-chain/db/filters"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/sirupsen/logrus"
 	"go.opencensus.io/trace"
@@ -31,81 +30,19 @@ func (s *State) MigrateToCold(ctx context.Context, finalizedSlot uint64, finaliz
 	}
 	s.stateSummaryCache.Clear()
 
-	lastArchivedIndex, err := s.beaconDB.LastArchivedIndex(ctx)
-	if err != nil {
-		return err
-	}
+	log.WithFields(logrus.Fields{
+		"lastFinalizedSlot": s.splitInfo.slot,
+		"newFinalizedSlot": finalizedSlot,
+	}).Info("Hot to cold migration")
 
-	// Move the states between split slot to finalized slot from hot section to the cold section.
-	filter := filters.NewFilter().SetStartSlot(currentSplitSlot).SetEndSlot(finalizedSlot - 1)
-	blockRoots, err := s.beaconDB.BlockRoots(ctx, filter)
-	if err != nil {
-		return err
-	}
-
-	for _, r := range blockRoots {
-		stateSummary, err := s.beaconDB.StateSummary(ctx, r)
-		if err != nil {
-			return err
-		}
-		if stateSummary == nil || stateSummary.Slot == 0 {
-			continue
-		}
-
-		archivedPointIndex := stateSummary.Slot / s.slotsPerArchivedPoint
-		nextArchivedPointSlot := (lastArchivedIndex + 1) * s.slotsPerArchivedPoint
-		// Only migrate if current slot is equal to or greater than next archived point slot.
-		if stateSummary.Slot >= nextArchivedPointSlot {
-			// If was a skipped archival index. The node should recover previous last archived index and state.
-			if skippedArchivedPoint(archivedPointIndex, lastArchivedIndex) {
-				recoveredIndex, err := s.recoverArchivedPoint(ctx, archivedPointIndex)
-				if err != nil {
-					return err
-				}
-				lastArchivedIndex = recoveredIndex
-			}
-
-			if !s.beaconDB.HasState(ctx, r) {
-				continue
-			}
-			if err := s.beaconDB.SaveArchivedPointRoot(ctx, r, archivedPointIndex); err != nil {
-				return err
-			}
-			if err := s.beaconDB.SaveLastArchivedIndex(ctx, archivedPointIndex); err != nil {
-				return err
-			}
-			lastArchivedIndex++
-			log.WithFields(logrus.Fields{
-				"slot":         stateSummary.Slot,
-				"archiveIndex": archivedPointIndex,
-				"root":         hex.EncodeToString(bytesutil.Trunc(r[:])),
-			}).Info("Saved archived point during state migration")
-		} else {
-			// Do not delete the current finalized state in case user wants to
-			// switch back to old state service, deleting the recent finalized state
-			// could cause issue switching back.
-			lastArchivedIndexRoot := s.beaconDB.LastArchivedIndexRoot(ctx)
-			if s.beaconDB.HasState(ctx, r) && r != lastArchivedIndexRoot && r != finalizedRoot {
-				if err := s.beaconDB.DeleteState(ctx, r); err != nil {
-					// For whatever reason if node is unable to delete a state due to
-					// state is finalized, it is more reasonable to continue than to exit.
-					log.Warnf("Unable to delete state during migration: %v", err)
-					continue
-				}
-				log.WithFields(logrus.Fields{
-					"slot": stateSummary.Slot,
-					"root": hex.EncodeToString(bytesutil.Trunc(r[:])),
-				}).Info("Deleted state during migration")
-			}
+	for i := s.splitInfo.slot; i <= finalizedSlot; i++ {
+		if i % s.slotsPerArchivedPoint == 0 {
+			log.Infof("Save state for slot %d", i)
 		}
 	}
 
 	// Update the split slot and root.
 	s.splitInfo = &splitSlotAndRoot{slot: finalizedSlot, root: finalizedRoot}
-	log.WithFields(logrus.Fields{
-		"slot": s.splitInfo.slot,
-		"root": hex.EncodeToString(bytesutil.Trunc(s.splitInfo.root[:])),
-	}).Info("Set hot and cold state split point")
 
 	// Update finalized state in memory.
 	fState, err := s.beaconDB.State(ctx, finalizedRoot)
