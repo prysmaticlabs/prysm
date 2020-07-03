d648abb768005afa723b431324fae8cbd033a55a
rauljordan <raul@prysmaticlabs.com>
2020-07-03 14:42:46 -0500

    doc

--- validator/keymanager/v2/direct/doc.go
+++ validator/keymanager/v2/direct/doc.go
@@ -1,11 +1,12 @@
-/* Package direct defines an implementation of an on-disk, EIP-2335 keystore.json
+/*
+Package direct defines an implementation of an on-disk, EIP-2335 keystore.json
 approach towards defining validator accounts in Prysm. A validating private key is
 encrypted using a passphrase and its resulting encrypted file is stored as a
 keystore.json file under a unique account namespace. This direct keymanager approach
 relies on storing account information on-disk, making it trivial to import, export and
 list all associated accounts for a user by
 
-EIP-2335 is a keystore format defined in for storing and defining encryption for BLS12-381
-private keys, utilized by eth2.
+EIP-2335 is a keystore format defined by https://eips.ethereum.org/EIPS/eip-2335 for
+storing and defining encryption for BLS12-381 private keys, utilized by eth2.
 */
 package direct
