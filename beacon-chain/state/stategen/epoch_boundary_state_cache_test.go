5e56891a03b4d2cb1b796692068ab317a34ed5c9
terence tsao <terence@prysmaticlabs.com>
2020-07-03 08:26:28 -0700

    Starting tests

---
+++ beacon-chain/state/stategen/epoch_boundary_state_cache_test.go
@@ -0,0 +1,92 @@
+package stategen
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/shared/testutil"
+)
+
+func TestEpochBoundaryStateCache_CanSave(t *testing.T) {
+	e := newBoundaryStateCache()
+	s := testutil.NewBeaconState()
+	if err := s.SetSlot(1); err != nil {
+		t.Fatal(err)
+	}
+	r := [32]byte{'a'}
+	if err := e.put(r, s); err != nil {
+		t.Fatal(err)
+	}
+
+	got, exists, err := e.getByRoot([32]byte{'b'})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Error("Should not exist")
+	}
+	if got != nil {
+		t.Error("Should not exist")
+	}
+
+	got, exists, err = e.getByRoot([32]byte{'a'})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Error("Should exist")
+	}
+	if !reflect.DeepEqual(got.state.InnerStateUnsafe(), s.InnerStateUnsafe()) {
+		t.Error("Should have the same state")
+	}
+
+	got, exists, err = e.getBySlot(2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Error("Should not exist")
+	}
+	if got != nil {
+		t.Error("Should not exist")
+	}
+
+	got, exists, err = e.getBySlot(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Error("Should exist")
+	}
+	if !reflect.DeepEqual(got.state.InnerStateUnsafe(), s.InnerStateUnsafe()) {
+		t.Error("Should have the same state")
+	}
+}
+
+func TestEpochBoundaryStateCache_CanTrim(t *testing.T) {
+	e := newBoundaryStateCache()
+	offSet := uint64(10)
+	for i := uint64(0); i < maxCacheSize+offSet; i++ {
+		s := testutil.NewBeaconState()
+		if err := s.SetSlot(i); err != nil {
+			t.Fatal(err)
+		}
+		r := [32]byte{byte(i)}
+		if err := e.put(r, s); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	if len(e.rootStateCache.ListKeys()) != int(maxCacheSize) {
+		t.Error("Did not trim to the correct amount")
+	}
+	for _, l := range e.rootStateCache.List() {
+		i, ok := l.(*rootStateInfo)
+		if !ok {
+			t.Fatal("Bad type assertion")
+		}
+		if i.state.Slot() < offSet {
+			t.Error("Did not trim the correct state")
+		}
+	}
+}
