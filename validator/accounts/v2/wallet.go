9d979de4edce1924d63b3b61b7f6ae48c4812d44
Raul Jordan <raul@prysmaticlabs.com>
2020-07-03 13:49:16 -0500

    Direct Keymanager: Implement Account Creation (#6466)
    * implementation using petname and keystore
    * writing new account to disk along with password
    * more logic for properly writing accounts
    * print out mnemonic
    * save deposit data rlp
    * write deposit tx and ssz deposit data to account path
    * wrap up account creation
    * fix prompt
    * generate deposit tx
    * direct account creation test
    * fix up formatting
    * lint
    * match formatting
    * more sustainable approach towards unmarshaling config file
    * resolve feedback
    * fix broken import
    * comprehensive tests for create account
    * tests pass
    * Merge branch 'master' into direct-keys
    * tidy
    * Merge branch 'direct-keys' of github.com:prysmaticlabs/prysm into direct-keys
    * Merge refs/heads/master into direct-keys
    * gaz
    * Merge branch 'direct-keys' of github.com:prysmaticlabs/prysm into direct-keys
    * nondeterministic names
    * comment
    * gaz
    * better error wrap
    * Merge refs/heads/master into direct-keys
    * docker deps
    * Merge branch 'direct-keys' of github.com:prysmaticlabs/prysm into direct-keys
    * Merge refs/heads/master into direct-keys
    * Merge refs/heads/master into direct-keys
    * Merge refs/heads/master into direct-keys
    * Merge refs/heads/master into direct-keys
    * Merge refs/heads/master into direct-keys
    * Merge refs/heads/master into direct-keys
    * Merge refs/heads/master into direct-keys
    * Merge refs/heads/master into direct-keys
    * Merge refs/heads/master into direct-keys
    * ivan feedback
    * Merge refs/heads/master into direct-keys
    * Update validator/accounts/v2/wallet.go
    
    Co-authored-by: Preston Van Loon <preston@prysmaticlabs.com>
    * fixed tests and comments
    * Merge refs/heads/master into direct-keys

--- validator/accounts/v2/wallet.go
+++ validator/accounts/v2/wallet.go
@@ -7,14 +7,20 @@ import (
 	"os"
 	"path"
 
+	petname "github.com/dustinkirkland/golang-petname"
 	"github.com/pkg/errors"
 	v2keymanager "github.com/prysmaticlabs/prysm/validator/keymanager/v2"
+	"github.com/sirupsen/logrus"
 )
 
 const (
 	keymanagerConfigFileName = "keymanageropts.json"
 	walletDefaultDirName     = ".prysm-wallet-v2"
 	passwordsDefaultDirName  = ".passwords"
+	passwordFileSuffix       = ".pass"
+	numAccountWords          = 3 // Number of words in account human-readable names.
+	accountFilePermissions   = os.O_CREATE | os.O_RDWR
+	directoryPermissions     = os.ModePerm
 )
 
 // WalletConfig for a wallet struct, containing important information
@@ -35,6 +41,10 @@ type Wallet struct {
 	keymanagerKind v2keymanager.Kind
 }
 
+func init() {
+	petname.NonDeterministicMode() // Set random account name generation.
+}
+
 // CreateWallet given a set of configuration options, will leverage
 // a keymanager to create and write a new wallet to disk for a Prysm validator.
 func CreateWallet(ctx context.Context, cfg *WalletConfig) (*Wallet, error) {
@@ -42,10 +52,10 @@ func CreateWallet(ctx context.Context, cfg *WalletConfig) (*Wallet, error) {
 		return nil, errors.New("wallet dir and passwords dir cannot be nil")
 	}
 	accountsPath := path.Join(cfg.WalletDir, cfg.KeymanagerKind.String())
-	if err := os.MkdirAll(accountsPath, os.ModePerm); err != nil {
+	if err := os.MkdirAll(accountsPath, directoryPermissions); err != nil {
 		return nil, errors.Wrap(err, "could not create wallet directory")
 	}
-	if err := os.MkdirAll(cfg.PasswordsDir, os.ModePerm); err != nil {
+	if err := os.MkdirAll(cfg.PasswordsDir, directoryPermissions); err != nil {
 		return nil, errors.Wrap(err, "could not create passwords directory")
 	}
 	w := &Wallet{
@@ -81,20 +91,65 @@ func (w *Wallet) KeymanagerKind() v2keymanager.Kind {
 	return w.keymanagerKind
 }
 
-// AccountsPath for the wallet.
-func (w *Wallet) AccountsPath() string {
+// AccountsDir for the wallet.
+func (w *Wallet) AccountsDir() string {
 	return w.accountsPath
 }
 
-// AccountPasswordsPath for the wallet's accounts.
-func (w *Wallet) AccountPasswordsPath() string {
-	return w.passwordsDir
+// WriteAccountToDisk creates an account directory under a unique namespace
+// within the wallet's path. It additionally writes the account's password to the
+// wallet's passwords directory. Returns the unique account name.
+func (w *Wallet) WriteAccountToDisk(ctx context.Context, password string) (string, error) {
+	accountName, err := w.generateAccountName()
+	if err != nil {
+		return "", errors.Wrap(err, "could not generate unique account name")
+	}
+	// Generate a directory for the new account name and
+	// write its associated password to disk.
+	accountPath := path.Join(w.accountsPath, accountName)
+	if err := os.MkdirAll(accountPath, directoryPermissions); err != nil {
+		return "", errors.Wrap(err, "could not create account directory")
+	}
+	if err := w.writePasswordToFile(accountName, password); err != nil {
+		return "", errors.Wrap(err, "could not write password to disk")
+	}
+	return accountName, nil
 }
 
-// WriteAccountToDisk writes an encoded account by its filename
-// within the wallet's directory.
-func (w *Wallet) WriteAccountToDisk(ctx context.Context, filename string, encoded []byte) error {
-	return errors.New("unimplemented")
+// WriteFileForAccount stores a unique file and its data under an account namespace
+// in the wallet's directory on-disk. Creates the file if it does not exist
+// and writes over it otherwise.
+func (w *Wallet) WriteFileForAccount(ctx context.Context, accountName string, fileName string, data []byte) error {
+	accountPath := path.Join(w.accountsPath, accountName)
+	exists, err := hasDir(accountPath)
+	if err != nil {
+		return errors.Wrapf(err, "could not check if account exists in directory: %s", w.accountsPath)
+	}
+	if !exists {
+		return errors.Wrapf(err, "account does not exist in wallet directory: %s", w.accountsPath)
+	}
+	filePath := path.Join(accountPath, fileName)
+	f, err := os.OpenFile(filePath, accountFilePermissions, directoryPermissions)
+	if err != nil {
+		return errors.Wrapf(err, "could not open file for account: %s", filePath)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.WithError(err).Error("Could not close file after writing")
+		}
+	}()
+	n, err := f.Write(data)
+	if err != nil {
+		return errors.Wrapf(err, "could not write file for account: %s", filePath)
+	}
+	if n != len(data) {
+		return fmt.Errorf("could only write %d/%d file bytes to disk", n, len(data))
+	}
+	log.WithFields(logrus.Fields{
+		"name": accountName,
+		"path": filePath,
+	}).Debug("Wrote new file for account")
+	return nil
 }
 
 // WriteKeymanagerConfigToDisk takes an encoded keymanager config file
@@ -111,7 +166,7 @@ func (w *Wallet) WriteKeymanagerConfigToDisk(ctx context.Context, encoded []byte
 	}
 	defer func() {
 		if err := f.Close(); err != nil {
-			log.Fatalf("Could not close keymanager opts file: %v", err)
+			log.WithError(err).Error("Could not close keymanager opts file")
 		}
 	}()
 	n, err := f.Write(encoded)
@@ -129,6 +184,45 @@ func (w *Wallet) WriteKeymanagerConfigToDisk(ctx context.Context, encoded []byte
 	return nil
 }
 
+// Writes the password file for an account namespace in the wallet's passwords directory.
+func (w *Wallet) writePasswordToFile(accountName string, password string) error {
+	passwordFilePath := path.Join(w.passwordsDir, accountName+passwordFileSuffix)
+	passwordFile, err := os.OpenFile(passwordFilePath, accountFilePermissions, directoryPermissions)
+	if err != nil {
+		return errors.Wrapf(err, "could not create password file in directory: %s", w.passwordsDir)
+	}
+	defer func() {
+		if err := passwordFile.Close(); err != nil {
+			log.WithError(err).Error("Could not close password file")
+		}
+	}()
+	n, err := passwordFile.WriteString(password)
+	if err != nil {
+		return errors.Wrap(err, "could not write account password to disk")
+	}
+	if n != len(password) {
+		return fmt.Errorf("could only write %d/%d password bytes to disk", n, len(password))
+	}
+	return nil
+}
+
+// Generates a human-readable name for an account. Checks for uniqueness in the accounts path.
+func (w *Wallet) generateAccountName() (string, error) {
+	var accountExists bool
+	var accountName string
+	for !accountExists {
+		accountName = petname.Generate(numAccountWords, "-" /* separator */)
+		exists, err := hasDir(path.Join(w.accountsPath, accountName))
+		if err != nil {
+			return "", errors.Wrapf(err, "could not check if account exists in dir: %s", w.accountsPath)
+		}
+		if !exists {
+			break
+		}
+	}
+	return accountName, nil
+}
+
 // Returns true if a file is not a directory and exists
 // at the specified path.
 func fileExists(filename string) bool {
@@ -137,4 +231,12 @@ func fileExists(filename string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+func hasDir(dirPath string) (bool, error) {
+	_, err := os.Stat(dirPath)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
