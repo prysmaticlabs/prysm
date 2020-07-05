019f67a83e8d52c15170813fdb6f73ff6de096a3
Nishant Das <nishdas93@gmail.com>
2020-07-05 18:59:12 +0800

    Update beacon-chain/core/state/transition.go
    Co-authored-by: terence tsao <terence@prysmaticlabs.com>

--- beacon-chain/core/state/transition.go
+++ beacon-chain/core/state/transition.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 
 	"github.com/prysmaticlabs/prysm/shared/bls"
-
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
