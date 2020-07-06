30f281d48380611ab9c5e8c5ecbe589c748b4fd7
dv8silencer <dv8silencer+github@gmail.com>
2020-07-05 22:23:16 -0500

    Improved the code to detect the last iteration of the range responses loop

--- validator/client/metrics_test.go
+++ validator/client/metrics_test.go
@@ -75,7 +75,7 @@ func TestUpdateLogAggregateStats(t *testing.T) {
 	var hook *logTest.Hook
 
 	for i, val := range responses {
-		if i == 2 { //check final log output
+		if i == len(responses)-1 { //check final log output
 			hook = logTest.NewGlobal()
 		}
 		v.UpdateLogAggregateStats(val, uint64(32*(i+1)))
