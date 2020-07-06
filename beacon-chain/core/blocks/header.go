9c0a666de2923e273b36da4177e35301d84206db
terence tsao <terence@prysmaticlabs.com>
2020-07-06 10:39:56 -0700

    Minor clena ups

--- beacon-chain/core/blocks/header.go
+++ beacon-chain/core/blocks/header.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"fmt"
 
-	"github.com/go-errors/errors"
+	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
