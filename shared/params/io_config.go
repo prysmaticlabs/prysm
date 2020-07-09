536286f5979442cce8b827e78f20c08480ff8d3a
Jim McDonald <Jim@mcdee.net>
2020-07-09 10:31:03 +0100

    Add separate permission for directories.

--- shared/params/io_config.go
+++ shared/params/io_config.go
@@ -4,11 +4,13 @@ import "os"
 
 // IoConfig defines the shared io parameters.
 type IoConfig struct {
-	ReadWritePermissions os.FileMode
+	ReadWritePermissions        os.FileMode
+	ReadWriteExecutePermissions os.FileMode
 }
 
 var defaultIoConfig = &IoConfig{
-	ReadWritePermissions: 0600, //-rw------- Read and Write permissions for user
+	ReadWritePermissions:        0600, //-rw------- Read and Write permissions for user
+	ReadWriteExecutePermissions: 0700, //-rwx------ Read Write and Execute (traverse) permissions for user
 }
 
 // BeaconIoConfig returns the current io config for
