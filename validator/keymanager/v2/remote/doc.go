61ce8afbe3a537d72a42b3814f42ddcef7f12d90
rauljordan <raul@prysmaticlabs.com>
2020-07-09 13:02:50 -0500

    finalize remote client doc

--- validator/keymanager/v2/remote/doc.go
+++ validator/keymanager/v2/remote/doc.go
@@ -6,35 +6,64 @@ keystore.json file under a unique, human-readable, account namespace. This direc
 relies on storing account information on-disk, making it trivial to import, export and
 list all associated accounts for a user.
 
-Package remote
+Package remote defines a keymanager implementation which connects to a remote signer
+server via gRPC. The connection is established via TLS using supplied paths to
+certificates and key files and allows for submitting remote signing requests for
+eth2 data structures as well as retrieving the available signing public keys from
+the remote server.
 
-The remote key manager connects to a walletd instance.  The options are:
-  - location This is the location to look for wallets.  If not supplied it will
-    use the standard (operating system-dependent) path.
-  - accounts This is a list of account specifiers.  An account specifier is of
-    the form <wallet name>/[account name],  where the account name can be a
-    regular expression.  If the account specifier is just <wallet name> all
-    accounts in that wallet will be used.  Multiple account specifiers can be
-    supplied if required.
-  - certificates This provides paths to certificates:
-    - ca_cert This is the path to the server's certificate authority certificate file
-    - client_cert This is the path to the client's certificate file
-    - client_key This is the path to the client's key file
+Remote sign requests are defined by the following protobuf schema:
 
-An sample keymanager options file (with annotations; these should be removed if
-using this as a template) is:
+ // SignRequest is a message type used by a keymanager
+ // as part of Prysm's accounts implementation.
+ message SignRequest {
+     // 48 byte public key corresponding to an associated private key
+     // being requested to sign data.
+     bytes public_key = 1;
 
-  {
-	"location":    "host.example.com:12345", // Connect to walletd at host.example.com on port 12345
-    "accounts":    ["Validators/Account.*"]  // Use all accounts in the 'Validators' wallet starting with 'Account'
-	"certificates": {
-	  "ca_cert": "/home/eth2/certs/ca.crt"         // Certificate file for the CA that signed the server's certificate
-	  "client_cert": "/home/eth2/certs/client.crt" // Certificate file for this client
-	  "client_key": "/home/eth2/certs/client.key"  // Key file for this client
-	}
-  }`
+     // Raw bytes data the client is requesting to sign.
+     bytes data = 2;
+
+     // Signature domain required for BLS signing.
+     bytes signature_domain = 3;
+
+     // The raw object being signed by the request.
+     oneof object {
+         ethereum.eth.v1alpha1.BeaconBlock block = 4;
+         ethereum.eth.v1alpha1.AttestationData attestation_data = 5;
+         ethereum.eth.v1alpha1.AggregateAttestationAndProof
+            aggregate_attestation_and_proof = 6;
+         ethereum.eth.v1alpha1.VoluntaryExit exit = 7;
+         uint64 slot = 8;
+     }
+ }
+
+Remote signing responses will contain a BLS12-381 signature along with the
+status of the signing response from the remote server, signifying the
+request either failed, was denied, or completed successfully.
+
+ message SignResponse {
+     enum Status {
+         UNKNOWN = 0;
+         SUCCEEDED = 1;
+         DENIED = 2;
+         FAILED = 3;
+     }
+
+     // BLS12-381 signature for the data specified in the request.
+     bytes signature = 1;
+ }
+
+The remote keymanager can be customized via a keymanageropts.json file
+which requires the following schema:
+
  {
-   direct_eip_version: string
+   "remote_address": "remoteserver.com:4000", // Remote gRPC server address.
+   "remote_cert": {
+     "crt_path": "/home/eth2/certs/client.crt", // Client certificate path.
+     "ca_crt_path": "/home/eth2/certs/ca.crt",  // Certificate authority cert path.
+     "key_path": "/home/eth2/certs/client.key", // Client key path.
+   }
  }
 */
 package remote
