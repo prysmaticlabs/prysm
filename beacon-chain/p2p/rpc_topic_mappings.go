33a4e68c140ae4abcedc10eeffa793a2c4d16925
nisdas <nishdas93@gmail.com>
2020-07-11 16:06:55 +0800

    add schema version change

--- beacon-chain/p2p/rpc_topic_mappings.go
+++ beacon-chain/p2p/rpc_topic_mappings.go
@@ -7,19 +7,22 @@ import (
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
+// Current schema version for our rpc protocol ID.
+const schemaVersionV1 = "/1"
+
 const (
 	// RPCStatusTopic defines the topic for the status rpc method.
-	RPCStatusTopic = "/eth2/beacon_chain/req/status/1"
+	RPCStatusTopic = "/eth2/beacon_chain/req/status" + schemaVersionV1
 	// RPCGoodByeTopic defines the topic for the goodbye rpc method.
-	RPCGoodByeTopic = "/eth2/beacon_chain/req/goodbye/1"
+	RPCGoodByeTopic = "/eth2/beacon_chain/req/goodbye" + schemaVersionV1
 	// RPCBlocksByRangeTopic defines the topic for the blocks by range rpc method.
-	RPCBlocksByRangeTopic = "/eth2/beacon_chain/req/beacon_blocks_by_range/1"
+	RPCBlocksByRangeTopic = "/eth2/beacon_chain/req/beacon_blocks_by_range" + schemaVersionV1
 	// RPCBlocksByRootTopic defines the topic for the blocks by root rpc method.
-	RPCBlocksByRootTopic = "/eth2/beacon_chain/req/beacon_blocks_by_root/1"
+	RPCBlocksByRootTopic = "/eth2/beacon_chain/req/beacon_blocks_by_root" + schemaVersionV1
 	// RPCPingTopic defines the topic for the ping rpc method.
-	RPCPingTopic = "/eth2/beacon_chain/req/ping/1"
+	RPCPingTopic = "/eth2/beacon_chain/req/ping" + schemaVersionV1
 	// RPCMetaDataTopic defines the topic for the metadata rpc method.
-	RPCMetaDataTopic = "/eth2/beacon_chain/req/metadata/1"
+	RPCMetaDataTopic = "/eth2/beacon_chain/req/metadata" + schemaVersionV1
 )
 
 // RPCTopicMappings map the base message type to the rpc request.
