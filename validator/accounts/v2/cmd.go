83be9a2fb1e1da9a1ea44f25b43ee687f51b0f4a
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-07-13 18:36:59 -0400

    Fix cmd

--- validator/accounts/v2/cmd.go
+++ validator/accounts/v2/cmd.go
@@ -1,7 +1,6 @@
 package v2
 
 import (
-	"github.com/prysmaticlabs/prysm/validator/flags"
 	"github.com/urfave/cli/v2"
 )
 
@@ -12,52 +11,10 @@ var Commands = &cli.Command{
 	Usage:    "defines commands for interacting with eth2 validator wallets (work in progress)",
 	Subcommands: []*cli.Command{
 		{
-			Name:     "accounts",
-			Category: "accounts",
-			Usage:    "defines commands for interacting with eth2 validator accounts (work in progress)",
-			Subcommands: []*cli.Command{
-				{
-					Name: "new",
-					Description: `creates a new validator account for eth2. If no account exists at the wallet path, creates a new wallet for a user based on
-specified input, capable of creating a direct, derived, or remote wallet.
-this command outputs a deposit data string which is required to become a validator in eth2.`,
-					Flags: []cli.Flag{
-						flags.WalletDirFlag,
-						flags.WalletPasswordsDirFlag,
-					},
-					Action: NewAccount,
-				},
-				{
-					Name:        "list",
-					Description: "Lists all validator accounts in a user's wallet directory",
-					Flags: []cli.Flag{
-						flags.WalletDirFlag,
-						flags.WalletPasswordsDirFlag,
-						flags.ShowDepositDataFlag,
-					},
-					Action: ListAccounts,
-				},
-				{
-					Name:        "export",
-					Description: `exports the account of a given directory into a zip of the provided output path. This zip can be used to later import the account to another directory`,
-					Flags: []cli.Flag{
-						flags.WalletDirFlag,
-						flags.WalletPasswordsDirFlag,
-						flags.BackupPathFlag,
-					},
-					Action: ExportAccount,
-				},
-				{
-					Name:        "import",
-					Description: `imports the accounts from a given zip file to the provided wallet path. This zip can be created using the export command`,
-					Flags: []cli.Flag{
-						flags.WalletDirFlag,
-						flags.WalletPasswordsDirFlag,
-						flags.BackupPathFlag,
-					},
-					Action: ImportAccount,
-				},
-			},
+			Name:        "accounts",
+			Category:    "accounts",
+			Usage:       "defines commands for interacting with eth2 validator accounts (work in progress)",
+			Subcommands: AccountCommands,
 		},
 	},
 }
