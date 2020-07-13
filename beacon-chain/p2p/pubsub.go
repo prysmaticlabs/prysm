be4dcaebe25e771522e1b5df5319a61dc50478bc
nisdas <nishdas93@gmail.com>
2020-07-13 10:51:15 +0800

    clean up

--- beacon-chain/p2p/pubsub.go
+++ beacon-chain/p2p/pubsub.go
@@ -2,8 +2,11 @@ package p2p
 
 import (
 	"context"
+	"encoding/base64"
 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	pubsub_pb "github.com/libp2p/go-libp2p-pubsub/pb"
+	"github.com/prysmaticlabs/prysm/shared/hashutil"
 )
 
 // JoinTopic will join PubSub topic, if not already joined.
@@ -47,4 +50,13 @@ func (s *Service) SubscribeToTopic(topic string, opts ...pubsub.SubOpt) (*pubsub
 		return nil, err
 	}
 	return topicHandle.Subscribe(opts...)
+}
+
+// Content addressable ID function.
+//
+// Loosely defined as Base64(sha2(data)) until a formal specification is determined.
+// Pending: https://github.com/ethereum/eth2.0-specs/issues/1528
+func msgIDFunction(pmsg *pubsub_pb.Message) string {
+	h := hashutil.FastSum256(pmsg.Data)
+	return base64.URLEncoding.EncodeToString(h[:])
 }
