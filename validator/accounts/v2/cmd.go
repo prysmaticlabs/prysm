cd9304423eff59ef33bcdb97cc17ccee9a78cf4f
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-07-13 18:17:45 -0400

    Change `validator accounts-v2` to `validator wallet-v2 accounts-v2

--- validator/accounts/v2/cmd.go
+++ validator/accounts/v2/cmd.go
@@ -7,50 +7,57 @@ import (
 
 // Commands for accounts-v2 for Prysm validators.
 var Commands = &cli.Command{
-	Name:     "accounts-v2",
-	Category: "accounts-v2",
-	Usage:    "defines commands for interacting with eth2 validator accounts (work in progress)",
+	Name:     "wallet-v2",
+	Category: "wallet-v2",
+	Usage:    "defines commands for interacting with eth2 validator wallets (work in progress)",
 	Subcommands: []*cli.Command{
 		{
-			Name: "new",
-			Description: `creates a new validator account for eth2. If no account exists at the wallet path, creates a new wallet for a user based on
+			Name:     "accounts-v2",
+			Category: "accounts-v2",
+			Usage:    "defines commands for interacting with eth2 validator accounts (work in progress)",
+			Subcommands: []*cli.Command{
+				{
+					Name: "new",
+					Description: `creates a new validator account for eth2. If no account exists at the wallet path, creates a new wallet for a user based on
 specified input, capable of creating a direct, derived, or remote wallet.
 this command outputs a deposit data string which is required to become a validator in eth2.`,
-			Flags: []cli.Flag{
-				flags.WalletDirFlag,
-				flags.WalletPasswordsDirFlag,
+					Flags: []cli.Flag{
+						flags.WalletDirFlag,
+						flags.WalletPasswordsDirFlag,
+					},
+					Action: NewAccount,
+				},
+				{
+					Name:        "list",
+					Description: "Lists all validator accounts in a user's wallet directory",
+					Flags: []cli.Flag{
+						flags.WalletDirFlag,
+						flags.WalletPasswordsDirFlag,
+						flags.ShowDepositDataFlag,
+					},
+					Action: ListAccounts,
+				},
+				{
+					Name:        "export",
+					Description: `exports the account of a given directory into a zip of the provided output path. This zip can be used to later import the account to another directory`,
+					Flags: []cli.Flag{
+						flags.WalletDirFlag,
+						flags.WalletPasswordsDirFlag,
+						flags.BackupPathFlag,
+					},
+					Action: ExportAccount,
+				},
+				{
+					Name:        "import",
+					Description: `imports the accounts from a given zip file to the provided wallet path. This zip can be created using the export command`,
+					Flags: []cli.Flag{
+						flags.WalletDirFlag,
+						flags.WalletPasswordsDirFlag,
+						flags.BackupPathFlag,
+					},
+					Action: ImportAccount,
+				},
 			},
-			Action: NewAccount,
-		},
-		{
-			Name:        "list",
-			Description: "Lists all validator accounts in a user's wallet directory",
-			Flags: []cli.Flag{
-				flags.WalletDirFlag,
-				flags.WalletPasswordsDirFlag,
-				flags.ShowDepositDataFlag,
-			},
-			Action: ListAccounts,
-		},
-		{
-			Name:        "export",
-			Description: `exports the account of a given directory into a zip of the provided output path. This zip can be used to later import the account to another directory`,
-			Flags: []cli.Flag{
-				flags.WalletDirFlag,
-				flags.WalletPasswordsDirFlag,
-				flags.BackupPathFlag,
-			},
-			Action: ExportAccount,
-		},
-		{
-			Name:        "import",
-			Description: `imports the accounts from a given zip file to the provided wallet path. This zip can be created using the export command`,
-			Flags: []cli.Flag{
-				flags.WalletDirFlag,
-				flags.WalletPasswordsDirFlag,
-				flags.BackupPathFlag,
-			},
-			Action: ImportAccount,
 		},
 	},
 }
