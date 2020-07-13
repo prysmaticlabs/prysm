ff213b5663f3f348ee87414458bee358fa96a38e
Victor Farazdagi <simple.square@gmail.com>
2020-07-13 19:58:09 +0300

    updates tests

--- beacon-chain/p2p/peers/score_bad_responses_test.go
+++ beacon-chain/p2p/peers/score_bad_responses_test.go
@@ -6,6 +6,8 @@ import (
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
 func TestPeerScorer_decayBadResponsesStats(t *testing.T) {
@@ -22,39 +24,40 @@ func TestPeerScorer_decayBadResponsesStats(t *testing.T) {
 	// Peer 1 has 0 bad responses.
 	pid1 := peer.ID("peer1")
 	scorer.AddPeer(pid1)
+	badResponsesCount, err := scorer.BadResponses(pid1)
+	require.NoError(t, err)
+	assert.Equal(t, 0, badResponsesCount)
+
 	// Peer 2 has 1 bad response.
 	pid2 := peer.ID("peer2")
 	scorer.AddPeer(pid2)
 	scorer.IncrementBadResponses(pid2)
+	badResponsesCount, err = scorer.BadResponses(pid2)
+	require.NoError(t, err)
+	assert.Equal(t, 1, badResponsesCount)
+
 	// Peer 3 has 2 bad response.
 	pid3 := peer.ID("peer3")
 	scorer.AddPeer(pid3)
 	scorer.IncrementBadResponses(pid3)
 	scorer.IncrementBadResponses(pid3)
+	badResponsesCount, err = scorer.BadResponses(pid3)
+	require.NoError(t, err)
+	assert.Equal(t, 2, badResponsesCount)
 
 	// Decay the values
 	scorer.decayBadResponsesStats()
 
 	// Ensure the new values are as expected
-	badResponses1, err := scorer.BadResponses(pid1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if badResponses1 != 0 {
-		t.Errorf("Unexpected bad responses for peer 1: expected 0, received %v", badResponses1)
-	}
-	badResponses2, err := scorer.BadResponses(pid2)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if badResponses2 != 0 {
-		t.Errorf("Unexpected bad responses for peer 2: expected 0, received %v", badResponses2)
-	}
-	badResponses3, err := scorer.BadResponses(pid3)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if badResponses3 != 1 {
-		t.Errorf("Unexpected bad responses for peer 3: expected 1, received %v", badResponses3)
-	}
+	badResponsesCount, err = scorer.BadResponses(pid1)
+	require.NoError(t, err)
+	assert.Equal(t, 0, badResponsesCount, "unexpected bad responses for pid1")
+
+	badResponsesCount, err = scorer.BadResponses(pid2)
+	require.NoError(t, err)
+	assert.Equal(t, 0, badResponsesCount, "unexpected bad responses for pid2")
+
+	badResponsesCount, err = scorer.BadResponses(pid3)
+	require.NoError(t, err)
+	assert.Equal(t, 1, badResponsesCount, "unexpected bad responses for pid3")
 }
