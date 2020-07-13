b2f53eecb6dd6ea062c0befc39de1d26d17791f4
rauljordan <raul@prysmaticlabs.com>
2020-07-13 14:10:21 -0500

    define wallet v2

--- validator/accounts/v2/cmd.go
+++ validator/accounts/v2/cmd.go
@@ -7,30 +7,41 @@ import (
 
 // Commands for accounts-v2 for Prysm validators.
 var Commands = &cli.Command{
-	Name:     "accounts-v2",
-	Category: "accounts-v2",
-	Usage:    "defines commands for interacting with eth2 validator accounts (work in progress)",
+	Name:     "wallet-v2",
+	Category: "wallet-v2",
+	Usage:    "defines commands for interacting with eth2 validator wallets (work in progress)",
 	Subcommands: []*cli.Command{
 		{
-			Name: "new",
-			Description: `creates a new validator account for eth2. If no account exists at the wallet path, creates a new wallet for a user based on
-specified input, capable of creating a direct, derived, or remote wallet.
-this command outputs a deposit data string which is required to become a validator in eth2.`,
-			Flags: []cli.Flag{
-				flags.WalletDirFlag,
-				flags.WalletPasswordsDirFlag,
-			},
-			Action: NewAccount,
+			Name:   "create",
+			Usage:  "creates a new wallet, either using remote credentials, derived HD functionality, or using directly-on-disk accounts",
+			Action: ListAccounts,
 		},
 		{
-			Name:        "list",
-			Description: "Lists all validator accounts in a user's wallet directory",
-			Flags: []cli.Flag{
-				flags.WalletDirFlag,
-				flags.WalletPasswordsDirFlag,
-				flags.ShowDepositDataFlag,
+			Name:  "accounts",
+			Usage: "defines commands for interacting with validator accounts (work in progress)",
+			Subcommands: []*cli.Command{
+				{
+					Name: "new",
+					Description: `creates a new validator account for eth2. If no account exists at the wallet path, creates a new wallet for a user based on
+specified input, capable of creating a direct, derived, or remote wallet.
+this command outputs a deposit data string which is required to become a validator in eth2.`,
+					Flags: []cli.Flag{
+						flags.WalletDirFlag,
+						flags.WalletPasswordsDirFlag,
+					},
+					Action: NewAccount,
+				},
+				{
+					Name:        "list",
+					Description: "Lists all validator accounts in a user's wallet directory",
+					Flags: []cli.Flag{
+						flags.WalletDirFlag,
+						flags.WalletPasswordsDirFlag,
+						flags.ShowDepositDataFlag,
+					},
+					Action: ListAccounts,
+				},
 			},
-			Action: ListAccounts,
 		},
 	},
 }
