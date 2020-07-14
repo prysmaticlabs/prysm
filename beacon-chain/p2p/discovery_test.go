563dd643a277374f52f4d773e139799fc2cb2e7e
Victor Farazdagi <simple.square@gmail.com>
2020-07-14 17:54:25 +0300

    applies assertion funcs to p2p/service tests

--- beacon-chain/p2p/discovery_test.go
+++ beacon-chain/p2p/discovery_test.go
@@ -19,6 +19,8 @@ import (
 	statefeed "github.com/prysmaticlabs/prysm/beacon-chain/core/feed/state"
 	"github.com/prysmaticlabs/prysm/shared/iputils"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
+	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	logTest "github.com/sirupsen/logrus/hooks/test"
 )
 
@@ -38,13 +40,9 @@ func createAddrAndPrivKey(t *testing.T) (net.IP, *ecdsa.PrivateKey) {
 	randNum := rand.Int()
 	tempPath := path.Join(temp, strconv.Itoa(randNum))
 	err = os.Mkdir(tempPath, 0700)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	pkey, err := privKey(&Config{DataDir: tempPath})
-	if err != nil {
-		t.Fatalf("Could not get private key: %v", err)
-	}
+	require.NoError(t, err, "Could not get private key")
 	return ipAddr, pkey
 }
 
@@ -57,18 +55,12 @@ func TestCreateListener(t *testing.T) {
 		cfg:                   &Config{UDPPort: uint(port)},
 	}
 	listener, err := s.createListener(ipAddr, pkey)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	defer listener.Close()
 
-	if !listener.Self().IP().Equal(ipAddr) {
-		t.Errorf("Ip address is not the expected type, wanted %s but got %s", ipAddr.String(), listener.Self().IP().String())
-	}
+	assert.Equal(t, true, listener.Self().IP().Equal(ipAddr), "IP address is not the expected type")
+	assert.Equal(t, port, listener.Self().UDP(), "Incorrect port number")
 
-	if port != listener.Self().UDP() {
-		t.Errorf("In correct port number, wanted %d but got %d", port, listener.Self().UDP())
-	}
 	pubkey := listener.Self().Pubkey()
 	XisSame := pkey.PublicKey.X.Cmp(pubkey.X) == 0
 	YisSame := pkey.PublicKey.Y.Cmp(pubkey.Y) == 0
@@ -89,9 +81,7 @@ func TestStartDiscV5_DiscoverAllPeers(t *testing.T) {
 		genesisValidatorsRoot: genesisValidatorsRoot,
 	}
 	bootListener, err := s.createListener(ipAddr, pkey)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	defer bootListener.Close()
 
 	bootNode := bootListener.Self()
@@ -110,9 +100,7 @@ func TestStartDiscV5_DiscoverAllPeers(t *testing.T) {
 			genesisValidatorsRoot: genesisValidatorsRoot,
 		}
 		listener, err := s.startDiscoveryV5(ipAddr, pkey)
-		if err != nil {
-			t.Errorf("Could not start discovery for node: %v", err)
-		}
+		assert.NoError(t, err, "Could not start discovery for node")
 		listeners = append(listeners, listener)
 	}
 	defer func() {
@@ -141,13 +129,9 @@ func TestMultiAddrsConversion_InvalidIPAddr(t *testing.T) {
 		genesisValidatorsRoot: []byte{'A'},
 	}
 	node, err := s.createLocalNode(pkey, addr, 0, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	multiAddr := convertToMultiAddr([]*enode.Node{node.Node()})
-	if len(multiAddr) != 0 {
-		t.Error("Invalid ip address converted successfully")
-	}
+	assert.Equal(t, 0, len(multiAddr), "Invalid ip address converted successfully")
 }
 
 func TestMultiAddrConversion_OK(t *testing.T) {
@@ -162,9 +146,7 @@ func TestMultiAddrConversion_OK(t *testing.T) {
 		genesisValidatorsRoot: []byte{'A'},
 	}
 	listener, err := s.createListener(ipAddr, pkey)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	defer listener.Close()
 
 	_ = convertToMultiAddr([]*enode.Node{listener.Self()})
@@ -201,9 +183,7 @@ func TestStaticPeering_PeersAreAdded(t *testing.T) {
 	cfg.StateNotifier = &mock.MockStateNotifier{}
 	cfg.NoDiscovery = true
 	s, err := NewService(cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	exitRoutine := make(chan bool)
 	go func() {
@@ -222,11 +202,7 @@ func TestStaticPeering_PeersAreAdded(t *testing.T) {
 	}
 	time.Sleep(4 * time.Second)
 	peers := s.host.Network().Peers()
-	if len(peers) != 5 {
-		t.Errorf("Not all peers added to peerstore, wanted %d but got %d", 5, len(peers))
-	}
-	if err := s.Stop(); err != nil {
-		t.Fatal(err)
-	}
+	assert.Equal(t, 5, len(peers), "Not all peers added to peerstore")
+	require.NoError(t, s.Stop())
 	exitRoutine <- true
 }
--- beacon-chain/p2p/fork_test.go
+++ beacon-chain/p2p/fork_test.go
@@ -18,6 +18,8 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/p2putils"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
+	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	"github.com/sirupsen/logrus"
 	logTest "github.com/sirupsen/logrus/hooks/test"
 )
@@ -33,9 +35,7 @@ func TestStartDiscv5_DifferentForkDigests(t *testing.T) {
 		genesisValidatorsRoot: genesisValidatorsRoot,
 	}
 	bootListener, err := s.createListener(ipAddr, pkey)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	defer bootListener.Close()
 
 	bootNode := bootListener.Self()
@@ -61,9 +61,7 @@ func TestStartDiscv5_DifferentForkDigests(t *testing.T) {
 			genesisValidatorsRoot: root,
 		}
 		listener, err := s.startDiscoveryV5(ipAddr, pkey)
-		if err != nil {
-			t.Errorf("Could not start discovery for node: %v", err)
-		}
+		assert.NoError(t, err, "Could not start discovery for node")
 		listeners = append(listeners, listener)
 	}
 	defer func() {
@@ -100,20 +98,14 @@ func TestStartDiscv5_DifferentForkDigests(t *testing.T) {
 	for _, n := range nodes {
 		if s.filterPeer(n) {
 			addr, err := convertToSingleMultiAddr(n)
-			if err != nil {
-				t.Fatal(err)
-			}
+			require.NoError(t, err)
 			addrs = append(addrs, addr)
 		}
 	}
 
 	// We should not have valid peers if the fork digest mismatched.
-	if len(addrs) != 0 {
-		t.Errorf("Expected 0 valid peers, got %d", len(addrs))
-	}
-	if err := s.Stop(); err != nil {
-		t.Fatal(err)
-	}
+	assert.Equal(t, 0, len(addrs), "Expected 0 valid peers")
+	require.NoError(t, s.Stop())
 }
 
 func TestStartDiscv5_SameForkDigests_DifferentNextForkData(t *testing.T) {
@@ -129,9 +121,7 @@ func TestStartDiscv5_SameForkDigests_DifferentNextForkData(t *testing.T) {
 		genesisValidatorsRoot: genesisValidatorsRoot,
 	}
 	bootListener, err := s.createListener(ipAddr, pkey)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	defer bootListener.Close()
 
 	bootNode := bootListener.Self()
@@ -161,9 +151,7 @@ func TestStartDiscv5_SameForkDigests_DifferentNextForkData(t *testing.T) {
 			genesisValidatorsRoot: genesisValidatorsRoot,
 		}
 		listener, err := s.startDiscoveryV5(ipAddr, pkey)
-		if err != nil {
-			t.Errorf("Could not start discovery for node: %v", err)
-		}
+		assert.NoError(t, err, "Could not start discovery for node")
 		listeners = append(listeners, listener)
 	}
 	defer func() {
@@ -189,9 +177,7 @@ func TestStartDiscv5_SameForkDigests_DifferentNextForkData(t *testing.T) {
 	cfg.TCPPort = 14001
 	cfg.MaxPeers = 30
 	s, err = NewService(cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	s.genesisTime = genesisTime
 	s.genesisValidatorsRoot = make([]byte, 32)
@@ -201,9 +187,7 @@ func TestStartDiscv5_SameForkDigests_DifferentNextForkData(t *testing.T) {
 	for _, n := range nodes {
 		if s.filterPeer(n) {
 			addr, err := convertToSingleMultiAddr(n)
-			if err != nil {
-				t.Fatal(err)
-			}
+			require.NoError(t, err)
 			addrs = append(addrs, addr)
 		}
 	}
@@ -212,9 +196,7 @@ func TestStartDiscv5_SameForkDigests_DifferentNextForkData(t *testing.T) {
 	}
 
 	testutil.AssertLogsContain(t, hook, "Peer matches fork digest but has different next fork epoch")
-	if err := s.Stop(); err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, s.Stop())
 }
 
 func TestDiscv5_AddRetrieveForkEntryENR(t *testing.T) {
@@ -233,83 +215,57 @@ func TestDiscv5_AddRetrieveForkEntryENR(t *testing.T) {
 	genesisTime := time.Now()
 	genesisValidatorsRoot := make([]byte, 32)
 	digest, err := p2putils.CreateForkDigest(genesisTime, make([]byte, 32))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	enrForkID := &pb.ENRForkID{
 		CurrentForkDigest: digest[:],
 		NextForkVersion:   nextForkVersion,
 		NextForkEpoch:     nextForkEpoch,
 	}
 	enc, err := enrForkID.MarshalSSZ()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	forkEntry := enr.WithEntry(eth2ENRKey, enc)
 	// In epoch 1 of current time, the fork version should be
 	// {0, 0, 0, 1} according to the configuration override above.
 	temp := testutil.TempDir()
 	randNum := rand.Int()
 	tempPath := path.Join(temp, strconv.Itoa(randNum))
-	if err := os.Mkdir(tempPath, 0700); err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, os.Mkdir(tempPath, 0700))
 	pkey, err := privKey(&Config{DataDir: tempPath})
-	if err != nil {
-		t.Fatalf("Could not get private key: %v", err)
-	}
+	require.NoError(t, err, "Could not get private key")
 	db, err := enode.OpenDB("")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	localNode := enode.NewLocalNode(db, pkey)
 	localNode.Set(forkEntry)
 
 	want, err := helpers.ComputeForkDigest([]byte{0, 0, 0, 0}, genesisValidatorsRoot)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	resp, err := retrieveForkEntry(localNode.Node().Record())
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if !bytes.Equal(resp.CurrentForkDigest, want[:]) {
 		t.Errorf("Wanted fork digest: %v, received %v", want, resp.CurrentForkDigest)
 	}
 	if !bytes.Equal(resp.NextForkVersion[:], nextForkVersion) {
 		t.Errorf("Wanted next fork version: %v, received %v", nextForkVersion, resp.NextForkVersion)
 	}
-	if resp.NextForkEpoch != nextForkEpoch {
-		t.Errorf("Wanted next for epoch: %d, received: %d", nextForkEpoch, resp.NextForkEpoch)
-	}
+	assert.Equal(t, nextForkEpoch, resp.NextForkEpoch, "Unexpected next fork epoch")
 }
 
 func TestAddForkEntry_Genesis(t *testing.T) {
 	temp := testutil.TempDir()
 	randNum := rand.Int()
 	tempPath := path.Join(temp, strconv.Itoa(randNum))
-	if err := os.Mkdir(tempPath, 0700); err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, os.Mkdir(tempPath, 0700))
 	pkey, err := privKey(&Config{DataDir: tempPath})
-	if err != nil {
-		t.Fatalf("Could not get private key: %v", err)
-	}
+	require.NoError(t, err, "Could not get private key")
 	db, err := enode.OpenDB("")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	localNode := enode.NewLocalNode(db, pkey)
 	localNode, err = addForkEntry(localNode, time.Now().Add(10*time.Second), []byte{'A', 'B', 'C', 'D'})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	forkEntry, err := retrieveForkEntry(localNode.Node().Record())
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	if !bytes.Equal(forkEntry.NextForkVersion, params.BeaconConfig().GenesisForkVersion) {
 		t.Errorf("Wanted Next Fork Version to be equal to genesis fork version, instead got %#x", forkEntry.NextForkVersion)
 	}
