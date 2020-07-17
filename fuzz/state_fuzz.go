47bb94b372b89371a99bc6e76ffd104e4511aae0
Preston Van Loon <preston@prysmaticlabs.com>
2020-07-17 14:04:00 -0700

    fix and lint

--- fuzz/state_fuzz.go
+++ fuzz/state_fuzz.go
@@ -2,6 +2,7 @@ package fuzz
 
 import pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 
+// BeaconStateFuzz --
 func BeaconStateFuzz(input []byte) int {
 	st := &pb.BeaconState{}
 	if err := st.UnmarshalSSZ(input); err != nil {
