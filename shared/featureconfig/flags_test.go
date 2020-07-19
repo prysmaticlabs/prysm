0544dd1f8ea45b6e34fde286846994010dd6b03c
terence tsao <terence@prysmaticlabs.com>
2020-07-19 14:08:29 -0700

    Applies assertion funcs to shared tests (#6643)
    * cmd tests
    * deposit util tests
    * feature config tests
    * hashutil tests
    * htr util tests
    * interop tests
    * ip util tests

--- shared/featureconfig/flags_test.go
+++ shared/featureconfig/flags_test.go
@@ -4,17 +4,15 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
 )
 
 func TestDeprecatedFlags(t *testing.T) {
 	for _, f := range deprecatedFlags {
 		fv := reflect.ValueOf(f)
 		field := reflect.Indirect(fv).FieldByName("Hidden")
-		if !field.IsValid() || !field.Bool() {
-			t.Errorf("%s must be hidden when deprecated.", f.Names()[0])
-		}
-		if !strings.Contains(reflect.Indirect(fv).FieldByName("Usage").String(), "DEPRECATED. DO NOT USE.") {
-			t.Errorf("Usage for %s must contain \"DEPRECATED. DO NOT USE.\"", f.Names()[0])
-		}
+		assert.Equal(t, false, !field.IsValid() || !field.Bool())
+		assert.Equal(t, false, !strings.Contains(reflect.Indirect(fv).FieldByName("Usage").String(), "DEPRECATED. DO NOT USE."))
 	}
 }
