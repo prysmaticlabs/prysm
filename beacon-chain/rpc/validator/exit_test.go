10d32756384f83a510ec8315751e5ca18cf168f0
Victor Farazdagi <simple.square@gmail.com>
2020-07-20 05:15:51 +0300

    Applies assertion funcs to rpc tests (#6641)
    * applies assertion funcs to beacon-chain/rpc tests
    * Merge branch 'master' into rpc-apply-testutils-assertions
    * applies assertion funcs to beacon-chain/rpc/beacon tests
    * Merge branch 'master' into rpc-apply-testutils-assertions
    * applies assertion funcs to beacon-chain/rpc/debug tests
    * applies assertion funcs to beacon-chain/rpc/validator tests
    * Merge branch 'master' into rpc-apply-testutils-assertions
    * Merge refs/heads/master into rpc-apply-testutils-assertions

--- beacon-chain/rpc/validator/exit_test.go
+++ beacon-chain/rpc/validator/exit_test.go
@@ -19,6 +19,8 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
+	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
 func TestSub(t *testing.T) {
@@ -26,21 +28,13 @@ func TestSub(t *testing.T) {
 	ctx := context.Background()
 	testutil.ResetCache()
 	deposits, keys, err := testutil.DeterministicDepositsAndKeys(params.BeaconConfig().MinGenesisActiveValidatorCount)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	beaconState, err := state.GenesisBeaconState(deposits, 0, &ethpb.Eth1Data{BlockHash: make([]byte, 32)})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	block := testutil.NewBeaconBlock()
-	if err := db.SaveBlock(ctx, block); err != nil {
-		t.Fatalf("Could not save genesis block: %v", err)
-	}
+	require.NoError(t, db.SaveBlock(ctx, block), "Could not save genesis block")
 	genesisRoot, err := stateutil.BlockRoot(block.Block)
-	if err != nil {
-		t.Fatalf("Could not get signing root %v", err)
-	}
+	require.NoError(t, err, "Could not get signing root")
 
 	// Set genesis time to be 100 epochs ago.
 	genesisTime := time.Now().Add(time.Duration(-100*int64(params.BeaconConfig().SecondsPerSlot*params.BeaconConfig().SlotsPerEpoch)) * time.Second)
@@ -71,19 +65,13 @@ func TestSub(t *testing.T) {
 		},
 	}
 	domain, err := helpers.Domain(beaconState.Fork(), epoch, params.BeaconConfig().DomainVoluntaryExit, beaconState.GenesisValidatorRoot())
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	sigRoot, err := helpers.ComputeSigningRoot(req.Exit, domain)
-	if err != nil {
-		t.Fatalf("Could not compute signing root: %v", err)
-	}
+	require.NoError(t, err, "Could not compute signing root")
 	req.Signature = keys[0].Sign(sigRoot[:]).Marshal()
 
 	_, err = server.ProposeExit(context.Background(), req)
-	if err != nil {
-		t.Fatalf("Unexpected error: %v", err)
-	}
+	require.NoError(t, err)
 
 	// Ensure the state notification was broadcast.
 	notificationFound := false
@@ -93,15 +81,9 @@ func TestSub(t *testing.T) {
 			if event.Type == opfeed.ExitReceived {
 				notificationFound = true
 				data, ok := event.Data.(*opfeed.ExitReceivedData)
-				if !ok {
-					t.Error("Entity is not of type *opfeed.ExitReceivedData")
-				}
-				if epoch != data.Exit.Exit.Epoch {
-					t.Errorf("Unexpected state feed epoch: expected %v, found %v", epoch, data.Exit.Exit.Epoch)
-				}
-				if validatorIndex != data.Exit.Exit.ValidatorIndex {
-					t.Errorf("Unexpected state feed validator index: expected %v, found %v", validatorIndex, data.Exit.Exit.ValidatorIndex)
-				}
+				assert.Equal(t, true, ok, "Entity is not of type *opfeed.ExitReceivedData")
+				assert.Equal(t, epoch, data.Exit.Exit.Epoch, "Unexpected state feed epoch")
+				assert.Equal(t, validatorIndex, data.Exit.Exit.ValidatorIndex, "Unexpected state feed validator index")
 			}
 		case <-opSub.Err():
 			t.Error("Subscription to state notifier failed")
@@ -115,21 +97,13 @@ func TestProposeExit_NoPanic(t *testing.T) {
 	ctx := context.Background()
 	testutil.ResetCache()
 	deposits, keys, err := testutil.DeterministicDepositsAndKeys(params.BeaconConfig().MinGenesisActiveValidatorCount)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	beaconState, err := state.GenesisBeaconState(deposits, 0, &ethpb.Eth1Data{BlockHash: make([]byte, 32)})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	block := testutil.NewBeaconBlock()
-	if err := db.SaveBlock(ctx, block); err != nil {
-		t.Fatalf("Could not save genesis block: %v", err)
-	}
+	require.NoError(t, db.SaveBlock(ctx, block), "Could not save genesis block")
 	genesisRoot, err := stateutil.BlockRoot(block.Block)
-	if err != nil {
-		t.Fatalf("Could not get signing root %v", err)
-	}
+	require.NoError(t, err, "Could not get signing root")
 
 	// Set genesis time to be 100 epochs ago.
 	genesisTime := time.Now().Add(time.Duration(-100*int64(params.BeaconConfig().SecondsPerSlot*params.BeaconConfig().SlotsPerEpoch)) * time.Second)
@@ -152,9 +126,7 @@ func TestProposeExit_NoPanic(t *testing.T) {
 
 	req := &ethpb.SignedVoluntaryExit{}
 	_, err = server.ProposeExit(context.Background(), req)
-	if err == nil {
-		t.Fatal("Expected error for no exit existing")
-	}
+	require.ErrorContains(t, "voluntary exit does not exist", err, "Expected error for no exit existing")
 
 	// Send the request, expect a result on the state feed.
 	epoch := uint64(2048)
@@ -167,28 +139,18 @@ func TestProposeExit_NoPanic(t *testing.T) {
 	}
 
 	_, err = server.ProposeExit(context.Background(), req)
-	if err == nil {
-		t.Fatal("Expected error for no signature exists")
-	}
+	require.ErrorContains(t, "invalid signature provided", err, "Expected error for no signature exists")
 	req.Signature = bytesutil.FromBytes48([48]byte{})
 
 	_, err = server.ProposeExit(context.Background(), req)
-	if err == nil {
-		t.Fatal("Expected error for invalid signature length")
-	}
+	require.ErrorContains(t, "invalid signature provided", err, "Expected error for invalid signature length")
 
 	domain, err := helpers.Domain(beaconState.Fork(), epoch, params.BeaconConfig().DomainVoluntaryExit, beaconState.GenesisValidatorRoot())
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	sigRoot, err := helpers.ComputeSigningRoot(req.Exit, domain)
-	if err != nil {
-		t.Fatalf("Could not compute signing root: %v", err)
-	}
+	require.NoError(t, err, "Could not compute signing root")
 	req.Signature = keys[0].Sign(sigRoot[:]).Marshal()
 
 	_, err = server.ProposeExit(context.Background(), req)
-	if err != nil {
-		t.Fatalf("Unexpected error: %v", err)
-	}
+	require.NoError(t, err)
 }
