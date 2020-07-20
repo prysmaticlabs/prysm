f8263da2f6dd8d5896cbefbb1c1ca81b0b403e63
rauljordan <raul@prysmaticlabs.com>
2020-07-20 15:18:06 -0500

    test interface conformity

--- validator/keymanager/v2/types_test.go
+++ validator/keymanager/v2/types_test.go
@@ -2,7 +2,13 @@ package v2_test
 
 import (
 	v2keymanager "github.com/prysmaticlabs/prysm/validator/keymanager/v2"
+	"github.com/prysmaticlabs/prysm/validator/keymanager/v2/derived"
 	"github.com/prysmaticlabs/prysm/validator/keymanager/v2/direct"
+	"github.com/prysmaticlabs/prysm/validator/keymanager/v2/remote"
 )
 
-var _ = v2keymanager.IKeymanager(&direct.Keymanager{})
+var (
+	_ = v2keymanager.IKeymanager(&direct.Keymanager{})
+	_ = v2keymanager.IKeymanager(&derived.Keymanager{})
+	_ = v2keymanager.IKeymanager(&remote.Keymanager{})
+)
