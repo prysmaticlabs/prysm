6338fb8b1722c14bdd7c0bd7a0c0a920621d01bf
terence tsao <terence@prysmaticlabs.com>
2020-07-21 10:41:39 -0700

    Update keystore

--- shared/keystore/key_test.go
+++ shared/keystore/key_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pborman/uuid"
 	"github.com/prysmaticlabs/prysm/shared/bls"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
 func TestMarshalAndUnmarshal(t *testing.T) {
@@ -20,9 +21,7 @@ func TestMarshalAndUnmarshal(t *testing.T) {
 		PublicKey: blsKey.PublicKey(),
 	}
 	marshalledObject, err := key.MarshalJSON()
-	if err != nil {
-		t.Fatalf("unable to marshall key %v", err)
-	}
+	require.NoError(t, err)
 	newKey := &Key{
 		ID:        []byte{},
 		SecretKey: blsKey,
@@ -30,13 +29,8 @@ func TestMarshalAndUnmarshal(t *testing.T) {
 	}
 
 	err = newKey.UnmarshalJSON(marshalledObject)
-	if err != nil {
-		t.Fatalf("unable to unmarshal object %v", err)
-	}
-
-	if !bytes.Equal(newKey.ID, testID) {
-		t.Fatalf("retrieved id not the same as pre serialized id: %v ", newKey.ID)
-	}
+	require.NoError(t, err)
+	require.Equal(t, true, bytes.Equal(newKey.ID, testID))
 }
 
 func TestStoreRandomKey(t *testing.T) {
@@ -47,33 +41,21 @@ func TestStoreRandomKey(t *testing.T) {
 		scryptN:     LightScryptN,
 		scryptP:     LightScryptP,
 	}
-
-	if err := storeNewRandomKey(ks, "password"); err != nil {
-		t.Fatalf("storage of random key unsuccessful %v", err)
-	}
+	require.NoError(t, storeNewRandomKey(ks, "password"))
 }
 
 func TestNewKeyFromBLS(t *testing.T) {
 	b := []byte("hi")
 	b32 := bytesutil.ToBytes32(b)
 	blskey, err := bls.SecretKeyFromBytes(b32[:])
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	key, err := NewKeyFromBLS(blskey)
-	if err != nil {
-		t.Fatalf("could not get new key from bls %v", err)
-	}
+	require.NoError(t, err)
 
 	expected := blskey.Marshal()
-	if !bytes.Equal(expected, key.SecretKey.Marshal()) {
-		t.Fatalf("secret key is not of the expected value %d", key.SecretKey.Marshal())
-	}
-
+	require.Equal(t, true, bytes.Equal(expected, key.SecretKey.Marshal()))
 	_, err = NewKey()
-	if err != nil {
-		t.Fatalf("random key unable to be generated: %v", err)
-	}
+	require.NoError(t, err)
 }
 
 func TestWriteFile(t *testing.T) {
@@ -83,16 +65,9 @@ func TestWriteFile(t *testing.T) {
 	testKeystore := []byte{'t', 'e', 's', 't'}
 
 	err := writeKeyFile(tempDir, testKeystore)
-	if err != nil {
-		t.Fatalf("unable to write file %v", err)
-	}
+	require.NoError(t, err)
 
 	keystore, err := ioutil.ReadFile(tempDir)
-	if err != nil {
-		t.Fatalf("unable to retrieve file %v", err)
-	}
-
-	if !bytes.Equal(keystore, testKeystore) {
-		t.Fatalf("retrieved keystore is not the same %v", keystore)
-	}
+	require.NoError(t, err)
+	require.Equal(t, true, bytes.Equal(keystore, testKeystore))
 }
--- shared/keystore/keystore_test.go
+++ shared/keystore/keystore_test.go
@@ -13,6 +13,8 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/bls"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
+	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
 func TestStoreAndGetKey(t *testing.T) {
@@ -25,22 +27,12 @@ func TestStoreAndGetKey(t *testing.T) {
 	}
 
 	key, err := NewKey()
-	if err != nil {
-		t.Fatalf("key generation failed %v", err)
-	}
-
-	if err := ks.StoreKey(tempDir, key, "password"); err != nil {
-		t.Fatalf("unable to store key %v", err)
-	}
+	require.NoError(t, err)
+	require.NoError(t, ks.StoreKey(tempDir, key, "password"))
 
 	decryptedKey, err := ks.GetKey(tempDir, "password")
-	if err != nil {
-		t.Fatalf("unable to get key %v", err)
-	}
-
-	if !bytes.Equal(decryptedKey.SecretKey.Marshal(), key.SecretKey.Marshal()) {
-		t.Fatalf("retrieved secret keys are not equal %v , %v", decryptedKey.SecretKey.Marshal(), key.SecretKey.Marshal())
-	}
+	require.NoError(t, err)
+	require.Equal(t, true, bytes.Equal(decryptedKey.SecretKey.Marshal(), key.SecretKey.Marshal()))
 }
 
 func TestStoreAndGetKeys(t *testing.T) {
@@ -53,29 +45,15 @@ func TestStoreAndGetKeys(t *testing.T) {
 	}
 
 	key, err := NewKey()
-	if err != nil {
-		t.Fatalf("key generation failed %v", err)
-	}
-
-	if err := ks.StoreKey(tempDir+"/test-1", key, "password"); err != nil {
-		t.Fatalf("unable to store key %v", err)
-	}
+	require.NoError(t, err)
+	require.NoError(t, ks.StoreKey(tempDir+"/test-1", key, "password"))
 	key2, err := NewKey()
-	if err != nil {
-		t.Fatalf("key generation failed %v", err)
-	}
-	if err := ks.StoreKey(tempDir+"/test-2", key2, "password"); err != nil {
-		t.Fatalf("unable to store key %v", err)
-	}
+	require.NoError(t, err)
+	require.NoError(t, ks.StoreKey(tempDir+"/test-2", key, "password"))
 	decryptedKeys, err := ks.GetKeys(tempDir, "test", "password", false)
-	if err != nil {
-		t.Fatalf("unable to get key %v", err)
-	}
+	require.NoError(t, err)
 	for _, s := range decryptedKeys {
-		if !bytes.Equal(s.SecretKey.Marshal(), key.SecretKey.Marshal()) && !bytes.Equal(s.SecretKey.Marshal(), key2.SecretKey.Marshal()) {
-			t.Fatalf("retrieved secret keys are not equal %v ", s.SecretKey.Marshal())
-		}
-
+		require.Equal(t, true, bytes.Equal(s.SecretKey.Marshal(), key.SecretKey.Marshal()) && !bytes.Equal(s.SecretKey.Marshal(), key2.SecretKey.Marshal()))
 	}
 }
 
@@ -86,9 +64,7 @@ func TestEncryptDecryptKey(t *testing.T) {
 	password := "test"
 
 	pk, err := bls.SecretKeyFromBytes(b32[:])
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 	key := &Key{
 		ID:        newID,
 		SecretKey: pk,
@@ -96,23 +72,13 @@ func TestEncryptDecryptKey(t *testing.T) {
 	}
 
 	keyJSON, err := EncryptKey(key, password, LightScryptN, LightScryptP)
-	if err != nil {
-		t.Fatalf("unable to encrypt key %v", err)
-	}
+	require.NoError(t, err)
 
 	decryptedKey, err := DecryptKey(keyJSON, password)
-	if err != nil {
-		t.Fatalf("unable to decrypt keystore %v", err)
-	}
-
-	if !bytes.Equal(decryptedKey.ID, newID) {
-		t.Fatalf("decrypted key's uuid doesn't match %v", decryptedKey.ID)
-	}
-
+	require.NoError(t, err)
+	require.Equal(t, true, bytes.Equal(decryptedKey.ID, newID))
 	expected := pk.Marshal()
-	if !bytes.Equal(decryptedKey.SecretKey.Marshal(), expected) {
-		t.Fatalf("decrypted key's value is not equal %v", decryptedKey.SecretKey.Marshal())
-	}
+	require.Equal(t, true, bytes.Equal(decryptedKey.SecretKey.Marshal(), expected))
 }
 
 func TestGetSymlinkedKeys(t *testing.T) {
@@ -124,38 +90,21 @@ func TestGetSymlinkedKeys(t *testing.T) {
 	}
 
 	key, err := NewKey()
-	if err != nil {
-		t.Fatalf("key generation failed %v", err)
-	}
-
-	if err := ks.StoreKey(tempDir+"/files/test-1", key, "password"); err != nil {
-		t.Fatalf("unable to store key %v", err)
-	}
-
-	if err := os.Symlink(tempDir+"/files/test-1", tempDir+"/test-1"); err != nil {
-		t.Fatalf("unable to create symlink: %v", err)
-	}
-
+	require.NoError(t, err)
+	require.NoError(t, ks.StoreKey(tempDir+"/files/test-1", key, "password"))
+	require.NoError(t, os.Symlink(tempDir+"/files/test-1", tempDir+"/test-1"))
 	decryptedKeys, err := ks.GetKeys(tempDir, "test", "password", false)
-	if err != nil {
-		t.Fatalf("unable to get key %v", err)
-	}
-	if len(decryptedKeys) != 1 {
-		t.Errorf("unexpected number of keys returned, want: %d, got: %d", 1, len(decryptedKeys))
-	}
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(decryptedKeys))
 	for _, s := range decryptedKeys {
-		if !bytes.Equal(s.SecretKey.Marshal(), key.SecretKey.Marshal()) {
-			t.Fatalf("retrieved secret keys are not equal %v ", s.SecretKey.Marshal())
-		}
+		require.Equal(t, true, bytes.Equal(s.SecretKey.Marshal(), key.SecretKey.Marshal()))
 	}
 }
 
 // setupTempKeystoreDir creates temporary directory for storing keystore files.
 func setupTempKeystoreDir(t *testing.T) (string, func()) {
 	randPath, err := rand.Int(rand.Reader, big.NewInt(1000000))
-	if err != nil {
-		t.Fatalf("could not generate random file path: %v", err)
-	}
+	require.NoError(t, err)
 	tempDir := path.Join(testutil.TempDir(), fmt.Sprintf("%d", randPath), "keystore")
 
 	return tempDir, func() {
