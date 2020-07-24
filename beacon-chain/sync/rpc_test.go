cbf51b1b044c70cdf0818d79f45e49bd1a1b4388
Nishant Das <nishdas93@gmail.com>
2020-07-24 20:20:43 +0800

    Update beacon-chain/sync/rpc_test.go

--- beacon-chain/sync/rpc_test.go
+++ beacon-chain/sync/rpc_test.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
-
 	libp2pcore "github.com/libp2p/go-libp2p-core"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
