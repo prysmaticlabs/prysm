19c488790de92fe2cebfcf4b2d6badb200835dfb
nisdas <nishdas93@gmail.com>
2020-07-29 20:44:16 +0800

    fix

--- beacon-chain/p2p/parameter_test.go
+++ beacon-chain/p2p/parameter_test.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -24,36 +25,44 @@ const (
 	gossipSubFanoutTTL = 60000000000 // TTL for fanout maps for topics we are not subscribed to but have published to, in nano seconds
 
 	// heartbeat interval
-	gossipSubHeartbeatInterval = 1 * time.Second // frequency of heartbeat, seconds
+	gossipSubHeartbeatInterval = 700 * time.Millisecond // frequency of heartbeat, milliseconds
 
 	// misc
 	randomSubD = 6 // random gossip target
 )
 
 func TestOverlayParameters(t *testing.T) {
+	setPubSubParameters()
 	assert.Equal(t, gossipSubD, pubsub.GossipSubD, "gossipSubD")
 	assert.Equal(t, gossipSubDlo, pubsub.GossipSubDlo, "gossipSubDlo")
 	assert.Equal(t, gossipSubDhi, pubsub.GossipSubDhi, "gossipSubDhi")
 }
 
 func TestGossipParameters(t *testing.T) {
+	setPubSubParameters()
 	assert.Equal(t, gossipSubMcacheLen, pubsub.GossipSubHistoryLength, "gossipSubMcacheLen")
 	assert.Equal(t, gossipSubMcacheGossip, pubsub.GossipSubHistoryGossip, "gossipSubMcacheGossip")
-	assert.Equal(t, gossipSubSeenTtl, (params.BeaconConfig().SlotsPerEpoch*params.BeaconConfig().SecondsPerSlot)/uint64(pubsub.GossipSubHeartbeatInterval.Seconds()), "gossipSubSeenTtl")
+	val := (params.BeaconConfig().SlotsPerEpoch * params.BeaconConfig().SecondsPerSlot * 1000) /
+		uint64(pubsub.GossipSubHeartbeatInterval.Milliseconds())
+	roundedUp := math.Round(float64(val) / 10)
+	assert.Equal(t, gossipSubSeenTtl, int(roundedUp)*10, "gossipSubSeenTtl")
 }
 
 func TestFanoutParameters(t *testing.T) {
+	setPubSubParameters()
 	if pubsub.GossipSubFanoutTTL != gossipSubFanoutTTL {
 		t.Errorf("gossipSubFanoutTTL, wanted: %d, got: %d", gossipSubFanoutTTL, pubsub.GossipSubFanoutTTL)
 	}
 }
 
 func TestHeartbeatParameters(t *testing.T) {
+	setPubSubParameters()
 	if pubsub.GossipSubHeartbeatInterval != gossipSubHeartbeatInterval {
 		t.Errorf("gossipSubHeartbeatInterval, wanted: %d, got: %d", gossipSubHeartbeatInterval, pubsub.GossipSubHeartbeatInterval)
 	}
 }
 
 func TestMiscParameters(t *testing.T) {
+	setPubSubParameters()
 	assert.Equal(t, randomSubD, pubsub.RandomSubD, "randomSubD")
 }
