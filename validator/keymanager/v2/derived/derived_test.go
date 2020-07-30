8920c223653658cc30c51590f7f8721e3033e2c6
rauljordan <raul@prysmaticlabs.com>
2020-07-30 16:13:12 -0500

    tests pass

--- validator/keymanager/v2/derived/derived_test.go
+++ validator/keymanager/v2/derived/derived_test.go
@@ -2,7 +2,6 @@ package derived
 
 import (
 	"context"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -18,9 +17,9 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	mock "github.com/prysmaticlabs/prysm/validator/accounts/v2/testing"
-	v2keymanager "github.com/prysmaticlabs/prysm/validator/keymanager/v2"
 	logTest "github.com/sirupsen/logrus/hooks/test"
 	"github.com/tyler-smith/go-bip39"
+	util "github.com/wealdtech/go-eth2-util"
 	keystorev4 "github.com/wealdtech/go-eth2-wallet-encryptor-keystorev4"
 )
 
@@ -77,41 +76,6 @@ func TestDerivedKeymanager_CreateAccount(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, "0", accountName)
 
-	// Ensure the keystore file was written to the wallet
-	// and ensure we can decrypt it using the EIP-2335 standard.
-	validatingAccount0 := fmt.Sprintf(ValidatingKeyDerivationPathTemplate, 0)
-	encodedKeystore, ok := wallet.Files[validatingAccount0][KeystoreFilePattern]
-	require.Equal(t, ok, true, fmt.Sprintf("Expected to have stored %s in wallet", KeystoreFilePattern))
-	keystoreFile := &v2keymanager.Keystore{}
-	require.NoError(t, json.Unmarshal(encodedKeystore, keystoreFile))
-
-	// We extract the validator signing private key from the keystore
-	// by utilizing the password and initialize a new BLS secret key from
-	// its raw bytes.
-	decryptor := keystorev4.New()
-	rawValidatingKey, err := decryptor.Decrypt(keystoreFile.Crypto, password)
-	require.NoError(t, err, "Could not decrypt validator signing key")
-
-	validatingKey, err := bls.SecretKeyFromBytes(rawValidatingKey)
-	require.NoError(t, err, "Could not instantiate bls secret key from bytes")
-
-	// Ensure the keystore file was written to the wallet
-	// and ensure we can decrypt it using the EIP-2335 standard.
-	withdrawalAccount0 := fmt.Sprintf(WithdrawalKeyDerivationPathTemplate, 0)
-	encodedKeystore, ok = wallet.Files[withdrawalAccount0][KeystoreFilePattern]
-	require.Equal(t, ok, true, fmt.Sprintf("Expected to have stored %s in wallet", KeystoreFilePattern))
-	keystoreFile = &v2keymanager.Keystore{}
-	require.NoError(t, json.Unmarshal(encodedKeystore, keystoreFile))
-
-	// We extract the validator signing private key from the keystore
-	// by utilizing the password and initialize a new BLS secret key from
-	// its raw bytes.
-	rawWithdrawalKey, err := decryptor.Decrypt(keystoreFile.Crypto, password)
-	require.NoError(t, err, "Could not decrypt validator withdrawal key")
-
-	withdrawalKey, err := bls.SecretKeyFromBytes(rawWithdrawalKey)
-	require.NoError(t, err, "Could not instantiate bls secret key from bytes")
-
 	// Assert the new value for next account increased and also
 	// check the config file was updated on disk with this new value.
 	assert.Equal(t, uint64(1), dr.seedCfg.NextAccount, "Wrong value for next account")
@@ -128,8 +92,6 @@ func TestDerivedKeymanager_CreateAccount(t *testing.T) {
 
 	// Ensure the new account information is displayed to stdout.
 	testutil.AssertLogsContain(t, hook, "Successfully created new validator account")
-	testutil.AssertLogsContain(t, hook, fmt.Sprintf("%#x", validatingKey.PublicKey().Marshal()))
-	testutil.AssertLogsContain(t, hook, fmt.Sprintf("%#x", withdrawalKey.PublicKey().Marshal()))
 }
 
 func TestDerivedKeymanager_FetchValidatingPublicKeys(t *testing.T) {
@@ -156,13 +118,9 @@ func TestDerivedKeymanager_FetchValidatingPublicKeys(t *testing.T) {
 		accountName, err = dr.CreateAccount(ctx, false /*logAccountInfo*/)
 		require.NoError(t, err)
 		validatingKeyPath := fmt.Sprintf(ValidatingKeyDerivationPathTemplate, i)
-		enc, err := wallet.ReadFileAtPath(ctx, validatingKeyPath, KeystoreFilePattern)
+		validatingKey, err := util.PrivateKeyFromSeedAndPath(dr.seed, validatingKeyPath)
 		require.NoError(t, err)
-		keystore := &v2keymanager.Keystore{}
-		require.NoError(t, json.Unmarshal(enc, keystore))
-		pubKey, err := hex.DecodeString(keystore.Pubkey)
-		require.NoError(t, err)
-		wantedPublicKeys[i] = bytesutil.ToBytes48(pubKey)
+		wantedPublicKeys[i] = bytesutil.ToBytes48(validatingKey.PublicKey().Marshal())
 	}
 	assert.Equal(t, fmt.Sprintf("%d", numAccounts-1), accountName)
 
