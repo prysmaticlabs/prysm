3f983e2a961e7694f03615925be4ffa38831e4eb
Preston Van Loon <preston@prysmaticlabs.com>
2020-07-31 13:17:39 -0700

    Accounts V2: Allow for user to specify single file rather than directory for keystore import (#6813)
    * Fix public key formatting
    * Accounts V2: Allow for user to specify single file rather than directory
    * Merge branch 'master' into fix-pubkey-formatting
    * Merge refs/heads/master into fix-pubkey-formatting

--- validator/accounts/v2/accounts_import_test.go
+++ validator/accounts/v2/accounts_import_test.go
@@ -79,7 +79,59 @@ func TestImport_Noninteractive(t *testing.T) {
 	assert.Equal(t, 2, len(keys))
 }
 
-func createKeystore(t *testing.T, path string) {
+func TestImport_Noninteractive_Filepath(t *testing.T) {
+	walletDir, passwordsDir, passwordFilePath := setupWalletAndPasswordsDir(t)
+	randPath, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	require.NoError(t, err, "Could not generate random file path")
+	keysDir := filepath.Join(testutil.TempDir(), fmt.Sprintf("/%d", randPath), "keysDir")
+	require.NoError(t, os.MkdirAll(keysDir, os.ModePerm))
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(keysDir), "Failed to remove directory")
+	})
+
+	cliCtx := setupWalletCtx(t, &testWalletConfig{
+		walletDir:           walletDir,
+		passwordsDir:        passwordsDir,
+		keysDir:             createKeystore(t, keysDir), // Using direct filepath to the new keystore.
+		keymanagerKind:      v2keymanager.Direct,
+		walletPasswordFile:  passwordFilePath,
+		accountPasswordFile: passwordFilePath,
+	})
+	wallet, err := NewWallet(cliCtx, v2keymanager.Direct)
+	require.NoError(t, err)
+	require.NoError(t, wallet.SaveWallet())
+	ctx := context.Background()
+	keymanagerCfg := direct.DefaultConfig()
+	keymanagerCfg.AccountPasswordsDirectory = passwordsDir
+	encodedCfg, err := direct.MarshalConfigFile(ctx, keymanagerCfg)
+	require.NoError(t, err)
+	require.NoError(t, wallet.WriteKeymanagerConfigToDisk(ctx, encodedCfg))
+	keymanager, err := direct.NewKeymanager(
+		ctx,
+		wallet,
+		keymanagerCfg,
+	)
+	require.NoError(t, err)
+
+	// Make sure there are no accounts at the start.
+	accounts, err := keymanager.ValidatingAccountNames()
+	require.NoError(t, err)
+	assert.Equal(t, len(accounts), 0)
+
+	require.NoError(t, ImportAccount(cliCtx))
+
+	wallet, err = OpenWallet(cliCtx)
+	require.NoError(t, err)
+	km, err := wallet.InitializeKeymanager(ctx, true)
+	require.NoError(t, err)
+	keys, err := km.FetchValidatingPublicKeys(ctx)
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, len(keys))
+}
+
+// Returns the fullPath to the newly created keystore file.
+func createKeystore(t *testing.T, path string) string {
 	validatingKey := bls.RandKey()
 	encryptor := keystorev4.New()
 	cryptoFields, err := encryptor.Encrypt(validatingKey.Marshal(), password)
@@ -99,4 +151,5 @@ func createKeystore(t *testing.T, path string) {
 	createdAt := roughtime.Now().Unix()
 	fullPath := filepath.Join(path, fmt.Sprintf(direct.KeystoreFileNameFormat, createdAt))
 	require.NoError(t, ioutil.WriteFile(fullPath, encoded, os.ModePerm))
+	return fullPath
 }
