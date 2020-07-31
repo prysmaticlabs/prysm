b96180afbc24e09336c164492245d7b9e23b047d
Victor Farazdagi <simple.square@gmail.com>
2020-07-31 17:02:46 +0300

    get rid of penalties + counters for requested/returned blocks

--- beacon-chain/p2p/peers/score_block_providers.go
+++ beacon-chain/p2p/peers/score_block_providers.go
@@ -15,22 +15,10 @@ const (
 	// DefaultBlockProviderStartScore defines initial score before any stats update takes place.
 	// By setting this to positive value, peers are given a chance to be used for the first time.
 	DefaultBlockProviderStartScore = 0.1
-	// DefaultBlockProviderReturnedBlocksWeight is a default weight of a returned/requested ratio
-	// on an overall score.
-	DefaultBlockProviderReturnedBlocksWeight = 0.2
-	// DefaultSlowReturnedBlocksPenalty is a default penalty for non-responsive peers.
-	// Total penalty is calculated as ((requested - returned)/requested) * penalty.
-	DefaultSlowReturnedBlocksPenalty = -0.1
-	// DefaultBlockProviderProcessedBlocksWeight is a default weight of a processed/requested ratio
-	// on an overall score.
-	DefaultBlockProviderProcessedBlocksWeight = 0.0
-	// DefaultSlowProcessedBlocksPenalty is a default penalty for peers feeding garbage.
-	// Total penalty is calculated as ((returned - processed)/returned) * penalty.
-	DefaultSlowProcessedBlocksPenalty = 0.0
-	// DefaultBlockProviderDecayInterval defines how often block provider's stats should be decayed.
-	DefaultBlockProviderDecayInterval = 1 * time.Minute
-	// DefaultBlockProviderDecay specifies a decay factor (as a left-over percentage of the original value).
-	DefaultBlockProviderDecay = 0.95
+	// DefaultBlockProviderProcessedBatchWeight is a default weight of a processed batch of blocks.
+	DefaultBlockProviderProcessedBatchWeight = 0.02
+	// DefaultBlockProviderDecayInterval defines how often to call a decaying routine.
+	DefaultBlockProviderDecayInterval = 5 * time.Minute
 )
 
 // BlockProviderScorer represents block provider scoring service.
@@ -38,6 +26,8 @@ type BlockProviderScorer struct {
 	ctx    context.Context
 	config *BlockProviderScorerConfig
 	store  *peerDataStore
+	// totalProcessedBlocks is a sum of all processed blocks, across all peers.
+	totalProcessedBlocks uint64
 }
 
 // BlockProviderScorerConfig holds configuration parameters for block providers scoring service.
@@ -46,22 +36,12 @@ type BlockProviderScorerConfig struct {
 	// opportunity to be selected for block fetching (allows new peers to start participating,
 	// when there are already scored peers).
 	StartScore float64
-	// ReturnedBlocksWeight defines weight of a returned/requested ratio in overall score.
-	ReturnedBlocksWeight float64
-	// SlowReturnedBlocksPenalty defines a penalty applied to score, if blocks were requested,
-	// but none have been returned yet (to distinguish between non-responsive peers and peers that
-	// haven't been requested any blocks yet).
-	SlowReturnedBlocksPenalty float64
-	// ProcessedBlocksWeight defines weight of a processed/requested ratio in overall score.
-	ProcessedBlocksWeight float64
-	// SlowProcessedBlocksPenalty defines a penalty applied to score, if blocks have been
-	// requested and returned, but none have been processed yet. Allows distinguishing between
-	// peers that haven't yet returned anything and peers that returned garbage.
-	SlowProcessedBlocksPenalty float64
+	// ProcessedBatchWeight defines a reward for a single processed batch of blocks.
+	ProcessedBatchWeight float64
 	// DecayInterval defines how often requested/returned/processed stats should be decayed.
 	DecayInterval time.Duration
-	// Decay specifies the factor (must be < 1.0) by which block provider's stats is decayed.
-	Decay float64
+	// Decay specifies number of blocks subtracted from stats on each decay step.
+	Decay uint64
 }
 
 // newBlockProviderScorer creates block provider scoring service.
@@ -78,23 +58,14 @@ func newBlockProviderScorer(
 	if scorer.config.StartScore == 0.0 {
 		scorer.config.StartScore = DefaultBlockProviderStartScore
 	}
-	if scorer.config.ReturnedBlocksWeight == 0.0 {
-		scorer.config.ReturnedBlocksWeight = DefaultBlockProviderReturnedBlocksWeight
-	}
-	if scorer.config.SlowReturnedBlocksPenalty == 0.0 {
-		scorer.config.SlowReturnedBlocksPenalty = DefaultSlowReturnedBlocksPenalty
-	}
-	if scorer.config.ProcessedBlocksWeight == 0.0 {
-		scorer.config.ProcessedBlocksWeight = DefaultBlockProviderProcessedBlocksWeight
-	}
-	if scorer.config.SlowProcessedBlocksPenalty == 0.0 {
-		scorer.config.SlowProcessedBlocksPenalty = DefaultSlowProcessedBlocksPenalty
+	if scorer.config.ProcessedBatchWeight == 0.0 {
+		scorer.config.ProcessedBatchWeight = DefaultBlockProviderProcessedBatchWeight
 	}
 	if scorer.config.DecayInterval == 0 {
 		scorer.config.DecayInterval = DefaultBlockProviderDecayInterval
 	}
-	if scorer.config.Decay == 0.0 {
-		scorer.config.Decay = DefaultBlockProviderDecay
+	if scorer.config.Decay == 0 {
+		scorer.config.Decay = uint64(flags.Get().BlockBatchLimit)
 	}
 	return scorer
 }
@@ -110,26 +81,9 @@ func (s *BlockProviderScorer) Score(pid peer.ID) float64 {
 func (s *BlockProviderScorer) score(pid peer.ID) float64 {
 	score := s.Params().StartScore
 	peerData, ok := s.store.peers[pid]
-	if ok && peerData.requestedBlocks > 0 {
-		// Score returned/requested ratio.
-		returnedBlocksScore := float64(peerData.returnedBlocks) / float64(peerData.requestedBlocks)
-		returnedBlocksScore = returnedBlocksScore * s.config.ReturnedBlocksWeight
-		score += returnedBlocksScore
-
-		if peerData.requestedBlocks > peerData.returnedBlocks {
-			factor := float64(peerData.requestedBlocks-peerData.returnedBlocks) / float64(peerData.requestedBlocks)
-			score += s.config.SlowReturnedBlocksPenalty * factor
-		}
-
-		// Score processed/requested ratio.
-		processedBlocksScore := float64(peerData.processedBlocks) / float64(peerData.requestedBlocks)
-		processedBlocksScore = processedBlocksScore * s.config.ProcessedBlocksWeight
-		score += processedBlocksScore
-
-		if peerData.returnedBlocks > 0 && peerData.returnedBlocks > peerData.processedBlocks {
-			factor := float64(peerData.returnedBlocks-peerData.processedBlocks) / float64(peerData.returnedBlocks)
-			score += s.config.SlowProcessedBlocksPenalty * factor
-		}
+	if ok && peerData.processedBlocks > 0 {
+		processedBatches := float64(peerData.processedBlocks / uint64(flags.Get().BlockBatchLimit))
+		score += processedBatches * s.config.ProcessedBatchWeight
 	} else {
 		// Boost peers that have never been selected.
 		return s.MaxScore()
@@ -142,58 +96,6 @@ func (s *BlockProviderScorer) Params() *BlockProviderScorerConfig {
 	return s.config
 }
 
-// IncrementRequestedBlocks increments the number of blocks that have been requested from peer.
-func (s *BlockProviderScorer) IncrementRequestedBlocks(pid peer.ID, cnt uint64) {
-	s.store.Lock()
-	defer s.store.Unlock()
-
-	if _, ok := s.store.peers[pid]; !ok {
-		s.store.peers[pid] = &peerData{}
-	}
-	s.store.peers[pid].requestedBlocks += cnt
-}
-
-// RequestedBlocks returns number of blocks requested from a peer.
-func (s *BlockProviderScorer) RequestedBlocks(pid peer.ID) uint64 {
-	s.store.RLock()
-	defer s.store.RUnlock()
-	return s.requestedBlocks(pid)
-}
-
-// requestedBlocks is a lock-free version of RequestedBlocks.
-func (s *BlockProviderScorer) requestedBlocks(pid peer.ID) uint64 {
-	if peerData, ok := s.store.peers[pid]; ok {
-		return peerData.requestedBlocks
-	}
-	return 0
-}
-
-// IncrementReturnedBlocks increments the number of blocks that have been returned by peer.
-func (s *BlockProviderScorer) IncrementReturnedBlocks(pid peer.ID, cnt uint64) {
-	s.store.Lock()
-	defer s.store.Unlock()
-
-	if _, ok := s.store.peers[pid]; !ok {
-		s.store.peers[pid] = &peerData{}
-	}
-	s.store.peers[pid].returnedBlocks += cnt
-}
-
-// ReturnedBlocks returns number of blocks returned by a peer.
-func (s *BlockProviderScorer) ReturnedBlocks(pid peer.ID) uint64 {
-	s.store.RLock()
-	defer s.store.RUnlock()
-	return s.returnedBlocks(pid)
-}
-
-// returnedBlocks is a lock-free version of ReturnedBlocks.
-func (s *BlockProviderScorer) returnedBlocks(pid peer.ID) uint64 {
-	if peerData, ok := s.store.peers[pid]; ok {
-		return peerData.returnedBlocks
-	}
-	return 0
-}
-
 // IncrementProcessedBlocks increments the number of blocks that have been successfully processed.
 func (s *BlockProviderScorer) IncrementProcessedBlocks(pid peer.ID, cnt uint64) {
 	s.store.Lock()
@@ -203,6 +105,7 @@ func (s *BlockProviderScorer) IncrementProcessedBlocks(pid peer.ID, cnt uint64) 
 		s.store.peers[pid] = &peerData{}
 	}
 	s.store.peers[pid].processedBlocks += cnt
+	s.totalProcessedBlocks += cnt
 }
 
 // ProcessedBlocks returns number of peer returned blocks that are successfully processed.
@@ -228,16 +131,10 @@ func (s *BlockProviderScorer) Decay() {
 	defer s.store.Unlock()
 
 	for _, peerData := range s.store.peers {
-		peerData.requestedBlocks = uint64(math.Round(float64(peerData.requestedBlocks) * s.config.Decay))
-		// Once requested blocks stats drops to the half of batch size, reset stats.
-		if peerData.requestedBlocks < uint64(flags.Get().BlockBatchLimit/2) {
-			peerData.requestedBlocks = 0
-			peerData.returnedBlocks = 0
-			peerData.processedBlocks = 0
-			continue
+		if peerData.processedBlocks > s.config.Decay {
+			peerData.processedBlocks -= s.config.Decay
+			s.totalProcessedBlocks -= s.config.Decay
 		}
-		peerData.returnedBlocks = uint64(math.Round(float64(peerData.returnedBlocks) * s.config.Decay))
-		peerData.processedBlocks = uint64(math.Round(float64(peerData.processedBlocks) * s.config.Decay))
 	}
 }
 
@@ -266,13 +163,12 @@ func (s *BlockProviderScorer) BlockProviderScorePretty(pid peer.ID) string {
 	s.store.Lock()
 	defer s.store.Unlock()
 	score := s.score(pid)
-	return fmt.Sprintf("[%0.2f%%, raw: %v,  req: %d, ret: %d, proc: %d]",
-		(score/s.MaxScore())*100, score,
-		s.requestedBlocks(pid), s.returnedBlocks(pid), s.processedBlocks(pid))
+	return fmt.Sprintf("[%0.2f%%, raw: %v,  blocks: %d]", (score/s.MaxScore())*100, score, s.processedBlocks(pid))
 }
 
 // MaxScore exposes maximum score attainable by peers.
 func (s *BlockProviderScorer) MaxScore() float64 {
-	score := s.Params().StartScore + s.config.ReturnedBlocksWeight + s.config.ProcessedBlocksWeight
+	totalProcessedBatches := float64(s.totalProcessedBlocks / uint64(flags.Get().BlockBatchLimit))
+	score := s.Params().StartScore + totalProcessedBatches*s.config.ProcessedBatchWeight
 	return math.Round(score*ScoreRoundingFactor) / ScoreRoundingFactor
 }
