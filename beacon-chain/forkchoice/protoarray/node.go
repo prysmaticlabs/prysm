567573365756d88e938d3b7a2fe5f2434547e7f4
terence tsao <terence@prysmaticlabs.com>
2020-08-07 16:42:12 -0700

    Protoarray: lint

--- beacon-chain/forkchoice/protoarray/node.go
+++ beacon-chain/forkchoice/protoarray/node.go
@@ -10,37 +10,37 @@ func (n *Node) Root() [32]byte {
 	return n.root
 }
 
-// parent of the fork choice node.
+// Parent of the fork choice node.
 func (n *Node) Parent() uint64 {
 	return n.parent
 }
 
-// justifiedEpoch of the fork choice node.
+// JustifiedEpoch of the fork choice node.
 func (n *Node) JustifiedEpoch() uint64 {
 	return n.justifiedEpoch
 }
 
-// finalizedEpoch of the fork choice node.
+// FinalizedEpoch of the fork choice node.
 func (n *Node) FinalizedEpoch() uint64 {
 	return n.finalizedEpoch
 }
 
-// weight of the fork choice node.
+// Weight of the fork choice node.
 func (n *Node) Weight() uint64 {
 	return n.weight
 }
 
-// bestChild of the fork choice node.
+// BestChild of the fork choice node.
 func (n *Node) BestChild() uint64 {
 	return n.bestChild
 }
 
-// bestDescendant of the fork choice node.
+// BestDescendant of the fork choice node.
 func (n *Node) BestDescendant() uint64 {
 	return n.bestDescendant
 }
 
-// graffiti of the fork choice node.
+// Graffiti of the fork choice node.
 func (n *Node) Graffiti() [32]byte {
 	return n.graffiti
 }
