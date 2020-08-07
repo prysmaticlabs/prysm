0856b229bc8e53048a0e7e499d15c8753dc08613
Preston Van Loon <preston@prysmaticlabs.com>
2020-08-07 11:14:18 -0700

    WIP

--- fuzz/block_fuzz.go
+++ fuzz/block_fuzz.go
@@ -11,10 +11,17 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	pubsub_pb "github.com/libp2p/go-libp2p-pubsub/pb"
+	"github.com/prysmaticlabs/go-ssz"
+
+	"github.com/prysmaticlabs/prysm/beacon-chain/blockchain"
 	"github.com/prysmaticlabs/prysm/beacon-chain/blockchain/testing"
 	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
+	"github.com/prysmaticlabs/prysm/beacon-chain/forkchoice/protoarray"
+	"github.com/prysmaticlabs/prysm/beacon-chain/operations/attestations"
+	"github.com/prysmaticlabs/prysm/beacon-chain/operations/slashings"
+	"github.com/prysmaticlabs/prysm/beacon-chain/operations/voluntaryexits"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p"
 	p2pt "github.com/prysmaticlabs/prysm/beacon-chain/p2p/testing"
 	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
@@ -23,6 +30,8 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/rand"
+	"github.com/prysmaticlabs/prysm/shared/testutil"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -56,6 +65,29 @@ func init() {
 	}
 }
 
+func setupDB() {
+	if err := db1.ClearDB(); err != nil {
+		_ = err
+	}
+
+	ctx := context.Background()
+	s := testutil.NewBeaconState()
+	b := testutil.NewBeaconBlock()
+	if err := db1.SaveBlock(ctx, b); err != nil {
+		panic(err)
+	}
+	br, err := ssz.HashTreeRoot(b)
+	if err != nil {
+		panic(err)
+	}
+	if err := db1.SaveState(ctx, s, br); err != nil {
+		panic(err)
+	}
+	if err := db1.SaveGenesisBlockRoot(ctx, br); err != nil {
+		panic(err)
+	}
+}
+
 type fakeChecker struct{}
 
 func (fakeChecker) Syncing() bool {
@@ -81,33 +113,66 @@ func BeaconFuzzBlock(b []byte) {
 		return
 	}
 
-	if err := db1.ClearDB(); err != nil {
-		_ = err
-	}
+	setupDB()
 
 	p2p := p2pt.NewFuzzTestP2P()
+	sgen := stategen.New(db1, ssc)
+	sn := &testing.MockStateNotifier{}
+	bn := &testing.MockBlockNotifier{}
+	an := &testing.MockOperationNotifier{}
+	ap := attestations.NewPool()
+	ep := voluntaryexits.NewPool()
+	sp := slashings.NewPool()
+	ops, err := attestations.NewService(context.Background(), &attestations.Config{Pool: ap})
+	if err != nil {
+		panic(err)
+	}
+
+	chain, err := blockchain.NewService(context.Background(), &blockchain.Config{
+		ChainStartFetcher: nil,
+		BeaconDB:          db1,
+		DepositCache:      nil,
+		AttPool:           ap,
+		ExitPool:          ep,
+		SlashingPool:      sp,
+		P2p:               p2p,
+		StateNotifier:     sn,
+		ForkChoiceStore:   protoarray.New(0, 0, [32]byte{}),
+		OpsService:        ops,
+		StateGen:          sgen,
+	})
+	if err != nil {
+		panic(err)
+	}
+	chain.Start()
+	//defer func() {
+	//	if err := chain.Stop(); err != nil {
+	//		_ = err
+	//	}
+	//}()
 
 	s := sync.NewRegularSyncFuzz(&sync.Config{
 		DB:  db1,
 		P2P: p2p,
-		Chain: &testing.ChainService{
-			FinalizedCheckPoint:         st.FinalizedCheckpoint(),
-			CurrentJustifiedCheckPoint:  st.CurrentJustifiedCheckpoint(),
-			PreviousJustifiedCheckPoint: st.PreviousJustifiedCheckpoint(),
-			State:                       st,
-			Genesis:                     st.GenesisUnixTime(),
-			Fork:                        st.Fork(),
-			ETH1Data:                    st.Eth1Data(),
-		},
+		Chain: chain,
+		//Chain: &testing.ChainService{
+		//	FinalizedCheckPoint:         st.FinalizedCheckpoint(),
+		//	CurrentJustifiedCheckPoint:  st.CurrentJustifiedCheckpoint(),
+		//	PreviousJustifiedCheckPoint: st.PreviousJustifiedCheckpoint(),
+		//	State:                       st,
+		//	Genesis:                     st.GenesisUnixTime(),
+		//	Fork:                        st.Fork(),
+		//	ETH1Data:                    st.Eth1Data(),
+		//},
 		InitialSync:         fakeChecker{},
-		StateNotifier:       &testing.MockStateNotifier{},
-		BlockNotifier:       &testing.MockBlockNotifier{},
-		AttestationNotifier: &testing.MockOperationNotifier{},
-		AttPool:             nil,
-		ExitPool:            nil,
-		SlashingPool:        nil,
+		StateNotifier:       sn,
+		BlockNotifier:       bn,
+		AttestationNotifier: an,
+		AttPool:             ap,
+		ExitPool:            ep,
+		SlashingPool:        sp,
 		StateSummaryCache:   ssc,
-		StateGen:            stategen.New(db1, ssc),
+		StateGen:            sgen,
 	})
 
 	if err := s.InitCaches(); err != nil {
