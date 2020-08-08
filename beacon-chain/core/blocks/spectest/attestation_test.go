bcaa2a01074f0f222c0bd2981f6bd321d7e2dddb
Victor Farazdagi <simple.square@gmail.com>
2020-08-08 03:26:47 +0300

    beacon-chain/core/blocks/spectest tests updated

--- beacon-chain/core/blocks/spectest/attestation_test.go
+++ beacon-chain/core/blocks/spectest/attestation_test.go
@@ -8,25 +8,20 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
 	"github.com/prysmaticlabs/prysm/shared/params/spectest"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
 func runAttestationTest(t *testing.T, config string) {
-	if err := spectest.SetConfig(t, config); err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, spectest.SetConfig(t, config))
 
 	testFolders, testsFolderPath := testutil.TestFolders(t, config, "operations/attestation/pyspec_tests")
 	for _, folder := range testFolders {
 		t.Run(folder.Name(), func(t *testing.T) {
 			folderPath := path.Join(testsFolderPath, folder.Name())
 			attestationFile, err := testutil.BazelFileBytes(folderPath, "attestation.ssz")
-			if err != nil {
-				t.Fatal(err)
-			}
+			require.NoError(t, err)
 			att := &ethpb.Attestation{}
-			if err := att.UnmarshalSSZ(attestationFile); err != nil {
-				t.Fatalf("Failed to unmarshal: %v", err)
-			}
+			require.NoError(t, att.UnmarshalSSZ(attestationFile), "Failed to unmarshal")
 
 			body := &ethpb.BeaconBlockBody{Attestations: []*ethpb.Attestation{att}}
 			testutil.RunBlockOperationTest(t, folderPath, body, blocks.ProcessAttestations)
