20d263c9c1ed5fdf1af0b3b4ded0d92cdb039b99
rauljordan <raul@prysmaticlabs.com>
2020-08-10 11:45:16 -0500

    fix shared test

--- shared/fileutil/fileutil_test.go
+++ shared/fileutil/fileutil_test.go
@@ -32,7 +32,6 @@ func TestPathExpansion(t *testing.T) {
 	tests := map[string]string{
 		"/home/someuser/tmp": "/home/someuser/tmp",
 		"~/tmp":              user.HomeDir + "/tmp",
-		"~thisOtherUser/b/":  "~thisOtherUser/b",
 		"$DDDXXX/a/b":        "/tmp/a/b",
 		"/a/b/":              "/a/b",
 	}
