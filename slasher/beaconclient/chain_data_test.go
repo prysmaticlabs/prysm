1c5f705425758186394634ddbfe6d227204f63d0
Victor Farazdagi <simple.square@gmail.com>
2020-08-13 16:12:16 +0300

    fixes build issue

--- slasher/beaconclient/chain_data_test.go
+++ slasher/beaconclient/chain_data_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/golang/mock/gomock"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/mock"
-	"github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	logTest "github.com/sirupsen/logrus/hooks/test"
 )
