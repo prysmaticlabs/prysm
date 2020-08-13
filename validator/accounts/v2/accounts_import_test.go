a4e1017066d259ccfb5ac0cd7c51fd36c3da773f
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-08-13 14:57:06 -0400

    Fix test

--- validator/accounts/v2/accounts_import_test.go
+++ validator/accounts/v2/accounts_import_test.go
@@ -134,6 +134,57 @@ func TestImport_Noninteractive_RandomName(t *testing.T) {
 	assert.Equal(t, 2, len(keys))
 }
 
+func TestImport_Noninteractive_Filepath(t *testing.T) {
+	walletDir, passwordsDir, passwordFilePath := setupWalletAndPasswordsDir(t)
+	randPath, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	require.NoError(t, err, "Could not generate random file path")
+	keysDir := filepath.Join(testutil.TempDir(), fmt.Sprintf("/%d", randPath), "keysDir")
+	require.NoError(t, os.MkdirAll(keysDir, os.ModePerm))
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(keysDir), "Failed to remove directory")
+	})
+
+	_, keystorePath := createKeystore(t, keysDir)
+	cliCtx := setupWalletCtx(t, &testWalletConfig{
+		walletDir:           walletDir,
+		passwordsDir:        passwordsDir,
+		keysDir:             keystorePath,
+		keymanagerKind:      v2keymanager.Direct,
+		walletPasswordFile:  passwordFilePath,
+		accountPasswordFile: passwordFilePath,
+	})
+	wallet, err := NewWallet(cliCtx, v2keymanager.Direct)
+	require.NoError(t, err)
+	require.NoError(t, wallet.SaveWallet())
+	ctx := context.Background()
+	keymanagerCfg := direct.DefaultConfig()
+	encodedCfg, err := direct.MarshalConfigFile(ctx, keymanagerCfg)
+	require.NoError(t, err)
+	require.NoError(t, wallet.WriteKeymanagerConfigToDisk(ctx, encodedCfg))
+	keymanager, err := direct.NewKeymanager(
+		cliCtx,
+		wallet,
+		keymanagerCfg,
+	)
+	require.NoError(t, err)
+
+	// Make sure there are no accounts at the start.
+	accounts, err := keymanager.ValidatingAccountNames()
+	require.NoError(t, err)
+	assert.Equal(t, len(accounts), 0)
+
+	require.NoError(t, ImportAccounts(cliCtx))
+
+	wallet, err = OpenWallet(cliCtx)
+	require.NoError(t, err)
+	km, err := wallet.InitializeKeymanager(cliCtx, true)
+	require.NoError(t, err)
+	keys, err := km.FetchValidatingPublicKeys(ctx)
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, len(keys))
+}
+
 func TestImport_SortByDerivationPath(t *testing.T) {
 	type test struct {
 		name  string
@@ -196,63 +247,6 @@ func TestImport_SortByDerivationPath(t *testing.T) {
 }
 
 func Test_importPrivateKeyAsAccount(t *testing.T) {
-	walletDir, _, passwordFilePath := setupWalletAndPasswordsDir(t)
-	randPath, err := rand.Int(rand.Reader, big.NewInt(1000000))
-	require.NoError(t, err, "Could not generate random file path")
-	privKeyDir := filepath.Join(testutil.TempDir(), fmt.Sprintf("/%d", randPath), "privKeys")
-	require.NoError(t, os.MkdirAll(privKeyDir, os.ModePerm))
-	t.Cleanup(func() {
-		require.NoError(t, os.RemoveAll(privKeyDir), "Failed to remove directory")
-	})
-	privKeyFileName := filepath.Join(privKeyDir, "privatekey.txt")
-
-	// We create a new private key and save it to a file on disk.
-	privKey := bls.RandKey()
-	privKeyHex := fmt.Sprintf("%x", privKey.Marshal())
-	require.NoError(
-		t,
-		ioutil.WriteFile(privKeyFileName, []byte(privKeyHex), params.BeaconIoConfig().ReadWritePermissions),
-	)
-
-	// We instantiate a new wallet from a cli context.
-	cliCtx := setupWalletCtx(t, &testWalletConfig{
-		walletDir:          walletDir,
-		keymanagerKind:     v2keymanager.Direct,
-		walletPasswordFile: passwordFilePath,
-		privateKeyFile:     privKeyFileName,
-	})
-	wallet, err := NewWallet(cliCtx, v2keymanager.Direct)
-	require.NoError(t, err)
-	require.NoError(t, wallet.SaveWallet())
-
-	// We create a new direct keymanager for the wallet.
-	ctx := context.Background()
-	keymanagerCfg := direct.DefaultConfig()
-	encodedCfg, err := direct.MarshalConfigFile(ctx, keymanagerCfg)
-	require.NoError(t, err)
-	require.NoError(t, wallet.WriteKeymanagerConfigToDisk(ctx, encodedCfg))
-	keymanager, err := direct.NewKeymanager(
-		cliCtx,
-		wallet,
-		keymanagerCfg,
-	)
-	require.NoError(t, err)
-	assert.NoError(t, importPrivateKeyAsAccount(cliCtx, wallet, keymanager))
-
-	// We re-instantiate the keymanager and check we now have 1 public key.
-	keymanager, err = direct.NewKeymanager(
-		cliCtx,
-		wallet,
-		keymanagerCfg,
-	)
-	require.NoError(t, err)
-	pubKeys, err := keymanager.FetchValidatingPublicKeys(ctx)
-	require.NoError(t, err)
-	require.Equal(t, 1, len(pubKeys))
-	assert.DeepEqual(t, pubKeys[0], bytesutil.ToBytes48(privKey.PublicKey().Marshal()))
-}
-
-func Test_importPrivateKeyAsAccount_RandomName(t *testing.T) {
 	walletDir, _, passwordFilePath := setupWalletAndPasswordsDir(t)
 	randPath, err := rand.Int(rand.Reader, big.NewInt(1000000))
 	require.NoError(t, err, "Could not generate random file path")
