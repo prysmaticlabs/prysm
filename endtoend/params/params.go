7fe2483c6bfbcc791d1cf405ec087d4660f7e4e8
rauljordan <raul@prysmaticlabs.com>
2020-08-13 21:28:13 -0500

    change port index

--- endtoend/params/params.go
+++ endtoend/params/params.go
@@ -80,7 +80,7 @@ func Init(beaconNodeCount int) error {
 		BeaconNodeRPCPort:     4150 + testIndex*100,
 		BeaconNodeMetricsPort: 5100 + testIndex*100,
 		ValidatorMetricsPort:  6100 + testIndex*100,
-		ValidatorGatewayPort:  9100 + testIndex*100,
+		ValidatorGatewayPort:  7150 + testIndex*100,
 		SlasherRPCPort:        7100 + testIndex*100,
 		SlasherMetricsPort:    8100 + testIndex*100,
 	}
