b6ed2f91ce7ff87648de337acd7783922795afd7
Victor Farazdagi <simple.square@gmail.com>
2020-08-15 01:11:05 +0300

    fix build

--- slasher/beaconclient/service_test.go
+++ slasher/beaconclient/service_test.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/prysmaticlabs/prysm/beacon-chain/flags"
-	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,13 +17,5 @@ func TestMain(m *testing.M) {
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetOutput(ioutil.Discard)
 
-	resetCfg := featureconfig.InitWithReset(&featureconfig.Flags{})
-	defer resetCfg()
-
-	resetFlags := flags.Get()
-	flags.Init(&flags.GlobalFlags{})
-	defer func() {
-		flags.Init(resetFlags)
-	}()
 	os.Exit(m.Run())
 }
--- slasher/db/kv/kv_test.go
+++ slasher/db/kv/kv_test.go
@@ -9,8 +9,6 @@ import (
 	"path"
 	"testing"
 
-	"github.com/prysmaticlabs/prysm/beacon-chain/flags"
-	"github.com/prysmaticlabs/prysm/shared/featureconfig"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	"github.com/sirupsen/logrus"
@@ -21,14 +19,6 @@ func TestMain(m *testing.M) {
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetOutput(ioutil.Discard)
 
-	resetCfg := featureconfig.InitWithReset(&featureconfig.Flags{})
-	defer resetCfg()
-
-	resetFlags := flags.Get()
-	flags.Init(&flags.GlobalFlags{})
-	defer func() {
-		flags.Init(resetFlags)
-	}()
 	os.Exit(m.Run())
 }
 
