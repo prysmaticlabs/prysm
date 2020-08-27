9f81ade0a76970b4f17abdae7d3b40d114fd339f
nisdas <nishdas93@gmail.com>
2020-08-27 18:43:38 +0800

    request headers instead of blocks

--- beacon-chain/powchain/block_cache_test.go
+++ beacon-chain/powchain/block_cache_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestHashKeyFn_OK(t *testing.T) {
-	bInfo := &blockInfo{
+	bInfo := &headerInfo{
 		Hash: common.HexToHash("0x0123456"),
 	}
 
@@ -22,11 +22,11 @@ func TestHashKeyFn_OK(t *testing.T) {
 
 func TestHashKeyFn_InvalidObj(t *testing.T) {
 	_, err := hashKeyFn("bad")
-	assert.Equal(t, ErrNotABlockInfo, err)
+	assert.Equal(t, ErrNotAHeaderInfo, err)
 }
 
 func TestHeightKeyFn_OK(t *testing.T) {
-	bInfo := &blockInfo{
+	bInfo := &headerInfo{
 		Number: big.NewInt(555),
 	}
 
@@ -37,50 +37,50 @@ func TestHeightKeyFn_OK(t *testing.T) {
 
 func TestHeightKeyFn_InvalidObj(t *testing.T) {
 	_, err := heightKeyFn("bad")
-	assert.Equal(t, ErrNotABlockInfo, err)
+	assert.Equal(t, ErrNotAHeaderInfo, err)
 }
 
 func TestBlockCache_byHash(t *testing.T) {
-	cache := newBlockCache()
+	cache := newHeaderCache()
 
 	header := &gethTypes.Header{
 		ParentHash: common.HexToHash("0x12345"),
 		Number:     big.NewInt(55),
 	}
 
-	exists, _, err := cache.BlockInfoByHash(header.Hash())
+	exists, _, err := cache.HeaderInfoByHash(header.Hash())
 	require.NoError(t, err)
 	assert.Equal(t, false, exists, "Expected block info not to exist in empty cache")
 
-	err = cache.AddBlock(gethTypes.NewBlockWithHeader(header))
+	err = cache.AddHeader(header)
 	require.NoError(t, err)
 
-	exists, fetchedInfo, err := cache.BlockInfoByHash(header.Hash())
+	exists, fetchedInfo, err := cache.HeaderInfoByHash(header.Hash())
 	require.NoError(t, err)
-	assert.Equal(t, true, exists, "Expected blockInfo to exist")
+	assert.Equal(t, true, exists, "Expected headerInfo to exist")
 	assert.Equal(t, 0, fetchedInfo.Number.Cmp(header.Number), "Expected fetched info number to be equal")
 	assert.Equal(t, header.Hash(), fetchedInfo.Hash, "Expected hash to be equal")
 
 }
 
 func TestBlockCache_byHeight(t *testing.T) {
-	cache := newBlockCache()
+	cache := newHeaderCache()
 
 	header := &gethTypes.Header{
 		ParentHash: common.HexToHash("0x12345"),
 		Number:     big.NewInt(55),
 	}
 
-	exists, _, err := cache.BlockInfoByHeight(header.Number)
+	exists, _, err := cache.HeaderInfoByHeight(header.Number)
 	require.NoError(t, err)
 	assert.Equal(t, false, exists, "Expected block info not to exist in empty cache")
 
-	err = cache.AddBlock(gethTypes.NewBlockWithHeader(header))
+	err = cache.AddHeader(header)
 	require.NoError(t, err)
 
-	exists, fetchedInfo, err := cache.BlockInfoByHeight(header.Number)
+	exists, fetchedInfo, err := cache.HeaderInfoByHeight(header.Number)
 	require.NoError(t, err)
-	assert.Equal(t, true, exists, "Expected blockInfo to exist")
+	assert.Equal(t, true, exists, "Expected headerInfo to exist")
 
 	assert.Equal(t, 0, fetchedInfo.Number.Cmp(header.Number), "Expected fetched info number to be equal")
 	assert.Equal(t, header.Hash(), fetchedInfo.Hash, "Expected hash to be equal")
@@ -88,13 +88,13 @@ func TestBlockCache_byHeight(t *testing.T) {
 }
 
 func TestBlockCache_maxSize(t *testing.T) {
-	cache := newBlockCache()
+	cache := newHeaderCache()
 
 	for i := int64(0); i < int64(maxCacheSize+10); i++ {
 		header := &gethTypes.Header{
 			Number: big.NewInt(i),
 		}
-		err := cache.AddBlock(gethTypes.NewBlockWithHeader(header))
+		err := cache.AddHeader(header)
 		require.NoError(t, err)
 
 	}
