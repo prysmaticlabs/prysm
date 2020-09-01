49abc3a12645a68d3829085e54422ac07e4f7ca5
nisdas <nishdas93@gmail.com>
2020-09-01 20:59:22 +0800

    fix build and tests

--- beacon-chain/p2p/rpc_topic_mappings_test.go
+++ beacon-chain/p2p/rpc_topic_mappings_test.go
@@ -14,7 +14,5 @@ func TestVerifyRPCMappings(t *testing.T) {
 	assert.NoError(t, VerifyTopicMapping(RPCMetaDataTopic, new(interface{})), "Failed to verify metadata rpc topic")
 	assert.NotNil(t, VerifyTopicMapping(RPCStatusTopic, new([]byte)), "Incorrect message type verified for metadata rpc topic")
 
-	// TODO(#6408) Remove once issue is resolved
 	assert.NoError(t, VerifyTopicMapping(RPCBlocksByRootTopic, [][32]byte{}), "Failed to verify blocks by root rpc topic")
-	assert.NoError(t, VerifyTopicMapping(RPCBlocksByRootTopic, new(pb.BeaconBlocksByRootRequest)), "Failed to verify blocks by root rpc topic")
 }
