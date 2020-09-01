2c9f58a4ca10c84c2f3ac01129308070f1b1c904
terence tsao <terence@prysmaticlabs.com>
2020-09-01 08:02:39 -0700

    Use state slot

--- beacon-chain/rpc/validator/exit_test.go
+++ beacon-chain/rpc/validator/exit_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
-func TestSub(t *testing.T) {
+func TestProposeExit_Notification(t *testing.T) {
 	db, _ := dbutil.SetupDB(t)
 	ctx := context.Background()
 	testutil.ResetCache()
@@ -30,6 +30,8 @@ func TestSub(t *testing.T) {
 	require.NoError(t, err)
 	beaconState, err := state.GenesisBeaconState(deposits, 0, &ethpb.Eth1Data{BlockHash: make([]byte, 32)})
 	require.NoError(t, err)
+	epoch := uint64(2048)
+	require.NoError(t, beaconState.SetSlot(epoch*params.BeaconConfig().SlotsPerEpoch))
 	block := testutil.NewBeaconBlock()
 	require.NoError(t, db.SaveBlock(ctx, block), "Could not save genesis block")
 	genesisRoot, err := block.Block.HashTreeRoot()
@@ -55,7 +57,6 @@ func TestSub(t *testing.T) {
 	defer opSub.Unsubscribe()
 
 	// Send the request, expect a result on the state feed.
-	epoch := uint64(2048)
 	validatorIndex := uint64(0)
 	req := &ethpb.SignedVoluntaryExit{
 		Exit: &ethpb.VoluntaryExit{
