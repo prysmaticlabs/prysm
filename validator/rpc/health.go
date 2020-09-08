6d837705340e3828ec4757fd8f4594a60a5f3dca
Raul Jordan <raul@prysmaticlabs.com>
2020-09-08 17:35:31 -0500

    Fix Small Issue in Validator RPC Health Endpoint (#7195)
    * amend node connection response
    * imports
    * fix err
    * fix conf
    * err
    * test fix
    * Merge branch 'master' into fix-health-endpoint

--- validator/rpc/health.go
+++ validator/rpc/health.go
@@ -6,25 +6,19 @@ import (
 
 	ptypes "github.com/gogo/protobuf/types"
 	pb "github.com/prysmaticlabs/prysm/proto/validator/accounts/v2"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 // GetBeaconNodeConnection retrieves the current beacon node connection
 // information, as well as its sync status.
 func (s *Server) GetBeaconNodeConnection(ctx context.Context, _ *ptypes.Empty) (*pb.NodeConnectionResponse, error) {
 	genesis, err := s.genesisFetcher.GenesisInfo(ctx)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Could not retrieve genesis information: %v", err)
-	}
 	syncStatus, err := s.syncChecker.Syncing(ctx)
 	if err != nil || s.validatorService.Status() != nil {
 		return &pb.NodeConnectionResponse{
-			GenesisTime:            uint64(time.Unix(genesis.GenesisTime.Seconds, 0).Unix()),
-			DepositContractAddress: genesis.DepositContractAddress,
-			BeaconNodeEndpoint:     s.nodeGatewayEndpoint,
-			Connected:              false,
-			Syncing:                false,
+			GenesisTime:        0,
+			BeaconNodeEndpoint: s.nodeGatewayEndpoint,
+			Connected:          false,
+			Syncing:            false,
 		}, nil
 	}
 	return &pb.NodeConnectionResponse{
