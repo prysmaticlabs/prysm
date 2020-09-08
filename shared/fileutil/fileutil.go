prysmaticlabs/prysm

commit f4848e46d40862d3d16537f7a287db5c93b89215

Safer validator protection db dir (#7176)
* Safer validator protection db dir
* handle old account structure
* Merge refs/heads/master into move_validator_protection_db
* Merge refs/heads/master into move_validator_protection_db
* Merge refs/heads/master into move_validator_protection_db
* Merge refs/heads/master into move_validator_protection_db
* fix e2e issue
* separate function
* Merge branch 'move_validator_protection_db' of github.com:prysmaticlabs/prysm into move_validator_protection_db
* Merge branch 'master' of github.com:prysmaticlabs/prysm into move_validator_protection_db
* Merge refs/heads/master into move_validator_protection_db
* Merge refs/heads/master into move_validator_protection_db
* Merge refs/heads/master into move_validator_protection_db
* Update shared/fileutil/fileutil.go
* Merge refs/heads/master into move_validator_protection_db

shared/fileutil/fileutil.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/pkg/errors"
 )
 
@@ -60,10 +61,13 @@ func FileExists(filename string) bool {
 		return false
 	}
 	info, err := os.Stat(filePath)
-	if os.IsNotExist(err) {
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.WithError(err).Info("Checking for file existence returned an error")
+		}
 		return false
 	}
-	return !info.IsDir()
+	return info != nil && !info.IsDir()
 }
 
 // ReadFileAsBytes expands a file name's absolute path and reads it as bytes from disk.

shared/fileutil/BUILD.bazel
@@ -6,7 +6,10 @@ go_library(
     srcs = ["fileutil.go"],
     importpath = "github.com/prysmaticlabs/prysm/shared/fileutil",
     visibility = ["//visibility:public"],
-    deps = ["@com_github_pkg_errors//:go_default_library"],
+    deps = [
+        "@com_github_pkg_errors//:go_default_library",
+        "@com_github_sirupsen_logrus//:go_default_library",
+    ],
 )
 
 go_test(

validator/db/kv/db.go
@@ -10,7 +10,8 @@ import (
 	bolt "go.etcd.io/bbolt"
 )
 
-var databaseFileName = "validator.db"
+// ProtectionDbFileName Validator slashing protection db file name.
+var ProtectionDbFileName = "validator.db"
 
 // Store defines an implementation of the Prysm Database interface
 // using BoltDB as the underlying persistent kv-store for eth2.
@@ -36,7 +37,7 @@ func (store *Store) ClearDB() error {
 	if _, err := os.Stat(store.databasePath); os.IsNotExist(err) {
 		return nil
 	}
-	return os.Remove(filepath.Join(store.databasePath, databaseFileName))
+	return os.Remove(filepath.Join(store.databasePath, ProtectionDbFileName))
 }
 
 // DatabasePath at which this database writes files.
@@ -60,7 +61,7 @@ func NewKVStore(dirPath string, pubKeys [][48]byte) (*Store, error) {
 	if err := os.MkdirAll(dirPath, params.BeaconIoConfig().ReadWriteExecutePermissions); err != nil {
 		return nil, err
 	}
-	datafile := filepath.Join(dirPath, databaseFileName)
+	datafile := filepath.Join(dirPath, ProtectionDbFileName)
 	boltDB, err := bolt.Open(datafile, params.BeaconIoConfig().ReadWritePermissions, &bolt.Options{Timeout: params.BeaconIoConfig().BoltTimeout})
 	if err != nil {
 		if err == bolt.ErrTimeout {
@@ -94,7 +95,7 @@ func NewKVStore(dirPath string, pubKeys [][48]byte) (*Store, error) {
 
 // GetKVStore returns the validator boltDB key-value store from directory. Returns nil if no such store exists.
 func GetKVStore(directory string) (*Store, error) {
-	fileName := filepath.Join(directory, databaseFileName)
+	fileName := filepath.Join(directory, ProtectionDbFileName)
 	if _, err := os.Stat(fileName); os.IsNotExist(err) {
 		return nil, nil
 	}

validator/node/BUILD.bazel
@@ -26,6 +26,7 @@ go_library(
         "//shared/debug:go_default_library",
         "//shared/event:go_default_library",
         "//shared/featureconfig:go_default_library",
+        "//shared/fileutil:go_default_library",
         "//shared/params:go_default_library",
         "//shared/prometheus:go_default_library",
         "//shared/tracing:go_default_library",

validator/node/node.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"strings"
 	"sync"
 	"syscall"
@@ -19,6 +20,7 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/debug"
 	"github.com/prysmaticlabs/prysm/shared/event"
 	"github.com/prysmaticlabs/prysm/shared/featureconfig"
+	"github.com/prysmaticlabs/prysm/shared/fileutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/prometheus"
 	"github.com/prysmaticlabs/prysm/shared/tracing"
@@ -154,6 +156,7 @@ func (s *ValidatorClient) initializeFromCLI(cliCtx *cli.Context) error {
 	var keyManagerV1 v1.KeyManager
 	var keyManagerV2 v2.IKeymanager
 	var err error
+	var accountsDir string
 	if featureconfig.Get().EnableAccountsV2 {
 		if cliCtx.IsSet(flags.InteropNumValidators.Name) {
 			numValidatorKeys := cliCtx.Uint64(flags.InteropNumValidators.Name)
@@ -162,6 +165,7 @@ func (s *ValidatorClient) initializeFromCLI(cliCtx *cli.Context) error {
 			if err != nil {
 				return errors.Wrap(err, "could not generate interop keys")
 			}
+			accountsDir = cliCtx.String(flags.KeystorePathFlag.Name)
 		} else {
 			// Read the wallet from the specified path.
 			wallet, err := accountsv2.OpenWalletOrElseCli(cliCtx, func(cliCtx *cli.Context) (*accountsv2.Wallet, error) {
@@ -180,6 +184,7 @@ func (s *ValidatorClient) initializeFromCLI(cliCtx *cli.Context) error {
 			if err := wallet.LockWalletConfigFile(cliCtx.Context); err != nil {
 				log.Fatalf("Could not get a lock on wallet file. Please check if you have another validator instance running and using the same wallet: %v", err)
 			}
+			accountsDir = s.wallet.AccountsDir()
 		}
 	} else {
 		keyManagerV1, err = selectV1Keymanager(cliCtx)
@@ -188,9 +193,9 @@ func (s *ValidatorClient) initializeFromCLI(cliCtx *cli.Context) error {
 		}
 	}
 
+	dataDir := moveDb(cliCtx, accountsDir)
 	clearFlag := cliCtx.Bool(cmd.ClearDB.Name)
 	forceClearFlag := cliCtx.Bool(cmd.ForceClearDB.Name)
-	dataDir := cliCtx.String(cmd.DataDirFlag.Name)
 	if clearFlag || forceClearFlag {
 		if dataDir == "" {
 			dataDir = cmd.DefaultDataDir()
@@ -231,6 +236,25 @@ func (s *ValidatorClient) initializeFromCLI(cliCtx *cli.Context) error {
 		return err
 	}
 	return nil
+}
+
+func moveDb(cliCtx *cli.Context, accountsDir string) string {
+	dataDir := cliCtx.String(cmd.DataDirFlag.Name)
+	if accountsDir != "" {
+		dataFile := filepath.Join(dataDir, kv.ProtectionDbFileName)
+		newDataFile := filepath.Join(accountsDir, kv.ProtectionDbFileName)
+		if fileutil.FileExists(dataFile) && !fileutil.FileExists(newDataFile) {
+			log.WithFields(logrus.Fields{
+				"oldDbPath": dataDir,
+				"walletDir": accountsDir,
+			}).Info("Moving validator protection db to wallet dir")
+			if err := os.Rename(dataFile, newDataFile); err != nil {
+				log.Fatal(err)
+			}
+		}
+		dataDir = accountsDir
+	}
+	return dataDir
 }
 
 func (s *ValidatorClient) initializeForWeb(cliCtx *cli.Context) error {

commit 1e7ba810a06b95920d4b753662387cd3f16bdf31

gofmt issues introduced in https://github.com/prysmaticlabs/prysm/pull/7176

shared/fileutil/fileutil.go
@@ -8,8 +8,8 @@ import (
 	"path/filepath"
 	"strings"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 // ExpandPath given a string which may be a relative path.