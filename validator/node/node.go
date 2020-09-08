9b0123ba92b71efbcfda50597f89fbd12c7df9d9
rauljordan <raul@prysmaticlabs.com>
2020-09-08 15:21:24 -0500

    amend node connection response

--- validator/node/node.go
+++ validator/node/node.go
@@ -392,6 +392,7 @@ func (s *ValidatorClient) registerRPCService(cliCtx *cli.Context) error {
 		WalletInitializedFeed: s.walletInitialized,
 		ValidatorService:      vs,
 		SyncChecker:           vs,
+		GenesisFetcher:        vs,
 		NodeGatewayEndpoint:   nodeGatewayEndpoint,
 	})
 	return s.services.RegisterService(server)
