11b6ecda8d2df11648d28db2b8737c1167458f7c
shayzluf <thezluf@gmail.com>
2020-09-09 09:43:55 +0300

    Local protection schema update

--- validator/db/iface/interface.go
+++ validator/db/iface/interface.go
@@ -18,6 +18,10 @@ type ValidatorDB interface {
 	// Proposer protection related methods.
 	ProposalHistoryForEpoch(ctx context.Context, publicKey []byte, epoch uint64) (bitfield.Bitlist, error)
 	SaveProposalHistoryForEpoch(ctx context.Context, publicKey []byte, epoch uint64, history bitfield.Bitlist) error
+	//new data structure methods
+	ProposalHistoryForSlot(ctx context.Context, publicKey []byte, slot uint64) ([32]byte, error)
+	SaveProposalHistoryForSlot(ctx context.Context, pubKey []byte, slot uint64, signingRoot []byte) error
+
 	// Attester protection related methods.
 	AttestationHistoryForPubKeys(ctx context.Context, publicKeys [][48]byte) (map[[48]byte]*slashpb.AttestationHistory, error)
 	SaveAttestationHistoryForPubKeys(ctx context.Context, historyByPubKey map[[48]byte]*slashpb.AttestationHistory) error
