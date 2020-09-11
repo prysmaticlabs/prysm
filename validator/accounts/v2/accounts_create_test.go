c0dbad3a5fdb85f76408e7a045eaf16d817a9d17
dv8silencer <dv8silencer+github@gmail.com>
2020-09-10 23:19:23 -0500

    TEMP, INCOMPLETE;  Working on regression test

--- validator/accounts/v2/accounts_create_test.go
+++ validator/accounts/v2/accounts_create_test.go
@@ -2,6 +2,8 @@ package v2
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
@@ -53,4 +55,58 @@ func TestCreateAccount_Derived(t *testing.T) {
 	names, err := km.ValidatingAccountNames(ctx)
 	assert.NoError(t, err)
 	require.Equal(t, len(names), int(numAccounts))
+}
+
+// Test_KeysConsistency_Direct checks that the password does not change due to account creation in a Direct wallet
+func Test_KeysConsistency_Direct(t *testing.T) {
+	walletDir, passwordsDir, walletPasswordFile := setupWalletAndPasswordsDir(t)
+
+	//Specify the 'initial'/correct password here
+	err := ioutil.WriteFile(walletPasswordFile, []byte("Pa$sW0rD0__Fo0xPr"), os.ModePerm)
+	require.NoError(t, err)
+
+	cliCtx := setupWalletCtx(t, &testWalletConfig{
+		walletDir:          walletDir,
+		passwordsDir:       passwordsDir,
+		keymanagerKind:     v2keymanager.Direct,
+		walletPasswordFile: walletPasswordFile,
+	})
+
+	wallet, err := CreateAndSaveWalletCli(cliCtx)
+	require.NoError(t, err)
+
+	err = CreateAccount(cliCtx.Context, &CreateAccountConfig{
+		Wallet:      wallet,
+		NumAccounts: 1,
+	})
+	require.NoError(t, err)
+
+	// Now change the password provided
+	require.NoError(t, ioutil.WriteFile(walletPasswordFile, []byte("SecoNDxyzPass__9!@#"), os.ModePerm))
+
+	wallet, err = OpenWalletOrElseCli(cliCtx, CreateAndSaveWalletCli)
+	require.NoError(t, err)
+
+	// The bug this regression test is for arises when first there is a wrong password followed by the correct one
+	// If the first password is incorrect, the program will ask for more via standard input
+
+	//tmpFile, err := ioutil.TempFile("", "testFile")
+	//tmpFile.Write([]byte("Pa$sW0rD0__Fo0xPr"))
+	//require.NoError(t, err)
+	//defer os.Remove(tmpFile.Name())
+	//_, err = tmpFile.Seek(0, 0)
+	//require.NoError(t, err)
+	//
+	//savedStdin := os.Stdin
+	//defer func() {
+	//	os.Stdin = savedStdin
+	//}()
+	//os.Stdin = tmpFile
+
+	err = CreateAccount(cliCtx.Context, &CreateAccountConfig{
+		Wallet:      wallet,
+		NumAccounts: 1,
+	})
+	require.NoError(t, err)
+
 }
