d40e77a30cf6faf54383de620fa8969115529c0e
Preston Van Loon <preston@prysmaticlabs.com>
2020-09-11 17:34:15 -0700

    A bit of refactoring ot explain why there is a committee_disabled file

--- beacon-chain/cache/committee.go
+++ beacon-chain/cache/committee.go
@@ -14,10 +14,6 @@ import (
 )
 
 var (
-	// ErrNotCommittee will be returned when a cache object is not a pointer to
-	// a Committee struct.
-	ErrNotCommittee = errors.New("object is not a committee struct")
-
 	// maxCommitteesCacheSize defines the max number of shuffled committees on per randao basis can cache.
 	// Due to reorgs and long finality, it's good to keep the old cache around for quickly switch over.
 	maxCommitteesCacheSize = uint64(32)
@@ -33,15 +29,6 @@ var (
 		Help: "The number of committee requests that are present in the cache.",
 	})
 )
-
-// Committees defines the shuffled committees seed.
-type Committees struct {
-	CommitteeCount  uint64
-	Seed            [32]byte
-	ShuffledIndices []uint64
-	SortedIndices   []uint64
-	ProposerIndices []uint64
-}
 
 // CommitteeCache is a struct with 1 queue for looking up shuffled indices list by seed.
 type CommitteeCache struct {
