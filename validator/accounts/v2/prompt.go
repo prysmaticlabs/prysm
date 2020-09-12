6d430317b0a24ba41df927425cfc46346df94279
dv8silencer <dv8silencer+github@gmail.com>
2020-09-11 20:43:53 -0500

    Fix a few more bugs

--- validator/accounts/v2/prompt.go
+++ validator/accounts/v2/prompt.go
@@ -2,7 +2,6 @@ package v2
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 
@@ -61,11 +60,7 @@ func inputWeakPassword(cliCtx *cli.Context, passwordFileFlag *cli.StringFlag, pr
 		if err != nil {
 			return "", errors.Wrap(err, "could not determine absolute path of password file")
 		}
-		data, err := ioutil.ReadFile(passwordFilePath)
-		if err != nil {
-			return "", errors.Wrap(err, "could not read password file")
-		}
-		return strings.TrimRight(string(data), "\r\n"), nil
+		return passwordFilePath, nil
 	}
 	walletPassword, err := promptutil.PasswordPrompt(promptText, promptutil.NotEmpty)
 	if err != nil {
