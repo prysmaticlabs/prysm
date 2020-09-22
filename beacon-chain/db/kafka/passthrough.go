85fa16f7a4726ec75b2193f554cc1ec35ec22455
terence tsao <terence@prysmaticlabs.com>
2020-09-22 11:43:28 -0700

    Delete pass through

--- beacon-chain/db/kafka/passthrough.go
+++ beacon-chain/db/kafka/passthrough.go
@@ -247,11 +247,6 @@ func (e Exporter) LastArchivedSlot(ctx context.Context) (uint64, error) {
 	return e.db.LastArchivedSlot(ctx)
 }
 
-// HistoricalStatesDeleted -- passthrough
-func (e Exporter) HistoricalStatesDeleted(ctx context.Context) error {
-	return e.db.HistoricalStatesDeleted(ctx)
-}
-
 // RunMigrations -- passthrough
 func (e Exporter) RunMigrations(ctx context.Context) error {
 	return e.db.RunMigrations(ctx)
