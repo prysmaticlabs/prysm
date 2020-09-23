fd41da3ad6e3bcb11ea7dd6976bef72528dfa5fe
shayzluf <thezluf@gmail.com>
2020-09-23 17:33:52 +0300

    add tests

--- validator/db/kv/attestation_history_new_test.go
+++ validator/db/kv/attestation_history_new_test.go
@@ -65,6 +65,66 @@ func TestGetTargetData(t *testing.T) {
 	require.ErrorContains(t, "is smaller then the requested target location", err)
 }
 
+func TestSetTargetData(t *testing.T) {
+	ctx := context.Background()
+	type testStruct struct {
+		name        string
+		enc         []byte
+		target      uint64
+		source      uint64
+		signingRoot []byte
+		expected    []byte
+		error       string
+	}
+	tests := []testStruct{
+		{
+			name:        "empty enc",
+			enc:         []byte{},
+			target:      0,
+			source:      100,
+			signingRoot: []byte{1, 2, 3},
+			expected:    nil,
+			error:       "encapsulated data size",
+		},
+		{
+			name:        "new enc",
+			enc:         newAttestationHistoryArray(0),
+			target:      0,
+			source:      100,
+			signingRoot: []byte{1, 2, 3},
+			expected:    []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x64, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x2, 0x3, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+			error:       "",
+		},
+		{
+			name:        "higher target",
+			enc:         newAttestationHistoryArray(0),
+			target:      2,
+			source:      100,
+			signingRoot: []byte{1, 2, 3},
+			expected:    []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x64, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x2, 0x3, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+			error:       "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			enc, err := setTargetData(ctx,
+				tt.enc, tt.target,
+				&HistoryData{
+					Source:      tt.source,
+					SigningRoot: tt.signingRoot,
+				})
+			if tt.error == "" {
+				require.NoError(t, err)
+
+			} else {
+				assert.ErrorContains(t, tt.error, err)
+			}
+			require.DeepEqual(t, tt.expected, enc)
+		})
+	}
+
+}
+
 func TestAttestationHistoryForPubKeysNew_EmptyVals(t *testing.T) {
 	pubkeys := [][48]byte{{30}, {25}, {20}}
 	db := setupDB(t, pubkeys)
@@ -73,7 +133,7 @@ func TestAttestationHistoryForPubKeysNew_EmptyVals(t *testing.T) {
 	require.NoError(t, err)
 
 	cleanAttHistoryForPubKeys := make(map[[48]byte][]byte)
-	clean := make([]byte, latestEpochWrittenSize)
+	clean := newAttestationHistoryArray(0)
 	for _, pubKey := range pubkeys {
 		cleanAttHistoryForPubKeys[pubKey] = clean
 	}
@@ -81,113 +141,30 @@ func TestAttestationHistoryForPubKeysNew_EmptyVals(t *testing.T) {
 	require.DeepEqual(t, cleanAttHistoryForPubKeys, historyForPubKeys, "Expected attestation history epoch bits to be empty")
 }
 
-//func TestSaveAttestationHistory_OK(t *testing.T) {
-//	pubKeys := [][48]byte{{3}, {4}}
-//	db := setupDB(t, pubKeys)
-//
-//	farFuture := params.BeaconConfig().FarFutureEpoch
-//	newMap := make(map[uint64]uint64)
-//	// The validator attested at target epoch 2 but had no attestations for target epochs 0 and 1.
-//	newMap[0] = farFuture
-//	newMap[1] = farFuture
-//	newMap[2] = 1
-//	history := &slashpb.AttestationHistory{
-//		TargetToSource:     newMap,
-//		LatestEpochWritten: 2,
-//	}
-//
-//	newMap2 := make(map[uint64]uint64)
-//	// The validator attested at target epoch 1 and 3 but had no attestations for target epochs 0 and 2.
-//	newMap2[0] = farFuture
-//	newMap2[1] = 0
-//	newMap2[2] = farFuture
-//	newMap2[3] = 2
-//	history2 := &slashpb.AttestationHistory{
-//		TargetToSource:     newMap2,
-//		LatestEpochWritten: 3,
-//	}
-//
-//	attestationHistory := make(map[[48]byte]*slashpb.AttestationHistory)
-//	attestationHistory[pubKeys[0]] = history
-//	attestationHistory[pubKeys[1]] = history2
-//
-//	require.NoError(t, db.SaveAttestationHistoryForPubKeys(context.Background(), attestationHistory), "Saving attestation history failed")
-//	savedHistories, err := db.AttestationHistoryForPubKeys(context.Background(), pubKeys)
-//	require.NoError(t, err, "Failed to get attestation history")
-//
-//	require.NotNil(t, savedHistories)
-//	require.DeepEqual(t, attestationHistory, savedHistories, "Expected DB to keep object the same, received: %v", history)
-//
-//	savedHistory := savedHistories[pubKeys[0]]
-//	require.Equal(t, newMap[2], savedHistory.TargetToSource[2], "Expected target epoch %d to have the same marked source epoch", 2)
-//	require.Equal(t, newMap[1], savedHistory.TargetToSource[1], "Expected target epoch %d to have the same marked source epoch", 1)
-//	require.Equal(t, newMap[0], savedHistory.TargetToSource[0], "Expected target epoch %d to have the same marked source epoch", 0)
-//
-//	savedHistory = savedHistories[pubKeys[1]]
-//	require.Equal(t, newMap2[3], savedHistory.TargetToSource[3], "Expected target epoch %d to have the same marked source epoch", 3)
-//	require.Equal(t, newMap2[2], savedHistory.TargetToSource[2], "Expected target epoch %d to have the same marked source epoch", 2)
-//	require.Equal(t, newMap2[1], savedHistory.TargetToSource[1], "Expected target epoch %d to have the same marked source epoch", 1)
-//}
-//
-//func TestSaveAttestationHistory_Overwrites(t *testing.T) {
-//	db := setupDB(t, [][48]byte{})
-//	farFuture := params.BeaconConfig().FarFutureEpoch
-//	newMap1 := make(map[uint64]uint64)
-//	newMap1[0] = farFuture
-//	newMap1[1] = 0
-//	newMap2 := make(map[uint64]uint64)
-//	newMap2[0] = farFuture
-//	newMap2[1] = farFuture
-//	newMap2[2] = 1
-//	newMap3 := make(map[uint64]uint64)
-//	newMap3[0] = farFuture
-//	newMap3[1] = farFuture
-//	newMap3[2] = farFuture
-//	newMap3[3] = 2
-//	tests := []struct {
-//		pubkey  [48]byte
-//		epoch   uint64
-//		history *slashpb.AttestationHistory
-//	}{
-//		{
-//			pubkey: [48]byte{0},
-//			epoch:  uint64(1),
-//			history: &slashpb.AttestationHistory{
-//				TargetToSource:     newMap1,
-//				LatestEpochWritten: 1,
-//			},
-//		},
-//		{
-//			pubkey: [48]byte{0},
-//			epoch:  uint64(2),
-//			history: &slashpb.AttestationHistory{
-//				TargetToSource:     newMap2,
-//				LatestEpochWritten: 2,
-//			},
-//		},
-//		{
-//			pubkey: [48]byte{0},
-//			epoch:  uint64(3),
-//			history: &slashpb.AttestationHistory{
-//				TargetToSource:     newMap3,
-//				LatestEpochWritten: 3,
-//			},
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		attHistory := make(map[[48]byte]*slashpb.AttestationHistory)
-//		attHistory[tt.pubkey] = tt.history
-//		require.NoError(t, db.SaveAttestationHistoryForPubKeys(context.Background(), attHistory), "Saving attestation history failed")
-//		histories, err := db.AttestationHistoryForPubKeys(context.Background(), [][48]byte{tt.pubkey})
-//		require.NoError(t, err, "Failed to get attestation history")
-//
-//		history := histories[tt.pubkey]
-//		require.NotNil(t, history)
-//		require.DeepEqual(t, tt.history, history, "Expected DB to keep object the same")
-//		require.Equal(t, tt.epoch-1, history.TargetToSource[tt.epoch],
-//			"Expected target epoch %d to be marked with correct source epoch %d", tt.epoch, history.TargetToSource[tt.epoch])
-//		require.Equal(t, farFuture, history.TargetToSource[tt.epoch-1],
-//			"Expected target epoch %d to not be marked as attested for, received %d", tt.epoch-1, history.TargetToSource[tt.epoch-1])
-//	}
-//}
+func TestAttestationHistoryForPubKeysNew_OK(t *testing.T) {
+	ctx := context.Background()
+	pubkeys := [][48]byte{{30}, {25}, {20}}
+	db := setupDB(t, pubkeys)
+
+	historyForPubKeys, err := db.AttestationHistoryNewForPubKeys(context.Background(), pubkeys)
+	require.NoError(t, err)
+
+	setAttHistoryForPubKeys := make(map[[48]byte][]byte)
+	clean := newAttestationHistoryArray(0)
+	for i, pubKey := range pubkeys {
+		enc, err := setTargetData(ctx,
+			clean, 10,
+			&HistoryData{
+				Source:      uint64(i),
+				SigningRoot: []byte{1, 2, 3},
+			})
+		require.NoError(t, err)
+		setAttHistoryForPubKeys[pubKey] = enc
+
+	}
+	err = db.SaveAttestationHistoryNewForPubKeys(context.Background(), setAttHistoryForPubKeys)
+	require.NoError(t, err)
+	historyForPubKeys, err = db.AttestationHistoryNewForPubKeys(context.Background(), pubkeys)
+	require.NoError(t, err)
+	require.DeepEqual(t, setAttHistoryForPubKeys, historyForPubKeys, "Expected attestation history epoch bits to be empty")
+}
