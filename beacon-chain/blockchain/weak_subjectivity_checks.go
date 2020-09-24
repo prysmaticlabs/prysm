96322c5d1769bd4c3a277d53ea35e04e89b6748b
terence tsao <terence@prysmaticlabs.com>
2020-09-24 14:18:45 -0700

    Comments

--- beacon-chain/blockchain/weak_subjectivity_checks.go
+++ beacon-chain/blockchain/weak_subjectivity_checks.go
@@ -10,12 +10,16 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
 
-// VerifyWeakSubjectivityRoot verifies the subjectivity root
+// VerifyWeakSubjectivityRoot verifies the weak subjectivity root in the service struct.
+// Reference design: https://github.com/ethereum/eth2.0-specs/blob/master/specs/phase0/weak-subjectivity.md#weak-subjectivity-sync-procedure
 func (s *Service) VerifyWeakSubjectivityRoot(ctx context.Context) error {
 	// TODO(7342): Remove the following to fully use weak subjectivity in production.
 	if len(s.wsRoot) == 0 && s.wsEpoch == 0 {
 		return nil
 	}
+
+	// Do nothing if the weak subjectivity has previously been verified,
+	// or weak subjectivity epoch is higher than last finalized epoch.
 	if s.wsVerified {
 		return nil
 	}
@@ -29,7 +33,7 @@ func (s *Service) VerifyWeakSubjectivityRoot(ctx context.Context) error {
 	if err := s.beaconDB.SaveBlocks(ctx, s.getInitSyncBlocks()); err != nil {
 		return err
 	}
-	// A node should have the block and the block root within the specified epoch.
+	// A node should have the weak subjectivity block in the DB.
 	if !s.beaconDB.HasBlock(ctx, r) {
 		return fmt.Errorf("node does not have root in DB: %#x", r)
 	}
@@ -38,6 +42,7 @@ func (s *Service) VerifyWeakSubjectivityRoot(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+	// A node should have the weak subjectivity block corresponds to the correct epoch in the DB.
 	filter := filters.NewFilter().SetStartSlot(startSlot).SetEndSlot(startSlot + params.BeaconConfig().SlotsPerEpoch)
 	roots, err := s.beaconDB.BlockRoots(ctx, filter)
 	if err != nil {
