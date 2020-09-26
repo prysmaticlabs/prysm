eb59134e3c792bf1eefd8c5fc54eb6a84ac0fdbc
dv8silencer <dv8silencer+github@gmail.com>
2020-09-26 11:59:03 -0500

    Prepare to merge master

--- validator/accounts/v2/wallet_recover_test.go
+++ validator/accounts/v2/wallet_recover_test.go
@@ -82,6 +82,43 @@ func TestRecoverDerivedWallet(t *testing.T) {
 
 }
 
+// TestRecoverDerivedWallet_OneAccount is a test for regression in cases where the number of accounts recovered is 1
+func TestRecoverDerivedWallet_OneAccount(t *testing.T) {
+	testDir := testutil.TempDir()
+	walletDir := filepath.Join(testDir, walletDirName)
+	defer func() {
+		assert.NoError(t, os.RemoveAll(walletDir))
+	}()
+
+	passwordFilePath := filepath.Join(testDir, passwordFileName)
+	require.NoError(t, ioutil.WriteFile(passwordFilePath, []byte(password), os.ModePerm))
+	mnemonicFilePath := filepath.Join(testDir, mnemonicFileName)
+	require.NoError(t, ioutil.WriteFile(mnemonicFilePath, []byte(mnemonic), os.ModePerm))
+
+	numAccounts := int64(1)
+	app := cli.App{}
+	set := flag.NewFlagSet("test", 0)
+	set.String(flags.WalletDirFlag.Name, walletDir, "")
+	set.String(flags.WalletPasswordFileFlag.Name, passwordFilePath, "")
+	set.String(flags.KeymanagerKindFlag.Name, v2keymanager.Derived.String(), "")
+	set.String(flags.MnemonicFileFlag.Name, mnemonicFilePath, "")
+	set.Int64(flags.NumAccountsFlag.Name, numAccounts, "")
+	assert.NoError(t, set.Set(flags.WalletDirFlag.Name, walletDir))
+	assert.NoError(t, set.Set(flags.WalletPasswordFileFlag.Name, passwordFilePath))
+	assert.NoError(t, set.Set(flags.KeymanagerKindFlag.Name, v2keymanager.Derived.String()))
+	assert.NoError(t, set.Set(flags.MnemonicFileFlag.Name, mnemonicFilePath))
+	assert.NoError(t, set.Set(flags.NumAccountsFlag.Name, strconv.Itoa(int(numAccounts))))
+	cliCtx := cli.NewContext(&app, set, nil)
+
+	require.NoError(t, RecoverWalletCli(cliCtx))
+
+	_, err := wallet.OpenWallet(cliCtx.Context, &wallet.Config{
+		WalletDir:      walletDir,
+		WalletPassword: password,
+	})
+	assert.NoError(t, err)
+}
+
 func TestRecoverDerivedWallet_AlreadyExists(t *testing.T) {
 	testDir := testutil.TempDir()
 	walletDir := filepath.Join(testDir, walletDirName)
