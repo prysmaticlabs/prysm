0921c00094920c5960551b2ff8c631a41a92cce2
Preston Van Loon <preston@prysmaticlabs.com>
2020-09-28 22:22:55 -0700

    Remove processSlots from slashing check (#7357)
    * Remove processSlots from slashing check
    
    * imports
    
    * imports
    
    * PR feedback from @nisdas
    
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/sync/validate_attester_slashing.go
+++ beacon-chain/sync/validate_attester_slashing.go
@@ -8,9 +8,7 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
-	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
 	"github.com/prysmaticlabs/prysm/shared/hashutil"
-	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/sliceutil"
 	"github.com/prysmaticlabs/prysm/shared/traceutil"
 	"go.opencensus.io/trace"
@@ -51,24 +49,10 @@ func (s *Service) validateAttesterSlashing(ctx context.Context, pid peer.ID, msg
 		return pubsub.ValidationIgnore
 	}
 
-	// Retrieve head state, advance state to the epoch slot used specified in slashing message.
 	headState, err := s.chain.HeadState(ctx)
 	if err != nil {
 		return pubsub.ValidationIgnore
 	}
-	slashSlot := slashing.Attestation_1.Data.Target.Epoch * params.BeaconConfig().SlotsPerEpoch
-	if headState.Slot() < slashSlot {
-		if ctx.Err() != nil {
-			return pubsub.ValidationIgnore
-		}
-
-		var err error
-		headState, err = state.ProcessSlots(ctx, headState, slashSlot)
-		if err != nil {
-			return pubsub.ValidationIgnore
-		}
-	}
-
 	if err := blocks.VerifyAttesterSlashing(ctx, headState, slashing); err != nil {
 		return pubsub.ValidationReject
 	}
