2b05cae64aadb43f09efeda3b2f66f7514c4bee9
rauljordan <raul@prysmaticlabs.com>
2020-09-30 15:31:52 -0500

    implement backup using zip

--- validator/rpc/accounts.go
+++ validator/rpc/accounts.go
@@ -1,11 +1,15 @@
 package rpc
 
 import (
+	"archive/zip"
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/pkg/errors"
 	pb "github.com/prysmaticlabs/prysm/proto/validator/accounts/v2"
+	"github.com/prysmaticlabs/prysm/shared/bls"
 	"github.com/prysmaticlabs/prysm/shared/petnames"
 	v2 "github.com/prysmaticlabs/prysm/validator/accounts/v2"
 	v2keymanager "github.com/prysmaticlabs/prysm/validator/keymanager/v2"
@@ -73,12 +77,16 @@ func (s *Server) ListAccounts(ctx context.Context, req *pb.ListAccountsRequest) 
 	}, nil
 }
 
-// BackupAccounts --
+// BackupAccounts creates a zip file containing EIP-2335 keystores for the user's
+// specified public keys by encrypting them with the specified password.
 func (s *Server) BackupAccounts(
 	ctx context.Context, req *pb.BackupAccountsRequest,
 ) (*pb.BackupAccountsResponse, error) {
 	if req.PublicKeys == nil || len(req.PublicKeys) < 1 {
-		return nil, status.Error(codes.FailedPrecondition, "No public keys specified to backup")
+		return nil, status.Error(codes.InvalidArgument, "No public keys specified to backup")
+	}
+	if req.BackupPassword == "" {
+		return nil, status.Error(codes.InvalidArgument, "Backup password cannot be empty")
 	}
 	if s.wallet == nil || s.keymanager == nil {
 		return nil, status.Error(codes.FailedPrecondition, "No wallet nor keymanager found")
@@ -86,7 +94,59 @@ func (s *Server) BackupAccounts(
 	if s.wallet.KeymanagerKind() != v2keymanager.Direct || s.wallet.KeymanagerKind() != v2keymanager.Derived {
 		return nil, status.Error(codes.FailedPrecondition, "Only HD or direct wallets can backup accounts")
 	}
-	return nil, status.Error(codes.Unimplemented, "Unimplemented")
+	pubKeys := make([]bls.PublicKey, len(req.PublicKeys))
+	for i, key := range req.PublicKeys {
+		pubKey, err := bls.PublicKeyFromBytes(key)
+		if err != nil {
+			return nil, err
+		}
+		pubKeys[i] = pubKey
+	}
+	var keystoresToBackup []*v2keymanager.Keystore
+	var err error
+	switch s.wallet.KeymanagerKind() {
+	case v2keymanager.Direct:
+		km, ok := s.keymanager.(*direct.Keymanager)
+		if !ok {
+			return nil, nil
+		}
+		keystoresToBackup, err = km.ExtractKeystores(ctx, pubKeys, req.BackupPassword)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not backup accounts for direct keymanager")
+		}
+	case v2keymanager.Derived:
+		km, ok := s.keymanager.(*derived.Keymanager)
+		if !ok {
+			return nil, errors.New("could not assert keymanager interface to concrete type")
+		}
+		keystoresToBackup, err = km.ExtractKeystores(ctx, pubKeys, req.BackupPassword)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not backup accounts for derived keymanager")
+		}
+	}
+	buf := new(bytes.Buffer)
+	writer := zip.NewWriter(buf)
+	defer func() {
+		if err := writer.Close(); err != nil {
+			log.WithError(err).Error("Could not close zip file after writing")
+		}
+	}()
+	for i, k := range keystoresToBackup {
+		encodedFile, err := json.MarshalIndent(k, "", "\t")
+		if err != nil {
+			return nil, errors.Wrap(err, "could not marshal keystore to JSON file")
+		}
+		f, err := writer.Create(fmt.Sprintf("keystore-%d.json", i))
+		if err != nil {
+			return nil, errors.Wrap(err, "could not write keystore file to zip")
+		}
+		if _, err = f.Write(encodedFile); err != nil {
+			return nil, errors.Wrap(err, "could not write keystore file contents")
+		}
+	}
+	return &pb.BackupAccountsResponse{
+		ZipFile: buf.Bytes(),
+	}, status.Error(codes.Unimplemented, "Unimplemented")
 }
 
 // DeleteAccounts deletes accounts from a user if their wallet is a non-HD wallet.
@@ -94,7 +154,7 @@ func (s *Server) DeleteAccounts(
 	ctx context.Context, req *pb.DeleteAccountsRequest,
 ) (*pb.DeleteAccountsResponse, error) {
 	if req.PublicKeys == nil || len(req.PublicKeys) < 1 {
-		return nil, status.Error(codes.FailedPrecondition, "No public keys specified to delete")
+		return nil, status.Error(codes.InvalidArgument, "No public keys specified to delete")
 	}
 	if s.wallet == nil || s.keymanager == nil {
 		return nil, status.Error(codes.FailedPrecondition, "No wallet nor keymanager found")
