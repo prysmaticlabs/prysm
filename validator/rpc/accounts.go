73214627542932694372a95417e83e5aec132d8e
Raul Jordan <raul@prysmaticlabs.com>
2020-10-02 14:56:54 -0500

    Pagination tests to list accounts

--- validator/rpc/accounts.go
+++ validator/rpc/accounts.go
@@ -10,6 +10,8 @@ import (
 	"github.com/pkg/errors"
 	pb "github.com/prysmaticlabs/prysm/proto/validator/accounts/v2"
 	"github.com/prysmaticlabs/prysm/shared/bls"
+	"github.com/prysmaticlabs/prysm/shared/cmd"
+	"github.com/prysmaticlabs/prysm/shared/pagination"
 	"github.com/prysmaticlabs/prysm/shared/petnames"
 	v2 "github.com/prysmaticlabs/prysm/validator/accounts/v2"
 	v2keymanager "github.com/prysmaticlabs/prysm/validator/keymanager/v2"
@@ -58,6 +60,10 @@ func (s *Server) ListAccounts(ctx context.Context, req *pb.ListAccountsRequest) 
 	if !s.walletInitialized {
 		return nil, status.Error(codes.FailedPrecondition, "Wallet not yet initialized")
 	}
+	if int(req.PageSize) > cmd.Get().MaxRPCPageSize {
+		return nil, status.Errorf(codes.InvalidArgument, "Requested page size %d can not be greater than max size %d",
+			req.PageSize, cmd.Get().MaxRPCPageSize)
+	}
 	keys, err := s.keymanager.FetchValidatingPublicKeys(ctx)
 	if err != nil {
 		return nil, err
@@ -72,8 +78,18 @@ func (s *Server) ListAccounts(ctx context.Context, req *pb.ListAccountsRequest) 
 			accounts[i].DerivationPath = fmt.Sprintf(derived.ValidatingKeyDerivationPathTemplate, i)
 		}
 	}
+	start, end, nextPageToken, err := pagination.StartAndEndPage(req.PageToken, int(req.PageSize), len(keys))
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Could not paginate results: %v",
+			err,
+		)
+	}
 	return &pb.ListAccountsResponse{
-		Accounts: accounts,
+		Accounts:      accounts[start:end],
+		TotalSize:     int32(len(keys)),
+		NextPageToken: nextPageToken,
 	}, nil
 }
 
