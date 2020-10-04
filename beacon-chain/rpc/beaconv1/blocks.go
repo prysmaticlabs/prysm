8d845a0bf383ac23cc9bd2936d2d0beaffad85ad
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-10-04 18:58:25 -0400

    Fix comments

--- beacon-chain/rpc/beaconv1/blocks.go
+++ beacon-chain/rpc/beaconv1/blocks.go
@@ -20,6 +20,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+// GetBlockHeader retrieves block header for given block id.
 func (bs *Server) GetBlockHeader(ctx context.Context, req *ethpb.BlockRequest) (*ethpb.BlockHeaderResponse, error) {
 	blk, err := bs.blockFromBlockID(ctx, req.BlockId)
 	if err != nil {
@@ -58,6 +59,7 @@ func (bs *Server) GetBlockHeader(ctx context.Context, req *ethpb.BlockRequest) (
 	}, nil
 }
 
+// ListBlockHeaders retrieves block headers matching given query. By default it will fetch current head slot blocks.
 func (bs *Server) ListBlockHeaders(ctx context.Context, req *ethpb.BlockHeadersRequest) (*ethpb.BlockHeadersResponse, error) {
 	var err error
 	var blks []*ethpb_alpha.SignedBeaconBlock
@@ -109,6 +111,11 @@ func (bs *Server) ListBlockHeaders(ctx context.Context, req *ethpb.BlockHeadersR
 	return &ethpb.BlockHeadersResponse{Data: blkHdrs}, nil
 }
 
+// SubmitBlock instructs the beacon node to broadcast a newly signed beacon block to the beacon network, to be
+// included in the beacon chain. The beacon node is not required to validate the signed BeaconBlock, and a successful
+// response (20X) only indicates that the broadcast has been successful. The beacon node is expected to integrate the
+// new block into its state, and therefore validate the block internally, however blocks which fail the validation are
+// still broadcast but a different status code is returned (202).
 func (bs *Server) SubmitBlock(ctx context.Context, req *ethpb.BeaconBlockContainer) (*ptypes.Empty, error) {
 	blk := req.Message
 	root, err := blk.HashTreeRoot()
@@ -146,6 +153,7 @@ func (bs *Server) SubmitBlock(ctx context.Context, req *ethpb.BeaconBlockContain
 	return &ptypes.Empty{}, nil
 }
 
+// GetBlock retrieves block details for given block id.
 func (bs *Server) GetBlock(ctx context.Context, req *ethpb.BlockRequest) (*ethpb.BlockResponse, error) {
 	blk, err := bs.blockFromBlockID(ctx, req.BlockId)
 	if err != nil {
@@ -168,6 +176,7 @@ func (bs *Server) GetBlock(ctx context.Context, req *ethpb.BlockRequest) (*ethpb
 	}, nil
 }
 
+// GetBlockRoot retrieves hashTreeRoot of BeaconBlock/BeaconBlockHeader.
 func (bs *Server) GetBlockRoot(ctx context.Context, req *ethpb.BlockRequest) (*ethpb.BlockRootResponse, error) {
 	switch string(req.BlockId) {
 	case "head":
@@ -238,6 +247,7 @@ func (bs *Server) GetBlockRoot(ctx context.Context, req *ethpb.BlockRequest) (*e
 	}
 }
 
+// ListBlockAttestations retrieves attestation included in requested block.
 func (bs *Server) ListBlockAttestations(ctx context.Context, req *ethpb.BlockRequest) (*ethpb.BlockAttestationsResponse, error) {
 	blk, err := bs.blockFromBlockID(ctx, req.BlockId)
 	if err != nil {
