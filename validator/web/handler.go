98a20c3f787c53c88b8f31b348d253a59fbb4f25
Preston Van Loon <preston@prysmaticlabs.com>
2020-10-08 13:26:06 -0700

    Determine mimetype

--- validator/web/handler.go
+++ validator/web/handler.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"mime"
 	"net/http"
 	"net/url"
 	"path"
@@ -23,12 +24,16 @@ var webHandler = func(res http.ResponseWriter, req *http.Request) {
 	p = path.Join(prefix, p)
 
 	if d, ok := site[p]; ok {
+		m := mime.TypeByExtension(path.Ext(p))
+		res.Header().Add("Content-Type", m)
 		res.WriteHeader(200)
 		if _, err := res.Write(d); err != nil {
 			log.WithError(err).Error("Failed to write http response")
 		}
 	// Serve index.html as default.
 	} else if d, ok := site[path.Join(prefix, "index.html")]; ok {
+		m := mime.TypeByExtension("html")
+		res.Header().Add("Content-Type", m)
 		res.WriteHeader(200)
 		if _, err := res.Write(d); err != nil {
 			log.WithError(err).Error("Failed to write http response")
