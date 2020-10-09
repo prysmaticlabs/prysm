63b72fcf8d926e0d56b7e03e0e41fff6bf03c96a
rkapka <rkapka@wp.pl>
2020-10-09 11:53:47 +0200

    Revert "active indices"
    This reverts commit 6e8385f3956a08ef9c8980b5343365a1bee48542.

--- beacon-chain/core/helpers/validators.go
+++ beacon-chain/core/helpers/validators.go
@@ -111,7 +111,7 @@ func ActiveValidatorCount(state *stateTrie.BeaconState, epoch uint64) (uint64, e
 		return 0, errors.Wrap(err, "could not interface with committee cache")
 	}
 	if activeCount != 0 && state.Slot() != 0 {
-		return activeCount, nil
+		return uint64(activeCount), nil
 	}
 
 	count := uint64(0)
