2f16ab7c3e97b0b009f0e98e0153fdd196b41c4f
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-10-11 15:41:17 -0400

    Pool impl wip

--- beacon-chain/rpc/beaconv1/pool.go
+++ beacon-chain/rpc/beaconv1/pool.go
@@ -5,14 +5,37 @@ import (
 	"errors"
 
 	ptypes "github.com/gogo/protobuf/types"
-
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
 )
 
 // ListPoolAttestations retrieves attestations known by the node but
 // not necessarily incorporated into any block.
 func (bs *Server) ListPoolAttestations(ctx context.Context, req *ethpb.AttestationsPoolRequest) (*ethpb.AttestationsPoolResponse, error) {
-	return nil, errors.New("unimplemented")
+	atts := bs.AttestationsPool.AggregatedAttestations()
+	numAtts := len(atts)
+	if numAtts == 0 {
+		return &ethpb.AttestationsPoolResponse{
+			Data: make([]*ethpb.Attestation, 0),
+		}, nil
+	}
+	v1Atts := make([]*ethpb.Attestation, len(atts))
+	for i, att := range atts {
+		marshaledAtt, err := att.Marshal()
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "Could not marshal attestation: %v", err)
+		}
+		v1Att := &ethpb.Attestation{}
+		if err := proto.Unmarshal(marshaledAtt, v1Att); err != nil {
+			return nil, status.Errorf(codes.Internal, "Could not unmarshal attestation: %v", err)
+		}
+		v1Atts[i] = v1Att
+	}
+	return &ethpb.AttestationsPoolResponse{
+		Data: v1Atts,
+	}, nil
 }
 
 // SubmitAttestation submits Attestation object to node. If attestation passes all validation
