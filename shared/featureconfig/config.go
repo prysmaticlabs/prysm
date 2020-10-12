e29488bd309fca62e51ecfc18ebc3d91de318f4a
terence tsao <terence@prysmaticlabs.com>
2020-10-12 16:56:45 -0700

    Remove skip BLS verify flag

--- shared/featureconfig/config.go
+++ shared/featureconfig/config.go
@@ -192,10 +192,6 @@ func ConfigureBeaconChain(ctx *cli.Context) {
 		log.Warn("Initial syncing while verifying only the block proposer signatures.")
 		cfg.InitSyncNoVerify = true
 	}
-	if ctx.Bool(skipBLSVerifyFlag.Name) {
-		log.Warn("UNSAFE: Skipping BLS verification at runtime")
-		cfg.SkipBLSVerify = true
-	}
 	if ctx.Bool(enableBackupWebhookFlag.Name) {
 		log.Warn("Allowing database backups to be triggered from HTTP webhook.")
 		cfg.EnableBackupWebhook = true
