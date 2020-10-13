cb9029f2880b70908d06a6631e46d1f47c9bf742
shayzluf <thezluf@gmail.com>
2020-10-13 18:18:07 +0300

    check import error

--- validator/node/node.go
+++ validator/node/node.go
@@ -104,9 +104,9 @@ func NewValidatorClient(cliCtx *cli.Context) (*ValidatorClient, error) {
 	if err := ValidatorClient.initializeFromCLI(cliCtx); err != nil {
 		return nil, err
 	}
-	if err := ValidatorClient.db.UpdateProposalsProtectionDb(cliCtx.Context); err != nil {
-		return nil, err
-	}
+	//if err := ValidatorClient.db.UpdateProposalsProtectionDb(cliCtx.Context); err != nil {
+	//	return nil, err
+	//}
 	return ValidatorClient, nil
 }
 
