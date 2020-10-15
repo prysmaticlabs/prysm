8f9dc1080819f2855833f12afc312d5a3619994f
shayzluf <thezluf@gmail.com>
2020-10-15 21:26:53 +0300

    fix block sign test

--- validator/client/propose_protect_test.go
+++ validator/client/propose_protect_test.go
@@ -6,6 +6,7 @@ import (
 
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/featureconfig"
+	"github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	mockSlasher "github.com/prysmaticlabs/prysm/validator/testing"
 )
@@ -69,18 +70,14 @@ func TestPostBlockSignUpdate(t *testing.T) {
 	defer finish()
 	pubKey := [48]byte{}
 	copy(pubKey[:], validatorKey.PublicKey().Marshal())
-
-	block := &ethpb.SignedBeaconBlock{
-		Block: &ethpb.BeaconBlock{
-			Slot:          10,
-			ProposerIndex: 0,
-		},
-	}
+	emptyBlock := testutil.NewBeaconBlock()
+	emptyBlock.Block.Slot = 10
+	emptyBlock.Block.ProposerIndex = 0
 	mockProtector := &mockSlasher.MockProtector{AllowBlock: false}
 	validator.protector = mockProtector
-	err := validator.postBlockSignUpdate(context.Background(), pubKey, block, nil)
+	err := validator.postBlockSignUpdate(context.Background(), pubKey, emptyBlock, nil)
 	require.ErrorContains(t, failedPostBlockSignErr, err, "Expected error when post signature update is detected as slashable")
 	mockProtector.AllowBlock = true
-	err = validator.postBlockSignUpdate(context.Background(), pubKey, block, nil)
+	err = validator.postBlockSignUpdate(context.Background(), pubKey, emptyBlock, &ethpb.DomainResponse{SignatureDomain: make([]byte, 32)})
 	require.NoError(t, err, "Expected allowed attestation not to throw error")
 }
