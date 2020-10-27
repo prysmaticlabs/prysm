8c2fff3a75ffe672781a8c8d61651f64c228c370
Raul Jordan <raul@prysmaticlabs.com>
2020-10-27 15:51:29 -0500

    Allow for 25th Word Passphrases in Mnemonics (#7645)
    * advanced functionality, enable 25th word mnemonic passphrase
    
    * 25th word passphrase
    
    * add test
    
    * added test to ensure differences when using the mnemonic 25th word
    
    * better message
    
    * passing tests
    
    * fix up logic

--- validator/accounts/accounts_exit.go
+++ validator/accounts/accounts_exit.go
@@ -12,6 +12,7 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/cmd"
 	"github.com/prysmaticlabs/prysm/shared/promptutil"
+	"github.com/prysmaticlabs/prysm/validator/accounts/iface"
 	"github.com/prysmaticlabs/prysm/validator/accounts/prompt"
 	"github.com/prysmaticlabs/prysm/validator/accounts/wallet"
 	"github.com/prysmaticlabs/prysm/validator/client"
@@ -82,7 +83,9 @@ func prepareWallet(cliCtx *cli.Context) ([][48]byte, keymanager.IKeymanager, err
 		return nil, nil, errors.Wrap(err, "could not open wallet")
 	}
 
-	keymanager, err := w.InitializeKeymanager(cliCtx.Context, false /* skip mnemonic confirm */)
+	keymanager, err := w.InitializeKeymanager(cliCtx.Context, &iface.InitializeKeymanagerConfig{
+		SkipMnemonicConfirm: false,
+	})
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "could not initialize keymanager")
 	}
