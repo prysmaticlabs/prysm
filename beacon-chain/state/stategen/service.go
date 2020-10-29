3d07c73cbe6057fcec1ec026c18967b4a83903e6
nisdas <nishdas93@gmail.com>
2020-10-29 20:56:11 +0800

    bug fixes

--- beacon-chain/state/stategen/service.go
+++ beacon-chain/state/stategen/service.go
@@ -10,6 +10,7 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"go.opencensus.io/trace"
 )
@@ -67,7 +68,11 @@ func (s *State) Resume(ctx context.Context) (*state.BeaconState, error) {
 	ctx, span := trace.StartSpan(ctx, "stateGen.Resume")
 	defer span.End()
 
-	lastArchivedRoot := s.beaconDB.LastArchivedRoot(ctx)
+	checkpoint, err := s.beaconDB.FinalizedCheckpoint(ctx)
+	if err != nil {
+		return nil, err
+	}
+	lastArchivedRoot := bytesutil.ToBytes32(checkpoint.Root)
 	lastArchivedState, err := s.beaconDB.State(ctx, lastArchivedRoot)
 	if err != nil {
 		return nil, err
