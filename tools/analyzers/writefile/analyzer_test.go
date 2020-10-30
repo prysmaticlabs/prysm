38f6f31432b3732962450c7e3bec77314ca11873
Raul Jordan <raul@prysmaticlabs.com>
2020-10-30 11:21:24 -0500

    write file and mkdirall analyzers

---
+++ tools/analyzers/writefile/analyzer_test.go
@@ -0,0 +1,11 @@
+package writefile
+
+import (
+	"testing"
+
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func TestAnalyzer(t *testing.T) {
+	analysistest.Run(t, analysistest.TestData(), Analyzer)
+}
