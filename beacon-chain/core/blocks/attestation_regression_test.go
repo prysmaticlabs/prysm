fd06e380ab8db898684e5d146c6c59415cb06eac
Preston Van Loon <preston@prysmaticlabs.com>
2020-10-29 18:45:58 -0700

    Describe bug in comments, fix bug

--- beacon-chain/core/blocks/attestation_regression_test.go
+++ beacon-chain/core/blocks/attestation_regression_test.go
@@ -9,9 +9,13 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
-// https://github.com/sigp/beacon-fuzz/issues/78
+// Beaconfuzz discovered an off by one issue where an attestation could be produced which would pass
+// validation when att.Data.CommitteeIndex is 1 and the committee count per slot is also. The only
+// valid att.Data.Committee index would be 0, so this is an off by one error.
+// See: https://github.com/sigp/beacon-fuzz/issues/78
 func TestProcessAttestationNoVerifySignature_BeaconFuzzIssue78(t *testing.T) {
 	attData, err := ioutil.ReadFile("testdata/beaconfuzz_78_attestation.ssz")
 	if err != nil {
@@ -34,9 +38,7 @@ func TestProcessAttestationNoVerifySignature_BeaconFuzzIssue78(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	_, err = blocks.ProcessAttestationNoVerifySignature(context.Background(), st, att)
-	t.Log(err)
-	if err == nil {
-		t.Error("Expected an error")
-	}
+	ctx := context.Background()
+	_, err = blocks.ProcessAttestationNoVerifySignature(ctx, st, att)
+	require.ErrorContains(t, "committee index 1 >= committee count 1", err)
 }
