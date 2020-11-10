09e3f0360eda5b82345ad1e80fa0d9dbec834690
Victor Farazdagi <simple.square@gmail.com>
2020-11-10 17:56:47 +0300

    Remove redundant calls to os.exit() in TestMain (#7761)
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/sync/sync_test.go
+++ beacon-chain/sync/sync_test.go
@@ -2,7 +2,6 @@ package sync
 
 import (
 	"io/ioutil"
-	"os"
 	"testing"
 
 	"github.com/prysmaticlabs/prysm/beacon-chain/flags"
@@ -13,10 +12,14 @@ func TestMain(m *testing.M) {
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetOutput(ioutil.Discard)
 
+	resetFlags := flags.Get()
 	flags.Init(&flags.GlobalFlags{
 		BlockBatchLimit:            64,
 		BlockBatchLimitBurstFactor: 10,
 	})
+	defer func() {
+		flags.Init(resetFlags)
+	}()
 
-	os.Exit(m.Run())
+	m.Run()
 }
