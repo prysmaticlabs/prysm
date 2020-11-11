1e8955323a87063f4fc9446bcb9addb7a8494de0
rauljordan <raul@prysmaticlabs.com>
2020-11-11 12:19:41 -0600

    confirmity tests

--- validator/keymanager/derived/eip_test.go
+++ validator/keymanager/derived/eip_test.go
@@ -37,12 +37,10 @@ func TestDerivationFromMnemonic(t *testing.T) {
 	require.NoError(t, err)
 	assert.DeepEqual(t, seedBytes, km.seed)
 
-	// We create an account, then check the master SK and the child SK.
-	withdrawalKeyPath := fmt.Sprintf(WithdrawalKeyDerivationPathTemplate, 0)
-	validatingKeyPath := fmt.Sprintf(ValidatingKeyDerivationPathTemplate, childIndex)
-	withdrawalKey, err := km.deriveKey(withdrawalKeyPath)
+	// We derive keys, then check the master SK and the child SK.
+	withdrawalKey, err := km.deriveKey("m")
 	require.NoError(t, err)
-	validatingKey, err := km.deriveKey(validatingKeyPath)
+	validatingKey, err := km.deriveKey(fmt.Sprintf("m/%d", childIndex))
 	require.NoError(t, err)
 
 	expectedMasterSK, err := bls.SecretKeyFromBigNum(masterSK)
@@ -51,8 +49,79 @@ func TestDerivationFromMnemonic(t *testing.T) {
 	require.NoError(t, err)
 	assert.DeepEqual(t, expectedMasterSK.Marshal(), withdrawalKey.Marshal())
 	assert.DeepEqual(t, expectedChildSK.Marshal(), validatingKey.Marshal())
-	fmt.Printf("Expected SK %#x\n", expectedMasterSK.Marshal())
-	fmt.Printf("Got %#x\n", withdrawalKey.Marshal())
-	fmt.Printf("Expected child SK %#x\n", expectedChildSK.Marshal())
-	fmt.Printf("Got %#x\n", validatingKey.Marshal())
+}
+
+func TestDerivationFromSeed(t *testing.T) {
+	type fields struct {
+		seed       string
+		childIndex int
+	}
+	type want struct {
+		masterSK string
+		childSK  string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   want
+	}{
+		{
+			name: "Case 1",
+			fields: fields{
+				seed:       "3141592653589793238462643383279502884197169399375105820974944592",
+				childIndex: 3141592653,
+			},
+			want: want{
+				masterSK: "29757020647961307431480504535336562678282505419141012933316116377660817309383",
+				childSK:  "25457201688850691947727629385191704516744796114925897962676248250929345014287",
+			},
+		},
+		{
+			name: "Case 2",
+			fields: fields{
+				seed:       "0099FF991111002299DD7744EE3355BBDD8844115566CC55663355668888CC00",
+				childIndex: 4294967295,
+			},
+			want: want{
+				masterSK: "27580842291869792442942448775674722299803720648445448686099262467207037398656",
+				childSK:  "29358610794459428860402234341874281240803786294062035874021252734817515685787",
+			},
+		},
+		{
+			name: "Case 3",
+			fields: fields{
+				seed:       "d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3",
+				childIndex: 42,
+			},
+			want: want{
+				masterSK: "19022158461524446591288038168518313374041767046816487870552872741050760015818",
+				childSK:  "31372231650479070279774297061823572166496564838472787488249775572789064611981",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "Case 1" || tt.name == "Case 2" {
+				t.Skip("Skipping due to https://github.com/wealdtech/go-eth2-util/issues/2")
+			}
+			seedBytes, err := hex.DecodeString(tt.fields.seed)
+			require.NoError(t, err)
+			km := &Keymanager{
+				seed: seedBytes,
+				opts: DefaultKeymanagerOpts(),
+			}
+			// We derive keys, then check the master SK and the child SK.
+			masterSK, err := km.deriveKey("m")
+			require.NoError(t, err)
+			childSK, err := km.deriveKey(fmt.Sprintf("m/%d", tt.fields.childIndex))
+			require.NoError(t, err)
+
+			expectedMasterSK, err := bls.SecretKeyFromBigNum(tt.want.masterSK)
+			require.NoError(t, err)
+			expectedChildSK, err := bls.SecretKeyFromBigNum(tt.want.childSK)
+			require.NoError(t, err)
+			assert.DeepEqual(t, expectedMasterSK.Marshal(), masterSK.Marshal())
+			assert.DeepEqual(t, expectedChildSK.Marshal(), childSK.Marshal())
+		})
+	}
 }
