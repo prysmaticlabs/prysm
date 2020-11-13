42681e34d6a9a91b3f46ab0885584912fe299c49
terence tsao <terence@prysmaticlabs.com>
2020-11-13 08:18:15 -0800

    Fixing interchange def part1

--- validator/slashing-protection/interchange.go
+++ validator/slashing-protection/interchange.go
@@ -3,75 +3,24 @@ package slashingprotection
 // Interchange defines the json scheme struct outlined in EIP3076.
 // https://eips.ethereum.org/EIPS/eip-3076#json-schema
 type Interchange struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Type        string `json:"type"`
-	Properties  struct {
-		Metadata struct {
-			Type       string `json:"type"`
+	Metadata struct {
+		InterchangeFormatVersion string `json:"interchange_format_version"`
+		GenesisValidatorsRoot    string `json:"genesis_validators_root"`
+	} `json:"metadata"`
+	Data []struct {
+		Pubkey       string `json:"pubkey"`
+		SignedBlocks []struct {
 			Properties struct {
-				InterchangeFormatVersion struct {
-					Type        string `json:"type"`
-					Description string `json:"description"`
-				} `json:"interchange_format_version"`
-				GenesisValidatorsRoot struct {
-					Type        string `json:"type"`
-					Description string `json:"description"`
-				} `json:"genesis_validators_root"`
+				Slot        string `json:"slot"`
+				SigningRoot string `json:"signing_root"`
 			} `json:"properties"`
-			Required []string `json:"required"`
-		} `json:"metadata"`
-		Data struct {
-			Type  string `json:"type"`
-			Items []struct {
-				Type       string `json:"type"`
-				Properties struct {
-					Pubkey struct {
-						Type        string `json:"type"`
-						Description string `json:"description"`
-					} `json:"pubkey"`
-					SignedBlocks struct {
-						Type  string `json:"type"`
-						Items []struct {
-							Type       string `json:"type"`
-							Properties struct {
-								Slot struct {
-									Type        string `json:"type"`
-									Description string `json:"description"`
-								} `json:"slot"`
-								SigningRoot struct {
-									Type        string `json:"type"`
-									Description string `json:"description"`
-								} `json:"signing_root"`
-							} `json:"properties"`
-							Required []string `json:"required"`
-						} `json:"items"`
-					} `json:"signed_blocks"`
-					SignedAttestations struct {
-						Type  string `json:"type"`
-						Items []struct {
-							Type       string `json:"type"`
-							Properties struct {
-								SourceEpoch struct {
-									Type        string `json:"type"`
-									Description string `json:"description"`
-								} `json:"source_epoch"`
-								TargetEpoch struct {
-									Type        string `json:"type"`
-									Description string `json:"description"`
-								} `json:"target_epoch"`
-								SigningRoot struct {
-									Type        string `json:"type"`
-									Description string `json:"description"`
-								} `json:"signing_root"`
-							} `json:"properties"`
-							Required []string `json:"required"`
-						} `json:"items"`
-					} `json:"signed_attestations"`
-				} `json:"properties"`
-				Required []string `json:"required"`
-			} `json:"items"`
-		} `json:"data"`
-	} `json:"properties"`
-	Required []string `json:"required"`
+		} `json:"signed_blocks"`
+		SignedAttestations []struct {
+			Properties struct {
+				SourceEpoch string `json:"source_epoch"`
+				TargetEpoch string `json:"target_epoch"`
+				SigningRoot string `json:"signing_root"`
+			} `json:"properties"`
+		} `json:"signed_attestations"`
+	} `json:"data"`
 }
