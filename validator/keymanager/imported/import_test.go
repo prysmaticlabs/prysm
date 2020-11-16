0e38286eae11f72e4f696182958993c582f035f7
dv8silencer <dv8silencer+github@gmail.com>
2020-11-16 16:03:06 -0600

    comments, wording

--- validator/keymanager/imported/import_test.go
+++ validator/keymanager/imported/import_test.go
@@ -104,7 +104,7 @@ func TestImportedKeymanager_ImportKeystores(t *testing.T) {
 		accountsStore: &AccountStore{},
 	}
 
-	// Create a duplicate keystore and attempt to import it.
+	// Create a duplicate keystore and attempt to import it. This should complete correctly though log specific output.
 	numAccounts := 5
 	keystores := make([]*keymanager.Keystore, numAccounts+1)
 	for i := 1; i < numAccounts+1; i++ {
