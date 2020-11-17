acb47f29202374fe46a979d496e02871aca46d9b
Shay Zluf <thezluf@gmail.com>
2020-11-18 00:37:43 +0200

    Implement Standard Slashing Protection JSON With Importing Logic (#7675)
    * Use new attestation protection
    
    * tests fixes
    
    * fix tests
    
    * fix comment
    
    * fix TestSetTargetData
    
    * fix tests
    
    * empty history handling
    
    * fix another test
    
    * mock domain request
    
    * fix empty handling
    
    * use far future epoch
    
    * use far future epoch
    
    * migrate data
    
    * copy byte array to resolve sigbus error
    
    * init validator protection on pre validation
    
    * Import interchange json
    
    * Import interchange json
    
    * reduce visibility
    
    * use return value
    
    * raul feedback
    
    * rename fixes
    
    * import test
    
    * checkout att v2 changes
    
    * define import method for interchange format in its own package
    
    * rename and made operations atomic
    
    * eip comment
    
    * begin amending test file
    
    * finish happy path for import tests
    
    * attempt the interchange import tests
    
    * fixed tests
    
    * happy and sad paths tested
    
    * good error messages
    
    * fix up comment with proper eip link
    
    * tests for helpers
    
    * helpers
    
    * all tests pass
    
    * proper test comment
    
    * terence feedback
    
    * validate metadata func
    
    * versioning check
    
    * begin handling duplicatesz
    
    * handle duplicate public keys with potentially different data, first pass
    
    * better handling of duplicate data
    
    * ensure duplicates are taken care of
    
    * comprehensive tests for deduplication of signed blocks
    
    * tests for deduplication
    
    * Update validator/slashing-protection/local/standard-protection-format/helpers_test.go
    
    Co-authored-by: Shay Zluf <thezluf@gmail.com>
    
    * Update validator/slashing-protection/local/standard-protection-format/helpers_test.go
    
    Co-authored-by: Shay Zluf <thezluf@gmail.com>
    
    * tests for maxuint64 and package level comment
    
    * tests passing
    
    * edge cases pass
    
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>

--- validator/db/iface/interface.go
+++ validator/db/iface/interface.go
@@ -22,6 +22,7 @@ type ValidatorDB interface {
 	//new data structure methods
 	ProposalHistoryForSlot(ctx context.Context, publicKey []byte, slot uint64) ([]byte, error)
 	SaveProposalHistoryForSlot(ctx context.Context, pubKey []byte, slot uint64, signingRoot []byte) error
+	SaveProposalHistoryForPubKeysV2(ctx context.Context, proposals map[[48]byte]kv.ProposalHistoryForPubkey) error
 
 	// Attester protection related methods.
 	AttestationHistoryForPubKeys(ctx context.Context, publicKeys [][48]byte) (map[[48]byte]*slashpb.AttestationHistory, error)
