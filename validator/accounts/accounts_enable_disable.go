4d2b9cac1c0fa2adfd28a77827532e7f30c7e3b3
Raul Jordan <raul@prysmaticlabs.com>
2020-11-16 20:40:04 -0600

    clarify imported

--- validator/accounts/accounts_enable_disable.go
+++ validator/accounts/accounts_enable_disable.go
@@ -11,7 +11,6 @@ import (
 	"github.com/prysmaticlabs/prysm/validator/accounts/prompt"
 	"github.com/prysmaticlabs/prysm/validator/accounts/wallet"
 	"github.com/prysmaticlabs/prysm/validator/flags"
-	"github.com/prysmaticlabs/prysm/validator/keymanager"
 	"github.com/prysmaticlabs/prysm/validator/keymanager/imported"
 	"github.com/urfave/cli/v2"
 )
@@ -82,26 +81,14 @@ func DisableAccountsCli(cliCtx *cli.Context) error {
 			}
 		}
 	}
-	switch w.KeymanagerKind() {
-	case keymanager.Imported:
-		importedKM, ok := km.(*imported.Keymanager)
-		if !ok {
-			return errors.New("not an imported keymanager")
-		}
-		if err := importedKM.DisableAccounts(cliCtx.Context, rawPublicKeys); err != nil {
-			return err
-		}
-		log.WithField("publicKeys", allAccountStr).Info("Accounts disabled")
-	case keymanager.Derived:
-		//derivedKM, ok := km.(*derived.Keymanager)
-		//if !ok {
-		//	return errors.New("not an derived keymanager")
-		//}
-		//if err := derivedKM.DisableAccounts(cliCtx.Context, rawPublicKeys); err != nil {
-		//	return err
-		//}
-		//log.WithField("publicKeys", allAccountStr).Info("Accounts disabled")
+	importedKM, ok := km.(*imported.Keymanager)
+	if !ok {
+		return errors.New("can only disable accounts for imported wallets")
 	}
+	if err := importedKM.DisableAccounts(cliCtx.Context, rawPublicKeys); err != nil {
+		return err
+	}
+	log.WithField("publicKeys", allAccountStr).Info("Accounts disabled")
 	return nil
 }
 
@@ -117,78 +104,69 @@ func EnableAccountsCli(cliCtx *cli.Context) error {
 	if err != nil {
 		return errors.Wrap(err, "could not initialize keymanager")
 	}
-	switch w.KeymanagerKind() {
-	case keymanager.Remote:
-		return errors.New("cannot enable accounts for a remote keymanager")
-	case keymanager.Imported:
-		km, ok := km.(*imported.Keymanager)
-		if !ok {
-			return errors.New("not a imported keymanager")
-		}
-		disabledPublicKeys := km.DisabledPublicKeys()
-		if len(disabledPublicKeys) == 0 {
-			return errors.New("No accounts are disabled.")
-		}
-		disabledPublicKeys48 := make([][48]byte, len(disabledPublicKeys))
-		for i := range disabledPublicKeys {
-			disabledPublicKeys48[i] = bytesutil.ToBytes48(disabledPublicKeys[i])
-		}
+	importedKM, ok := km.(*imported.Keymanager)
+	if !ok {
+		return errors.New("can only enable/disable accounts for imported wallets")
+	}
+	disabledPublicKeys := importedKM.DisabledPublicKeys()
+	if len(disabledPublicKeys) == 0 {
+		return errors.New("No accounts are disabled.")
+	}
+	disabledPublicKeys48 := make([][48]byte, len(disabledPublicKeys))
+	for i := range disabledPublicKeys {
+		disabledPublicKeys48[i] = bytesutil.ToBytes48(disabledPublicKeys[i])
+	}
 
-		// Allow the user to interactively select the accounts to enable or optionally
-		// provide them via cli flags as a string of comma-separated, hex strings.
-		filteredPubKeys, err := filterPublicKeysFromUserInput(
-			cliCtx,
-			flags.EnablePublicKeysFlag,
-			disabledPublicKeys48,
-			prompt.SelectAccountsEnablePromptText,
-		)
-		if err != nil {
-			return errors.Wrap(err, "could not filter public keys for activation")
-		}
-		rawPublicKeys := make([][]byte, len(filteredPubKeys))
-		formattedPubKeys := make([]string, len(filteredPubKeys))
-		for i, pk := range filteredPubKeys {
-			pubKeyBytes := pk.Marshal()
-			rawPublicKeys[i] = pubKeyBytes
-			formattedPubKeys[i] = fmt.Sprintf("%#x", bytesutil.Trunc(pubKeyBytes))
-		}
-		allAccountStr := strings.Join(formattedPubKeys, ", ")
-		if !cliCtx.IsSet(flags.EnablePublicKeysFlag.Name) {
-			if len(filteredPubKeys) == 1 {
-				promptText := "Are you sure you want to enable 1 account? (%s) Y/N"
-				resp, err := promptutil.ValidatePrompt(
-					os.Stdin, fmt.Sprintf(promptText, au.BrightGreen(formattedPubKeys[0])), promptutil.ValidateYesOrNo,
-				)
-				if err != nil {
-					return err
-				}
-				if strings.ToLower(resp) == "n" {
-					return nil
-				}
+	// Allow the user to interactively select the accounts to enable or optionally
+	// provide them via cli flags as a string of comma-separated, hex strings.
+	filteredPubKeys, err := filterPublicKeysFromUserInput(
+		cliCtx,
+		flags.EnablePublicKeysFlag,
+		disabledPublicKeys48,
+		prompt.SelectAccountsEnablePromptText,
+	)
+	if err != nil {
+		return errors.Wrap(err, "could not filter public keys for activation")
+	}
+	rawPublicKeys := make([][]byte, len(filteredPubKeys))
+	formattedPubKeys := make([]string, len(filteredPubKeys))
+	for i, pk := range filteredPubKeys {
+		pubKeyBytes := pk.Marshal()
+		rawPublicKeys[i] = pubKeyBytes
+		formattedPubKeys[i] = fmt.Sprintf("%#x", bytesutil.Trunc(pubKeyBytes))
+	}
+	allAccountStr := strings.Join(formattedPubKeys, ", ")
+	if !cliCtx.IsSet(flags.EnablePublicKeysFlag.Name) {
+		if len(filteredPubKeys) == 1 {
+			promptText := "Are you sure you want to enable 1 account? (%s) Y/N"
+			resp, err := promptutil.ValidatePrompt(
+				os.Stdin, fmt.Sprintf(promptText, au.BrightGreen(formattedPubKeys[0])), promptutil.ValidateYesOrNo,
+			)
+			if err != nil {
+				return err
+			}
+			if strings.ToLower(resp) == "n" {
+				return nil
+			}
+		} else {
+			promptText := "Are you sure you want to enable %d accounts? (%s) Y/N"
+			if len(filteredPubKeys) == len(disabledPublicKeys48) {
+				promptText = fmt.Sprintf("Are you sure you want to enable all accounts? Y/N (%s)", au.BrightGreen(allAccountStr))
 			} else {
-				promptText := "Are you sure you want to enable %d accounts? (%s) Y/N"
-				if len(filteredPubKeys) == len(disabledPublicKeys48) {
-					promptText = fmt.Sprintf("Are you sure you want to enable all accounts? Y/N (%s)", au.BrightGreen(allAccountStr))
-				} else {
-					promptText = fmt.Sprintf(promptText, len(filteredPubKeys), au.BrightGreen(allAccountStr))
-				}
-				resp, err := promptutil.ValidatePrompt(os.Stdin, promptText, promptutil.ValidateYesOrNo)
-				if err != nil {
-					return err
-				}
-				if strings.ToLower(resp) == "n" {
-					return nil
-				}
+				promptText = fmt.Sprintf(promptText, len(filteredPubKeys), au.BrightGreen(allAccountStr))
+			}
+			resp, err := promptutil.ValidatePrompt(os.Stdin, promptText, promptutil.ValidateYesOrNo)
+			if err != nil {
+				return err
+			}
+			if strings.ToLower(resp) == "n" {
+				return nil
 			}
 		}
-		if err := km.EnableAccounts(cliCtx.Context, rawPublicKeys); err != nil {
-			return err
-		}
-		log.WithField("publicKeys", allAccountStr).Info("Accounts enabled")
-		return nil
-	case keymanager.Derived:
-		return errors.New("cannot enable accounts for a derived keymanager")
-	default:
-		return fmt.Errorf("keymanager kind %s not supported", w.KeymanagerKind())
 	}
+	if err := importedKM.EnableAccounts(cliCtx.Context, rawPublicKeys); err != nil {
+		return err
+	}
+	log.WithField("publicKeys", allAccountStr).Info("Accounts enabled")
+	return nil
 }
