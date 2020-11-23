658dd953134877b0b66c3f50101a81836b231464
Preston Van Loon <preston@prysmaticlabs.com>
2020-11-22 17:20:55 -0800

    Add teku's bootnode (#7919)
    * Add teku's bootnode
    
    * Ignore mainnet config for TODO check

--- shared/params/mainnet_config.go
+++ shared/params/mainnet_config.go
@@ -35,7 +35,10 @@ var mainnetNetworkConfig = &NetworkConfig{
 	DepositContractAddress:            "0x00000000219ab540356cBB839Cbe05303d7705Fa",
 	ChainID:                           1, // Chain ID of eth1 mainnet.
 	NetworkID:                         1, // Network ID of eth1 mainnet.
-	BootstrapNodes:                    []string{},
+	BootstrapNodes: []string{
+		// Teku team's bootnode
+		"enr:-KG4QOtcP9X1FbIMOe17QNMKqDxCpm14jcX5tiOE4_TyMrFqbmhPZHK_ZPG2Gxb1GE2xdtodOfx9-cgvNtxnRyHEmC0ghGV0aDKQ9aX9QgAAAAD__4JpZIJ2NIJpcIQDE8KdiXNlY3AyNTZrMaEDhpehBDbZjM_L9ek699Y7vhUJ-eAdMyQW_Fil522Y0fODdGNwgiMog3VkcIIjKA",
+	},
 }
 
 var mainnetBeaconConfig = &BeaconChainConfig{
