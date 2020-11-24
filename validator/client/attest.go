b5c16348fd6ce319fa71c5ac505ef2ef7c815998
shayzluf <thezluf@gmail.com>
2020-11-24 08:34:02 +0200

    move save before propose

--- validator/client/attest.go
+++ validator/client/attest.go
@@ -113,7 +113,9 @@ func (v *validator) SubmitAttestation(ctx context.Context, slot uint64, pubKey [
 		).Debug("Attempted slashable attestation details")
 		return
 	}
-
+	if err := v.SaveProtection(ctx, pubKey); err != nil {
+		log.WithError(err).Errorf("Could not save validator: %#x protection", pubKey)
+	}
 	attResp, err := v.validatorClient.ProposeAttestation(ctx, attestation)
 	if err != nil {
 		log.WithError(err).Error("Could not submit attestation to beacon node")
@@ -121,9 +123,6 @@ func (v *validator) SubmitAttestation(ctx context.Context, slot uint64, pubKey [
 			ValidatorAttestFailVec.WithLabelValues(fmtKey).Inc()
 		}
 		return
-	}
-	if err := v.SaveProtection(ctx, pubKey); err != nil {
-		log.WithError(err).Errorf("Could not save validator: %#x protection", pubKey)
 	}
 
 	if err := v.saveAttesterIndexToData(data, duty.ValidatorIndex); err != nil {
