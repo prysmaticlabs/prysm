ce0cb42da504b9fd73c49e51ddd26ab3a7a88f49
shayzluf <thezluf@gmail.com>
2020-11-24 09:59:45 +0200

    wait before go func

--- validator/client/runner.go
+++ validator/client/runner.go
@@ -154,10 +154,9 @@ func run(ctx context.Context, v Validator) {
 				}
 			}
 			// Wait for all processes to complete, then report span complete.
+			wg.Wait()
+			v.LogAttestationsSubmitted()
 			go func() {
-				wg.Wait()
-				v.ResetAttesterProtectionData()
-				v.LogAttestationsSubmitted()
 				// Log this client performance in the previous epoch
 				if err := v.LogValidatorGainsAndLosses(slotCtx, slot); err != nil {
 					log.WithError(err).Error("Could not report validator's rewards/penalties")
