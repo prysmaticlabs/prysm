d9f478231820731c96859f1b996e7f5ccb208dd8
Terence Tsao <terence@prysmaticlabs.com>
2020-11-29 09:51:08 -0800

    Fix tests

--- beacon-chain/state/stategen/getter_test.go
+++ beacon-chain/state/stategen/getter_test.go
@@ -244,7 +244,7 @@ func TestStateSummary_CanGetFromCacheOrDB(t *testing.T) {
 	r = [32]byte{'b'}
 	summary = &pb.StateSummary{Root: r[:], Slot: 101}
 	_, err = service.stateSummary(ctx, r)
-	require.ErrorContains(t, errUnknownStateSummary.Error(), err)
+	require.ErrorContains(t, "could not find block in DB", err)
 
 	require.NoError(t, service.beaconDB.SaveStateSummary(ctx, summary))
 	got, err = service.stateSummary(ctx, r)
