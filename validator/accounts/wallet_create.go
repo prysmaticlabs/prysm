33b301695ccdf6b863f1087ab8f3dddaf5c1b5eb
Radosław Kapka <rkapka@wp.pl>
2020-11-30 10:20:26 +0100

    correct errror message

--- validator/accounts/wallet_create.go
+++ validator/accounts/wallet_create.go
@@ -36,7 +36,7 @@ func CreateAndSaveWalletCli(cliCtx *cli.Context) (*wallet.Wallet, error) {
 		return nil, err
 	}
 	if keymanagerKind == keymanager.Imported {
-		return nil, errors.New("could not initialize wallet for imported accounts. Please use `validator accounts import` instead")
+		return nil, errors.New("could not initialize wallet for imported accounts - please use `validator accounts import` instead")
 	}
 	createWalletConfig, err := extractWalletCreationConfigFromCli(cliCtx, keymanagerKind)
 	if err != nil {
