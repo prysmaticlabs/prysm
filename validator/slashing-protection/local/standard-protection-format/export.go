4b7a8d6cf2537a4dabc5cb7bdd9a818596f85a32
Raul Jordan <raul@prysmaticlabs.com>
2020-12-02 11:06:25 -0600

    comment

--- validator/slashing-protection/local/standard-protection-format/export.go
+++ validator/slashing-protection/local/standard-protection-format/export.go
@@ -56,6 +56,10 @@ func ExportStandardProtectionJSON(ctx context.Context, validatorDB db.Database) 
 }
 
 func getSignedBlocksByPubKey(ctx context.Context, validatorDB db.Database, pubKey [48]byte) ([]*SignedBlock, error) {
+	// If a key does not have a lowest or highest signed proposal history
+	// in our database, we return nil. This way, a user will be able to export their
+	// slashing protection history even if one of their keys does not have a history
+	// of signed blocks.
 	lowestSignedSlot, exists, err := validatorDB.LowestSignedProposal(ctx, pubKey)
 	if err != nil {
 		return nil, err
