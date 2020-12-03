c090c6a1c52bb11a5bf203ff0cdbc9804cb2bccd
terence tsao <terence@prysmaticlabs.com>
2020-12-03 12:38:00 -0800

    Revert "Logging with `PadLevelText` to true" (#8036)
    This reverts commit c51754fa8a3cd2d731ab772352b5a2ef92153621.
    
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- validator/main.go
+++ validator/main.go
@@ -23,6 +23,7 @@ import (
 	"github.com/prysmaticlabs/prysm/validator/node"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
 var log = logrus.WithField("prefix", "main")
@@ -118,13 +119,12 @@ func main() {
 		format := ctx.String(cmd.LogFormat.Name)
 		switch format {
 		case "text":
-			formatter := new(logrus.TextFormatter)
+			formatter := new(prefixed.TextFormatter)
 			formatter.TimestampFormat = "2006-01-02 15:04:05"
 			formatter.FullTimestamp = true
 			// If persistent log files are written - we disable the log messages coloring because
 			// the colors are ANSI codes and seen as Gibberish in the log files.
 			formatter.DisableColors = ctx.String(cmd.LogFileName.Name) != ""
-			formatter.PadLevelText = true
 			logrus.SetFormatter(formatter)
 		case "fluentd":
 			f := joonix.NewFormatter()
