fe3f9e156886a48bca789836ea68852f7bef362f
terence <terence@prysmaticlabs.com>
2020-12-07 11:27:52 -0800

    Remove custom block body root and block root methods

--- shared/blockutil/block_utils.go
+++ shared/blockutil/block_utils.go
@@ -3,12 +3,11 @@ package blockutil
 import (
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	"github.com/prysmaticlabs/prysm/beacon-chain/state/stateutil"
 )
 
 // SignedBeaconBlockHeaderFromBlock function to retrieve signed block header from block.
 func SignedBeaconBlockHeaderFromBlock(block *ethpb.SignedBeaconBlock) (*ethpb.SignedBeaconBlockHeader, error) {
-	bodyRoot, err := stateutil.BlockBodyRoot(block.Block.Body)
+	bodyRoot, err := block.Block.Body.HashTreeRoot()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get body root of block")
 	}
@@ -26,7 +25,7 @@ func SignedBeaconBlockHeaderFromBlock(block *ethpb.SignedBeaconBlock) (*ethpb.Si
 
 // BeaconBlockHeaderFromBlock function to retrieve block header from block.
 func BeaconBlockHeaderFromBlock(block *ethpb.BeaconBlock) (*ethpb.BeaconBlockHeader, error) {
-	bodyRoot, err := stateutil.BlockBodyRoot(block.Body)
+	bodyRoot, err := block.Body.HashTreeRoot()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get body root of block")
 	}
