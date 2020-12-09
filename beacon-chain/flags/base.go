8e832764faa1b7a6cc7670acf5161967078adee7
shayzluf <thezluf@gmail.com>
2020-12-09 15:47:50 +0200

    usage update

--- beacon-chain/flags/base.go
+++ beacon-chain/flags/base.go
@@ -11,7 +11,7 @@ var (
 	// HTTPWeb3ProviderFlag provides an HTTP access endpoint to an ETH 1.0 RPC.
 	HTTPWeb3ProviderFlag = &cli.StringFlag{
 		Name:  "http-web3provider",
-		Usage: "A mainchain web3 provider string http endpoint",
+		Usage: "A mainchain web3 provider string http endpoint. Can contain auth header as well in the format --http-web3provider=\"https://goerli.infura.io/v3/xxxx,Basic xxx\" for project secret(base64 encoded) and --http-web3provider=\"https://goerli.infura.io/v3/xxxx,Bearer xxx\" for jwt use",
 		Value: "",
 	}
 	// DepositContractFlag defines a flag for the deposit contract address.
