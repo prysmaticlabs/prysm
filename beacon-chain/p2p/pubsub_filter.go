70c0bb106b8efe0d6bd0e122b272aee1cde32b6c
terence tsao <terence@prysmaticlabs.com>
2020-12-14 15:41:24 -0800

    Beacon node code health improvements (#8109)
    * Apply code health fixes after code inspect
    
    * Remove attestationRoot
    
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>

--- beacon-chain/p2p/pubsub_filter.go
+++ beacon-chain/p2p/pubsub_filter.go
@@ -73,7 +73,7 @@ func scanfcheck(input, format string) (int, error) {
 	// This method only checks that the input conforms to the format, the arguments are not used and
 	// therefore we can reuse the same integer pointer.
 	var cnt = strings.Count(format, "%")
-	var args = []interface{}{}
+	var args []interface{}
 	for i := 0; i < cnt; i++ {
 		args = append(args, &t)
 	}
