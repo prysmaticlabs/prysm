079a5018f3e048b90f4987cda748c8d1b001f0dc
rauljordan <raul@prysmaticlabs.com>
2020-12-16 14:56:14 -0600

    implement remaining endpoint

--- validator/rpc/beacon.go
+++ validator/rpc/beacon.go
@@ -12,13 +12,13 @@ import (
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
-
 	pb "github.com/prysmaticlabs/prysm/proto/validator/accounts/v2"
 	"github.com/prysmaticlabs/prysm/validator/client"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
+// Initialize a client connect to a beacon node gRPC endpoint.
 func (s *Server) registerBeaconClient() error {
 	streamInterceptor := grpc.WithStreamInterceptor(middleware.ChainStreamClient(
 		grpc_opentracing.StreamClientInterceptor(),
@@ -57,47 +57,67 @@ func (s *Server) registerBeaconClient() error {
 	return nil
 }
 
+// GetBeaconStatus retrieves information about the beacon node gRPC connection
+// and certain chain metadata, such as the genesis time, the chain head, and the
+// deposit contract address.
 func (s *Server) GetBeaconStatus(ctx context.Context, _ *ptypes.Empty) (*pb.BeaconStatusResponse, error) {
-	genesis, err := s.genesisFetcher.GenesisInfo(ctx)
+	syncStatus, err := s.beaconNodeClient.GetSyncStatus(ctx, &ptypes.Empty{})
+	if err != nil {
+		return &pb.BeaconStatusResponse{
+			BeaconNodeEndpoint: s.nodeGatewayEndpoint,
+			Connected:          false,
+			Syncing:            false,
+		}, nil
+	}
+	genesis, err := s.beaconNodeClient.GetGenesis(ctx, &ptypes.Empty{})
 	if err != nil {
 		return nil, err
 	}
 	genesisTime := uint64(time.Unix(genesis.GenesisTime.Seconds, 0).Unix())
 	address := genesis.DepositContractAddress
+	chainHead, err := s.beaconChainClient.GetChainHead(ctx, &ptypes.Empty{})
+	if err != nil {
+		return nil, err
+	}
 	return &pb.BeaconStatusResponse{
 		BeaconNodeEndpoint:     s.beaconClientEndpoint,
-		Connected:              false,
-		Syncing:                false,
+		Connected:              true,
+		Syncing:                syncStatus.Syncing,
 		GenesisTime:            genesisTime,
 		DepositContractAddress: address,
-		ChainHead:              nil,
+		ChainHead:              chainHead,
 	}, nil
 }
 
+// GetValidatorParticipation is a wrapper around the /eth/v1alpha1 endpoint of the same name.
 func (s *Server) GetValidatorParticipation(
 	ctx context.Context, req *ethpb.GetValidatorParticipationRequest,
 ) (*ethpb.ValidatorParticipationResponse, error) {
 	return s.beaconChainClient.GetValidatorParticipation(ctx, req)
 }
 
+// GetValidatorPerformance is a wrapper around the /eth/v1alpha1 endpoint of the same name.
 func (s *Server) GetValidatorPerformance(
 	ctx context.Context, req *ethpb.ValidatorPerformanceRequest,
 ) (*ethpb.ValidatorPerformanceResponse, error) {
 	return s.beaconChainClient.GetValidatorPerformance(ctx, req)
 }
 
+// GetValidatorBalance is a wrapper around the /eth/v1alpha1 endpoint of the same name.
 func (s *Server) GetValidatorBalances(
 	ctx context.Context, req *ethpb.ListValidatorBalancesRequest,
 ) (*ethpb.ValidatorBalances, error) {
 	return s.beaconChainClient.ListValidatorBalances(ctx, req)
 }
 
+// GetValidatorQueue is a wrapper around the /eth/v1alpha1 endpoint of the same name.
 func (s *Server) GetValidatorQueue(
 	ctx context.Context, req *ptypes.Empty,
 ) (*ethpb.ValidatorQueue, error) {
 	return s.beaconChainClient.GetValidatorQueue(ctx, req)
 }
 
+// GetPeers is a wrapper around the /eth/v1alpha1 endpoint of the same name.
 func (s *Server) GetPeers(
 	ctx context.Context, req *ptypes.Empty,
 ) (*ethpb.Peers, error) {
