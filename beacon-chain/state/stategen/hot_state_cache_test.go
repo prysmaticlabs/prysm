96a28f8255fe663ea92989aeb2e1c1f5a5e99e46
terence tsao <terence@prysmaticlabs.com>
2020-12-17 11:33:22 -0800

    Move hot state cache to stategen

--- beacon-chain/state/stategen/hot_state_cache_test.go
+++ beacon-chain/state/stategen/hot_state_cache_test.go
@@ -1,9 +1,8 @@
-package cache_test
+package stategen
 
 import (
 	"testing"
 
-	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
 	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
@@ -11,24 +10,24 @@ import (
 )
 
 func TestHotStateCache_RoundTrip(t *testing.T) {
-	c := cache.NewHotStateCache()
+	c := newHotStateCache()
 	root := [32]byte{'A'}
-	state := c.Get(root)
+	state := c.get(root)
 	assert.Equal(t, (*stateTrie.BeaconState)(nil), state)
-	assert.Equal(t, false, c.Has(root), "Empty cache has an object")
+	assert.Equal(t, false, c.has(root), "Empty cache has an object")
 
 	state, err := stateTrie.InitializeFromProto(&pb.BeaconState{
 		Slot: 10,
 	})
 	require.NoError(t, err)
 
-	c.Put(root, state)
-	assert.Equal(t, true, c.Has(root), "Empty cache does not have an object")
+	c.put(root, state)
+	assert.Equal(t, true, c.has(root), "Empty cache does not have an object")
 
-	res := c.Get(root)
+	res := c.get(root)
 	assert.NotNil(t, state)
 	assert.DeepEqual(t, res.CloneInnerState(), state.CloneInnerState(), "Expected equal protos to return from cache")
 
-	c.Delete(root)
-	assert.Equal(t, false, c.Has(root), "Cache not supposed to have the object")
+	c.delete(root)
+	assert.Equal(t, false, c.has(root), "Cache not supposed to have the object")
 }
