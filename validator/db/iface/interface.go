8d53fda16501ab3d1d4796a4e47aa5da3d5cd7aa
shayzluf <thezluf@gmail.com>
2021-01-04 23:01:45 +0200

    fix import order

--- validator/db/iface/interface.go
+++ validator/db/iface/interface.go
@@ -3,9 +3,9 @@ package iface
 
 import (
 	"context"
-	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"io"
 
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/backuputil"
 	"github.com/prysmaticlabs/prysm/validator/db/kv"
 )
