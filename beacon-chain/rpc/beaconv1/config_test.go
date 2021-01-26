0783d48da359e4ff0235e63a0d846a6de2ab35ca
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-01-26 16:21:27 -0500

    Fixes

--- beacon-chain/rpc/beaconv1/config_test.go
+++ beacon-chain/rpc/beaconv1/config_test.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"testing"
 
-	ptypes "github.com/gogo/protobuf/types"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func TestGetDepositContract(t *testing.T) {
@@ -21,7 +21,7 @@ func TestGetDepositContract(t *testing.T) {
 	params.OverrideBeaconConfig(config)
 
 	s := Server{}
-	resp, err := s.GetDepositContract(context.Background(), &ptypes.Empty{})
+	resp, err := s.GetDepositContract(context.Background(), &emptypb.Empty{})
 	require.NoError(t, err)
 	assert.Equal(t, uint64(chainId), resp.Data.ChainId)
 	assert.Equal(t, address, resp.Data.Address)
