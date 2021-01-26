bf9ad7f1b68a6f0db81190f194c6c8cf5ea5a011
Raul Jordan <raul@prysmaticlabs.com>
2021-01-25 19:44:21 -0600

    thorough comments

--- beacon-chain/slasher/config.go
+++ beacon-chain/slasher/config.go
@@ -1,48 +1,133 @@
 package slasher
 
-// Config parameters for slashing detection.
+// Config parameters for slashing detection. Validator min and max spans
+// split into chunks of length C for easy detection and lazy loading.
+//
+// A regular pair of min and max spans for a validator look as follows
+// with length = H where H is the amount of epochs worth of history
+// we want to persist for slashing detection.
+//
+//  validator_1_min_span = [2, 2, 2, ..., 2]
+//  validator_1_max_span = [0, 0, 0, ..., 0]
+//
+// After chunking, we obtain chunks of length C. For C = 3:
+//
+//  validator_1_min_span_chunk_0 = [2, 2, 2]
+//  validator_1_max_span_chunk_0 = [2, 2, 2]
+//
+// On disk, we take chunks for K validators, and store them as flat byte slices.
+// For example, if H = 3, C = 3, and K = 3, then we can store 3 validators' chunks as a flat
+// slice as follows:
+//
+//     val0     val1     val2
+//      |        |        |
+//   {     }  {     }  {     }
+//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+//
+// This is known as 2D chunking, pioneered by the Sigma Prime team here:
+// https://hackmd.io/@sproul/min-max-slasher
+//
+// To properly access the element at epoch `e` for a validator index `i`, we leverage helper
+// functions from config values as a nice abstraction. the following parameters are
+// required for the helper functions defined in this file.
+//
+// ChunkSize defines how many elements are in a chunk for a validator
+// min or max span slice.
+// ValidatorChunkSize defines how many validators' chunks we store in a single
+// flat byte slice on disk.
+// HistoryLength defines how many epochs we keep of min or max spans.
 type Config struct {
 	ChunkSize          uint64
 	ValidatorChunkSize uint64
 	HistoryLength      uint64
 }
 
-// DefaultConfig --
+// DefaultConfig defines default values for slasher's configuration, defined
+// based on optimization analysis for best and worst case scenarios for
+// slasher's performance.
 func DefaultConfig() *Config {
 	return &Config{
+		// The default values for ChunkSize and ValidatorChunkSize were
+		// decided after an optimization analysis performed by the Sigma Prime team.
+		// See: https://hackmd.io/@sproul/min-max-slasher#1D-vs-2D for more information.
+		// We decide to keep 4096 epochs worth of data in each validator's min max spans.
 		ChunkSize:          16,
 		ValidatorChunkSize: 256,
 		HistoryLength:      4096,
 	}
 }
 
+// Validator min and max spans are split into chunks of length C = ChunkSize.
+// That is, if we are keeping N epochs worth of attesting history, finding what
+// chunk a certain epoch, e, falls into can be computed as (e % N) / C. For example,
+// if we are keeping 6 epochs worth of data, and we have chunks of size 2, then epoch
+// 4 will fall into chunk index (4 % 6) / 2 = 2.
+//
+//  span    = [2, 2, 2, 2, 2, 2]
+//  chunked = [[2, 2], [2, 2], [2, 2]]
+//                              |-> epoch 4, chunk idx 2
 func (c *Config) chunkIndex(epoch uint64) uint64 {
 	return (epoch % c.HistoryLength) / c.ChunkSize
 }
 
+// When storing data on disk, we take K validators' chunks. To figure out
+// which validator chunk index a validator index is for, we simply divide
+// the validator index, v, by K.
 func (c *Config) validatorChunkIndex(validatorIndex uint64) uint64 {
 	return validatorIndex / c.ValidatorChunkSize
 }
 
+// Given a validator index, and epoch, we compute the exact index
+// into our flat slice on disk which stores K validators' chunks, each
+// chunk of size C. For example, if C = 3 and K = 3, the data we store
+// on disk is a flat slice as follows:
+//
+//     val0     val1     val2
+//      |        |        |
+//   {     }  {     }  {     }
+//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+//
+// Then, figuring out the exact cell index for epoch 1 for validator 2 is computed
+// with (validatorIndex % K)*C + (epoch % C), which gives us:
+//
+//  (2 % 3)*3 + (1 % 3) =
+//  (2*3) + (1)         =
+//  7
+//
+//     val0     val1     val2
+//      |        |        |
+//   {     }  {     }  {     }
+//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+//                        |-> epoch 1 for val2
+//
+func (c *Config) cellIndex(validatorIndex uint64, epoch uint64) uint64 {
+	validatorChunkOffset := c.validatorOffset(validatorIndex)
+	chunkOffset := c.chunkOffset(epoch)
+	return validatorChunkOffset*c.ChunkSize + chunkOffset
+}
+
+// Computes the start index of a chunk given an epoch.
 func (c *Config) chunkOffset(epoch uint64) uint64 {
 	return epoch % c.ChunkSize
 }
 
+// Computes the start index of a validator chunk given a validator index.
 func (c *Config) validatorOffset(validatorIndex uint64) uint64 {
 	return validatorIndex % c.ValidatorChunkSize
 }
 
-// Map the validator and epoch chunk indexes into a single value for use as a database key.
-func (c *Config) diskKey(validatorChunkIndex uint64, chunkIndex uint64) uint64 {
+// Construct a key for our database schema given a validator index and epoch.
+// This calculation gives us a uint that uniquely represents
+// a 2D chunk given a validator index and epoch value.
+func (c *Config) diskKey(validatorIndex uint64, epoch uint64) uint64 {
+	validatorChunkIndex := c.validatorChunkIndex(validatorIndex)
+	chunkIndex := c.chunkIndex(epoch)
 	width := c.HistoryLength / c.ChunkSize
 	return validatorChunkIndex*width + chunkIndex
 }
 
-// Map the validator and epoch offsets into an index for chunk data.
-func (c *Config) cellIndex(validatorOffset uint64, chunkOffset uint64) uint64 {
-	return validatorOffset*c.ChunkSize + chunkOffset
-}
-
+// Given a validator chunk index, we determine all of the validator
+// indices that will belong in that chunk.
 func (c *Config) validatorIndicesInChunk(validatorChunkIdx uint64) []uint64 {
 	validatorIndices := make([]uint64, 0)
 	low := validatorChunkIdx * c.ValidatorChunkSize
--- beacon-chain/slasher/config_test.go
+++ beacon-chain/slasher/config_test.go
@@ -6,22 +6,130 @@ import (
 )
 
 func TestConfig_cellIndex(t *testing.T) {
-	type fields struct {
-		ChunkSize          uint64
-		ValidatorChunkSize uint64
-		HistoryLength      uint64
-	}
 	type args struct {
-		validatorOffset uint64
-		chunkOffset     uint64
+		validatorIndex uint64
+		epoch          uint64
 	}
 	tests := []struct {
 		name   string
-		fields fields
+		fields *Config
 		args   args
 		want   uint64
 	}{
-		// TODO: Add test cases.
+		{
+			name: "epoch 0 and validator index 0",
+			fields: &Config{
+				ChunkSize:          3,
+				ValidatorChunkSize: 3,
+			},
+			args: args{
+				validatorIndex: 0,
+				epoch:          0,
+			},
+			want: 0,
+		},
+		{
+			//     val0     val1     val2
+			//      |        |        |
+			//   {     }  {     }  {     }
+			//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+			//                        |-> epoch 1, validator 2
+			name: "epoch < ChunkSize and validatorIndex < ValidatorChunkSize",
+			fields: &Config{
+				ChunkSize:          3,
+				ValidatorChunkSize: 3,
+			},
+			args: args{
+				validatorIndex: 2,
+				epoch:          1,
+			},
+			want: 7,
+		},
+		{
+			//     val0     val1     val2
+			//      |        |        |
+			//   {     }  {     }  {     }
+			//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+			//                        |-> epoch 4, validator 2 (wrap around)
+			name: "epoch > ChunkSize and validatorIndex < ValidatorChunkSize",
+			fields: &Config{
+				ChunkSize:          3,
+				ValidatorChunkSize: 3,
+			},
+			args: args{
+				validatorIndex: 2,
+				epoch:          4,
+			},
+			want: 7,
+		},
+		{
+			//     val0     val1     val2
+			//      |        |        |
+			//   {     }  {     }  {     }
+			//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+			//                     |-> epoch 3, validator 2 (wrap around)
+			name: "epoch = ChunkSize and validatorIndex < ValidatorChunkSize",
+			fields: &Config{
+				ChunkSize:          3,
+				ValidatorChunkSize: 3,
+			},
+			args: args{
+				validatorIndex: 2,
+				epoch:          3,
+			},
+			want: 6,
+		},
+		{
+			//     val0     val1     val2
+			//      |        |        |
+			//   {     }  {     }  {     }
+			//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+			//   |-> epoch 0, validator 3 (wrap around)
+			name: "epoch < ChunkSize and validatorIndex = ValidatorChunkSize",
+			fields: &Config{
+				ChunkSize:          3,
+				ValidatorChunkSize: 3,
+			},
+			args: args{
+				validatorIndex: 3,
+				epoch:          0,
+			},
+			want: 0,
+		},
+		{
+			//     val0     val1     val2
+			//      |        |        |
+			//   {     }  {     }  {     }
+			//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+			//            |-> epoch 0, validator 4 (wrap around)
+			name: "epoch < ChunkSize and validatorIndex > ValidatorChunkSize",
+			fields: &Config{
+				ChunkSize:          3,
+				ValidatorChunkSize: 3,
+			},
+			args: args{
+				validatorIndex: 4,
+				epoch:          0,
+			},
+			want: 3,
+		},
+		{
+			//     val0     val1     val2
+			//      |        |        |
+			//   {     }  {     }  {     }
+			//  [2, 2, 2, 2, 2, 2, 2, 2, 2]
+			//   |-> epoch 3, validator 3 (wrap around)
+			name: "epoch = ChunkSize and validatorIndex = ValidatorChunkSize",
+			fields: &Config{
+				ChunkSize:          3,
+				ValidatorChunkSize: 3,
+			},
+			args: args{
+				validatorIndex: 3,
+				epoch:          3,
+			},
+			want: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -30,7 +138,7 @@ func TestConfig_cellIndex(t *testing.T) {
 				ValidatorChunkSize: tt.fields.ValidatorChunkSize,
 				HistoryLength:      tt.fields.HistoryLength,
 			}
-			if got := c.cellIndex(tt.args.validatorOffset, tt.args.chunkOffset); got != tt.want {
+			if got := c.cellIndex(tt.args.validatorIndex, tt.args.epoch); got != tt.want {
 				t.Errorf("cellIndex() = %v, want %v", got, tt.want)
 			}
 		})
@@ -38,30 +146,92 @@ func TestConfig_cellIndex(t *testing.T) {
 }
 
 func TestConfig_chunkIndex(t *testing.T) {
-	type fields struct {
-		ChunkSize          uint64
-		ValidatorChunkSize uint64
-		HistoryLength      uint64
-	}
-	type args struct {
-		epoch uint64
-	}
 	tests := []struct {
 		name   string
-		fields fields
-		args   args
+		fields *Config
+		epoch  uint64
 		want   uint64
 	}{
-		// TODO: Add test cases.
+		{
+			name: "epoch 0",
+			fields: &Config{
+				ChunkSize:     3,
+				HistoryLength: 3,
+			},
+			epoch: 0,
+			want:  0,
+		},
+		{
+			name: "epoch < HistoryLength, epoch < ChunkSize",
+			fields: &Config{
+				ChunkSize:     3,
+				HistoryLength: 3,
+			},
+			epoch: 2,
+			want:  0,
+		},
+		{
+			name: "epoch = HistoryLength, epoch < ChunkSize",
+			fields: &Config{
+				ChunkSize:     4,
+				HistoryLength: 3,
+			},
+			epoch: 3,
+			want:  0,
+		},
+		{
+			name: "epoch > HistoryLength, epoch < ChunkSize",
+			fields: &Config{
+				ChunkSize:     5,
+				HistoryLength: 3,
+			},
+			epoch: 4,
+			want:  0,
+		},
+		{
+			name: "epoch < HistoryLength, epoch < ChunkSize",
+			fields: &Config{
+				ChunkSize:     3,
+				HistoryLength: 3,
+			},
+			epoch: 2,
+			want:  0,
+		},
+		{
+			name: "epoch = HistoryLength, epoch < ChunkSize",
+			fields: &Config{
+				ChunkSize:     4,
+				HistoryLength: 3,
+			},
+			epoch: 3,
+			want:  0,
+		},
+		{
+			name: "epoch < HistoryLength, epoch = ChunkSize",
+			fields: &Config{
+				ChunkSize:     2,
+				HistoryLength: 3,
+			},
+			epoch: 2,
+			want:  1,
+		},
+		{
+			name: "epoch < HistoryLength, epoch > ChunkSize",
+			fields: &Config{
+				ChunkSize:     2,
+				HistoryLength: 4,
+			},
+			epoch: 3,
+			want:  1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &Config{
-				ChunkSize:          tt.fields.ChunkSize,
-				ValidatorChunkSize: tt.fields.ValidatorChunkSize,
-				HistoryLength:      tt.fields.HistoryLength,
+				ChunkSize:     tt.fields.ChunkSize,
+				HistoryLength: tt.fields.HistoryLength,
 			}
-			if got := c.chunkIndex(tt.args.epoch); got != tt.want {
+			if got := c.chunkIndex(tt.epoch); got != tt.want {
 				t.Errorf("chunkIndex() = %v, want %v", got, tt.want)
 			}
 		})
@@ -132,30 +302,43 @@ func TestConfig_diskKey(t *testing.T) {
 }
 
 func TestConfig_validatorChunkIndex(t *testing.T) {
-	type fields struct {
-		ChunkSize          uint64
-		ValidatorChunkSize uint64
-		HistoryLength      uint64
-	}
-	type args struct {
-		validatorIndex uint64
-	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   uint64
+		name           string
+		fields         *Config
+		validatorIndex uint64
+		want           uint64
 	}{
-		// TODO: Add test cases.
+		{
+			name: "validator index < ValidatorChunkSize",
+			fields: &Config{
+				ValidatorChunkSize: 3,
+			},
+			validatorIndex: 2,
+			want:           0,
+		},
+		{
+			name: "validator index = ValidatorChunkSize",
+			fields: &Config{
+				ValidatorChunkSize: 3,
+			},
+			validatorIndex: 3,
+			want:           1,
+		},
+		{
+			name: "validator index > ValidatorChunkSize",
+			fields: &Config{
+				ValidatorChunkSize: 3,
+			},
+			validatorIndex: 99,
+			want:           33,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &Config{
-				ChunkSize:          tt.fields.ChunkSize,
 				ValidatorChunkSize: tt.fields.ValidatorChunkSize,
-				HistoryLength:      tt.fields.HistoryLength,
 			}
-			if got := c.validatorChunkIndex(tt.args.validatorIndex); got != tt.want {
+			if got := c.validatorChunkIndex(tt.validatorIndex); got != tt.want {
 				t.Errorf("validatorChunkIndex() = %v, want %v", got, tt.want)
 			}
 		})
