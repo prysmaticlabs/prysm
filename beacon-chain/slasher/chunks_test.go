125bbc186297d52e518e50c3ae2ba01a6b8339f9
Raul Jordan <raul@prysmaticlabs.com>
2021-01-27 13:52:25 -0600

    more chunker tests

--- beacon-chain/slasher/chunks_test.go
+++ beacon-chain/slasher/chunks_test.go
@@ -1,3 +1,60 @@
 package slasher
 
+import (
+	"math"
+	"testing"
+
+	types "github.com/prysmaticlabs/eth2-types"
+	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
+)
+
 var _ = Chunker(&MinSpanChunk{})
+
+func TestMinSpanChunk_Chunk(t *testing.T) {
+	chunk := EmptyMinSpanChunk(&Parameters{
+		chunkSize:          2,
+		validatorChunkSize: 2,
+	})
+	wanted := []uint16{math.MaxUint16, math.MaxUint16, math.MaxUint16, math.MaxUint16}
+	require.Equal(t, wanted, chunk.Chunk())
+}
+
+func TestMinSpanChunk_NeutralElement(t *testing.T) {
+	chunk := EmptyMinSpanChunk(&Parameters{})
+	require.Equal(t, math.MaxUint16, chunk.NeutralElement())
+}
+
+func Test_chunkDataAtEpoch_SetRetrieve(t *testing.T) {
+	// We initialize a slice for 2 validators and with chunk size 3,
+	// which will look as follows:
+	//
+	//     val0     val1
+	//   {     }  {     }
+	//  [2, 2, 2, 2, 2, 2]
+	//
+	// To give an example, epoch 1 for validator 1 will be at the following position:
+	//
+	//  [2, 2, 2, 2, 2, 2]
+	//               |-> epoch 1, validator 1.
+	params := &Parameters{
+		chunkSize:          3,
+		validatorChunkSize: 2,
+	}
+	chunk := []uint16{2, 2, 2, 2, 2, 2}
+	validatorIdx := types.ValidatorIndex(1)
+	epochInChunk := types.Epoch(1)
+
+	// We expect a chunk with the wrong length to throw an error.
+	_, err := chunkDataAtEpoch(params, []uint16{}, validatorIdx, epochInChunk)
+	require.ErrorContains(t, "chunk has wrong length", err)
+
+	// We update the value for epoch 1 using target epoch 6.
+	targetEpoch := types.Epoch(6)
+	err = setChunkDataAtEpoch(params, chunk, validatorIdx, epochInChunk, targetEpoch)
+	require.NoError(t, err)
+	// We expect the retrieved value at epoch 1 is the target epoch 6.
+	received, err := chunkDataAtEpoch(params, chunk, validatorIdx, epochInChunk)
+	require.NoError(t, err)
+	assert.Equal(t, targetEpoch, received)
+}
