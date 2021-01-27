7e318e8589ae2da6ac7f670d6d7ade53c818d229
Raul Jordan <raul@prysmaticlabs.com>
2021-01-27 15:54:26 -0600

    add more chunk tests

--- beacon-chain/slasher/chunks_test.go
+++ beacon-chain/slasher/chunks_test.go
@@ -1,10 +1,16 @@
 package slasher
 
 import (
+	"context"
+	"fmt"
 	"math"
 	"testing"
 
 	types "github.com/prysmaticlabs/eth2-types"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+
+	dbtest "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
+	slashertypes "github.com/prysmaticlabs/prysm/beacon-chain/slasher/types"
 	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
@@ -23,6 +29,98 @@ func TestMinSpanChunk_Chunk(t *testing.T) {
 func TestMinSpanChunk_NeutralElement(t *testing.T) {
 	chunk := EmptyMinSpanChunk(&Parameters{})
 	require.Equal(t, math.MaxUint16, chunk.NeutralElement())
+}
+
+func TestMinSpanChunk_MinChunkSpanFrom(t *testing.T) {
+	params := &Parameters{
+		chunkSize:          3,
+		validatorChunkSize: 2,
+	}
+	_, err := MinChunkSpanFrom(params, []uint16{})
+	require.ErrorContains(t, "chunk has wrong length", err)
+
+	data := []uint16{2, 2, 2, 2, 2, 2}
+	chunk, err := MinChunkSpanFrom(&Parameters{
+		chunkSize:          3,
+		validatorChunkSize: 2,
+	}, data)
+	require.NoError(t, err)
+	require.DeepEqual(t, data, chunk.Chunk())
+}
+
+func TestMinSpanChunk_CheckSlashable(t *testing.T) {
+	ctx := context.Background()
+	beaconDB := dbtest.SetupDB(t)
+	params := &Parameters{
+		chunkSize:          3,
+		validatorChunkSize: 2,
+		historyLength:      3,
+	}
+	validatorIdx := types.ValidatorIndex(1)
+	source := types.Epoch(1)
+	target := types.Epoch(2)
+	att := createAttestation(source, target)
+
+	// A faulty chunk should lead to error.
+	chunk := &MinSpanChunk{
+		params: params,
+		data:   []uint16{},
+	}
+	_, _, err := chunk.CheckSlashable(ctx, nil, validatorIdx, att)
+	require.ErrorContains(t, "could not get min target for validator", err)
+
+	// We initialize a proper slice with 2 chunks with chunk size 3, 2 validators, and
+	// a history length of 3 representing a perfect attesting history.
+	//
+	//     val0     val1
+	//   {     }  {     }
+	//  [2, 2, 2, 2, 2, 2]
+	data := []uint16{2, 2, 2, 2, 2, 2}
+	chunk, err = MinChunkSpanFrom(params, data)
+	require.NoError(t, err)
+
+	// An attestation with source 1 and target 2 should not be slashable
+	// based on our min chunk for either validator.
+	slashable, kind, err := chunk.CheckSlashable(ctx, beaconDB, validatorIdx, att)
+	require.NoError(t, err)
+	require.Equal(t, false, slashable)
+	require.Equal(t, slashertypes.NotSlashable, kind)
+
+	slashable, kind, err = chunk.CheckSlashable(ctx, beaconDB, validatorIdx.Sub(1), att)
+	require.NoError(t, err)
+	require.Equal(t, false, slashable)
+	require.Equal(t, slashertypes.NotSlashable, kind)
+
+	// Next up we initialize an empty chunk and mark an attestation
+	// with (source 1, target 2) as attested.
+	chunk = EmptyMinSpanChunk(params)
+	source = types.Epoch(1)
+	target = types.Epoch(2)
+	att = createAttestation(source, target)
+	err = setChunkDataAtEpoch(params, chunk.Chunk(), validatorIdx, source, target)
+	require.NoError(t, err)
+
+	// Next up, we create a surrounding vote, but it should NOT be slashable
+	// because we have not stored the old attestation record in our database.
+	source = types.Epoch(0)
+	target = types.Epoch(3)
+	surroundingVote := createAttestation(source, target)
+
+	slashable, kind, err = chunk.CheckSlashable(ctx, beaconDB, validatorIdx, surroundingVote)
+	require.NoError(t, err)
+	require.Equal(t, false, slashable)
+	require.Equal(t, slashertypes.NotSlashable, kind)
+
+	// Next up, we save the old attestation record, then check if the
+	// surrounding vote is indeed slashable.
+	err = beaconDB.SaveAttestationRecordForValidator(ctx, validatorIdx, [32]byte{1}, att)
+	require.NoError(t, err)
+
+	fmt.Println(chunk.Chunk())
+	slashable, kind, err = chunk.CheckSlashable(ctx, beaconDB, validatorIdx, surroundingVote)
+	require.NoError(t, err)
+	require.Equal(t, true, slashable)
+	require.Equal(t, slashertypes.SurroundingVote, kind)
 }
 
 func Test_chunkDataAtEpoch_SetRetrieve(t *testing.T) {
@@ -57,4 +155,17 @@ func Test_chunkDataAtEpoch_SetRetrieve(t *testing.T) {
 	received, err := chunkDataAtEpoch(params, chunk, validatorIdx, epochInChunk)
 	require.NoError(t, err)
 	assert.Equal(t, targetEpoch, received)
+}
+
+func createAttestation(source, target types.Epoch) *ethpb.IndexedAttestation {
+	return &ethpb.IndexedAttestation{
+		Data: &ethpb.AttestationData{
+			Source: &ethpb.Checkpoint{
+				Epoch: uint64(source),
+			},
+			Target: &ethpb.Checkpoint{
+				Epoch: uint64(target),
+			},
+		},
+	}
 }
