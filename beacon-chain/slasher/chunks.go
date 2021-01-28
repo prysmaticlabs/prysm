971a15f9074b9972c28d13b7a17d7addbc689469
Raul Jordan <raul@prysmaticlabs.com>
2021-01-28 14:21:13 -0600

    Defining and Implementing the Chunker Interface for MinSpans (#8350)
    * chunker methods and comments
    
    * more chunker tests
    
    * add more chunk tests
    
    * tests passing, now missing update test
    
    * better comment
    
    * gaz
    
    * better annotations
    
    * test for updating a single chunk
    
    * multiple chunks test
    
    * fix test to use deepequal
    
    * chunks tests now properly passing
    
    * deep source fixes
    
    * better annotation for epoch staggering
    
    * victor comments

---
+++ beacon-chain/slasher/chunks.go
@@ -0,0 +1,309 @@
+package slasher
+
+import (
+	"context"
+	"fmt"
+	"math"
+
+	"github.com/pkg/errors"
+	"github.com/prysmaticlabs/eth2-types"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/beacon-chain/db"
+	slashertypes "github.com/prysmaticlabs/prysm/beacon-chain/slasher/types"
+)
+
+// Chunker defines a struct which represents a slice containing a chunk for K different validator's
+// min spans used for surround vote detection in slasher. The interface defines methods used to check
+// if an attestation is slashable for a validator index based on the contents of
+// the chunk as well as the ability to update the data in the chunk with incoming information.
+// For detailed information on what a chunk is and how it works, refer to our design document:
+// https://hackmd.io/@Yl0VNGYRR6aeDrHHQNhuaA/prysm-slasher.
+type Chunker interface {
+	NeutralElement() uint16
+	Chunk() []uint16
+	CheckSlashable(
+		ctx context.Context,
+		slasherDB db.Database,
+		validatorIdx types.ValidatorIndex,
+		attestation *ethpb.IndexedAttestation,
+	) (bool, slashertypes.SlashingKind, error)
+	Update(
+		chunkIdx uint64,
+		validatorIdx types.ValidatorIndex,
+		startEpoch,
+		currentEpoch,
+		newTargetEpoch types.Epoch,
+	) (bool, error)
+}
+
+// MinSpanChunksSlice represents a slice containing a chunk for K different validator's min spans.
+// MinSpans and chunks are defined in our design document: https://hackmd.io/@Yl0VNGYRR6aeDrHHQNhuaA/prysm-slasher.
+//
+// For a given epoch, e, and attestations a validator index has produced, atts,
+// such that min_spans[e] is defined as min((att.target.epoch - e) for att in attestations)
+// where att.source.epoch > e. That is, it is the minimum distance between the
+// specified epoch and all attestation target epochs a validator has created
+// where att.source.epoch > e.
+//
+// Under ideal network conditions, where every target epoch immediately follows its source,
+// min spans for a validator will look as follows:
+//
+//  min_spans = [2, 2, 2, ..., 2]
+//
+// Next, we can chunk this list of min spans into chunks of length C. For C = 2, for example:
+//
+//                       chunk0  chunk1       chunkN
+//                        {  }   {   }         {  }
+//  chunked_min_spans = [[2, 2], [2, 2], ..., [2, 2]]
+//
+// Finally, we can store each chunk index for K validators into a single flat slice. For K = 3:
+//
+//                                     val0    val1    val2
+//                                     {  }    {  }    {  }
+//   chunk_0_for_validators_0_to_3 = [[2, 2], [2, 2], [2, 2]]
+//
+//                                     val0    val1    val2
+//                                     {  }    {  }    {  }
+//   chunk_1_for_validators_0_to_3 = [[2, 2], [2, 2], [2, 2]]
+//
+//                            ...
+//
+//                                     val0    val1    val2
+//                                     {  }    {  }    {  }
+//   chunk_N_for_validators_0_to_3 = [[2, 2], [2, 2], [2, 2]]
+//
+// MinSpanChunksSlice represents the data structure above for a single chunk index.
+type MinSpanChunksSlice struct {
+	params *Parameters
+	data   []uint16
+}
+
+// EmptyMinSpanChunksSlice initializes a min span chunk of length C*K for
+// C = chunkSize and K = validatorChunkSize filled with neutral elements.
+// For min spans, the neutral element is `undefined`, represented by MaxUint16.
+func EmptyMinSpanChunksSlice(config *Parameters) *MinSpanChunksSlice {
+	m := &MinSpanChunksSlice{
+		params: config,
+	}
+	data := make([]uint16, config.chunkSize*config.validatorChunkSize)
+	for i := 0; i < len(data); i++ {
+		data[i] = m.NeutralElement()
+	}
+	m.data = data
+	return m
+}
+
+// MinChunkSpansSliceFrom initializes a min span chunks slice from a slice of uint16 values.
+// Returns an error if the slice is not of length C*K for C = chunkSize and K = validatorChunkSize.
+func MinChunkSpansSliceFrom(config *Parameters, chunk []uint16) (*MinSpanChunksSlice, error) {
+	requiredLen := config.chunkSize * config.validatorChunkSize
+	if uint64(len(chunk)) != requiredLen {
+		return nil, fmt.Errorf("chunk has wrong length, %d, expected %d", len(chunk), requiredLen)
+	}
+	return &MinSpanChunksSlice{
+		params: config,
+		data:   chunk,
+	}, nil
+}
+
+// NeutralElement for a min span chunks slice is undefined, in this case
+// using MaxUint16 as a sane value given it is impossible we reach it.
+func (m *MinSpanChunksSlice) NeutralElement() uint16 {
+	return math.MaxUint16
+}
+
+// Chunk returns the underlying slice of uint16's for the min chunks slice.
+func (m *MinSpanChunksSlice) Chunk() []uint16 {
+	return m.data
+}
+
+// CheckSlashable takes in a validator index and an incoming attestation
+// and checks if the validator is slashable depending on the data
+// within the min span chunks slice. Recall that for an incoming attestation, B, and an
+// existing attestation, A:
+//
+//  B surrounds A if and only if B.target > min_spans[B.source]
+//
+// That is, this condition is sufficient to check if an incoming attestation
+// is surrounding a previous one. We also check if we indeed have an existing
+// attestation record in the database if the condition holds true in order
+// to be confident of a slashable offense.
+func (m *MinSpanChunksSlice) CheckSlashable(
+	ctx context.Context,
+	slasherDB db.Database,
+	validatorIdx types.ValidatorIndex,
+	attestation *ethpb.IndexedAttestation,
+) (bool, slashertypes.SlashingKind, error) {
+	sourceEpoch := types.Epoch(attestation.Data.Source.Epoch)
+	targetEpoch := types.Epoch(attestation.Data.Target.Epoch)
+	minTarget, err := chunkDataAtEpoch(m.params, m.data, validatorIdx, sourceEpoch)
+	if err != nil {
+		return false, slashertypes.NotSlashable, errors.Wrapf(
+			err, "could not get min target for validator %d at epoch %d", validatorIdx, sourceEpoch,
+		)
+	}
+	if targetEpoch > minTarget {
+		existingAttRecord, err := slasherDB.AttestationRecordForValidator(ctx, validatorIdx, minTarget)
+		if err != nil {
+			return false, slashertypes.NotSlashable, err
+		}
+		if existingAttRecord != nil {
+			if sourceEpoch < types.Epoch(existingAttRecord.Source) {
+				return true, slashertypes.SurroundingVote, nil
+			}
+		}
+	}
+	return false, slashertypes.NotSlashable, nil
+}
+
+// Update a min span chunk for a validator index starting at the current epoch, e_c, then updating
+// down to e_c - H where H is the historyLength we keep for each span. This historyLength
+// corresponds to the weak subjectivity period of eth2, see:
+// https://github.com/ethereum/eth2.0-specs/blob/dev/specs/phase0/weak-subjectivity.md.
+// This means our updates are done in a sliding window manner. For example, if the current epoch
+// is 20 and the historyLength is 12, then we will update every value for the validator's min span
+// from epoch 20 down to epoch 8 (since 20 - 12 = 8).
+//
+// Recall that for an epoch, e, min((att.target - e) for att in attestations where att.source > e)
+// That is, it is the minimum distance between the specified epoch and all attestation
+// target epochs a validator has created where att.source.epoch > e.
+//
+// Recall that a MinSpanChunksSlice struct represents a single slice for a chunk index
+// from the collection below:
+//
+//                                     val0    val1    val2
+//                                     {  }    {  }    {  }
+//   chunk_0_for_validators_0_to_3 = [[2, 2], [2, 2], [2, 2]]
+//
+//                                     val0    val1    val2
+//                                     {  }    {  }    {  }
+//   chunk_1_for_validators_0_to_3 = [[2, 2], [2, 2], [2, 2]]
+//
+//                            ...
+//
+//                                     val0    val1    val2
+//                                     {  }    {  }    {  }
+//   chunk_N_for_validators_0_to_3 = [[2, 2], [2, 2], [2, 2]]
+//
+// Let's take a look at how this update will look for a real set of min span chunk:
+// For the purposes of a simple example, let's set H = 2, meaning a min span
+// will hold 2 epochs worth of attesting history. Then we set C = 2 meaning we will
+// chunk the min span into arrays each of length 2.
+//
+// So assume we get an epoch 4 and validator 0, then, we need to update every epoch in the span from
+// 4 down to 2. First, we find out which chunk epoch 4 falls into, which is calculated as:
+// chunk_idx = (epoch % H) / C = (4 % 2) / 2 = 0
+//
+//
+//                                     val0    val1    val2
+//                                     {  }    {  }    {  }
+//   chunk_0_for_validators_0_to_3 = [[2, 2], [2, 2], [2, 2]]
+//                                     |
+//                                     |-> epoch 4 for validator 0
+//
+// Next up, we proceed with the update process for validator index 0, starting epoch 4
+// al the way down to epoch 2. We will need to go down the array once
+// and then wrap around at the end back down to epoch 6.
+//
+// update every epoch from 4 down to 2 for validator 0
+//
+//             epoch 4
+//               |
+//             <-|
+//          epoch2 epoch3
+//              |   |
+//             <----<
+//             [[2, 2], [2, 2], [2, 2]]
+//
+// Once we finish updating a chunk, we need to move on to the next chunk. This function
+// returns a boolean named keepGoing which allows the caller to determine if we should
+// continue and update another chunk index. We stop whenever we reach the min epoch we need
+// to update, in our example, we stop at 2, which is still part of chunk 0, so no need
+// to jump to another min span chunks slice to perform updates.
+func (m *MinSpanChunksSlice) Update(
+	chunkIdx uint64,
+	validatorIdx types.ValidatorIndex,
+	startEpoch,
+	currentEpoch,
+	newTargetEpoch types.Epoch,
+) (bool, error) {
+	// The lowest epoch we need to update. This is a sliding window from (current epoch - H) where
+	// H is the history length a min span for a validator stores.
+	var minEpoch types.Epoch
+	if uint64(currentEpoch) > (m.params.historyLength - 1) {
+		minEpoch = currentEpoch.Sub(m.params.historyLength - 1)
+	}
+	epochInChunk := startEpoch
+	// We go down the chunk for the validator, updating every value starting at start_epoch down to min_epoch.
+	// As long as the epoch, e, in the same chunk index and e >= min_epoch, we proceed with
+	// a for loop.
+	for m.params.chunkIndex(epochInChunk) == chunkIdx && epochInChunk >= minEpoch {
+		chunkTarget, err := chunkDataAtEpoch(m.params, m.data, validatorIdx, epochInChunk)
+		if err != nil {
+			return false, err
+		}
+		// If the newly incoming value is < the existing value, we update
+		// the data in the min span to meet with its definition.
+		if newTargetEpoch < chunkTarget {
+			if err = setChunkDataAtEpoch(m.params, m.data, validatorIdx, epochInChunk, newTargetEpoch); err != nil {
+				return false, err
+			}
+		} else {
+			// We can stop because spans are guaranteed to be minimums and
+			// if we did not meet the minimum condition, there is nothing to update.
+			return false, nil
+		}
+		// We decrease our epoch index variable as long as it is > 0.
+		if epochInChunk > 0 {
+			epochInChunk -= 1
+		}
+	}
+	// We should keep going and update the previous chunk if we are yet to reach
+	// the minimum epoch required for the update procedure.
+	keepGoing := epochInChunk >= minEpoch
+	return keepGoing, nil
+}
+
+// Given a validator index and epoch, retrieves the target epoch at its specific
+// index for the validator index + epoch pair in a min/max span chunk.
+func chunkDataAtEpoch(
+	params *Parameters, chunk []uint16, validatorIdx types.ValidatorIndex, epoch types.Epoch,
+) (types.Epoch, error) {
+	requiredLen := params.chunkSize * params.validatorChunkSize
+	if uint64(len(chunk)) != requiredLen {
+		return 0, fmt.Errorf("chunk has wrong length, %d, expected %d", len(chunk), requiredLen)
+	}
+	cellIdx := params.cellIndex(validatorIdx, epoch)
+	if cellIdx >= uint64(len(chunk)) {
+		return 0, fmt.Errorf("cell index %d out of bounds (len(chunk) = %d)", cellIdx, len(chunk))
+	}
+	distance := chunk[cellIdx]
+	return epoch.Add(uint64(distance)), nil
+}
+
+// Updates the value at a specific index in a chunk for a validator index + epoch
+// pair to a specified distance. Recall that for min spans, each element in a chunk
+// is the minimum distance between the a given epoch, e, and all attestation target epochs
+// a validator has created where att.source.epoch > e.
+func setChunkDataAtEpoch(
+	config *Parameters,
+	chunk []uint16,
+	validatorIdx types.ValidatorIndex,
+	epochInChunk,
+	targetEpoch types.Epoch,
+) error {
+	distance := epochDistance(targetEpoch, epochInChunk)
+	cellIdx := config.cellIndex(validatorIdx, epochInChunk)
+	if cellIdx >= uint64(len(chunk)) {
+		return fmt.Errorf("cell index %d out of bounds (len(chunk) = %d)", cellIdx, len(chunk))
+	}
+	chunk[cellIdx] = distance
+	return nil
+}
+
+// Computes a distance between two epochs. Given the result stored in
+// min/max spans is maximum WEAK_SUBJECTIVITY_PERIOD, we are guaranteed the
+// distance can be represented as a uint16 safely.
+func epochDistance(epoch, baseEpoch types.Epoch) uint16 {
+	return uint16(epoch.Sub(uint64(baseEpoch)))
+}
---
+++ beacon-chain/slasher/chunks_test.go
@@ -0,0 +1,282 @@
+package slasher
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/prysmaticlabs/eth2-types"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	dbtest "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
+	slashertypes "github.com/prysmaticlabs/prysm/beacon-chain/slasher/types"
+	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
+)
+
+var _ = Chunker(&MinSpanChunksSlice{})
+
+func TestMinSpanChunksSlice_Chunk(t *testing.T) {
+	chunk := EmptyMinSpanChunksSlice(&Parameters{
+		chunkSize:          2,
+		validatorChunkSize: 2,
+	})
+	wanted := []uint16{math.MaxUint16, math.MaxUint16, math.MaxUint16, math.MaxUint16}
+	require.DeepEqual(t, wanted, chunk.Chunk())
+}
+
+func TestMinSpanChunksSlice_NeutralElement(t *testing.T) {
+	chunk := EmptyMinSpanChunksSlice(&Parameters{})
+	require.Equal(t, uint16(math.MaxUint16), chunk.NeutralElement())
+}
+
+func TestMinSpanChunksSlice_MinChunkSpanFrom(t *testing.T) {
+	params := &Parameters{
+		chunkSize:          3,
+		validatorChunkSize: 2,
+	}
+	_, err := MinChunkSpansSliceFrom(params, []uint16{})
+	require.ErrorContains(t, "chunk has wrong length", err)
+
+	data := []uint16{2, 2, 2, 2, 2, 2}
+	chunk, err := MinChunkSpansSliceFrom(&Parameters{
+		chunkSize:          3,
+		validatorChunkSize: 2,
+	}, data)
+	require.NoError(t, err)
+	require.DeepEqual(t, data, chunk.Chunk())
+}
+
+func TestMinSpanChunksSlice_CheckSlashable(t *testing.T) {
+	ctx := context.Background()
+	beaconDB := dbtest.SetupDB(t)
+	params := &Parameters{
+		chunkSize:          3,
+		validatorChunkSize: 2,
+		historyLength:      3,
+	}
+	validatorIdx := types.ValidatorIndex(1)
+	source := types.Epoch(1)
+	target := types.Epoch(2)
+	att := createAttestation(source, target)
+
+	// A faulty chunk should lead to error.
+	chunk := &MinSpanChunksSlice{
+		params: params,
+		data:   []uint16{},
+	}
+	_, _, err := chunk.CheckSlashable(ctx, nil, validatorIdx, att)
+	require.ErrorContains(t, "could not get min target for validator", err)
+
+	// We initialize a proper slice with 2 chunks with chunk size 3, 2 validators, and
+	// a history length of 3 representing a perfect attesting history.
+	//
+	//     val0     val1
+	//   {     }  {     }
+	//  [2, 2, 2, 2, 2, 2]
+	data := []uint16{2, 2, 2, 2, 2, 2}
+	chunk, err = MinChunkSpansSliceFrom(params, data)
+	require.NoError(t, err)
+
+	// An attestation with source 1 and target 2 should not be slashable
+	// based on our min chunk for either validator.
+	slashable, kind, err := chunk.CheckSlashable(ctx, beaconDB, validatorIdx, att)
+	require.NoError(t, err)
+	require.Equal(t, false, slashable)
+	require.Equal(t, slashertypes.NotSlashable, kind)
+
+	slashable, kind, err = chunk.CheckSlashable(ctx, beaconDB, validatorIdx.Sub(1), att)
+	require.NoError(t, err)
+	require.Equal(t, false, slashable)
+	require.Equal(t, slashertypes.NotSlashable, kind)
+
+	// Next up we initialize an empty chunks slice and mark an attestation
+	// with (source 1, target 2) as attested.
+	chunk = EmptyMinSpanChunksSlice(params)
+	source = types.Epoch(1)
+	target = types.Epoch(2)
+	att = createAttestation(source, target)
+	chunkIdx := uint64(0)
+	startEpoch := target
+	currentEpoch := target
+	_, err = chunk.Update(chunkIdx, validatorIdx, startEpoch, currentEpoch, target)
+	require.NoError(t, err)
+	fmt.Println(chunk.Chunk())
+
+	// Next up, we create a surrounding vote, but it should NOT be slashable
+	// because we have an existing attestation record in our database at the min target epoch.
+	source = types.Epoch(0)
+	target = types.Epoch(3)
+	surroundingVote := createAttestation(source, target)
+
+	slashable, kind, err = chunk.CheckSlashable(ctx, beaconDB, validatorIdx, surroundingVote)
+	require.NoError(t, err)
+	require.Equal(t, false, slashable)
+	require.Equal(t, slashertypes.NotSlashable, kind)
+
+	// Next up, we save the old attestation record, then check if the
+	// surrounding vote is indeed slashable.
+	err = beaconDB.SaveAttestationRecordForValidator(ctx, validatorIdx, [32]byte{1}, att)
+	require.NoError(t, err)
+
+	slashable, kind, err = chunk.CheckSlashable(ctx, beaconDB, validatorIdx, surroundingVote)
+	require.NoError(t, err)
+	require.Equal(t, true, slashable)
+	require.Equal(t, slashertypes.SurroundingVote, kind)
+}
+
+func TestMinSpanChunksSlice_Update_MultipleChunks(t *testing.T) {
+	// Let's set H = historyLength = 2, meaning a min span
+	// will hold 2 epochs worth of attesting history. Then we set C = 2 meaning we will
+	// chunk the min span into arrays each of length 2 and K = 3 meaning we store each chunk index
+	// for 3 validators at a time.
+	//
+	// So assume we get a target 3 for source 0 and validator 0, then, we need to update every epoch in the span from
+	// 3 to 0 inclusive. First, we find out which chunk epoch 3 falls into, which is calculated as:
+	// chunk_idx = (epoch % H) / C = (3 % 4) / 2 = 1
+	//
+	//                                       val0        val1        val2
+	//                                     {     }     {      }    {      }
+	//   chunk_1_for_validators_0_to_3 = [[nil, nil], [nil, nil], [nil, nil]]
+	//                                      |    |
+	//                                      |    |-> epoch 3 for validator 0
+	//                                      |
+	//                                      |-> epoch 2 for validator 0
+	//
+	//                                       val0        val1        val2
+	//                                     {     }     {      }    {      }
+	//   chunk_0_for_validators_0_to_3 = [[nil, nil], [nil, nil], [nil, nil]]
+	//                                      |    |
+	//                                      |    |-> epoch 1 for validator 0
+	//                                      |
+	//                                      |-> epoch 0 for validator 0
+	//
+	// Next up, we proceed with the update process for validator index 0, starting epoch 3
+	// updating every value along the way according to the update rules for min spans.
+	//
+	// Once we finish updating a chunk, we need to move on to the next chunk. This function
+	// returns a boolean named keepGoing which allows the caller to determine if we should
+	// continue and update another chunk index. We stop whenever we reach the min epoch we need
+	// to update, in our example, we stop at 0, which is a part chunk 0, so we need to perform updates
+	// across 2 different min span chunk slices as shown above.
+	params := &Parameters{
+		chunkSize:          2,
+		validatorChunkSize: 3,
+		historyLength:      4,
+	}
+	chunk := EmptyMinSpanChunksSlice(params)
+	target := types.Epoch(3)
+	chunkIdx := uint64(1)
+	validatorIdx := types.ValidatorIndex(0)
+	startEpoch := target
+	currentEpoch := target
+	keepGoing, err := chunk.Update(chunkIdx, validatorIdx, startEpoch, currentEpoch, target)
+	require.NoError(t, err)
+
+	// We should keep going! We still have to update the data for chunk index 0.
+	require.Equal(t, true, keepGoing)
+	want := []uint16{1, 0, math.MaxUint16, math.MaxUint16, math.MaxUint16, math.MaxUint16}
+	require.DeepEqual(t, want, chunk.Chunk())
+
+	// Now we update for chunk index 0.
+	chunk = EmptyMinSpanChunksSlice(params)
+	chunkIdx = uint64(0)
+	validatorIdx = types.ValidatorIndex(0)
+	startEpoch = types.Epoch(1)
+	currentEpoch = target
+	keepGoing, err = chunk.Update(chunkIdx, validatorIdx, startEpoch, currentEpoch, target)
+	require.NoError(t, err)
+	require.Equal(t, false, keepGoing)
+	want = []uint16{3, 2, math.MaxUint16, math.MaxUint16, math.MaxUint16, math.MaxUint16}
+	require.DeepEqual(t, want, chunk.Chunk())
+}
+
+func TestMinSpanChunksSlice_Update_SingleChunk(t *testing.T) {
+	// Let's set H = historyLength = 2, meaning a min span
+	// will hold 2 epochs worth of attesting history. Then we set C = 2 meaning we will
+	// chunk the min span into arrays each of length 2 and K = 3 meaning we store each chunk index
+	// for 3 validators at a time.
+	//
+	// So assume we get a target 1 for source 0 and validator 0, then, we need to update every epoch in the span from
+	// 1 to 0 inclusive. First, we find out which chunk epoch 4 falls into, which is calculated as:
+	// chunk_idx = (epoch % H) / C = (1 % 2) / 2 = 0
+	//
+	//                                       val0        val1        val2
+	//                                     {     }     {      }    {      }
+	//   chunk_0_for_validators_0_to_3 = [[nil, nil], [nil, nil], [nil, nil]]
+	//                                           |
+	//                                           |-> epoch 1 for validator 0
+	//
+	// Next up, we proceed with the update process for validator index 0, starting epoch 1
+	// updating every value along the way according to the update rules for min spans.
+	//
+	// Once we finish updating a chunk, we need to move on to the next chunk. This function
+	// returns a boolean named keepGoing which allows the caller to determine if we should
+	// continue and update another chunk index. We stop whenever we reach the min epoch we need
+	// to update, in our example, we stop at 0, which is still part of chunk 0, so there is no
+	// need to keep going.
+	params := &Parameters{
+		chunkSize:          2,
+		validatorChunkSize: 3,
+		historyLength:      2,
+	}
+	chunk := EmptyMinSpanChunksSlice(params)
+	target := types.Epoch(1)
+	chunkIdx := uint64(0)
+	validatorIdx := types.ValidatorIndex(0)
+	startEpoch := target
+	currentEpoch := target
+	keepGoing, err := chunk.Update(chunkIdx, validatorIdx, startEpoch, currentEpoch, target)
+	require.NoError(t, err)
+	require.Equal(t, false, keepGoing)
+	want := []uint16{1, 0, math.MaxUint16, math.MaxUint16, math.MaxUint16, math.MaxUint16}
+	require.DeepEqual(t, want, chunk.Chunk())
+}
+
+func Test_chunkDataAtEpoch_SetRetrieve(t *testing.T) {
+	// We initialize a chunks slice for 2 validators and with chunk size 3,
+	// which will look as follows:
+	//
+	//     val0     val1
+	//   {     }  {     }
+	//  [2, 2, 2, 2, 2, 2]
+	//
+	// To give an example, epoch 1 for validator 1 will be at the following position:
+	//
+	//  [2, 2, 2, 2, 2, 2]
+	//               |-> epoch 1, validator 1.
+	params := &Parameters{
+		chunkSize:          3,
+		validatorChunkSize: 2,
+	}
+	chunk := []uint16{2, 2, 2, 2, 2, 2}
+	validatorIdx := types.ValidatorIndex(1)
+	epochInChunk := types.Epoch(1)
+
+	// We expect a chunk with the wrong length to throw an error.
+	_, err := chunkDataAtEpoch(params, []uint16{}, validatorIdx, epochInChunk)
+	require.ErrorContains(t, "chunk has wrong length", err)
+
+	// We update the value for epoch 1 using target epoch 6.
+	targetEpoch := types.Epoch(6)
+	err = setChunkDataAtEpoch(params, chunk, validatorIdx, epochInChunk, targetEpoch)
+	require.NoError(t, err)
+
+	// We expect the retrieved value at epoch 1 is the target epoch 6.
+	received, err := chunkDataAtEpoch(params, chunk, validatorIdx, epochInChunk)
+	require.NoError(t, err)
+	assert.Equal(t, targetEpoch, received)
+}
+
+func createAttestation(source, target types.Epoch) *ethpb.IndexedAttestation {
+	return &ethpb.IndexedAttestation{
+		Data: &ethpb.AttestationData{
+			Source: &ethpb.Checkpoint{
+				Epoch: uint64(source),
+			},
+			Target: &ethpb.Checkpoint{
+				Epoch: uint64(target),
+			},
+		},
+	}
+}
