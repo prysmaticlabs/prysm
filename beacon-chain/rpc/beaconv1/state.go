29a45ac5d96233a4d388f0b4746184f583a06c85
rkapka <rkapka@wp.pl>
2021-01-29 16:02:58 +0100

    initial implementation

--- beacon-chain/rpc/beaconv1/state.go
+++ beacon-chain/rpc/beaconv1/state.go
@@ -1,12 +1,16 @@
 package beaconv1
 
 import (
+	"bytes"
 	"context"
 	"errors"
 
 	ptypes "github.com/gogo/protobuf/types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"go.opencensus.io/trace"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // GetGenesis retrieves details of the chain's genesis which can be used to identify chain.
@@ -14,7 +18,24 @@ func (bs *Server) GetGenesis(ctx context.Context, _ *ptypes.Empty) (*ethpb.Genes
 	ctx, span := trace.StartSpan(ctx, "beaconv1.GetGenesis")
 	defer span.End()
 
-	return nil, errors.New("unimplemented")
+	genesisTime := bs.GenesisTimeFetcher.GenesisTime()
+	if genesisTime.IsZero() {
+		return nil, status.Errorf(codes.NotFound, "Chain genesis info is not yet known")
+	}
+	validatorRoot := bs.ChainInfoFetcher.GenesisValidatorRoot()
+	if bytes.Equal(validatorRoot[:], params.BeaconConfig().ZeroHash[:]) {
+		return nil, status.Errorf(codes.NotFound, "Chain genesis info is not yet known")
+	}
+	forkVersion := params.BeaconConfig().GenesisForkVersion
+
+	return &ethpb.GenesisResponse{
+		GenesisTime: &ptypes.Timestamp{
+			Seconds: genesisTime.Unix(),
+			Nanos:   int32(genesisTime.UnixNano()),
+		},
+		GenesisValidatorsRoot: validatorRoot[:],
+		GenesisForkVersion:    forkVersion,
+	}, nil
 }
 
 // GetStateRoot calculates HashTreeRoot for state with given 'stateId'. If stateId is root, same value will be returned.
