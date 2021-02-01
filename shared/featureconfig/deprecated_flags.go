a247031bf0d6bef9f31af0e63c63b0d8ba3fc309
Victor Farazdagi <simple.square@gmail.com>
2021-02-01 12:54:31 +0300

    Deprecate enableSyncBacktracking flag

--- shared/featureconfig/deprecated_flags.go
+++ shared/featureconfig/deprecated_flags.go
@@ -12,14 +12,8 @@ var (
 		Usage:  deprecatedUsage,
 		Hidden: true,
 	}
-	deprecatedDisableSyncBacktracking = &cli.StringFlag{
-		Name:   "enable-sync-backtracking",
-		Usage:  deprecatedUsage,
-		Hidden: true,
-	}
 )
 
 var deprecatedFlags = []cli.Flag{
 	exampleDeprecatedFeatureFlag,
-	deprecatedDisableSyncBacktracking,
 }
