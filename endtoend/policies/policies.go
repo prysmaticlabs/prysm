7dc23ab90ade386fab8cfcec382e525eada38b48
Victor Farazdagi <simple.square@gmail.com>
2021-02-05 03:39:42 +0300

    update e2e tests

--- endtoend/policies/policies.go
+++ endtoend/policies/policies.go
@@ -1,27 +1,29 @@
 package policies
 
+import "github.com/prysmaticlabs/eth2-types"
+
 // AfterNthEpoch runs for every epoch after the provided epoch.
-func AfterNthEpoch(afterEpoch uint64) func(uint64) bool {
-	return func(currentEpoch uint64) bool {
+func AfterNthEpoch(afterEpoch types.Epoch) func(epoch types.Epoch) bool {
+	return func(currentEpoch types.Epoch) bool {
 		return currentEpoch > afterEpoch
 	}
 }
 
 // AllEpochs runs for all epochs.
-func AllEpochs(_ uint64) bool {
+func AllEpochs(_ types.Epoch) bool {
 	return true
 }
 
 // OnEpoch runs only for the provided epoch.
-func OnEpoch(epoch uint64) func(uint64) bool {
-	return func(currentEpoch uint64) bool {
+func OnEpoch(epoch types.Epoch) func(types.Epoch) bool {
+	return func(currentEpoch types.Epoch) bool {
 		return currentEpoch == epoch
 	}
 }
 
 // BetweenEpochs runs for every epoch that is between the provided epochs.
-func BetweenEpochs(fromEpoch, toEpoch uint64) func(uint64) bool {
-	return func(currentEpoch uint64) bool {
+func BetweenEpochs(fromEpoch, toEpoch types.Epoch) func(types.Epoch) bool {
+	return func(currentEpoch types.Epoch) bool {
 		return fromEpoch < currentEpoch && currentEpoch < toEpoch
 	}
 }
