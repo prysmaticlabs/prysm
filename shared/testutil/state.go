8d915f85902dfa15d841bd284b9738a1169bef23
rkapka <rkapka@wp.pl>
2021-02-05 09:21:18 +0100

    populate roots in helper state

--- shared/testutil/state.go
+++ shared/testutil/state.go
@@ -1,16 +1,20 @@
 package testutil
 
 import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-bitfield"
 	stateTrie "github.com/prysmaticlabs/prysm/beacon-chain/state"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/params"
+	log "github.com/sirupsen/logrus"
 )
 
 var st, _ = stateTrie.InitializeFromProtoUnsafe(&pb.BeaconState{
-	BlockRoots:                 filledByteSlice2D(params.BeaconConfig().SlotsPerHistoricalRoot, 32),
-	StateRoots:                 filledByteSlice2D(params.BeaconConfig().SlotsPerHistoricalRoot, 32),
+	BlockRoots:                 filledRootSlice2D(params.BeaconConfig().SlotsPerHistoricalRoot),
+	StateRoots:                 filledRootSlice2D(params.BeaconConfig().SlotsPerHistoricalRoot),
 	Slashings:                  make([]uint64, params.BeaconConfig().EpochsPerSlashingsVector),
 	RandaoMixes:                filledByteSlice2D(params.BeaconConfig().EpochsPerHistoricalVector, 32),
 	Validators:                 make([]*ethpb.Validator, 0),
@@ -36,6 +40,19 @@ var st, _ = stateTrie.InitializeFromProtoUnsafe(&pb.BeaconState{
 // NewBeaconState creates a beacon state with minimum marshalable fields.
 func NewBeaconState() *stateTrie.BeaconState {
 	return st.Copy()
+}
+
+func filledRootSlice2D(length uint64) [][]byte {
+	slice2D := filledByteSlice2D(length, 32)
+	for i := uint64(0); i < uint64(len(slice2D)); i++ {
+		s := fmt.Sprintf("%064s", hexutil.EncodeUint64(i)[2:])
+		b, err := hexutil.Decode("0x" + s)
+		if err != nil {
+			log.Debug("Failed to decode root " + s)
+		}
+		slice2D[i] = b
+	}
+	return slice2D
 }
 
 // SSZ will fill 2D byte slices with their respective values, so we must fill these in too for round
