f6cb89b140790900773259d62534f83ea1ff36ee
Victor Farazdagi <simple.square@gmail.com>
2021-02-05 23:53:19 +0300

    more unit test fixes

--- shared/sszutil/deep_equal.go
+++ shared/sszutil/deep_equal.go
@@ -120,6 +120,8 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 		switch v1.Type().Name() {
 		case "Epoch":
 			return v1.Interface().(types.Epoch) == v2.Interface().(types.Epoch)
+		case "Slot":
+			return v1.Interface().(types.Slot) == v2.Interface().(types.Slot)
 		}
 		return v1.Interface().(uint64) == v2.Interface().(uint64)
 	case reflect.Uint32:
