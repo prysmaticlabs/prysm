41ba605f9225a194e05858d6be0a0af0e3545ea3
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-02-09 01:41:45 -0500

    Fix deep equal

--- shared/sszutil/deep_equal.go
+++ shared/sszutil/deep_equal.go
@@ -3,6 +3,8 @@ package sszutil
 import (
 	"reflect"
 	"unsafe"
+
+	"google.golang.org/protobuf/proto"
 )
 
 // During deepValueEqual, must keep track of checks that are
@@ -26,14 +28,13 @@ type visit struct {
 // Tests for deep equality using reflected types. The map argument tracks
 // comparisons that have already been seen, which allows short circuiting on
 // recursive types.
-func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) bool {
+func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int, unexportedOnly bool) bool {
 	if !v1.IsValid() || !v2.IsValid() {
 		return v1.IsValid() == v2.IsValid()
 	}
 	if v1.Type() != v2.Type() {
 		return false
 	}
-
 	// We want to avoid putting more in the visited map than we need to.
 	// For any possible reference cycle that might be encountered,
 	// hard(t) needs to return true for at least one of the types in the cycle.
@@ -70,7 +71,7 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 		return v1.String() == v2.String()
 	case reflect.Array:
 		for i := 0; i < v1.Len(); i++ {
-			if !deepValueEqual(v1.Index(i), v2.Index(i), visited, depth+1) {
+			if !deepValueEqual(v1.Index(i), v2.Index(i), visited, depth+1, unexportedOnly) {
 				return false
 			}
 		}
@@ -92,7 +93,7 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 			return true
 		}
 		for i := 0; i < v1.Len(); i++ {
-			if !deepValueEqual(v1.Index(i), v2.Index(i), visited, depth+1) {
+			if !deepValueEqual(v1.Index(i), v2.Index(i), visited, depth+1, unexportedOnly) {
 				return false
 			}
 		}
@@ -101,15 +102,21 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 		if v1.IsNil() || v2.IsNil() {
 			return v1.IsNil() == v2.IsNil()
 		}
-		return deepValueEqual(v1.Elem(), v2.Elem(), visited, depth+1)
+		return deepValueEqual(v1.Elem(), v2.Elem(), visited, depth+1, unexportedOnly)
 	case reflect.Ptr:
 		if v1.Pointer() == v2.Pointer() {
 			return true
 		}
-		return deepValueEqual(v1.Elem(), v2.Elem(), visited, depth+1)
+		return deepValueEqual(v1.Elem(), v2.Elem(), visited, depth+1, unexportedOnly)
 	case reflect.Struct:
 		for i, n := 0, v1.NumField(); i < n; i++ {
-			if !deepValueEqual(v1.Field(i), v2.Field(i), visited, depth+1) {
+			v1Field := v1.Field(i)
+			v2Field := v2.Field(i)
+			if unexportedOnly && (!v1Field.CanInterface() || !v2Field.CanInterface()) {
+				// Continue for unexported fields, since they cannot be read anyways.
+				continue
+			}
+			if !deepValueEqual(v1.Field(i), v2.Field(i), visited, depth+1, unexportedOnly) {
 				return false
 			}
 		}
@@ -187,5 +194,6 @@ func DeepEqual(x, y interface{}) bool {
 	if v1.Type() != v2.Type() {
 		return false
 	}
-	return deepValueEqual(v1, v2, make(map[visit]bool), 0)
+	_, isProto := x.(proto.Message)
+	return deepValueEqual(v1, v2, make(map[visit]bool), 0, isProto)
 }
