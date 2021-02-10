0fcfb71717baa7a41bcb414ace914ff49e90c7fb
Victor Farazdagi <simple.square@gmail.com>
2021-02-10 18:42:38 +0300

    fix testutil/state

--- shared/testutil/state.go
+++ shared/testutil/state.go
@@ -13,7 +13,7 @@ func NewBeaconState(options ...func(state *pb.BeaconState)) (*stateTrie.BeaconSt
 	seed := &pb.BeaconState{
 		BlockRoots:                 filledByteSlice2D(uint64(params.MainnetConfig().SlotsPerHistoricalRoot), 32),
 		StateRoots:                 filledByteSlice2D(uint64(params.MainnetConfig().SlotsPerHistoricalRoot), 32),
-		Slashings:                  make([]uint64, uint64(params.MainnetConfig().EpochsPerSlashingsVector)),
+		Slashings:                  make([]uint64, params.MainnetConfig().EpochsPerSlashingsVector),
 		RandaoMixes:                filledByteSlice2D(uint64(params.MainnetConfig().EpochsPerHistoricalVector), 32),
 		Validators:                 make([]*ethpb.Validator, 0),
 		CurrentJustifiedCheckpoint: &ethpb.Checkpoint{Root: make([]byte, 32)},
