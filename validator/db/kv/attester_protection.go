9c4b419dfb519569beda1482f18d6b50bddb8a97
Preston Van Loon <preston@prysmaticlabs.com>
2021-02-11 12:31:31 -0600

    merge from dev

--- validator/db/kv/attester_protection.go
+++ validator/db/kv/attester_protection.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	"github.com/prysmaticlabs/eth2-types"
+	types "github.com/prysmaticlabs/eth2-types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/slashutil"
@@ -263,7 +263,7 @@ func (s *Store) flushAttestationRecords(ctx context.Context) {
 	err := s.saveAttestationRecords(ctx, s.batchedAttestations)
 	// If there was no error, we reset the batched attestations slice.
 	if err == nil {
-		log.WithField("duration", time.Now().Sub(start)).Debug("Successfully flushed batched attestations to DB")
+		log.WithField("duration", time.Since(start)).Debug("Successfully flushed batched attestations to DB")
 		s.batchedAttestations = make([]*AttestationRecord, 0, attestationBatchCapacity)
 	}
 	// Forward the error, if any, to all subscribers via an event feed.
