47fdb3b99a677b76bac26b7a18f1e547b3717c7d
Radosław Kapka <rkapka@wp.pl>
2021-02-12 18:45:22 +0100

    Revert "Rename `NewService` to `New` (#8337)" (#8440)
    * Revert "Rename `NewService` to `New` (#8337)"
    
    This reverts commit d121b19145d506df85fdb05f6b9b42e35f35b22c.
    
    # Conflicts:
    #	beacon-chain/sync/initial-sync/round_robin_test.go
    
    * fix name in test
    
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/sync/initial-sync/service_test.go
+++ beacon-chain/sync/initial-sync/service_test.go
@@ -155,7 +155,7 @@ func TestService_InitStartStop(t *testing.T) {
 			}
 			// Initialize feed
 			notifier := &mock.MockStateNotifier{}
-			s := New(ctx, &Config{
+			s := NewService(ctx, &Config{
 				P2P:           p,
 				Chain:         mc,
 				StateNotifier: notifier,
@@ -322,7 +322,7 @@ func TestService_markSynced(t *testing.T) {
 	mc := &mock.ChainService{}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	s := New(ctx, &Config{
+	s := NewService(ctx, &Config{
 		Chain:         mc,
 		StateNotifier: mc.StateNotifier(),
 	})
@@ -420,7 +420,7 @@ func TestService_Resync(t *testing.T) {
 			if tt.chainService != nil {
 				mc = tt.chainService()
 			}
-			s := New(ctx, &Config{
+			s := NewService(ctx, &Config{
 				DB:            beaconDB,
 				P2P:           p,
 				Chain:         mc,
@@ -442,7 +442,7 @@ func TestService_Resync(t *testing.T) {
 }
 
 func TestService_Initialized(t *testing.T) {
-	s := New(context.Background(), &Config{})
+	s := NewService(context.Background(), &Config{})
 	s.chainStarted.Set()
 	assert.Equal(t, true, s.Initialized())
 	s.chainStarted.UnSet()
