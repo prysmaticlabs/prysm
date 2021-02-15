28631e77917b53786237af49056a6ffcc9ab21dd
terence tsao <terence@prysmaticlabs.com>
2021-02-15 07:11:25 -0800

    Verify nil block helper (#8447)

--- beacon-chain/core/blocks/attester_slashing.go
+++ beacon-chain/core/blocks/attester_slashing.go
@@ -38,8 +38,8 @@ func ProcessAttesterSlashings(
 	beaconState *stateTrie.BeaconState,
 	b *ethpb.SignedBeaconBlock,
 ) (*stateTrie.BeaconState, error) {
-	if b.Block == nil || b.Block.Body == nil {
-		return nil, errors.New("block and block body can't be nil")
+	if err := helpers.VerifyNilBeaconBlock(b); err != nil {
+		return nil, err
 	}
 
 	body := b.Block.Body
