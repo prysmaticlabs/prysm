313ee07c82c5d33e65a7e595a6e9888c83e6aafb
Raul Jordan <raul@prysmaticlabs.com>
2021-02-26 10:28:28 -0600

    revert config

--- shared/params/mainnet_config.go
+++ shared/params/mainnet_config.go
@@ -86,8 +86,8 @@ var mainnetBeaconConfig = &BeaconChainConfig{
 
 	// Time parameter constants.
 	MinAttestationInclusionDelay:     1,
-	SecondsPerSlot:                   6,
-	SlotsPerEpoch:                    6,
+	SecondsPerSlot:                   12,
+	SlotsPerEpoch:                    32,
 	MinSeedLookahead:                 1,
 	MaxSeedLookahead:                 4,
 	EpochsPerEth1VotingPeriod:        64,
