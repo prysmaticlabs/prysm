3e3b2beb4bfbc649f40004732391d552a07123b9
Benoit Perroud <benoit@sqooba.io>
2021-02-27 21:32:27 +0100

    Found a better place where to update the counters

--- validator/client/validator.go
+++ validator/client/validator.go
@@ -630,14 +630,13 @@ func (v *validator) logDuties(slot types.Slot, duties []*ethpb.DutiesResponse_Du
 	for i := range attesterKeys {
 		attesterKeys[i] = make([]string, 0)
 	}
-	validatorKeysLookup := make(map[string]string)
 	proposerKeys := make([]string, params.BeaconConfig().SlotsPerEpoch)
 	slotOffset := slot - (slot % params.BeaconConfig().SlotsPerEpoch)
 	var totalAttestingKeys uint64
 	for _, duty := range duties {
+		validatorNotTruncatedKey := fmt.Sprintf("%#x", duty.PublicKey)
 		if v.emitAccountMetrics {
-			fmtKey := fmt.Sprintf("%#x", duty.PublicKey)
-			ValidatorStatusesGaugeVec.WithLabelValues(fmtKey).Set(float64(duty.Status))
+			ValidatorStatusesGaugeVec.WithLabelValues(validatorNotTruncatedKey).Set(float64(duty.Status))
 		}
 
 		// Only interested in validators who are attesting/proposing.
@@ -652,8 +651,8 @@ func (v *validator) logDuties(slot types.Slot, duties []*ethpb.DutiesResponse_Du
 			log.WithField("duty", duty).Warn("Invalid attester slot")
 		} else {
 			attesterKeys[duty.AttesterSlot-slotOffset] = append(attesterKeys[duty.AttesterSlot-slotOffset], validatorKey)
-			validatorKeysLookup[validatorKey] = fmt.Sprintf("%#x", duty.PublicKey)
 			totalAttestingKeys++
+			ValidatorNextAttestationSlotGaugeVec.WithLabelValues(validatorNotTruncatedKey).Set(float64(duty.AttesterSlot))
 		}
 
 		for _, proposerSlot := range duty.ProposerSlots {
@@ -663,15 +662,11 @@ func (v *validator) logDuties(slot types.Slot, duties []*ethpb.DutiesResponse_Du
 			} else {
 				proposerKeys[proposerIndex] = validatorKey
 			}
+			ValidatorNextProposalSlotGaugeVec.WithLabelValues(validatorNotTruncatedKey).Set(float64(proposerSlot))
 		}
 	}
 	for i := types.Slot(0); i < params.BeaconConfig().SlotsPerEpoch; i++ {
 		if len(attesterKeys[i]) > 0 {
-			for _, truncKey := range attesterKeys[i] {
-				if key, ok := validatorKeysLookup[truncKey]; ok {
-					ValidatorNextAttestationSlotGaugeVec.WithLabelValues(key).Set(float64(slotOffset + i))
-				}
-			}
 			log.WithFields(logrus.Fields{
 				"slot":                  slotOffset + i,
 				"slotInEpoch":           (slotOffset + i) % params.BeaconConfig().SlotsPerEpoch,
@@ -681,9 +676,6 @@ func (v *validator) logDuties(slot types.Slot, duties []*ethpb.DutiesResponse_Du
 			}).Info("Attestation schedule")
 		}
 		if proposerKeys[i] != "" {
-			if key, ok := validatorKeysLookup[proposerKeys[i]]; ok {
-				ValidatorNextProposalSlotGaugeVec.WithLabelValues(key).Set(float64(slotOffset + i))
-			}
 			log.WithField("slot", slotOffset+i).WithField("pubKey", proposerKeys[i]).Info("Proposal schedule")
 		}
 	}
