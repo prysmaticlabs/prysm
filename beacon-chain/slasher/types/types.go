52c888f0c4f3a3222714156e74f7ad7f85beac80
Raul Jordan <raul@prysmaticlabs.com>
2021-03-09 16:19:41 -0600

    gaz

--- beacon-chain/slasher/types/types.go
+++ beacon-chain/slasher/types/types.go
@@ -3,7 +3,6 @@ package types
 import (
 	types "github.com/prysmaticlabs/eth2-types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
-	"github.com/prysmaticlabs/prysm/beacon-chain/core/feed"
 )
 
 // ChunkKind to differentiate what kind of span we are working
@@ -13,11 +12,6 @@ type ChunkKind uint
 const (
 	MinSpan ChunkKind = iota
 	MaxSpan
-)
-
-const (
-	AttesterSlashing feed.EventType = iota
-	ProposerSlashing
 )
 
 // IndexedAttestationWrapper contains an indexed attestation with its
