3d6840e0b19f147f670025113763b08354192262
terence tsao <terence@prysmaticlabs.com>
2021-03-09 12:48:32 -0800

    Add stateV1

---
+++ beacon-chain/state/stateV1/types_bench_test.go
@@ -0,0 +1,15 @@
+package stateV1
+
+import (
+	"math"
+	"testing"
+)
+
+func BenchmarkReference_MinusRef(b *testing.B) {
+	ref := &reference{
+		refs: math.MaxUint64,
+	}
+	for i := 0; i < b.N; i++ {
+		ref.MinusRef()
+	}
+}
