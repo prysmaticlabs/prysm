fa92766095b27e65337e4af9305ce6098459b5c5
Victor Farazdagi <simple.square@gmail.com>
2021-03-10 23:03:26 +0300

    Fixes flaky TestBlocksFetcher_nonSkippedSlotAfter test (#8586)
    * Fixes flaky TestBlocksFetcher_nonSkippedSlotAfter test
    
    * fix conditional

--- beacon-chain/sync/initial-sync/blocks_fetcher_utils_test.go
+++ beacon-chain/sync/initial-sync/blocks_fetcher_utils_test.go
@@ -3,7 +3,9 @@ package initialsync
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/kevinms/leakybucket-go"
 	"github.com/libp2p/go-libp2p-core/network"
@@ -85,17 +87,23 @@ func TestBlocksFetcher_nonSkippedSlotAfter(t *testing.T) {
 	t.Run("test isolated non-skipped slot", func(t *testing.T) {
 		seekSlot := types.Slot(51264)
 		expectedSlot := types.Slot(55000)
-		found := false
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
 		var i int
-		for i = 0; i < 100; i++ {
-			slot, err := fetcher.nonSkippedSlotAfter(ctx, seekSlot)
-			assert.NoError(t, err)
-			if slot == expectedSlot {
-				found = true
-				break
+		go func() {
+			for {
+				i++
+				slot, err := fetcher.nonSkippedSlotAfter(ctx, seekSlot)
+				assert.NoError(t, err)
+				if slot == expectedSlot {
+					wg.Done()
+					break
+				}
 			}
-		}
-		if !found {
+		}()
+		if testutil.WaitTimeout(&wg, 5*time.Second) {
 			t.Errorf("Isolated non-skipped slot not found in %d iterations: %v", i, expectedSlot)
 		} else {
 			log.Debugf("Isolated non-skipped slot found in %d iterations", i)
