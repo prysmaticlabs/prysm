18529b3d5656c6284bc3e280b7a625bd70916b7c
rauljordan <raul@prysmaticlabs.com>
2021-03-17 13:59:10 -0500

    add validate funcs

--- beacon-chain/slasher/process_slashings.go
+++ beacon-chain/slasher/process_slashings.go
@@ -1,9 +1,33 @@
 package slasher
 
-import slashertypes "github.com/prysmaticlabs/prysm/beacon-chain/slasher/types"
+import (
+	"context"
 
-func (s *Service) processAttesterSlashings(slashings []*slashertypes.Slashing) {
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
+	slashertypes "github.com/prysmaticlabs/prysm/beacon-chain/slasher/types"
+)
+
+func (s *Service) processAttesterSlashings(ctx context.Context, slashings []*slashertypes.Slashing) {
 	for _, attSlashing := range slashings {
-		preState, err := s.serviceCfg.StateFetcher.AttestationPreState(ctx)
+		if err := s.verifyAttSignature(ctx, attSlashing.Attestation); err != nil {
+			continue
+		}
+		if err := s.verifyAttSignature(ctx, attSlashing.PrevAttestation); err != nil {
+			continue
+		}
+		// Log the slashing event.
+		logSlashingEvent(attSlashing)
+
+		// Submit to the slashing operations pool.
+		// TODO: Implement.
 	}
+}
+
+func (s *Service) verifyAttSignature(ctx context.Context, att *ethpb.IndexedAttestation) error {
+	preState, err := s.serviceCfg.StateFetcher.AttestationPreState(ctx, att.Data.Target)
+	if err != nil {
+		return err
+	}
+	return blocks.VerifyIndexedAttestation(ctx, preState, att)
 }
