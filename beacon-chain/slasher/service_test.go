e819ed0059dcfbdd31b069b893af1a57cd276505
rauljordan <raul@prysmaticlabs.com>
2021-03-17 12:08:45 -0500

    add in tests for logging

--- beacon-chain/slasher/service_test.go
+++ beacon-chain/slasher/service_test.go
@@ -9,6 +9,7 @@ import (
 	mock "github.com/prysmaticlabs/prysm/beacon-chain/blockchain/testing"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/feed"
 	statefeed "github.com/prysmaticlabs/prysm/beacon-chain/core/feed/state"
+	dbtest "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
 	"github.com/prysmaticlabs/prysm/shared/event"
 	"github.com/prysmaticlabs/prysm/shared/slotutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
@@ -24,19 +25,22 @@ func TestMain(m *testing.M) {
 }
 
 func TestService_StartStop_ChainStartEvent(t *testing.T) {
+	beaconDB := dbtest.SetupDB(t)
 	hook := logTest.NewGlobal()
 	srv, err := New(context.Background(), &ServiceConfig{
 		IndexedAttestationsFeed: new(event.Feed),
+		BeaconBlockHeadersFeed:  new(event.Feed),
 		StateNotifier:           &mock.MockStateNotifier{},
+		Database:                beaconDB,
 	})
 	require.NoError(t, err)
 	go srv.Start()
-	time.Sleep(time.Second)
+	time.Sleep(time.Millisecond * 100)
 	srv.serviceCfg.StateNotifier.StateFeed().Send(&feed.Event{
 		Type: statefeed.ChainStarted,
 		Data: &statefeed.ChainStartedData{StartTime: time.Now()},
 	})
-	time.Sleep(time.Second)
+	time.Sleep(time.Millisecond * 100)
 	srv.slotTicker = &slotutil.SlotTicker{}
 	require.NoError(t, srv.Stop())
 	require.NoError(t, srv.Status())
@@ -44,10 +48,13 @@ func TestService_StartStop_ChainStartEvent(t *testing.T) {
 }
 
 func TestService_StartStop_ChainAlreadyInitialized(t *testing.T) {
+	beaconDB := dbtest.SetupDB(t)
 	hook := logTest.NewGlobal()
 	srv, err := New(context.Background(), &ServiceConfig{
 		IndexedAttestationsFeed: new(event.Feed),
+		BeaconBlockHeadersFeed:  new(event.Feed),
 		StateNotifier:           &mock.MockStateNotifier{},
+		Database:                beaconDB,
 	})
 	require.NoError(t, err)
 	go srv.Start()
@@ -56,6 +63,7 @@ func TestService_StartStop_ChainAlreadyInitialized(t *testing.T) {
 		Type: statefeed.Initialized,
 		Data: &statefeed.InitializedData{StartTime: time.Now()},
 	})
+	time.Sleep(time.Millisecond * 100)
 	srv.slotTicker = &slotutil.SlotTicker{}
 	require.NoError(t, srv.Stop())
 	require.NoError(t, srv.Status())
