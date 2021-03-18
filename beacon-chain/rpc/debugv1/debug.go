eb2a909ae1af5556d94f701dd447e39213c55153
rkapka <rkapka@wp.pl>
2021-03-18 12:56:40 +0100

    implement grpc function

--- beacon-chain/rpc/debugv1/debug.go
+++ beacon-chain/rpc/debugv1/debug.go
@@ -6,15 +6,23 @@ import (
 	ptypes "github.com/gogo/protobuf/types"
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // GetBeaconState returns the full beacon state for a given state id.
-func (bs *Server) GetBeaconState(ctx context.Context, req *ethpb.StateRequest) (*ethpb.BeaconStateResponse, error) {
+func (ds *Server) GetBeaconState(ctx context.Context, req *ethpb.StateRequest) (*ethpb.BeaconStateResponse, error) {
+	state, err := ds.StateFetcher.State(ctx, req.StateId)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "could not get state: %v", err)
+	}
 
-	return nil, errors.New("unimplemented")
+	return &ethpb.BeaconStateResponse{
+		Data: state.ToProto(),
+	}, nil
 }
 
 // ListForkChoiceHeads retrieves the fork choice leaves for the current head.
-func (bs *Server) ListForkChoiceHeads(ctx context.Context, _ *ptypes.Empty) (*ethpb.ForkChoiceHeadsResponse, error) {
+func (ds *Server) ListForkChoiceHeads(ctx context.Context, _ *ptypes.Empty) (*ethpb.ForkChoiceHeadsResponse, error) {
 	return nil, errors.New("unimplemented")
 }
---
+++ beacon-chain/rpc/debugv1/debug_test.go
@@ -0,0 +1,1 @@
+package debugv1
