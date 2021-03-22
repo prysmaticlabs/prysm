c69c3524c5f3fb881918c0fd6a0f9dc31c18ad18
Raul Jordan <raul@prysmaticlabs.com>
2021-03-22 17:10:58 -0500

    attempt more test fixes

--- beacon-chain/slasher/detect_blocks_test.go
+++ beacon-chain/slasher/detect_blocks_test.go
@@ -7,10 +7,13 @@ import (
 	types "github.com/prysmaticlabs/eth2-types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	mock "github.com/prysmaticlabs/prysm/beacon-chain/blockchain/testing"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	dbtest "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
 	"github.com/prysmaticlabs/prysm/beacon-chain/operations/slashings"
 	slashertypes "github.com/prysmaticlabs/prysm/beacon-chain/slasher/types"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stategen"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/bls"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
@@ -22,8 +25,32 @@ func Test_processQueuedBlocks_DetectsDoubleProposals(t *testing.T) {
 	hook := logTest.NewGlobal()
 	beaconDB := dbtest.SetupDB(t)
 	ctx, cancel := context.WithCancel(context.Background())
+
 	beaconState, err := testutil.NewBeaconState()
 	require.NoError(t, err)
+
+	// Initialize validators in the state.
+	numVals := params.BeaconConfig().MinGenesisActiveValidatorCount
+	validators := make([]*ethpb.Validator, numVals)
+	privKeys := make([]bls.SecretKey, numVals)
+	for i := range validators {
+		privKey, err := bls.RandKey()
+		require.NoError(t, err)
+		privKeys[i] = privKey
+		validators[i] = &ethpb.Validator{
+			PublicKey:             privKey.PublicKey().Marshal(),
+			WithdrawalCredentials: make([]byte, 32),
+		}
+	}
+	err = beaconState.SetValidators(validators)
+	require.NoError(t, err)
+	domain, err := helpers.Domain(
+		beaconState.Fork(),
+		0,
+		params.BeaconConfig().DomainBeaconProposer,
+		beaconState.GenesisValidatorRoot(),
+	)
+
 	mockChain := &mock.ChainService{
 		State: beaconState,
 	}
@@ -49,12 +76,31 @@ func Test_processQueuedBlocks_DetectsDoubleProposals(t *testing.T) {
 		s.processQueuedBlocks(ctx, currentSlotChan)
 		exitChan <- struct{}{}
 	}()
-	s.blksQueue.extend([]*slashertypes.SignedBlockHeaderWrapper{
-		createProposalWrapper(t, 4, 1, []byte{1}),
-		createProposalWrapper(t, 4, 1, []byte{1}),
+
+	signedBlkHeaders := []*slashertypes.SignedBlockHeaderWrapper{
+		//createProposalWrapper(t, 4, 1, []byte{1}),
+		//createProposalWrapper(t, 4, 1, []byte{1}),
 		createProposalWrapper(t, 4, 1, []byte{1}),
 		createProposalWrapper(t, 4, 1, []byte{2}),
-	})
+	}
+
+	// Add valid signatures to the block headers we are testing.
+	for _, proposalWrapper := range signedBlkHeaders {
+		proposalWrapper.SignedBeaconBlockHeader.Header.ParentRoot = parentRoot[:]
+		headerHtr, err := proposalWrapper.SignedBeaconBlockHeader.Header.HashTreeRoot()
+		require.NoError(t, err)
+		container := &pb.SigningData{
+			ObjectRoot: headerHtr[:],
+			Domain:     domain,
+		}
+		signingRoot, err := container.HashTreeRoot()
+		require.NoError(t, err)
+		privKey := privKeys[proposalWrapper.SignedBeaconBlockHeader.Header.ProposerIndex]
+		proposalWrapper.SignedBeaconBlockHeader.Signature = privKey.Sign(signingRoot[:]).Marshal()
+	}
+
+	s.blksQueue.extend(signedBlkHeaders)
+
 	currentSlot := types.Slot(4)
 	currentSlotChan <- currentSlot
 	cancel()
