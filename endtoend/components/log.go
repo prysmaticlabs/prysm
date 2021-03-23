510015de2ad63e044f0a8d299ab972c45513894f
Victor Farazdagi <simple.square@gmail.com>
2021-03-23 04:39:42 +0300

    update log formatting

--- endtoend/components/log.go
+++ endtoend/components/log.go
@@ -1,5 +1,11 @@
 package components
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/sirupsen/logrus"
+)
 
 var log = logrus.WithField("prefix", "components")
+
+func init() {
+	logrus.SetReportCaller(true)
+}
