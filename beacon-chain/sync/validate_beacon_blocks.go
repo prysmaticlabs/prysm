d629297b80d55ede272a4ca6f4797b14f5847fd1
Raul Jordan <raul@prysmaticlabs.com>
2021-03-26 16:38:26 -0500

    fix feed items

--- beacon-chain/sync/validate_beacon_blocks.go
+++ beacon-chain/sync/validate_beacon_blocks.go
@@ -82,7 +82,7 @@ func (s *Service) validateBeaconBlockPubSub(ctx context.Context, pid peer.ID, ms
 			if err != nil {
 				log.WithError(err).WithField("blockSlot", blk.Block.Slot).Warn("Could not extract block header")
 			}
-			s.slasherBlockHeadersFeed.Send(blockHeader)
+			s.cfg.SlasherBlockHeadersFeed.Send(blockHeader)
 		}()
 	}
 
