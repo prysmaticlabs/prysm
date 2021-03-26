bd648c528eb43c5610ffb16418de3414ccf1a560
ahadda5 <ahadda5@gmail.com>
2021-03-26 19:46:58 +0400

    separate createwallet into imported and derived. Recover is derived

--- validator/rpc/wallet.go
+++ validator/rpc/wallet.go
@@ -60,8 +60,7 @@ func (s *Server) CreateWallet(ctx context.Context, req *pb.CreateWalletRequest) 
 			},
 		}, nil
 	}
-	switch req.Keymanager {
-	case pb.KeymanagerKind_IMPORTED:
+	if req.Keymanager == pb.KeymanagerKind_IMPORTED {
 		_, err := accounts.CreateWalletWithKeymanager(ctx, &accounts.CreateWalletConfig{
 			WalletCfg: &wallet.Config{
 				WalletDir:      walletDir,
@@ -89,42 +88,8 @@ func (s *Server) CreateWallet(ctx context.Context, req *pb.CreateWalletRequest) 
 				KeymanagerKind: pb.KeymanagerKind_IMPORTED,
 			},
 		}, nil
-	case pb.KeymanagerKind_DERIVED:
-		if req.NumAccounts < 1 {
-			return nil, status.Error(codes.InvalidArgument, "Must create at least 1 validator account")
-		}
-		if req.Mnemonic == "" {
-			return nil, status.Error(codes.InvalidArgument, "Must include mnemonic in request")
-		}
-		if _, err := accounts.RecoverWallet(ctx, &accounts.RecoverWalletConfig{
-			WalletDir:      walletDir,
-			WalletPassword: req.WalletPassword,
-			Mnemonic:       req.Mnemonic,
-			NumAccounts:    int(req.NumAccounts),
-		}); err != nil {
-			return nil, err
-		}
-		if err := s.initializeWallet(ctx, &wallet.Config{
-			WalletDir:      walletDir,
-			KeymanagerKind: keymanager.Imported,
-			WalletPassword: req.WalletPassword,
-		}); err != nil {
-			return nil, err
-		}
-		if err := writeWalletPasswordToDisk(walletDir, req.WalletPassword); err != nil {
-			return nil, status.Error(codes.Internal, "Could not write wallet password to disk")
-		}
-		return &pb.CreateWalletResponse{
-			Wallet: &pb.WalletResponse{
-				WalletPath:     walletDir,
-				KeymanagerKind: pb.KeymanagerKind_DERIVED,
-			},
-		}, nil
-	case pb.KeymanagerKind_REMOTE:
-		return nil, status.Error(codes.Unimplemented, "Remote keymanager not yet supported")
-	default:
-		return nil, status.Errorf(codes.InvalidArgument, "Keymanager type %T not yet supported", req.Keymanager)
 	}
+	return nil, status.Errorf(codes.InvalidArgument, "Keymanager type %T not supported through web", req.Keymanager)
 }
 
 // WalletConfig returns the wallet's configuration. If no wallet exists, we return an empty response.
@@ -174,9 +139,9 @@ func (s *Server) WalletConfig(ctx context.Context, _ *empty.Empty) (*pb.WalletRe
 // Set the wallet password to req.WalletPassword (needs password validation)
 // Create the wallet
 // Return &ptypes.Empty{}, nil if nothing went wrong
-func (s *Server) RecoverWallet(ctx context.Context, req *pb.RecoverWalletRequest) error {
+func (s *Server) RecoverWallet(ctx context.Context, req *pb.RecoverWalletRequest) (*pb.CreateWalletResponse, error)  {
 
-	//retreive all params(numAccounts,lang,password etc) and call accounts.RecoverWallet 
+	//retreive all params(numAccounts,lang,password etc) and call the RecoverWalletConfig from the inputs
 	
 	//check validate mnemonic with chosen language
 	// this language list should be dynamic in the future
@@ -192,12 +157,12 @@ func (s *Server) RecoverWallet(ctx context.Context, req *pb.RecoverWalletRequest
 		"spanish":             wordlists.Spanish,
 	}
 	if _, ok := allowedLanguages[language]; !ok {
-		return errors.New("input not in the list of allowed languages")
+		return nil,errors.New("input not in the list of allowed languages")
 	}
 	bip39.SetWordList(language)
 	mnemonic := req.mnemonic
-	if err := validateMnemonic(mnemonic); err != nil {
-		return err
+	if err := accounts.validateMnemonic(mnemonic); err != nil {
+		return nil,err
 	}
 	
 	//skipMnemonic25 is not supported through web-ui for now
@@ -209,25 +174,40 @@ func (s *Server) RecoverWallet(ctx context.Context, req *pb.RecoverWalletRequest
 	//web-ui should check the new and confirmed password are equal
 	walletPassword  := req.walletPassword
 	if err := promptutil.ValidatePasswordInput(walletPassword); err != nil {
-		return errors.Wrap(err, "password did not pass validation")
+		return nil,errors.Wrap(err, "password did not pass validation")
 	}
 
 	numAccounts  := int(req.numAccounts)
 	if numAccounts < 1 {
-		return status.Error(codes.InvalidArgument, "Must create at least 1 validator account")
+		return nil,status.Error(codes.InvalidArgument, "Must create at least 1 validator account")
 	}
 
-	//try to recover 
+	//recover
 	if _, err := accounts.RecoverWallet(ctx, &accounts.RecoverWalletConfig{
-			WalletDir:      walletDir,
-			WalletPassword: walletPassword,
-			Mnemonic:       mnemonic,
-			NumAccounts:    numAccounts,
+		WalletDir:      walletDir,
+		WalletPassword: walletPassword,
+		Mnemonic:       mnemonic,
+		NumAccounts:    numAccounts,
+		//Mnemonic25thWord: req.Mnemonic25thWord, support Mnemonic
 	}); err != nil {
-			return err
+		return nil, err
 	}
-	return nil
-		
+	if err := s.initializeWallet(ctx, &wallet.Config{
+		WalletDir:      walletDir,
+		KeymanagerKind: pb.KeymanagerKind_DERIVED,
+		WalletPassword: walletPassword,
+	}); err != nil {
+		return nil, err
+	}
+	if err := writeWalletPasswordToDisk(walletDir, walletPassword); err != nil {
+		return nil, status.Error(codes.Internal, "Could not write wallet password to disk")
+	}
+	return &pb.CreateWalletResponse{
+		Wallet: &pb.WalletResponse{
+			WalletPath:     walletDir,
+			KeymanagerKind: pb.KeymanagerKind_DERIVED,
+		},
+	}, nil
 }
 
 // GenerateMnemonic creates a new, random bip39 mnemonic phrase.
