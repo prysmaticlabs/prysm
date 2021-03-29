5f5b94068535a905d06814a9e5b92468cee9aa87
terence tsao <terence@prysmaticlabs.com>
2021-03-29 07:16:20 -0700

    Preston's feedback and tests

--- beacon-chain/state/state-altair/deprecated_setters.go
+++ beacon-chain/state/state-altair/deprecated_setters.go
@@ -1,25 +1,26 @@
 package state_altair
 
 import (
+	"github.com/pkg/errors"
 	pbp2p "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 )
 
 // SetPreviousEpochAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) SetPreviousEpochAttestations(val []*pbp2p.PendingAttestation) error {
-	panic("CurrentEpochAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("CurrentEpochAttestations is not supported for hard fork 1 beacon state")
 }
 
 // SetCurrentEpochAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) SetCurrentEpochAttestations(val []*pbp2p.PendingAttestation) error {
-	panic("CurrentEpochAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("CurrentEpochAttestations is not supported for hard fork 1 beacon state")
 }
 
 // AppendCurrentEpochAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) AppendCurrentEpochAttestations(val *pbp2p.PendingAttestation) error {
-	panic("AppendCurrentEpochAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("AppendCurrentEpochAttestations is not supported for hard fork 1 beacon state")
 }
 
 // AppendPreviousEpochAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) AppendPreviousEpochAttestations(val *pbp2p.PendingAttestation) error {
-	panic("AppendPreviousEpochAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("AppendPreviousEpochAttestations is not supported for hard fork 1 beacon state")
 }
