440b57ec3f1107b25ec0e044e43e28397ef76a3e
ahadda5 <ahadda5@gmail.com>
2021-03-31 21:33:10 +0400

    unrelated files

--- proto/beacon/p2p/v1/messages.pb.go
+++ proto/beacon/p2p/v1/messages.pb.go
@@ -5,13 +5,14 @@ package ethereum_beacon_p2p_v1
 
 import (
 	fmt "fmt"
+	io "io"
+	math "math"
+	math_bits "math/bits"
+
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	github_com_prysmaticlabs_eth2_types "github.com/prysmaticlabs/eth2-types"
 	github_com_prysmaticlabs_go_bitfield "github.com/prysmaticlabs/go-bitfield"
-	io "io"
-	math "math"
-	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
