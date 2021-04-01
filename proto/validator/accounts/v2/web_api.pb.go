3f7e7245c2c8cbccc63f1eac49165ba865c37462
ahadda5 <ahadda5@gmail.com>
2021-04-01 17:56:23 +0400

    restore imports

--- proto/validator/accounts/v2/web_api.pb.go
+++ proto/validator/accounts/v2/web_api.pb.go
@@ -6,6 +6,10 @@ package ethereum_validator_accounts_v2
 import (
 	context "context"
 	fmt "fmt"
+	io "io"
+	math "math"
+	math_bits "math/bits"
+
 	proto "github.com/gogo/protobuf/proto"
 	empty "github.com/golang/protobuf/ptypes/empty"
 	v1alpha1 "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
@@ -14,9 +18,6 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	io "io"
-	math "math"
-	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
