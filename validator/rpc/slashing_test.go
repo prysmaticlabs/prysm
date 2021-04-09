631cf54a4c52bebf99a5d70d61469156587157cb
ahadda5 <ahadda5@gmail.com>
2021-04-09 17:21:51 +0400

    round trip test

--- validator/rpc/slashing_test.go
+++ validator/rpc/slashing_test.go
@@ -13,6 +13,8 @@ import (
 	"github.com/prysmaticlabs/prysm/validator/db/kv"
 	"github.com/prysmaticlabs/prysm/validator/keymanager"
 	mocks "github.com/prysmaticlabs/prysm/validator/testing"
+	"github.com/prysmaticlabs/prysm/validator/slashing-protection/local/standard-protection-format/format"
+
 )
 
 func TestImportSlashingProtection_Preconditions(t *testing.T) {
@@ -95,18 +97,6 @@ func TestExportSlashingProtection_Preconditions(t *testing.T) {
 	_, err := s.ExportSlashingProtection(ctx, &empty.Empty{})
 	require.ErrorContains(t, "err finding validator database at path", err)
 
-	// Create Wallet and add to server for more realistic testing.
-	w, err := accounts.CreateWalletWithKeymanager(ctx, &accounts.CreateWalletConfig{
-		WalletCfg: &wallet.Config{
-			WalletDir:      defaultWalletPath,
-			KeymanagerKind: keymanager.Imported,
-			WalletPassword: strongPass,
-		},
-		SkipMnemonicConfirm: true,
-	})
-	require.NoError(t, err)
-	s.wallet = w
-
 	numValidators := 10
 	// Create public keys for the mock validator DB.
 	pubKeys, err := mocks.CreateRandomPubKeys(numValidators)
@@ -126,4 +116,62 @@ func TestExportSlashingProtection_Preconditions(t *testing.T) {
 
 	_, err = s.ExportSlashingProtection(ctx, &empty.Empty{})
 	require.NoError(t, err)
+}
+
+func TestImportExportSlashingProtection_RoundTrip(t *testing.T) {
+	ctx := context.Background()
+	localWalletDir := setupWalletDir(t)
+	defaultWalletPath = localWalletDir
+
+	s := &Server{
+		walletDir: defaultWalletPath,
+	}
+
+	numValidators := 10
+	// Create public keys for the mock validator DB.
+	pubKeys, err := mocks.CreateRandomPubKeys(numValidators)
+	require.NoError(t, err)
+
+	// Create a validator database.
+	validatorDB, err := kv.NewKVStore(ctx, defaultWalletPath, &kv.Config{
+		PubKeys: pubKeys,
+	})
+	require.NoError(t, err)
+	s.valDB = validatorDB
+
+	// Have to close it after import is done otherwise it complains db is not open.
+	defer func() {
+		require.NoError(t, validatorDB.Close())
+	}()
+
+	// Generate mock slashing history.
+	attestingHistory := make([][]*kv.AttestationRecord, 0)
+	proposalHistory := make([]kv.ProposalHistoryForPubkey, len(pubKeys))
+	for i := 0; i < len(pubKeys); i++ {
+		proposalHistory[i].Proposals = make([]kv.Proposal, 0)
+	}
+	mockJSON, err := mocks.MockSlashingProtectionJSON(pubKeys, attestingHistory, proposalHistory)
+	require.NoError(t, err)
+
+	// JSON encode the protection JSON and save it in rpc req.
+	encoded, err := json.Marshal(mockJSON)
+	require.NoError(t, err)
+	req := &pb.ImportSlashingProtectionRequest{
+		SlashingProtectionJson: string(encoded),
+	}
+
+	_, err = s.ImportSlashingProtection(ctx, req)
+	require.NoError(t, err)
+
+	reqE, err := s.ExportSlashingProtection(ctx, &empty.Empty{})
+	require.NoError(t, err)
+
+	// Attempt to read the exported data and convert from string to EIP-3076.
+	enc := []byte(reqE.File)
+
+	receivedJSON := &format.EIPSlashingProtectionFormat{}
+	err = json.Unmarshal(enc, receivedJSON)
+	require.NoError(t, err)
+
+	require.DeepEqual(t, mockJSON.Metadata, receivedJSON.Metadata)
 }
