57f79714dd8e24b1feab48aaba568a8631564285
Victor Farazdagi <simple.square@gmail.com>
2021-04-13 12:17:39 +0300

    combine params

--- beacon-chain/core/altair/attestation.go
+++ beacon-chain/core/altair/attestation.go
@@ -237,11 +237,11 @@ func rewardProposer(beaconState iface.BeaconState, proposerRewardNumerator uint6
 }
 
 // HasValidatorFlag returns true if the flag at position has set.
-func HasValidatorFlag(flag uint8, flagPosition uint8) bool {
+func HasValidatorFlag(flag, flagPosition uint8) bool {
 	return ((flag >> flagPosition) & 1) == 1
 }
 
 // AddValidatorFlag adds new validator flag to existing one.
-func AddValidatorFlag(flag uint8, flagPosition uint8) uint8 {
+func AddValidatorFlag(flag, flagPosition uint8) uint8 {
 	return flag | (1 << flagPosition)
 }
