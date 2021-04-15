2b2cb40bfd0698fbfa89ba3cd5cad69546dc1178
Raul Jordan <raul@prysmaticlabs.com>
2021-04-14 23:08:22 -0500

    begin same pruning process for attestations, correctness first, then deduplication later

--- beacon-chain/db/slasherkv/pruning.go
+++ beacon-chain/db/slasherkv/pruning.go
@@ -63,7 +63,7 @@ func (s *Store) PruneProposals(
 			c := proposalBkt.Cursor()
 
 			var lastPrunedEpoch, epochsPruned types.Epoch
-			// We begin a pruning iteration at starting from the current slot cursor.
+			// We begin a pruning iteration at starting from the first item in the bucket.
 			for k, _ := c.First(); k != nil; k, _ = c.Next() {
 				// We check the slot from the current key in the database.
 				// If we have hit a slot that is greater than the end slot of the pruning process,
@@ -87,7 +87,6 @@ func (s *Store) PruneProposals(
 				//  (slot = 3 ++ validatorIndex = 2) => ...
 				// so we only mark an epoch as pruned if the epoch of the current object
 				// under the cursor has changed.
-				// We delete the proposal object from the database.
 				if err := proposalBkt.Delete(k); err != nil {
 					return err
 				}
@@ -110,29 +109,85 @@ func (s *Store) PruneProposals(
 // PruneAttestations prunes all proposal data older than historyLength.
 func (s *Store) PruneAttestations(ctx context.Context, currentEpoch, pruningEpochIncrements, historyLength types.Epoch) error {
 	if currentEpoch < historyLength {
+		log.Debugf("Current epoch %d < history length %d, nothing to prune", currentEpoch, historyLength)
 		return nil
 	}
-	// + 1 here so we can prune everything less than this, but not equal.
+	// We can prune everything less than the current epoch - history length.
 	endPruneEpoch := currentEpoch - historyLength
-	epochEnc := encodeTargetEpoch(endPruneEpoch, historyLength)
-	return s.db.Update(func(tx *bolt.Tx) error {
-		signingRootsBkt := tx.Bucket(attestationDataRootsBucket)
-		attRecordsBkt := tx.Bucket(attestationRecordsBucket)
-		c := signingRootsBkt.Cursor()
-		for k, v := c.Seek(epochEnc); k != nil; k, v = c.Prev() {
-			if !epochPrefixLessThan(k, epochEnc) {
-				continue
-			}
-			slasherAttestationsPrunedTotal.Inc()
-			if err := signingRootsBkt.Delete(k); err != nil {
-				return err
-			}
-			if err := attRecordsBkt.Delete(v); err != nil {
-				return err
-			}
-		}
+	encodedEndPruneEpoch := fssz.MarshalUint64([]byte{}, uint64(endPruneEpoch))
+
+	// We retrieve the lowest stored epoch in the proposals bucket.
+	var lowestEpoch types.Epoch
+	if err := s.db.View(func(tx *bolt.Tx) error {
+		bkt := tx.Bucket(attestationDataRootsBucket)
+		c := bkt.Cursor()
+		k, _ := c.First()
+		lowestEpoch = types.Epoch(binary.LittleEndian.Uint64(k[:2]))
 		return nil
-	})
+	}); err != nil {
+		return err
+	}
+
+	// If the lowest slot is greater than or equal to the end pruning slot,
+	// there is nothing to prune, so we return early.
+	if lowestEpoch >= endPruneEpoch {
+		log.Debugf("Lowest epoch %d is >= pruning epoch %d, nothing to prune", lowestEpoch, endPruneEpoch)
+		return nil
+	}
+
+	// We prune in increments of `pruningEpochIncrements` at a time to prevent
+	// a long-running bolt transaction which overwhelms CPU and memory.
+	// While we still have epochs to prune based on a cursor, we continue the pruning process.
+	epochAtCursor := lowestEpoch
+	for endPruneEpoch-epochAtCursor > 0 {
+		// Each pruning iteration involves a unique bolt transaction. Given pruning can be
+		// a very expensive process which puts pressure on the database, we perform
+		// the process in a batch-based method using a cursor to proceed to the next batch.
+		log.Debugf("Pruned %d/%d epochs worth of attestations", epochAtCursor, endPruneEpoch-1)
+		if err := s.db.Update(func(tx *bolt.Tx) error {
+			bkt := tx.Bucket(attestationDataRootsBucket)
+			c := bkt.Cursor()
+
+			var lastPrunedEpoch, epochsPruned types.Epoch
+			// We begin a pruning iteration at starting from the first item in the bucket.
+			for k, _ := c.First(); k != nil; k, _ = c.Next() {
+				// We check the epoch from the current key in the database.
+				// If we have hit an epoch that is greater than the end epoch of the pruning process,
+				// we then completely exit the process as we are done.
+				if !epochPrefixLessThan(k, encodedEndPruneEpoch) {
+					epochAtCursor = endPruneEpoch
+					return nil
+				}
+				// If we have pruned N epochs in this pruning iteration,
+				// we exit from the bolt transaction.
+				if epochsPruned >= pruningEpochIncrements {
+					return nil
+				}
+				epochAtCursor = types.Epoch(binary.LittleEndian.Uint64(k[:2]))
+
+				// Attestation in the database look like this:
+				//  (target_epoch ++ _) => encode(attestation)
+				// so it is possible we have a few adjacent objects that have the same slot, such as
+				//  (target_epoch = 3 ++ _) => encode(attestation)
+				// so we only mark an epoch as pruned if the epoch of the current object
+				// under the cursor has changed.
+				if err := bkt.Delete(k); err != nil {
+					return err
+				}
+				if epochAtCursor == 0 {
+					epochsPruned = 1
+				} else if epochAtCursor > lastPrunedEpoch {
+					epochsPruned++
+					lastPrunedEpoch = epochAtCursor
+				}
+				slasherAttestationsPrunedTotal.Inc()
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func slotFromProposalKey(key []byte) types.Slot {
--- beacon-chain/db/slasherkv/pruning_test.go
+++ beacon-chain/db/slasherkv/pruning_test.go
@@ -23,7 +23,7 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
-func TestStore_PruneProposals_OK(t *testing.T) {
+func TestStore_PruneProposals(t *testing.T) {
 	ctx := context.Background()
 
 	// If the current slot is less than the history length we track in slasher, there
@@ -157,125 +157,138 @@ func TestStore_PruneProposals_OK(t *testing.T) {
 	})
 }
 
-func TestStore_PruneProposals_Logs(t *testing.T) {
+func TestStore_PruneAttestations_OK(t *testing.T) {
 	ctx := context.Background()
-	proposalsInDB := []*slashertypes.SignedBlockHeaderWrapper{
-		createProposalWrapper(t, types.Slot(2), 0, []byte{1}),
-		createProposalWrapper(t, types.Slot(8), 0, []byte{1}),
-		createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-	}
-	afterPruning := []*slashertypes.SignedBlockHeaderWrapper{
-		createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-	}
-	epoch := types.Epoch(2)
-	beaconDB := setupDB(t)
-	require.NoError(t, beaconDB.SaveBlockProposals(ctx, proposalsInDB))
 
-	epochPruningIncrements := types.Epoch(100)
-	err := beaconDB.PruneProposals(ctx, epoch, epochPruningIncrements, defaultHistoryLength)
+	// If the current slot is less than the history length we track in slasher, there
+	// is nothing to prune yet, so we expect exiting early.
+	t.Run("current_epoch_less_than_history_length", func(t *testing.T) {
+		hook := logTest.NewGlobal()
+		beaconDB := setupDB(t)
+		epochPruningIncrements := types.Epoch(100)
+		currentEpoch := types.Epoch(1)
+		historyLength := types.Epoch(2)
+		err := beaconDB.PruneAttestations(ctx, currentEpoch, epochPruningIncrements, historyLength)
+		require.NoError(t, err)
+		require.LogsContain(t, hook, "Current epoch 1 < history length 2, nothing to prune")
+	})
 
-	// Second time checking same proposals but all with different signing root should
-	// return all double proposals.
-	slashable := make([]*slashertypes.SignedBlockHeaderWrapper, len(afterPruning))
-	for i := 0; i < len(afterPruning); i++ {
-		slashable[i] = createProposalWrapper(t, afterPruning[i].SignedBeaconBlockHeader.Header.Slot, afterPruning[i].SignedBeaconBlockHeader.Header.ProposerIndex, []byte{2})
-	}
+	// If the lowest stored epoch in the database is >= the end epoch of the pruning process,
+	// there is nothing to prune, so we also expect exiting early.
+	t.Run("lowest_stored_epoch_greater_than_pruning_limit_epoch", func(t *testing.T) {
+		hook := logTest.NewGlobal()
+		beaconDB := setupDB(t)
+		epochPruningIncrements := types.Epoch(100)
 
-	doubleProposals, err := beaconDB.CheckDoubleBlockProposals(ctx, slashable)
-	require.NoError(t, err)
-	require.Equal(t, len(afterPruning), len(doubleProposals))
-	for i, existing := range doubleProposals {
-		require.DeepSSZEqual(t, existing.Header_1, afterPruning[i].SignedBeaconBlockHeader)
-	}
-}
+		// With a current epoch of 20 and a history length of 10, we should be pruning
+		// everything before epoch (20 - 10) = 10.
+		currentEpoch := types.Epoch(20)
+		historyLength := types.Epoch(10)
 
-func TestStore_PruneProposals(t *testing.T) {
-	ctx := context.Background()
-	tests := []struct {
-		name          string
-		proposalsInDB []*slashertypes.SignedBlockHeaderWrapper
-		afterPruning  []*slashertypes.SignedBlockHeaderWrapper
-		epoch         types.Epoch
-		historyLength types.Epoch
-		wantErr       bool
-	}{
-		{
-			name: "should delete all proposals under epoch 2",
-			proposalsInDB: []*slashertypes.SignedBlockHeaderWrapper{
-				createProposalWrapper(t, types.Slot(2), 0, []byte{1}),
-				createProposalWrapper(t, types.Slot(8), 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-			},
-			afterPruning: []*slashertypes.SignedBlockHeaderWrapper{
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-			},
-			epoch: 2,
-		},
-		{
-			name: "should delete all proposals under epoch 5 - historyLength 3",
-			proposalsInDB: []*slashertypes.SignedBlockHeaderWrapper{
-				createProposalWrapper(t, types.Slot(2), 0, []byte{1}),
-				createProposalWrapper(t, types.Slot(8), 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*4, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*5, 0, []byte{1}),
-			},
-			afterPruning: []*slashertypes.SignedBlockHeaderWrapper{
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*4, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*5, 0, []byte{1}),
-			},
-			historyLength: 3,
-			epoch:         5,
-		},
-		{
-			name: "should delete all proposals under epoch 4",
-			proposalsInDB: []*slashertypes.SignedBlockHeaderWrapper{
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*0, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*1, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*2, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-			},
-			afterPruning: []*slashertypes.SignedBlockHeaderWrapper{},
-			epoch:        4,
-		},
-		{
-			name: "no proposal to delete under epoch 1",
-			proposalsInDB: []*slashertypes.SignedBlockHeaderWrapper{
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*2, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-			},
-			afterPruning: []*slashertypes.SignedBlockHeaderWrapper{
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*2, 0, []byte{1}),
-				createProposalWrapper(t, params.BeaconConfig().SlotsPerEpoch*3, 0, []byte{1}),
-			},
-			epoch: 5,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			epochPruningIncrements := types.Epoch(100)
-			beaconDB := setupDB(t)
-			require.NoError(t, beaconDB.SaveBlockProposals(ctx, tt.proposalsInDB))
-			if err := beaconDB.PruneProposals(ctx, tt.epoch, epochPruningIncrements, tt.historyLength); (err != nil) != tt.wantErr {
-				t.Errorf("PruneProposals() error = %v, wantErr %v", err, tt.wantErr)
+		pruningLimitEpoch := currentEpoch - historyLength
+		lowestStoredSlot, err := helpers.StartSlot(pruningLimitEpoch)
+		require.NoError(t, err)
+
+		err = beaconDB.db.Update(func(tx *bolt.Tx) error {
+			bkt := tx.Bucket(attestationDataRootsBucket)
+			key, err := keyForValidatorProposal(&slashertypes.SignedBlockHeaderWrapper{
+				SignedBeaconBlockHeader: &ethpb.SignedBeaconBlockHeader{
+					Header: &ethpb.BeaconBlockHeader{
+						Slot:          lowestStoredSlot,
+						ProposerIndex: 0,
+					},
+				},
+			})
+			if err != nil {
+				return err
 			}
-
-			// Second time checking same proposals but all with different signing root should
-			// return all double proposals.
-			slashable := make([]*slashertypes.SignedBlockHeaderWrapper, len(tt.afterPruning))
-			for i := 0; i < len(tt.afterPruning); i++ {
-				slashable[i] = createProposalWrapper(t, tt.afterPruning[i].SignedBeaconBlockHeader.Header.Slot, tt.afterPruning[i].SignedBeaconBlockHeader.Header.ProposerIndex, []byte{2})
-			}
-
-			doubleProposals, err := beaconDB.CheckDoubleBlockProposals(ctx, slashable)
-			require.NoError(t, err)
-			require.Equal(t, len(tt.afterPruning), len(doubleProposals))
-			for i, existing := range doubleProposals {
-				require.DeepSSZEqual(t, existing.Header_1, tt.afterPruning[i].SignedBeaconBlockHeader)
-			}
+			return bkt.Put(key, []byte("hi"))
 		})
-	}
+		require.NoError(t, err)
+
+		err = beaconDB.PruneAttestations(ctx, currentEpoch, epochPruningIncrements, historyLength)
+		require.NoError(t, err)
+		expectedLog := fmt.Sprintf(
+			"Lowest slot %d is >= pruning slot %d, nothing to prune", lowestStoredSlot, lowestStoredSlot,
+		)
+		require.LogsContain(t, hook, expectedLog)
+	})
+
+	// Prune in increments until the cursor reaches the pruning limit epoch, and we expect
+	// that all the proposals written are deleted from the database, while those above the
+	// pruning limit epoch are kept intact.
+	t.Run("prune_in_full_increments_and_verify_deletions", func(t *testing.T) {
+		hook := logTest.NewGlobal()
+		beaconDB := setupDB(t)
+
+		config := params.BeaconConfig()
+		copyConfig := config.Copy()
+		copyConfig.SlotsPerEpoch = 2
+		params.OverrideBeaconConfig(copyConfig)
+		defer params.OverrideBeaconConfig(config)
+
+		epochPruningIncrements := types.Epoch(2)
+		historyLength := types.Epoch(10)
+		currentEpoch := types.Epoch(20)
+		pruningLimitEpoch := currentEpoch - historyLength
+
+		// We create proposals from genesis to the current epoch, with 2 proposals
+		// at each slot to ensure the entire pruning logic works correctly.
+		slotsPerEpoch := params.BeaconConfig().SlotsPerEpoch
+		proposals := make([]*slashertypes.SignedBlockHeaderWrapper, 0, uint64(currentEpoch)*uint64(slotsPerEpoch)*2)
+		for i := types.Epoch(0); i < currentEpoch; i++ {
+			startSlot, err := helpers.StartSlot(i)
+			require.NoError(t, err)
+			endSlot, err := helpers.StartSlot(i + 1)
+			require.NoError(t, err)
+			for j := startSlot; j < endSlot; j++ {
+				prop1 := createProposalWrapper(t, j, 0 /* proposer index */, []byte{0})
+				prop2 := createProposalWrapper(t, j, 2 /* proposer index */, []byte{1})
+				proposals = append(proposals, prop1, prop2)
+			}
+		}
+
+		require.NoError(t, beaconDB.SaveBlockProposals(ctx, proposals))
+
+		// We expect pruning completes without an issue and properly logs progress.
+		err := beaconDB.PruneProposals(ctx, currentEpoch, epochPruningIncrements, historyLength)
+		require.NoError(t, err)
+
+		for i := types.Epoch(0); i < pruningLimitEpoch; i++ {
+			wantedLog := fmt.Sprintf("Pruned %d/%d epochs worth of proposals", i, pruningLimitEpoch-1)
+			require.LogsContain(t, hook, wantedLog)
+		}
+
+		// Everything before epoch 10 should be deleted.
+		for i := types.Epoch(0); i < pruningLimitEpoch; i++ {
+			err = beaconDB.db.View(func(tx *bolt.Tx) error {
+				bkt := tx.Bucket(proposalRecordsBucket)
+				startSlot, err := helpers.StartSlot(i)
+				require.NoError(t, err)
+				endSlot, err := helpers.StartSlot(i + 1)
+				for j := startSlot; j < endSlot; j++ {
+					prop1 := createProposalWrapper(t, j, 0 /* proposer index */, []byte{0})
+					prop1Key, err := keyForValidatorProposal(prop1)
+					if err != nil {
+						return err
+					}
+					prop2 := createProposalWrapper(t, j, 2 /* proposer index */, []byte{1})
+					prop2Key, err := keyForValidatorProposal(prop2)
+					if err != nil {
+						return err
+					}
+					if bkt.Get(prop1Key) != nil {
+						return fmt.Errorf("proposal still exists for epoch %d, validator 0", j)
+					}
+					if bkt.Get(prop2Key) != nil {
+						return fmt.Errorf("proposal still exists for slot %d, validator 1", j)
+					}
+				}
+				return nil
+			})
+			require.NoError(t, err)
+		}
+	})
 }
 
 func TestStore_PruneAttestations(t *testing.T) {
