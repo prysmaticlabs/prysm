73e0cee4d4e7487f78da5ea4590417de21757186
Victor Farazdagi <simple.square@gmail.com>
2021-04-16 18:37:46 +0300

    Move TestMain into dedicated file

---
+++ validator/db/kv/kv_test.go
@@ -0,0 +1,15 @@
+package kv
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestMain(m *testing.M) {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetOutput(ioutil.Discard)
+
+	m.Run()
+}
