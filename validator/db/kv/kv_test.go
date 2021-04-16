c1e44c2670832a8f96b415bb9bf51a0176084a1b
Victor Farazdagi <simple.square@gmail.com>
2021-04-16 19:04:15 +0300

    update db_test

--- validator/db/kv/kv_test.go
+++ validator/db/kv/kv_test.go
@@ -4,6 +4,9 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"context"
+
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	"github.com/sirupsen/logrus"
 )
 
@@ -12,4 +15,19 @@ func TestMain(m *testing.M) {
 	logrus.SetOutput(ioutil.Discard)
 
 	m.Run()
+}
+
+// setupDB instantiates and returns a DB instance for the validator client.
+func setupDB(t testing.TB, pubkeys [][48]byte) *Store {
+	db, err := NewKVStore(context.Background(), t.TempDir(), &Config{
+		PubKeys: pubkeys,
+	})
+	require.NoError(t, err, "Failed to instantiate DB")
+	err = db.UpdatePublicKeysBuckets(pubkeys)
+	require.NoError(t, err, "Failed to create old buckets for public keys")
+	t.Cleanup(func() {
+		require.NoError(t, db.Close(), "Failed to close database")
+		require.NoError(t, db.ClearDB(), "Failed to clear database")
+	})
+	return db
 }
