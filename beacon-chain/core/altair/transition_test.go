9d22ea840e8989baa6635d616ee201515298cef5
terence tsao <terence@prysmaticlabs.com>
2021-04-16 09:09:22 -0700

    Altair: Implement `ProcessSlots` and tests (#8774)

--- beacon-chain/core/altair/transition_test.go
+++ beacon-chain/core/altair/transition_test.go
@@ -1,4 +1,4 @@
-package altair
+package altair_test
 
 import (
 	"context"
@@ -8,10 +8,12 @@ import (
 	types "github.com/prysmaticlabs/eth2-types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/go-bitfield"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/altair"
 	statealtair "github.com/prysmaticlabs/prysm/beacon-chain/state/state-altair"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stateV0"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
 	"github.com/prysmaticlabs/prysm/shared/params"
+	altair2 "github.com/prysmaticlabs/prysm/shared/testutil/altair"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
@@ -30,7 +32,7 @@ func TestProcessEpoch_CanProcess(t *testing.T) {
 	}
 	s, err := statealtair.InitializeFromProto(base)
 	require.NoError(t, err)
-	newState, err := ProcessEpoch(context.Background(), s)
+	newState, err := altair.ProcessEpoch(context.Background(), s)
 	require.NoError(t, err)
 	require.Equal(t, uint64(0), newState.Slashings()[2], "Unexpected slashed balance")
 }
@@ -42,7 +44,51 @@ func TestFuzzProcessEpoch_1000(t *testing.T) {
 	fuzzer.NilChance(0.1)
 	for i := 0; i < 1000; i++ {
 		fuzzer.Fuzz(state)
-		s, err := ProcessEpoch(ctx, state)
+		s, err := altair.ProcessEpoch(ctx, state)
+		if err != nil && s != nil {
+			t.Fatalf("state should be nil on err. found: %v on error: %v for state: %v", s, err, state)
+		}
+	}
+}
+
+func TestProcessSlots_CanProcess(t *testing.T) {
+	s, _ := altair2.DeterministicGenesisStateAltair(t, params.BeaconConfig().MaxValidatorsPerCommittee)
+	slot := types.Slot(100)
+	newState, err := altair.ProcessSlots(context.Background(), s, slot)
+	require.NoError(t, err)
+	require.Equal(t, slot, newState.Slot())
+}
+
+func TestProcessSlots_SameSlotAsParentState(t *testing.T) {
+	slot := types.Slot(2)
+	parentState, err := stateV0.InitializeFromProto(&pb.BeaconState{Slot: slot})
+	require.NoError(t, err)
+
+	_, err = altair.ProcessSlots(context.Background(), parentState, slot)
+	require.ErrorContains(t, "expected state.slot 2 < slot 2", err)
+}
+
+func TestProcessSlots_LowerSlotAsParentState(t *testing.T) {
+	slot := types.Slot(2)
+	parentState, err := stateV0.InitializeFromProto(&pb.BeaconState{Slot: slot})
+	require.NoError(t, err)
+
+	_, err = altair.ProcessSlots(context.Background(), parentState, slot-1)
+	require.ErrorContains(t, "expected state.slot 2 < slot 1", err)
+}
+
+func TestFuzzProcessSlots_1000(t *testing.T) {
+	altair.SkipSlotCache.Disable()
+	defer altair.SkipSlotCache.Enable()
+	ctx := context.Background()
+	state := &statealtair.BeaconState{}
+	slot := types.Slot(0)
+	fuzzer := fuzz.NewWithSeed(0)
+	fuzzer.NilChance(0.1)
+	for i := 0; i < 1000; i++ {
+		fuzzer.Fuzz(state)
+		fuzzer.Fuzz(&slot)
+		s, err := altair.ProcessSlots(ctx, state, slot)
 		if err != nil && s != nil {
 			t.Fatalf("state should be nil on err. found: %v on error: %v for state: %v", s, err, state)
 		}
