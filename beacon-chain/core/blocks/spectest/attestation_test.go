32c68702bb28fc3d1f97b63d30a5362019e83d4b
Victor Farazdagi <simple.square@gmail.com>
2021-04-20 17:59:09 +0300

    fix attestation_test

--- beacon-chain/core/blocks/spectest/attestation_test.go
+++ beacon-chain/core/blocks/spectest/attestation_test.go
@@ -4,6 +4,7 @@ import (
 	"path"
 	"testing"
 
+	"github.com/golang/snappy"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
 	"github.com/prysmaticlabs/prysm/shared/params/spectest"
@@ -14,14 +15,16 @@ import (
 func runAttestationTest(t *testing.T, config string) {
 	require.NoError(t, spectest.SetConfig(t, config))
 
-	testFolders, testsFolderPath := testutil.TestFolders(t, config, "operations/attestation/pyspec_tests")
+	testFolders, testsFolderPath := testutil.TestFolders(t, config, "phase0", "operations/attestation/pyspec_tests")
 	for _, folder := range testFolders {
 		t.Run(folder.Name(), func(t *testing.T) {
 			folderPath := path.Join(testsFolderPath, folder.Name())
-			attestationFile, err := testutil.BazelFileBytes(folderPath, "attestation.ssz")
+			attestationFile, err := testutil.BazelFileBytes(folderPath, "attestation.ssz_snappy")
 			require.NoError(t, err)
+			attestationSSZ, err := snappy.Decode(nil /* dst */, attestationFile)
+			require.NoError(t, err, "Failed to decompress")
 			att := &ethpb.Attestation{}
-			require.NoError(t, att.UnmarshalSSZ(attestationFile), "Failed to unmarshal")
+			require.NoError(t, att.UnmarshalSSZ(attestationSSZ), "Failed to unmarshal")
 
 			body := &ethpb.BeaconBlockBody{Attestations: []*ethpb.Attestation{att}}
 			testutil.RunBlockOperationTest(t, folderPath, body, blocks.ProcessAttestations)
