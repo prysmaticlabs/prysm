cc5df7e2a757d9a53537dc0a4e2cc09e773a8c2b
Victor Farazdagi <simple.square@gmail.com>
2021-04-22 03:39:10 +0300

    fix import

--- spectest/minimal/phase0/sanity/blocks_test.go
+++ spectest/minimal/phase0/sanity/blocks_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/params/spectest"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
-	sharedRunner "github.com/prysmaticlabs/prysm/spectest/shared/phase0/sanity"
+	"github.com/prysmaticlabs/prysm/spectest/shared/phase0/sanity"
 )
 
 func TestMinimal_Phase0_Sanity_Blocks(t *testing.T) {
 	config := "minimal"
 	require.NoError(t, spectest.SetConfig(t, config))
 	testFolders, testsFolderPath := testutil.TestFolders(t, config, "phase0", "sanity/blocks/pyspec_tests")
-	sharedRunner.RunBlockProcessingTest(t, testFolders, testsFolderPath)
+	sanity.RunBlockProcessingTest(t, testFolders, testsFolderPath)
 }
