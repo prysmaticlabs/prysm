d1dadd9350c3c7db88af018032e7fa8ba0c86a40
Victor Farazdagi <simple.square@gmail.com>
2021-04-22 03:51:11 +0300

    phase0/sanity/slots

--- spectest/shared/phase0/sanity/slot_processing.go
+++ spectest/shared/phase0/sanity/slot_processing.go
@@ -1,7 +1,8 @@
-package spectest
+package sanity
 
 import (
 	"context"
+	"os"
 	"strconv"
 	"testing"
 
@@ -10,7 +11,6 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stateV0"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
-	"github.com/prysmaticlabs/prysm/shared/params/spectest"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	"gopkg.in/d4l3k/messagediff.v1"
@@ -20,11 +20,7 @@ func init() {
 	state.SkipSlotCache.Disable()
 }
 
-func runSlotProcessingTests(t *testing.T, config string) {
-	require.NoError(t, spectest.SetConfig(t, config))
-
-	testFolders, testsFolderPath := testutil.TestFolders(t, config, "phase0", "sanity/slots/pyspec_tests")
-
+func RunSlotProcessingTests(t *testing.T, testFolders []os.FileInfo, testsFolderPath string) {
 	for _, folder := range testFolders {
 		t.Run(folder.Name(), func(t *testing.T) {
 			preBeaconStateFile, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), "pre.ssz_snappy")
