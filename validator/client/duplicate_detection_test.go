4485eee3b7e9584263de06d7f529732e1c77dd82
ahadda5 <ahadda5@gmail.com>
2021-05-11 14:01:47 +0400

    removed hardcoded variables

--- validator/client/duplicate_detection_test.go
+++ validator/client/duplicate_detection_test.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"context"
-	//"errors"
 	"testing"
 	"time"
 
@@ -14,16 +13,11 @@ import (
 	"github.com/prysmaticlabs/prysm/shared/timeutils"
 
 	slotutil "github.com/prysmaticlabs/prysm/shared/slotutil"
-	//"github.com/prysmaticlabs/prysm/validator/client/iface"
-	//slotutilmock "github.com/prysmaticlabs/prysm/shared/slotutil/testing"
 	dbTest "github.com/prysmaticlabs/prysm/validator/db/testing"
-
-	//"github.com/prysmaticlabs/prysm/shared/testutil/assert"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
-	//"github.com/prysmaticlabs/prysm/validator/client/testutil"
 )
 
-func TestSleeping_ForTwoEpochs(t *testing.T) {
+func TestSleeping_ForDuplicateDetectionEpochs(t *testing.T) {
 	ctx := context.Background()
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
@@ -36,8 +30,7 @@ func TestSleeping_ForTwoEpochs(t *testing.T) {
 	currentEpoch := helpers.SlotToEpoch(slot)
 	genesisTime := uint64(timeutils.Now().Unix()) - uint64(slot.Mul(params.BeaconConfig().SecondsPerSlot))
 
-	ticker := slotutil.NewSlotTickerWithOffset(time.Unix(int64(genesisTime), 0),
-		time.Duration(slot.Mul( params.BeaconConfig().SecondsPerSlot)) ,
+	ticker := slotutil.NewSlotTicker(time.Unix(int64(genesisTime), 0),
 		params.BeaconConfig().SecondsPerSlot)
 
 	defer ticker.Done()
@@ -51,6 +44,6 @@ func TestSleeping_ForTwoEpochs(t *testing.T) {
 	err := v.startDoppelgangerService(ctx)
 	oneEpochs := helpers.SlotToEpoch(<-v.NextSlot())
 	require.NoError(t, err)
-	require.Equal(t, currentEpoch.Add(1), oneEpochs, "Initial Epoch (%d) vs After 1 "+
-		"epochs (%d)", currentEpoch, oneEpochs)
+	require.Equal(t, currentEpoch.Add(uint64(params.BeaconConfig().DuplicateValidatorEpochsCheck)),
+		oneEpochs, "Initial Epoch (%d) vs After 1 epochs (%d)", currentEpoch, oneEpochs)
 }
