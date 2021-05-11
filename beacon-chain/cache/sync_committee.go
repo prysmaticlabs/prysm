3e3e3ccc04b82cf069d1bb9b242daca6490eceb4
terence tsao <terence@prysmaticlabs.com>
2021-05-11 15:41:30 -0700

    Better comments and tests

--- beacon-chain/cache/sync_committee.go
+++ beacon-chain/cache/sync_committee.go
@@ -4,46 +4,45 @@ import (
 	"errors"
 	"sync"
 
-	statealtair "github.com/prysmaticlabs/prysm/beacon-chain/state/state-altair"
+	iface "github.com/prysmaticlabs/prysm/beacon-chain/state/interface"
 	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"k8s.io/client-go/tools/cache"
 )
 
-var errNonExistingSyncCommitteeKey = errors.New("does not exist sync committee key")
-var errNotSyncCommitteeIndexPosition = errors.New("obj is not syncCommitteeIndexPosition struct")
-var maxSyncCommitteeSize = uint64(3)
+var ErrNonExistingSyncCommitteeKey = errors.New("does not exist sync committee key")
+var errNotSyncCommitteeIndexPosition = errors.New("not syncCommitteeIndexPosition struct")
+var maxSyncCommitteeSize = uint64(3) // Allows 3 forks to happen around `EPOCHS_PER_SYNC_COMMITTEE_PERIOD` boundary.
 
+// SyncCommitteeCache utilizes a FIFO cache to sufficiently cache validator position within sync committee.
+// It is thread safe with concurrent read write.
 type SyncCommitteeCache struct {
 	cache *cache.FIFO
 	lock sync.RWMutex
 }
 
+// Index position of all validators in sync committee where `currentSyncCommitteeRoot` is the key and `vIndexToPositionMap` is value.
+// Inside `vIndexToPositionMap`, validator positions are cached where key is the 48byte public key and the value is the `positionInCommittee` struct.
 type syncCommitteeIndexPosition struct {
 	currentSyncCommitteeRoot [32]byte
 	vIndexToPositionMap map[[48]byte]*positionInCommittee
 }
 
+// Index position of individual validator of current epoch and previous epoch sync committee.
 type positionInCommittee struct {
 	currentEpoch []uint64
 	nextEpoch []uint64
 }
 
+// NewSyncCommittee initializes and returns a new SyncCommitteeCache.
 func NewSyncCommittee() *SyncCommitteeCache {
 	return &SyncCommitteeCache{
 		cache: cache.NewFIFO(keyFn),
 	}
 }
 
-func keyFn(obj interface{}) (string, error) {
-	info, ok := obj.(*syncCommitteeIndexPosition)
-	if !ok {
-		return "", errNotSyncCommitteeIndexPosition
-	}
-
-	return string(info.currentSyncCommitteeRoot[:]), nil
-}
-
-
+// CurrentEpochIndexPosition returns current epoch index position of validator pubKey with respect with sync committee.
+// If the input pubKey has no assignment, an empty list will be returned.
+// If the input root does not exist in cache, ErrNonExistingSyncCommitteeKey is returned. Then performing manual checking of state for index position in state is recommended.
 func (s *SyncCommitteeCache) CurrentEpochIndexPosition(root [32]byte, pubKey [48]byte) ([]uint64, error) {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
@@ -55,9 +54,13 @@ func (s *SyncCommitteeCache) CurrentEpochIndexPosition(root [32]byte, pubKey [48
 	if pos == nil {
 		return []uint64{}, nil
 	}
+
 	return pos.currentEpoch, nil
 }
 
+// NextEpochIndexPosition returns next epoch index position of validator pubKey in respect with sync committee.
+// If the input pubKey has no assignment, an empty list will be returned.
+// If the input root does not exist in cache, ErrNonExistingSyncCommitteeKey is returned. Then performing manual checking of state for index position in state is recommended.
 func (s *SyncCommitteeCache) NextEpochIndexPosition(root [32]byte, pubKey [48]byte) ([]uint64, error) {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
@@ -72,13 +75,15 @@ func (s *SyncCommitteeCache) NextEpochIndexPosition(root [32]byte, pubKey [48]by
 	return pos.nextEpoch, nil
 }
 
+// Helper function for `CurrentEpochIndexPosition` and `NextEpochIndexPosition` to return a mapping
+// of validator pubKey to its index(s) position.
 func (s *SyncCommitteeCache) idxPositionInCommittee(root [32]byte, pubKey [48]byte) (*positionInCommittee, error) {
 	obj, exists, err := s.cache.GetByKey(key(root))
 	if err != nil {
 		return nil, err
 	}
 	if !exists {
-		return nil, errNonExistingSyncCommitteeKey
+		return nil, ErrNonExistingSyncCommitteeKey
 	}
 	item, ok := obj.(*syncCommitteeIndexPosition)
 	if !ok {
@@ -91,7 +96,10 @@ func (s *SyncCommitteeCache) idxPositionInCommittee(root [32]byte, pubKey [48]by
 	return idxInCommittee, nil
 }
 
-func (s *SyncCommitteeCache) UpdatePositionsInCommittee(state *statealtair.BeaconState) error {
+// UpdatePositionsInCommittee updates caching of validators position in sync committee in respect to
+// current epoch and next epoch. This should be called when `current_sync_committee` and `next_sync_committee`
+// change and that happens every `EPOCHS_PER_SYNC_COMMITTEE_PERIOD`.
+func (s *SyncCommitteeCache) UpdatePositionsInCommittee(state iface.BeaconStateAltair) error {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
@@ -136,4 +144,16 @@ func (s *SyncCommitteeCache) UpdatePositionsInCommittee(state *statealtair.Beaco
 	trim(s.cache, maxSyncCommitteeSize)
 
 	return nil
+}
+
+// Given the `syncCommitteeIndexPosition` object, this returns the key of the object.
+// The key is the `currentSyncCommitteeRoot` within the field.
+// Error gets returned if input does not comply with `currentSyncCommitteeRoot` object.
+func keyFn(obj interface{}) (string, error) {
+	info, ok := obj.(*syncCommitteeIndexPosition)
+	if !ok {
+		return "", errNotSyncCommitteeIndexPosition
+	}
+
+	return string(info.currentSyncCommitteeRoot[:]), nil
 }
---
+++ beacon-chain/cache/sync_committee_test.go
@@ -0,0 +1,183 @@
+package cache_test
+
+import (
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
+	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
+	"github.com/prysmaticlabs/prysm/shared/testutil/altair"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
+)
+
+func TestSyncCommitteeCache_CanUpdateAndRetrieve(t *testing.T) {
+	tests := []struct {
+		name    string
+		currentSyncCommittee *pb.SyncCommittee
+		nextSyncCommittee *pb.SyncCommittee
+		currentSyncMap map[[48]byte][]uint64
+		nextSyncMap map[[48]byte][]uint64
+	}{
+		{
+			name:                 "only current epoch",
+			currentSyncCommittee: convertToCommittee([][]byte{{1},{2},{3},{2},{2}}),
+			nextSyncCommittee: convertToCommittee([][]byte{}),
+			currentSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {0},
+				[48]byte{2}: {1, 3, 4},
+				[48]byte{3}: {2},
+			},
+			nextSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {},
+				[48]byte{2}: {},
+				[48]byte{3}: {},
+			},
+		},
+		{
+			name:                 "only next epoch",
+			currentSyncCommittee: convertToCommittee([][]byte{}),
+			nextSyncCommittee:    convertToCommittee([][]byte{{1},{2},{3},{2},{2}}),
+			currentSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {},
+				[48]byte{2}: {},
+				[48]byte{3}: {},
+			},
+			nextSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {0},
+				[48]byte{2}: {1, 3, 4},
+				[48]byte{3}: {2},
+			},
+		},
+		{
+			name:                 "some current epoch and some next epoch",
+			currentSyncCommittee: convertToCommittee([][]byte{{1},{2},{3},{2},{2}}),
+			nextSyncCommittee:    convertToCommittee([][]byte{{7},{6},{5},{4},{7}}),
+			currentSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {0},
+				[48]byte{2}: {1, 3, 4},
+				[48]byte{3}: {2},
+			},
+			nextSyncMap: map[[48]byte][]uint64{
+				[48]byte{7}: {0,4},
+				[48]byte{6}: {1},
+				[48]byte{5}: {2},
+				[48]byte{4}: {3},
+			},
+		},
+		{
+			name:                 "some current epoch and some next epoch duplicated across",
+			currentSyncCommittee: convertToCommittee([][]byte{{1},{2},{3},{2},{2}}),
+			nextSyncCommittee:    convertToCommittee([][]byte{{2},{1},{3},{2},{1}}),
+			currentSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {0},
+				[48]byte{2}: {1, 3, 4},
+				[48]byte{3}: {2},
+			},
+			nextSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {1,4},
+				[48]byte{2}: {0,3},
+				[48]byte{3}: {2},
+			},
+		},
+		{
+			name:                 "all duplicated",
+			currentSyncCommittee: convertToCommittee([][]byte{{100},{100},{100},{100}}),
+			nextSyncCommittee:    convertToCommittee([][]byte{{100},{100},{100},{100}}),
+			currentSyncMap: map[[48]byte][]uint64{
+				[48]byte{100}: {0,1,2,3},
+			},
+			nextSyncMap: map[[48]byte][]uint64{
+				[48]byte{100}: {0,1,2,3},
+			},
+		},
+		{
+			name:                 "unknown keys",
+			currentSyncCommittee: convertToCommittee([][]byte{{100},{100},{100},{100}}),
+			nextSyncCommittee:    convertToCommittee([][]byte{{100},{100},{100},{100}}),
+			currentSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {},
+			},
+			nextSyncMap: map[[48]byte][]uint64{
+				[48]byte{1}: {},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, _ := altair.DeterministicGenesisStateAltair(t, 64)
+			require.NoError(t, s.SetCurrentSyncCommittee(tt.currentSyncCommittee))
+			require.NoError(t, s.SetNextSyncCommittee(tt.nextSyncCommittee))
+			cache := cache.NewSyncCommittee()
+			require.NoError(t, cache.UpdatePositionsInCommittee(s))
+			csc, err := s.CurrentSyncCommittee()
+			require.NoError(t, err)
+			r, err := csc.HashTreeRoot()
+			require.NoError(t, err)
+			for key, indices := range tt.currentSyncMap {
+				pos, err := cache.CurrentEpochIndexPosition(r, key)
+				require.NoError(t, err)
+				require.DeepEqual(t, indices, pos)
+			}
+			for key, indices := range tt.nextSyncMap {
+				pos, err := cache.NextEpochIndexPosition(r, key)
+				require.NoError(t, err)
+				require.DeepEqual(t, indices, pos)
+			}
+		})
+	}
+}
+
+func TestSyncCommitteeCache_RootDoesNotExist(t *testing.T) {
+	c := cache.NewSyncCommittee()
+	_, err := c.CurrentEpochIndexPosition([32]byte{}, [48]byte{})
+	require.Equal(t, cache.ErrNonExistingSyncCommitteeKey, err)
+}
+
+func TestSyncCommitteeCache_CanRotate(t *testing.T) {
+	c := cache.NewSyncCommittee()
+	s, _ := altair.DeterministicGenesisStateAltair(t, 64)
+	require.NoError(t, s.SetCurrentSyncCommittee(convertToCommittee([][]byte{{1}})))
+	require.NoError(t, c.UpdatePositionsInCommittee(s))
+
+	csc, err := s.CurrentSyncCommittee()
+	require.NoError(t, err)
+	r, err := csc.HashTreeRoot()
+	require.NoError(t, err)
+
+	require.NoError(t, s.SetCurrentSyncCommittee(convertToCommittee([][]byte{{2}})))
+	require.NoError(t, c.UpdatePositionsInCommittee(s))
+	require.NoError(t, s.SetCurrentSyncCommittee(convertToCommittee([][]byte{{3}})))
+	require.NoError(t, c.UpdatePositionsInCommittee(s))
+	require.NoError(t, s.SetCurrentSyncCommittee(convertToCommittee([][]byte{{4}})))
+	require.NoError(t, c.UpdatePositionsInCommittee(s))
+
+	_, err = c.CurrentEpochIndexPosition(r, [48]byte{})
+	require.Equal(t, cache.ErrNonExistingSyncCommitteeKey, err)
+
+	csc, err = s.CurrentSyncCommittee()
+	require.NoError(t, err)
+	r, err = csc.HashTreeRoot()
+	require.NoError(t, err)
+	_, err = c.CurrentEpochIndexPosition(r, [48]byte{})
+	require.NoError(t, err)
+}
+
+func convertToCommittee(inputKeys [][]byte) *pb.SyncCommittee {
+	var pubKeys [][]byte
+	for i := uint64(0); i < params.BeaconConfig().SyncCommitteeSize; i++ {
+		if i < uint64(len(inputKeys)) {
+			pubKeys = append(pubKeys, bytesutil.PadTo(inputKeys[i], params.BeaconConfig().BLSPubkeyLength))
+		} else {
+			pubKeys = append(pubKeys, bytesutil.PadTo([]byte{}, params.BeaconConfig().BLSPubkeyLength))
+		}
+	}
+	var aggregatedKeys [][]byte
+	for i := uint64(0); i < (params.BeaconConfig().SyncCommitteeSize / params.BeaconConfig().SyncPubkeysPerAggregate); i++ {
+		aggregatedKeys = append(aggregatedKeys, bytesutil.PadTo([]byte{}, params.BeaconConfig().BLSPubkeyLength))
+	}
+	return &pb.SyncCommittee{
+		Pubkeys: pubKeys,
+		PubkeyAggregates: aggregatedKeys,
+	}
+}
