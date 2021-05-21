b0ef9cbb0dd90e919950d52124f904fa9ac8f817
terence tsao <terence@prysmaticlabs.com>
2021-05-21 12:18:06 -0700

    Trigger upgrade to altair

--- beacon-chain/core/state/transition.go
+++ beacon-chain/core/state/transition.go
@@ -12,6 +12,7 @@ import (
 	types "github.com/prysmaticlabs/eth2-types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/cache"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/altair"
 	b "github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
 	e "github.com/prysmaticlabs/prysm/beacon-chain/core/epoch"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/epoch/precompute"
@@ -281,6 +282,12 @@ func ProcessSlots(ctx context.Context, state iface.BeaconState, slot types.Slot)
 		if err := state.SetSlot(state.Slot() + 1); err != nil {
 			traceutil.AnnotateError(span, err)
 			return nil, errors.Wrap(err, "failed to increment state slot")
+		}
+		if helpers.IsEpochStart(state.Slot()) && helpers.SlotToEpoch(state.Slot()) == params.BeaconConfig().AltairForkEpoch {
+			state, err = altair.UpgradeToAltair(state)
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 
