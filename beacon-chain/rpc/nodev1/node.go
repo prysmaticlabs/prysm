f615b55081a038d65db5a984fab4ea93f941788d
rkapka <rkapka@wp.pl>
2021-05-25 17:14:24 +0200

    comment on return value

--- beacon-chain/rpc/nodev1/node.go
+++ beacon-chain/rpc/nodev1/node.go
@@ -293,6 +293,7 @@ func (ns *Server) GetHealth(ctx context.Context, _ *emptypb.Empty) (*emptypb.Emp
 	}
 	if ns.SyncChecker.Syncing() || ns.SyncChecker.Initialized() {
 		if err := grpc.SetHeader(ctx, metadata.Pairs(grpcutils.HttpCodeMetadataKey, strconv.Itoa(http.StatusPartialContent))); err != nil {
+			// We return a positive result because failing to set a non-gRPC related header should not cause the gRPC call to fail.
 			return &emptypb.Empty{}, nil
 		}
 		return &emptypb.Empty{}, nil
