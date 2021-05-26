213db9ae5668a52ac91be0b056580494e75a6728
Raul Jordan <raul@prysmaticlabs.com>
2021-05-26 16:33:48 -0500

    logs

--- tools/gitmirror/mirror.go
+++ tools/gitmirror/mirror.go
@@ -47,33 +47,45 @@ func (g *gitCLI) CopyDir(sourceRepo, targetRepo, dir string) error {
 // - Git add all, git commit, and git push to master branch of the mirror repo
 func mirrorChanges(config *Config, manager GitManager, payload ReleasePayload) error {
 	repoName := payload.Repository.Name
+	log.Infof("Fetching changes from %s", repoName)
 	if err := manager.Fetch(repoName); err != nil {
 		return err
 	}
+	log.Infof("Checking out branch %s in %s", payload.Release.TagName, repoName)
 	if err := manager.Checkout(repoName, payload.Release.TagName); err != nil {
 		return err
 	}
 	var repo ConfigRepo
+	var found bool
 	for _, repository := range config.Repositories {
 		if repository.RemoteName == repoName {
 			repo = repository
+			found = true
 		}
 	}
+	if !found {
+		return fmt.Errorf("could not find repo %s from release event in config", repoName)
+	}
 	for _, dir := range repo.MirrorDirectories {
+		log.Infof("Fetching latest changes from mirror repo %s", repo.MirrorName)
 		if err := manager.Fetch(repo.MirrorName); err != nil {
 			return err
 		}
+		log.Infof("Copying directory %s from source %s to mirror repo %s", dir, repoName, repo.MirrorName)
 		if err := manager.CopyDir(repoName, repo.MirrorName, dir); err != nil {
 			return err
 		}
 	}
+	log.Infof("Staging all changes in mirror %s", repo.MirrorName)
 	if err := manager.Add(repo.MirrorName); err != nil {
 		return err
 	}
 	commitMsg := payload.Release.Name
+	log.Infof("Committing changes in mirror %s with message `%s`", repo.MirrorName, commitMsg)
 	if err := manager.Commit(repo.MirrorName, commitMsg); err != nil {
 		return err
 	}
+	log.Infof("Pushing changes to mirror %s", repo.MirrorName)
 	if err := manager.Push(repo.MirrorName); err != nil {
 		return err
 	}
