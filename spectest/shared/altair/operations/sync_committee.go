95f62de465918ab7a2f815a5bedf4d940f31b04e
terence tsao <terence@prysmaticlabs.com>
2021-06-01 12:32:12 -0700

    Add sync committee test (#8967)

---
+++ spectest/shared/altair/operations/sync_committee.go
@@ -0,0 +1,37 @@
+package operations
+
+import (
+	"context"
+	"path"
+	"testing"
+
+	"github.com/golang/snappy"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/altair"
+	iface "github.com/prysmaticlabs/prysm/beacon-chain/state/interface"
+	"github.com/prysmaticlabs/prysm/shared/interfaces"
+	"github.com/prysmaticlabs/prysm/shared/testutil"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
+	"github.com/prysmaticlabs/prysm/spectest/utils"
+)
+
+func RunSyncCommitteeTest(t *testing.T, config string) {
+	require.NoError(t, utils.SetConfig(t, config))
+	testFolders, testsFolderPath := utils.TestFolders(t, config, "altair", "operations/sync_committee/pyspec_tests")
+	for _, folder := range testFolders {
+		t.Run(folder.Name(), func(t *testing.T) {
+			folderPath := path.Join(testsFolderPath, folder.Name())
+			syncCommitteeFile, err := testutil.BazelFileBytes(folderPath, "sync_aggregate.ssz_snappy")
+			require.NoError(t, err)
+			syncCommitteeSSZ, err := snappy.Decode(nil /* dst */, syncCommitteeFile)
+			require.NoError(t, err, "Failed to decompress")
+			sc := &ethpb.SyncAggregate{}
+			require.NoError(t, sc.UnmarshalSSZ(syncCommitteeSSZ), "Failed to unmarshal")
+
+			body := &ethpb.BeaconBlockBodyAltair{SyncAggregate: sc}
+			RunBlockOperationTest(t, folderPath, body, func(ctx context.Context, s iface.BeaconState, b interfaces.SignedBeaconBlock) (iface.BeaconState, error) {
+				return altair.ProcessSyncCommittee(s, body.SyncAggregate)
+			})
+		})
+	}
+}
