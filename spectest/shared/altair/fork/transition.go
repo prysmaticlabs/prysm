44d6bf66728762104e38fa331c5e361499b03bed
terence tsao <terence@prysmaticlabs.com>
2021-06-02 10:27:43 -0700

    Complete spec test

--- spectest/shared/altair/fork/transition.go
+++ spectest/shared/altair/fork/transition.go
@@ -1,14 +1,21 @@
 package fork
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/golang/snappy"
+	types "github.com/prysmaticlabs/eth2-types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
+	iface "github.com/prysmaticlabs/prysm/beacon-chain/state/interface"
+	stateAltair "github.com/prysmaticlabs/prysm/beacon-chain/state/state-altair"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stateV0"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	"github.com/prysmaticlabs/prysm/shared/interfaces"
+	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 	"github.com/prysmaticlabs/prysm/spectest/utils"
@@ -29,38 +36,49 @@ func RunForkTransitionTest(t *testing.T, config string) {
 	for _, folder := range testFolders {
 		t.Run(folder.Name(), func(t *testing.T) {
 			helpers.ClearCache()
-
 			file, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), "meta.yaml")
 			require.NoError(t, err)
-
-			metaYaml := &ForkConfig{}
-			require.NoError(t, utils.UnmarshalYaml(file, metaYaml), "Failed to Unmarshal")
+			config := &ForkConfig{}
+			require.NoError(t, utils.UnmarshalYaml(file, config), "Failed to Unmarshal")
 
 			preforkBlocks := make([]*ethpb.SignedBeaconBlock, 0)
 			postforkBlocks := make([]*ethpb.SignedBeaconBlockAltair, 0)
-			for i := 0; i <= metaYaml.ForkBlock; i++ {
-				fileName := fmt.Sprint("blocks_", i, ".ssz_snappy")
-				blockFile, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), fileName)
-				require.NoError(t, err)
-				blockSSZ, err := snappy.Decode(nil /* dst */, blockFile)
-				require.NoError(t, err, "Failed to decompress")
-				block := &ethpb.SignedBeaconBlock{}
-				require.NoError(t, block.UnmarshalSSZ(blockSSZ), "Failed to unmarshal")
-				preforkBlocks = append(preforkBlocks, block)
-			}
-			t.Error(preforkBlocks[0].Block.StateRoot)
-			for i := metaYaml.ForkBlock + 1; i < metaYaml.BlocksCount; i++ {
-				fileName := fmt.Sprint("blocks_", i, ".ssz_snappy")
-				blockFile, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), fileName)
-				require.NoError(t, err)
-				blockSSZ, err := snappy.Decode(nil /* dst */, blockFile)
-				require.NoError(t, err, "Failed to decompress")
-				block := &ethpb.SignedBeaconBlockAltair{}
-				require.NoError(t, block.UnmarshalSSZ(blockSSZ), "Failed to unmarshal")
-				postforkBlocks = append(postforkBlocks, block)
+			// Fork happens without any pre-fork blocks.
+			if config.ForkBlock == 0 {
+				for i := 0; i < config.BlocksCount; i++ {
+					fileName := fmt.Sprint("blocks_", i, ".ssz_snappy")
+					blockFile, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), fileName)
+					require.NoError(t, err)
+					blockSSZ, err := snappy.Decode(nil /* dst */, blockFile)
+					require.NoError(t, err, "Failed to decompress")
+					block := &ethpb.SignedBeaconBlockAltair{}
+					require.NoError(t, block.UnmarshalSSZ(blockSSZ), "Failed to unmarshal")
+					postforkBlocks = append(postforkBlocks, block)
+				}
+				// Fork happens with pre-fork blocks.
+			} else {
+				for i := 0; i <= config.ForkBlock; i++ {
+					fileName := fmt.Sprint("blocks_", i, ".ssz_snappy")
+					blockFile, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), fileName)
+					require.NoError(t, err)
+					blockSSZ, err := snappy.Decode(nil /* dst */, blockFile)
+					require.NoError(t, err, "Failed to decompress")
+					block := &ethpb.SignedBeaconBlock{}
+					require.NoError(t, block.UnmarshalSSZ(blockSSZ), "Failed to unmarshal")
+					preforkBlocks = append(preforkBlocks, block)
+				}
+				for i := config.ForkBlock + 1; i < config.BlocksCount; i++ {
+					fileName := fmt.Sprint("blocks_", i, ".ssz_snappy")
+					blockFile, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), fileName)
+					require.NoError(t, err)
+					blockSSZ, err := snappy.Decode(nil /* dst */, blockFile)
+					require.NoError(t, err, "Failed to decompress")
+					block := &ethpb.SignedBeaconBlockAltair{}
+					require.NoError(t, block.UnmarshalSSZ(blockSSZ), "Failed to unmarshal")
+					postforkBlocks = append(postforkBlocks, block)
+				}
 			}
 
-			helpers.ClearCache()
 			preBeaconStateFile, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), "pre.ssz_snappy")
 			require.NoError(t, err)
 			preBeaconStateSSZ, err := snappy.Decode(nil /* dst */, preBeaconStateFile)
@@ -69,7 +87,37 @@ func RunForkTransitionTest(t *testing.T, config string) {
 			require.NoError(t, beaconStateBase.UnmarshalSSZ(preBeaconStateSSZ), "Failed to unmarshal")
 			beaconState, err := stateV0.InitializeFromProto(beaconStateBase)
 			require.NoError(t, err)
-			t.Error(beaconState.Slot())
+
+			bc := params.BeaconConfig()
+			bc.AltairForkEpoch = types.Epoch(config.ForkEpoch)
+			params.OverrideBeaconConfig(bc)
+
+			ctx := context.Background()
+			var ok bool
+			for _, b := range preforkBlocks {
+				state, err := state.ExecuteStateTransition(ctx, beaconState, interfaces.WrappedPhase0SignedBeaconBlock(b))
+				require.NoError(t, err)
+				beaconState, ok = state.(*stateV0.BeaconState)
+				require.Equal(t, true, ok)
+			}
+			altairState := iface.BeaconStateAltair(beaconState)
+			for _, b := range postforkBlocks {
+				state, err := state.ExecuteStateTransition(ctx, altairState, interfaces.WrappedAltairSignedBeaconBlock(b))
+				require.NoError(t, err)
+				altairState, ok = state.(*stateAltair.BeaconState)
+				require.Equal(t, true, ok)
+			}
+
+			postBeaconStateFile, err := testutil.BazelFileBytes(testsFolderPath, folder.Name(), "post.ssz_snappy")
+			require.NoError(t, err)
+			postBeaconStateSSZ, err := snappy.Decode(nil /* dst */, postBeaconStateFile)
+			require.NoError(t, err, "Failed to decompress")
+			postBeaconState := &pb.BeaconStateAltair{}
+			require.NoError(t, postBeaconState.UnmarshalSSZ(postBeaconStateSSZ), "Failed to unmarshal")
+
+			pbState, err := stateAltair.ProtobufBeaconState(altairState.CloneInnerState())
+			require.NoError(t, err)
+			require.DeepSSZEqual(t, pbState, postBeaconState)
 		})
 	}
 }
