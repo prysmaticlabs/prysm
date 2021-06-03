657cdce5aaaa3ada15b57b06bc0fa08c1299805c
nisdas <nishdas93@gmail.com>
2021-06-03 21:34:30 +0800

    add initializer

--- beacon-chain/p2p/types/object_mapping.go
+++ beacon-chain/p2p/types/object_mapping.go
@@ -9,35 +9,53 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+func init() {
+	// Initialize data maps.
+	InitializeDataMaps()
+}
+
 // This file provides a mapping of fork version to the respective data type. This is
 // to allow any service to appropriately use the correct data type with a provided
 // fork-version.
 
-// BlockMap maps the fork-version to the underlying data type for that
-// particular fork period.
-var BlockMap = map[[4]byte]func() interfaces.SignedBeaconBlock{
-	bytesutil.ToBytes4(params.BeaconConfig().GenesisForkVersion): func() interfaces.SignedBeaconBlock {
-		return interfaces.WrappedPhase0SignedBeaconBlock(&ethpb.SignedBeaconBlock{})
-	},
-	bytesutil.ToBytes4(params.BeaconConfig().AltairForkVersion): func() interfaces.SignedBeaconBlock {
-		return interfaces.WrappedAltairSignedBeaconBlock(&ethpb.SignedBeaconBlockAltair{})
-	},
-}
+var (
+	// BlockMap maps the fork-version to the underlying data type for that
+	// particular fork period.
+	BlockMap map[[4]byte]func() interfaces.SignedBeaconBlock
+	// StateMap maps the fork-version to the underlying data type for that
+	// particular fork period.
+	StateMap map[[4]byte]proto.Message
+	// MetaDataMap maps the fork-version to the underlying data type for that
+	// particular fork period.
+	MetaDataMap map[[4]byte]func() interfaces.Metadata
+)
 
-// StateMap maps the fork-version to the underlying data type for that
-// particular fork period.
-var StateMap = map[[4]byte]proto.Message{
-	bytesutil.ToBytes4(params.BeaconConfig().GenesisForkVersion): &pbp2p.BeaconState{},
-	bytesutil.ToBytes4(params.BeaconConfig().AltairForkVersion):  &pbp2p.BeaconStateAltair{},
-}
+// InitializeDataMaps initializes all the relevant object maps. This function is called to
+// reset maps and reinitialize them.
+func InitializeDataMaps() {
+	// Reset our block map.
+	BlockMap = map[[4]byte]func() interfaces.SignedBeaconBlock{
+		bytesutil.ToBytes4(params.BeaconConfig().GenesisForkVersion): func() interfaces.SignedBeaconBlock {
+			return interfaces.WrappedPhase0SignedBeaconBlock(&ethpb.SignedBeaconBlock{})
+		},
+		bytesutil.ToBytes4(params.BeaconConfig().AltairForkVersion): func() interfaces.SignedBeaconBlock {
+			return interfaces.WrappedAltairSignedBeaconBlock(&ethpb.SignedBeaconBlockAltair{})
+		},
+	}
 
-// MetaDataMap maps the fork-version to the underlying data type for that
-// particular fork period.
-var MetaDataMap = map[[4]byte]func() interfaces.Metadata{
-	bytesutil.ToBytes4(params.BeaconConfig().GenesisForkVersion): func() interfaces.Metadata {
-		return interfaces.WrappedMetadataV0(&pbp2p.MetaDataV0{})
-	},
-	bytesutil.ToBytes4(params.BeaconConfig().AltairForkVersion): func() interfaces.Metadata {
-		return interfaces.WrappedMetadataV1(&pbp2p.MetaDataV1{})
-	},
+	// Reset our state map.
+	StateMap = map[[4]byte]proto.Message{
+		bytesutil.ToBytes4(params.BeaconConfig().GenesisForkVersion): &pbp2p.BeaconState{},
+		bytesutil.ToBytes4(params.BeaconConfig().AltairForkVersion):  &pbp2p.BeaconStateAltair{},
+	}
+
+	// Reset our metadata map.
+	MetaDataMap = map[[4]byte]func() interfaces.Metadata{
+		bytesutil.ToBytes4(params.BeaconConfig().GenesisForkVersion): func() interfaces.Metadata {
+			return interfaces.WrappedMetadataV0(&pbp2p.MetaDataV0{})
+		},
+		bytesutil.ToBytes4(params.BeaconConfig().AltairForkVersion): func() interfaces.Metadata {
+			return interfaces.WrappedMetadataV1(&pbp2p.MetaDataV1{})
+		},
+	}
 }
