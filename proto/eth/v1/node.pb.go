aa4c21d6df5241532c6e4b99c269fa32bdba0dbc
terence tsao <terence@prysmaticlabs.com>
2021-06-05 22:39:16 -0700

    Run go imports (#8995)

--- proto/eth/v1/node.pb.go
+++ proto/eth/v1/node.pb.go
@@ -8,6 +8,9 @@ package v1
 
 import (
 	context "context"
+	reflect "reflect"
+	sync "sync"
+
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/golang/protobuf/protoc-gen-go/descriptor"
 	empty "github.com/golang/protobuf/ptypes/empty"
@@ -20,8 +23,6 @@ import (
 	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
