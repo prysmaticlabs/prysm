40aafc836e637f57bacb754a2612cc0c220c9e07
terence tsao <terence@prysmaticlabs.com>
2021-06-09 17:04:22 -0700

    Update bitvector

--- shared/testutil/block.go
+++ shared/testutil/block.go
@@ -82,7 +82,7 @@ func NewBeaconBlockAltair() *ethpb.SignedBeaconBlockAltair {
 				ProposerSlashings: []*ethpb.ProposerSlashing{},
 				VoluntaryExits:    []*ethpb.SignedVoluntaryExit{},
 				SyncAggregate: &ethpb.SyncAggregate{
-					SyncCommitteeBits:      make([]byte, len(bitfield.NewBitvector512())),
+					SyncCommitteeBits:      bitfield.NewBitvector512(),
 					SyncCommitteeSignature: make([]byte, 96),
 				},
 			},
