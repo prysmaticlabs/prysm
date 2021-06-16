f1b291d82ed49817139c55aca7adedbf42615c77
terence tsao <terence@prysmaticlabs.com>
2021-06-15 17:52:19 -0700

    Tests

---
+++ beacon-chain/rpc/validator/sync_committee_test.go
@@ -0,0 +1,39 @@
+package validator
+
+import (
+	"context"
+	"testing"
+
+	mock "github.com/prysmaticlabs/prysm/beacon-chain/blockchain/testing"
+	"github.com/prysmaticlabs/prysm/beacon-chain/operations/synccommittee"
+	mockp2p "github.com/prysmaticlabs/prysm/beacon-chain/p2p/testing"
+	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/testutil/require"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestGetSyncMessageBlockRoot_OK(t *testing.T) {
+	r := []byte{'a'}
+	server := &Server{
+		HeadFetcher: &mock.ChainService{Root: r},
+	}
+	res, err := server.GetSyncMessageBlockRoot(context.Background(), &emptypb.Empty{})
+	require.NoError(t, err)
+	require.DeepEqual(t, r, res.Root)
+}
+
+func TestSubmitSyncMessage_OK(t *testing.T) {
+	server := &Server{
+		SyncCommitteePool: synccommittee.NewStore(),
+		P2P:               &mockp2p.MockBroadcaster{},
+	}
+	msg := &ethpb.SyncCommitteeMessage{
+		Slot:           1,
+		ValidatorIndex: 2,
+	}
+	_, err := server.SubmitSyncMessage(context.Background(), msg)
+	require.NoError(t, err)
+	savedMsgs, err := server.SyncCommitteePool.SyncCommitteeMessages(1)
+	require.NoError(t, err)
+	require.DeepEqual(t, []*ethpb.SyncCommitteeMessage{msg}, savedMsgs)
+}
