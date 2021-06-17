d238eac6f402bc28958b08d330ccd65aecff8b31
terence tsao <terence@prysmaticlabs.com>
2021-06-17 11:27:33 -0700

    Conflict from earlier

--- beacon-chain/rpc/validator/sync_committee.go
+++ beacon-chain/rpc/validator/sync_committee.go
@@ -7,6 +7,7 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state"
 	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -68,4 +69,31 @@ func (vs *Server) SubmitSyncMessage(ctx context.Context, msg *ethpb.SyncCommitte
 	// Wait for p2p broadcast to complete and return the first error (if any)
 	err := errs.Wait()
 	return nil, err
+}
+
+// GetSyncSubcommitteeIndex is called by a sync committee participant to get its subcommittee index for aggregation duty.
+func (vs *Server) GetSyncSubcommitteeIndex(ctx context.Context, req *ethpb.SyncSubcommitteeIndexRequest) (*ethpb.SyncSubcommitteeIndexRespond, error) {
+	headState, err := vs.HeadFetcher.HeadState(ctx)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not get head state: %v", err)
+	}
+	if req.Slot > headState.Slot() {
+		headState, err = state.ProcessSlots(ctx, headState, req.Slot)
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "Could not process slots: %v", err)
+		}
+	}
+
+	committee, err := headState.CurrentSyncCommittee()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not get sync committee in head state: %v", err)
+	}
+	indices, err := helpers.CurrentEpochSyncSubcommitteeIndices(committee, bytesutil.ToBytes48(req.PublicKey))
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not get sync subcommittee indices: %v", err)
+	}
+
+	return &ethpb.SyncSubcommitteeIndexRespond{
+		Indices: indices,
+	}, nil
 }
--- validator/client/sync_committee.go
+++ validator/client/sync_committee.go
@@ -7,8 +7,10 @@ import (
 	types "github.com/prysmaticlabs/eth2-types"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	pb "github.com/prysmaticlabs/prysm/proto/beacon/p2p/v1"
+	ethpb "github.com/prysmaticlabs/prysm/proto/eth/v1alpha1"
 	validatorpb "github.com/prysmaticlabs/prysm/proto/validator/accounts/v2"
 	"github.com/prysmaticlabs/prysm/shared/params"
+	"github.com/prysmaticlabs/prysm/shared/traceutil"
 	"go.opencensus.io/trace"
 )
 
