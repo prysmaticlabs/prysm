afa8a118f5d6abce80bde2791c42dfff39869d46
Raul Jordan <raul@prysmaticlabs.com>
2021-06-18 17:42:51 -0500

    add in generated files

--- proto/eth/v1/events_service.pb.go
+++ proto/eth/v1/events_service.pb.go
@@ -8,9 +8,6 @@ package v1
 
 import (
 	context "context"
-	reflect "reflect"
-	sync "sync"
-
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/golang/protobuf/protoc-gen-go/descriptor"
 	gateway "github.com/grpc-ecosystem/grpc-gateway/v2/proto/gateway"
@@ -22,6 +19,8 @@ import (
 	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
