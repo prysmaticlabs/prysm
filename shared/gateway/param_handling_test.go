e63db6091b79ef1af47c6820c38366accc93871a
rkapka <rkapka@wp.pl>
2021-06-25 17:42:45 +0200

    Handle comma-separated query parameters

--- shared/gateway/param_handling_test.go
+++ shared/gateway/param_handling_test.go
@@ -109,3 +109,16 @@ func TestIsRequestParam(t *testing.T) {
 		assert.Equal(t, tt.b, b)
 	}
 }
+
+func TestNormalizeQueryValues(t *testing.T) {
+	input := make(map[string][]string)
+	input["key"] = []string{"value1", "value2,value3,value4", "value5"}
+
+	normalizeQueryValues(input)
+	require.Equal(t, 5, len(input["key"]))
+	assert.Equal(t, "value1", input["key"][0])
+	assert.Equal(t, "value2", input["key"][1])
+	assert.Equal(t, "value3", input["key"][2])
+	assert.Equal(t, "value4", input["key"][3])
+	assert.Equal(t, "value5", input["key"][4])
+}
