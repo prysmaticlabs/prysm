8f858d4173836acbc1b3b19e45dd545e1a32b384
prestonvanloon <preston@prysmaticlabs.com>
2021-07-07 16:06:20 -0500

    fix most tests

--- shared/aggregation/attestations/attestations_test.go
+++ shared/aggregation/attestations/attestations_test.go
@@ -101,6 +101,7 @@ func TestAggregateAttestations_Aggregate(t *testing.T) {
 		name   string
 		inputs []bitfield.Bitlist
 		want   []bitfield.Bitlist
+		err    error
 	}{
 		{
 			name:   "empty list",
@@ -208,12 +209,17 @@ func TestAggregateAttestations_Aggregate(t *testing.T) {
 				{0b00000111, 0b100},
 				{0b00000100, 0b1},
 			},
+			err: bitfield.ErrBitlistDifferentLength,
 		},
 	}
 
 	for _, tt := range tests {
 		runner := func() {
 			got, err := Aggregate(aggtesting.MakeAttestationsFromBitlists(tt.inputs))
+			if tt.err != nil {
+				require.ErrorContains(t, tt.err.Error(), err)
+				return
+			}
 			require.NoError(t, err)
 			sort.Slice(got, func(i, j int) bool {
 				return got[i].AggregationBits.Bytes()[0] < got[j].AggregationBits.Bytes()[0]
