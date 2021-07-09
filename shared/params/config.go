2b349a1b06a869056c4d99bb0b32d26f42663664
Nishant Das <nishdas93@gmail.com>
2021-07-09 11:33:44 +0800

    Interop Fixes (#9163)

--- shared/params/config.go
+++ shared/params/config.go
@@ -170,6 +170,8 @@ type BeaconChainConfig struct {
 
 // InitializeForkSchedule initializes the schedules forks baked into the config.
 func (b *BeaconChainConfig) InitializeForkSchedule() {
+	// Reset Fork Version Schedule.
+	b.ForkVersionSchedule = map[[4]byte]types.Epoch{}
 	// Set Genesis fork data.
 	b.ForkVersionSchedule[bytesutil.ToBytes4(b.GenesisForkVersion)] = b.GenesisEpoch
 	// Set Altair fork data.
