ee771e8af3526ce54b7038f6a1d6628dbfde515a
Raul Jordan <raul@prysmaticlabs.com>
2021-07-13 10:11:15 -0400

    handle errvalues

--- beacon-chain/slasher/detect_attestations_test.go
+++ beacon-chain/slasher/detect_attestations_test.go
@@ -883,7 +883,9 @@ func runAttestationsBenchmark(b *testing.B, s *Service, numAtts uint64, numValid
 		totalSeconds := numEpochs * uint64(params.BeaconConfig().SlotsPerEpoch) * params.BeaconConfig().SecondsPerSlot
 		genesisTime := time.Now().Add(-time.Second * time.Duration(totalSeconds))
 		s.genesisTime = genesisTime
-		require.NoError(b, s.checkSlashableAttestations(context.Background(), atts))
+
+		_, err := s.checkSlashableAttestations(context.Background(), atts)
+		require.NoError(b, err)
 	}
 }
 
