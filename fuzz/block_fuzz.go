b22935a72481a62e356545b8eb53388bff73c1a6
terence tsao <terence@prysmaticlabs.com>
2021-07-14 18:32:03 -0700

    Rename AttestationNotifier (#9197)

--- fuzz/block_fuzz.go
+++ fuzz/block_fuzz.go
@@ -156,17 +156,17 @@ func BeaconFuzzBlock(b []byte) {
 	chain.Start()
 
 	s := sync.NewRegularSyncFuzz(&sync.Config{
-		DB:                  db1,
-		P2P:                 p2p,
-		Chain:               chain,
-		InitialSync:         fakeChecker{},
-		StateNotifier:       sn,
-		BlockNotifier:       bn,
-		AttestationNotifier: an,
-		AttPool:             ap,
-		ExitPool:            ep,
-		SlashingPool:        sp,
-		StateGen:            sgen,
+		DB:                db1,
+		P2P:               p2p,
+		Chain:             chain,
+		InitialSync:       fakeChecker{},
+		StateNotifier:     sn,
+		BlockNotifier:     bn,
+		OperationNotifier: an,
+		AttPool:           ap,
+		ExitPool:          ep,
+		SlashingPool:      sp,
+		StateGen:          sgen,
 	})
 
 	if err := s.InitCaches(); err != nil {
