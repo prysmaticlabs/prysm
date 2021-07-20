5c610e5245efdb8373e041c88a0cd170e9de7157
terence tsao <terence@prysmaticlabs.com>
2021-07-20 11:39:45 -0700

    Update testnet_e2e_config.go

--- shared/params/testnet_e2e_config.go
+++ shared/params/testnet_e2e_config.go
@@ -22,6 +22,7 @@ func E2ETestConfig() *BeaconChainConfig {
 	e2eConfig.GenesisDelay = 10 // 10 seconds so E2E has enough time to process deposits and get started.
 
 	// Time parameters.
+	e2eConfig.SecondsPerSlot = 8
 	e2eConfig.SlotsPerEpoch = 6
 	e2eConfig.SecondsPerETH1Block = 2
 	e2eConfig.Eth1FollowDistance = 4
