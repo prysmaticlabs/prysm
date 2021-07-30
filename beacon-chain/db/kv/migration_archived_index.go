0d818a5709bf324e5f563ab65a9b0270b59b29c3
Mohamed Zahoor <zahoor@prysmaticlabs.com>
2021-07-30 19:08:54 +0530

    Reduce memory overhead during DB migrations (#9298)
    * wip
    
    * migration happening
    
    * wip
    
    * fix memory utilization, add the testcases
    
    * migration issues
    
    * the gazel
    
    * remove debug logging
    
    * goimport
    
    * gazel

--- beacon-chain/db/kv/migration_archived_index.go
+++ beacon-chain/db/kv/migration_archived_index.go
@@ -12,51 +12,61 @@ import (
 
 var migrationArchivedIndex0Key = []byte("archive_index_0")
 
-func migrateArchivedIndex(tx *bolt.Tx) error {
-	mb := tx.Bucket(migrationsBucket)
-	if b := mb.Get(migrationArchivedIndex0Key); bytes.Equal(b, migrationCompleted) {
-		return nil // Migration already completed.
-	}
-
-	bkt := tx.Bucket(archivedRootBucket)
-	if bkt == nil {
-		return nil
-	}
-	// Remove "last archived index" key before iterating over all keys.
-	if err := bkt.Delete(lastArchivedIndexKey); err != nil {
-		return err
-	}
-
-	var highest types.Slot
-	c := bkt.Cursor()
-	for k, v := c.First(); k != nil; k, v = c.Next() {
-		// Look up actual slot from block
-		b := tx.Bucket(blocksBucket).Get(v)
-		// Skip this key if there is no block for whatever reason.
-		if b == nil {
-			continue
+func migrateArchivedIndex(ctx context.Context, db *bolt.DB) error {
+	if updateErr := db.Update(func(tx *bolt.Tx) error {
+		mb := tx.Bucket(migrationsBucket)
+		if b := mb.Get(migrationArchivedIndex0Key); bytes.Equal(b, migrationCompleted) {
+			return nil // Migration already completed.
 		}
-		blk := &ethpb.SignedBeaconBlock{}
-		if err := decode(context.TODO(), b, blk); err != nil {
+
+		bkt := tx.Bucket(archivedRootBucket)
+		if bkt == nil {
+			return nil
+		}
+		// Remove "last archived index" key before iterating over all keys.
+		if err := bkt.Delete(lastArchivedIndexKey); err != nil {
 			return err
 		}
-		if err := tx.Bucket(stateSlotIndicesBucket).Put(bytesutil.SlotToBytesBigEndian(blk.Block.Slot), v); err != nil {
-			return err
-		}
-		if blk.Block.Slot > highest {
-			highest = blk.Block.Slot
-		}
-	}
 
-	// Delete deprecated buckets.
-	for _, bkt := range [][]byte{slotsHasObjectBucket, archivedRootBucket} {
-		if tx.Bucket(bkt) != nil {
-			if err := tx.DeleteBucket(bkt); err != nil {
+		var highest types.Slot
+		c := bkt.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			// Look up actual slot from block
+			b := tx.Bucket(blocksBucket).Get(v)
+			// Skip this key if there is no block for whatever reason.
+			if b == nil {
+				continue
+			}
+			blk := &ethpb.SignedBeaconBlock{}
+			if err := decode(context.TODO(), b, blk); err != nil {
 				return err
 			}
+			if err := tx.Bucket(stateSlotIndicesBucket).Put(bytesutil.SlotToBytesBigEndian(blk.Block.Slot), v); err != nil {
+				return err
+			}
+			if blk.Block.Slot > highest {
+				highest = blk.Block.Slot
+			}
+			// check if context is cancelled in between
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
 		}
-	}
 
-	// Mark migration complete.
-	return mb.Put(migrationArchivedIndex0Key, migrationCompleted)
+		// Delete deprecated buckets.
+		for _, bkt := range [][]byte{slotsHasObjectBucket, archivedRootBucket} {
+			if tx.Bucket(bkt) != nil {
+				if err := tx.DeleteBucket(bkt); err != nil {
+					return err
+				}
+			}
+		}
+
+		// Mark migration complete.
+		return mb.Put(migrationArchivedIndex0Key, migrationCompleted)
+	}); updateErr != nil {
+		log.WithError(updateErr).Errorf("could not migrate bucket: %s", archivedRootBucket)
+		return updateErr
+	}
+	return nil
 }
