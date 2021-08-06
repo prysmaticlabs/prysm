1fd6c1c1175fa2ec4ef7a8407f0aed24715c3c92
prestonvanloon <preston@prysmaticlabs.com>
2021-08-05 22:17:05 -0500

    Initial progress on eth1data votes reporting tool

---
+++ tools/eth1voting/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	v1alpha1 "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/params"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+)
+
+var (
+	beacon = flag.String("beacon", "127.0.0.1:4000", "gRPC address of the Prysm beacon node")
+	genesis = flag.Uint64("genesis", 1606824023, "Genesis time. mainnet=1606824023, prater=1616508000")
+)
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+
+	cc, err := grpc.DialContext(ctx, *beacon, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	c := v1alpha1.NewBeaconChainClient(cc)
+	g, ctx := errgroup.WithContext(ctx)
+	v := NewVotes()
+
+	current := helpers.SlotToEpoch(helpers.CurrentSlot(*genesis))
+	start := current.Div(uint64(params.BeaconConfig().EpochsPerEth1VotingPeriod)).Mul(uint64(params.BeaconConfig().EpochsPerEth1VotingPeriod))
+	nextStart := start.AddEpoch(params.BeaconConfig().EpochsPerEth1VotingPeriod)
+
+	fmt.Printf("Looking back from current epoch %d back to %d\n", current, start)
+	nextStartSlot, err := helpers.StartSlot(nextStart)
+	if err != nil {
+		panic(err)
+	}
+	nextStartTime, err := helpers.SlotToTime(*genesis, nextStartSlot)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Next period starts at epoch %d (%s)\n", nextStart, time.Until(nextStartTime))
+
+	for i := 0; i < int(current.Sub(uint64(start))); i++ {
+		j := i
+		g.Go(func() error {
+			resp, err := c.ListBlocks(ctx, &v1alpha1.ListBlocksRequest{
+				QueryFilter: &v1alpha1.ListBlocksRequest_Epoch{Epoch: current.Sub(uint64(j))},
+			})
+			if err != nil {
+				return err
+			}
+			for _, c := range resp.GetBlockContainers() {
+				v.Insert(c.Block.Block)
+			}
+
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(v.Report())
+}
+
