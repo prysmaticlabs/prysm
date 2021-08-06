a822451f7a009fe1909128e69c8c89e2be3fa0f6
terence tsao <terence@prysmaticlabs.com>
2021-08-06 12:27:00 -0700

    Tests

--- beacon-chain/core/altair/attestation.go
+++ beacon-chain/core/altair/attestation.go
@@ -9,13 +9,8 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/state"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/shared/attestationutil"
-	"github.com/prysmaticlabs/prysm/shared/mathutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 )
-
-type matchedSource bool
-type matchedTarget bool
-type matchedHead bool
 
 // HasValidatorFlag returns true if the flag at position has set.
 func HasValidatorFlag(flag, flagPosition uint8) bool {
@@ -27,9 +22,9 @@ func AddValidatorFlag(flag, flagPosition uint8) uint8 {
 	return flag | (1 << flagPosition)
 }
 
-// This retrieves a map of attestation scoring based on Altair's participation flag indices.
+// AttestationParticipationFlagIndices retrieves a map of attestation scoring based on Altair's participation flag indices.
 // This is used to facilitate process attestation during state transition and during upgrade to altair state.
-func attestationParticipationFlagIndices(beaconState state.BeaconStateAltair, data *ethpb.AttestationData, delay types.Slot) (map[uint8]bool, error) {
+func AttestationParticipationFlagIndices(beaconState state.BeaconStateAltair, data *ethpb.AttestationData, delay types.Slot) (map[uint8]bool, error) {
 	currEpoch := helpers.CurrentEpoch(beaconState)
 	var justifiedCheckpt *ethpb.Checkpoint
 	if data.Target.Epoch == currEpoch {
@@ -38,7 +33,7 @@ func attestationParticipationFlagIndices(beaconState state.BeaconStateAltair, da
 		justifiedCheckpt = beaconState.PreviousJustifiedCheckpoint()
 	}
 
-	matchingSource, matchingTarget, matchingHead, err := matchingStatus(beaconState, data, justifiedCheckpt)
+	matchingSource, matchedTgt, matchedHead, err := MatchingStatus(beaconState, data, justifiedCheckpt)
 	if err != nil {
 		return nil, err
 	}
@@ -52,34 +47,34 @@ func attestationParticipationFlagIndices(beaconState state.BeaconStateAltair, da
 	targetFlagIndex := cfg.TimelyTargetFlagIndex
 	headFlagIndex := cfg.TimelyHeadFlagIndex
 	slotsPerEpoch := cfg.SlotsPerEpoch
-	sqtRootSlots := mathutil.IntegerSquareRoot(uint64(slotsPerEpoch))
-	if matchingSource && delay <= types.Slot(sqtRootSlots) {
+	sqtRootSlots := cfg.SqrRootSlotsPerEpoch
+	if matchingSource && delay <= sqtRootSlots {
 		participatedFlags[sourceFlagIndex] = true
 	}
-	if matchingTarget && delay <= slotsPerEpoch {
+	if matchedTgt && delay <= slotsPerEpoch {
 		participatedFlags[targetFlagIndex] = true
 	}
-	matchingHeadTarget := bool(matchingHead) && bool(matchingTarget)
+	matchingHeadTarget := matchedHead && matchedTgt
 	if matchingHeadTarget && delay == cfg.MinAttestationInclusionDelay {
 		participatedFlags[headFlagIndex] = true
 	}
 	return participatedFlags, nil
 }
 
-// This returns the matching statues for attestation data's source target and head.
-func matchingStatus(beaconState state.BeaconState, data *ethpb.AttestationData, cp *ethpb.Checkpoint) (s matchedSource, t matchedTarget, h matchedHead, err error) {
-	s = matchedSource(attestationutil.CheckPointIsEqual(data.Source, cp))
+// MatchingStatus returns the matching statues for attestation data's source target and head.
+func MatchingStatus(beaconState state.BeaconState, data *ethpb.AttestationData, cp *ethpb.Checkpoint) (matchedSrc bool, matchedTgt bool, matchedHead bool, err error) {
+	matchedSrc = attestationutil.CheckPointIsEqual(data.Source, cp)
 
 	r, err := helpers.BlockRoot(beaconState, data.Target.Epoch)
 	if err != nil {
 		return false, false, false, err
 	}
-	t = matchedTarget(bytes.Equal(r, data.Target.Root))
+	matchedTgt = bytes.Equal(r, data.Target.Root)
 
 	r, err = helpers.BlockRootAtSlot(beaconState, data.Slot)
 	if err != nil {
 		return false, false, false, err
 	}
-	h = matchedHead(bytes.Equal(r, data.BeaconBlockRoot))
+	matchedHead = bytes.Equal(r, data.BeaconBlockRoot)
 	return
 }
