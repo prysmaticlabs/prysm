079ef32146dba8e6378893cc2070d99557978f60
Raul Jordan <raul@prysmaticlabs.com>
2021-08-06 15:12:45 -0600

    add encoding and gzip

--- endtoend/params/params.go
+++ endtoend/params/params.go
@@ -37,7 +37,7 @@ var TestParams *params
 var BootNodeLogFileName = "bootnode.log"
 
 // TracingRequestSinkFileName is the file name for writing raw trace requests.
-var TracingRequestSinkFileName = "tracing-http-requests.log"
+var TracingRequestSinkFileName = "tracing-http-requests.log.gz"
 
 // BeaconNodeLogFileName is the file name used for the beacon chain node logs.
 var BeaconNodeLogFileName = "beacon-%d.log"
