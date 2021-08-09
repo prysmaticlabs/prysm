9911530e25177168ab4465f9c458c93d8240c14a
prestonvanloon <preston@prysmaticlabs.com>
2021-08-09 11:09:44 -0500

    add missing minimal config params

--- shared/params/minimal_config.go
+++ shared/params/minimal_config.go
@@ -90,5 +90,10 @@ func MinimalSpecConfig() *BeaconChainConfig {
 	minimalConfig.InactivityScoreBias = 4
 	minimalConfig.EpochsPerSyncCommitteePeriod = 8
 
+	// Ethereum PoW parameters.
+	minimalConfig.DepositChainID = 5   // Chain ID of eth1 goerli.
+	minimalConfig.DepositNetworkID = 5 // Network ID of eth1 goerli.
+	minimalConfig.DepositContractAddress = "0x1234567890123456789012345678901234567890"
+
 	return minimalConfig
 }
