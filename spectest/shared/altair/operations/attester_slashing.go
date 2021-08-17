c3bd437a7b722a30bc022ff3983d432d2856f229
prestonvanloon <preston@prysmaticlabs.com>
2021-08-17 15:25:05 -0500

    remove beacon-chain/core/altair/validator.go and beacon-chain/core/altair/validator_test.go after PR #9321

--- spectest/shared/altair/operations/attester_slashing.go
+++ spectest/shared/altair/operations/attester_slashing.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 
 	"github.com/golang/snappy"
-	"github.com/prysmaticlabs/prysm/beacon-chain/core/altair"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/validators"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1/block"
@@ -31,7 +31,7 @@ func RunAttesterSlashingTest(t *testing.T, config string) {
 
 			body := &ethpb.BeaconBlockBodyAltair{AttesterSlashings: []*ethpb.AttesterSlashing{attSlashing}}
 			RunBlockOperationTest(t, folderPath, body, func(ctx context.Context, s state.BeaconState, b block.SignedBeaconBlock) (state.BeaconState, error) {
-				return blocks.ProcessAttesterSlashings(ctx, s, b.Block().Body().AttesterSlashings(), altair.SlashValidator)
+				return blocks.ProcessAttesterSlashings(ctx, s, b.Block().Body().AttesterSlashings(), validators.SlashValidator)
 			})
 		})
 	}
