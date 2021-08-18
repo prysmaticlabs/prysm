abf3c096c1d35f6dee4a89712019f370958405ae
rkapka <rkapka@wp.pl>
2021-08-18 17:28:17 +0200

    use sync committee setter

--- beacon-chain/rpc/eth/beacon/sync_committee_test.go
+++ beacon-chain/rpc/eth/beacon/sync_committee_test.go
@@ -6,7 +6,9 @@ import (
 
 	types "github.com/prysmaticlabs/eth2-types"
 	"github.com/prysmaticlabs/prysm/beacon-chain/rpc/testutil"
-	"github.com/prysmaticlabs/prysm/proto/eth/v2"
+	ethpbv2 "github.com/prysmaticlabs/prysm/proto/eth/v2"
+	ethpbalpha "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
 	"github.com/prysmaticlabs/prysm/shared/params"
 	sharedtestutil "github.com/prysmaticlabs/prysm/shared/testutil"
 	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
@@ -16,6 +18,15 @@ import (
 func TestListSyncCommittees(t *testing.T) {
 	ctx := context.Background()
 	st, _ := sharedtestutil.DeterministicGenesisStateAltair(t, params.BeaconConfig().SyncCommitteeSize)
+	syncCommittee := make([][]byte, params.BeaconConfig().SyncCommitteeSize)
+	vals := st.Validators()
+	for i := 0; i < len(syncCommittee); i++ {
+		syncCommittee[i] = vals[i].PublicKey
+	}
+	require.NoError(t, st.SetCurrentSyncCommittee(&ethpbalpha.SyncCommittee{
+		Pubkeys:         syncCommittee,
+		AggregatePubkey: bytesutil.PadTo([]byte{}, params.BeaconConfig().BLSPubkeyLength),
+	}))
 	stRoot, err := st.HashTreeRoot(ctx)
 	require.NoError(t, err)
 
@@ -24,7 +35,7 @@ func TestListSyncCommittees(t *testing.T) {
 			BeaconState: st,
 		},
 	}
-	req := &eth.StateSyncCommitteesRequest{StateId: stRoot[:]}
+	req := &ethpbv2.StateSyncCommitteesRequest{StateId: stRoot[:]}
 	resp, err := s.ListSyncCommittees(ctx, req)
 	require.NoError(t, err)
 	require.NotNil(t, resp.Data)
