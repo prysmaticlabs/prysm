360265b2285eb8e373ab0d5eaf2c5e5c47d16a1d
terence tsao <terence@prysmaticlabs.com>
2021-08-17 20:47:27 -0700

    Validator sync committee methods

---
+++ validator/client/sync_committee.go
@@ -0,0 +1,225 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	emptypb "github.com/golang/protobuf/ptypes/empty"
+	types "github.com/prysmaticlabs/eth2-types"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/altair"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/shared/bls"
+	"github.com/prysmaticlabs/prysm/shared/bytesutil"
+	"github.com/prysmaticlabs/prysm/shared/params"
+	"github.com/prysmaticlabs/prysm/shared/traceutil"
+	"github.com/sirupsen/logrus"
+	"go.opencensus.io/trace"
+)
+
+// SubmitSyncCommitteeMessage submits the sync committee message to the beacon chain.
+func (v *validator) SubmitSyncCommitteeMessage(ctx context.Context, slot types.Slot, pubKey [48]byte) {
+	ctx, span := trace.StartSpan(ctx, "validator.SubmitSyncCommitteeMessage")
+	defer span.End()
+	span.AddAttributes(trace.StringAttribute("validator", fmt.Sprintf("%#x", pubKey)))
+
+	v.waitOneThirdOrValidBlock(ctx, slot)
+
+	res, err := v.validatorClient.GetSyncMessageBlockRoot(ctx, &emptypb.Empty{})
+	if err != nil {
+		log.WithError(err).Error("Could not request sync message block root to sign")
+		traceutil.AnnotateError(span, err)
+		return
+	}
+
+	duty, err := v.duty(pubKey)
+	if err != nil {
+		log.WithError(err).Error("Could not fetch validator assignment")
+		return
+	}
+
+	d, err := v.domainData(ctx, helpers.SlotToEpoch(slot), params.BeaconConfig().DomainSyncCommittee[:])
+	if err != nil {
+		log.WithError(err).Error("Could not get sync committee domain data")
+		return
+	}
+	sszRoot := types.SSZBytes(res.Root)
+	r, err := helpers.ComputeSigningRoot(&sszRoot, d.SignatureDomain)
+	if err != nil {
+		log.WithError(err).Error("Could not get sync committee message signing root")
+		return
+	}
+	sig, err := v.keyManager.Sign(ctx, &ethpb.SignRequest{
+		PublicKey:       pubKey[:],
+		SigningRoot:     r[:],
+		SignatureDomain: d.SignatureDomain,
+	})
+	if err != nil {
+		log.WithError(err).Error("Could not sign sync committee message")
+		return
+	}
+
+	msg := &ethpb.SyncCommitteeMessage{
+		Slot:           slot,
+		BlockRoot:      res.Root,
+		ValidatorIndex: duty.ValidatorIndex,
+		Signature:      sig.Marshal(),
+	}
+	if _, err := v.validatorClient.SubmitSyncMessage(ctx, msg); err != nil {
+		log.WithError(err).Error("Could not submit sync committee message")
+		return
+	}
+
+	log.WithFields(logrus.Fields{
+		"slot":           msg.Slot,
+		"blockRoot":      fmt.Sprintf("%#x", bytesutil.Trunc(msg.BlockRoot)),
+		"validatorIndex": msg.ValidatorIndex,
+	}).Info("Submitted new sync message")
+}
+
+// SubmitSignedContributionAndProof submits the signed sync committee contribution and proof to the beacon chain.
+func (v *validator) SubmitSignedContributionAndProof(ctx context.Context, slot types.Slot, pubKey [48]byte) {
+	ctx, span := trace.StartSpan(ctx, "validator.SubmitSignedContributionAndProof")
+	defer span.End()
+	span.AddAttributes(trace.StringAttribute("validator", fmt.Sprintf("%#x", pubKey)))
+
+	duty, err := v.duty(pubKey)
+	if err != nil {
+		log.Errorf("Could not fetch validator assignment: %v", err)
+		return
+	}
+
+	indexRes, err := v.validatorClient.GetSyncSubcommitteeIndex(ctx, &ethpb.SyncSubcommitteeIndexRequest{
+		PublicKey: pubKey[:],
+		Slot:      slot,
+	})
+	if err != nil {
+		log.Errorf("Could not get sync subcommittee index: %v", err)
+		return
+	}
+	if len(indexRes.Indices) == 0 {
+		log.Debug("Empty subcommittee index list, do nothing")
+		return
+	}
+
+	selectionProofs := make([][]byte, len(indexRes.Indices))
+	for i, index := range indexRes.Indices {
+		subCommitteeSize := params.BeaconConfig().SyncCommitteeSize / params.BeaconConfig().SyncCommitteeSubnetCount
+		subnet := uint64(index) / subCommitteeSize
+		selectionProof, err := v.signSyncSelectionData(ctx, pubKey, subnet, slot)
+		if err != nil {
+			log.Errorf("Could not sign selection data: %v", err)
+			return
+		}
+		selectionProofs[i] = selectionProof
+	}
+
+	v.waitToSlotTwoThirds(ctx, slot)
+
+	for i, comIdx := range indexRes.Indices {
+		isAggregator, err := altair.IsSyncCommitteeAggregator(selectionProofs[i])
+		if err != nil {
+			log.Errorf("Could check in aggregator: %v", err)
+			return
+		}
+		if !isAggregator {
+			continue
+		}
+		subCommitteeSize := params.BeaconConfig().SyncCommitteeSize / params.BeaconConfig().SyncCommitteeSubnetCount
+		subnet := uint64(comIdx) / subCommitteeSize
+		contribution, err := v.validatorClient.GetSyncCommitteeContribution(ctx, &ethpb.SyncCommitteeContributionRequest{
+			Slot:      slot,
+			PublicKey: pubKey[:],
+			SubnetId:  subnet,
+		})
+		if err != nil {
+			log.Errorf("Could not get sync committee contribution: %v", err)
+			return
+		}
+		if contribution.AggregationBits.Count() == 0 {
+			log.WithFields(logrus.Fields{
+				"slot":   slot,
+				"pubkey": pubKey,
+				"subnet": subnet,
+			}).Warn("Sync contribution for validator has no bits set.")
+			continue
+		}
+
+		contributionAndProof := &ethpb.ContributionAndProof{
+			AggregatorIndex: duty.ValidatorIndex,
+			Contribution:    contribution,
+			SelectionProof:  selectionProofs[i],
+		}
+		sig, err := v.signContributionAndProof(ctx, pubKey, contributionAndProof)
+		if err != nil {
+			log.Errorf("Could not sign contribution and proof: %v", err)
+			return
+		}
+
+		if _, err := v.validatorClient.SubmitSignedContributionAndProof(ctx, &ethpb.SignedContributionAndProof{
+			Message:   contributionAndProof,
+			Signature: sig,
+		}); err != nil {
+			log.Errorf("Could not submit signed contribution and proof: %v", err)
+			return
+		}
+
+		log.WithFields(logrus.Fields{
+			"slot":              contributionAndProof.Contribution.Slot,
+			"blockRoot":         fmt.Sprintf("%#x", bytesutil.Trunc(contributionAndProof.Contribution.BlockRoot)),
+			"subcommitteeIndex": contributionAndProof.Contribution.SubcommitteeIndex,
+			"aggregatorIndex":   contributionAndProof.AggregatorIndex,
+			"bitsCount":         contributionAndProof.Contribution.AggregationBits.Count(),
+		}).Info("Submitted new sync contribution and proof")
+	}
+}
+
+// Signs input slot with domain sync committee selection proof. This is used to create the signature for sync committee selection.
+func (v *validator) signSyncSelectionData(ctx context.Context, pubKey [48]byte, index uint64, slot types.Slot) (signature []byte, err error) {
+	domain, err := v.domainData(ctx, helpers.SlotToEpoch(slot), params.BeaconConfig().DomainSyncCommitteeSelectionProof[:])
+	if err != nil {
+		return nil, err
+	}
+
+	data := &ethpb.SyncAggregatorSelectionData{
+		Slot:              slot,
+		SubcommitteeIndex: index,
+	}
+	root, err := helpers.ComputeSigningRoot(data, domain.SignatureDomain)
+	if err != nil {
+		return nil, err
+	}
+	sig, err := v.keyManager.Sign(ctx, &ethpb.SignRequest{
+		PublicKey:       pubKey[:],
+		SigningRoot:     root[:],
+		SignatureDomain: domain.SignatureDomain,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return sig.Marshal(), nil
+}
+
+// This returns the signature of validator signing over sync committee contribution and proof object.
+func (v *validator) signContributionAndProof(ctx context.Context, pubKey [48]byte, c *ethpb.ContributionAndProof) ([]byte, error) {
+	d, err := v.domainData(ctx, helpers.SlotToEpoch(c.Contribution.Slot), params.BeaconConfig().DomainContributionAndProof[:])
+	if err != nil {
+		return nil, err
+	}
+	var sig bls.Signature
+	root, err := helpers.ComputeSigningRoot(c, d.SignatureDomain)
+	if err != nil {
+		return nil, err
+	}
+	sig, err = v.keyManager.Sign(ctx, &ethpb.SignRequest{
+		PublicKey:       pubKey[:],
+		SigningRoot:     root[:],
+		SignatureDomain: d.SignatureDomain,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return sig.Marshal(), nil
+}
