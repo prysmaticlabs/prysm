8179400b2ac7cd302c1bca0f191145d017838f5d
rkapka <rkapka@wp.pl>
2021-08-20 12:48:15 +0200

    fix gateway config test

--- beacon-chain/gateway/helpers_test.go
+++ beacon-chain/gateway/helpers_test.go
@@ -11,8 +11,9 @@ func TestDefaultConfig(t *testing.T) {
 	t.Run("Without debug endpoints", func(t *testing.T) {
 		cfg := DefaultConfig(false)
 		assert.NotNil(t, cfg.V1PbMux.Mux)
-		require.Equal(t, 1, len(cfg.V1PbMux.Patterns))
+		require.Equal(t, 2, len(cfg.V1PbMux.Patterns))
 		assert.Equal(t, "/eth/v1/", cfg.V1PbMux.Patterns[0])
+		assert.Equal(t, "/eth/v2/", cfg.V1PbMux.Patterns[1])
 		assert.Equal(t, 4, len(cfg.V1PbMux.Registrations))
 		assert.NotNil(t, cfg.V1Alpha1PbMux.Mux)
 		require.Equal(t, 1, len(cfg.V1Alpha1PbMux.Patterns))
@@ -23,8 +24,9 @@ func TestDefaultConfig(t *testing.T) {
 	t.Run("With debug endpoints", func(t *testing.T) {
 		cfg := DefaultConfig(true)
 		assert.NotNil(t, cfg.V1PbMux.Mux)
-		require.Equal(t, 1, len(cfg.V1PbMux.Patterns))
+		require.Equal(t, 2, len(cfg.V1PbMux.Patterns))
 		assert.Equal(t, "/eth/v1/", cfg.V1PbMux.Patterns[0])
+		assert.Equal(t, "/eth/v2/", cfg.V1PbMux.Patterns[1])
 		assert.Equal(t, 5, len(cfg.V1PbMux.Registrations))
 		assert.NotNil(t, cfg.V1Alpha1PbMux.Mux)
 		require.Equal(t, 1, len(cfg.V1Alpha1PbMux.Patterns))
