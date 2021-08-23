9acf66927ddc4f43ef00645fd42988f138f0ac05
terence tsao <terence@prysmaticlabs.com>
2021-08-22 17:52:35 -0700

    Remove canonical attestaitons

--- beacon-chain/sync/subscriber_beacon_blocks.go
+++ beacon-chain/sync/subscriber_beacon_blocks.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 
-	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/state/interop"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1/wrapper"
@@ -37,29 +36,5 @@ func (s *Service) beaconBlockSubscriber(ctx context.Context, msg proto.Message) 
 		return err
 	}
 
-	// Delete attestations from the block in the pool to avoid inclusion in future block.
-	if err := s.deleteAttsInPool(block.Body().Attestations()); err != nil {
-		log.Debugf("Could not delete attestations in pool: %v", err)
-		return nil
-	}
-
-	return err
-}
-
-// The input attestations are seen by the network, this deletes them from pool
-// so proposers don't include them in a block for the future.
-func (s *Service) deleteAttsInPool(atts []*ethpb.Attestation) error {
-	for _, att := range atts {
-		if helpers.IsAggregated(att) {
-			if err := s.cfg.AttPool.DeleteAggregatedAttestation(att); err != nil {
-				return err
-			}
-		} else {
-			// Ideally there's shouldn't be any unaggregated attestation in the block.
-			if err := s.cfg.AttPool.DeleteUnaggregatedAttestation(att); err != nil {
-				return err
-			}
-		}
-	}
 	return nil
 }
