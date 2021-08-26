ec697abebd1bebb09404d6ef2f3ec1266798e2dd
nisdas <nishdas93@gmail.com>
2021-08-26 18:31:24 +0800

    add topic switch

--- beacon-chain/sync/decode_pubsub.go
+++ beacon-chain/sync/decode_pubsub.go
@@ -1,11 +1,14 @@
 package sync
 
 import (
-	"errors"
+	"reflect"
 	"strings"
 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p"
+	eth "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -22,8 +25,18 @@ func (s *Service) decodePubsubMessage(msg *pubsub.Message) (proto.Message, error
 	if err != nil {
 		return nil, err
 	}
-	base, ok := p2p.GossipTopicMappings[topic]
-	if !ok {
+	// Specially handle subnet messages.
+	switch {
+	case strings.Contains(topic, p2p.GossipAttestationMessage):
+		topic = p2p.GossipTypeMapping[reflect.TypeOf(&eth.Attestation{})]
+		// Given that both sync message related subnets have the same message name, we have to
+		// differentiate them below.
+	case strings.Contains(topic, p2p.GossipSyncCommitteeMessage) && !strings.Contains(topic, p2p.SyncContributionAndProofSubnetTopicFormat):
+		topic = p2p.GossipTypeMapping[reflect.TypeOf(&ethpb.SyncCommitteeMessage{})]
+	}
+
+	base := p2p.GossipTopicMappings[topic]
+	if base == nil {
 		return nil, p2p.ErrMessageNotMapped
 	}
 	m := proto.Clone(base)
