95198333ca92b2ddbcdb708b82c387c06ebba593
Raul Jordan <raul@prysmaticlabs.com>
2021-08-26 15:37:00 -0500

    more readable

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer_sync_aggregate.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer_sync_aggregate.go
@@ -50,17 +50,22 @@ func (cs proposerSyncContributions) dedup() (proposerSyncContributions, error) {
 			a := cs[i]
 			for j := i + 1; j < len(cs); j++ {
 				b := cs[j]
-				if c, err := a.AggregationBits.Contains(b.AggregationBits); err != nil {
+				aContainsB, err := a.AggregationBits.Contains(b.AggregationBits)
+				if err != nil {
 					return nil, err
-				} else if c {
+				}
+				if aContainsB {
 					// a contains b, b is redundant.
 					cs[j] = cs[len(cs)-1]
 					cs[len(cs)-1] = nil
 					cs = cs[:len(cs)-1]
 					j--
-				} else if c, err := b.AggregationBits.Contains(a.GetAggregationBits()); err != nil {
+				}
+				bContainsA, err := b.AggregationBits.Contains(a.GetAggregationBits())
+				if err != nil {
 					return nil, err
-				} else if c {
+				}
+				if bContainsA {
 					// b contains a, a is redundant.
 					cs[i] = cs[len(cs)-1]
 					cs[len(cs)-1] = nil
@@ -82,6 +87,9 @@ func (cs proposerSyncContributions) mostProfitable() *eth.SyncCommitteeContribut
 		return nil
 	}
 	mostProfitable := cs[0]
+	if len(cs) < 2 {
+		return mostProfitable
+	}
 	for _, c := range cs[1:] {
 		if c.AggregationBits.Count() > mostProfitable.AggregationBits.Count() {
 			mostProfitable = c
