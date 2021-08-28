ecf28774da45ec416c3d856b1f0356490371ba40
Marcin Gorzynski <marcin@gorzynski.me>
2021-08-28 20:08:31 +0200

    Add tests for lru wrapper

---
+++ shared/lruwrpr/lru_wrpr_test.go
@@ -0,0 +1,36 @@
+package lruwrpr
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	assert.NotPanics(t, func() {
+		New(10)
+	})
+}
+
+func TestNew_ZeroOrNegativeSize(t *testing.T) {
+	assert.Panics(t, func() {
+		New(0)
+	})
+	assert.Panics(t, func() {
+		New(-1)
+	})
+}
+
+func TestNewWithEvict(t *testing.T) {
+	assert.NotPanics(t, func() {
+		NewWithEvict(10, func(key interface{}, value interface{}) {})
+	})
+}
+
+func TestNewWithEvict_ZeroOrNegativeSize(t *testing.T) {
+	assert.Panics(t, func() {
+		NewWithEvict(0, func(key interface{}, value interface{}) {})
+	})
+	assert.Panics(t, func() {
+		NewWithEvict(-1, func(key interface{}, value interface{}) {})
+	})
+}
