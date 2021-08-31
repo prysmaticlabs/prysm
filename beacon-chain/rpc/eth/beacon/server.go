3014027825380676b38b69e3c9a3f7f014c8b315
Radosław Kapka <rkapka@wp.pl>
2021-08-31 22:30:21 +0200

    Implement `SubmitPoolSyncCommitteeSignatures` in the beacon API (#9464)

--- beacon-chain/rpc/eth/beacon/server.go
+++ beacon-chain/rpc/eth/beacon/server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/operations/slashings"
 	"github.com/prysmaticlabs/prysm/beacon-chain/operations/voluntaryexits"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p"
+	v1alpha1validator "github.com/prysmaticlabs/prysm/beacon-chain/rpc/prysm/v1alpha1/validator"
 	"github.com/prysmaticlabs/prysm/beacon-chain/rpc/statefetcher"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stategen"
 )
@@ -19,17 +20,18 @@ import (
 // Server defines a server implementation of the gRPC Beacon Chain service,
 // providing RPC endpoints to access data relevant to the Ethereum Beacon Chain.
 type Server struct {
-	BeaconDB           db.ReadOnlyDatabase
-	ChainInfoFetcher   blockchain.ChainInfoFetcher
-	GenesisTimeFetcher blockchain.TimeFetcher
-	BlockReceiver      blockchain.BlockReceiver
-	BlockNotifier      blockfeed.Notifier
-	OperationNotifier  operation.Notifier
-	Broadcaster        p2p.Broadcaster
-	AttestationsPool   attestations.Pool
-	SlashingsPool      slashings.PoolManager
-	VoluntaryExitsPool voluntaryexits.PoolManager
-	StateGenService    stategen.StateManager
-	StateFetcher       statefetcher.Fetcher
-	HeadFetcher        blockchain.HeadFetcher
+	BeaconDB                db.ReadOnlyDatabase
+	ChainInfoFetcher        blockchain.ChainInfoFetcher
+	GenesisTimeFetcher      blockchain.TimeFetcher
+	BlockReceiver           blockchain.BlockReceiver
+	BlockNotifier           blockfeed.Notifier
+	OperationNotifier       operation.Notifier
+	Broadcaster             p2p.Broadcaster
+	AttestationsPool        attestations.Pool
+	SlashingsPool           slashings.PoolManager
+	VoluntaryExitsPool      voluntaryexits.PoolManager
+	StateGenService         stategen.StateManager
+	StateFetcher            statefetcher.Fetcher
+	HeadFetcher             blockchain.HeadFetcher
+	V1Alpha1ValidatorServer *v1alpha1validator.Server
 }
