4d5c2f310f24e0e57869939b7ec580b8cbcfbb3f
nisdas <nishdas93@gmail.com>
2021-08-31 20:19:30 +0800

    add changes

--- beacon-chain/sync/context.go
+++ beacon-chain/sync/context.go
@@ -1,15 +1,18 @@
 package sync
 
 import (
-	"errors"
-
 	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/beacon-chain/blockchain"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p"
 )
 
+// Specifies the fixed size context length.
+const digestLength = 4
+
 // writes peer's current context for the expected payload to the stream.
-func writeContextToStream(stream network.Stream, chain blockchain.ChainInfoFetcher) error {
+func writeContextToStream(objCtx []byte, stream network.Stream, chain blockchain.ChainInfoFetcher) error {
 	rpcCtx, err := rpcContext(stream, chain)
 	if err != nil {
 		return err
@@ -17,6 +20,10 @@ func writeContextToStream(stream network.Stream, chain blockchain.ChainInfoFetch
 	// Exit early if there is an empty context.
 	if len(rpcCtx) == 0 {
 		return nil
+	}
+	// Always choose the object's context when writing to the stream.
+	if objCtx != nil {
+		rpcCtx = objCtx
 	}
 	_, err = stream.Write(rpcCtx)
 	return err
@@ -32,7 +39,7 @@ func readContextFromStream(stream network.Stream, chain blockchain.ChainInfoFetc
 		return []byte{}, nil
 	}
 	// Read context (fork-digest) from stream
-	b := make([]byte, 4)
+	b := make([]byte, digestLength)
 	if _, err := stream.Read(b); err != nil {
 		return nil, err
 	}
@@ -49,7 +56,27 @@ func rpcContext(stream network.Stream, chain blockchain.ChainInfoFetcher) ([]byt
 	case p2p.SchemaVersionV1:
 		// Return empty context for a v1 method.
 		return []byte{}, nil
+	case p2p.SchemaVersionV2:
+		currFork := chain.CurrentFork()
+		genVersion := chain.GenesisValidatorRoot()
+		digest, err := helpers.ComputeForkDigest(currFork.CurrentVersion, genVersion[:])
+		if err != nil {
+			return nil, err
+		}
+		return digest[:], nil
 	default:
 		return nil, errors.New("invalid version of %s registered for topic: %s")
 	}
+}
+
+// Validates that the rpc topic matches the provided version.
+func validateVersion(version string, stream network.Stream) error {
+	_, _, streamVersion, err := p2p.TopicDeconstructor(string(stream.Protocol()))
+	if err != nil {
+		return err
+	}
+	if streamVersion != version {
+		return errors.Errorf("stream version of %s doesn't match provided version %s", streamVersion, version)
+	}
+	return nil
 }
--- beacon-chain/sync/initial-sync/blocks_queue_test.go
+++ beacon-chain/sync/initial-sync/blocks_queue_test.go
@@ -1052,6 +1052,8 @@ func TestBlocksQueue_stuckInUnfavourableFork(t *testing.T) {
 			Epoch: finalizedEpoch,
 			Root:  []byte(fmt.Sprintf("finalized_root %d", finalizedEpoch)),
 		},
+		Genesis:        time.Now(),
+		ValidatorsRoot: [32]byte{},
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -1251,6 +1253,8 @@ func TestBlocksQueue_stuckWhenHeadIsSetToOrphanedBlock(t *testing.T) {
 			Epoch: finalizedEpoch,
 			Root:  []byte(fmt.Sprintf("finalized_root %d", finalizedEpoch)),
 		},
+		Genesis:        time.Now(),
+		ValidatorsRoot: [32]byte{},
 	}
 
 	// Populate database with blocks with part of the chain, orphaned block will be added on top.
