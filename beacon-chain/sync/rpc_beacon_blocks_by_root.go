cc790ceb2e7a3d135a6c76e635e34a930d470b37
Nishant Das <nishdas93@gmail.com>
2021-09-04 00:00:56 +0800

    Add RPC Changes to Sync For Block Methods (#9497)
    * add changes
    
    * radek's review
    
    * kasey's review
    
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/sync/rpc_beacon_blocks_by_root.go
+++ beacon-chain/sync/rpc_beacon_blocks_by_root.go
@@ -71,7 +71,7 @@ func (s *Service) beaconBlocksRootRPCHandler(ctx context.Context, msg interface{
 		if blk == nil || blk.IsNil() {
 			continue
 		}
-		if err := s.chunkWriter(stream, blk.Proto()); err != nil {
+		if err := s.chunkBlockWriter(stream, blk); err != nil {
 			return err
 		}
 	}
