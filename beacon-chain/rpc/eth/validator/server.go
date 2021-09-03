e875fe3dfcfaa7e173ecc9396bca6996d0ccfc53
Radosław Kapka <rkapka@wp.pl>
2021-09-03 16:47:35 +0200

    Implement `ProduceSyncCommitteeContribution` in the beacon API (#9524)
    * implementation
    
    * test
    
    * change func signature
    
    * remove broadcaster from test

--- beacon-chain/rpc/eth/validator/server.go
+++ beacon-chain/rpc/eth/validator/server.go
@@ -3,6 +3,7 @@ package validator
 import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/blockchain"
 	"github.com/prysmaticlabs/prysm/beacon-chain/operations/attestations"
+	"github.com/prysmaticlabs/prysm/beacon-chain/operations/synccommittee"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p"
 	v1alpha1validator "github.com/prysmaticlabs/prysm/beacon-chain/rpc/prysm/v1alpha1/validator"
 	"github.com/prysmaticlabs/prysm/beacon-chain/rpc/statefetcher"
@@ -12,12 +13,13 @@ import (
 // Server defines a server implementation of the gRPC Validator service,
 // providing RPC endpoints intended for validator clients.
 type Server struct {
-	HeadFetcher      blockchain.HeadFetcher
-	TimeFetcher      blockchain.TimeFetcher
-	SyncChecker      sync.Checker
-	AttestationsPool attestations.Pool
-	PeerManager      p2p.PeerManager
-	Broadcaster      p2p.Broadcaster
-	StateFetcher     statefetcher.Fetcher
-	V1Alpha1Server   *v1alpha1validator.Server
+	HeadFetcher       blockchain.HeadFetcher
+	TimeFetcher       blockchain.TimeFetcher
+	SyncChecker       sync.Checker
+	AttestationsPool  attestations.Pool
+	PeerManager       p2p.PeerManager
+	Broadcaster       p2p.Broadcaster
+	StateFetcher      statefetcher.Fetcher
+	SyncCommitteePool synccommittee.Pool
+	V1Alpha1Server    *v1alpha1validator.Server
 }
