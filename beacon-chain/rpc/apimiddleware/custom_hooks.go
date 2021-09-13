562e12825176835c0a4826ed1aa3f658f85bb48a
Radosław Kapka <rkapka@wp.pl>
2021-09-13 21:14:05 +0200

    Align `GetSyncCommitteeDuties` to API HTTP spec (#9570)
    * Align `GetSyncCommitteeDuties` to API HTTP spec
    
    * doc
    
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/rpc/apimiddleware/custom_hooks.go
+++ beacon-chain/rpc/apimiddleware/custom_hooks.go
@@ -32,15 +32,15 @@ func wrapAttestationsArray(endpoint gateway.Endpoint, _ http.ResponseWriter, req
 	return nil
 }
 
-// https://ethereum.github.io/beacon-apis/#/Validator/getAttesterDuties expects posting a top-level array.
-// We make it more proto-friendly by wrapping it in a struct with an 'index' field.
+// Some endpoints e.g. https://ethereum.github.io/beacon-apis/#/Validator/getAttesterDuties expect posting a top-level array.
+// We make it more proto-friendly by wrapping it in a struct with an 'Index' field.
 func wrapValidatorIndicesArray(endpoint gateway.Endpoint, _ http.ResponseWriter, req *http.Request) gateway.ErrorJson {
-	if _, ok := endpoint.PostRequest.(*attesterDutiesRequestJson); ok {
+	if _, ok := endpoint.PostRequest.(*dutiesRequestJson); ok {
 		indices := make([]string, 0)
 		if err := json.NewDecoder(req.Body).Decode(&indices); err != nil {
 			return gateway.InternalServerErrorWithMessage(err, "could not decode body")
 		}
-		j := &attesterDutiesRequestJson{Index: indices}
+		j := &dutiesRequestJson{Index: indices}
 		b, err := json.Marshal(j)
 		if err != nil {
 			return gateway.InternalServerErrorWithMessage(err, "could not marshal wrapped body")
