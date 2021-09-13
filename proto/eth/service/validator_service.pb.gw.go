ed68dd757f4ebdf089f0c55785f50231075401fe
Radosław Kapka <rkapka@wp.pl>
2021-09-13 15:58:42 +0200

    Implement `ProduceBlockV2` in the beacon API (#9560)
    * Rename service func to ProduceBlockV2
    
    # Conflicts:
    #	beacon-chain/rpc/eth/validator/validator.go
    
    * initial implementation
    
    # Conflicts:
    #	beacon-chain/rpc/eth/validator/validator.go
    #	proto/eth/v2/validator.pb.go
    
    * test first try
    
    * try new signature in test
    
    * Fix test's sync committee signature
    
    * Use 0xAA instead of 0xFF
    
    * properly get altair block
    
    * final implementation
    
    * Revert "Auxiliary commit to revert individual files from 17779d4228b8c65452343fe5c51ef5764766f732"
    
    This reverts commit 3f352a145c70c8edf8dd283f684fecf93f6d6487.
    
    # Conflicts:
    #	beacon-chain/rpc/eth/beacon/blocks.go
    
    * revert pb.go changes to block
    
    * add signature to return value
    
    * fix ineffectual assignment
    
    * simplify if/else statement
    
    Co-authored-by: terence tsao <terence@prysmaticlabs.com>

--- proto/eth/service/validator_service.pb.gw.go
+++ proto/eth/service/validator_service.pb.gw.go
@@ -306,11 +306,11 @@ func local_request_BeaconValidator_ProduceBlock_0(ctx context.Context, marshaler
 }
 
 var (
-	filter_BeaconValidator_ProduceBlockAltair_0 = &utilities.DoubleArray{Encoding: map[string]int{"slot": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+	filter_BeaconValidator_ProduceBlockV2_0 = &utilities.DoubleArray{Encoding: map[string]int{"slot": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
-func request_BeaconValidator_ProduceBlockAltair_0(ctx context.Context, marshaler runtime.Marshaler, client BeaconValidatorClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq eth.ProduceBlockRequestV2
+func request_BeaconValidator_ProduceBlockV2_0(ctx context.Context, marshaler runtime.Marshaler, client BeaconValidatorClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq v1.ProduceBlockRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -334,17 +334,17 @@ func request_BeaconValidator_ProduceBlockAltair_0(ctx context.Context, marshaler
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BeaconValidator_ProduceBlockAltair_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BeaconValidator_ProduceBlockV2_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.ProduceBlockAltair(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.ProduceBlockV2(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_BeaconValidator_ProduceBlockAltair_0(ctx context.Context, marshaler runtime.Marshaler, server BeaconValidatorServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq eth.ProduceBlockRequestV2
+func local_request_BeaconValidator_ProduceBlockV2_0(ctx context.Context, marshaler runtime.Marshaler, server BeaconValidatorServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq v1.ProduceBlockRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -368,11 +368,11 @@ func local_request_BeaconValidator_ProduceBlockAltair_0(ctx context.Context, mar
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BeaconValidator_ProduceBlockAltair_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BeaconValidator_ProduceBlockV2_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.ProduceBlockAltair(ctx, &protoReq)
+	msg, err := server.ProduceBlockV2(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -719,18 +719,18 @@ func RegisterBeaconValidatorHandlerServer(ctx context.Context, mux *runtime.Serv
 
 	})
 
-	mux.Handle("GET", pattern_BeaconValidator_ProduceBlockAltair_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_BeaconValidator_ProduceBlockV2_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/ethereum.eth.service.BeaconValidator/ProduceBlockAltair")
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/ethereum.eth.service.BeaconValidator/ProduceBlockV2")
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_BeaconValidator_ProduceBlockAltair_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_BeaconValidator_ProduceBlockV2_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -738,7 +738,7 @@ func RegisterBeaconValidatorHandlerServer(ctx context.Context, mux *runtime.Serv
 			return
 		}
 
-		forward_BeaconValidator_ProduceBlockAltair_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_BeaconValidator_ProduceBlockV2_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1024,23 +1024,23 @@ func RegisterBeaconValidatorHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
-	mux.Handle("GET", pattern_BeaconValidator_ProduceBlockAltair_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_BeaconValidator_ProduceBlockV2_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/ethereum.eth.service.BeaconValidator/ProduceBlockAltair")
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/ethereum.eth.service.BeaconValidator/ProduceBlockV2")
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_BeaconValidator_ProduceBlockAltair_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_BeaconValidator_ProduceBlockV2_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_BeaconValidator_ProduceBlockAltair_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_BeaconValidator_ProduceBlockV2_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1196,7 +1196,7 @@ var (
 
 	pattern_BeaconValidator_ProduceBlock_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"eth", "v1", "validator", "blocks", "slot"}, ""))
 
-	pattern_BeaconValidator_ProduceBlockAltair_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"eth", "v2", "validator", "blocks", "slot"}, ""))
+	pattern_BeaconValidator_ProduceBlockV2_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"eth", "v2", "validator", "blocks", "slot"}, ""))
 
 	pattern_BeaconValidator_ProduceAttestationData_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"eth", "v1", "validator", "attestation_data"}, ""))
 
@@ -1222,7 +1222,7 @@ var (
 
 	forward_BeaconValidator_ProduceBlock_0 = runtime.ForwardResponseMessage
 
-	forward_BeaconValidator_ProduceBlockAltair_0 = runtime.ForwardResponseMessage
+	forward_BeaconValidator_ProduceBlockV2_0 = runtime.ForwardResponseMessage
 
 	forward_BeaconValidator_ProduceAttestationData_0 = runtime.ForwardResponseMessage
 
