62e93cfbfd42142a5bfe60aa4172c0a477c16b51
rauljordan <raul@prysmaticlabs.com>
2021-09-14 11:07:27 -0400

    move spectest to testing

---
+++ testing/spectest/mainnet/altair/operations/voluntary_exit_test.go
@@ -0,0 +1,11 @@
+package operations
+
+import (
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/testing/spectest/shared/altair/operations"
+)
+
+func TestMainnet_Altair_Operations_VoluntaryExit(t *testing.T) {
+	operations.RunVoluntaryExitTest(t, "mainnet")
+}
