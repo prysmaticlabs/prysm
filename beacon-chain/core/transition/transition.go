2b9dd392005d47d2edd526663baf77e1428a3555
terence tsao <terence@prysmaticlabs.com>
2021-09-21 11:24:20 -0700

    Fix package comment typo

--- beacon-chain/core/transition/transition.go
+++ beacon-chain/core/transition/transition.go
@@ -1,4 +1,4 @@
-// Package state implements the whole state transition
+// Package transition implements the whole state transition
 // function which consists of per slot, per-epoch transitions.
 // It also bootstraps the genesis beacon state for slot 0.
 package transition
