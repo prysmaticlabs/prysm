58e83581085159b2d4c864ed8c86b915908b0656
rkapka <rkapka@wp.pl>
2021-09-21 12:20:13 +0200

    test fix

--- beacon-chain/gateway/helpers_test.go
+++ beacon-chain/gateway/helpers_test.go
@@ -3,14 +3,14 @@ package gateway
 import (
 	"testing"
 
-	"github.com/prysmaticlabs/prysm/shared/gateway"
+	"github.com/prysmaticlabs/prysm/api/gateway"
 	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
 	"github.com/prysmaticlabs/prysm/shared/testutil/require"
 )
 
 func TestDefaultConfig(t *testing.T) {
 	t.Run("Without debug endpoints", func(t *testing.T) {
-		cfg := DefaultConfig(false, true, true)
+		cfg := DefaultConfig(false, "eth,prysm")
 		assert.NotNil(t, cfg.V1PbMux.Mux)
 		require.Equal(t, 1, len(cfg.V1PbMux.Patterns))
 		assert.Equal(t, "/eth/v1/", cfg.V1PbMux.Patterns[0])
@@ -22,7 +22,7 @@ func TestDefaultConfig(t *testing.T) {
 	})
 
 	t.Run("With debug endpoints", func(t *testing.T) {
-		cfg := DefaultConfig(true, true, true)
+		cfg := DefaultConfig(true, "eth,prysm")
 		assert.NotNil(t, cfg.V1PbMux.Mux)
 		require.Equal(t, 1, len(cfg.V1PbMux.Patterns))
 		assert.Equal(t, "/eth/v1/", cfg.V1PbMux.Patterns[0])
@@ -33,7 +33,7 @@ func TestDefaultConfig(t *testing.T) {
 		assert.Equal(t, 5, len(cfg.V1Alpha1PbMux.Registrations))
 	})
 	t.Run("Without Prysm API", func(t *testing.T) {
-		cfg := DefaultConfig(true, false, true)
+		cfg := DefaultConfig(true, "eth")
 		assert.NotNil(t, cfg.V1PbMux.Mux)
 		require.Equal(t, 1, len(cfg.V1PbMux.Patterns))
 		assert.Equal(t, "/eth/v1/", cfg.V1PbMux.Patterns[0])
@@ -41,7 +41,7 @@ func TestDefaultConfig(t *testing.T) {
 		assert.Equal(t, (*gateway.PbMux)(nil), cfg.V1Alpha1PbMux)
 	})
 	t.Run("Without Eth API", func(t *testing.T) {
-		cfg := DefaultConfig(true, true, false)
+		cfg := DefaultConfig(true, "prysm")
 		assert.Equal(t, (*gateway.PbMux)(nil), cfg.V1PbMux)
 		assert.NotNil(t, cfg.V1Alpha1PbMux.Mux)
 		require.Equal(t, 1, len(cfg.V1Alpha1PbMux.Patterns))
