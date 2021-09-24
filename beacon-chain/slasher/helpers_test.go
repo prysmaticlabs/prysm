e23df7efbaeed24cf7f381a9c3331cde9980e5f1
Raul Jordan <raul@prysmaticlabs.com>
2021-09-24 13:58:11 -0400

    params

--- beacon-chain/slasher/helpers_test.go
+++ beacon-chain/slasher/helpers_test.go
@@ -6,9 +6,9 @@ import (
 
 	types "github.com/prysmaticlabs/eth2-types"
 	slashertypes "github.com/prysmaticlabs/prysm/beacon-chain/slasher/types"
+	"github.com/prysmaticlabs/prysm/config/params"
 	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
-	"github.com/prysmaticlabs/prysm/shared/params"
 	"github.com/prysmaticlabs/prysm/testing/require"
 	logTest "github.com/sirupsen/logrus/hooks/test"
 )
