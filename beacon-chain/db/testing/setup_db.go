e3609ac59ee65c534235963291acaa0b627e23a5
rauljordan <raul@prysmaticlabs.com>
2021-09-24 15:05:14 -0400

    testing helper

--- beacon-chain/db/testing/setup_db.go
+++ beacon-chain/db/testing/setup_db.go
@@ -7,12 +7,28 @@ import (
 	"testing"
 
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
+	"github.com/prysmaticlabs/prysm/beacon-chain/db/iface"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db/kv"
+	"github.com/prysmaticlabs/prysm/beacon-chain/db/slasherkv"
 )
 
 // SetupDB instantiates and returns database backed by key value store.
 func SetupDB(t testing.TB) db.Database {
 	s, err := kv.NewKVStore(context.Background(), t.TempDir(), &kv.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		if err := s.Close(); err != nil {
+			t.Fatalf("failed to close database: %v", err)
+		}
+	})
+	return s
+}
+
+// SetupSlasherDB --
+func SetupSlasherDB(t testing.TB) iface.SlasherDatabase {
+	s, err := slasherkv.NewKVStore(context.Background(), t.TempDir(), &slasherkv.Config{})
 	if err != nil {
 		t.Fatal(err)
 	}
