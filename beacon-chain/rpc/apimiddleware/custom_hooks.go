41e5162e63abbde510f0fd745016bfd0db4db2ea
rkapka <rkapka@wp.pl>
2021-09-28 18:22:07 +0200

    compare slot against slot

--- beacon-chain/rpc/apimiddleware/custom_hooks.go
+++ beacon-chain/rpc/apimiddleware/custom_hooks.go
@@ -10,7 +10,9 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/pkg/errors"
+	types "github.com/prysmaticlabs/eth2-types"
 	"github.com/prysmaticlabs/prysm/api/gateway/apimiddleware"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core"
 	"github.com/prysmaticlabs/prysm/config/params"
 	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
 	ethpbv2 "github.com/prysmaticlabs/prysm/proto/eth/v2"
@@ -206,7 +208,7 @@ func setInitialPublishBlockPostRequest(endpoint *apimiddleware.Endpoint,
 	if err != nil {
 		return false, apimiddleware.InternalServerErrorWithMessage(err, "slot is not an unsigned integer")
 	}
-	if slot < uint64(params.BeaconConfig().AltairForkEpoch) {
+	if core.SlotToEpoch(types.Slot(slot)) < params.BeaconConfig().AltairForkEpoch {
 		endpoint.PostRequest = &signedBeaconBlockContainerJson{}
 	} else {
 		endpoint.PostRequest = &signedBeaconBlockAltairContainerJson{}
