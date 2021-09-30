3c052d917f36567ed89aea1ba85cfb6cb8fbe63e
Nishant Das <nishdas93@gmail.com>
2021-09-30 11:39:53 +0800

    Use Scoring Parameters To Enable Global Score (#8794)
    * fix score parameters
    
    * add check
    
    * fix deadlock issues
    
    * fix
    
    * fix again
    
    * gaz
    
    * comment
    
    * fix tests and victor's review
    
    * gaz
    
    * clean up
    
    * fix tests
    
    * fix tests
    
    * gate behind flag
    
    * add scorer to dev
    
    Co-authored-by: prestonvanloon <preston@prysmaticlabs.com>
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/sync/validate_aggregate_proof.go
+++ beacon-chain/sync/validate_aggregate_proof.go
@@ -56,6 +56,10 @@ func (s *Service) validateAggregateAndProof(ctx context.Context, pid peer.ID, ms
 	if err := helpers.ValidateNilAttestation(m.Message.Aggregate); err != nil {
 		return pubsub.ValidationReject, err
 	}
+	// Do not process slot 0 aggregates.
+	if m.Message.Aggregate.Data.Slot == 0 {
+		return pubsub.ValidationIgnore, nil
+	}
 
 	// Broadcast the aggregated attestation on a feed to notify other services in the beacon node
 	// of a received aggregated attestation.
