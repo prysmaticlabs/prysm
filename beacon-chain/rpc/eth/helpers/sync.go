35aeb47e81344309ace25d62a5c317cd15f6b0e1
rkapka <rkapka@wp.pl>
2021-10-07 19:04:17 +0200

    early exit

--- beacon-chain/rpc/eth/helpers/sync.go
+++ beacon-chain/rpc/eth/helpers/sync.go
@@ -14,25 +14,24 @@ import (
 // ValidateSync checks whether the node is currently syncing and returns an error if it is.
 // It also appends syncing info to gRPC headers.
 func ValidateSync(ctx context.Context, syncChecker sync.Checker, headFetcher blockchain.HeadFetcher, timeFetcher blockchain.TimeFetcher) error {
-	if syncChecker.Syncing() {
-		headSlot := headFetcher.HeadSlot()
-		syncDetailsContainer := &SyncDetailsContainer{
-			SyncDetails: &SyncDetails{
-				HeadSlot:     strconv.FormatUint(uint64(headSlot), 10),
-				SyncDistance: strconv.FormatUint(uint64(timeFetcher.CurrentSlot()-headSlot), 10),
-				IsSyncing:    true,
-			},
-		}
-		err := grpc.AppendCustomErrorHeader(ctx, syncDetailsContainer)
-		if err != nil {
-			return status.Errorf(
-				codes.InvalidArgument,
-				"Syncing to latest head, not ready to respond. Could not prepare sync details: %v",
-				err,
-			)
-		}
-
-		return status.Error(codes.Unavailable, "Syncing to latest head, not ready to respond")
+	if !syncChecker.Syncing() {
+		return nil
 	}
-	return nil
+	headSlot := headFetcher.HeadSlot()
+	syncDetailsContainer := &SyncDetailsContainer{
+		SyncDetails: &SyncDetails{
+			HeadSlot:     strconv.FormatUint(uint64(headSlot), 10),
+			SyncDistance: strconv.FormatUint(uint64(timeFetcher.CurrentSlot()-headSlot), 10),
+			IsSyncing:    true,
+		},
+	}
+	err := grpc.AppendCustomErrorHeader(ctx, syncDetailsContainer)
+	if err != nil {
+		return status.Errorf(
+			codes.InvalidArgument,
+			"Syncing to latest head, not ready to respond. Could not prepare sync details: %v",
+			err,
+		)
+	}
+	return status.Error(codes.Unavailable, "Syncing to latest head, not ready to respond")
 }
