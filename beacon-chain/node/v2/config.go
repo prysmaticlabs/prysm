f8cddcd62634e89d9d18b614500c7ed5a9302cb7
Raul Jordan <raul@prysmaticlabs.com>
2021-10-14 16:58:28 -0400

    simplify

--- beacon-chain/node/v2/config.go
+++ beacon-chain/node/v2/config.go
@@ -1,37 +1,10 @@
 package v2
 
-func WithDataDirectory(dirPath string) Opt {
-	return func(node *BeaconNode) {
-		node.cfg.dataDir = dirPath
-	}
-}
+import "github.com/prysmaticlabs/prysm/beacon-chain/p2p"
 
-func WithBoltMMapInitialSize(size int) Opt {
-	return func(node *BeaconNode) {
-		node.cfg.mmapInitialSize = size
-	}
-}
-
-func WithNoDiscovery() Opt {
-	return func(node *BeaconNode) {
-		node.p2pCfg.NoDiscovery = true
-	}
-}
-
-func WithStaticPeers(peers []string) Opt {
-	return func(node *BeaconNode) {
-		node.p2pCfg.StaticPeers = peers
-	}
-}
-
-func WithRelayNodeAddr(addr string) Opt {
-	return func(node *BeaconNode) {
-		node.p2pCfg.RelayNodeAddr = addr
-	}
-}
-
-func WithWeakSubjectivityCheckpoint(checkpoint string) Opt {
-	return func(node *BeaconNode) {
-		node.cfg.wsCheckpointStr = checkpoint
+func WithP2POptions(opts []p2p.Option) Option {
+	return func(node *BeaconNode) error {
+		node.p2pOpts = opts
+		return nil
 	}
 }
--- beacon-chain/node/v2/node.go
+++ beacon-chain/node/v2/node.go
@@ -2,18 +2,10 @@ package v2
 
 import (
 	"context"
-	"path/filepath"
 
-	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/async/event"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
-	"github.com/prysmaticlabs/prysm/beacon-chain/db/kv"
-	"github.com/prysmaticlabs/prysm/beacon-chain/forkchoice/protoarray"
-	"github.com/prysmaticlabs/prysm/beacon-chain/operations/attestations"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p"
-	"github.com/prysmaticlabs/prysm/beacon-chain/powchain"
-	"github.com/prysmaticlabs/prysm/cmd"
-	"github.com/prysmaticlabs/prysm/config/params"
 	"github.com/prysmaticlabs/prysm/runtime"
 	"github.com/sirupsen/logrus"
 )
@@ -23,10 +15,9 @@ var log = logrus.WithField("prefix", "node")
 type Option func(*BeaconNode) error
 
 type BeaconNode struct {
-	services    []runtime.Service
-	cfg         *config
-	powchainCfg *powchain.Web3ServiceConfig
-	stateFeed   *event.Feed
+	services  []runtime.Service
+	p2pOpts   []p2p.Option
+	stateFeed *event.Feed
 }
 
 // StateFeed implements statefeed.Notifier.
@@ -34,19 +25,8 @@ func (b *BeaconNode) StateFeed() *event.Feed {
 	return b.stateFeed
 }
 
-type config struct {
-	dataDir            string
-	shouldClearDB      bool
-	shouldForceClearDB bool
-	mmapInitialSize    int
-	wsCheckpointStr    string
-}
-
 func New(ctx context.Context, opts ...Option) (*BeaconNode, error) {
 	bn := &BeaconNode{}
-
-	params.BeaconConfig().InitializeForkSchedule()
-
 	for _, opt := range opts {
 		if err := opt(bn); err != nil {
 			return nil, err
@@ -56,55 +36,6 @@ func New(ctx context.Context, opts ...Option) (*BeaconNode, error) {
 }
 
 func (b *BeaconNode) Start() error {
-	attestationPool := attestations.NewPool()
-	_ = attestationPool
-
-	forkChoiceStore := protoarray.New(
-		0, /* justified epoch */
-		0, /* finalized epoch */
-		params.BeaconConfig().ZeroHash,
-	)
-	_ = forkChoiceStore
-	for i := len(b.services); i > 0; i-- {
-		b.services[i].Start()
-	}
-	return nil
-}
-
-func (b *BeaconNode) startDB(ctx context.Context) error {
-	dbPath := filepath.Join(b.cfg.dataDir, kv.BeaconNodeDbDirName)
-	log.WithField("databasePath", dbPath).Info("Checking DB")
-	beaconDB, err := db.NewDB(ctx, dbPath, &kv.Config{
-		InitialMMapSize: b.cfg.mmapInitialSize,
-	})
-	if err != nil {
-		return err
-	}
-	clearDBConfirmed := false
-	if b.cfg.shouldClearDB && !b.cfg.shouldForceClearDB {
-		actionText := "This will delete your beacon chain database stored in your data directory. " +
-			"Your database backups will not be removed - do you want to proceed? (Y/N)"
-		deniedText := "Database will not be deleted. No changes have been made."
-		clearDBConfirmed, err = cmd.ConfirmAction(actionText, deniedText)
-		if err != nil {
-			return err
-		}
-	}
-	if clearDBConfirmed || b.cfg.shouldForceClearDB {
-		log.Warning("Removing database")
-		if err := beaconDB.Close(); err != nil {
-			return errors.Wrap(err, "could not close db prior to clearing")
-		}
-		if err := beaconDB.ClearDB(); err != nil {
-			return errors.Wrap(err, "could not clear database")
-		}
-		beaconDB, err = db.NewDB(ctx, dbPath, &kv.Config{
-			InitialMMapSize: b.cfg.mmapInitialSize,
-		})
-		if err != nil {
-			return errors.Wrap(err, "could not create new database")
-		}
-	}
 	return nil
 }
 
