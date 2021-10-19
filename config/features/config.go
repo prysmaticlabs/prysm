544248f60f9eb43a4ff29e1e34cd8d6f6ea0f48d
terence tsao <terence@prysmaticlabs.com>
2021-10-18 22:38:57 -0700

    Go fmt

--- config/features/config.go
+++ config/features/config.go
@@ -37,7 +37,7 @@ const disabledFeatureFlag = "Disabled feature flag"
 type Flags struct {
 	// Testnet Flags.
 	PyrmontTestnet bool // PyrmontTestnet defines the flag through which we can enable the node to run on the Pyrmont testnet.
-	MergeTestnet bool // MergeTestnet defines the flag through which we can enable node to run on the merge testnet.
+	MergeTestnet   bool // MergeTestnet defines the flag through which we can enable node to run on the merge testnet.
 
 	// Feature related flags.
 	RemoteSlasherProtection             bool // RemoteSlasherProtection utilizes a beacon node with --slasher mode for validator slashing protection.
@@ -131,7 +131,7 @@ func configureTestnet(ctx *cli.Context, cfg *Flags) {
 		log.Warn("Running on the Prater Testnet")
 		params.UsePraterConfig()
 		params.UsePraterNetworkConfig()
-	} else if ctx.Bool(MergeTestnet.Name){
+	} else if ctx.Bool(MergeTestnet.Name) {
 		log.Warn("Running on the Merge Testnet")
 		params.UseMergeTestConfig()
 		params.UseMergeTestNetworkConfig()
