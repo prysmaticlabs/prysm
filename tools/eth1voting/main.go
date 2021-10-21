88f76b455b528bbbdd9aec5f73afe869454837cb
rkapka <rkapka@wp.pl>
2021-10-21 18:42:23 +0200

    correct Prater genesis time

--- tools/eth1voting/main.go
+++ tools/eth1voting/main.go
@@ -17,7 +17,7 @@ import (
 
 var (
 	beacon  = flag.String("beacon", "127.0.0.1:4000", "gRPC address of the Prysm beacon node")
-	genesis = flag.Uint64("genesis", 1606824023, "Genesis time. mainnet=1606824023, prater=1616526000, pyrmont=1605722407")
+	genesis = flag.Uint64("genesis", 1606824023, "Genesis time. mainnet=1606824023, prater=1616508000, pyrmont=1605722407")
 )
 
 func main() {
