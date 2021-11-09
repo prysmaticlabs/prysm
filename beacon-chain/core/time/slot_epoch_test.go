e532bc4528843e84b2bb0238ef92efb7b52f0ca7
terence tsao <terence@prysmaticlabs.com>
2021-11-09 10:09:28 -0800

    Typo

--- beacon-chain/core/time/slot_epoch_test.go
+++ beacon-chain/core/time/slot_epoch_test.go
@@ -142,7 +142,7 @@ func TestCanUpgradeToMerge(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := CanUpgradeToMerge(tt.slot); got != tt.want {
-				t.Errorf("canUpgradeToAltair() = %v, want %v", got, tt.want)
+				t.Errorf("CanUpgradeToMerge() = %v, want %v", got, tt.want)
 			}
 		})
 	}
