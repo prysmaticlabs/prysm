e55843a4ae7d41901ef92808f32c0ebaf52162f3
terence tsao <terence@prysmaticlabs.com>
2021-11-09 09:58:49 -0800

    Add and use merge penalty params

--- runtime/version/fork.go
+++ runtime/version/fork.go
@@ -3,6 +3,7 @@ package version
 const (
 	Phase0 = iota
 	Altair
+	Merge // TODO: subject to change, community is deciding on a star name: https://ethereum-magicians.org/t/ethereum-roadmapping-improvements/6653/14
 )
 
 func String(version int) string {
@@ -11,6 +12,8 @@ func String(version int) string {
 		return "phase0"
 	case Altair:
 		return "altair"
+	case Merge:
+		return "merge"
 	default:
 		return "unknown version"
 	}
