808652dbf316a25f35361b39233bcc14b7323d5f
Potuz <potuz@prysmaticlabs.com>
2021-11-12 15:08:02 -0300

    format roots in slashings

--- beacon-chain/monitor/process_block.go
+++ beacon-chain/monitor/process_block.go
@@ -101,8 +101,8 @@ func (s *Service) processSlashings(blk block.BeaconBlock) {
 				"ProposerIndex":  idx,
 				"SlashedInSlot:": blk.Slot(),
 				"Slot":           slashing.Header_1.Header.Slot,
-				"Root1":          slashing.Header_1.Header.BodyRoot,
-				"Root2":          slashing.Header_2.Header.BodyRoot,
+				"Root1":          fmt.Sprintf("%#x", bytesutil.Trunc(slashing.Header_1.Header.BodyRoot)),
+				"Root2":          fmt.Sprintf("%#x", bytesutil.Trunc(slashing.Header_2.Header.BodyRoot)),
 			}).Info("Proposer slashing was included")
 		}
 	}
@@ -113,11 +113,11 @@ func (s *Service) processSlashings(blk block.BeaconBlock) {
 				log.WithFields(logrus.Fields{
 					"AttesterIndex": idx,
 					"Slot1":         slashing.Attestation_1.Data.Slot,
-					"Root1":         slashing.Attestation_1.Data.BeaconBlockRoot,
+					"Root1":         fmt.Sprintf("%#x", bytesutil.Trunc(slashing.Attestation_1.Data.BeaconBlockRoot)),
 					"Source1":       slashing.Attestation_1.Data.Source.Epoch,
 					"Target1":       slashing.Attestation_1.Data.Target.Epoch,
 					"Slot2":         slashing.Attestation_2.Data.Slot,
-					"Root2":         slashing.Attestation_2.Data.BeaconBlockRoot,
+					"Root2":         fmt.Sprintf("%#x", bytesutil.Trunc(slashing.Attestation_2.Data.BeaconBlockRoot)),
 					"Source2":       slashing.Attestation_2.Data.Source.Epoch,
 					"Target2":       slashing.Attestation_2.Data.Target.Epoch,
 				}).Info("Attester slashing was included")
