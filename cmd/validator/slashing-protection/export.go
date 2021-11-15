36e6b143f2a3c08e7c24386e5ce90af322289fa5
Raul Jordan <raul@prysmaticlabs.com>
2021-11-15 15:39:05 -0500

    warning in case not found

--- cmd/validator/slashing-protection/export.go
+++ cmd/validator/slashing-protection/export.go
@@ -67,6 +67,20 @@ func exportSlashingProtectionJSON(cliCtx *cli.Context) error {
 	if err != nil {
 		return errors.Wrap(err, "could not export slashing protection history")
 	}
+
+	// Check if JSON data is empty and issue a warning about common problems to the user.
+	if eipJSON == nil || len(eipJSON.Data) == 0 {
+		log.Warnf(
+			"No slashing protection data was found in the %s directory. "+
+				"This may be because your validator database is stored in a --datadir which could be a different "+
+				"directory than the --wallet-dir you set when running your validator. You might have an empty "+
+				"validator.db in your --wallet-dir that was created by accident in a previous Prysm version. "+
+				"Check if the directory you are passing in has a validator.db file in it and then run "+
+				"the command with right directory",
+			dataDir,
+		)
+	}
+
 	outputDir, err := userprompt.InputDirectory(
 		cliCtx,
 		"Enter your desired output directory for your slashing protection history file",
