554204d746630ac5f5d0c906f94b1390d294932e
rkapka <rkapka@wp.pl>
2021-11-16 16:26:10 +0100

    bring back SetStateRoots

--- beacon-chain/state/phase0.go
+++ beacon-chain/state/phase0.go
@@ -148,6 +148,7 @@ type WriteOnlyBlockRoots interface {
 
 // WriteOnlyStateRoots defines a struct which only has write access to state roots methods.
 type WriteOnlyStateRoots interface {
+	SetStateRoots(val *[8192][32]byte) error
 	UpdateStateRootAtIndex(idx uint64, stateRoot [32]byte) error
 }
 
