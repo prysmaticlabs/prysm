816d96870f540030086ea8aecddd5b7c7b361415
terence tsao <terence@prysmaticlabs.com>
2021-11-17 08:43:49 -0800

    add deprecated setters & getters

---
+++ beacon-chain/state/v3/deprecated_getters.go
@@ -0,0 +1,16 @@
+package v3
+
+import (
+	"github.com/pkg/errors"
+	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+)
+
+// PreviousEpochAttestations is not supported for HF1 beacon state.
+func (b *BeaconState) PreviousEpochAttestations() ([]*ethpb.PendingAttestation, error) {
+	return nil, errors.New("PreviousEpochAttestations is not supported for hard fork 1 beacon state")
+}
+
+// CurrentEpochAttestations is not supported for HF1 beacon state.
+func (b *BeaconState) CurrentEpochAttestations() ([]*ethpb.PendingAttestation, error) {
+	return nil, errors.New("CurrentEpochAttestations is not supported for hard fork 1 beacon state")
+}
