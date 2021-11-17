d9953998189497c06a98536c8ed0c5a62fbf9b71
Raul Jordan <raul@prysmaticlabs.com>
2021-11-17 12:03:51 -0500

    base

--- beacon-chain/db/slasherkv/slasher.go
+++ beacon-chain/db/slasherkv/slasher.go
@@ -121,7 +121,7 @@ func (s *Store) CheckAttesterDoubleVotes(
 						continue
 					}
 					existingSigningRoot := bytesutil.ToBytes32(attRecordsKey[:signingRootSize])
-					fmt.Printf("On disk: Checking att signing root %#x vs existing signing root %#x\n", attToProcess.SigningRoot, existingSigningRoot)
+					log.Infof("On disk: Checking att signing root %#x vs existing signing root %#x\n", attToProcess.SigningRoot, existingSigningRoot)
 					if existingSigningRoot != attToProcess.SigningRoot {
 						existingAttRecord, err := decodeAttestationRecord(encExistingAttRecord)
 						if err != nil {
--- beacon-chain/slasher/detect_attestations.go
+++ beacon-chain/slasher/detect_attestations.go
@@ -148,7 +148,7 @@ func (s *Service) checkDoubleVotes(
 	defer span.End()
 	// We check if there are any slashable double votes in the input list
 	// of attestations with respect to each other.
-	fmt.Println("Checking double votes for atts", len(attestations))
+	log.Info("Checking double votes for atts", len(attestations))
 	slashings := make([]*ethpb.AttesterSlashing, 0)
 	existingAtts := make(map[string]*slashertypes.IndexedAttestationWrapper)
 	for _, att := range attestations {
@@ -159,7 +159,7 @@ func (s *Service) checkDoubleVotes(
 				existingAtts[key] = att
 				continue
 			}
-			fmt.Printf("Checking att signing root %#x vs existing signing root %#x\n", att.SigningRoot, existingAtt.SigningRoot)
+			log.Infof("Checking att signing root %#x vs existing signing root %#x\n", att.SigningRoot, existingAtt.SigningRoot)
 			if att.SigningRoot != existingAtt.SigningRoot {
 				doubleVotesTotal.Inc()
 				slashings = append(slashings, &ethpb.AttesterSlashing{
