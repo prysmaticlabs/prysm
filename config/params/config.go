5f42910562598b7e68238349d985aa7e528e0fc7
terence tsao <terence@prysmaticlabs.com>
2021-11-18 09:08:31 -0800

    Rename coinbase to fee recipient

--- config/params/config.go
+++ config/params/config.go
@@ -182,7 +182,7 @@ type BeaconChainConfig struct {
 	TerminalBlockHash                common.Hash    `yaml:"TERMINAL_BLOCK_HASH" spec:"true"`                  // TerminalBlockHash of beacon chain.
 	TerminalBlockHashActivationEpoch types.Epoch    `yaml:"TERMINAL_BLOCK_HASH_ACTIVATION_EPOCH" spec:"true"` // TerminalBlockHashActivationEpoch of beacon chain.
 	TerminalTotalDifficulty          uint64         `yaml:"TERMINAL_TOTAL_DIFFICULTY" spec:"true"`            // TerminalTotalDifficulty is part of the experimental merge spec. This value is type is currently TBD: https://github.com/ethereum/consensus-specs/blob/dev/specs/merge/beacon-chain.md#transition-settings
-	Coinbase                         common.Address // Coinbase where the transaction fee goes to.
+	FeeRecipient                     common.Address // FeeRecipient where the transaction fee goes to.
 }
 
 // InitializeForkSchedule initializes the schedules forks baked into the config.
