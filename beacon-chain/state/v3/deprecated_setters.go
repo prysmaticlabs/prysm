53637c4c92130d99c86bf27c939f34c309716a7b
terence tsao <terence@prysmaticlabs.com>
2021-11-22 07:52:46 -0800

    Rename error msgs

--- beacon-chain/state/v3/deprecated_setters.go
+++ beacon-chain/state/v3/deprecated_setters.go
@@ -7,25 +7,25 @@ import (
 
 // SetPreviousEpochAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) SetPreviousEpochAttestations(val []*ethpb.PendingAttestation) error {
-	return errors.New("SetPreviousEpochAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("SetPreviousEpochAttestations is not supported for version Merge beacon state")
 }
 
 // SetCurrentEpochAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) SetCurrentEpochAttestations(val []*ethpb.PendingAttestation) error {
-	return errors.New("SetCurrentEpochAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("SetCurrentEpochAttestations is not supported for version Merge beacon state")
 }
 
 // AppendCurrentEpochAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) AppendCurrentEpochAttestations(val *ethpb.PendingAttestation) error {
-	return errors.New("AppendCurrentEpochAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("AppendCurrentEpochAttestations is not supported for version Merge beacon state")
 }
 
 // AppendPreviousEpochAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) AppendPreviousEpochAttestations(val *ethpb.PendingAttestation) error {
-	return errors.New("AppendPreviousEpochAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("AppendPreviousEpochAttestations is not supported for version Merge beacon state")
 }
 
 // RotateAttestations is not supported for HF1 beacon state.
 func (b *BeaconState) RotateAttestations() error {
-	return errors.New("RotateAttestations is not supported for hard fork 1 beacon state")
+	return errors.New("RotateAttestations is not supported for version Merge beacon state")
 }
