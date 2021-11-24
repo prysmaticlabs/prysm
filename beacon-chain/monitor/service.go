a2c1185032d2de8f47e8823d96dc986be04f03da
Potuz <potuz@prysmaticlabs.com>
2021-11-23 22:56:34 -0300

    Monitor sync committee  (#9923)
    * Add sync committeee contributions to monitor
    
    * gaz
    
    * Raul's review
    
    * Added lock around TrackedValidators
    
    * add comment to trackedIndex
    
    * add missing locks because of trackedIndex
    
    * Terence fixes 2
    
    * moved TrackedValidator to service from config
    
    * Terence comment fix
    
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>

--- beacon-chain/monitor/service.go
+++ beacon-chain/monitor/service.go
@@ -1,8 +1,13 @@
 package monitor
 
 import (
+	"sync"
+
 	types "github.com/prysmaticlabs/eth2-types"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	"github.com/prysmaticlabs/prysm/beacon-chain/state"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stategen"
+	"github.com/prysmaticlabs/prysm/time/slots"
 )
 
 // ValidatorLatestPerformance keeps track of the latest participation of the validator
@@ -13,41 +18,71 @@ type ValidatorLatestPerformance struct {
 	timelyTarget  bool
 	timelyHead    bool
 	balance       uint64
-	balanceChange uint64
+	balanceChange int64
 }
 
 // ValidatorAggregatedPerformance keeps track of the accumulated performance of
 // the validator since launch
 type ValidatorAggregatedPerformance struct {
-	totalAttestedCount  uint64
-	totalRequestedCount uint64
-	totalDistance       uint64
-	totalCorrectSource  uint64
-	totalCorrectTarget  uint64
-	totalCorrectHead    uint64
-	totalProposedCount  uint64
-	totalAggregations   uint64
+	totalAttestedCount             uint64
+	totalRequestedCount            uint64
+	totalDistance                  uint64
+	totalCorrectSource             uint64
+	totalCorrectTarget             uint64
+	totalCorrectHead               uint64
+	totalProposedCount             uint64
+	totalAggregations              uint64
+	totalSyncComitteeContributions uint64
+	totalSyncComitteeAggregations  uint64
 }
 
 // ValidatorMonitorConfig contains the list of validator indices that the
 // monitor service tracks, as well as the event feed notifier that the
 // monitor needs to subscribe.
 type ValidatorMonitorConfig struct {
-	StateGen          stategen.StateManager
-	TrackedValidators map[types.ValidatorIndex]interface{}
+	StateGen stategen.StateManager
 }
 
 // Service is the main structure that tracks validators and reports logs and
 // metrics of their performances throughout their lifetime.
 type Service struct {
-	config                *ValidatorMonitorConfig
-	latestPerformance     map[types.ValidatorIndex]ValidatorLatestPerformance
-	aggregatedPerformance map[types.ValidatorIndex]ValidatorAggregatedPerformance
+	config *ValidatorMonitorConfig
+
+	// Locks access to TrackedValidators, latestPerformance, aggregatedPerformance,
+	// trackedSyncedCommitteeIndices and lastSyncedEpoch
+	sync.RWMutex
+
+	TrackedValidators           map[types.ValidatorIndex]interface{}
+	latestPerformance           map[types.ValidatorIndex]ValidatorLatestPerformance
+	aggregatedPerformance       map[types.ValidatorIndex]ValidatorAggregatedPerformance
+	trackedSyncCommitteeIndices map[types.ValidatorIndex][]types.CommitteeIndex
+	lastSyncedEpoch             types.Epoch
 }
 
 // TrackedIndex returns if the given validator index corresponds to one of the
-// validators we follow
-func (s *Service) TrackedIndex(idx types.ValidatorIndex) bool {
-	_, ok := s.config.TrackedValidators[idx]
+// validators we follow.
+// It assumes the caller holds the service Lock
+func (s *Service) trackedIndex(idx types.ValidatorIndex) bool {
+	_, ok := s.TrackedValidators[idx]
 	return ok
+}
+
+// updateSyncCommitteeTrackedVals updates the sync committee assignments of our
+// tracked validators. It gets called when we sync a block after the Sync Period changes.
+func (s *Service) updateSyncCommitteeTrackedVals(state state.BeaconState) {
+	s.Lock()
+	defer s.Unlock()
+	for idx := range s.TrackedValidators {
+		syncIdx, err := helpers.CurrentPeriodSyncSubcommitteeIndices(state, idx)
+		if err != nil {
+			log.WithError(err).WithField("ValidatorIndex", idx).Error(
+				"Sync committee assignments will not be reported")
+			delete(s.trackedSyncCommitteeIndices, idx)
+		} else if len(syncIdx) == 0 {
+			delete(s.trackedSyncCommitteeIndices, idx)
+		} else {
+			s.trackedSyncCommitteeIndices[idx] = syncIdx
+		}
+	}
+	s.lastSyncedEpoch = slots.ToEpoch(state.Slot())
 }
