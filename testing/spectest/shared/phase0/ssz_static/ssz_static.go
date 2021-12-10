c84c6ab5478b44eeef8d3278c3cd6d51f86ee05c
Antonio Sanso <antonio.sanso@gmail.com>
2021-12-10 22:18:57 +0100

    Add  Missing spec tests in sss_static.go (#10003)
    * Update ssz_static.go
    
    * Update ssz_static.go
    
    * Update testing/spectest/shared/phase0/ssz_static/ssz_static.go
    
    Co-authored-by: Preston Van Loon <preston@prysmaticlabs.com>
    
    * Add Missing spec tests in sss_static.go
    
    * Update testing/spectest/shared/altair/ssz_static/ssz_static.go
    
    Co-authored-by: Preston Van Loon <preston@prysmaticlabs.com>

--- testing/spectest/shared/phase0/ssz_static/ssz_static.go
+++ testing/spectest/shared/phase0/ssz_static/ssz_static.go
@@ -25,66 +25,70 @@ type SSZRoots struct {
 // RunSSZStaticTests executes "ssz_static" tests.
 func RunSSZStaticTests(t *testing.T, config string) {
 	require.NoError(t, utils.SetConfig(t, config))
-
 	testFolders, _ := utils.TestFolders(t, config, "phase0", "ssz_static")
 	for _, folder := range testFolders {
-		innerPath := path.Join("ssz_static", folder.Name(), "ssz_random")
-		innerTestFolders, innerTestsFolderPath := utils.TestFolders(t, config, "phase0", innerPath)
+		modePath := path.Join("ssz_static", folder.Name())
+		modeFolders, _ := utils.TestFolders(t, config, "phase0", modePath)
 
-		for _, innerFolder := range innerTestFolders {
-			t.Run(path.Join(folder.Name(), innerFolder.Name()), func(t *testing.T) {
-				serializedBytes, err := util.BazelFileBytes(innerTestsFolderPath, innerFolder.Name(), "serialized.ssz_snappy")
-				require.NoError(t, err)
-				serializedSSZ, err := snappy.Decode(nil /* dst */, serializedBytes)
-				require.NoError(t, err, "Failed to decompress")
-				object, err := UnmarshalledSSZ(t, serializedSSZ, folder.Name())
-				require.NoError(t, err, "Could not unmarshall serialized SSZ")
+		for _, modeFolder := range modeFolders {
+			innerPath := path.Join(modePath, modeFolder.Name())
+			innerTestFolders, innerTestsFolderPath := utils.TestFolders(t, config, "phase0", innerPath)
 
-				rootsYamlFile, err := util.BazelFileBytes(innerTestsFolderPath, innerFolder.Name(), "roots.yaml")
-				require.NoError(t, err)
-				rootsYaml := &SSZRoots{}
-				require.NoError(t, utils.UnmarshalYaml(rootsYamlFile, rootsYaml), "Failed to Unmarshal")
+			for _, innerFolder := range innerTestFolders {
+				t.Run(path.Join(modeFolder.Name(), folder.Name(), innerFolder.Name()), func(t *testing.T) {
+					serializedBytes, err := util.BazelFileBytes(innerTestsFolderPath, innerFolder.Name(), "serialized.ssz_snappy")
+					require.NoError(t, err)
+					serializedSSZ, err := snappy.Decode(nil /* dst */, serializedBytes)
+					require.NoError(t, err, "Failed to decompress")
+					object, err := UnmarshalledSSZ(t, serializedSSZ, folder.Name())
+					require.NoError(t, err, "Could not unmarshall serialized SSZ")
 
-				// Custom hash tree root for beacon state.
-				var htr func(interface{}) ([32]byte, error)
-				if _, ok := object.(*ethpb.BeaconState); ok {
-					htr = func(s interface{}) ([32]byte, error) {
-						beaconState, err := v1.InitializeFromProto(s.(*ethpb.BeaconState))
-						require.NoError(t, err)
-						return beaconState.HashTreeRoot(context.Background())
-					}
-				} else {
-					htr = func(s interface{}) ([32]byte, error) {
-						sszObj, ok := s.(fssz.HashRoot)
-						if !ok {
-							return [32]byte{}, errors.New("could not get hash root, not compatible object")
+					rootsYamlFile, err := util.BazelFileBytes(innerTestsFolderPath, innerFolder.Name(), "roots.yaml")
+					require.NoError(t, err)
+					rootsYaml := &SSZRoots{}
+					require.NoError(t, utils.UnmarshalYaml(rootsYamlFile, rootsYaml), "Failed to Unmarshal")
+
+					// Custom hash tree root for beacon state.
+					var htr func(interface{}) ([32]byte, error)
+					if _, ok := object.(*ethpb.BeaconState); ok {
+						htr = func(s interface{}) ([32]byte, error) {
+							beaconState, err := v1.InitializeFromProto(s.(*ethpb.BeaconState))
+							require.NoError(t, err)
+							return beaconState.HashTreeRoot(context.Background())
 						}
-						return sszObj.HashTreeRoot()
+					} else {
+						htr = func(s interface{}) ([32]byte, error) {
+							sszObj, ok := s.(fssz.HashRoot)
+							if !ok {
+								return [32]byte{}, errors.New("could not get hash root, not compatible object")
+							}
+							return sszObj.HashTreeRoot()
+						}
 					}
-				}
 
-				root, err := htr(object)
-				require.NoError(t, err)
-				rootBytes, err := hex.DecodeString(rootsYaml.Root[2:])
-				require.NoError(t, err)
-				require.DeepEqual(t, rootBytes, root[:], "Did not receive expected hash tree root")
+					root, err := htr(object)
+					require.NoError(t, err)
+					rootBytes, err := hex.DecodeString(rootsYaml.Root[2:])
+					require.NoError(t, err)
+					require.DeepEqual(t, rootBytes, root[:], "Did not receive expected hash tree root")
 
-				if rootsYaml.SigningRoot == "" {
-					return
-				}
+					if rootsYaml.SigningRoot == "" {
+						return
+					}
 
-				var signingRoot [32]byte
-				if v, ok := object.(fssz.HashRoot); ok {
-					signingRoot, err = v.HashTreeRoot()
-				} else {
-					t.Fatal("object does not meet fssz.HashRoot")
-				}
+					var signingRoot [32]byte
+					if v, ok := object.(fssz.HashRoot); ok {
+						signingRoot, err = v.HashTreeRoot()
+					} else {
+						t.Fatal("object does not meet fssz.HashRoot")
+					}
 
-				require.NoError(t, err)
-				signingRootBytes, err := hex.DecodeString(rootsYaml.SigningRoot[2:])
-				require.NoError(t, err)
-				require.DeepEqual(t, signingRootBytes, signingRoot[:], "Did not receive expected signing root")
-			})
+					require.NoError(t, err)
+					signingRootBytes, err := hex.DecodeString(rootsYaml.SigningRoot[2:])
+					require.NoError(t, err)
+					require.DeepEqual(t, signingRootBytes, signingRoot[:], "Did not receive expected signing root")
+				})
+			}
 		}
 	}
 }
