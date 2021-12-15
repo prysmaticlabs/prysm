f1c563235c7a02309a94df0b5f7a11f41ff2080a
terence tsao <terence@prysmaticlabs.com>
2021-12-15 07:20:24 -0800

    fix tests

--- proto/prysm/v1alpha1/block/block_utils_test.go
+++ proto/prysm/v1alpha1/block/block_utils_test.go
@@ -3,7 +3,7 @@ package block_test
 import (
 	"testing"
 
-	"github.com/prysmaticlabs/prysm/config/params"
+	fieldparams "github.com/prysmaticlabs/prysm/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
 	eth "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1/block"
