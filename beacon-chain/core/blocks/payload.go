13e50d02c28c187192b728010d4b968d82d3c5c7
terence tsao <terence@prysmaticlabs.com>
2021-12-18 06:42:21 -0800

    Add validate payload helpers

--- beacon-chain/core/blocks/payload.go
+++ beacon-chain/core/blocks/payload.go
@@ -3,9 +3,13 @@ package blocks
 import (
 	"bytes"
 
+	"github.com/pkg/errors"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/time"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state"
 	fieldparams "github.com/prysmaticlabs/prysm/config/fieldparams"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/time/slots"
 )
 
 // MergeComplete returns true if the transition to merge has completed.
@@ -21,6 +25,59 @@ func MergeComplete(st state.BeaconState) (bool, error) {
 	}
 
 	return !isEmptyHeader(h), nil
+}
+
+// ValidatePayloadWhenMergeCompletes validates if payload is valid versus input beacon state.
+// These validation steps ONLY apply to post merge.
+//
+// Spec code:
+//    # Verify consistency of the parent hash with respect to the previous execution payload header
+//    if is_merge_complete(state):
+//        assert payload.parent_hash == state.latest_execution_payload_header.block_hash
+func ValidatePayloadWhenMergeCompletes(st state.BeaconState, payload *ethpb.ExecutionPayload) error {
+	complete, err := MergeComplete(st)
+	if err != nil {
+		return err
+	}
+	if !complete {
+		return nil
+	}
+
+	header, err := st.LatestExecutionPayloadHeader()
+	if err != nil {
+		return err
+	}
+	if !bytes.Equal(payload.ParentHash, header.BlockHash) {
+		return errors.New("incorrect block hash")
+	}
+	return nil
+}
+
+// ValidatePayload validates if payload is valid versus input beacon state.
+// These validation steps apply to both pre merge and post merge.
+//
+// Spec code:
+//    # Verify random
+//    assert payload.random == get_randao_mix(state, get_current_epoch(state))
+//    # Verify timestamp
+//    assert payload.timestamp == compute_timestamp_at_slot(state, state.slot)
+func ValidatePayload(st state.BeaconState, payload *ethpb.ExecutionPayload) error {
+	random, err := helpers.RandaoMix(st, time.CurrentEpoch(st))
+	if err != nil {
+		return err
+	}
+
+	if !bytes.Equal(payload.Random, random) {
+		return errors.New("incorrect random")
+	}
+	t, err := slots.ToTime(st.GenesisTime(), st.Slot())
+	if err != nil {
+		return err
+	}
+	if payload.Timestamp != uint64(t.Unix()) {
+		return errors.New("incorrect timestamp")
+	}
+	return nil
 }
 
 func isEmptyHeader(h *ethpb.ExecutionPayloadHeader) bool {
--- beacon-chain/core/blocks/payload_test.go
+++ beacon-chain/core/blocks/payload_test.go
@@ -3,12 +3,16 @@ package blocks_test
 import (
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/time"
 	fieldparams "github.com/prysmaticlabs/prysm/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/testing/require"
 	"github.com/prysmaticlabs/prysm/testing/util"
+	"github.com/prysmaticlabs/prysm/time/slots"
 )
 
 func Test_MergeComplete(t *testing.T) {
@@ -162,6 +166,106 @@ func Test_MergeComplete(t *testing.T) {
 	}
 }
 
+func Test_ValidatePayloadWhenMergeCompletes(t *testing.T) {
+	tests := []struct {
+		name    string
+		payload *ethpb.ExecutionPayload
+		header  *ethpb.ExecutionPayloadHeader
+		err     error
+	}{
+		{
+			name:    "merge incomplete",
+			payload: emptyPayload(),
+			header:  emptyPayloadHeader(),
+			err:     nil,
+		},
+		{
+			name:    "validate passes",
+			payload: emptyPayload(),
+			header: func() *ethpb.ExecutionPayloadHeader {
+				h := emptyPayloadHeader()
+				h.ParentHash = bytesutil.PadTo([]byte{'a'}, fieldparams.RootLength)
+				return h
+			}(),
+			err: nil,
+		},
+		{
+			name: "incorrect blockhash",
+			payload: func() *ethpb.ExecutionPayload {
+				h := emptyPayload()
+				h.ParentHash = bytesutil.PadTo([]byte{'a'}, fieldparams.RootLength)
+				return h
+			}(),
+			header: func() *ethpb.ExecutionPayloadHeader {
+				h := emptyPayloadHeader()
+				h.ParentHash = bytesutil.PadTo([]byte{'a'}, fieldparams.RootLength)
+				return h
+			}(),
+			err: errors.New("incorrect block hash"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			st, _ := util.DeterministicGenesisStateMerge(t, 1)
+			require.NoError(t, st.SetLatestExecutionPayloadHeader(tt.header))
+			err := blocks.ValidatePayloadWhenMergeCompletes(st, tt.payload)
+			if err != nil {
+				require.Equal(t, tt.err.Error(), err.Error())
+			} else {
+				require.Equal(t, tt.err, err)
+			}
+		})
+	}
+}
+
+func Test_ValidatePayload(t *testing.T) {
+	st, _ := util.DeterministicGenesisStateMerge(t, 1)
+	random, err := helpers.RandaoMix(st, time.CurrentEpoch(st))
+	require.NoError(t, err)
+	ts, err := slots.ToTime(st.GenesisTime(), st.Slot())
+	require.NoError(t, err)
+	tests := []struct {
+		name    string
+		payload *ethpb.ExecutionPayload
+		err     error
+	}{
+		{
+			name: "validate passes",
+			payload: func() *ethpb.ExecutionPayload {
+				h := emptyPayload()
+				h.Random = random
+				h.Timestamp = uint64(ts.Unix())
+				return h
+			}(), err: nil,
+		},
+		{
+			name:    "incorrect random",
+			payload: emptyPayload(),
+			err:     errors.New("incorrect random"),
+		},
+		{
+			name: "incorrect timestamp",
+			payload: func() *ethpb.ExecutionPayload {
+				h := emptyPayload()
+				h.Random = random
+				h.Timestamp = 1
+				return h
+			}(),
+			err: errors.New("incorrect timestamp"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := blocks.ValidatePayload(st, tt.payload)
+			if err != nil {
+				require.Equal(t, tt.err.Error(), err.Error())
+			} else {
+				require.Equal(t, tt.err, err)
+			}
+		})
+	}
+}
+
 func BenchmarkMergeComplete(b *testing.B) {
 	st, _ := util.DeterministicGenesisStateMerge(b, 1)
 	require.NoError(b, st.SetLatestExecutionPayloadHeader(emptyPayloadHeader()))
@@ -184,5 +288,18 @@ func emptyPayloadHeader() *ethpb.ExecutionPayloadHeader {
 		BaseFeePerGas:    make([]byte, fieldparams.RootLength),
 		BlockHash:        make([]byte, fieldparams.RootLength),
 		TransactionsRoot: make([]byte, fieldparams.RootLength),
+	}
+}
+
+func emptyPayload() *ethpb.ExecutionPayload {
+	return &ethpb.ExecutionPayload{
+		ParentHash:    make([]byte, fieldparams.RootLength),
+		FeeRecipient:  make([]byte, fieldparams.FeeRecipientLength),
+		StateRoot:     make([]byte, fieldparams.RootLength),
+		ReceiptRoot:   make([]byte, fieldparams.RootLength),
+		LogsBloom:     make([]byte, fieldparams.LogsBloomLength),
+		Random:        make([]byte, fieldparams.RootLength),
+		BaseFeePerGas: make([]byte, fieldparams.RootLength),
+		BlockHash:     make([]byte, fieldparams.RootLength),
 	}
 }
