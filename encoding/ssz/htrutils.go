d9f0c6fb281d08ef2f46431f796ecaa469d007e9
terence tsao <terence@prysmaticlabs.com>
2021-12-19 09:58:54 -0800

    Add htr for txs

--- encoding/ssz/htrutils.go
+++ encoding/ssz/htrutils.go
@@ -90,3 +90,82 @@ func SlashingsRoot(slashings []uint64) ([32]byte, error) {
 	}
 	return BitwiseMerkleize(hash.CustomSHA256Hasher(), slashingChunks, uint64(len(slashingChunks)), uint64(len(slashingChunks)))
 }
+
+// TransactionsRoot computes the HTR for the Transactions' property of the ExecutionPayload
+// The code was largely copy/pasted from the code generated to compute the HTR of the entire
+// ExecutionPayload.
+func TransactionsRoot(txs [][]byte) ([32]byte, error) {
+	hasher := hash.CustomSHA256Hasher()
+	listMarshaling := make([][]byte, 0)
+	for i := 0; i < len(txs); i++ {
+		rt, err := transactionRoot(txs[i])
+		if err != nil {
+			return [32]byte{}, err
+		}
+		listMarshaling = append(listMarshaling, rt[:])
+	}
+
+	bytesRoot, err := BitwiseMerkleize(hasher, listMarshaling, uint64(len(listMarshaling)), fieldparams.MaxTxsPerPayloadLength)
+	if err != nil {
+		return [32]byte{}, errors.Wrap(err, "could not compute  merkleization")
+	}
+	bytesRootBuf := new(bytes.Buffer)
+	if err := binary.Write(bytesRootBuf, binary.LittleEndian, uint64(len(txs))); err != nil {
+		return [32]byte{}, errors.Wrap(err, "could not marshal length")
+	}
+	bytesRootBufRoot := make([]byte, 32)
+	copy(bytesRootBufRoot, bytesRootBuf.Bytes())
+	return MixInLength(bytesRoot, bytesRootBufRoot), nil
+}
+
+func transactionRoot(tx []byte) ([32]byte, error) {
+	hasher := hash.CustomSHA256Hasher()
+	chunkedRoots, err := packChunks(tx)
+	if err != nil {
+		return [32]byte{}, err
+	}
+
+	maxLength := (fieldparams.MaxBytesPerTxLength + 31) / 32
+	bytesRoot, err := BitwiseMerkleize(hasher, chunkedRoots, uint64(len(chunkedRoots)), uint64(maxLength))
+	if err != nil {
+		return [32]byte{}, errors.Wrap(err, "could not compute merkleization")
+	}
+	bytesRootBuf := new(bytes.Buffer)
+	if err := binary.Write(bytesRootBuf, binary.LittleEndian, uint64(len(tx))); err != nil {
+		return [32]byte{}, errors.Wrap(err, "could not marshal length")
+	}
+	bytesRootBufRoot := make([]byte, 32)
+	copy(bytesRootBufRoot, bytesRootBuf.Bytes())
+	return MixInLength(bytesRoot, bytesRootBufRoot), nil
+}
+
+// Pack a given byte array into chunks. It'll pad the last chunk with zero bytes if
+// it does not have length bytes per chunk.
+func packChunks(bytes []byte) ([][]byte, error) {
+	numItems := len(bytes)
+	var chunks [][]byte
+	for i := 0; i < numItems; i += 32 {
+		j := i + 32
+		// We create our upper bound index of the chunk, if it is greater than numItems,
+		// we set it as numItems itself.
+		if j > numItems {
+			j = numItems
+		}
+		// We create chunks from the list of items based on the
+		// indices determined above.
+		chunks = append(chunks, bytes[i:j])
+	}
+
+	if len(chunks) == 0 {
+		return chunks, nil
+	}
+
+	// Right-pad the last chunk with zero bytes if it does not
+	// have length bytes.
+	lastChunk := chunks[len(chunks)-1]
+	for len(lastChunk) < 32 {
+		lastChunk = append(lastChunk, 0)
+	}
+	chunks[len(chunks)-1] = lastChunk
+	return chunks, nil
+}
