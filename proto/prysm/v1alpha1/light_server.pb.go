b7c97fad4e96f9f9e7b4b3232b6009a8e8033b54
terence tsao <terence@prysmaticlabs.com>
2021-12-22 18:55:28 -0800

    Update light_server.pb.go

--- proto/prysm/v1alpha1/light_server.pb.go
+++ proto/prysm/v1alpha1/light_server.pb.go
@@ -7,6 +7,9 @@
 package eth
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/golang/protobuf/ptypes/empty"
 	github_com_prysmaticlabs_go_bitfield "github.com/prysmaticlabs/go-bitfield"
@@ -14,8 +17,6 @@ import (
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
