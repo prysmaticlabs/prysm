1be24d1da2887f6de7904bd6813b45695f356bfe
Raul Jordan <raul@prysmaticlabs.com>
2022-01-13 12:21:53 -0500

    boosting

---
+++ beacon-chain/forkchoice/protoarray/proposer_boost_test.go
@@ -0,0 +1,77 @@
+package protoarray
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/prysmaticlabs/prysm/config/params"
+	"github.com/prysmaticlabs/prysm/testing/require"
+)
+
+func TestForkChoice_BoostProposerRoot(t *testing.T) {
+	params.SetupTestConfigCleanup(t)
+	cfg := params.BeaconConfig()
+	cfg.SecondsPerSlot = 6
+	cfg.IntervalsPerSlot = 3
+	params.OverrideBeaconConfig(cfg)
+	ctx := context.Background()
+
+	t.Run("does not boost block from different slot", func(t *testing.T) {
+		f := &ForkChoice{
+			store: &Store{},
+		}
+		// Genesis set to 1 slot ago.
+		genesis := time.Now().Add(-time.Duration(cfg.SecondsPerSlot) * time.Second)
+		blockRoot := [32]byte{'A'}
+
+		// Trying to boost a block from slot 0 should not work.
+		err := f.BoostProposerRoot(ctx, 0 /* slot */, blockRoot, genesis)
+		require.NoError(t, err)
+		require.DeepEqual(t, [32]byte{}, f.store.proposerBoostRoot)
+	})
+	t.Run("does not boost untimely block from same slot", func(t *testing.T) {
+		f := &ForkChoice{
+			store: &Store{},
+		}
+		// Genesis set to 1 slot ago + X where X > attesting interval.
+		genesis := time.Now().Add(-time.Duration(cfg.SecondsPerSlot) * time.Second)
+		attestingInterval := time.Duration(cfg.SecondsPerSlot / cfg.IntervalsPerSlot)
+		greaterThanAttestingInterval := attestingInterval + 100*time.Millisecond
+		genesis = genesis.Add(-greaterThanAttestingInterval * time.Second)
+		blockRoot := [32]byte{'A'}
+
+		// Trying to boost a block from slot 1 that is untimely should not work.
+		err := f.BoostProposerRoot(ctx, 1 /* slot */, blockRoot, genesis)
+		require.NoError(t, err)
+		require.DeepEqual(t, [32]byte{}, f.store.proposerBoostRoot)
+	})
+	t.Run("boosts perfectly timely block from same slot", func(t *testing.T) {
+		f := &ForkChoice{
+			store: &Store{},
+		}
+		// Genesis set to 1 slot ago + 0 seconds into the attesting interval.
+		genesis := time.Now().Add(-time.Duration(cfg.SecondsPerSlot) * time.Second)
+		fmt.Println(genesis)
+		blockRoot := [32]byte{'A'}
+
+		err := f.BoostProposerRoot(ctx, 1 /* slot */, blockRoot, genesis)
+		require.NoError(t, err)
+		require.DeepEqual(t, [32]byte{'A'}, f.store.proposerBoostRoot)
+	})
+	t.Run("boosts timely block from same slot", func(t *testing.T) {
+		f := &ForkChoice{
+			store: &Store{},
+		}
+		// Genesis set to 1 slot ago + (attesting interval / 2).
+		genesis := time.Now().Add(-time.Duration(cfg.SecondsPerSlot) * time.Second)
+		blockRoot := [32]byte{'A'}
+		halfAttestingInterval := time.Second
+		genesis = genesis.Add(-halfAttestingInterval)
+
+		err := f.BoostProposerRoot(ctx, 1 /* slot */, blockRoot, genesis)
+		require.NoError(t, err)
+		require.DeepEqual(t, [32]byte{'A'}, f.store.proposerBoostRoot)
+	})
+}
