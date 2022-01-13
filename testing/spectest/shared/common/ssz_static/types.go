2bd85cc364488ea40d1ddc59d64ad254538da825
prestonvanloon <preston@prysmaticlabs.com>
2022-01-13 11:56:31 -0600

    godoc commentary

--- testing/spectest/shared/common/ssz_static/types.go
+++ testing/spectest/shared/common/ssz_static/types.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 )
 
+// HTR is the HashTreeRoot function signature.
 type HTR func(interface{}) ([32]byte, error)
 
 // SSZRoots is the format used to read spectest test data.
@@ -16,4 +17,6 @@ type SSZRoots struct {
 // serializedBytes. This method may call t.Skip if the type is not supported.
 type Unmarshaller func(t *testing.T, serializedBytes []byte, objectName string) (interface{}, error)
 
+// CustomHTRAdder adds any custom HTR methods for the given object. This method should return a HTR
+// slice with the custom HTR method applied.
 type CustomHTRAdder func(t *testing.T, htrs []HTR, object interface{}) []HTR
