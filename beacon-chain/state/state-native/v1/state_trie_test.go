e869e36f69d6cbffa364f5f1f0fd8cf47082bba3
rkapka <rkapka@wp.pl>
2022-01-18 10:48:18 +0100

    revert error messages

--- beacon-chain/state/state-native/v1/state_trie_test.go
+++ beacon-chain/state/state-native/v1/state_trie_test.go
@@ -170,7 +170,7 @@ func TestBeaconState_HashTreeRoot(t *testing.T) {
 			assert.DeepNotEqual(t, []byte{}, root[:], "Received empty hash tree root")
 			assert.DeepEqual(t, genericHTR[:], root[:], "Expected hash tree root to match generic")
 			if len(oldHTR) != 0 && bytes.Equal(root[:], oldHTR) {
-				t.Errorf("Expected HashTreeRoot to change, received %#x == old %#x", root, oldHTR)
+				t.Errorf("Expected HTR to change, received %#x == old %#x", root, oldHTR)
 			}
 			oldHTR = root[:]
 		})
@@ -239,7 +239,7 @@ func TestBeaconState_HashTreeRoot_FieldTrie(t *testing.T) {
 			assert.DeepNotEqual(t, []byte{}, root[:], "Received empty hash tree root")
 			assert.DeepEqual(t, genericHTR[:], root[:], "Expected hash tree root to match generic")
 			if len(oldHTR) != 0 && bytes.Equal(root[:], oldHTR) {
-				t.Errorf("Expected HashTreeRoot to change, received %#x == old %#x", root, oldHTR)
+				t.Errorf("Expected HTR to change, received %#x == old %#x", root, oldHTR)
 			}
 			oldHTR = root[:]
 		})
