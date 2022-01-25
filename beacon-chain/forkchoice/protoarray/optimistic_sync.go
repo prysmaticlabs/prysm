cc1e9481dedccba863d6ab2ad575ab43a0278a18
Potuz <potuz@prysmaticlabs.com>
2022-01-24 21:03:54 -0300

    Add ForkChoice.Optimistic()
    Adds the logic implementing whether a synced block is optimistic or not.

---
+++ beacon-chain/forkchoice/protoarray/optimistic_sync.go
@@ -0,0 +1,77 @@
+package protoarray
+
+import (
+	"fmt"
+
+	types "github.com/prysmaticlabs/eth2-types"
+	"github.com/prysmaticlabs/prysm/config/params"
+	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
+)
+
+// This returns the minimum and maximum slot of the synced_tips tree
+func (f *ForkChoice) boundarySyncedTips() (types.Slot, types.Slot) {
+	f.syncedTips.RLock()
+	defer f.syncedTips.RUnlock()
+
+	min := params.BeaconConfig().FarFutureSlot
+	max := types.Slot(0)
+	for _, slot := range f.syncedTips.validatedTips {
+		if slot > max {
+			max = slot
+		}
+		if slot < min {
+			min = slot
+		}
+	}
+	return min, max
+}
+
+// Optimistic returns true if this node is optimistically synced
+// WARNING: this function does not check if slot corresponds to the
+//          block with the given root. The caller is responsible for
+//          checking this. A consensus bug could be a cause of getting
+//          this wrong, so think twice before passing a wrong pair.
+func (f *ForkChoice) Optimistic(root [32]byte, slot types.Slot) (bool, error) {
+
+	f.syncedTips.RLock()
+	// If the node is a synced tip, then it's fully validated
+	_, ok := f.syncedTips.validatedTips[root]
+	if ok {
+		return false, nil
+	}
+	f.syncedTips.RUnlock()
+
+	// If the slot is higher than the max synced tip, it's optimistic
+	min, max := f.boundarySyncedTips()
+	if slot > max {
+		return true, nil
+	}
+
+	// If the slot is lower than the min synced tip, it's fully validated
+	if slot <= min {
+		return false, nil
+	}
+
+	// If we reached this point then the block has to be in the Fork Choice
+	// Store!
+	index, ok := f.store.nodesIndices[root]
+	if ok {
+		node := f.store.nodes[index]
+
+		// if the node is a leaf of the Fork Choice tree, then it's
+		// optimistic
+		childIndex := node.BestChild()
+		if childIndex == index {
+			return true, nil
+		}
+
+		// recurse to the child
+		child := f.store.nodes[childIndex]
+		root = child.root
+		slot = child.slot
+		return f.Optimistic(root, slot)
+	}
+	// This should not happen
+	return false, fmt.Errorf("invalid root, slot combination, got %#x, %d",
+		bytesutil.Trunc(root[:]), slot)
+}
