249422366359fde8218fd0fa8e43793f0ee8686d
Raul Jordan <raul@prysmaticlabs.com>
2022-01-26 15:43:40 -0600

    cfg test

--- config/params/loader_test.go
+++ config/params/loader_test.go
@@ -14,7 +14,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-var placeholderFields = []string{"PROPOSER_SCORE_BOOST", "UPDATE_TIMEOUT", "INTERVALS_PER_SLOT"}
+var placeholderFields = []string{"UPDATE_TIMEOUT", "INTERVALS_PER_SLOT"}
 
 func TestLoadConfigFileMainnet(t *testing.T) {
 	// See https://media.githubusercontent.com/media/ethereum/consensus-spec-tests/master/tests/minimal/config/phase0.yaml
