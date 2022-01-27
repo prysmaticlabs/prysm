024dd439c9404e0fd09212dfa9196355dbff1f67
Raul Jordan <raul@prysmaticlabs.com>
2022-01-27 14:58:49 -0500

    modify e2e config

--- config/params/testnet_e2e_config.go
+++ config/params/testnet_e2e_config.go
@@ -21,14 +21,15 @@ func E2ETestConfig() *BeaconChainConfig {
 	e2eConfig.ChurnLimitQuotient = 65536
 
 	// Time parameters.
-	e2eConfig.SecondsPerSlot = 10
-	e2eConfig.SlotsPerEpoch = 6
+	e2eConfig.SecondsPerSlot = 8
+	e2eConfig.SlotsPerEpoch = 8
 	e2eConfig.SqrRootSlotsPerEpoch = 2
 	e2eConfig.SecondsPerETH1Block = 2
 	e2eConfig.Eth1FollowDistance = 4
 	e2eConfig.EpochsPerEth1VotingPeriod = 2
 	e2eConfig.ShardCommitteePeriod = 4
 	e2eConfig.MaxSeedLookahead = 1
+	e2eConfig.IntervalsPerSlot = 2
 
 	// PoW parameters.
 	e2eConfig.DepositChainID = 1337   // Chain ID of eth1 dev net.
