5659bede37eec59e66d8dd47f0f461398a5c2986
James He <james@prysmaticlabs.com>
2022-02-03 14:10:16 -0500

    switching to table driven test

--- encoding/bytesutil/bytes_test.go
+++ encoding/bytesutil/bytes_test.go
@@ -535,11 +535,50 @@ func TestIsRoot(t *testing.T) {
 }
 
 func TestIsValidRoot(t *testing.T) {
-	input := make([]byte, fieldparams.RootLength)
-	output := bytesutil.IsValidRoot(input)
-	assert.Equal(t, false, output)
-	copy(input[2:], "a")
-	copy(input[3:], "b")
-	output = bytesutil.IsValidRoot(input)
-	assert.Equal(t, true, output)
+
+	zeroRoot := make([]byte, fieldparams.RootLength)
+
+	validRoot := make([]byte, fieldparams.RootLength)
+	validRoot[0] = 'a'
+
+	wrongLengthRoot := make([]byte, fieldparams.RootLength-4)
+	wrongLengthRoot[0] = 'a'
+
+	type args struct {
+		root []byte
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Is ZeroRoot",
+			args: args{
+				root: zeroRoot,
+			},
+			want: false,
+		},
+		{
+			name: "Is ValidRoot",
+			args: args{
+				root: validRoot,
+			},
+			want: true,
+		},
+		{
+			name: "Is NonZeroRoot but not length 32",
+			args: args{
+				root: wrongLengthRoot,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := bytesutil.IsValidRoot(tt.args.root)
+			require.Equal(t, got, tt.want)
+		})
+	}
 }
