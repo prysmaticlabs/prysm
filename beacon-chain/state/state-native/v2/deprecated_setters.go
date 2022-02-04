06c1ef83114791b976785295208742e0506f756b
rkapka <rkapka@wp.pl>
2022-02-04 15:41:13 +0100

    Revert "Auxiliary commit to revert individual files from 6bb528c2c5df2446ad18450009f63f44318d41a9"
    This reverts commit 5d5e6f2884d21caec7530c16ad2a0d0d27c44aa1.

--- beacon-chain/state/state-native/v2/deprecated_setters.go
+++ beacon-chain/state/state-native/v2/deprecated_setters.go
@@ -5,6 +5,16 @@ import (
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 )
 
+// SetPreviousEpochAttestations is not supported for HF1 beacon state.
+func (*BeaconState) SetPreviousEpochAttestations(_ []*ethpb.PendingAttestation) error {
+	return errors.New("SetPreviousEpochAttestations is not supported for hard fork 1 beacon state")
+}
+
+// SetCurrentEpochAttestations is not supported for HF1 beacon state.
+func (*BeaconState) SetCurrentEpochAttestations(_ []*ethpb.PendingAttestation) error {
+	return errors.New("SetCurrentEpochAttestations is not supported for hard fork 1 beacon state")
+}
+
 // AppendCurrentEpochAttestations is not supported for HF1 beacon state.
 func (*BeaconState) AppendCurrentEpochAttestations(_ *ethpb.PendingAttestation) error {
 	return errors.New("AppendCurrentEpochAttestations is not supported for hard fork 1 beacon state")
