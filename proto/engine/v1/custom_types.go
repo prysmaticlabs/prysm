54d9595dc08b10d0363c9f962b1b3d1cb9c391d6
Raul Jordan <raul@prysmaticlabs.com>
2022-02-04 11:55:34 -0500

    marshalers

--- proto/engine/v1/custom_types.go
+++ proto/engine/v1/custom_types.go
@@ -1,29 +1,37 @@
 package enginev1
 
-type BytesList [][]byte
+import (
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"strings"
+)
+
 type Bytes []byte
 type Quantity uint64
 
-func (b BytesList) MarshalJSON() ([]byte, error) {
-	return nil, nil
-}
-
-func (b BytesList) UnmarshalJSON(enc []byte) error {
-	return nil
-}
-
 func (b Bytes) MarshalJSON() ([]byte, error) {
-	return nil, nil
+	return []byte(fmt.Sprintf("%#x", b)), nil
 }
 
 func (b Bytes) UnmarshalJSON(enc []byte) error {
+	decoded, err := hex.DecodeString(strings.TrimPrefix(string(enc), "0x"))
+	if err != nil {
+		return err
+	}
+	b = decoded
 	return nil
 }
 
 func (q Quantity) MarshalJSON() ([]byte, error) {
-	return nil, nil
+	return []byte(fmt.Sprintf("%d", q)), nil
 }
 
 func (q Quantity) UnmarshalJSON(enc []byte) error {
+	decoded, err := hex.DecodeString(strings.TrimPrefix(string(enc), "0x"))
+	if err != nil {
+		return err
+	}
+	q = binary.LittleEndian.Uint64(decoded)
 	return nil
 }
