1f79b34d13afaaafa5fd6a0852bde40ba2b7b31a
Raul Jordan <raul@prysmaticlabs.com>
2022-02-04 13:24:36 -0500

    more alias magic

--- proto/engine/v1/json_marshal_unmarshal.go
+++ proto/engine/v1/json_marshal_unmarshal.go
@@ -1,8 +1,11 @@
 package enginev1
 
 import (
+	"encoding/binary"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"google.golang.org/protobuf/encoding/protojson"
 )
@@ -20,11 +23,13 @@ func (e *executionPayloadAlias) MarshalJSON() ([]byte, error) {
 // proper struct tag naming conventions required for the JSON-RPC engine API to work.
 func (e *ExecutionPayload) MarshalJSON() ([]byte, error) {
 	return json.Marshal(&struct {
-		LogsBloom string `json:"logsBloom"`
+		LogsBloom []byte `json:"logsBloom"`
 		*executionPayloadAlias
 	}{
-		LogsBloom:             fmt.Sprintf("%#x", e.LogsBloom),
-		executionPayloadAlias: (*executionPayloadAlias)(&executionPayloadAlias{ExecutionPayload: e}),
+		LogsBloom: hexBytes(e.LogsBloom),
+		executionPayloadAlias: (*executionPayloadAlias)(&executionPayloadAlias{
+			ExecutionPayload: e,
+		}),
 	})
 }
 
@@ -63,4 +68,35 @@ func (f *ForkchoiceState) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON --
 func (f *ForkchoiceState) UnmarshalJSON(enc []byte) error {
 	return protojson.Unmarshal(enc, f)
+}
+
+type hexBytes []byte
+type quantity uint64
+
+func (b hexBytes) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%#x", b)), nil
+}
+
+func (b hexBytes) UnmarshalJSON(enc []byte) error {
+	decoded, err := hex.DecodeString(strings.TrimPrefix(string(enc), "0x"))
+	if err != nil {
+		return err
+	}
+	b = decoded
+	return nil
+}
+
+func (q quantity) MarshalJSON() ([]byte, error) {
+	enc := make([]byte, 8)
+	binary.BigEndian.PutUint64(enc, uint64(q))
+	return enc, nil
+}
+
+func (q quantity) UnmarshalJSON(enc []byte) error {
+	decoded, err := hex.DecodeString(strings.TrimPrefix(string(enc), "0x"))
+	if err != nil {
+		return err
+	}
+	q = quantity(binary.BigEndian.Uint64(decoded))
+	return nil
 }
