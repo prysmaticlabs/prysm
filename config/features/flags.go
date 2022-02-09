8c0c1e386392fd23580aafa5f9e8ea66592c0470
nisdas <nishdas93@gmail.com>
2022-02-09 18:44:37 +0800

    add peer scorer to dev mode

--- config/features/flags.go
+++ config/features/flags.go
@@ -135,6 +135,7 @@ var (
 // devModeFlags holds list of flags that are set when development mode is on.
 var devModeFlags = []cli.Flag{
 	enableLargerGossipHistory,
+	enablePeerScorer,
 }
 
 // ValidatorFlags contains a list of all the feature flags that apply to the validator client.
