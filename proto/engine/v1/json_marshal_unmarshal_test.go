19782d25633806363d665bb46022a54f598c7015
terence tsao <terence@prysmaticlabs.com>
2022-02-15 11:00:58 -0800

    Use pointers

--- proto/engine/v1/json_marshal_unmarshal_test.go
+++ proto/engine/v1/json_marshal_unmarshal_test.go
@@ -30,11 +30,10 @@ func TestJsonMarshalUnmarshal(t *testing.T) {
 	})
 	t.Run("payload status", func(t *testing.T) {
 		hash := bytesutil.PadTo([]byte("hash"), fieldparams.RootLength)
-		errString := "failed validation"
 		jsonPayload := &enginev1.PayloadStatus{
 			Status:          enginev1.PayloadStatus_INVALID,
 			LatestValidHash: hash,
-			ValidationError: errString,
+			ValidationError: "failed validation",
 		}
 		enc, err := json.Marshal(jsonPayload)
 		require.NoError(t, err)
