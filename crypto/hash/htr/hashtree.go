95b3184731def34c22406090f1b863f37a8f8ec7
nisdas <nishdas93@gmail.com>
2022-02-16 21:34:32 +0800

    cleanup

--- crypto/hash/htr/hashtree.go
+++ crypto/hash/htr/hashtree.go
@@ -1,70 +1,19 @@
 package htr
 
-import "C"
 import (
-	"fmt"
-
 	"github.com/prysmaticlabs/gohashtree"
-	"github.com/prysmaticlabs/prysm/container/trie"
-	mathutil "github.com/prysmaticlabs/prysm/math"
 )
 
+// VectorizedSha256 takes a list of roots and hashes them using CPU
+// specific vector instructions. Depending on host machine's specific
+// hardware configuration, using this routine can lead to a significant
+// performance improvement compared to the default method of hashing
+// lists.
 func VectorizedSha256(arrayList [][32]byte) [][32]byte {
 	dList := make([][32]byte, len(arrayList)/2)
 	err := gohashtree.Hash(dList, arrayList)
 	if err != nil {
 		panic(err)
 	}
-	return dList
-}
-
-func SinglePassRoot(arrayList [][32]byte, depth int) [32]byte {
-	numOfElems := len(arrayList)
-	elemSum := 0
-	layerLen := numOfElems
-	for i := 0; i < depth; i++ {
-		oddNodeLength := layerLen%2 == 1
-		if oddNodeLength {
-			layerLen++
-		}
-		elemSum += layerLen
-		layerLen /= 2
-		layerLen = int(mathutil.Max(1, uint64(layerLen)))
-		if i+1 == depth {
-			elemSum++
-		}
-	}
-
-	resList := make([][32]byte, elemSum-numOfElems)
-	resList = append(arrayList, resList...)
-	if len(resList) == numOfElems {
-		panic(fmt.Sprintf("incorrect length: %d, %d,%d, %d", len(resList), numOfElems, elemSum, depth))
-	}
-	layerLen = numOfElems
-	currIdx := -1
-	for i := 0; i < depth; i++ {
-		currIdx += layerLen
-		oddNodeLength := layerLen%2 == 1
-		if oddNodeLength {
-			zerohash := trie.ZeroHashes[i]
-			layerLen++
-			currIdx++
-			resList[currIdx] = zerohash
-		}
-		layerLen /= 2
-		layerLen = int(mathutil.Max(1, uint64(layerLen)))
-		if i+1 == depth {
-			elemSum++
-		}
-	}
-	//sPtr := unsafe.Pointer(&resList[0])
-	//C.sha256_8_avx2((*C.uchar)(unsafe.Pointer(&resList[numOfElems])), (*C.uchar)(sPtr), C.ulong(len(resList)/2))
-	return resList[len(resList)-1]
-}
-
-func Hash2Chunks(arrayList [][32]byte) [32]byte {
-	//sPtr := unsafe.Pointer(&arrayList[0])
-	dList := [32]byte{}
-	//C.sha256_1_avx((*C.uchar)(unsafe.Pointer(&dList)), (*C.uchar)(sPtr), C.ulong(len(arrayList)/2))
 	return dList
 }
