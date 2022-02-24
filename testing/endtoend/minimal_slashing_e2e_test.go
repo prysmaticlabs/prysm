965a94ae6725e586a70a49ba786c4a66747fb06f
prestonvanloon <preston@prysmaticlabs.com>
2022-02-24 11:52:24 -0600

    Add slasher case for web3signer

--- testing/endtoend/minimal_slashing_e2e_test.go
+++ testing/endtoend/minimal_slashing_e2e_test.go
@@ -34,3 +34,29 @@ func TestEndToEnd_Slasher_MinimalConfig(t *testing.T) {
 
 	newTestRunner(t, testConfig).run()
 }
+
+func TestEndToEnd_Slasher_MinimalConfig_Web3Signer(t *testing.T) {
+	params.UseE2EConfig()
+	require.NoError(t, e2eParams.Init(e2eParams.StandardBeaconCount))
+
+	testConfig := &types.E2EConfig{
+		BeaconFlags: []string{
+			"--slasher",
+		},
+		ValidatorFlags: []string{},
+		EpochsToRun:    4,
+		TestSync:       false,
+		TestDeposits:   false,
+		Evaluators: []types.Evaluator{
+			ev.PeersConnect,
+			ev.HealthzCheck,
+			ev.ValidatorsSlashedAfterEpoch(4),
+			ev.SlashedValidatorsLoseBalanceAfterEpoch(4),
+			ev.InjectDoubleVoteOnEpoch(2),
+			ev.InjectDoubleBlockOnEpoch(2),
+		},
+		UseWeb3RemoteSigner: true,
+	}
+
+	newTestRunner(t, testConfig).run()
+}
