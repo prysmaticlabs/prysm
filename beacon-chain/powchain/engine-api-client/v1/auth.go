0282a27031f9e14fc3979a3c748b5fc5ff38d368
Raul Jordan <raul@prysmaticlabs.com>
2022-02-24 12:46:13 -0500

    update jwt libs

--- beacon-chain/powchain/engine-api-client/v1/auth.go
+++ beacon-chain/powchain/engine-api-client/v1/auth.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/pkg/errors"
 )
 
--- beacon-chain/powchain/engine-api-client/v1/auth_test.go
+++ beacon-chain/powchain/engine-api-client/v1/auth_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
 	"github.com/prysmaticlabs/prysm/testing/require"
 )
