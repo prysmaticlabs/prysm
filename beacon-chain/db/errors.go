238c5eb70cc01b58004812afee2b5f5ef3e46a94
Kasey Kirkham <kasey@users.noreply.github.com>
2022-03-01 13:51:07 -0600

    more comment improvements per Radek

--- beacon-chain/db/errors.go
+++ beacon-chain/db/errors.go
@@ -7,5 +7,7 @@ import "github.com/prysmaticlabs/prysm/beacon-chain/db/kv"
 // i/o error. This variable copies the value in the kv package to the same scope as the Database interfaces,
 // so that it is available to code paths that do not interact directly with the kv package.
 var ErrNotFound = kv.ErrNotFound
+// ErrNotFoundState wraps ErrNotFound for an error specific to a state not being found in the database.
 var ErrNotFoundState = kv.ErrNotFoundState
+// ErrNotFoundOriginBlockRoot wraps ErrNotFound for an error specific to the origin block root.
 var ErrNotFoundOriginBlockRoot = kv.ErrNotFoundOriginBlockRoot
