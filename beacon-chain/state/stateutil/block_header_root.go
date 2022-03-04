1af11885ee76422c3a8c1dcc19a683e15f1e972c
Nishant Das <nishdas93@gmail.com>
2022-03-04 23:19:07 +0800

    Remove Support for 2d-list Hashers (#10290)
    * add changes
    
    * fix logic bug
    
    * fix
    
    * potuz's review
    
    * Update beacon-chain/state/stateutil/eth1_root.go
    
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>
    
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>

--- beacon-chain/state/stateutil/block_header_root.go
+++ beacon-chain/state/stateutil/block_header_root.go
@@ -13,22 +13,22 @@ import (
 // a BeaconBlockHeader struct according to the Ethereum
 // Simple Serialize specification.
 func BlockHeaderRoot(header *ethpb.BeaconBlockHeader) ([32]byte, error) {
-	fieldRoots := make([][]byte, 5)
+	fieldRoots := make([][32]byte, 5)
 	if header != nil {
 		headerSlotBuf := make([]byte, 8)
 		binary.LittleEndian.PutUint64(headerSlotBuf, uint64(header.Slot))
 		headerSlotRoot := bytesutil.ToBytes32(headerSlotBuf)
-		fieldRoots[0] = headerSlotRoot[:]
+		fieldRoots[0] = headerSlotRoot
 		proposerIdxBuf := make([]byte, 8)
 		binary.LittleEndian.PutUint64(proposerIdxBuf, uint64(header.ProposerIndex))
 		proposerIndexRoot := bytesutil.ToBytes32(proposerIdxBuf)
-		fieldRoots[1] = proposerIndexRoot[:]
+		fieldRoots[1] = proposerIndexRoot
 		parentRoot := bytesutil.ToBytes32(header.ParentRoot)
-		fieldRoots[2] = parentRoot[:]
+		fieldRoots[2] = parentRoot
 		stateRoot := bytesutil.ToBytes32(header.StateRoot)
-		fieldRoots[3] = stateRoot[:]
+		fieldRoots[3] = stateRoot
 		bodyRoot := bytesutil.ToBytes32(header.BodyRoot)
-		fieldRoots[4] = bodyRoot[:]
+		fieldRoots[4] = bodyRoot
 	}
 	return ssz.BitwiseMerkleize(hash.CustomSHA256Hasher(), fieldRoots, uint64(len(fieldRoots)), uint64(len(fieldRoots)))
 }
