0e0f5989ef4272c02732b9745738de7328d95e2c
terence tsao <terence@prysmaticlabs.com>
2022-03-04 15:30:31 -0800

    Update doc

--- beacon-chain/forkchoice/doubly-linked-tree/doc.go
+++ beacon-chain/forkchoice/doubly-linked-tree/doc.go
@@ -1,4 +1,4 @@
 /*
-Package doublylinkedtree implements fork choice as doubly linked tree implementation
+Package doublylinkedtree implements eth2 LMD GHOST fork choice using the doubly linked proto array node structure.
 */
 package doublylinkedtree
