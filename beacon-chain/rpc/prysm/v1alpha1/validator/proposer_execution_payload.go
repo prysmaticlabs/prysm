40e5a5d796ade4c430761715c31e2cde33eb3d97
terence tsao <terence@prysmaticlabs.com>
2022-03-07 10:43:20 -0800

    RPC: add prepare execution payload (#10311)
    * Add prepare execution payload
    
    * Add prepare execution payload
    
    * Add prepare execution payload
    
    * Update client.go
    
    * Update proposer_execution_payload.go
    
    * Update proposer_execution_payload_test.go
    
    * Handle post bellatrix finalied blk
    
    * Update beacon-chain/rpc/prysm/v1alpha1/validator/proposer_execution_payload.go
    
    * Use BeaconBlockIsNil
    
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer_execution_payload.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer_execution_payload.go
@@ -8,10 +8,105 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/holiman/uint256"
 	"github.com/pkg/errors"
+	types "github.com/prysmaticlabs/eth2-types"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/blocks"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/time"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/transition"
+	fieldparams "github.com/prysmaticlabs/prysm/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/config/params"
 	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
+	enginev1 "github.com/prysmaticlabs/prysm/proto/engine/v1"
+	"github.com/prysmaticlabs/prysm/runtime/version"
+	"github.com/prysmaticlabs/prysm/time/slots"
 	"github.com/sirupsen/logrus"
 )
+
+// This returns the execution payload of a given slot. The function has full awareness of pre and post merge.
+// The payload is computed given the respected time of merge.
+func (vs *Server) getExecutionPayload(ctx context.Context, slot types.Slot) (*enginev1.ExecutionPayload, error) {
+	st, err := vs.HeadFetcher.HeadState(ctx)
+	if err != nil {
+		return nil, err
+	}
+	st, err = transition.ProcessSlotsIfPossible(ctx, st, slot)
+	if err != nil {
+		return nil, err
+	}
+
+	var parentHash []byte
+	var hasTerminalBlock bool
+	mergeComplete, err := blocks.MergeTransitionComplete(st)
+	if err != nil {
+		return nil, err
+	}
+
+	if mergeComplete {
+		header, err := st.LatestExecutionPayloadHeader()
+		if err != nil {
+			return nil, err
+		}
+		parentHash = header.BlockHash
+	} else {
+		if activationEpochNotReached(slot) {
+			return emptyPayload(), nil
+		}
+		parentHash, hasTerminalBlock, err = vs.getTerminalBlockHashIfExists(ctx)
+		if err != nil {
+			return nil, err
+		}
+		if !hasTerminalBlock {
+			return emptyPayload(), nil
+		}
+	}
+
+	t, err := slots.ToTime(st.GenesisTime(), slot)
+	if err != nil {
+		return nil, err
+	}
+	random, err := helpers.RandaoMix(st, time.CurrentEpoch(st))
+	if err != nil {
+		return nil, err
+	}
+
+	finalizedBlock, err := vs.BeaconDB.Block(ctx, bytesutil.ToBytes32(st.FinalizedCheckpoint().Root))
+	if err != nil {
+		return nil, err
+	}
+	if err := helpers.BeaconBlockIsNil(finalizedBlock); err != nil {
+		return nil, err
+	}
+	var finalizedBlockHash []byte
+	switch finalizedBlock.Version() {
+	case version.Phase0, version.Altair: // Blocks before Bellatrix don't have execution payloads. Use zeros as the hash.
+		finalizedBlockHash = params.BeaconConfig().ZeroHash[:]
+	default:
+		finalizedPayload, err := finalizedBlock.Block().Body().ExecutionPayload()
+		if err != nil {
+			return nil, err
+		}
+		finalizedBlockHash = finalizedPayload.BlockHash
+	}
+
+	f := &enginev1.ForkchoiceState{
+		HeadBlockHash:      parentHash,
+		SafeBlockHash:      parentHash,
+		FinalizedBlockHash: finalizedBlockHash,
+	}
+	p := &enginev1.PayloadAttributes{
+		Timestamp:             uint64(t.Unix()),
+		PrevRandao:            random,
+		SuggestedFeeRecipient: params.BeaconConfig().FeeRecipient.Bytes(),
+	}
+	payloadID, _, err := vs.ExecutionEngineCaller.ForkchoiceUpdated(ctx, f, p)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not prepare payload")
+	}
+	if payloadID == nil {
+		return nil, errors.New("nil payload id")
+	}
+	return vs.ExecutionEngineCaller.GetPayload(ctx, *payloadID)
+}
 
 // This returns the valid terminal block hash with an existence bool value.
 //
@@ -113,6 +208,20 @@ func (vs *Server) getPowBlockHashAtTerminalTotalDifficulty(ctx context.Context) 
 	}
 }
 
+// activationEpochNotReached returns true if activation epoch has not been reach.
+// Which satisfy the following conditions in spec:
+//        is_terminal_block_hash_set = TERMINAL_BLOCK_HASH != Hash32()
+//        is_activation_epoch_reached = get_current_epoch(state) >= TERMINAL_BLOCK_HASH_ACTIVATION_EPOCH
+//        if is_terminal_block_hash_set and not is_activation_epoch_reached:
+//      	return True
+func activationEpochNotReached(slot types.Slot) bool {
+	terminalBlockHashSet := bytesutil.ToBytes32(params.BeaconConfig().TerminalBlockHash.Bytes()) != [32]byte{}
+	if terminalBlockHashSet {
+		return params.BeaconConfig().TerminalBlockHashActivationEpoch > slots.ToEpoch(slot)
+	}
+	return false
+}
+
 func tDStringToUint256(td string) (*uint256.Int, error) {
 	b, err := hexutil.DecodeBig(td)
 	if err != nil {
@@ -123,4 +232,17 @@ func tDStringToUint256(td string) (*uint256.Int, error) {
 		return nil, errors.New("total difficulty overflowed")
 	}
 	return i, nil
+}
+
+func emptyPayload() *enginev1.ExecutionPayload {
+	return &enginev1.ExecutionPayload{
+		ParentHash:    make([]byte, fieldparams.RootLength),
+		FeeRecipient:  make([]byte, fieldparams.FeeRecipientLength),
+		StateRoot:     make([]byte, fieldparams.RootLength),
+		ReceiptsRoot:  make([]byte, fieldparams.RootLength),
+		LogsBloom:     make([]byte, fieldparams.LogsBloomLength),
+		PrevRandao:    make([]byte, fieldparams.RootLength),
+		BaseFeePerGas: make([]byte, fieldparams.RootLength),
+		BlockHash:     make([]byte, fieldparams.RootLength),
+	}
 }
