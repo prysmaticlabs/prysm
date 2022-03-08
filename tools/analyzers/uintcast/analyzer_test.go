d2dbc2a2f1749f3268922d4eb0c2dc99a372b1a9
prestonvanloon <preston@prysmaticlabs.com>
2022-03-08 11:52:47 -0600

    fix bazel build for go analysis runs

--- tools/analyzers/uintcast/analyzer_test.go
+++ tools/analyzers/uintcast/analyzer_test.go
@@ -3,10 +3,19 @@ package uintcast_test
 import (
 	"testing"
 
+	"github.com/prysmaticlabs/prysm/build/bazel"
 	"github.com/prysmaticlabs/prysm/tools/analyzers/uintcast"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
+func init() {
+	if bazel.BuiltWithBazel() {
+		bazel.SetGoEnv()
+	}
+}
+
 func TestAnalyzer(t *testing.T) {
-	analysistest.Run(t, analysistest.TestData(), uintcast.Analyzer)
+	testdata := bazel.TestDataPath(t)
+	analysistest.TestData = func() string { return testdata }
+	analysistest.Run(t, testdata, uintcast.Analyzer)
 }
