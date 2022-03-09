c11c549db1349cdfd6940fa7160410dd125467ce
terence tsao <terence@prysmaticlabs.com>
2022-03-09 08:56:02 -0800

    Add deprecation

--- beacon-chain/core/blocks/payload.go
+++ beacon-chain/core/blocks/payload.go
@@ -23,6 +23,8 @@ import (
 // Spec code:
 // def is_merge_transition_complete(state: BeaconState) -> bool:
 //    return state.latest_execution_payload_header != ExecutionPayloadHeader()
+//
+// DEPRECATED: Use `IsMergeTransitionBlockUsingPayloadHeader` instead.
 func MergeTransitionComplete(st state.BeaconState) (bool, error) {
 	h, err := st.LatestExecutionPayloadHeader()
 	if err != nil {
