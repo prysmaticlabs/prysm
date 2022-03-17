280fd696b6ad50a79c1d6716c93260ccc639f402
nisdas <nishdas93@gmail.com>
2022-03-17 09:16:36 +0800

    add in native state

--- beacon-chain/state/v1/state_fuzz_test.go
+++ beacon-chain/state/v1/state_fuzz_test.go
@@ -9,7 +9,9 @@ import (
 
 	types "github.com/prysmaticlabs/eth2-types"
 	coreState "github.com/prysmaticlabs/prysm/beacon-chain/core/transition"
+	v12 "github.com/prysmaticlabs/prysm/beacon-chain/state/state-native/v1"
 	v1 "github.com/prysmaticlabs/prysm/beacon-chain/state/v1"
+	"github.com/prysmaticlabs/prysm/crypto/rand"
 	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/testing/assert"
@@ -20,7 +22,10 @@ func FuzzV1StateHashTreeRoot(f *testing.F) {
 	gState, _ := util.DeterministicGenesisState(f, 100)
 	output, err := gState.MarshalSSZ()
 	assert.NoError(f, err)
-	f.Add(output[:100], uint64(10))
+	randPool := make([]byte, 100)
+	_, err = rand.NewDeterministicGenerator().Read(randPool)
+	assert.NoError(f, err)
+	f.Add(randPool, uint64(10))
 	f.Fuzz(func(t *testing.T, diffBuffer []byte, slotsToTransition uint64) {
 		stateSSZ := bytesutil.SafeCopyBytes(output)
 		for i := 0; i < len(diffBuffer); i += 9 {
@@ -37,6 +42,9 @@ func FuzzV1StateHashTreeRoot(f *testing.F) {
 		if err != nil {
 			return
 		}
+		nativeState, err := v12.InitializeFromProto(pbState)
+		assert.NoError(t, err)
+
 		slotsToTransition %= 100
 		stateObj, err := v1.InitializeFromProtoUnsafe(pbState)
 		assert.NoError(t, err)
@@ -44,6 +52,10 @@ func FuzzV1StateHashTreeRoot(f *testing.F) {
 			stateObj, err = coreState.ProcessSlots(context.Background(), stateObj, stateObj.Slot()+1)
 			assert.NoError(t, err)
 			stateObj.Copy()
+
+			nativeState, err = coreState.ProcessSlots(context.Background(), nativeState, nativeState.Slot()+1)
+			assert.NoError(t, err)
+			nativeState.Copy()
 		}
 		assert.NoError(t, err)
 		// Perform a cold HTR calculation by initializing a new state.
@@ -53,10 +65,13 @@ func FuzzV1StateHashTreeRoot(f *testing.F) {
 
 		newRt, newErr := newState.HashTreeRoot(context.Background())
 		rt, err := stateObj.HashTreeRoot(context.Background())
+		nativeRt, nativeErr := nativeState.HashTreeRoot(context.Background())
 
 		assert.Equal(t, newErr != nil, err != nil)
+		assert.Equal(t, newErr != nil, nativeErr != nil)
 		if err == nil {
 			assert.Equal(t, rt, newRt)
+			assert.Equal(t, rt, nativeRt)
 		}
 	})
 }
