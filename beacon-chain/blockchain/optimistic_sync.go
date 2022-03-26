f59aa60f7793f5d5b95bdeb3515ca41a36629998
terence tsao <terence@prysmaticlabs.com>
2022-03-26 13:21:43 -0700

    Fix payload cache

--- beacon-chain/blockchain/optimistic_sync.go
+++ beacon-chain/blockchain/optimistic_sync.go
@@ -211,7 +211,7 @@ func (s *Service) getPayloadAttribute(ctx context.Context, st state.BeaconState,
 	st = st.Copy()
 	st, err := transition.ProcessSlotsIfPossible(ctx, st, slot)
 	if err != nil {
-		return false, nil, 0, nil
+		return false, nil, 0, err
 	}
 	random, err := helpers.RandaoMix(st, time.CurrentEpoch(st))
 	if err != nil {
@@ -234,7 +234,7 @@ func (s *Service) getPayloadAttribute(ctx context.Context, st state.BeaconState,
 	}
 	t, err := slots.ToTime(uint64(s.genesisTime.Unix()), slot)
 	if err != nil {
-		return false, nil, 0, nil
+		return false, nil, 0, err
 	}
 	attr := &enginev1.PayloadAttributes{
 		Timestamp:             uint64(t.Unix()),
