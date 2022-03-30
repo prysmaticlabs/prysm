8b35846a20e2a4e5cadf72ff5bdacb021c6490d5
Raul Jordan <raul@prysmaticlabs.com>
2022-03-30 15:11:32 -0400

    lint

--- beacon-chain/powchain/service.go
+++ beacon-chain/powchain/service.go
@@ -73,8 +73,6 @@ var (
 	logPeriod = 1 * time.Minute
 	// threshold of how old we will accept an eth1 node's head to be.
 	eth1Threshold = 20 * time.Minute
-	// error when eth1 node is not synced.
-	errNotSynced = errors.New("eth1 node is still syncing")
 	// error when eth1 node is too far behind.
 	errFarBehind = errors.Errorf("eth1 head is more than %s behind from current wall clock time", eth1Threshold.String())
 )
