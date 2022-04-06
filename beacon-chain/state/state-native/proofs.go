241a98f9718459b420c511f14b80f852bfb9a6b7
rkapka <rkapka@wp.pl>
2022-04-06 19:16:16 +0200

    use correct field num in FinalizedRootProof

--- beacon-chain/state/state-native/proofs.go
+++ beacon-chain/state/state-native/proofs.go
@@ -49,7 +49,7 @@ func (b *BeaconState) FinalizedRootProof(ctx context.Context) ([][]byte, error) 
 	epochRoot := bytesutil.ToBytes32(epochBuf)
 	proof := make([][]byte, 0)
 	proof = append(proof, epochRoot[:])
-	branch := fieldtrie.ProofFromMerkleLayers(b.merkleLayers, int(v0types.FinalizedCheckpoint))
+	branch := fieldtrie.ProofFromMerkleLayers(b.merkleLayers, b.fieldIndexesRev[v0types.FinalizedCheckpoint])
 	proof = append(proof, branch...)
 	return proof, nil
 }
