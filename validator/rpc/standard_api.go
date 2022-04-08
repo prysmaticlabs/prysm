58efca770fecdc9947b9bad371ed2fbac3b036e6
james-prysm <90280386+james-prysm@users.noreply.github.com>
2022-04-08 13:45:42 -0500

    Update validator/rpc/standard_api.go
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- validator/rpc/standard_api.go
+++ validator/rpc/standard_api.go
@@ -262,7 +262,7 @@ func (s *Server) ListRemoteKeys(ctx context.Context, _ *empty.Empty) (*ethpbserv
 	}
 	km, err := s.validatorService.Keymanager()
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Could not get Prysm keymanager (possibly due beacon node unavailable): %v", err)
+		return nil, status.Errorf(codes.Internal, "Could not get Prysm keymanager (possibly due to beacon node unavailable): %v", err)
 	}
 	if s.wallet.KeymanagerKind() != keymanager.Web3Signer {
 		return nil, status.Errorf(codes.FailedPrecondition, "Prysm Wallet is not of type Web3Signer. Please execute validator client with web3signer flags.")
