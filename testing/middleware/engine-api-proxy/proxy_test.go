83fccc4e86997db6236eca11e3b492195215aa96
Raul Jordan <raul@prysmaticlabs.com>
2022-04-15 13:58:10 -0400

    all tests in

--- testing/middleware/engine-api-proxy/proxy_test.go
+++ testing/middleware/engine-api-proxy/proxy_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rpc"
 	pb "github.com/prysmaticlabs/prysm/proto/engine/v1"
 	"github.com/prysmaticlabs/prysm/testing/require"
@@ -77,35 +78,206 @@ func TestProxy(t *testing.T) {
 
 func TestProxy_CustomInterceptors(t *testing.T) {
 	t.Run("only intercepts engine API methods", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
+		type syncingResponse struct {
+			Syncing bool `json:"syncing"`
+		}
+
+		wantDestinationResponse := &syncingResponse{Syncing: true}
+		srv := destinationServerSetup(t, wantDestinationResponse)
+		defer srv.Close()
+
+		// Destination address server responds to JSON-RPC requests.
+		proxy, err := New(
+			WithDestinationAddress(srv.URL),
+		)
+		require.NoError(t, err)
+		go func() {
+			if err := proxy.Start(ctx); err != nil {
+				t.Log(err)
+			}
+		}()
+
+		method := "eth_syncing"
+
+		// RPC method to intercept.
+		proxy.AddRequestInterceptor(
+			method,
+			&syncingResponse{Syncing: false}, // Custom response.
+			func() bool {
+				return true // Always intercept with a custom response.
+			},
+		)
+
+		// Dials the proxy.
+		rpcClient, err := rpc.DialHTTP("http://" + proxy.Address())
+		require.NoError(t, err)
+
+		// Expect the result from the proxy is the same as that one returned
+		// by the destination address.
+		proxyResult := &syncingResponse{}
+		err = rpcClient.CallContext(ctx, proxyResult, method)
+		require.NoError(t, err)
+
+		// The interception SHOULD NOT work because we should only intercept `engine` namespace methods.
+		require.DeepEqual(t, wantDestinationResponse, proxyResult)
 	})
 	t.Run("only intercepts if trigger function returns true", func(t *testing.T) {
-		//proxyNode.AddInterceptor(func(reqBytes []byte, w http.ResponseWriter, r *http.Request) bool {
-		//	currSlot := slots.CurrentSlot(uint64(genesis.GenesisTime.AsTime().Unix()))
-		//	if currSlot < params.BeaconConfig().SlotsPerEpoch.Mul(uint64(9)) {
-		//		return false
-		//	}
-		//	if currSlot >= params.BeaconConfig().SlotsPerEpoch.Mul(uint64(10)) {
-		//		return false
-		//	}
-		//	return proxyNode.SyncingInterceptor()(reqBytes, w, r)
-		//})
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		type engineResponse struct {
+			BlockHash common.Hash `json:"blockHash"`
+		}
+
+		destinationResponse := &engineResponse{BlockHash: common.BytesToHash([]byte("foo"))}
+		srv := destinationServerSetup(t, destinationResponse)
+		defer srv.Close()
+
+		// Destination address server responds to JSON-RPC requests.
+		proxy, err := New(
+			WithDestinationAddress(srv.URL),
+		)
+		require.NoError(t, err)
+		go func() {
+			if err := proxy.Start(ctx); err != nil {
+				t.Log(err)
+			}
+		}()
+
+		method := "engine_newPayloadV1"
+
+		// RPC method to intercept.
+		wantInterceptedResponse := &engineResponse{BlockHash: common.BytesToHash([]byte("bar"))}
+		conditional := false
+		proxy.AddRequestInterceptor(
+			method,
+			wantInterceptedResponse,
+			func() bool {
+				return conditional // Conditional trigger.
+			},
+		)
+
+		// Dials the proxy.
+		rpcClient, err := rpc.DialHTTP("http://" + proxy.Address())
+		require.NoError(t, err)
+
+		proxyResult := &engineResponse{}
+		err = rpcClient.CallContext(ctx, proxyResult, method)
+		require.NoError(t, err)
+
+		// The interception SHOULD NOT work because we should only intercept if trigger conditional is true.
+		require.DeepEqual(t, destinationResponse, proxyResult)
+
+		conditional = true
+		proxy.AddRequestInterceptor(
+			method,
+			wantInterceptedResponse,
+			func() bool {
+				return conditional // Conditional trigger.
+			},
+		)
+
+		// The interception should work and we should not be getting the destination
+		// response but rather a custom response from the interceptor config now that the trigger is true.
+		proxyResult = &engineResponse{}
+		err = rpcClient.CallContext(ctx, proxyResult, method)
+		require.NoError(t, err)
+		require.DeepEqual(t, wantInterceptedResponse, proxyResult)
 	})
 	t.Run("triggers interceptor response correctly", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
+		type engineResponse struct {
+			BlockHash common.Hash `json:"blockHash"`
+		}
+
+		destinationResponse := &engineResponse{BlockHash: common.BytesToHash([]byte("foo"))}
+		srv := destinationServerSetup(t, destinationResponse)
+		defer srv.Close()
+
+		// Destination address server responds to JSON-RPC requests.
+		proxy, err := New(
+			WithDestinationAddress(srv.URL),
+		)
+		require.NoError(t, err)
+		go func() {
+			if err := proxy.Start(ctx); err != nil {
+				t.Log(err)
+			}
+		}()
+
+		method := "engine_newPayloadV1"
+
+		// RPC method to intercept.
+		wantInterceptedResponse := &engineResponse{BlockHash: common.BytesToHash([]byte("bar"))}
+		proxy.AddRequestInterceptor(
+			method,
+			wantInterceptedResponse,
+			func() bool {
+				return true // Always intercept with a custom response.
+			},
+		)
+
+		// Dials the proxy.
+		rpcClient, err := rpc.DialHTTP("http://" + proxy.Address())
+		require.NoError(t, err)
+
+		proxyResult := &engineResponse{}
+		err = rpcClient.CallContext(ctx, proxyResult, method)
+		require.NoError(t, err)
+
+		// The interception should work and we should not be getting the destination
+		// response but rather a custom response from the interceptor config.
+		require.DeepEqual(t, wantInterceptedResponse, proxyResult)
 	})
 }
 
 func Test_isEngineAPICall(t *testing.T) {
-	t.Run("naked array returns false", func(t *testing.T) {
-
-	})
-	t.Run("non-engine call returns false", func(t *testing.T) {
-
-	})
-	t.Run("engine call returns true", func(t *testing.T) {
-
-	})
+	type args struct {
+		reqBytes []byte
+	}
+	tests := []struct {
+		name string
+		args *jsonRPCObject
+		want bool
+	}{
+		{
+			name: "nil data",
+			args: nil,
+			want: false,
+		},
+		{
+			name: "engine method",
+			args: &jsonRPCObject{
+				Method: "engine_newPayloadV1",
+				ID:     1,
+				Result: 5,
+			},
+			want: true,
+		},
+		{
+			name: "non-engine method",
+			args: &jsonRPCObject{
+				Method: "eth_syncing",
+				ID:     1,
+				Result: false,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			enc, err := json.Marshal(tt.args)
+			require.NoError(t, err)
+			if got := isEngineAPICall(enc); got != tt.want {
+				t.Errorf("isEngineAPICall() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
 func destinationServerSetup(t *testing.T, response interface{}) *httptest.Server {
