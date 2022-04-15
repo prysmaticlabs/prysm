68b1e2c4b959cdbf46076c6bd1713fb686953b47
Raul Jordan <raul@prysmaticlabs.com>
2022-04-15 12:21:16 -0400

    defines tests

--- testing/middleware/engine-api-proxy/proxy_test.go
+++ testing/middleware/engine-api-proxy/proxy_test.go
@@ -1,1 +1,36 @@
 package proxy
+
+import "testing"
+
+func TestProxy(t *testing.T) {
+	t.Run("fails to proxy if destination is down", func(t *testing.T) {
+
+	})
+	t.Run("properly proxies request/response", func(t *testing.T) {
+
+	})
+}
+
+func TestProxy_CustomInterceptors(t *testing.T) {
+	t.Run("only intercepts engine API methods", func(t *testing.T) {
+
+	})
+	t.Run("only intercepts if trigger function returns true", func(t *testing.T) {
+
+	})
+	t.Run("triggers interceptor response correctly", func(t *testing.T) {
+
+	})
+}
+
+func Test_isEngineAPICall(t *testing.T) {
+	t.Run("naked array returns false", func(t *testing.T) {
+
+	})
+	t.Run("non-engine call returns false", func(t *testing.T) {
+
+	})
+	t.Run("engine call returns true", func(t *testing.T) {
+
+	})
+}
