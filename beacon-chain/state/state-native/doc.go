73d254e5cc680425b13bf745e56ab35d5ef90123
rkapka <rkapka@wp.pl>
2022-04-26 15:03:04 +0200

    update doc.go

--- beacon-chain/state/state-native/doc.go
+++ beacon-chain/state/state-native/doc.go
@@ -1,22 +1,15 @@
-// Package v1 defines how the beacon chain state for Ethereum
+// Package state_native defines how the beacon chain state for Ethereum
 // functions in the running beacon node, using an advanced,
 // immutable implementation of the state data structure.
 //
 // BeaconState getters may be accessed from inside or outside the package. To
 // avoid duplicating locks, we have internal and external versions of the
-// getter The external function carries out the short-circuit conditions,
-// obtains a read lock, then calls the internal function.  The internal function
-// carries out the short-circuit conditions and returns the required data
-// without further locking, allowing it to be used by other package-level
-// functions that already hold a lock. Hence the functions look something
-// like this:
+// getter. The external function obtains a read lock, then calls the internal function.
+// The internal function returns the required data without further locking,
+// allowing it to be used by other package-level functions that already hold a lock.
+// Hence the functions look something like this:
 //
 //  func (b *BeaconState) Foo() uint64 {
-//    // Short-circuit conditions.
-//    if !b.hasInnerState() {
-//      return 0
-//    }
-//
 //    // Read lock.
 //    b.lock.RLock()
 //    defer b.lock.RUnlock()
@@ -26,12 +19,9 @@
 //  }
 //
 //  func (b *BeaconState) foo() uint64 {
-//    // Short-circuit conditions.
-//    if !b.hasInnerState() {
-//      return 0
-//    }
+//    (...) // Some processing logic.
 //
-//    return b.state.foo
+//    return b.foo
 //  }
 //
 // Although it is technically possible to remove the short-circuit conditions
