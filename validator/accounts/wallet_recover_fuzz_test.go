a1439f82b3850b3d7f38cb36c35b4af71748c7fd
Raul Jordan <raul@prysmaticlabs.com>
2022-04-28 13:17:56 -0400

    builds

--- validator/accounts/wallet_recover_fuzz_test.go
+++ validator/accounts/wallet_recover_fuzz_test.go
@@ -14,6 +14,8 @@ func FuzzValidateMnemonic(f *testing.F) {
 	f.Add("bag wagon luxury iron swarm yellow course merge trumpet wide cash idea disagree deny teach canvas profit iron sting warfare slide")
 	f.Add("bag wagon luxury iron swarm yellow course merge trumpet cash wide decide profit cash idea disagree deny teach canvas profit slice beach iron sting warfare slide")
 	f.Fuzz(func(t *testing.T, input string) {
-		accounts.ValidateMnemonic(input)
+		if err := accounts.ValidateMnemonic(input); err != nil {
+			t.Error(err)
+		}
 	})
 }
