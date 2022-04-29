e94690da5e53964212a0a387ac08d17548af61ac
rkapka <rkapka@wp.pl>
2022-04-29 13:03:29 +0200

    versioning of ToProto

--- beacon-chain/state/state-native/getters_state.go
+++ beacon-chain/state/state-native/getters_state.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/pkg/errors"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/runtime/version"
 )
 
 // ToProtoUnsafe returns the pointer value of the underlying
@@ -16,28 +17,88 @@ func (b *BeaconState) ToProtoUnsafe() interface{} {
 
 	gvrCopy := b.genesisValidatorsRoot
 
-	return &ethpb.BeaconState{
-		GenesisTime:                 b.genesisTime,
-		GenesisValidatorsRoot:       gvrCopy[:],
-		Slot:                        b.slot,
-		Fork:                        b.fork,
-		LatestBlockHeader:           b.latestBlockHeader,
-		BlockRoots:                  b.blockRoots.Slice(),
-		StateRoots:                  b.stateRoots.Slice(),
-		HistoricalRoots:             b.historicalRoots.Slice(),
-		Eth1Data:                    b.eth1Data,
-		Eth1DataVotes:               b.eth1DataVotes,
-		Eth1DepositIndex:            b.eth1DepositIndex,
-		Validators:                  b.validators,
-		Balances:                    b.balances,
-		RandaoMixes:                 b.randaoMixes.Slice(),
-		Slashings:                   b.slashings,
-		PreviousEpochAttestations:   b.previousEpochAttestations,
-		CurrentEpochAttestations:    b.currentEpochAttestations,
-		JustificationBits:           b.justificationBits,
-		PreviousJustifiedCheckpoint: b.previousJustifiedCheckpoint,
-		CurrentJustifiedCheckpoint:  b.currentJustifiedCheckpoint,
-		FinalizedCheckpoint:         b.finalizedCheckpoint,
+	switch b.version {
+	case version.Phase0:
+		return &ethpb.BeaconState{
+			GenesisTime:                 b.genesisTime,
+			GenesisValidatorsRoot:       gvrCopy[:],
+			Slot:                        b.slot,
+			Fork:                        b.fork,
+			LatestBlockHeader:           b.latestBlockHeader,
+			BlockRoots:                  b.blockRoots.Slice(),
+			StateRoots:                  b.stateRoots.Slice(),
+			HistoricalRoots:             b.historicalRoots.Slice(),
+			Eth1Data:                    b.eth1Data,
+			Eth1DataVotes:               b.eth1DataVotes,
+			Eth1DepositIndex:            b.eth1DepositIndex,
+			Validators:                  b.validators,
+			Balances:                    b.balances,
+			RandaoMixes:                 b.randaoMixes.Slice(),
+			Slashings:                   b.slashings,
+			PreviousEpochAttestations:   b.previousEpochAttestations,
+			CurrentEpochAttestations:    b.currentEpochAttestations,
+			JustificationBits:           b.justificationBits,
+			PreviousJustifiedCheckpoint: b.previousJustifiedCheckpoint,
+			CurrentJustifiedCheckpoint:  b.currentJustifiedCheckpoint,
+			FinalizedCheckpoint:         b.finalizedCheckpoint,
+		}
+	case version.Altair:
+		return &ethpb.BeaconStateAltair{
+			GenesisTime:                 b.genesisTime,
+			GenesisValidatorsRoot:       gvrCopy[:],
+			Slot:                        b.slot,
+			Fork:                        b.fork,
+			LatestBlockHeader:           b.latestBlockHeader,
+			BlockRoots:                  b.blockRoots.Slice(),
+			StateRoots:                  b.stateRoots.Slice(),
+			HistoricalRoots:             b.historicalRoots.Slice(),
+			Eth1Data:                    b.eth1Data,
+			Eth1DataVotes:               b.eth1DataVotes,
+			Eth1DepositIndex:            b.eth1DepositIndex,
+			Validators:                  b.validators,
+			Balances:                    b.balances,
+			RandaoMixes:                 b.randaoMixes.Slice(),
+			Slashings:                   b.slashings,
+			PreviousEpochParticipation:  b.previousEpochParticipation,
+			CurrentEpochParticipation:   b.currentEpochParticipation,
+			JustificationBits:           b.justificationBits,
+			PreviousJustifiedCheckpoint: b.previousJustifiedCheckpoint,
+			CurrentJustifiedCheckpoint:  b.currentJustifiedCheckpoint,
+			FinalizedCheckpoint:         b.finalizedCheckpoint,
+			InactivityScores:            b.inactivityScores,
+			CurrentSyncCommittee:        b.currentSyncCommittee,
+			NextSyncCommittee:           b.nextSyncCommittee,
+		}
+	case version.Bellatrix:
+		return &ethpb.BeaconStateBellatrix{
+			GenesisTime:                  b.genesisTime,
+			GenesisValidatorsRoot:        gvrCopy[:],
+			Slot:                         b.slot,
+			Fork:                         b.fork,
+			LatestBlockHeader:            b.latestBlockHeader,
+			BlockRoots:                   b.blockRoots.Slice(),
+			StateRoots:                   b.stateRoots.Slice(),
+			HistoricalRoots:              b.historicalRoots.Slice(),
+			Eth1Data:                     b.eth1Data,
+			Eth1DataVotes:                b.eth1DataVotes,
+			Eth1DepositIndex:             b.eth1DepositIndex,
+			Validators:                   b.validators,
+			Balances:                     b.balances,
+			RandaoMixes:                  b.randaoMixes.Slice(),
+			Slashings:                    b.slashings,
+			PreviousEpochParticipation:   b.previousEpochParticipation,
+			CurrentEpochParticipation:    b.currentEpochParticipation,
+			JustificationBits:            b.justificationBits,
+			PreviousJustifiedCheckpoint:  b.previousJustifiedCheckpoint,
+			CurrentJustifiedCheckpoint:   b.currentJustifiedCheckpoint,
+			FinalizedCheckpoint:          b.finalizedCheckpoint,
+			InactivityScores:             b.inactivityScores,
+			CurrentSyncCommittee:         b.currentSyncCommittee,
+			NextSyncCommittee:            b.nextSyncCommittee,
+			LatestExecutionPayloadHeader: b.latestExecutionPayloadHeader,
+		}
+	default:
+		return nil
 	}
 }
 
@@ -52,28 +113,88 @@ func (b *BeaconState) ToProto() interface{} {
 
 	gvrCopy := b.genesisValidatorsRoot
 
-	return &ethpb.BeaconState{
-		GenesisTime:                 b.genesisTime,
-		GenesisValidatorsRoot:       gvrCopy[:],
-		Slot:                        b.slot,
-		Fork:                        b.forkVal(),
-		LatestBlockHeader:           b.latestBlockHeaderVal(),
-		BlockRoots:                  b.blockRoots.Slice(),
-		StateRoots:                  b.stateRoots.Slice(),
-		HistoricalRoots:             b.historicalRoots.Slice(),
-		Eth1Data:                    b.eth1DataVal(),
-		Eth1DataVotes:               b.eth1DataVotesVal(),
-		Eth1DepositIndex:            b.eth1DepositIndex,
-		Validators:                  b.validatorsVal(),
-		Balances:                    b.balancesVal(),
-		RandaoMixes:                 b.randaoMixes.Slice(),
-		Slashings:                   b.slashingsVal(),
-		PreviousEpochAttestations:   b.previousEpochAttestationsVal(),
-		CurrentEpochAttestations:    b.currentEpochAttestationsVal(),
-		JustificationBits:           b.justificationBitsVal(),
-		PreviousJustifiedCheckpoint: b.previousJustifiedCheckpointVal(),
-		CurrentJustifiedCheckpoint:  b.currentJustifiedCheckpointVal(),
-		FinalizedCheckpoint:         b.finalizedCheckpointVal(),
+	switch b.version {
+	case version.Phase0:
+		return &ethpb.BeaconState{
+			GenesisTime:                 b.genesisTime,
+			GenesisValidatorsRoot:       gvrCopy[:],
+			Slot:                        b.slot,
+			Fork:                        b.forkVal(),
+			LatestBlockHeader:           b.latestBlockHeaderVal(),
+			BlockRoots:                  b.blockRoots.Slice(),
+			StateRoots:                  b.stateRoots.Slice(),
+			HistoricalRoots:             b.historicalRoots.Slice(),
+			Eth1Data:                    b.eth1DataVal(),
+			Eth1DataVotes:               b.eth1DataVotesVal(),
+			Eth1DepositIndex:            b.eth1DepositIndex,
+			Validators:                  b.validatorsVal(),
+			Balances:                    b.balancesVal(),
+			RandaoMixes:                 b.randaoMixes.Slice(),
+			Slashings:                   b.slashingsVal(),
+			PreviousEpochAttestations:   b.previousEpochAttestationsVal(),
+			CurrentEpochAttestations:    b.currentEpochAttestationsVal(),
+			JustificationBits:           b.justificationBitsVal(),
+			PreviousJustifiedCheckpoint: b.previousJustifiedCheckpointVal(),
+			CurrentJustifiedCheckpoint:  b.currentJustifiedCheckpointVal(),
+			FinalizedCheckpoint:         b.finalizedCheckpointVal(),
+		}
+	case version.Altair:
+		return &ethpb.BeaconStateAltair{
+			GenesisTime:                 b.genesisTime,
+			GenesisValidatorsRoot:       gvrCopy[:],
+			Slot:                        b.slot,
+			Fork:                        b.forkVal(),
+			LatestBlockHeader:           b.latestBlockHeaderVal(),
+			BlockRoots:                  b.blockRoots.Slice(),
+			StateRoots:                  b.stateRoots.Slice(),
+			HistoricalRoots:             b.historicalRoots.Slice(),
+			Eth1Data:                    b.eth1DataVal(),
+			Eth1DataVotes:               b.eth1DataVotesVal(),
+			Eth1DepositIndex:            b.eth1DepositIndex,
+			Validators:                  b.validatorsVal(),
+			Balances:                    b.balancesVal(),
+			RandaoMixes:                 b.randaoMixes.Slice(),
+			Slashings:                   b.slashingsVal(),
+			PreviousEpochParticipation:  b.previousEpochParticipationVal(),
+			CurrentEpochParticipation:   b.currentEpochParticipationVal(),
+			JustificationBits:           b.justificationBitsVal(),
+			PreviousJustifiedCheckpoint: b.previousJustifiedCheckpointVal(),
+			CurrentJustifiedCheckpoint:  b.currentJustifiedCheckpointVal(),
+			FinalizedCheckpoint:         b.finalizedCheckpointVal(),
+			InactivityScores:            b.inactivityScoresVal(),
+			CurrentSyncCommittee:        b.currentSyncCommitteeVal(),
+			NextSyncCommittee:           b.nextSyncCommitteeVal(),
+		}
+	case version.Bellatrix:
+		return &ethpb.BeaconStateBellatrix{
+			GenesisTime:                  b.genesisTime,
+			GenesisValidatorsRoot:        gvrCopy[:],
+			Slot:                         b.slot,
+			Fork:                         b.forkVal(),
+			LatestBlockHeader:            b.latestBlockHeaderVal(),
+			BlockRoots:                   b.blockRoots.Slice(),
+			StateRoots:                   b.stateRoots.Slice(),
+			HistoricalRoots:              b.historicalRoots.Slice(),
+			Eth1Data:                     b.eth1DataVal(),
+			Eth1DataVotes:                b.eth1DataVotesVal(),
+			Eth1DepositIndex:             b.eth1DepositIndex,
+			Validators:                   b.validatorsVal(),
+			Balances:                     b.balancesVal(),
+			RandaoMixes:                  b.randaoMixes.Slice(),
+			Slashings:                    b.slashingsVal(),
+			PreviousEpochParticipation:   b.previousEpochParticipationVal(),
+			CurrentEpochParticipation:    b.currentEpochParticipationVal(),
+			JustificationBits:            b.justificationBitsVal(),
+			PreviousJustifiedCheckpoint:  b.previousJustifiedCheckpointVal(),
+			CurrentJustifiedCheckpoint:   b.currentJustifiedCheckpointVal(),
+			FinalizedCheckpoint:          b.finalizedCheckpointVal(),
+			InactivityScores:             b.inactivityScoresVal(),
+			CurrentSyncCommittee:         b.currentSyncCommitteeVal(),
+			NextSyncCommittee:            b.nextSyncCommitteeVal(),
+			LatestExecutionPayloadHeader: b.latestExecutionPayloadHeaderVal(),
+		}
+	default:
+		return nil
 	}
 }
 
