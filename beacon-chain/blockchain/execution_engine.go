f9113dfd06adc9f32916d7c1a9278596002bef8e
Potuz <potuz@prysmaticlabs.com>
2022-05-02 10:53:22 -0300

    Forkchoice use parent hash when removing invalid blocks (#10594)

--- beacon-chain/blockchain/execution_engine.go
+++ beacon-chain/blockchain/execution_engine.go
@@ -147,7 +147,7 @@ func (s *Service) notifyNewPayload(ctx context.Context, postStateVersion int,
 		if err != nil {
 			return false, err
 		}
-		invalidRoots, err := s.ForkChoicer().SetOptimisticToInvalid(ctx, root, bytesutil.ToBytes32(lastValidHash))
+		invalidRoots, err := s.ForkChoicer().SetOptimisticToInvalid(ctx, root, bytesutil.ToBytes32(blk.Block().ParentRoot()), bytesutil.ToBytes32(lastValidHash))
 		if err != nil {
 			return false, err
 		}
