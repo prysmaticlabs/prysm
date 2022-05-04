97dd6d3339de53c7dca7a9c8e8507ef9fb6993cc
rkapka <rkapka@wp.pl>
2022-05-04 19:39:41 +0200

    use RealPosition in hasher

--- beacon-chain/state/state-native/hasher.go
+++ beacon-chain/state/state-native/hasher.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 
 	"github.com/pkg/errors"
+	nativetypes "github.com/prysmaticlabs/prysm/beacon-chain/state/state-native/types"
 	"github.com/prysmaticlabs/prysm/beacon-chain/state/stateutil"
 	fieldparams "github.com/prysmaticlabs/prysm/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/config/params"
@@ -34,39 +35,32 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 		fieldRoots = make([][]byte, params.BeaconConfig().BeaconStateBellatrixFieldCount)
 	}
 
-	fieldRootIx := 0
-
 	// Genesis time root.
 	genesisRoot := ssz.Uint64Root(state.genesisTime)
-	fieldRoots[fieldRootIx] = genesisRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.GenesisTime.RealPosition(state)] = genesisRoot[:]
 
 	// Genesis validators root.
 	r := [32]byte{}
 	copy(r[:], state.genesisValidatorsRoot[:])
-	fieldRoots[fieldRootIx] = r[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.GenesisValidatorsRoot.RealPosition(state)] = r[:]
 
 	// Slot root.
 	slotRoot := ssz.Uint64Root(uint64(state.slot))
-	fieldRoots[fieldRootIx] = slotRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.Slot.RealPosition(state)] = slotRoot[:]
 
 	// Fork data structure root.
 	forkHashTreeRoot, err := ssz.ForkRoot(state.fork)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute fork merkleization")
 	}
-	fieldRoots[fieldRootIx] = forkHashTreeRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.Fork.RealPosition(state)] = forkHashTreeRoot[:]
 
 	// BeaconBlockHeader data structure root.
 	headerHashTreeRoot, err := stateutil.BlockHeaderRoot(state.latestBlockHeader)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute block header merkleization")
 	}
-	fieldRoots[fieldRootIx] = headerHashTreeRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.LatestBlockHeader.RealPosition(state)] = headerHashTreeRoot[:]
 
 	// BlockRoots array root.
 	bRoots := make([][]byte, len(state.blockRoots))
@@ -77,8 +71,7 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute block roots merkleization")
 	}
-	fieldRoots[fieldRootIx] = blockRootsRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.BlockRoots.RealPosition(state)] = blockRootsRoot[:]
 
 	// StateRoots array root.
 	sRoots := make([][]byte, len(state.stateRoots))
@@ -89,8 +82,7 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute state roots merkleization")
 	}
-	fieldRoots[fieldRootIx] = stateRootsRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.StateRoots.RealPosition(state)] = stateRootsRoot[:]
 
 	// HistoricalRoots slice root.
 	hRoots := make([][]byte, len(state.historicalRoots))
@@ -101,47 +93,41 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute historical roots merkleization")
 	}
-	fieldRoots[fieldRootIx] = historicalRootsRt[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.HistoricalRoots.RealPosition(state)] = historicalRootsRt[:]
 
 	// Eth1Data data structure root.
 	eth1HashTreeRoot, err := stateutil.Eth1Root(hasher, state.eth1Data)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute eth1data merkleization")
 	}
-	fieldRoots[fieldRootIx] = eth1HashTreeRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.Eth1Data.RealPosition(state)] = eth1HashTreeRoot[:]
 
 	// Eth1DataVotes slice root.
 	eth1VotesRoot, err := stateutil.Eth1DataVotesRoot(state.eth1DataVotes)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute eth1data votes merkleization")
 	}
-	fieldRoots[fieldRootIx] = eth1VotesRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.Eth1DataVotes.RealPosition(state)] = eth1VotesRoot[:]
 
 	// Eth1DepositIndex root.
 	eth1DepositIndexBuf := make([]byte, 8)
 	binary.LittleEndian.PutUint64(eth1DepositIndexBuf, state.eth1DepositIndex)
 	eth1DepositBuf := bytesutil.ToBytes32(eth1DepositIndexBuf)
-	fieldRoots[fieldRootIx] = eth1DepositBuf[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.Eth1DepositIndex.RealPosition(state)] = eth1DepositBuf[:]
 
 	// Validators slice root.
 	validatorsRoot, err := stateutil.ValidatorRegistryRoot(state.validators)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute validator registry merkleization")
 	}
-	fieldRoots[fieldRootIx] = validatorsRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.Validators.RealPosition(state)] = validatorsRoot[:]
 
 	// Balances slice root.
 	balancesRoot, err := stateutil.Uint64ListRootWithRegistryLimit(state.balances)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute validator balances merkleization")
 	}
-	fieldRoots[fieldRootIx] = balancesRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.Balances.RealPosition(state)] = balancesRoot[:]
 
 	// RandaoMixes array root.
 	mixes := make([][]byte, len(state.randaoMixes))
@@ -152,16 +138,14 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute randao roots merkleization")
 	}
-	fieldRoots[fieldRootIx] = randaoRootsRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.RandaoMixes.RealPosition(state)] = randaoRootsRoot[:]
 
 	// Slashings array root.
 	slashingsRootsRoot, err := ssz.SlashingsRoot(state.slashings)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute slashings merkleization")
 	}
-	fieldRoots[fieldRootIx] = slashingsRootsRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.Slashings.RealPosition(state)] = slashingsRootsRoot[:]
 
 	if state.version == version.Phase0 {
 		// PreviousEpochAttestations slice root.
@@ -169,16 +153,14 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 		if err != nil {
 			return nil, errors.Wrap(err, "could not compute previous epoch attestations merkleization")
 		}
-		fieldRoots[fieldRootIx] = prevAttsRoot[:]
-		fieldRootIx++
+		fieldRoots[nativetypes.PreviousEpochAttestations.RealPosition(state)] = prevAttsRoot[:]
 
 		// CurrentEpochAttestations slice root.
 		currAttsRoot, err := stateutil.EpochAttestationsRoot(state.currentEpochAttestations)
 		if err != nil {
 			return nil, errors.Wrap(err, "could not compute current epoch attestations merkleization")
 		}
-		fieldRoots[fieldRootIx] = currAttsRoot[:]
-		fieldRootIx++
+		fieldRoots[nativetypes.CurrentEpochAttestations.RealPosition(state)] = currAttsRoot[:]
 	}
 
 	if state.version == version.Altair || state.version == version.Bellatrix {
@@ -187,46 +169,40 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 		if err != nil {
 			return nil, errors.Wrap(err, "could not compute previous epoch participation merkleization")
 		}
-		fieldRoots[fieldRootIx] = prevParticipationRoot[:]
-		fieldRootIx++
+		fieldRoots[nativetypes.PreviousEpochParticipationBits.RealPosition(state)] = prevParticipationRoot[:]
 
 		// CurrentEpochParticipation slice root.
 		currParticipationRoot, err := stateutil.ParticipationBitsRoot(state.currentEpochParticipation)
 		if err != nil {
 			return nil, errors.Wrap(err, "could not compute current epoch participation merkleization")
 		}
-		fieldRoots[fieldRootIx] = currParticipationRoot[:]
-		fieldRootIx++
+		fieldRoots[nativetypes.CurrentEpochParticipationBits.RealPosition(state)] = currParticipationRoot[:]
 	}
 
 	// JustificationBits root.
 	justifiedBitsRoot := bytesutil.ToBytes32(state.justificationBits)
-	fieldRoots[fieldRootIx] = justifiedBitsRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.JustificationBits.RealPosition(state)] = justifiedBitsRoot[:]
 
 	// PreviousJustifiedCheckpoint data structure root.
 	prevCheckRoot, err := ssz.CheckpointRoot(hasher, state.previousJustifiedCheckpoint)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute previous justified checkpoint merkleization")
 	}
-	fieldRoots[fieldRootIx] = prevCheckRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.PreviousJustifiedCheckpoint.RealPosition(state)] = prevCheckRoot[:]
 
 	// CurrentJustifiedCheckpoint data structure root.
 	currJustRoot, err := ssz.CheckpointRoot(hasher, state.currentJustifiedCheckpoint)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute current justified checkpoint merkleization")
 	}
-	fieldRoots[fieldRootIx] = currJustRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.CurrentJustifiedCheckpoint.RealPosition(state)] = currJustRoot[:]
 
 	// FinalizedCheckpoint data structure root.
 	finalRoot, err := ssz.CheckpointRoot(hasher, state.finalizedCheckpoint)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not compute finalized checkpoint merkleization")
 	}
-	fieldRoots[fieldRootIx] = finalRoot[:]
-	fieldRootIx++
+	fieldRoots[nativetypes.FinalizedCheckpoint.RealPosition(state)] = finalRoot[:]
 
 	if state.version == version.Altair || state.version == version.Bellatrix {
 		// Inactivity scores root.
@@ -234,24 +210,21 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 		if err != nil {
 			return nil, errors.Wrap(err, "could not compute inactivityScoreRoot")
 		}
-		fieldRoots[fieldRootIx] = inactivityScoresRoot[:]
-		fieldRootIx++
+		fieldRoots[nativetypes.InactivityScores.RealPosition(state)] = inactivityScoresRoot[:]
 
 		// Current sync committee root.
 		currentSyncCommitteeRoot, err := stateutil.SyncCommitteeRoot(state.currentSyncCommittee)
 		if err != nil {
 			return nil, errors.Wrap(err, "could not compute sync committee merkleization")
 		}
-		fieldRoots[fieldRootIx] = currentSyncCommitteeRoot[:]
-		fieldRootIx++
+		fieldRoots[nativetypes.CurrentSyncCommittee.RealPosition(state)] = currentSyncCommitteeRoot[:]
 
 		// Next sync committee root.
 		nextSyncCommitteeRoot, err := stateutil.SyncCommitteeRoot(state.nextSyncCommittee)
 		if err != nil {
 			return nil, errors.Wrap(err, "could not compute sync committee merkleization")
 		}
-		fieldRoots[fieldRootIx] = nextSyncCommitteeRoot[:]
-		fieldRootIx++
+		fieldRoots[nativetypes.NextSyncCommittee.RealPosition(state)] = nextSyncCommitteeRoot[:]
 	}
 
 	if state.version == version.Bellatrix {
@@ -260,7 +233,7 @@ func ComputeFieldRootsWithHasher(ctx context.Context, state *BeaconState) ([][]b
 		if err != nil {
 			return nil, err
 		}
-		fieldRoots[fieldRootIx] = executionPayloadRoot[:]
+		fieldRoots[nativetypes.LatestExecutionPayloadHeader.RealPosition(state)] = executionPayloadRoot[:]
 	}
 
 	return fieldRoots, nil
