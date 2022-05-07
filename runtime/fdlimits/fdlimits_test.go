60c4b1c9e4af7073c77e36eb1793d46c63349e31
nisdas <nishdas93@gmail.com>
2022-05-07 10:39:31 +0800

    kasey's review

--- runtime/fdlimits/fdlimits_test.go
+++ runtime/fdlimits/fdlimits_test.go
@@ -1,31 +1,22 @@
 package fdlimits_test
 
 import (
-	"syscall"
 	"testing"
 
+	gethLimit "github.com/ethereum/go-ethereum/common/fdlimit"
 	"github.com/prysmaticlabs/prysm/runtime/fdlimits"
 	"github.com/prysmaticlabs/prysm/testing/assert"
 )
 
 func TestSetMaxFdLimits(t *testing.T) {
-	var limit syscall.Rlimit
-	assert.NoError(t, syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit))
-	wantedMax := limit.Max
-
-	// Set it to a low value.
-	limit.Cur = 2000
-	assert.NoError(t, syscall.Setrlimit(syscall.RLIMIT_NOFILE, &limit))
-	limit = syscall.Rlimit{}
-
-	// Double check it works
-	assert.NoError(t, syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit))
-	assert.Equal(t, uint64(2000), limit.Cur)
-
 	assert.NoError(t, fdlimits.SetMaxFdLimits())
-	// Retrieve fd limit again.
-	assert.NoError(t, syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit))
 
-	assert.Equal(t, wantedMax, limit.Cur)
-	assert.NotEqual(t, 2000, limit.Cur)
+	curr, err := gethLimit.Current()
+	assert.NoError(t, err)
+
+	max, err := gethLimit.Maximum()
+	assert.NoError(t, err)
+
+	assert.Equal(t, max, curr, "current and maximum file descriptor limits do not match up.")
+
 }
