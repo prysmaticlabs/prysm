677b2e1101686837e12f462fb9549cd4e3789b14
terence tsao <terence@prysmaticlabs.com>
2022-05-17 13:33:14 -0700

    Update validate_sync_contribution_proof_test.go

--- beacon-chain/sync/validate_sync_contribution_proof_test.go
+++ beacon-chain/sync/validate_sync_contribution_proof_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/transition"
 	"github.com/prysmaticlabs/prysm/beacon-chain/db"
 	testingDB "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
+	"github.com/prysmaticlabs/prysm/beacon-chain/operations/synccommittee"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p"
 	"github.com/prysmaticlabs/prysm/beacon-chain/p2p/encoder"
 	mockp2p "github.com/prysmaticlabs/prysm/beacon-chain/p2p/testing"
@@ -72,6 +73,7 @@ func TestService_ValidateSyncContributionAndProof(t *testing.T) {
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithSyncCommsPool(synccommittee.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedContributionAndProof) *Service {
 				s.cfg.stateGen = stategen.New(db)
@@ -265,6 +267,7 @@ func TestService_ValidateSyncContributionAndProof(t *testing.T) {
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithSyncCommsPool(synccommittee.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedContributionAndProof) *Service {
 				s.cfg.stateGen = stategen.New(db)
@@ -309,6 +312,7 @@ func TestService_ValidateSyncContributionAndProof(t *testing.T) {
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithSyncCommsPool(synccommittee.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedContributionAndProof) *Service {
 				s.cfg.stateGen = stategen.New(db)
@@ -371,6 +375,7 @@ func TestService_ValidateSyncContributionAndProof(t *testing.T) {
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithSyncCommsPool(synccommittee.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedContributionAndProof) *Service {
 				s.cfg.stateGen = stategen.New(db)
@@ -436,6 +441,7 @@ func TestService_ValidateSyncContributionAndProof(t *testing.T) {
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithSyncCommsPool(synccommittee.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedContributionAndProof) *Service {
 				s.cfg.stateGen = stategen.New(db)
@@ -516,6 +522,7 @@ func TestService_ValidateSyncContributionAndProof(t *testing.T) {
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithSyncCommsPool(synccommittee.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedContributionAndProof) *Service {
 				s.cfg.stateGen = stategen.New(db)
@@ -682,6 +689,7 @@ func TestService_ValidateSyncContributionAndProof(t *testing.T) {
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithSyncCommsPool(synccommittee.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedContributionAndProof) *Service {
 				s.cfg.stateGen = stategen.New(db)
@@ -778,6 +786,7 @@ func TestService_ValidateSyncContributionAndProof(t *testing.T) {
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithSyncCommsPool(synccommittee.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedContributionAndProof) *Service {
 				s.cfg.stateGen = stategen.New(db)
@@ -925,6 +934,7 @@ func TestValidateSyncContributionAndProof(t *testing.T) {
 		WithChainService(chainService),
 		WithStateNotifier(chainService.StateNotifier()),
 		WithOperationNotifier(chainService.OperationNotifier()),
+		WithSyncCommsPool(synccommittee.NewPool()),
 	)
 	go s.verifierRoutine()
 	s.cfg.stateGen = stategen.New(db)
