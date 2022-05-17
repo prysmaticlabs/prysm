d6edf662ceda0791b45dca08fc87edc468e8609d
Kasey Kirkham <kasey@users.noreply.github.com>
2022-05-17 11:41:31 -0500

    remove unused KnownConfigs

--- config/params/values.go
+++ config/params/values.go
@@ -9,12 +9,3 @@ const (
 	DevnetName          = "devnet"
 	MainnetTestName     = "mainnet-test"
 )
-
-// KnownConfigs provides an index of all known BeaconChainConfig values.
-var KnownConfigs = map[string]func() *BeaconChainConfig{
-	MainnetName:         MainnetConfig,
-	PraterName:          PraterConfig,
-	MinimalName:         MinimalSpecConfig,
-	EndToEndName:        E2ETestConfig,
-	EndToEndMainnetName: E2EMainnetTestConfig,
-}
