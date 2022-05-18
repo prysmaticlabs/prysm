f82d7d3dd818e306f0a03d1a3f151a3df48d1ba9
Kasey Kirkham <kasey@users.noreply.github.com>
2022-05-18 17:01:11 -0500

    add ropstein to configset

--- config/params/init.go
+++ config/params/init.go
@@ -8,6 +8,7 @@ func init() {
 		E2ETestConfig(),
 		E2EMainnetTestConfig(),
 		InteropConfig(),
+		RopstenConfig(),
 	}
 	configs = newConfigset(defaults...)
 	// ensure that main net is always present and active by default
