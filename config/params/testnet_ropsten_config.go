6a2ac5bbd53ddbbc8b3a065c3240abb2a9b6f966
terence tsao <terence@prysmaticlabs.com>
2022-05-18 07:45:45 -0700

    Update genesis time

--- config/params/testnet_ropsten_config.go
+++ config/params/testnet_ropsten_config.go
@@ -9,7 +9,10 @@ func UseRopstenNetworkConfig() {
 	cfg := BeaconNetworkConfig().Copy()
 	cfg.ContractDeploymentBlock = 12269949
 	cfg.BootstrapNodes = []string{
+		// EF boot node
 		"enr:-Iq4QMCTfIMXnow27baRUb35Q8iiFHSIDBJh6hQM5Axohhf4b6Kr_cOCu0htQ5WvVqKvFgY28893DHAg8gnBAXsAVqmGAX53x8JggmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk",
+		// Teku boot node
+		"enr:-KG4QMJSJ7DHk6v2p-W8zQ3Xv7FfssZ_1E3p2eY6kN13staMObUonAurqyWhODoeY6edXtV8e9eL9RnhgZ9va2SMDRQMhGV0aDKQS-iVMYAAAHD0AQAAAAAAAIJpZIJ2NIJpcIQDhAAhiXNlY3AyNTZrMaEDXBVUZhhmdy1MYor1eGdRJ4vHYghFKDgjyHgt6sJ-IlCDdGNwgiMog3VkcIIjKA",
 	}
 	OverrideBeaconNetworkConfig(cfg)
 }
@@ -22,8 +25,9 @@ func UseRopstenConfig() {
 // RopstenConfig defines the config for the Ropsten beacon chain testnet.
 func RopstenConfig() *BeaconChainConfig {
 	cfg := MainnetConfig().Copy()
-	cfg.MinGenesisTime = 1653922800
-	cfg.GenesisDelay = 300
+	cfg.MinGenesisTime = 1653318000
+	cfg.GenesisDelay = 604800
+	cfg.MinGenesisActiveValidatorCount = 100000
 	cfg.ConfigName = RopstenName
 	cfg.GenesisForkVersion = []byte{0x80, 0x00, 0x00, 0x69}
 	cfg.SecondsPerETH1Block = 14
