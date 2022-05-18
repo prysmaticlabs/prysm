f7c3315677a45ce096804555926f3772b9a439f9
Kasey Kirkham <kasey@users.noreply.github.com>
2022-05-18 15:12:07 -0500

    improve var name

--- config/params/interop.go
+++ config/params/interop.go
@@ -2,15 +2,15 @@ package params
 
 // InteropConfig provides a generic config suitable for interop testing.
 func InteropConfig() *BeaconChainConfig {
-	e2eConfig := MainnetConfig().Copy()
+	c := MainnetConfig().Copy()
 
 	// Prysm constants.
-	e2eConfig.ConfigName = InteropName
-	e2eConfig.GenesisForkVersion = []byte{0, 0, 0, 235}
-	e2eConfig.AltairForkVersion = []byte{1, 0, 0, 235}
-	e2eConfig.BellatrixForkVersion = []byte{2, 0, 0, 235}
-	e2eConfig.ShardingForkVersion = []byte{3, 0, 0, 235}
+	c.ConfigName = InteropName
+	c.GenesisForkVersion = []byte{0, 0, 0, 235}
+	c.AltairForkVersion = []byte{1, 0, 0, 235}
+	c.BellatrixForkVersion = []byte{2, 0, 0, 235}
+	c.ShardingForkVersion = []byte{3, 0, 0, 235}
 
-	e2eConfig.InitializeForkSchedule()
-	return e2eConfig
+	c.InitializeForkSchedule()
+	return c
 }
