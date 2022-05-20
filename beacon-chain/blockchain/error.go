addb3cd665d7fe7383caa377bd45c90f03b1666e
terencechain <terence@prysmaticlabs.com>
2022-05-20 10:05:39 -0700

    More selective on marking block as bad (#10681)
    * Starting, looking for feedbacks
    
    * Update error.go
    
    * Wrap invalid blocks to rest of the processings
    
    * More tests
    
    * More tests
    
    * Fix tests
    
    * Update process_block_test.go
    
    * Update execution_engine_test.go
    
    * Update BUILD.bazel
    
    * Nishant's feedback and Kasey's recommendation
    
    * Add comments on what an invalid block is
    
    * Update beacon-chain/blockchain/error.go
    
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>
    
    * Update beacon-chain/blockchain/error.go
    
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>
    
    * Rm faulty invalid conditions
    
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/blockchain/error.go
+++ beacon-chain/blockchain/error.go
@@ -17,3 +17,35 @@ var (
 	// block is not a valid optimistic candidate block
 	errNotOptimisticCandidate = errors.New("block is not suitable for optimistic sync")
 )
+
+// An invalid block is the block that fails state transition based on the core protocol rules.
+// The beacon node shall not be accepting nor building blocks that branch off from an invalid block.
+// Some examples of invalid blocks are:
+// The block violates state transition rules.
+// The block is deemed invalid according to execution layer client.
+// The block violates certain fork choice rules (before finalized slot, not finalized ancestor)
+type invalidBlock struct {
+	error
+}
+
+type invalidBlockError interface {
+	Error() string
+	InvalidBlock() bool
+}
+
+// InvalidBlock returns true for `invalidBlock`.
+func (e invalidBlock) InvalidBlock() bool {
+	return true
+}
+
+// IsInvalidBlock returns true if the error has `invalidBlock`.
+func IsInvalidBlock(e error) bool {
+	if e == nil {
+		return false
+	}
+	d, ok := e.(invalidBlockError)
+	if !ok {
+		return IsInvalidBlock(errors.Unwrap(e))
+	}
+	return d.InvalidBlock()
+}
