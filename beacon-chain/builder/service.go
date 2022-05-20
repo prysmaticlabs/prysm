e14d3c9c6d28089a9cec6b44ced3079227b1aa3c
terence tsao <terence@prysmaticlabs.com>
2022-05-20 06:39:38 -0700

    Update service.go

--- beacon-chain/builder/service.go
+++ beacon-chain/builder/service.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"time"
 
 	"github.com/prysmaticlabs/go-bitfield"
 	"github.com/prysmaticlabs/prysm/api/client/builder"
 	fieldparams "github.com/prysmaticlabs/prysm/config/fieldparams"
-	"github.com/prysmaticlabs/prysm/config/params"
 	types "github.com/prysmaticlabs/prysm/consensus-types/primitives"
-	"github.com/prysmaticlabs/prysm/crypto/bls"
 	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
 	"github.com/prysmaticlabs/prysm/network"
 	v1 "github.com/prysmaticlabs/prysm/proto/engine/v1"
@@ -50,25 +47,25 @@ func NewService(ctx context.Context, opts ...Option) (*Service, error) {
 	if err != nil {
 		return nil, err
 	}
-	sk, err := bls.RandKey()
-	if err != nil {
-		return nil, err
-	}
+	//sk, err := bls.RandKey()
+	//if err != nil {
+	//	return nil, err
+	//}
 
-	reg := &ethpb.ValidatorRegistrationV1{
-		FeeRecipient: params.BeaconConfig().DefaultFeeRecipient.Bytes(),
-		GasLimit:     100000000,
-		Timestamp:    uint64(time.Now().Unix()),
-		Pubkey:       sk.PublicKey().Marshal(),
-	}
-	sig := sk.Sign(reg.Pubkey)
+	//reg := &ethpb.ValidatorRegistrationV1{
+	//	FeeRecipient: params.BeaconConfig().DefaultFeeRecipient.Bytes(),
+	//	GasLimit:     100000000,
+	//	Timestamp:    uint64(time.Now().Unix()),
+	//	Pubkey:       sk.PublicKey().Marshal(),
+	//}
+	//sig := sk.Sign(reg.Pubkey)
 
-	if err := c.RegisterValidator(ctx, &ethpb.SignedValidatorRegistrationV1{
-		Message:   reg,
-		Signature: sig.Marshal(),
-	}); err != nil {
-		return nil, err
-	}
+	//if err := c.RegisterValidator(ctx, &ethpb.SignedValidatorRegistrationV1{
+	//	Message:   reg,
+	//	Signature: sig.Marshal(),
+	//}); err != nil {
+	//	return nil, err
+	//}
 
 	h := "a0513a503d5bd6e89a144c3268e5b7e9da9dbf63df125a360e3950a7d0d67131"
 	data, err := hex.DecodeString(h)
@@ -90,7 +87,15 @@ func NewService(ctx context.Context, opts ...Option) (*Service, error) {
 		"gasUsed":      header.GasUsed,
 	}).Info("Received builder bid")
 
-	sb := HydrateSignedBlindedBeaconBlockBellatrix(&ethpb.SignedBlindedBeaconBlockBellatrix{})
+	sb := HydrateSignedBlindedBeaconBlockBellatrix(&ethpb.SignedBlindedBeaconBlockBellatrix{
+		Block: &ethpb.BlindedBeaconBlockBellatrix{
+			Body: &ethpb.BlindedBeaconBlockBodyBellatrix{
+				Attestations: []*ethpb.Attestation{
+					{Signature: []byte{1, 2, 3, 4}},
+				},
+			},
+		},
+	})
 	sb.Block.Body.ExecutionPayloadHeader = header
 	sb.Block.Body.SyncAggregate.SyncCommitteeBits = bitfield.NewBitvector512()
 	if _, err := c.SubmitBlindedBlock(ctx, sb); err != nil {
