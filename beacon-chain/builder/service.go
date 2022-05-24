9c75f463fc1b5059fe5bb5bd2c8cd9467fbc5a98
terence tsao <terence@prysmaticlabs.com>
2022-05-24 00:09:36 -0700

    Add metrics

--- beacon-chain/builder/service.go
+++ beacon-chain/builder/service.go
@@ -3,10 +3,13 @@ package builder
 import (
 	"context"
 
+	"github.com/prysmaticlabs/prysm/api/client/builder"
 	types "github.com/prysmaticlabs/prysm/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/network"
 	v1 "github.com/prysmaticlabs/prysm/proto/engine/v1"
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/time"
+	"go.opencensus.io/trace"
 )
 
 type BlockBuilder interface {
@@ -23,6 +26,8 @@ type config struct {
 
 type Service struct {
 	cfg *config
+	c   *builder.Client
+	err error
 }
 
 func NewService(ctx context.Context, opts ...Option) (*Service, error) {
@@ -34,27 +39,61 @@ func NewService(ctx context.Context, opts ...Option) (*Service, error) {
 		}
 	}
 
+	if s.cfg.builderEndpoint.Url != "" {
+		c, err := builder.NewClient(s.cfg.builderEndpoint.Url)
+		if err != nil {
+			return nil, err
+		}
+		s.c = c
+	}
+
 	return s, nil
 }
 
-func (s *Service) Start() {}
+func (s *Service) Start() {
+
+}
 
 func (s *Service) Stop() error {
 	return nil
 }
 
-func (s *Service) SubmitBlindedBlock(context.Context, *ethpb.SignedBlindedBeaconBlockBellatrix) (*v1.ExecutionPayload, error) {
-	panic("implement me")
+func (s *Service) SubmitBlindedBlock(ctx context.Context, blk *ethpb.SignedBlindedBeaconBlockBellatrix) (*v1.ExecutionPayload, error) {
+	ctx, span := trace.StartSpan(ctx, "builder.SubmitBlindedBlock")
+	defer span.End()
+	start := time.Now()
+	defer func() {
+		submitBlindedBlockLatency.Observe(float64(time.Since(start).Milliseconds()))
+	}()
+
+	return s.c.SubmitBlindedBlock(ctx, blk)
 }
 
-func (s *Service) GetHeader(context.Context, types.Slot, [32]byte, [48]byte) (*ethpb.SignedBuilderBid, error) {
-	panic("implement me")
+func (s *Service) GetHeader(ctx context.Context, slot types.Slot, parentHash [32]byte, pubKey [48]byte) (*ethpb.SignedBuilderBid, error) {
+	ctx, span := trace.StartSpan(ctx, "builder.GetHeader")
+	defer span.End()
+	start := time.Now()
+	defer func() {
+		getHeaderLatency.Observe(float64(time.Since(start).Milliseconds()))
+	}()
+
+	return s.c.GetHeader(ctx, slot, parentHash, pubKey)
 }
 
 func (s *Service) Status() error {
-	panic("implement me")
+	if s.cfg.builderEndpoint.Url == "" {
+		return ErrNotRunning
+	}
+	return s.err
 }
 
-func (s *Service) RegisterValidator(context.Context, *ethpb.SignedValidatorRegistrationV1) error {
-	panic("implement me")
+func (s *Service) RegisterValidator(ctx context.Context, reg *ethpb.SignedValidatorRegistrationV1) error {
+	ctx, span := trace.StartSpan(ctx, "builder.RegisterValidator")
+	defer span.End()
+	start := time.Now()
+	defer func() {
+		registerValidatorLatency.Observe(float64(time.Since(start).Milliseconds()))
+	}()
+
+	return s.c.RegisterValidator(ctx, reg)
 }
