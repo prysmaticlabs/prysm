4503cab7530717a691066fb75400aa75e0d98a4a
prestonvanloon <preston@prysmaticlabs.com>
2022-06-06 17:22:47 -0500

    gofmt

--- beacon-chain/blockchain/merge_ascii_art.go
+++ beacon-chain/blockchain/merge_ascii_art.go
@@ -1,7 +1,6 @@
 package blockchain
 
-var mergeAsciiArt = 
-`                                                                                                                                                  	                                                                                                                                                      
+var mergeAsciiArt = `                                                                                                                                                  	                                                                                                                                                      
 		                                                                                                                                                      
 		                                 +?$$$$$$?*;                         ;*?$$$$?*;                         +!$$$$$$?!;                                   
 		                                 !##$???$@##$+                      !&#@$??$&#@*                      +@#&$????$##+                                   
