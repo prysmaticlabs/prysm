82ba338f4d96d6d3e46aaab1293210c4e877d887
James He <james@prysmaticlabs.com>
2022-06-06 12:42:34 -0500

    resolving more conflicts

--- validator/node/node_test.go
+++ validator/node/node_test.go
@@ -356,17 +356,19 @@ func TestProposerSettings(t *testing.T) {
 					defaultfee: "0x6e35733c5af9B61374A128e6F85f553aF09ff89B",
 				},
 			},
-			want: func() *validator_service_config.FeeRecipientConfig {
+			want: func() *validator_service_config.ProposerSettings {
 				key1, err := hexutil.Decode("0xa057816155ad77931185101128655c0191bd0214c201ca48ed887f6c4c6adf334070efcd75140eada5ac83a92506dd7a")
 				require.NoError(t, err)
-				return &validator_service_config.FeeRecipientConfig{
-					ProposeConfig: map[[fieldparams.BLSPubkeyLength]byte]*validator_service_config.FeeRecipientOptions{
+				return &validator_service_config.ProposerSettings{
+					ProposeConfig: map[[fieldparams.BLSPubkeyLength]byte]*validator_service_config.ProposerOption{
 						bytesutil.ToBytes48(key1): {
 							FeeRecipient: common.HexToAddress("0x50155530FCE8a85ec7055A5F8b2bE214B3DaeFd3"),
+							GasLimit:     params.BeaconConfig().DefaultBuilderGasLimit,
 						},
 					},
-					DefaultConfig: &validator_service_config.FeeRecipientOptions{
+					DefaultConfig: &validator_service_config.ProposerOption{
 						FeeRecipient: common.HexToAddress("0x6e35733c5af9B61374A128e6F85f553aF09ff89A"),
+						GasLimit:     params.BeaconConfig().DefaultBuilderGasLimit,
 					},
 				}
 			},
