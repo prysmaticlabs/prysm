1fa864cb1ac00462897f30ee5134a3ed7459c5dc
kasey <489222+kasey@users.noreply.github.com>
2022-06-07 11:47:42 -0500

    use slot:block index correctly (#10820)
    * adding splitRoots, refactor to use it
    
    * use splitRoots & work in roots only
    
    the most common use case for this method is to get a list of
    candidate roots and check if they are canonical. there isn't a great
    reason to look up all the non-canonical blocks, because forkchoice
    checks based on the root only, so just return roots and defer the
    responsibility of resolving those to full blocks.
    
    * update comment
    
    * clean up shadowing
    
    * more clear non-error return
    
    * add test case for single root in index slot
    
    * fmt
    
    Co-authored-by: Kasey Kirkham <kasey@users.noreply.github.com>
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/db/iface/interface.go
+++ beacon-chain/db/iface/interface.go
@@ -23,14 +23,14 @@ type ReadOnlyDatabase interface {
 	Block(ctx context.Context, blockRoot [32]byte) (interfaces.SignedBeaconBlock, error)
 	Blocks(ctx context.Context, f *filters.QueryFilter) ([]interfaces.SignedBeaconBlock, [][32]byte, error)
 	BlockRoots(ctx context.Context, f *filters.QueryFilter) ([][32]byte, error)
-	BlocksBySlot(ctx context.Context, slot types.Slot) (bool, []interfaces.SignedBeaconBlock, error)
+	BlocksBySlot(ctx context.Context, slot types.Slot) ([]interfaces.SignedBeaconBlock, error)
 	BlockRootsBySlot(ctx context.Context, slot types.Slot) (bool, [][32]byte, error)
 	HasBlock(ctx context.Context, blockRoot [32]byte) bool
 	GenesisBlock(ctx context.Context) (interfaces.SignedBeaconBlock, error)
 	GenesisBlockRoot(ctx context.Context) ([32]byte, error)
 	IsFinalizedBlock(ctx context.Context, blockRoot [32]byte) bool
 	FinalizedChildBlock(ctx context.Context, blockRoot [32]byte) (interfaces.SignedBeaconBlock, error)
-	HighestSlotBlocksBelow(ctx context.Context, slot types.Slot) ([]interfaces.SignedBeaconBlock, error)
+	HighestRootsBelowSlot(ctx context.Context, slot types.Slot) (types.Slot, [][32]byte, error)
 	// State related methods.
 	State(ctx context.Context, blockRoot [32]byte) (state.BeaconState, error)
 	StateOrError(ctx context.Context, blockRoot [32]byte) (state.BeaconState, error)
