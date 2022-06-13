fdc3b718887591f0e8b13108a60e4d36d22b36b9
terence tsao <terence@prysmaticlabs.com>
2022-06-13 16:44:06 -0700

    Move computeCheckpoints to private

--- beacon-chain/core/epoch/precompute/justification_finalization.go
+++ beacon-chain/core/epoch/precompute/justification_finalization.go
@@ -77,7 +77,7 @@ func processJustificationBits(state state.BeaconState, totalActiveBalance, prevE
 // updateJustificationAndFinalization processes justification and finalization during
 // epoch processing. This is where a beacon node can justify and finalize a new epoch.
 func weighJustificationAndFinalization(state state.BeaconState, newBits bitfield.Bitvector4) (state.BeaconState, error) {
-	jc, fc, err := ComputeCheckpoints(state, newBits)
+	jc, fc, err := computeCheckpoints(state, newBits)
 	if err != nil {
 		return nil, err
 	}
@@ -99,6 +99,8 @@ func weighJustificationAndFinalization(state state.BeaconState, newBits bitfield
 	}
 	return state, nil
 }
+
+var ComputeCheckpoints = computeCheckpoints // For test only.
 
 // ComputeCheckpoints computes the new Justification and Finalization
 // checkpoints at epoch transition
@@ -139,7 +141,7 @@ func weighJustificationAndFinalization(state state.BeaconState, newBits bitfield
 //    # The 1st/2nd most recent epochs are justified, the 1st using the 2nd as source
 //    if all(bits[0:2]) and old_current_justified_checkpoint.epoch + 1 == current_epoch:
 //        state.finalized_checkpoint = old_current_justified_checkpoint
-func ComputeCheckpoints(state state.BeaconState, newBits bitfield.Bitvector4) (*ethpb.Checkpoint, *ethpb.Checkpoint, error) {
+func computeCheckpoints(state state.BeaconState, newBits bitfield.Bitvector4) (*ethpb.Checkpoint, *ethpb.Checkpoint, error) {
 	prevEpoch := time.PrevEpoch(state)
 	currentEpoch := time.CurrentEpoch(state)
 	oldPrevJustifiedCheckpoint := state.PreviousJustifiedCheckpoint()
