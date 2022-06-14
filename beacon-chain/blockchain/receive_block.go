cf9094d79fb5825624150b024610ed787c7fa177
Saolyn <sammy@rosso.red>
2022-06-14 16:16:39 +0200

    Added error logging to log statements
    Changed the error handling from log statements. Instead of returning the
    error we log the error.

--- beacon-chain/blockchain/receive_block.go
+++ beacon-chain/blockchain/receive_block.go
@@ -71,7 +71,7 @@ func (s *Service) ReceiveBlock(ctx context.Context, block interfaces.SignedBeaco
 
 	// Log block sync status.
 	if err := logBlockSyncStatus(blockCopy.Block(), blockRoot, justified, finalized, receivedTime, uint64(s.genesisTime.Unix())); err != nil {
-		return err
+		log.WithError(err).Error("Unable to log block sync status")
 	}
 	// Log payload data
 	if err := logPayload(blockCopy.Block()); err != nil {
@@ -79,7 +79,7 @@ func (s *Service) ReceiveBlock(ctx context.Context, block interfaces.SignedBeaco
 	}
 	// Log state transition data.
 	if err := logStateTransitionData(blockCopy.Block()); err != nil {
-		return err
+		log.WithError(err).Error("Unable to log state transition data")
 	}
 
 	return nil
