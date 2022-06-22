4876dccd00e4aeab32b2d3c31f3ff0d88fa1564b
Delweng <delweng@gmail.com>
2022-06-22 10:32:07 +0800

    cmd/beacon-chain: add jwtcommands
    Signed-off-by: Delweng <delweng@gmail.com>

--- cmd/beacon-chain/main.go
+++ cmd/beacon-chain/main.go
@@ -16,6 +16,7 @@ import (
 	blockchaincmd "github.com/prysmaticlabs/prysm/cmd/beacon-chain/blockchain"
 	dbcommands "github.com/prysmaticlabs/prysm/cmd/beacon-chain/db"
 	"github.com/prysmaticlabs/prysm/cmd/beacon-chain/flags"
+	jwtcommands "github.com/prysmaticlabs/prysm/cmd/beacon-chain/jwt"
 	powchaincmd "github.com/prysmaticlabs/prysm/cmd/beacon-chain/powchain"
 	"github.com/prysmaticlabs/prysm/cmd/beacon-chain/sync/checkpoint"
 	"github.com/prysmaticlabs/prysm/cmd/beacon-chain/sync/genesis"
@@ -138,6 +139,7 @@ func main() {
 	app.Version = version.Version()
 	app.Commands = []*cli.Command{
 		dbcommands.Commands,
+		jwtcommands.Commands,
 	}
 
 	app.Flags = appFlags
