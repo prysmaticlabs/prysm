8b59d4ed8bc8a20e122930b0ebac27ed5bcc5004
nisdas <nishdas93@gmail.com>
2022-06-30 20:55:04 +0800

    fix build

--- crypto/ecdsa/utils.go
+++ crypto/ecdsa/utils.go
@@ -4,7 +4,7 @@ import (
 	"crypto/ecdsa"
 	"math/big"
 
-	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/btcec/v2"
 	gcrypto "github.com/ethereum/go-ethereum/crypto"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/pkg/errors"
