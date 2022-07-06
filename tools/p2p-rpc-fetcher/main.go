56734200b545d2e51d50878812ec2897d3ae7935
Raul Jordan <raul@prysmaticlabs.com>
2022-07-06 17:38:48 -0400

    attempt p2p connect send tool

--- tools/p2p-rpc-fetcher/main.go
+++ tools/p2p-rpc-fetcher/main.go
@@ -1,5 +1,485 @@
 package main
 
-func main() {
+import (
+	"context"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"net"
+	"time"
 
+	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/libp2p/go-libp2p"
+	libp2pcore "github.com/libp2p/go-libp2p-core"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/host"
+	corenet "github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	"github.com/libp2p/go-libp2p/p2p/security/noise"
+	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
+	ma "github.com/multiformats/go-multiaddr"
+	"github.com/pkg/errors"
+	"github.com/prysmaticlabs/go-bitfield"
+	"github.com/prysmaticlabs/prysm/beacon-chain/blockchain"
+	mock "github.com/prysmaticlabs/prysm/beacon-chain/blockchain/testing"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/signing"
+	"github.com/prysmaticlabs/prysm/beacon-chain/p2p"
+	"github.com/prysmaticlabs/prysm/beacon-chain/p2p/encoder"
+	p2ptypes "github.com/prysmaticlabs/prysm/beacon-chain/p2p/types"
+	"github.com/prysmaticlabs/prysm/beacon-chain/sync"
+	"github.com/prysmaticlabs/prysm/config/params"
+	"github.com/prysmaticlabs/prysm/consensus-types/interfaces"
+	types "github.com/prysmaticlabs/prysm/consensus-types/primitives"
+	"github.com/prysmaticlabs/prysm/consensus-types/wrapper"
+	ecdsaprysm "github.com/prysmaticlabs/prysm/crypto/ecdsa"
+	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
+	"github.com/prysmaticlabs/prysm/monitoring/tracing"
+	"github.com/prysmaticlabs/prysm/network"
+	pb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1/metadata"
+	"github.com/prysmaticlabs/prysm/runtime/version"
+	"github.com/prysmaticlabs/prysm/time/slots"
+	log "github.com/sirupsen/logrus"
+	"go.opencensus.io/trace"
+)
+
+var (
+	peerMultiaddr = "/ip4/10.0.0.74/tcp/13000/p2p/16Uiu2HAkyBAs6VWbpi1ZvZQDJSSWbueCBQzHc2oZbcUGRpjYWoZV"
+)
+
+type service struct {
+	h host.Host
+}
+
+func (s *service) Encoding() encoder.NetworkEncoding {
+	return &encoder.SszNetworkEncoder{}
+}
+
+func main() {
+	ipAdd := ipAddr()
+	priv, err := privKey()
+	if err != nil {
+		panic(err)
+	}
+	listen, err := multiAddressBuilder(ipAdd.String(), 13001)
+	if err != nil {
+		panic(err)
+	}
+	options := []libp2p.Option{
+		privKeyOption(priv),
+		libp2p.ListenAddrs(listen),
+		libp2p.UserAgent(version.BuildData()),
+		libp2p.Transport(tcp.NewTCPTransport),
+	}
+	options = append(options, libp2p.Security(noise.ID, noise.New))
+	options = append(options, libp2p.Ping(false))
+	host, err := libp2p.New(options...)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(host.Addrs())
+	peers, err := peersFromStringAddrs([]string{peerMultiaddr})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("GOT PEERS", peers)
+
+	// Connect with all peers.
+	addrInfos, err := peer.AddrInfosFromP2pAddrs(peers...)
+	if err != nil {
+		panic(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*20)
+	defer cancel()
+	for _, info := range addrInfos {
+		if info.ID == host.ID() {
+			continue
+		}
+		if err := host.Connect(ctx, info); err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("CONNECTED: ", host.Peerstore().Peers())
+
+	srv := &service{h: host}
+	for _, pr := range host.Peerstore().Peers() {
+		req := &pb.BeaconBlocksByRangeRequest{}
+		chain := &mock.ChainService{Genesis: time.Now(), ValidatorsRoot: [32]byte{}}
+		blocks, err := SendBeaconBlocksByRangeRequest(ctx, chain, srv, pr, req)
+		if err != nil {
+			fmt.Println("GOT ERR IN SEND REQ", err)
+		}
+		fmt.Println("Got this many blocks", len(blocks))
+	}
+	if err := host.Close(); err != nil {
+		panic(err)
+	}
+}
+
+// Determines a private key for p2p networking from the p2p service's
+// configuration struct. If no key is found, it generates a new one.
+func privKey() (*ecdsa.PrivateKey, error) {
+	priv, _, err := crypto.GenerateSecp256k1Key(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	return ecdsaprysm.ConvertFromInterfacePrivKey(priv)
+}
+
+func readMetadata() (metadata.Metadata, error) {
+	metaData := &pb.MetaDataV0{
+		SeqNumber: 0,
+		Attnets:   bitfield.NewBitvector64(),
+	}
+	return wrapper.WrappedMetadataV0(metaData), nil
+}
+
+// Retrieves an external ipv4 address and converts into a libp2p formatted value.
+func ipAddr() net.IP {
+	ip, err := network.ExternalIP()
+	if err != nil {
+		panic(err)
+	}
+	return net.ParseIP(ip)
+}
+
+func multiAddressBuilder(ipAddr string, port uint) (ma.Multiaddr, error) {
+	parsedIP := net.ParseIP(ipAddr)
+	if parsedIP.To4() == nil && parsedIP.To16() == nil {
+		return nil, errors.Errorf("invalid ip address provided: %s", ipAddr)
+	}
+	if parsedIP.To4() != nil {
+		return ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%d", ipAddr, port))
+	}
+	return ma.NewMultiaddr(fmt.Sprintf("/ip6/%s/tcp/%d", ipAddr, port))
+}
+
+func multiAddressBuilderWithID(ipAddr, protocol string, port uint, id peer.ID) (ma.Multiaddr, error) {
+	parsedIP := net.ParseIP(ipAddr)
+	if parsedIP.To4() == nil && parsedIP.To16() == nil {
+		return nil, errors.Errorf("invalid ip address provided: %s", ipAddr)
+	}
+	if id.String() == "" {
+		return nil, errors.New("empty peer id given")
+	}
+	if parsedIP.To4() != nil {
+		return ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/%s/%d/p2p/%s", ipAddr, protocol, port, id.String()))
+	}
+	return ma.NewMultiaddr(fmt.Sprintf("/ip6/%s/%s/%d/p2p/%s", ipAddr, protocol, port, id.String()))
+}
+
+// Adds a private key to the libp2p option if the option was provided.
+// If the private key file is missing or cannot be read, or if the
+// private key contents cannot be marshaled, an exception is thrown.
+func privKeyOption(privkey *ecdsa.PrivateKey) libp2p.Option {
+	return func(cfg *libp2p.Config) error {
+		ifaceKey, err := ecdsaprysm.ConvertToInterfacePrivkey(privkey)
+		if err != nil {
+			return err
+		}
+		log.Debug("ECDSA private key generated")
+		return cfg.Apply(libp2p.Identity(ifaceKey))
+	}
+}
+
+func peersFromStringAddrs(addrs []string) ([]ma.Multiaddr, error) {
+	var allAddrs []ma.Multiaddr
+	enodeString, multiAddrString := parseGenericAddrs(addrs)
+	for _, stringAddr := range multiAddrString {
+		addr, err := multiAddrFromString(stringAddr)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Could not get multiaddr from string")
+		}
+		allAddrs = append(allAddrs, addr)
+	}
+	for _, stringAddr := range enodeString {
+		enodeAddr, err := enode.Parse(enode.ValidSchemes, stringAddr)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Could not get enode from string")
+		}
+		addr, err := convertToSingleMultiAddr(enodeAddr)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Could not get multiaddr")
+		}
+		allAddrs = append(allAddrs, addr)
+	}
+	return allAddrs, nil
+}
+
+func parseGenericAddrs(addrs []string) (enodeString, multiAddrString []string) {
+	for _, addr := range addrs {
+		if addr == "" {
+			// Ignore empty entries
+			continue
+		}
+		_, err := enode.Parse(enode.ValidSchemes, addr)
+		if err == nil {
+			enodeString = append(enodeString, addr)
+			continue
+		}
+		_, err = multiAddrFromString(addr)
+		if err == nil {
+			multiAddrString = append(multiAddrString, addr)
+			continue
+		}
+		log.Errorf("Invalid address of %s provided: %v", addr, err)
+	}
+	return enodeString, multiAddrString
+}
+
+func convertToSingleMultiAddr(node *enode.Node) (ma.Multiaddr, error) {
+	pubkey := node.Pubkey()
+	assertedKey, err := ecdsaprysm.ConvertToInterfacePubkey(pubkey)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get pubkey")
+	}
+	id, err := peer.IDFromPublicKey(assertedKey)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get peer id")
+	}
+	return multiAddressBuilderWithID(node.IP().String(), "tcp", uint(node.TCP()), id)
+}
+
+func multiAddrFromString(address string) (ma.Multiaddr, error) {
+	return ma.NewMultiaddr(address)
+}
+
+// SendBeaconBlocksByRangeRequest sends BeaconBlocksByRange and returns fetched blocks, if any.
+func SendBeaconBlocksByRangeRequest(
+	ctx context.Context, chain blockchain.ChainInfoFetcher, p2pProvider *service, pid peer.ID,
+	req *pb.BeaconBlocksByRangeRequest,
+) ([]interfaces.SignedBeaconBlock, error) {
+	topic, err := p2p.TopicFromMessage(p2p.BeaconBlocksByRangeMessageName, slots.ToEpoch(chain.CurrentSlot()))
+	if err != nil {
+		return nil, errors.Wrap(err, "topic cannot find")
+	}
+	stream, err := p2pProvider.Send(ctx, req, topic, pid)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot send")
+	}
+	defer closeStream(stream)
+
+	// Augment block processing function, if non-nil block processor is provided.
+	blocks := make([]interfaces.SignedBeaconBlock, 0, req.Count)
+	process := func(blk interfaces.SignedBeaconBlock) error {
+		blocks = append(blocks, blk)
+		return nil
+	}
+	var prevSlot types.Slot
+	for i := uint64(0); ; i++ {
+		isFirstChunk := i == 0
+		blk, err := ReadChunkedBlock(stream, chain, p2pProvider, isFirstChunk)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		// The response MUST contain no more than `count` blocks, and no more than
+		// MAX_REQUEST_BLOCKS blocks.
+		if i >= req.Count || i >= params.BeaconNetworkConfig().MaxRequestBlocks {
+			return nil, errors.New("invalid data")
+		}
+		// Returned blocks MUST be in the slot range [start_slot, start_slot + count * step).
+		if blk.Block().Slot() < req.StartSlot || blk.Block().Slot() >= req.StartSlot.Add(req.Count*req.Step) {
+			return nil, errors.New("invalid data")
+		}
+		// Returned blocks, where they exist, MUST be sent in a consecutive order.
+		// Consecutive blocks MUST have values in `step` increments (slots may be skipped in between).
+		isSlotOutOfOrder := false
+		if prevSlot >= blk.Block().Slot() {
+			isSlotOutOfOrder = true
+		} else if req.Step != 0 && blk.Block().Slot().SubSlot(prevSlot).Mod(req.Step) != 0 {
+			isSlotOutOfOrder = true
+		}
+		if !isFirstChunk && isSlotOutOfOrder {
+			return nil, errors.New("invalid data")
+		}
+		prevSlot = blk.Block().Slot()
+		if err := process(blk); err != nil {
+			return nil, err
+		}
+	}
+	return blocks, nil
+}
+
+func closeStream(stream corenet.Stream) {
+	if err := stream.Close(); err != nil {
+		log.Println(err)
+	}
+}
+
+// Send a message to a specific peer. The returned stream may be used for reading, but has been
+// closed for writing.
+//
+// When done, the caller must Close or Reset on the stream.
+func (s *service) Send(ctx context.Context, message interface{}, baseTopic string, pid peer.ID) (corenet.Stream, error) {
+	ctx, span := trace.StartSpan(ctx, "p2p.Send")
+	defer span.End()
+	topic := baseTopic + s.Encoding().ProtocolSuffix()
+	span.AddAttributes(trace.StringAttribute("topic", topic))
+
+	// Apply max dial timeout when opening a new stream.
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Minute)
+	defer cancel()
+
+	fmt.Println(topic)
+	stream, err := s.h.NewStream(ctx, pid, protocol.ID(topic))
+	if err != nil {
+		tracing.AnnotateError(span, err)
+		return nil, errors.Wrap(err, "could not open new stream")
+	}
+	// Close stream for writing.
+	if err := stream.CloseWrite(); err != nil {
+		tracing.AnnotateError(span, err)
+		_err := stream.Reset()
+		_ = _err
+		return nil, errors.Wrap(err, "could not close write")
+	}
+
+	return stream, nil
+}
+
+// ReadChunkedBlock handles each response chunk that is sent by the
+// peer and converts it into a beacon block.
+func ReadChunkedBlock(stream libp2pcore.Stream, chain blockchain.ChainInfoFetcher, p2p *service, isFirstChunk bool) (interfaces.SignedBeaconBlock, error) {
+	// Handle deadlines differently for first chunk
+	if isFirstChunk {
+		return readFirstChunkedBlock(stream, chain, p2p)
+	}
+
+	return readResponseChunk(stream, chain, p2p)
+}
+
+// readFirstChunkedBlock reads the first chunked block and applies the appropriate deadlines to
+// it.
+func readFirstChunkedBlock(stream libp2pcore.Stream, chain blockchain.ChainInfoFetcher, p2p *service) (interfaces.SignedBeaconBlock, error) {
+	code, errMsg, err := sync.ReadStatusCode(stream, p2p.Encoding())
+	if err != nil {
+		return nil, err
+	}
+	if code != 0 {
+		return nil, errors.New(errMsg)
+	}
+	rpcCtx, err := readContextFromStream(stream, chain)
+	if err != nil {
+		return nil, err
+	}
+	blk, err := extractBlockDataType(rpcCtx, chain)
+	if err != nil {
+		return nil, err
+	}
+	err = p2p.Encoding().DecodeWithMaxLength(stream, blk)
+	return blk, err
+}
+
+// readResponseChunk reads the response from the stream and decodes it into the
+// provided message type.
+func readResponseChunk(stream libp2pcore.Stream, chain blockchain.ChainInfoFetcher, p2p *service) (interfaces.SignedBeaconBlock, error) {
+	code, errMsg, err := readStatusCodeNoDeadline(stream, p2p.Encoding())
+	if err != nil {
+		return nil, err
+	}
+	if code != 0 {
+		return nil, errors.New(errMsg)
+	}
+	// No-op for now with the rpc context.
+	rpcCtx, err := readContextFromStream(stream, chain)
+	if err != nil {
+		return nil, err
+	}
+	blk, err := extractBlockDataType(rpcCtx, chain)
+	if err != nil {
+		return nil, err
+	}
+	err = p2p.Encoding().DecodeWithMaxLength(stream, blk)
+	return blk, err
+}
+
+func extractBlockDataType(digest []byte, chain blockchain.ChainInfoFetcher) (interfaces.SignedBeaconBlock, error) {
+	if len(digest) == 0 {
+		bFunc, ok := p2ptypes.BlockMap[bytesutil.ToBytes4(params.BeaconConfig().GenesisForkVersion)]
+		if !ok {
+			return nil, errors.New("no block type exists for the genesis fork version.")
+		}
+		return bFunc()
+	}
+	if len(digest) != 4 {
+		return nil, errors.Errorf("invalid digest returned, wanted a length of %d but received %d", 4, len(digest))
+	}
+	vRoot := chain.GenesisValidatorsRoot()
+	for k, blkFunc := range p2ptypes.BlockMap {
+		rDigest, err := signing.ComputeForkDigest(k[:], vRoot[:])
+		if err != nil {
+			return nil, err
+		}
+		if rDigest == bytesutil.ToBytes4(digest) {
+			return blkFunc()
+		}
+	}
+	return nil, errors.New("no valid digest matched")
+}
+
+// reads any attached context-bytes to the payload.
+func readContextFromStream(stream corenet.Stream, chain blockchain.ChainInfoFetcher) ([]byte, error) {
+	rpcCtx, err := rpcContext(stream, chain)
+	if err != nil {
+		return nil, err
+	}
+	if len(rpcCtx) == 0 {
+		return []byte{}, nil
+	}
+	// Read context (fork-digest) from stream
+	b := make([]byte, 4)
+	if _, err := stream.Read(b); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// retrieve expected context depending on rpc topic schema version.
+func rpcContext(stream corenet.Stream, chain blockchain.ChainInfoFetcher) ([]byte, error) {
+	_, _, version, err := p2p.TopicDeconstructor(string(stream.Protocol()))
+	if err != nil {
+		return nil, err
+	}
+	switch version {
+	case p2p.SchemaVersionV1:
+		// Return empty context for a v1 method.
+		return []byte{}, nil
+	case p2p.SchemaVersionV2:
+		currFork := chain.CurrentFork()
+		genRoot := chain.GenesisValidatorsRoot()
+		digest, err := signing.ComputeForkDigest(currFork.CurrentVersion, genRoot[:])
+		if err != nil {
+			return nil, err
+		}
+		return digest[:], nil
+	default:
+		return nil, errors.New("invalid version of %s registered for topic: %s")
+	}
+}
+
+var responseCodeSuccess = byte(0x00)
+
+// reads data from the stream without applying any timeouts.
+func readStatusCodeNoDeadline(stream corenet.Stream, encoding encoder.NetworkEncoding) (uint8, string, error) {
+	b := make([]byte, 1)
+	_, err := stream.Read(b)
+	if err != nil {
+		return 0, "", err
+	}
+
+	if b[0] == responseCodeSuccess {
+		return 0, "", nil
+	}
+
+	msg := &p2ptypes.ErrorMessage{}
+	if err := encoding.DecodeWithMaxLength(stream, msg); err != nil {
+		return 0, "", err
+	}
+
+	return b[0], string(*msg), nil
 }
