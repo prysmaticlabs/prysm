e6f98c88fa7e4b72dc7e87ef2eadf0b307c8f5e0
Raul Jordan <raul@prysmaticlabs.com>
2022-07-12 11:24:21 -0400

    el test

--- consensus-types/wrapper/execution.go
+++ consensus-types/wrapper/execution.go
@@ -7,6 +7,8 @@ import (
 	fastssz "github.com/prysmaticlabs/fastssz"
 	fieldparams "github.com/prysmaticlabs/prysm/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/consensus-types/interfaces"
+	"github.com/prysmaticlabs/prysm/encoding/bytesutil"
+	"github.com/prysmaticlabs/prysm/encoding/ssz"
 	enginev1 "github.com/prysmaticlabs/prysm/proto/engine/v1"
 	"google.golang.org/protobuf/proto"
 )
@@ -271,6 +273,34 @@ func (executionPayloadHeader) Transactions() ([][]byte, error) {
 // TransactionsRoot --
 func (e executionPayloadHeader) TransactionsRoot() ([]byte, error) {
 	return e.p.TransactionsRoot, nil
+}
+
+// PayloadToHeader converts `payload` into execution payload header format.
+func PayloadToHeader(payload interfaces.ExecutionData) (*enginev1.ExecutionPayloadHeader, error) {
+	txs, err := payload.Transactions()
+	if err != nil {
+		return nil, err
+	}
+	txRoot, err := ssz.TransactionsRoot(txs)
+	if err != nil {
+		return nil, err
+	}
+	return &enginev1.ExecutionPayloadHeader{
+		ParentHash:       bytesutil.SafeCopyBytes(payload.ParentHash()),
+		FeeRecipient:     bytesutil.SafeCopyBytes(payload.FeeRecipient()),
+		StateRoot:        bytesutil.SafeCopyBytes(payload.StateRoot()),
+		ReceiptsRoot:     bytesutil.SafeCopyBytes(payload.ReceiptsRoot()),
+		LogsBloom:        bytesutil.SafeCopyBytes(payload.LogsBloom()),
+		PrevRandao:       bytesutil.SafeCopyBytes(payload.PrevRandao()),
+		BlockNumber:      payload.BlockNumber(),
+		GasLimit:         payload.GasLimit(),
+		GasUsed:          payload.GasUsed(),
+		Timestamp:        payload.Timestamp(),
+		ExtraData:        bytesutil.SafeCopyBytes(payload.ExtraData()),
+		BaseFeePerGas:    bytesutil.SafeCopyBytes(payload.BaseFeePerGas()),
+		BlockHash:        bytesutil.SafeCopyBytes(payload.BlockHash()),
+		TransactionsRoot: txRoot[:],
+	}, nil
 }
 
 // IsEmptyExecutionData checks if an execution data is empty underneath. If a single field has
---
+++ consensus-types/wrapper/execution_test.go
@@ -0,0 +1,113 @@
+package wrapper_test
+
+import (
+	"testing"
+
+	fieldparams "github.com/prysmaticlabs/prysm/config/fieldparams"
+	"github.com/prysmaticlabs/prysm/consensus-types/wrapper"
+	enginev1 "github.com/prysmaticlabs/prysm/proto/engine/v1"
+	"github.com/prysmaticlabs/prysm/testing/assert"
+	"github.com/prysmaticlabs/prysm/testing/require"
+)
+
+func TestWrapExecutionPayload(t *testing.T) {
+	data := &enginev1.ExecutionPayload{GasUsed: 54}
+	wsb, err := wrapper.WrappedExecutionPayload(data)
+	require.NoError(t, err)
+
+	assert.DeepEqual(t, data, wsb.Proto())
+}
+
+func TestWrapExecutionPayloadHeader(t *testing.T) {
+	data := &enginev1.ExecutionPayloadHeader{GasUsed: 54}
+	wsb, err := wrapper.WrappedExecutionPayloadHeader(data)
+	require.NoError(t, err)
+
+	assert.DeepEqual(t, data, wsb.Proto())
+}
+
+func TestWrapExecutionPayload_IsNil(t *testing.T) {
+	_, err := wrapper.WrappedExecutionPayload(nil)
+	require.Equal(t, wrapper.ErrNilObjectWrapped, err)
+
+	data := &enginev1.ExecutionPayload{GasUsed: 54}
+	wsb, err := wrapper.WrappedExecutionPayload(data)
+	require.NoError(t, err)
+
+	assert.Equal(t, false, wsb.IsNil())
+}
+
+func TestWrapExecutionPayloadHeader_IsNil(t *testing.T) {
+	_, err := wrapper.WrappedExecutionPayloadHeader(nil)
+	require.Equal(t, wrapper.ErrNilObjectWrapped, err)
+
+	data := &enginev1.ExecutionPayloadHeader{GasUsed: 54}
+	wsb, err := wrapper.WrappedExecutionPayloadHeader(data)
+	require.NoError(t, err)
+
+	assert.Equal(t, false, wsb.IsNil())
+}
+
+func TestWrapExecutionPayload_SSZ(t *testing.T) {
+	wsb, err := wrapper.WrappedExecutionPayload(&enginev1.ExecutionPayload{
+		ParentHash:    make([]byte, fieldparams.RootLength),
+		FeeRecipient:  make([]byte, fieldparams.FeeRecipientLength),
+		StateRoot:     make([]byte, fieldparams.RootLength),
+		ReceiptsRoot:  make([]byte, fieldparams.RootLength),
+		LogsBloom:     make([]byte, fieldparams.LogsBloomLength),
+		PrevRandao:    make([]byte, fieldparams.BLSSignatureLength),
+		BlockNumber:   0,
+		GasLimit:      0,
+		GasUsed:       0,
+		Timestamp:     0,
+		ExtraData:     make([]byte, 0),
+		BaseFeePerGas: make([]byte, fieldparams.RootLength),
+		BlockHash:     make([]byte, fieldparams.RootLength),
+		Transactions:  make([][]byte, 0),
+	})
+	assert.NoError(t, err)
+
+	var b []byte
+	b, err = wsb.MarshalSSZTo(b)
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, len(b))
+	encoded, err := wsb.MarshalSSZ()
+	require.NoError(t, err)
+	assert.NotEqual(t, 0, wsb.SizeSSZ())
+	assert.NoError(t, wsb.UnmarshalSSZ(encoded))
+	rt, err := wsb.HashTreeRoot()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, rt)
+}
+
+func TestWrapExecutionPayloadHeader_SSZ(t *testing.T) {
+	wsb, err := wrapper.WrappedExecutionPayloadHeader(&enginev1.ExecutionPayloadHeader{
+		ParentHash:       make([]byte, fieldparams.RootLength),
+		FeeRecipient:     make([]byte, fieldparams.FeeRecipientLength),
+		StateRoot:        make([]byte, fieldparams.RootLength),
+		ReceiptsRoot:     make([]byte, fieldparams.RootLength),
+		LogsBloom:        make([]byte, fieldparams.LogsBloomLength),
+		PrevRandao:       make([]byte, fieldparams.BLSSignatureLength),
+		BlockNumber:      0,
+		GasLimit:         0,
+		GasUsed:          0,
+		Timestamp:        0,
+		ExtraData:        make([]byte, 0),
+		BaseFeePerGas:    make([]byte, fieldparams.RootLength),
+		BlockHash:        make([]byte, fieldparams.RootLength),
+		TransactionsRoot: make([]byte, fieldparams.RootLength),
+	})
+	assert.NoError(t, err)
+
+	var b []byte
+	b, err = wsb.MarshalSSZTo(b)
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, len(b))
+	encoded, err := wsb.MarshalSSZ()
+	require.NoError(t, err)
+	assert.NotEqual(t, 0, wsb.SizeSSZ())
+	assert.NoError(t, wsb.UnmarshalSSZ(encoded))
+	rt, err := wsb.HashTreeRoot()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, rt)
+}
