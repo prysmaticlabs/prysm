ec809fff91dba814fd69c8128c4359dbd0f6ee43
terence tsao <terence@prysmaticlabs.com>
2022-07-12 14:27:00 -0700

    Log error instead of data

--- beacon-chain/powchain/engine_client.go
+++ beacon-chain/powchain/engine_client.go
@@ -417,7 +417,7 @@ func handleRPCError(err error) error {
 		if !ok {
 			return errors.Wrapf(err, "got an unexpected error in JSON-RPC response")
 		}
-		return errors.Wrapf(ErrServer, "%v", errWithData.ErrorData())
+		return errors.Wrapf(ErrServer, "%v", errWithData.Error())
 	default:
 		return err
 	}
