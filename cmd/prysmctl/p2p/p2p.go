392346433f1cb4ceeedd1a07c7ed494176de89e1
Raul Jordan <raul@prysmaticlabs.com>
2022-07-13 13:39:16 -0400

    send sub

--- cmd/prysmctl/p2p/p2p.go
+++ cmd/prysmctl/p2p/p2p.go
@@ -4,8 +4,14 @@ import "github.com/urfave/cli/v2"
 
 var Commands = []*cli.Command{
 	{
-		Name:        "p2p",
-		Usage:       "commands for interacting with beacon nodes via p2p",
-		Subcommands: []*cli.Command{requestBlocksCmd},
+		Name:  "p2p",
+		Usage: "commands for interacting with beacon nodes via p2p",
+		Subcommands: []*cli.Command{
+			{
+				Name:        "send",
+				Usage:       "commands for sending p2p rpc requests to beacon nodes",
+				Subcommands: []*cli.Command{requestBlocksCmd},
+			},
+		},
 	},
 }
