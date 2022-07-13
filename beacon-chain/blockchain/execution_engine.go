ab1defc5deb1ceabae3da48dab369af22b2dde90
terencechain <terence@prysmaticlabs.com>
2022-07-13 05:49:40 -0700

    Improve error verbosity when payload_id is nil (#11042)
    * Improve error verbosity with payload ID is nil
    
    * Conflict
    
    * Fix test
    
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/blockchain/execution_engine.go
+++ beacon-chain/blockchain/execution_engine.go
@@ -149,6 +149,11 @@ func (s *Service) notifyForkchoiceUpdate(ctx context.Context, arg *notifyForkcho
 		var pId [8]byte
 		copy(pId[:], payloadID[:])
 		s.cfg.ProposerSlotIndexCache.SetProposerAndPayloadIDs(nextSlot, proposerId, pId)
+	} else if payloadID == nil {
+		log.WithFields(logrus.Fields{
+			"blockHash": fmt.Sprintf("%#x", headPayload.BlockHash()),
+			"slot":      headBlk.Slot(),
+		}).Error("Received nil payload ID on VALID engine response")
 	}
 	return payloadID, nil
 }
