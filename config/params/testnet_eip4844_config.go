41f34aa6ddf41fb55682b81fc912f8f7bb76b8cb
inphi <mlaw2501@gmail.com>
2022-07-13 11:56:16 -0400

    tweak eip4844 testnet configs

--- config/params/testnet_eip4844_config.go
+++ config/params/testnet_eip4844_config.go
@@ -16,11 +16,11 @@ func UseEIP4844NetworkConfig() {
 func EIP4844Config() *BeaconChainConfig {
 	cfg := MainnetConfig().Copy()
 	cfg.MinGenesisTime = 1653318000
-	cfg.GenesisDelay = 604800
-	cfg.MinGenesisActiveValidatorCount = 1
+	cfg.GenesisDelay = 86400
+	cfg.MinGenesisActiveValidatorCount = 2
 	cfg.ConfigName = EIP4844Name
 	cfg.GenesisForkVersion = []byte{0x00, 0x00, 0x0f, 0xfd}
-	cfg.SecondsPerETH1Block = 14
+	cfg.SecondsPerETH1Block = 5
 	cfg.DepositChainID = 1331
 	cfg.DepositNetworkID = 69
 	cfg.AltairForkEpoch = 1
@@ -29,7 +29,7 @@ func EIP4844Config() *BeaconChainConfig {
 	cfg.BellatrixForkVersion = []byte{0x02, 0x00, 0x0f, 0xfd}
 	cfg.Eip4844ForkEpoch = 3
 	cfg.Eip4844ForkVersion = []byte{0x83, 0x00, 0x0f, 0xfd}
-	cfg.TerminalTotalDifficulty = "10"
+	cfg.TerminalTotalDifficulty = "2"
 	cfg.DepositContractAddress = "0x8A04d14125D0FDCDc742F4A05C051De07232EDa4"
 	cfg.DomainBlobsSidecar = [4]byte{0x0a, 0x00, 0x00, 0x00}
 	cfg.InitializeForkSchedule()
