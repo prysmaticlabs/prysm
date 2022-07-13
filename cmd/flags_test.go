fa6cd312ec522414c1d5c889a65f8c938bb1ea51
James He <james@prysmaticlabs.com>
2022-07-13 17:30:52 -0500

    migrating unit test into validator flags

--- cmd/flags_test.go
+++ cmd/flags_test.go
@@ -2,12 +2,9 @@ package cmd
 
 import (
 	"flag"
-	"fmt"
 	"os"
-	"strings"
 	"testing"
 
-	"github.com/prysmaticlabs/prysm/cmd/validator/flags"
 	"github.com/prysmaticlabs/prysm/testing/require"
 	"github.com/urfave/cli/v2"
 )
@@ -36,42 +33,6 @@ func TestLoadFlagsFromConfig(t *testing.T) {
 		},
 		Action: func(cliCtx *cli.Context) error {
 			require.Equal(t, 100, cliCtx.Int("testflag"))
-			return nil
-		},
-	}
-	require.NoError(t, command.Run(context))
-	require.NoError(t, os.Remove("flags_test.yaml"))
-}
-
-func TestLoadFlagsFromConfig_PreProcessing_Web3signer(t *testing.T) {
-	app := cli.App{}
-	set := flag.NewFlagSet("test", 0)
-	context := cli.NewContext(&app, set, nil)
-
-	pubkey1 := "0xbd36226746676565cd40141a7f0fe1445b9a3fbeb222288b226392c4b230ed0b"
-	pubkey2 := "0xbd36226746676565cd40141a7f0fe1445b9a3fbeb222288b226392c4b230ed0a"
-
-	require.NoError(t, os.WriteFile("flags_test.yaml", []byte(fmt.Sprintf("%s:\n - %s\n - %s\n", flags.Web3SignerPublicValidatorKeysFlag.Name,
-		pubkey1,
-		pubkey2)), 0666))
-
-	require.NoError(t, set.Parse([]string{"test-command", "--" + ConfigFileFlag.Name, "flags_test.yaml"}))
-	command := &cli.Command{
-		Name: "test-command",
-		Flags: WrapFlags([]cli.Flag{
-			&cli.StringFlag{
-				Name: ConfigFileFlag.Name,
-			},
-			&cli.StringSliceFlag{
-				Name: flags.Web3SignerPublicValidatorKeysFlag.Name,
-			},
-		}),
-		Before: func(cliCtx *cli.Context) error {
-			return LoadFlagsFromConfig(cliCtx, cliCtx.Command.Flags)
-		},
-		Action: func(cliCtx *cli.Context) error {
-			require.Equal(t, strings.Join([]string{pubkey1, pubkey2}, ","),
-				strings.Join(cliCtx.StringSlice(flags.Web3SignerPublicValidatorKeysFlag.Name), ","))
 			return nil
 		},
 	}
