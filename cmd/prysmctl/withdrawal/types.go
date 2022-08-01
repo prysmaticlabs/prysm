047fd914c26c540eae0716ca94d8f7946af2e774
James He <james@prysmaticlabs.com>
2022-08-01 12:56:52 -0500

    work in progress

---
+++ cmd/prysmctl/withdrawal/types.go
@@ -0,0 +1,34 @@
+package withdrawal
+
+import (
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/prysmaticlabs/prysm/crypto/bls"
+)
+
+type BlsToExecutionEngineMessage struct {
+	ValidatorIndex     uint64 `json:"validator_index" yaml:"validator_index"`
+	FromBlsPubkey      string `json:"from_bls_pubkey" yaml:"from_bls_pubkey"`
+	ToExecutionAddress string `json:"to_execution_address" yaml:"to_execution_address"`
+}
+
+func (d *BlsToExecutionEngineMessage) FromBlsPubkeyAsBytes() ([]byte, error) {
+	return hexutil.Decode(d.FromBlsPubkey)
+}
+
+func (d *BlsToExecutionEngineMessage) ToExecutionAddressAsBytes() ([]byte, error) {
+	return hexutil.Decode(d.ToExecutionAddress)
+}
+
+type BlsToExecutionEngineFile struct {
+	Version   uint64                       `json:"version" yaml:"version"`
+	Message   *BlsToExecutionEngineMessage `json:"message" yaml:"message"`
+	Signature string                       `json:"signature" yaml:"signature"`
+}
+
+func (b *BlsToExecutionEngineFile) SignatureAsBls() (bls.Signature, error) {
+	bytevalue, err := hexutil.Decode(b.Signature)
+	if err != nil {
+		return nil, err
+	}
+	return bls.SignatureFromBytes(bytevalue)
+}
--- cmd/prysmctl/withdrawal/withdrawal.go
+++ cmd/prysmctl/withdrawal/withdrawal.go
@@ -1,11 +1,18 @@
 package withdrawal
 
 import (
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"github.com/prysmaticlabs/prysm/api/client/beacon"
 	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v2"
 )
 
 var withdrawalFlags = struct {
-	File string
+	BeaconNodeHost string
+	File           string
 }{}
 
 var Commands = []*cli.Command{
@@ -16,15 +23,41 @@ var Commands = []*cli.Command{
 		Action:  cliActionLatest,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
+				Name:        "beacon-node-host",
+				Usage:       "host:port for beacon node to query",
+				Destination: &withdrawalFlags.BeaconNodeHost,
+				Value:       "http://localhost:3500",
+			},
+			&cli.StringFlag{
 				Name:        "file",
 				Usage:       "file location for for the blsToExecutionAddress JSON or Yaml",
 				Destination: &withdrawalFlags.File,
-				Value:       "",
+				Value:       "./blsToExecutionAddress.json",
 			},
 		},
 	},
 }
 
 func cliActionLatest(_ *cli.Context) error {
+	f := withdrawalFlags
+
+	cleanpath := filepath.Clean(f.File)
+	b, err := os.ReadFile(cleanpath)
+	if err != nil {
+		return errors.Wrap(err, "failed to open file")
+	}
+	var to BlsToExecutionEngineFile
+	if err := yaml.Unmarshal(b, to); err != nil {
+		return errors.Wrap(err, "failed to unmarshal file")
+	}
+	if to.Message == nil {
+		return errors.New("the message field in file is empty")
+	}
+
+	_, err = beacon.NewClient(withdrawalFlags.BeaconNodeHost)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
