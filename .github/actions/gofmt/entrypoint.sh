8e1b098ffe78556468ba7246deceff249f8b6774
Taranpreet26311 <taran@prysmaticlabs.com>
2022-08-05 20:19:51 +0530

    Added gofmt detail flag

--- .github/actions/gofmt/entrypoint.sh
+++ .github/actions/gofmt/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 cd $GITHUB_WORKSPACE
 
 # Check if any files are not formatted.
-nonformatted="$(gofmt -l $1 2>&1)"
+nonformatted="$(gofmt -l -e $1 2>&1)"
 
 # Return if `go fmt` passes.
 [ -z "$nonformatted" ] && exit 0
