eb57e9c3bc363e7deb22336ef84c5b6861fc0073
terencechain <terence@prysmaticlabs.com>
2022-08-11 10:47:17 -0700

    Update beacon-chain/blockchain/service.go
    Co-authored-by: Preston Van Loon <preston@prysmaticlabs.com>

--- beacon-chain/blockchain/service.go
+++ beacon-chain/blockchain/service.go
@@ -158,7 +158,7 @@ func (s *Service) Stop() error {
 func (s *Service) Status() error {
 	optimistic, err := s.IsOptimistic(s.ctx)
 	if err != nil {
-		return fmt.Errorf("failed to check if service is optimistic: %v", err)
+		return errors.Wrap(err, "failed to check if service is optimistic")
 	}
 	if optimistic {
 		return errors.New("service is optimistic, and only limited service functionality is provided " +
