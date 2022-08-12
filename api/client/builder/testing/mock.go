320c3c4d3a345bfe12a53d5cae95d9365cd39324
rkapka <rkapka@wp.pl>
2022-08-12 17:14:51 +0200

    add comments to mock

--- api/client/builder/testing/mock.go
+++ api/client/builder/testing/mock.go
@@ -9,22 +9,27 @@ import (
 	ethpb "github.com/prysmaticlabs/prysm/proto/prysm/v1alpha1"
 )
 
+// MockClient is a mock implementation of BuilderClient.
 type MockClient struct {
 	RegisteredVals map[[48]byte]bool
 }
 
+// NewClient creates a new, correctly initialized mock.
 func NewClient() MockClient {
 	return MockClient{RegisteredVals: map[[48]byte]bool{}}
 }
 
+// NodeURL --
 func (MockClient) NodeURL() string {
 	return ""
 }
 
+// GetHeader --
 func (MockClient) GetHeader(_ context.Context, _ types.Slot, _ [32]byte, _ [48]byte) (*ethpb.SignedBuilderBid, error) {
 	return nil, nil
 }
 
+// RegisterValidator --
 func (m MockClient) RegisterValidator(_ context.Context, svr []*ethpb.SignedValidatorRegistrationV1) error {
 	for _, r := range svr {
 		b := bytesutil.ToBytes48(r.Message.Pubkey)
@@ -33,10 +38,12 @@ func (m MockClient) RegisterValidator(_ context.Context, svr []*ethpb.SignedVali
 	return nil
 }
 
+// SubmitBlindedBlock --
 func (MockClient) SubmitBlindedBlock(_ context.Context, _ *ethpb.SignedBlindedBeaconBlockBellatrix) (*v1.ExecutionPayload, error) {
 	return nil, nil
 }
 
+// Status --
 func (MockClient) Status(_ context.Context) error {
 	return nil
 }
