ee2d22559d9ad1459fad0f71126edbe8eda9eec6
nisdas <nishdas93@gmail.com>
2022-08-12 20:24:21 +0800

    remove other deprecated flags

--- config/features/deprecated_flags.go
+++ config/features/deprecated_flags.go
@@ -12,32 +12,8 @@ var (
 		Usage:  deprecatedUsage,
 		Hidden: true,
 	}
-	deprecatedEnableNativeState = &cli.BoolFlag{
-		Name:   "enable-native-state",
-		Usage:  deprecatedUsage,
-		Hidden: true,
-	}
-	deprecatedEnablePeerScorer = &cli.BoolFlag{
-		Name:   "enable-peer-scorer",
-		Usage:  deprecatedUsage,
-		Hidden: true,
-	}
-	deprecatedEnableGossipBatchAggregation = &cli.BoolFlag{
-		Name:   "enable-gossip-batch-aggregation",
-		Usage:  deprecatedUsage,
-		Hidden: true,
-	}
-	deprecatedEnablePullTips = &cli.BoolFlag{
-		Name:   "experimental-disable-boundary-checks",
-		Usage:  deprecatedUsage,
-		Hidden: true,
-	}
 )
 
 var deprecatedFlags = []cli.Flag{
 	exampleDeprecatedFeatureFlag,
-	deprecatedEnableNativeState,
-	deprecatedEnablePeerScorer,
-	deprecatedEnableGossipBatchAggregation,
-	deprecatedEnablePullTips,
 }
