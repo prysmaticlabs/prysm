0edbab1aafafd5e542cdbdbec9f425eb88b4326c
nisdas <nishdas93@gmail.com>
2022-08-17 13:12:55 +0800

    remove mmap flag

--- beacon-chain/db/slasherkv/kv_test.go
+++ beacon-chain/db/slasherkv/kv_test.go
@@ -9,7 +9,7 @@ import (
 
 // setupDB instantiates and returns a Store instance.
 func setupDB(t testing.TB) *Store {
-	db, err := NewKVStore(context.Background(), t.TempDir(), &Config{})
+	db, err := NewKVStore(context.Background(), t.TempDir())
 	require.NoError(t, err, "Failed to instantiate DB")
 	t.Cleanup(func() {
 		require.NoError(t, db.Close(), "Failed to close database")
