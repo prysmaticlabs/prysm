029c81a2e4550d92b450845378e30b164d716ee7
Nishant Das <nishdas93@gmail.com>
2022-08-19 21:09:29 +0800

    Switch Down Libp2p Logging Level By Default (#11266)
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- cmd/beacon-chain/main.go
+++ cmd/beacon-chain/main.go
@@ -231,6 +231,13 @@ func startNode(ctx *cli.Context) error {
 		return err
 	}
 	logrus.SetLevel(level)
+	// Set libp2p logger to only panic logs for the info level.
+	golog.SetAllLoggers(golog.LevelPanic)
+
+	if level == logrus.DebugLevel {
+		// Set libp2p logger to error logs for the debug level.
+		golog.SetAllLoggers(golog.LevelError)
+	}
 	if level == logrus.TraceLevel {
 		// libp2p specific logging.
 		golog.SetAllLoggers(golog.LevelDebug)
