657010c9bb84622f4f5cf47a728b24b49871044c
Potuz <potuz@prysmaticlabs.com>
2022-09-01 11:02:43 -0300

    fix tests

--- beacon-chain/blockchain/process_block_helpers.go
+++ beacon-chain/blockchain/process_block_helpers.go
@@ -277,7 +277,7 @@ func (s *Service) fillInForkChoiceMissingBlocks(ctx context.Context, blk interfa
 	if len(pendingNodes) == 1 {
 		return nil
 	}
-	if !s.ForkChoicer().HasNode(s.ensureRootNotZeros(root)) {
+	if root != s.ensureRootNotZeros(finalized.Root) && !s.ForkChoicer().HasNode(root) {
 		return errNotDescendantOfFinalized
 	}
 	return s.cfg.ForkChoiceStore.InsertOptimisticChain(ctx, pendingNodes)
