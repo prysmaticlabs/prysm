8743e6a688dabcd4a65d5e71f6c75eb8eb737b3b
Potuz <potuz@prysmaticlabs.com>
2022-09-04 23:26:10 -0300

    Harden witholding fix (#11397)
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- beacon-chain/forkchoice/protoarray/store.go
+++ beacon-chain/forkchoice/protoarray/store.go
@@ -890,11 +890,12 @@ func (s *Store) viableForHead(node *Node) bool {
 		if !justified && s.justifiedCheckpoint.Epoch+1 == currentEpoch {
 			if node.unrealizedJustifiedEpoch+1 >= currentEpoch {
 				justified = true
+			}
+			if node.unrealizedFinalizedEpoch >= s.finalizedCheckpoint.Epoch {
 				finalized = true
 			}
 		}
 	}
-
 	return justified && finalized
 }
 
