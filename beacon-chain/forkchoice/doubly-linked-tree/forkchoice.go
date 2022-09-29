ea3e174a9260b5a9eb8e81e480572ef2ab9067fe
Potuz <potuz@prysmaticlabs.com>
2022-09-29 15:37:53 -0300

    DeepSource complains

--- beacon-chain/forkchoice/doubly-linked-tree/forkchoice.go
+++ beacon-chain/forkchoice/doubly-linked-tree/forkchoice.go
@@ -665,7 +665,7 @@ func (f *ForkChoice) ForkChoiceDump(ctx context.Context) (*v1.ForkChoiceResponse
 
 }
 
-//func SetBalancesByRoot sets the balanceByRoot handler in forkchoice
+// SetBalancesByRoot sets the balanceByRoot handler in forkchoice
 func (f *ForkChoice) SetBalancesByRoot(handler func(context.Context, [32]byte) ([]uint64, error)) {
 	f.balancesByRoot = handler
 }
--- beacon-chain/state/stategen/mock/mock.go
+++ beacon-chain/state/stategen/mock/mock.go
@@ -52,7 +52,7 @@ func (m *MockStateManager) StateByRoot(_ context.Context, blockRoot [32]byte) (s
 }
 
 // BalancesByRoot --
-func (m *MockStateManager) BalancesByRoot(_ context.Context, blockRoot [32]byte) ([]uint64, error) {
+func (_ *MockStateManager) BalancesByRoot(_ context.Context, _ [32]byte) ([]uint64, error) {
 	return []uint64{}, nil
 }
 
