ba2edbc50ce5be3094b646a0586aca018546d923
terence tsao <terence@prysmaticlabs.com>
2022-10-16 20:29:00 -0700

    Fix complains and bad test

--- proto/prysm/v1alpha1/cloners_test.go
+++ proto/prysm/v1alpha1/cloners_test.go
@@ -667,21 +667,6 @@ func genSignedBeaconBlockBellatrix() *v1alpha1.SignedBeaconBlockBellatrix {
 	}
 }
 
-func genBlindedBeaconBlockBodyBellatrix() *v1alpha1.BlindedBeaconBlockBodyBellatrix {
-	return &v1alpha1.BlindedBeaconBlockBodyBellatrix{
-		RandaoReveal:           bytes(),
-		Eth1Data:               genEth1Data(),
-		Graffiti:               bytes(),
-		ProposerSlashings:      genProposerSlashings(5),
-		AttesterSlashings:      genAttesterSlashings(5),
-		Attestations:           genAttestations(10),
-		Deposits:               genDeposits(5),
-		VoluntaryExits:         genSignedVoluntaryExits(12),
-		SyncAggregate:          genSyncAggregate(),
-		ExecutionPayloadHeader: genPayloadHeader(),
-	}
-}
-
 func genSyncCommitteeMessage() *v1alpha1.SyncCommitteeMessage {
 	return &v1alpha1.SyncCommitteeMessage{
 		Slot:           424555,
