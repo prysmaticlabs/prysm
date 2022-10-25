3eecb636b17c0d3f339b6c053832260323bb933e
James He <james@prysmaticlabs.com>
2022-10-25 14:38:51 -0500

    testing adding delay to evaluator

--- testing/endtoend/evaluators/node.go
+++ testing/endtoend/evaluators/node.go
@@ -101,6 +101,7 @@ func peersConnect(conns ...*grpc.ClientConn) error {
 	ctx := context.Background()
 	for _, conn := range conns {
 		nodeClient := eth.NewNodeClient(conn)
+		time.Sleep(connTimeDelay * 3)
 		peersResp, err := nodeClient.ListPeers(ctx, &emptypb.Empty{})
 		if err != nil {
 			return err
