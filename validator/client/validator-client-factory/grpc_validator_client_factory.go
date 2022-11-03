fda14df32737883a24338ad4e95e33aec31a8d64
Patrice Vignola <vignola.patrice@gmail.com>
2022-11-02 21:13:22 -0700

    Refactor to use iface.ValidatorClient instead of ethpb.BeaconNodeValidatorClient

---
+++ validator/client/validator-client-factory/grpc_validator_client_factory.go
@@ -0,0 +1,11 @@
+package validator_client_factory
+
+import (
+	grpcApi "github.com/prysmaticlabs/prysm/v3/validator/client/grpc-api"
+	"github.com/prysmaticlabs/prysm/v3/validator/client/iface"
+	"google.golang.org/grpc"
+)
+
+func NewValidatorClient(cc grpc.ClientConnInterface) iface.ValidatorClient {
+	return grpcApi.NewGrpcValidatorClient(cc)
+}
