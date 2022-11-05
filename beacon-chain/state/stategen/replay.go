afc48c648523e93f744781cbd2ce52df899fc98f
terence tsao <terence@prysmaticlabs.com>
2022-11-05 12:45:01 -0700

    State change and config changes for Capella

--- beacon-chain/state/stategen/replay.go
+++ beacon-chain/state/stategen/replay.go
@@ -203,7 +203,7 @@ func ReplayProcessSlots(ctx context.Context, state state.BeaconState, slot types
 					tracing.AnnotateError(span, err)
 					return nil, errors.Wrap(err, "could not process epoch with optimizations")
 				}
-			case version.Altair, version.Bellatrix:
+			case version.Altair, version.Bellatrix, version.Capella:
 				state, err = altair.ProcessEpoch(ctx, state)
 				if err != nil {
 					tracing.AnnotateError(span, err)
