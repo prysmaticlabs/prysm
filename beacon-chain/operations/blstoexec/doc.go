35deaf742cc66738f00a035960cbb12864eed0e1
rkapka <rkapka@wp.pl>
2022-11-07 19:18:01 +0100

    rename package

--- beacon-chain/operations/blstoexec/doc.go
+++ beacon-chain/operations/blstoexec/doc.go
@@ -2,4 +2,4 @@
 // BLS to execution change events by the beacon node, handling their lifecycle
 // and performing integrity checks before serving them as objects
 // for validators to include in blocks.
-package blstoexecchanges
+package blstoexec
--- beacon-chain/operations/blstoexec/pool_test.go
+++ beacon-chain/operations/blstoexec/pool_test.go
@@ -1,4 +1,4 @@
-package blstoexecchanges
+package blstoexec
 
 import (
 	"sync"
