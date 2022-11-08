28c250ae49291f501f9a3eb060c1bcb1a6cb2b97
Saolyn <sammy@rosso.red>
2022-11-08 16:14:43 +0100

    Initial deposit tree test

---
+++ beacon-chain/cache/depositsnapshot/deposit_tree_test.go
@@ -0,0 +1,21 @@
+package depositsnapshot
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/v3/testing/assert"
+)
+
+func Test_AddToEmptyDepositTree(t *testing.T) {
+	dt := &DepositTree{
+		tree:                    &ZeroNode{depth: DepositContractDepth},
+		depositCount:            0,
+		finalizedExecutionblock: [32]byte{},
+	}
+	newDt := NewDepositTree()
+	assert.Equal(t, dt.tree.GetRoot(), newDt.tree.GetRoot())
+	err := newDt.AddDeposit(hexString(t, fmt.Sprintf("%064d", 1)), uint64(0))
+	assert.NoError(t, err)
+	assert.NotEqual(t, dt.tree.GetRoot(), newDt.tree.GetRoot())
+}
