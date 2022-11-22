e053aeee3fe2b46c15df1fcca95e6f24cf6094ee
Giuseppe Valente <giuseppe@polymerlabs.org>
2022-11-22 13:48:15 -0800

    feat: implement update validation

--- beacon-chain/light-client/store.go
+++ beacon-chain/light-client/store.go
@@ -6,8 +6,12 @@ package light_client
 import (
 	"bytes"
 	"errors"
+	"github.com/prysmaticlabs/prysm/v3/beacon-chain/core/signing"
 	types "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v3/container/trie"
+	"github.com/prysmaticlabs/prysm/v3/crypto/bls/blst"
+	"github.com/prysmaticlabs/prysm/v3/crypto/bls/common"
+	"github.com/prysmaticlabs/prysm/v3/encoding/bytesutil"
 	ethpbv1 "github.com/prysmaticlabs/prysm/v3/proto/eth/v1"
 	ethpbv2 "github.com/prysmaticlabs/prysm/v3/proto/eth/v2"
 	ethpb "github.com/prysmaticlabs/prysm/v3/proto/prysm/v1alpha1"
@@ -17,8 +21,10 @@ import (
 const (
 	finalizedRootIndex        = uint64(105)
 	currentSyncCommitteeIndex = uint64(54)
-	// TODO: read this from the config
+	altairForkEpoch           = 74240
+	// TODO: read these from the config
 	minSyncCommitteeParticipants = uint64(1)
+	genesisSlot                  = types.Slot(0)
 )
 
 type Store struct {
@@ -35,6 +41,19 @@ func getSubtreeIndex(index uint64) uint64 {
 	return index % uint64(math.Pow(2, float64(floorLog2(index-1))))
 }
 
+// TODO: this should be in the proto
+func hashTreeRoot(committee *ethpbv2.SyncCommittee) []byte {
+	v1alpha1Committee := ethpb.SyncCommittee{
+		Pubkeys:         committee.GetPubkeys(),
+		AggregatePubkey: committee.GetAggregatePubkey(),
+	}
+	root, err := v1alpha1Committee.HashTreeRoot()
+	if err != nil {
+		panic(err)
+	}
+	return root[:]
+}
+
 // NewStore implements initialize_light_client_store from the spec.
 func NewStore(trustedBlockRoot [32]byte,
 	bootstrap ethpbv2.Bootstrap) *Store {
@@ -45,14 +64,9 @@ func NewStore(trustedBlockRoot [32]byte,
 	if !bytes.Equal(trustedBlockRoot[:], bootstrapRoot[:]) {
 		panic("trusted block root does not match bootstrap header")
 	}
-	v1alpha1Committee := ethpb.SyncCommittee{
-		Pubkeys:         bootstrap.CurrentSyncCommittee.GetPubkeys(),
-		AggregatePubkey: bootstrap.CurrentSyncCommittee.GetAggregatePubkey(),
-	}
-	syncCommitteeRoot, err := v1alpha1Committee.HashTreeRoot()
 	if !trie.VerifyMerkleProofWithDepth(
 		bootstrap.Header.StateRoot,
-		syncCommitteeRoot[:],
+		hashTreeRoot(bootstrap.CurrentSyncCommittee),
 		getSubtreeIndex(currentSyncCommitteeIndex),
 		bootstrap.CurrentSyncCommitteeBranch,
 		uint64(floorLog2(currentSyncCommitteeIndex))) {
@@ -75,6 +89,16 @@ func (s *Store) getSafetyThreshold() uint64 {
 		float64(s.currentMaxActiveParticipants)) / 2))
 }
 
+func computeForkVersion(epoch uint64) [4]byte {
+	// TODO: export these
+	altairForkVersion := bytesutil.Uint32ToBytes4(0x01000000)
+	genesisForkVersion := bytesutil.Uint32ToBytes4(0x00000000)
+	if epoch >= altairForkEpoch {
+		return altairForkVersion
+	}
+	return genesisForkVersion
+}
+
 func (s *Store) ValidateUpdate(update *Update,
 	currentSlot types.Slot,
 	genesisValidatorsRoot []byte) error {
@@ -90,7 +114,112 @@ func (s *Store) ValidateUpdate(update *Update,
 		update.GetAttestedHeader().Slot >= update.GetFinalizedHeader().Slot) {
 		return errors.New("update skips a sync committee period")
 	}
-	// TODO: resume
+	storePeriod := computeSyncCommitteePeriodAtSlot(s.finalizedHeader.Slot)
+	updateSignaturePeriod := computeSyncCommitteePeriodAtSlot(update.GetSignatureSlot())
+	if s.isNextSyncCommitteeKnown() {
+		if !(updateSignaturePeriod == storePeriod || updateSignaturePeriod == storePeriod+1) {
+			return errors.New("update skips a sync committee period")
+		}
+	} else {
+		if updateSignaturePeriod != storePeriod {
+			return errors.New("update skips a sync committee period")
+		}
+	}
+
+	// Verify update is relevant
+	updateAttestedPeriod := computeSyncCommitteePeriodAtSlot(update.GetAttestedHeader().Slot)
+	updateHasNextSyncCommittee := !s.isNextSyncCommitteeKnown() && (update.IsSyncCommiteeUpdate() && updateAttestedPeriod == storePeriod)
+	if !(update.GetAttestedHeader().Slot > s.finalizedHeader.Slot || updateHasNextSyncCommittee) {
+		return errors.New("update is not relevant")
+	}
+
+	// Verify that the finality branch, if present, confirms finalized header to match the finalized checkpoint root
+	// saved in the state of attested header. Note that the genesis finalized checkpoint root is represented as a zero
+	// hash.
+	if !update.IsFinalityUpdate() {
+		if update.GetFinalizedHeader() != &(ethpbv1.BeaconBlockHeader{}) {
+			return errors.New("finality branch is present but update is not finality")
+		}
+	} else {
+		var finalizedRoot [32]byte
+		if update.GetFinalizedHeader().Slot == genesisSlot {
+			if update.GetFinalizedHeader() != &(ethpbv1.BeaconBlockHeader{}) {
+				return errors.New("finality branch is present but update is not finality")
+			}
+			finalizedRoot = [32]byte{}
+		} else {
+			var err error
+			if finalizedRoot, err = update.GetFinalizedHeader().HashTreeRoot(); err != nil {
+				return err
+			}
+		}
+		if !trie.VerifyMerkleProofWithDepth(
+			update.GetAttestedHeader().StateRoot,
+			finalizedRoot[:],
+			getSubtreeIndex(finalizedRootIndex),
+			update.GetFinalityBranch(),
+			uint64(floorLog2(finalizedRootIndex))) {
+			return errors.New("finality branch is invalid")
+		}
+	}
+
+	// Verify that the next sync committee, if present, actually is the next sync committee saved in the state of the
+	// attested header
+	if !update.IsSyncCommiteeUpdate() {
+		if update.GetNextSyncCommittee() != &(ethpbv2.SyncCommittee{}) {
+			return errors.New("sync committee branch is present but update is not sync committee")
+		}
+	} else {
+		if updateAttestedPeriod == storePeriod && s.isNextSyncCommitteeKnown() {
+			if update.GetNextSyncCommittee() != s.nextSyncCommittee {
+				return errors.New("next sync committee is not known")
+			}
+		}
+		if !trie.VerifyMerkleProofWithDepth(
+			update.GetAttestedHeader().StateRoot,
+			hashTreeRoot(update.GetNextSyncCommittee())[:],
+			getSubtreeIndex(ethpbv2.NextSyncCommitteeIndex),
+			update.GetNextSyncCommitteeBranch(),
+			uint64(floorLog2(ethpbv2.NextSyncCommitteeIndex))) {
+			return errors.New("sync committee branch is invalid")
+		}
+	}
+
+	var syncCommittee *ethpbv2.SyncCommittee
+	// Verify sync committee aggregate signature
+	if updateSignaturePeriod == storePeriod {
+		syncCommittee = s.currentSyncCommittee
+	} else {
+		syncCommittee = s.nextSyncCommittee
+	}
+	participantPubkeys := []common.PublicKey{}
+	for i, bit := range syncAggregate.SyncCommitteeBits {
+		if bit > 0 {
+			publicKey, err := blst.PublicKeyFromBytes(syncCommittee.Pubkeys[i])
+			if err != nil {
+				return err
+			}
+			participantPubkeys = append(participantPubkeys, publicKey)
+		}
+	}
+	forkVersion := computeForkVersion(computeEpochAtSlot(update.GetSignatureSlot()))
+	// TODO: export this somewhere
+	domainSyncCommittee := bytesutil.Uint32ToBytes4(0x07000000)
+	domain, err := signing.ComputeDomain(domainSyncCommittee, forkVersion[:], genesisValidatorsRoot)
+	if err != nil {
+		return err
+	}
+	signingRoot, err := signing.ComputeSigningRoot(update.GetAttestedHeader(), domain)
+	if err != nil {
+		return err
+	}
+	signature, err := blst.SignatureFromBytes(syncAggregate.SyncCommitteeSignature)
+	if err != nil {
+		return err
+	}
+	if !signature.FastAggregateVerify(participantPubkeys, signingRoot) {
+		return errors.New("sync committee signature is invalid")
+	}
 	return nil
 }
 
