975295814cdcef4f5a9e6d807451516851a56a78
James He <james@prysmaticlabs.com>
2022-11-23 15:27:07 -0600

    small adjustments to logic

--- cmd/prysmctl/withdrawal/withdrawal.go
+++ cmd/prysmctl/withdrawal/withdrawal.go
@@ -2,6 +2,7 @@ package withdrawal
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -39,6 +40,7 @@ func setWithdrawlAddress(c *cli.Context, r io.Reader) error {
 	if to.Message == nil {
 		return errors.New("the message field in file is empty")
 	}
+
 	u, err := url.ParseRequestURI(f.BeaconNodeHost)
 	if err != nil {
 		return errors.Wrap(err, "invalid format, unable to parse url")
@@ -46,7 +48,6 @@ func setWithdrawlAddress(c *cli.Context, r io.Reader) error {
 	if u.Scheme == "" || u.Host == "" {
 		return fmt.Errorf("url must be in the format of http(s)://host:port url used: %v", f.BeaconNodeHost)
 	}
-
 	withdrawalConfirmation := to.Message.ToExecutionAddress
 
 	withdraw, err := withdrawalPrompt(withdrawalConfirmation, r)
@@ -59,21 +60,24 @@ func setWithdrawlAddress(c *cli.Context, r io.Reader) error {
 		return nil
 	}
 
+	body, err := json.Marshal(to) //:todo: change this to apimiddleware object.
+	if err != nil {
+		return errors.Wrap(err, "error encoding the BlsToExecutionEngineFile")
+	}
+
 	ctx, span := trace.StartSpan(c.Context, "withdrawal.blsToExecutionAddress")
 	defer span.End()
 
 	fullpath := f.BeaconNodeHost + apiPath
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fullpath, bytes.NewBuffer(b)) //TODO:change this b
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fullpath, bytes.NewBuffer(body))
 	if err != nil {
 		return errors.Wrap(err, "invalid format, failed to create new Post Request Object")
 	}
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
-	//start := time.Now()
 	resp, err := client.Do(req)
-	//duration := time.Since(start)
 	if err != nil {
 		return err
 	}
@@ -88,11 +92,15 @@ func withdrawalPrompt(confirmationMessage string, r io.Reader) (string, error) {
 
 	au := aurora.NewAurora(true)
 	promptHeader := au.Red("===============IMPORTANT===============")
-	promptDescription := "Withdrawing funds is not yet possible. " +
+	promptDescription := "YOU ARE ATTEMPTING TO CHANGE THE BLS WITHDRAWAL ADDRESS TO AN ETHEREUM ADDRESS. " +
+		"This action will allow you to partially withdraw any amount over the 32 staked eth in your validator balance. " +
+		"You will also be entitled to the full withdrawal if your validator has exited. " +
 		"Please navigate to the following website and make sure you understand the current implications " +
-		"of a voluntary exit before making the final decision:"
+		"of changing your bls withdrawal address to an ethereum address. " +
+		"THIS ACTION WILL NOT BE REVERSIBLE ONCE INCLUDED. " +
+		"You will NOT be able to change the address again once changed. "
 	promptURL := au.Blue("https://docs.prylabs.network/docs/wallet/exiting-a-validator/#withdrawal-delay-warning")
-	promptQuestion := "If you still want to continue with the voluntary exit, please input a phrase found at the end " +
+	promptQuestion := "If you still want to continue with changing the bls withdrawal address, please input a phrase found at the end " +
 		"of the page from the above URL"
 	promptText := fmt.Sprintf("%s\n%s\n%s\n%s", promptHeader, promptDescription, promptURL, promptQuestion)
 	return prompt.ValidatePrompt(r, promptText, func(input string) error {
