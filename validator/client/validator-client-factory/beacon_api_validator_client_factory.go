7ab5851c547f0783ff0d8e23e7f4909bab64ceb9
Patrice Vignola <vignola.patrice@gmail.com>
2022-11-24 03:09:07 -0800

    Add a gRPC fallback mode to the validator Beacon REST API (#11679)
    * Add a gRPC fallback mode to the validator Beacon REST API
    
    * Remove --beacon_api_grpc_fallback flag
    
    * Add missing bazel dependency
    
    * Reorder dependency per gazelle check
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- validator/client/validator-client-factory/beacon_api_validator_client_factory.go
+++ validator/client/validator-client-factory/beacon_api_validator_client_factory.go
@@ -5,10 +5,12 @@ package validator_client_factory
 
 import (
 	beaconApi "github.com/prysmaticlabs/prysm/v3/validator/client/beacon-api"
+	grpcApi "github.com/prysmaticlabs/prysm/v3/validator/client/grpc-api"
 	"github.com/prysmaticlabs/prysm/v3/validator/client/iface"
 	validatorHelpers "github.com/prysmaticlabs/prysm/v3/validator/helpers"
 )
 
 func NewValidatorClient(validatorConn validatorHelpers.NodeConnection) iface.ValidatorClient {
-	return beaconApi.NewBeaconApiValidatorClient(validatorConn.GetBeaconApiUrl(), validatorConn.GetBeaconApiTimeout())
+	fallbackClient := grpcApi.NewGrpcValidatorClient(validatorConn.GetGrpcClientConn())
+	return beaconApi.NewBeaconApiValidatorClientWithFallback(validatorConn.GetBeaconApiUrl(), validatorConn.GetBeaconApiTimeout(), fallbackClient)
 }
