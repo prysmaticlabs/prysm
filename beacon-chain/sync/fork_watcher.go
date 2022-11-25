dda60721f55e9b3d4afca24b14f32b1f5aaf4dde
nisdas <nishdas93@gmail.com>
2022-11-25 18:34:14 +0800

    satisfy linter

--- beacon-chain/sync/fork_watcher.go
+++ beacon-chain/sync/fork_watcher.go
@@ -100,10 +100,10 @@ func (s *Service) deregisterFromPastFork(currEpoch types.Epoch) error {
 			return errors.Wrap(err, "failed to determine previous epoch fork data")
 		}
 
-		switch prevFork.Epoch {
-		case params.BeaconConfig().GenesisEpoch:
+		if prevFork.Epoch == params.BeaconConfig().GenesisEpoch {
 			s.unregisterPhase0Handlers()
 		}
+
 		// Run through all our current active topics and see
 		// if there are any subscriptions to be removed.
 		for _, t := range s.subHandler.allTopics() {
