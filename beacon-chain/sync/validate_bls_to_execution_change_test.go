764e1f523c1993538f55bd731f2aea0efd98d8a9
nisdas <nishdas93@gmail.com>
2022-11-26 20:05:39 +0800

    make it all work

--- beacon-chain/sync/validate_bls_to_execution_change_test.go
+++ beacon-chain/sync/validate_bls_to_execution_change_test.go
@@ -18,23 +18,20 @@ import (
 	"github.com/prysmaticlabs/prysm/v3/beacon-chain/p2p"
 	"github.com/prysmaticlabs/prysm/v3/beacon-chain/p2p/encoder"
 	mockp2p "github.com/prysmaticlabs/prysm/v3/beacon-chain/p2p/testing"
-	p2ptypes "github.com/prysmaticlabs/prysm/v3/beacon-chain/p2p/types"
 	"github.com/prysmaticlabs/prysm/v3/beacon-chain/state/stategen"
 	mockSync "github.com/prysmaticlabs/prysm/v3/beacon-chain/sync/initial-sync/testing"
 	"github.com/prysmaticlabs/prysm/v3/config/params"
-	types "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
-	"github.com/prysmaticlabs/prysm/v3/encoding/bytesutil"
 	ethpb "github.com/prysmaticlabs/prysm/v3/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/v3/testing/assert"
+	"github.com/prysmaticlabs/prysm/v3/testing/util"
 	"github.com/prysmaticlabs/prysm/v3/time/slots"
 )
 
 func TestService_ValidateBlsToExecutionChange(t *testing.T) {
 	beaconDB := testingdb.SetupDB(t)
-	headRoot, keys := fillUpBlocksAndState(context.Background(), t, beaconDB)
 	defaultTopic := p2p.BlsToExecutionChangeSubnetTopicFormat
 	fakeDigest := []byte{0xAB, 0x00, 0xCC, 0x9E}
-	//wantedExecAddress := []byte{0xd8, 0xdA, 0x6B, 0xF2, 0x69, 0x64, 0xaF, 0x9D, 0x7e, 0xEd, 0x9e, 0x03, 0xE5, 0x34, 0x15, 0xD3, 0x7a, 0xA9, 0x60, 0x45}
+	wantedExecAddress := []byte{0xd8, 0xdA, 0x6B, 0xF2, 0x69, 0x64, 0xaF, 0x9D, 0x7e, 0xEd, 0x9e, 0x03, 0xE5, 0x34, 0x15, 0xD3, 0x7a, 0xA9, 0x60, 0x45}
 	defaultTopic = defaultTopic + "/" + encoder.ProtocolSuffixSSZSnappy
 	chainService := &mockChain.ChainService{
 		Genesis:        time.Now(),
@@ -151,81 +148,33 @@ func TestService_ValidateBlsToExecutionChange(t *testing.T) {
 			want: pubsub.ValidationIgnore,
 		},
 		{
-			name: "Non-existent block root",
+			name: "Non-existent Validator Index",
 			svc: NewService(context.Background(),
 				WithP2P(mockp2p.NewTestP2P(t)),
 				WithInitialSync(&mockSync.Sync{IsSyncing: false}),
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithBlsToExecPool(blstoexec.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedBLSToExecutionChange, topic string) (*Service, string) {
 				s.cfg.stateGen = stategen.New(beaconDB, doublylinkedtree.New())
 				s.cfg.beaconDB = beaconDB
 				s.initCaches()
+				st, _ := util.DeterministicGenesisStateCapella(t, 128)
 				s.cfg.chain = &mockChain.ChainService{
 					ValidatorsRoot: [32]byte{'A'},
 					Genesis:        time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(10)),
+					State:          st,
 				}
-				incorrectRoot := [32]byte{0xBB}
-				//msg.BlockRoot = incorrectRoot[:]
-				_ = incorrectRoot
+
+				msg.Message.ValidatorIndex = 130
 				return s, topic
 			},
 			args: args{
 				ctx:   context.Background(),
 				pid:   "random",
-				topic: fmt.Sprintf(defaultTopic, fakeDigest, 0),
-				msg: &ethpb.SignedBLSToExecutionChange{
-					Message: &ethpb.BLSToExecutionChange{
-						ValidatorIndex:     0,
-						FromBlsPubkey:      make([]byte, 48),
-						ToExecutionAddress: make([]byte, 20),
-					},
-					Signature: emptySig[:],
-				}},
-			want: pubsub.ValidationIgnore,
-		},
-		{
-			name: "Subnet is non-existent",
-			svc: NewService(context.Background(),
-				WithP2P(mockp2p.NewTestP2P(t)),
-				WithInitialSync(&mockSync.Sync{IsSyncing: false}),
-				WithChainService(chainService),
-				WithStateNotifier(chainService.StateNotifier()),
-				WithOperationNotifier(chainService.OperationNotifier()),
-			),
-			setupSvc: func(s *Service, msg *ethpb.SignedBLSToExecutionChange, topic string) (*Service, string) {
-				s.cfg.stateGen = stategen.New(beaconDB, doublylinkedtree.New())
-				s.cfg.beaconDB = beaconDB
-				s.initCaches()
-				//msg.BlockRoot = headRoot[:]
-				hState, err := beaconDB.State(context.Background(), headRoot)
-				assert.NoError(t, err)
-				s.cfg.chain = &mockChain.ChainService{
-					SyncCommitteeIndices: []types.CommitteeIndex{0},
-					ValidatorsRoot:       [32]byte{'A'},
-					Genesis:              time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(hState.Slot()-1)),
-				}
-				numOfVals := hState.NumValidators()
-
-				chosenVal := numOfVals - 10
-				msg.Signature = emptySig[:]
-				//msg.BlockRoot = headRoot[:]
-				//msg.ValidatorIndex = types.ValidatorIndex(chosenVal)
-				//msg.Slot = slots.PrevSlot(hState.Slot())
-				_ = chosenVal
-				// Set Bad Topic and Subnet
-				digest, err := s.currentForkDigest()
-				assert.NoError(t, err)
-				actualTopic := fmt.Sprintf(defaultTopic, digest, 5)
-
-				return s, actualTopic
-			},
-			args: args{
-				ctx:   context.Background(),
-				pid:   "random",
-				topic: defaultTopic,
+				topic: fmt.Sprintf(defaultTopic, fakeDigest),
 				msg: &ethpb.SignedBLSToExecutionChange{
 					Message: &ethpb.BLSToExecutionChange{
 						ValidatorIndex:     0,
@@ -237,101 +186,36 @@ func TestService_ValidateBlsToExecutionChange(t *testing.T) {
 			want: pubsub.ValidationReject,
 		},
 		{
-			name: "Validator is non-existent",
+			name: "Invalid Withdrawal Pubkey",
 			svc: NewService(context.Background(),
 				WithP2P(mockp2p.NewTestP2P(t)),
 				WithInitialSync(&mockSync.Sync{IsSyncing: false}),
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithBlsToExecPool(blstoexec.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedBLSToExecutionChange, topic string) (*Service, string) {
 				s.cfg.stateGen = stategen.New(beaconDB, doublylinkedtree.New())
 				s.cfg.beaconDB = beaconDB
 				s.initCaches()
-				//msg.BlockRoot = headRoot[:]
-				hState, err := beaconDB.State(context.Background(), headRoot)
-				assert.NoError(t, err)
+				st, keys := util.DeterministicGenesisStateCapella(t, 128)
 				s.cfg.chain = &mockChain.ChainService{
 					ValidatorsRoot: [32]byte{'A'},
-					Genesis:        time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(hState.Slot()-1)),
+					Genesis:        time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(10)),
+					State:          st,
 				}
 
-				numOfVals := hState.NumValidators()
-
-				chosenVal := numOfVals + 10
-				msg.Signature = emptySig[:]
-				//msg.BlockRoot = headRoot[:]
-				//msg.ValidatorIndex = types.ValidatorIndex(chosenVal)
-				//msg.Slot = slots.PrevSlot(hState.Slot())
-				_ = chosenVal
-				digest, err := s.currentForkDigest()
-				assert.NoError(t, err)
-				actualTopic := fmt.Sprintf(defaultTopic, digest, 1)
-
-				return s, actualTopic
+				msg.Message.ValidatorIndex = 50
+				// Provide invalid withdrawal key for validator
+				msg.Message.FromBlsPubkey = keys[0].PublicKey().Marshal()
+				msg.Message.ToExecutionAddress = wantedExecAddress
+				return s, topic
 			},
 			args: args{
 				ctx:   context.Background(),
 				pid:   "random",
-				topic: defaultTopic,
-				msg: &ethpb.SignedBLSToExecutionChange{
-					Message: &ethpb.BLSToExecutionChange{
-						ValidatorIndex:     0,
-						FromBlsPubkey:      make([]byte, 48),
-						ToExecutionAddress: make([]byte, 20),
-					},
-					Signature: emptySig[:],
-				}},
-			want: pubsub.ValidationIgnore,
-		},
-		{
-			name: "Invalid Sync Committee Signature",
-			svc: NewService(context.Background(),
-				WithP2P(mockp2p.NewTestP2P(t)),
-				WithInitialSync(&mockSync.Sync{IsSyncing: false}),
-				WithChainService(chainService),
-				WithStateNotifier(chainService.StateNotifier()),
-				WithOperationNotifier(chainService.OperationNotifier()),
-			),
-			setupSvc: func(s *Service, msg *ethpb.SignedBLSToExecutionChange, topic string) (*Service, string) {
-				s.cfg.stateGen = stategen.New(beaconDB, doublylinkedtree.New())
-				s.cfg.beaconDB = beaconDB
-				s.initCaches()
-				//msg.BlockRoot = headRoot[:]
-				hState, err := beaconDB.State(context.Background(), headRoot)
-				assert.NoError(t, err)
-
-				numOfVals := hState.NumValidators()
-
-				chosenVal := numOfVals - 10
-				msg.Signature = emptySig[:]
-				//msg.BlockRoot = headRoot[:]
-				//msg.ValidatorIndex = types.ValidatorIndex(chosenVal)
-				//msg.Slot = slots.PrevSlot(hState.Slot())
-
-				d, err := signing.Domain(hState.Fork(), slots.ToEpoch(hState.Slot()), params.BeaconConfig().DomainSyncCommittee, hState.GenesisValidatorsRoot())
-				assert.NoError(t, err)
-				subCommitteeSize := params.BeaconConfig().SyncCommitteeSize / params.BeaconConfig().SyncCommitteeSubnetCount
-				s.cfg.chain = &mockChain.ChainService{
-					SyncCommitteeIndices: []types.CommitteeIndex{types.CommitteeIndex(subCommitteeSize)},
-					ValidatorsRoot:       [32]byte{'A'},
-					Genesis:              time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(hState.Slot()-1)),
-					SyncCommitteeDomain:  d,
-					PublicKey:            bytesutil.ToBytes48(keys[chosenVal].PublicKey().Marshal()),
-				}
-
-				// Set Topic and Subnet
-				digest, err := s.currentForkDigest()
-				assert.NoError(t, err)
-				actualTopic := fmt.Sprintf(defaultTopic, digest, 1)
-
-				return s, actualTopic
-			},
-			args: args{
-				ctx:   context.Background(),
-				pid:   "random",
-				topic: defaultTopic,
+				topic: fmt.Sprintf(defaultTopic, fakeDigest),
 				msg: &ethpb.SignedBLSToExecutionChange{
 					Message: &ethpb.BLSToExecutionChange{
 						ValidatorIndex:     0,
@@ -343,56 +227,134 @@ func TestService_ValidateBlsToExecutionChange(t *testing.T) {
 			want: pubsub.ValidationReject,
 		},
 		{
-			name: "Valid Sync Committee Signature",
+			name: "Invalid Credentials in State",
 			svc: NewService(context.Background(),
 				WithP2P(mockp2p.NewTestP2P(t)),
 				WithInitialSync(&mockSync.Sync{IsSyncing: false}),
 				WithChainService(chainService),
 				WithStateNotifier(chainService.StateNotifier()),
 				WithOperationNotifier(chainService.OperationNotifier()),
+				WithBlsToExecPool(blstoexec.NewPool()),
 			),
 			setupSvc: func(s *Service, msg *ethpb.SignedBLSToExecutionChange, topic string) (*Service, string) {
 				s.cfg.stateGen = stategen.New(beaconDB, doublylinkedtree.New())
 				s.cfg.beaconDB = beaconDB
 				s.initCaches()
-				//		msg.BlockRoot = headRoot[:]
-				hState, err := beaconDB.State(context.Background(), headRoot)
-				assert.NoError(t, err)
-				subCommitteeSize := params.BeaconConfig().SyncCommitteeSize / params.BeaconConfig().SyncCommitteeSubnetCount
-
-				numOfVals := hState.NumValidators()
-
-				chosenVal := numOfVals - 10
-				d, err := signing.Domain(hState.Fork(), slots.ToEpoch(hState.Slot()), params.BeaconConfig().DomainSyncCommittee, hState.GenesisValidatorsRoot())
-				assert.NoError(t, err)
-				rawBytes := p2ptypes.SSZBytes(headRoot[:])
-				sigRoot, err := signing.ComputeSigningRoot(&rawBytes, d)
-				assert.NoError(t, err)
-
+				st, keys := util.DeterministicGenesisStateCapella(t, 128)
+				assert.NoError(t, st.ApplyToEveryValidator(func(idx int, val *ethpb.Validator) (bool, *ethpb.Validator, error) {
+					newCreds := make([]byte, 32)
+					newCreds[0] = params.BeaconConfig().ETH1AddressWithdrawalPrefixByte
+					copy(newCreds[12:], wantedExecAddress)
+					val.WithdrawalCredentials = newCreds
+					return true, val, nil
+				}))
 				s.cfg.chain = &mockChain.ChainService{
-					SyncCommitteeIndices: []types.CommitteeIndex{types.CommitteeIndex(subCommitteeSize)},
-					ValidatorsRoot:       [32]byte{'A'},
-					Genesis:              time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(hState.Slot()-1)),
-					SyncCommitteeDomain:  d,
-					PublicKey:            bytesutil.ToBytes48(keys[chosenVal].PublicKey().Marshal()),
+					ValidatorsRoot: [32]byte{'A'},
+					Genesis:        time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(10)),
+					State:          st,
 				}
 
-				msg.Signature = keys[chosenVal].Sign(sigRoot[:]).Marshal()
-				//msg.BlockRoot = headRoot[:]
-				//msg.ValidatorIndex = types.ValidatorIndex(chosenVal)
-				//msg.Slot = slots.PrevSlot(hState.Slot())
-
-				// Set Topic and Subnet
-				digest, err := s.currentForkDigest()
-				assert.NoError(t, err)
-				actualTopic := fmt.Sprintf(defaultTopic, digest, 1)
-
-				return s, actualTopic
+				msg.Message.ValidatorIndex = 50
+				// Provide Correct withdrawal pubkey
+				msg.Message.FromBlsPubkey = keys[51].PublicKey().Marshal()
+				msg.Message.ToExecutionAddress = wantedExecAddress
+				return s, topic
 			},
 			args: args{
 				ctx:   context.Background(),
 				pid:   "random",
-				topic: defaultTopic,
+				topic: fmt.Sprintf(defaultTopic, fakeDigest),
+				msg: &ethpb.SignedBLSToExecutionChange{
+					Message: &ethpb.BLSToExecutionChange{
+						ValidatorIndex:     0,
+						FromBlsPubkey:      make([]byte, 48),
+						ToExecutionAddress: make([]byte, 20),
+					},
+					Signature: emptySig[:],
+				}},
+			want: pubsub.ValidationReject,
+		},
+		{
+			name: "Invalid Execution Change Signature",
+			svc: NewService(context.Background(),
+				WithP2P(mockp2p.NewTestP2P(t)),
+				WithInitialSync(&mockSync.Sync{IsSyncing: false}),
+				WithChainService(chainService),
+				WithStateNotifier(chainService.StateNotifier()),
+				WithOperationNotifier(chainService.OperationNotifier()),
+				WithBlsToExecPool(blstoexec.NewPool()),
+			),
+			setupSvc: func(s *Service, msg *ethpb.SignedBLSToExecutionChange, topic string) (*Service, string) {
+				s.cfg.stateGen = stategen.New(beaconDB, doublylinkedtree.New())
+				s.cfg.beaconDB = beaconDB
+				s.initCaches()
+				st, keys := util.DeterministicGenesisStateCapella(t, 128)
+				s.cfg.chain = &mockChain.ChainService{
+					ValidatorsRoot: [32]byte{'A'},
+					Genesis:        time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(10)),
+					State:          st,
+				}
+
+				msg.Message.ValidatorIndex = 50
+				// Provide invalid withdrawal key for validator
+				msg.Message.FromBlsPubkey = keys[51].PublicKey().Marshal()
+				msg.Message.ToExecutionAddress = wantedExecAddress
+				badSig := make([]byte, 96)
+				copy(badSig, []byte{'j', 'u', 'n', 'k'})
+				msg.Signature = badSig
+				return s, topic
+			},
+			args: args{
+				ctx:   context.Background(),
+				pid:   "random",
+				topic: fmt.Sprintf(defaultTopic, fakeDigest),
+				msg: &ethpb.SignedBLSToExecutionChange{
+					Message: &ethpb.BLSToExecutionChange{
+						ValidatorIndex:     0,
+						FromBlsPubkey:      make([]byte, 48),
+						ToExecutionAddress: make([]byte, 20),
+					},
+					Signature: emptySig[:],
+				}},
+			want: pubsub.ValidationReject,
+		},
+		{
+			name: "Valid Execution Change Message",
+			svc: NewService(context.Background(),
+				WithP2P(mockp2p.NewTestP2P(t)),
+				WithInitialSync(&mockSync.Sync{IsSyncing: false}),
+				WithChainService(chainService),
+				WithStateNotifier(chainService.StateNotifier()),
+				WithOperationNotifier(chainService.OperationNotifier()),
+				WithBlsToExecPool(blstoexec.NewPool()),
+			),
+			setupSvc: func(s *Service, msg *ethpb.SignedBLSToExecutionChange, topic string) (*Service, string) {
+				s.cfg.stateGen = stategen.New(beaconDB, doublylinkedtree.New())
+				s.cfg.beaconDB = beaconDB
+				s.initCaches()
+				st, keys := util.DeterministicGenesisStateCapella(t, 128)
+				s.cfg.chain = &mockChain.ChainService{
+					ValidatorsRoot: [32]byte{'A'},
+					Genesis:        time.Now().Add(-time.Second * time.Duration(params.BeaconConfig().SecondsPerSlot) * time.Duration(10)),
+					State:          st,
+				}
+
+				msg.Message.ValidatorIndex = 50
+				// Provide invalid withdrawal key for validator
+				msg.Message.FromBlsPubkey = keys[51].PublicKey().Marshal()
+				msg.Message.ToExecutionAddress = wantedExecAddress
+				epoch := slots.ToEpoch(st.Slot())
+				domain, err := signing.Domain(st.Fork(), epoch, params.BeaconConfig().DomainBLSToExecutionChange, st.GenesisValidatorsRoot())
+				assert.NoError(t, err)
+				htr, err := signing.SigningData(msg.Message.HashTreeRoot, domain)
+				assert.NoError(t, err)
+				msg.Signature = keys[51].Sign(htr[:]).Marshal()
+				return s, topic
+			},
+			args: args{
+				ctx:   context.Background(),
+				pid:   "random",
+				topic: fmt.Sprintf(defaultTopic, fakeDigest),
 				msg: &ethpb.SignedBLSToExecutionChange{
 					Message: &ethpb.BLSToExecutionChange{
 						ValidatorIndex:     0,
