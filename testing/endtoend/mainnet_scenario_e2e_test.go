8cffc364917b4b6c4b20564c7707ee718e5dc730
james-prysm <james@prysmaticlabs.com>
2022-12-02 15:30:38 -0600

    testing multi run again

--- testing/endtoend/mainnet_scenario_e2e_test.go
+++ testing/endtoend/mainnet_scenario_e2e_test.go
@@ -2,21 +2,23 @@ package endtoend
 
 import (
 	"testing"
+	"time"
 
 	"github.com/prysmaticlabs/prysm/v3/testing/endtoend/types"
 )
 
-//func TestEndToEnd_MainnetConfig_MultiClient(t *testing.T) {
-//	e2eMainnet(t, false /*usePrysmSh*/, true /*useMultiClient*/).run()
-//}
+func TestEndToEnd_MainnetConfig_MultiClient(t *testing.T) {
+	e2eMainnet(t, false /*usePrysmSh*/, true /*useMultiClient*/).run()
+}
 
 func TestEndToEnd_MainnetConfig_Multiclient_CrossClient(t *testing.T) {
+	time.Sleep(120 * time.Second)
 	e2eMainnet(t, false /*usePrysmSh*/, true /*useMultiClient*/, types.WithValidatorCrossClient()).run()
 }
 
-//func TestEndToEnd_MultiScenarioRun_Multiclient(t *testing.T) {
-//	runner := e2eMainnet(t, false /*usePrysmSh*/, true /*useMultiClient*/, types.WithEpochs(22))
-//	runner.config.Evaluators = scenarioEvalsMulti()
-//	runner.config.EvalInterceptor = runner.multiScenarioMulticlient
-//	runner.scenarioRunner()
-//}
+func TestEndToEnd_MultiScenarioRun_Multiclient(t *testing.T) {
+	runner := e2eMainnet(t, false /*usePrysmSh*/, true /*useMultiClient*/, types.WithEpochs(22))
+	runner.config.Evaluators = scenarioEvalsMulti()
+	runner.config.EvalInterceptor = runner.multiScenarioMulticlient
+	runner.scenarioRunner()
+}
