8c75d3c66f81a56002a56e8f45ea1306af651dfe
Saolyn <sammy@rosso.red>
2022-12-05 18:08:41 +0100

    Add deposit tree snapshot

---
+++ beacon-chain/cache/depositsnapshot/deposit_tree_snapshot.go
@@ -0,0 +1,41 @@
+package depositsnapshot
+
+import (
+	"crypto/sha256"
+
+	"github.com/prysmaticlabs/prysm/v3/encoding/bytesutil"
+)
+
+type DepositTreeSnapshot struct {
+	finalized            [][32]byte
+	depositRoot          [32]byte
+	depositCount         uint64
+	executionBlockHash   [32]byte
+	executionBlockHeight uint64
+}
+
+func (ds *DepositTreeSnapshot) CalculateRoot() [32]byte {
+	size := ds.depositCount
+	index := len(ds.finalized)
+	root := Zerohashes[0]
+	for i := 0; i < DepositContractDepth; i++ {
+		if (size & 1) == 1 {
+			index -= 1
+			root = sha256.Sum256(append(ds.finalized[index][:], root[:]...))
+		} else {
+			root = sha256.Sum256(append(root[:], Zerohashes[i][:]...))
+		}
+		size >>= 1
+	}
+	return sha256.Sum256(append(root[:], bytesutil.Uint64ToBytesLittleEndian(ds.depositCount)...))
+}
+
+func fromTreeParts(finalised [][32]byte, depositCount uint64, executionBlockHash [32]byte, executionBlockDepth uint64) DepositTreeSnapshot {
+	return DepositTreeSnapshot{
+		finalized:            finalised,
+		depositRoot:          Zerohashes[0],
+		depositCount:         depositCount,
+		executionBlockHash:   executionBlockHash,
+		executionBlockHeight: executionBlockDepth,
+	}
+}
