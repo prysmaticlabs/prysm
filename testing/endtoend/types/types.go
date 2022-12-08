ca2618110fe2df86a9870ace1b14563aa2980625
Preston Van Loon <preston@prysmaticlabs.com>
2022-12-07 22:48:16 -0600

    e2e: Print process IDs for debugging. (#11734)
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

--- testing/endtoend/types/types.go
+++ testing/endtoend/types/types.go
@@ -4,6 +4,7 @@ package types
 
 import (
 	"context"
+	"os"
 
 	types "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
 	"google.golang.org/grpc"
@@ -101,6 +102,8 @@ type ComponentRunner interface {
 	Resume() error
 	// Stop stops a component.
 	Stop() error
+	// UnderlyingProcess is the underlying process, once started.
+	UnderlyingProcess() *os.Process
 }
 
 type MultipleComponentRunners interface {
