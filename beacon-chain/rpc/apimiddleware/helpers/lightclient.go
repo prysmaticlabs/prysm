3e3de50c03bfb78af3de5164c43fcc6d05bcb20f
Giuseppe Valente <giuseppe@polymerlabs.org>
2022-12-08 14:01:13 -0800

    chore: move apimiddleware helpers to their own package to fix cycle

---
+++ beacon-chain/rpc/apimiddleware/helpers/lightclient.go
@@ -0,0 +1,166 @@
+package helpers
+
+import (
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethrpc "github.com/prysmaticlabs/prysm/v3/beacon-chain/rpc/apimiddleware"
+	v11 "github.com/prysmaticlabs/prysm/v3/proto/engine/v1"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"math/bits"
+	"strconv"
+	"time"
+
+	types "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
+	ethpbv1 "github.com/prysmaticlabs/prysm/v3/proto/eth/v1"
+	ethpbv2 "github.com/prysmaticlabs/prysm/v3/proto/eth/v2"
+)
+
+const (
+	CurrentSyncCommitteeIndex = uint64(54)
+	NextSyncCommitteeIndex    = uint64(55)
+	FinalizedRootIndex        = uint64(105)
+)
+
+func NewExecutionPayloadHeaderFromJSON(header *ethrpc.ExecutionPayloadHeaderJson) (*v11.ExecutionPayloadHeader,
+	error) {
+	blockNumber, err := strconv.ParseUint(header.BlockNumber, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	gasLimit, err := strconv.ParseUint(header.GasLimit, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	gasUsed, err := strconv.ParseUint(header.GasUsed, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	timestamp, err := strconv.ParseUint(header.TimeStamp, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return &v11.ExecutionPayloadHeader{
+		ParentHash:       hexutil.MustDecode(header.ParentHash),
+		FeeRecipient:     hexutil.MustDecode(header.FeeRecipient),
+		StateRoot:        hexutil.MustDecode(header.StateRoot),
+		ReceiptsRoot:     hexutil.MustDecode(header.ReceiptsRoot),
+		LogsBloom:        hexutil.MustDecode(header.LogsBloom),
+		PrevRandao:       hexutil.MustDecode(header.PrevRandao),
+		BlockNumber:      blockNumber,
+		GasLimit:         gasLimit,
+		GasUsed:          gasUsed,
+		Timestamp:        timestamp,
+		ExtraData:        hexutil.MustDecode(header.ExtraData),
+		BaseFeePerGas:    hexutil.MustDecode(header.BaseFeePerGas),
+		BlockHash:        hexutil.MustDecode(header.BlockHash),
+		TransactionsRoot: hexutil.MustDecode(header.TransactionsRoot),
+	}, nil
+}
+
+// TODO: move this somewhere common
+func FloorLog2(x uint64) int {
+	return bits.Len64(uint64(x - 1))
+}
+
+func NewSyncAggregateFromJSON(syncAggregate *ethrpc.SyncAggregateJson) *ethpbv1.SyncAggregate {
+	return &ethpbv1.SyncAggregate{
+		SyncCommitteeBits:      hexutil.MustDecode(syncAggregate.SyncCommitteeBits),
+		SyncCommitteeSignature: hexutil.MustDecode(syncAggregate.SyncCommitteeSignature),
+	}
+}
+
+func timeFromJSON(timestamp string) (*time.Time, error) {
+	timeInt, err := strconv.ParseInt(timestamp, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	t := time.Unix(timeInt, 0)
+	return &t, nil
+}
+
+func NewGenesisResponse_GenesisFromJSON(genesis *ethrpc.GenesisResponse_GenesisJson) (
+	*ethpbv1.GenesisResponse_Genesis, error) {
+	genesisTime, err := timeFromJSON(genesis.GenesisTime)
+	if err != nil {
+		return nil, err
+	}
+	return &ethpbv1.GenesisResponse_Genesis{
+		GenesisTime:           timestamppb.New(*genesisTime),
+		GenesisValidatorsRoot: hexutil.MustDecode(genesis.GenesisValidatorsRoot),
+		GenesisForkVersion:    hexutil.MustDecode(genesis.GenesisForkVersion),
+	}, nil
+}
+
+func headerFromJSON(header *ethrpc.BeaconBlockHeaderJson) (*ethpbv1.BeaconBlockHeader, error) {
+	slot, err := strconv.ParseUint(header.Slot, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	proposerIndex, err := strconv.ParseUint(header.ProposerIndex, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return &ethpbv1.BeaconBlockHeader{
+		Slot:          types.Slot(slot),
+		ProposerIndex: types.ValidatorIndex(proposerIndex),
+		ParentRoot:    hexutil.MustDecode(header.ParentRoot),
+		StateRoot:     hexutil.MustDecode(header.StateRoot),
+		BodyRoot:      hexutil.MustDecode(header.BodyRoot),
+	}, nil
+}
+
+func syncCommitteeFromJSON(syncCommittee *ethrpc.SyncCommitteeJson) *ethpbv2.SyncCommittee {
+	pubKeys := make([][]byte, len(syncCommittee.Pubkeys))
+	for i, pubKey := range syncCommittee.Pubkeys {
+		pubKeys[i] = hexutil.MustDecode(pubKey)
+	}
+	return &ethpbv2.SyncCommittee{
+		Pubkeys:         pubKeys,
+		AggregatePubkey: hexutil.MustDecode(syncCommittee.AggregatePubkey),
+	}
+}
+
+func branchFromJSON(branch []string) [][]byte {
+	branchBytes := [][]byte{}
+	for _, root := range branch {
+		branchBytes = append(branchBytes, hexutil.MustDecode(root))
+	}
+	return branchBytes
+}
+
+func trustedBlockRoot(bootstrap *ethpbv2.LightClientBootstrap) ([32]byte, error) {
+	return bootstrap.Header.HashTreeRoot()
+}
+
+func NewLightClientBootstrapFromJSON(bootstrap *ethrpc.LightClientBootstrapJson) (*ethpbv2.LightClientBootstrap,
+	error) {
+	header, err := headerFromJSON(bootstrap.Header)
+	if err != nil {
+		return nil, err
+	}
+	return &ethpbv2.LightClientBootstrap{
+		Header:                     header,
+		CurrentSyncCommittee:       syncCommitteeFromJSON(bootstrap.CurrentSyncCommittee),
+		CurrentSyncCommitteeBranch: branchFromJSON(bootstrap.CurrentSyncCommitteeBranch),
+	}, nil
+}
+
+func NewLightClientUpdateFromJSON(update *ethrpc.LightClientUpdateDataJson) (*ethpbv2.LightClientUpdate, error) {
+	attestedHeader, err := headerFromJSON(update.AttestedHeader)
+	if err != nil {
+		return nil, err
+	}
+	finalizedHeader, err := headerFromJSON(update.FinalizedHeader)
+	if err != nil {
+		return nil, err
+	}
+	signatureSlot, err := strconv.ParseUint(update.SignatureSlot, 10, 64)
+	return &ethpbv2.LightClientUpdate{
+		AttestedHeader:          attestedHeader,
+		NextSyncCommittee:       syncCommitteeFromJSON(update.NextSyncCommittee),
+		NextSyncCommitteeBranch: branchFromJSON(update.NextSyncCommitteeBranch),
+		FinalizedHeader:         finalizedHeader,
+		FinalityBranch:          branchFromJSON(update.FinalityBranch),
+		SyncAggregate:           NewSyncAggregateFromJSON(update.SyncAggregate),
+		SignatureSlot:           types.Slot(signatureSlot),
+	}, nil
+}
--- proto/eth/v2/custom.go
+++ proto/eth/v2/custom.go
@@ -2,35 +2,7 @@ package eth
 
 import (
 	"bytes"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	ethrpc "github.com/prysmaticlabs/prysm/v3/beacon-chain/rpc/apimiddleware"
-	types "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
-	ethpbv1 "github.com/prysmaticlabs/prysm/v3/proto/eth/v1"
-	"math/bits"
-	"strconv"
 )
-
-const (
-	CurrentSyncCommitteeIndex = uint64(54)
-	NextSyncCommitteeIndex    = uint64(55)
-	FinalizedRootIndex        = uint64(105)
-)
-
-type LightClientGenericUpdate interface {
-	GetAttestedHeader() *ethpbv1.BeaconBlockHeader
-	GetNextSyncCommittee() *SyncCommittee
-	GetNextSyncCommitteeBranch() [][]byte
-	GetFinalizedHeader() *ethpbv1.BeaconBlockHeader
-	SetFinalizedHeader(*ethpbv1.BeaconBlockHeader)
-	GetFinalityBranch() [][]byte
-	GetSyncAggregate() *ethpbv1.SyncAggregate
-	GetSignatureSlot() types.Slot
-}
-
-// TODO: move this somewhere common
-func FloorLog2(x uint64) int {
-	return bits.Len64(uint64(x - 1))
-}
 
 func (x *SyncCommittee) Equals(other *SyncCommittee) bool {
 	if len(x.Pubkeys) != len(other.Pubkeys) {
@@ -42,78 +14,4 @@ func (x *SyncCommittee) Equals(other *SyncCommittee) bool {
 		}
 	}
 	return bytes.Equal(x.AggregatePubkey, other.AggregatePubkey)
-}
-
-func headerFromJSON(header *ethrpc.BeaconBlockHeaderJson) (*ethpbv1.BeaconBlockHeader, error) {
-	slot, err := strconv.ParseUint(header.Slot, 10, 64)
-	if err != nil {
-		return nil, err
-	}
-	proposerIndex, err := strconv.ParseUint(header.ProposerIndex, 10, 64)
-	if err != nil {
-		return nil, err
-	}
-	return &ethpbv1.BeaconBlockHeader{
-		Slot:          types.Slot(slot),
-		ProposerIndex: types.ValidatorIndex(proposerIndex),
-		ParentRoot:    hexutil.MustDecode(header.ParentRoot),
-		StateRoot:     hexutil.MustDecode(header.StateRoot),
-		BodyRoot:      hexutil.MustDecode(header.BodyRoot),
-	}, nil
-}
-
-func syncCommitteeFromJSON(syncCommittee *ethrpc.SyncCommitteeJson) *SyncCommittee {
-	pubKeys := make([][]byte, len(syncCommittee.Pubkeys))
-	for i, pubKey := range syncCommittee.Pubkeys {
-		pubKeys[i] = hexutil.MustDecode(pubKey)
-	}
-	return &SyncCommittee{
-		Pubkeys:         pubKeys,
-		AggregatePubkey: hexutil.MustDecode(syncCommittee.AggregatePubkey),
-	}
-}
-
-func branchFromJSON(branch []string) [][]byte {
-	branchBytes := [][]byte{}
-	for _, root := range branch {
-		branchBytes = append(branchBytes, hexutil.MustDecode(root))
-	}
-	return branchBytes
-}
-
-func trustedBlockRoot(bootstrap *LightClientBootstrap) ([32]byte, error) {
-	return bootstrap.Header.HashTreeRoot()
-}
-
-func NewLightClientBootstrapFromJSON(bootstrap *ethrpc.LightClientBootstrapJson) (*LightClientBootstrap, error) {
-	header, err := headerFromJSON(bootstrap.Header)
-	if err != nil {
-		return nil, err
-	}
-	return &LightClientBootstrap{
-		Header:                     header,
-		CurrentSyncCommittee:       syncCommitteeFromJSON(bootstrap.CurrentSyncCommittee),
-		CurrentSyncCommitteeBranch: branchFromJSON(bootstrap.CurrentSyncCommitteeBranch),
-	}, nil
-}
-
-func NewLightClientUpdateFromJSON(update *ethrpc.LightClientUpdateDataJson) (*LightClientUpdate, error) {
-	attestedHeader, err := headerFromJSON(update.AttestedHeader)
-	if err != nil {
-		return nil, err
-	}
-	finalizedHeader, err := headerFromJSON(update.FinalizedHeader)
-	if err != nil {
-		return nil, err
-	}
-	signatureSlot, err := strconv.ParseUint(update.SignatureSlot, 10, 64)
-	return &LightClientUpdate{
-		AttestedHeader:          attestedHeader,
-		NextSyncCommittee:       syncCommitteeFromJSON(update.NextSyncCommittee),
-		NextSyncCommitteeBranch: branchFromJSON(update.NextSyncCommitteeBranch),
-		FinalizedHeader:         finalizedHeader,
-		FinalityBranch:          branchFromJSON(update.FinalityBranch),
-		SyncAggregate:           ethpbv1.NewSyncAggregateFromJSON(update.SyncAggregate),
-		SignatureSlot:           types.Slot(signatureSlot),
-	}, nil
 }
