786511294e632ffacbb9ce33ddda22dceffd73f5
Potuz <potuz@prysmaticlabs.com>
2022-12-13 09:27:59 -0300

    Check BLS_TO_EXECUTION_CHANGE as if they were from Capella
    In the event we receive a BLS_TO_EXECUTION_CHANGE and our head state is
    before Capella, verify it's signature with the Capella fork version.

--- beacon-chain/core/blocks/withdrawals.go
+++ beacon-chain/core/blocks/withdrawals.go
@@ -165,7 +165,17 @@ func BLSChangesSignatureBatch(
 		Messages:   make([][32]byte, len(changes)),
 	}
 	epoch := slots.ToEpoch(st.Slot())
-	domain, err := signing.Domain(st.Fork(), epoch, params.BeaconConfig().DomainBLSToExecutionChange, st.GenesisValidatorsRoot())
+	var fork *ethpb.Fork
+	if st.Version() < version.Capella {
+		fork = &ethpb.Fork{
+			PreviousVersion: params.BeaconConfig().BellatrixForkVersion,
+			CurrentVersion:  params.BeaconConfig().CapellaForkVersion,
+			Epoch:           params.BeaconConfig().CapellaForkEpoch,
+		}
+	} else {
+		fork = st.Fork()
+	}
+	domain, err := signing.Domain(fork, epoch, params.BeaconConfig().DomainBLSToExecutionChange, st.GenesisValidatorsRoot())
 	if err != nil {
 		return nil, err
 	}
