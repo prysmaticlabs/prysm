6fbedf056e866bfe99f49e229c8be62c125fc24f
Saolyn <sammy@rosso.red>
2022-12-20 12:20:04 +0100

    fixed failing tests

--- beacon-chain/cache/depositsnapshot/merkle_tree_test.go
+++ beacon-chain/cache/depositsnapshot/merkle_tree_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/prysmaticlabs/prysm/v3/testing/assert"
 )
 
 func hexString(t *testing.T, hexStr string) [32]byte {
@@ -80,21 +82,60 @@ func Test_fromSnapshotParts(t *testing.T) {
 				hash:         [32]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 			},
 		},
-		//{
-		//	name:      "multiple deposits and 1 Finalized",
-		//	Finalized: [][32]byte{hexString(t, fmt.Sprintf("%064d", 0))},
-		//	deposits:  2,
-		//	level:     4,
-		//	want: &InnerNode{
-		//		left:  &InnerNode{&InnerNode{}, &ZeroNode{}},
-		//		right: &ZeroNode{},
-		//	},
-		//},
+		{
+			name:      "multiple deposits and 1 Finalized",
+			finalized: [][32]byte{hexString(t, fmt.Sprintf("%064d", 0))},
+			deposits:  2,
+			level:     4,
+			want: &InnerNode{
+				left:  &InnerNode{&InnerNode{&FinalizedNode{depositCount: 2, hash: hexString(t, fmt.Sprintf("%064d", 0))}, &ZeroNode{1}}, &ZeroNode{2}},
+				right: &ZeroNode{3},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := fromSnapshotParts(tt.finalized, tt.deposits, tt.level); !reflect.DeepEqual(got, tt.want) {
+				tmp := got
+				fmt.Println(tmp)
 				t.Errorf("fromSnapshotParts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_generateProof(t *testing.T) {
+	tests := []struct {
+		name   string
+		leaves uint64
+	}{
+		{
+			name:   "1 leaf",
+			leaves: 1,
+		},
+		{
+			name:   "4 leaves",
+			leaves: 4,
+		},
+		{
+			name:   "10 leaves",
+			leaves: 10,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testCases, err := readTestCases("test_cases.yaml")
+			assert.NoError(t, err)
+			tree := New()
+			for _, c := range testCases[:tt.leaves] {
+				err = tree.pushLeaf(c.DepositDataRoot)
+				assert.NoError(t, err)
+			}
+			for i := uint64(0); i < tt.leaves; i++ {
+				leaf, proof := generateProof(tree.tree, i, DepositContractDepth)
+				assert.Equal(t, leaf, testCases[i].DepositDataRoot)
+				calcRoot := merkleRootFromBranch(leaf, proof, i)
+				assert.Equal(t, tree.tree.GetRoot(), calcRoot)
 			}
 		})
 	}
