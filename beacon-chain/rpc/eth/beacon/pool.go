255e9693ee7665e994d48e97f87d96f650598087
James He <james@prysmaticlabs.com>
2022-12-20 22:42:31 -0600

    fixing typo on comments

--- beacon-chain/rpc/eth/beacon/pool.go
+++ beacon-chain/rpc/eth/beacon/pool.go
@@ -307,7 +307,7 @@ func (bs *Server) SubmitVoluntaryExit(ctx context.Context, req *ethpbv1.SignedVo
 	return &emptypb.Empty{}, nil
 }
 
-// SubmitSignedBLSToExecutionChange submits said object to the node's pool
+// SubmitSignedBLSToExecutionChanges submits said object to the node's pool
 // if it passes validation the node must broadcast it to the network.
 func (bs *Server) SubmitSignedBLSToExecutionChanges(ctx context.Context, req *ethpbv2.SubmitBLSToExecutionChangesRequest) (*emptypb.Empty, error) {
 	ctx, span := trace.StartSpan(ctx, "beacon.SubmitVoluntaryExit")
