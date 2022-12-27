88048e86d0e4edb24274657fa1f329278eb170bc
Emmanuel NALEPA <emmanuel.nalepa@kiln.fi>
2022-12-27 15:00:57 +0100

    Remove unused context

--- validator/client/beacon-api/beacon_api_validator_client.go
+++ validator/client/beacon-api/beacon_api_validator_client.go
@@ -195,7 +195,7 @@ func (c *beaconApiValidatorClient) SubmitSyncMessage(ctx context.Context, in *et
 	panic("beaconApiValidatorClient.SubmitSyncMessage is not implemented. To use a fallback client, create this validator with NewBeaconApiValidatorClientWithFallback instead.")
 }
 
-func (c *beaconApiValidatorClient) SubmitValidatorRegistrations(ctx context.Context, in *ethpb.SignedValidatorRegistrationsV1) (*empty.Empty, error) {
+func (c *beaconApiValidatorClient) SubmitValidatorRegistrations(_ context.Context, in *ethpb.SignedValidatorRegistrationsV1) (*empty.Empty, error) {
 	return new(empty.Empty), c.submitValidatorRegistrations(in.Messages)
 }
 
