358e92c316bcdafa75af996a2b30692cab02608d
James He <james@prysmaticlabs.com>
2023-01-10 11:04:17 -0600

    fixing more unit tests

--- validator/keymanager/remote-web3signer/internal/client_test.go
+++ validator/keymanager/remote-web3signer/internal/client_test.go
@@ -11,9 +11,9 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/prysmaticlabs/prysm/v3/testing/assert"
 	"github.com/prysmaticlabs/prysm/v3/testing/require"
 	"github.com/prysmaticlabs/prysm/v3/validator/keymanager/remote-web3signer/internal"
-	"github.com/stretchr/testify/assert"
 )
 
 // mockTransport is the mock Transport object
@@ -47,8 +47,8 @@ func TestClient_Sign_HappyPath(t *testing.T) {
 	assert.NoError(t, err)
 	resp, err := cl.Sign(context.Background(), "a2b5aaad9c6efefe7bb9b1243a043404f3362937cfb6b31833929833173f476630ea2cfeb0d9ddf15f97ca8685948820", jsonRequest)
 	assert.NotNil(t, resp)
-	assert.Nil(t, err)
-	assert.EqualValues(t, "0xb3baa751d0a9132cfe93e4e3d5ff9075111100e3789dca219ade5a24d27e19d16b3353149da1833e9b691bb38634e8dc04469be7032132906c927d7e1a49b414730612877bc6b2810c8f202daf793d1ab0d6b5cb21d52f9e52e883859887a5d9", fmt.Sprintf("%#x", resp.Marshal()))
+	assert.NoError(t, err)
+	assert.Equal(t, "0xb3baa751d0a9132cfe93e4e3d5ff9075111100e3789dca219ade5a24d27e19d16b3353149da1833e9b691bb38634e8dc04469be7032132906c927d7e1a49b414730612877bc6b2810c8f202daf793d1ab0d6b5cb21d52f9e52e883859887a5d9", fmt.Sprintf("%#x", resp.Marshal()))
 }
 
 func TestClient_Sign_HappyPath_Jsontype(t *testing.T) {
@@ -76,8 +76,8 @@ func TestClient_Sign_HappyPath_Jsontype(t *testing.T) {
 	assert.NoError(t, err)
 	resp, err := cl.Sign(context.Background(), "a2b5aaad9c6efefe7bb9b1243a043404f3362937cfb6b31833929833173f476630ea2cfeb0d9ddf15f97ca8685948820", jsonRequest)
 	assert.NotNil(t, resp)
-	assert.Nil(t, err)
-	assert.EqualValues(t, "0xb3baa751d0a9132cfe93e4e3d5ff9075111100e3789dca219ade5a24d27e19d16b3353149da1833e9b691bb38634e8dc04469be7032132906c927d7e1a49b414730612877bc6b2810c8f202daf793d1ab0d6b5cb21d52f9e52e883859887a5d9", fmt.Sprintf("%#x", resp.Marshal()))
+	assert.NoError(t, err)
+	assert.Equal(t, "0xb3baa751d0a9132cfe93e4e3d5ff9075111100e3789dca219ade5a24d27e19d16b3353149da1833e9b691bb38634e8dc04469be7032132906c927d7e1a49b414730612877bc6b2810c8f202daf793d1ab0d6b5cb21d52f9e52e883859887a5d9", fmt.Sprintf("%#x", resp.Marshal()))
 }
 
 func TestClient_Sign_500(t *testing.T) {
@@ -95,7 +95,7 @@ func TestClient_Sign_500(t *testing.T) {
 	assert.NoError(t, err)
 	resp, err := cl.Sign(context.Background(), "a2b5aaad9c6efefe7bb9b1243a043404f3362937cfb6b31833929833173f476630ea2cfeb0d9ddf15f97ca8685948820", jsonRequest)
 	assert.NotNil(t, err)
-	assert.Nil(t, resp)
+	assert.Equal(t, nil, resp)
 
 }
 
@@ -114,7 +114,7 @@ func TestClient_Sign_412(t *testing.T) {
 	assert.NoError(t, err)
 	resp, err := cl.Sign(context.Background(), "a2b5aaad9c6efefe7bb9b1243a043404f3362937cfb6b31833929833173f476630ea2cfeb0d9ddf15f97ca8685948820", jsonRequest)
 	assert.NotNil(t, err)
-	assert.Nil(t, resp)
+	assert.Equal(t, nil, resp)
 
 }
 
@@ -133,7 +133,7 @@ func TestClient_Sign_400(t *testing.T) {
 	assert.NoError(t, err)
 	resp, err := cl.Sign(context.Background(), "a2b5aaad9c6efefe7bb9b1243a043404f3362937cfb6b31833929833173f476630ea2cfeb0d9ddf15f97ca8685948820", jsonRequest)
 	assert.NotNil(t, err)
-	assert.Nil(t, resp)
+	assert.Equal(t, nil, resp)
 
 }
 
@@ -151,9 +151,9 @@ func TestClient_GetPublicKeys_HappyPath(t *testing.T) {
 	cl := internal.ApiClient{BaseURL: u, RestClient: &http.Client{Transport: mock}}
 	resp, err := cl.GetPublicKeys(context.Background(), "example.com/api/publickeys")
 	assert.NotNil(t, resp)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	// we would like them as 48byte base64 without 0x
-	assert.EqualValues(t, "[162 181 170 173 156 110 254 254 123 185 177 36 58 4 52 4 243 54 41 55 207 182 179 24 51 146 152 51 23 63 71 102 48 234 44 254 176 217 221 241 95 151 202 134 133 148 136 32]", fmt.Sprintf("%v", resp[0][:]))
+	assert.Equal(t, "[162 181 170 173 156 110 254 254 123 185 177 36 58 4 52 4 243 54 41 55 207 182 179 24 51 146 152 51 23 63 71 102 48 234 44 254 176 217 221 241 95 151 202 134 133 148 136 32]", fmt.Sprintf("%v", resp[0][:]))
 }
 
 func TestClient_GetPublicKeys_EncodingError(t *testing.T) {
@@ -170,7 +170,7 @@ func TestClient_GetPublicKeys_EncodingError(t *testing.T) {
 	cl := internal.ApiClient{BaseURL: u, RestClient: &http.Client{Transport: mock}}
 	resp, err := cl.GetPublicKeys(context.Background(), "example.com/api/publickeys")
 	assert.Equal(t, err.Error(), "failed to decode from Hex from the following public key index locations: 0, 1, 2, ")
-	assert.Nil(t, resp)
+	assert.Equal(t, nil, resp)
 }
 
 // TODO: not really in use, should be revisited
@@ -183,7 +183,7 @@ func TestClient_ReloadSignerKeys_HappyPath(t *testing.T) {
 	assert.NoError(t, err)
 	cl := internal.ApiClient{BaseURL: u, RestClient: &http.Client{Transport: mock}}
 	err = cl.ReloadSignerKeys(context.Background())
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 }
 
 // TODO: not really in use, should be revisited
@@ -199,5 +199,5 @@ func TestClient_GetServerStatus_HappyPath(t *testing.T) {
 	cl := internal.ApiClient{BaseURL: u, RestClient: &http.Client{Transport: mock}}
 	resp, err := cl.GetServerStatus(context.Background())
 	assert.NotNil(t, resp)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 }
