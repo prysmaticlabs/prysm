a430204f5ddde2f4dda5602c7f8edde5f8c25436
james-prysm <90280386+james-prysm@users.noreply.github.com>
2023-01-12 09:05:22 -0600

    Update cmd/prysmctl/validator/cmd.go
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- cmd/prysmctl/validator/cmd.go
+++ cmd/prysmctl/validator/cmd.go
@@ -70,7 +70,7 @@ var Commands = []*cli.Command{
 						fmt.Println(au.Red("THIS ACTION WILL NOT BE REVERSIBLE ONCE INCLUDED. "))
 						fmt.Println(au.Red("You will NOT be able to change the address again once changed. "))
 						return fmt.Errorf("both the `--%s` and `--%s` flags are required to run this command. \n"+
-							"by providing these flags the user has read and accepts the TERMS AND CONDITIONS: https://github.com/prysmaticlabs/prysm/blob/master/TERMS_OF_SERVICE.md "+
+							"By providing these flags the user has read and accepts the TERMS AND CONDITIONS: https://github.com/prysmaticlabs/prysm/blob/master/TERMS_OF_SERVICE.md "+
 							"and confirms the action of setting withdrawals addresses", cmd.AcceptTosFlag.Name, ConfirmFlag.Name)
 					} else {
 						return nil
