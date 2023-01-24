38f095d556bfa2c44e35783590586498ada1d4ff
james-prysm <90280386+james-prysm@users.noreply.github.com>
2023-01-24 04:05:55 -0600

    prysmctl: changeblstoexecutionaddress ( withdrawals) (#11790)
    * adding unit test for withdrawal
    
    * updating to handle api changes
    
    * Update cmd/prysmctl/withdrawal/withdrawal.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/withdrawal/cmd.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/withdrawal/withdrawal.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/withdrawal/cmd.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/withdrawal/withdrawal.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/withdrawal/withdrawal.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/withdrawal/withdrawal.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * updating based on feedback
    
    * rewording prints
    
    * Update cmd/prysmctl/withdrawal/withdrawal.go
    
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>
    
    * add error handling when status not ok
    
    * addressing some of the feedback, adding new tests
    
    * adding unit test with error return
    
    * adjusting based on review comments
    
    * addressing more comments and fixing unit test
    
    * updating prompts
    
    * migrating some functions to api/client
    
    * removing unused function
    
    * adding review feedback
    
    * removing unused functions
    
    * migrating command, old command under accounts still exist
    
    * reverting tos.go
    
    * fixing bazel lint
    
    * fixing build
    
    * resolving unused parameter caught by deepsource
    
    * fixing unit tests
    
    * Update cmd/prysmctl/validator/cmd.go
    
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>
    
    * Update cmd/prysmctl/validator/cmd.go
    
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>
    
    * adding in website
    
    * addressing review comment
    
    * adding new verify only flag
    
    * Update cmd/prysmctl/validator/withdraw.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/withdraw.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/withdraw.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/withdraw.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/beacon/client.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/beacon/client.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/beacon/client.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/beacon/client.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/withdraw.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * addressing comments
    
    * fixing unit test
    
    * Update cmd/prysmctl/validator/cmd.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/cmd.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/cmd.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/cmd.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * address comments
    
    * Update cmd/prysmctl/validator/cmd.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * adjusting object for new provided format
    
    * adding omit empty
    
    * fixing unit test, and linting
    
    * adjusting based on cli changes
    
    * fixing test data and metadata property
    
    * addressing backwards compatability
    
    * reverting based on review comment
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    Co-authored-by: Potuz <potuz@prysmaticlabs.com>
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>
    Co-authored-by: prylabs-bulldozer[bot] <58059840+prylabs-bulldozer[bot]@users.noreply.github.com>

---
+++ cmd/prysmctl/validator/withdraw.go
@@ -0,0 +1,171 @@
+package validator
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/logrusorgru/aurora"
+	"github.com/pkg/errors"
+	"github.com/prysmaticlabs/prysm/v3/api/client/beacon"
+	"github.com/prysmaticlabs/prysm/v3/beacon-chain/rpc/apimiddleware"
+	fieldparams "github.com/prysmaticlabs/prysm/v3/config/fieldparams"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+	"go.opencensus.io/trace"
+)
+
+func setWithdrawalAddresses(c *cli.Context) error {
+	ctx, span := trace.StartSpan(c.Context, "withdrawal.setWithdrawalAddresses")
+	defer span.End()
+	au := aurora.NewAurora(true)
+	beaconNodeHost := c.String(BeaconHostFlag.Name)
+	if !c.IsSet(PathFlag.Name) {
+		return fmt.Errorf("no --%s flag value was provided", PathFlag.Name)
+	}
+	setWithdrawalAddressJsons, err := getWithdrawalMessagesFromPathFlag(c)
+	if err != nil {
+		return err
+	}
+	for _, request := range setWithdrawalAddressJsons {
+		fmt.Println("SETTING VALIDATOR INDEX " + au.Red(request.Message.ValidatorIndex).String() + " TO WITHDRAWAL ADDRESS " + au.Red(request.Message.ToExecutionAddress).String())
+	}
+	return callWithdrawalEndpoints(ctx, beaconNodeHost, setWithdrawalAddressJsons)
+}
+
+func getWithdrawalMessagesFromPathFlag(c *cli.Context) ([]*apimiddleware.SignedBLSToExecutionChangeJson, error) {
+	setWithdrawalAddressJsons := make([]*apimiddleware.SignedBLSToExecutionChangeJson, 0)
+	foundFilePaths, err := findWithdrawalFiles(c.String(PathFlag.Name))
+	if err != nil {
+		return setWithdrawalAddressJsons, errors.Wrap(err, "failed to find withdrawal files")
+	}
+	for _, foundFilePath := range foundFilePaths {
+		b, err := os.ReadFile(filepath.Clean(foundFilePath))
+		if err != nil {
+			return setWithdrawalAddressJsons, errors.Wrap(err, "failed to open file")
+		}
+		var to []*apimiddleware.SignedBLSToExecutionChangeJson
+		if err := json.Unmarshal(b, &to); err != nil {
+			log.Warnf("provided file: %s, is not a list of signed withdrawal messages", foundFilePath)
+			continue
+		}
+		// verify 0x from file and add if needed
+		for i, obj := range to {
+			if len(obj.Message.FromBLSPubkey) == fieldparams.BLSPubkeyLength*2 {
+				to[i].Message.FromBLSPubkey = fmt.Sprintf("0x%s", obj.Message.FromBLSPubkey)
+			}
+			if len(obj.Message.ToExecutionAddress) == common.AddressLength*2 {
+				to[i].Message.ToExecutionAddress = fmt.Sprintf("0x%s", obj.Message.ToExecutionAddress)
+			}
+			if len(obj.Signature) == fieldparams.BLSSignatureLength*2 {
+				to[i].Signature = fmt.Sprintf("0x%s", obj.Signature)
+			}
+			setWithdrawalAddressJsons = append(setWithdrawalAddressJsons, &apimiddleware.SignedBLSToExecutionChangeJson{
+				Message: &apimiddleware.BLSToExecutionChangeJson{
+					ValidatorIndex:     to[i].Message.ValidatorIndex,
+					FromBLSPubkey:      to[i].Message.FromBLSPubkey,
+					ToExecutionAddress: to[i].Message.ToExecutionAddress,
+				},
+				Signature: to[i].Signature,
+			})
+		}
+
+	}
+	if len(setWithdrawalAddressJsons) == 0 {
+		return setWithdrawalAddressJsons, errors.New("the list of signed requests is empty")
+	}
+	return setWithdrawalAddressJsons, nil
+}
+
+func callWithdrawalEndpoints(ctx context.Context, host string, request []*apimiddleware.SignedBLSToExecutionChangeJson) error {
+	client, err := beacon.NewClient(host)
+	if err != nil {
+		return err
+	}
+	if err := client.SubmitChangeBLStoExecution(ctx, request); err != nil {
+		return err
+	}
+	log.Infof("Successfully published messages to update %d withdrawal addresses.", len(request))
+	return checkIfWithdrawsAreInPool(ctx, client, request)
+}
+
+func checkIfWithdrawsAreInPool(ctx context.Context, client *beacon.Client, request []*apimiddleware.SignedBLSToExecutionChangeJson) error {
+	log.Info("Verifying requested withdrawal messages known to node...")
+	poolResponse, err := client.GetBLStoExecutionChanges(ctx)
+	if err != nil {
+		return err
+	}
+	requestMap := make(map[string]string)
+	for _, w := range request {
+		requestMap[w.Message.ValidatorIndex] = w.Message.ToExecutionAddress
+	}
+	totalMessages := len(requestMap)
+	for _, resp := range poolResponse.Data {
+		value, found := requestMap[resp.Message.ValidatorIndex]
+		if found && value == resp.Message.ToExecutionAddress {
+			delete(requestMap, resp.Message.ValidatorIndex)
+		}
+	}
+	if len(requestMap) != 0 {
+		for key, address := range requestMap {
+			log.WithFields(log.Fields{
+				"validator_index":    key,
+				"execution_address:": address,
+			}).Warn("Set withdrawal address message not found in the node's operations pool.")
+		}
+		log.Warn("Please check before resubmitting. Set withdrawal address messages that were not found in the pool may have been already included into a block.")
+	} else {
+		log.Infof("All (total:%d) signed withdrawal messages were found in the pool.", totalMessages)
+	}
+	return nil
+}
+
+func findWithdrawalFiles(path string) ([]string, error) {
+	var foundpaths []string
+	maxdepth := 3
+	cleanpath := filepath.Clean(path)
+	if err := filepath.WalkDir(cleanpath, func(s string, d fs.DirEntry, e error) error {
+		if e != nil {
+			return e
+		}
+		if d.IsDir() && strings.Count(cleanpath, string(os.PathSeparator)) > maxdepth {
+			return fs.SkipDir
+		}
+
+		if filepath.Ext(d.Name()) == ".json" {
+			foundpaths = append(foundpaths, s)
+		}
+		return nil
+	}); err != nil {
+		return nil, errors.Wrap(err, "unable to find compatible files")
+	}
+	if len(foundpaths) == 0 {
+		return nil, errors.New("no compatible files were found")
+	}
+	log.Infof("found JSON files for setting withdrawals: %v", foundpaths)
+	return foundpaths, nil
+}
+
+func verifyWithdrawalsInPool(c *cli.Context) error {
+	ctx, span := trace.StartSpan(c.Context, "withdrawal.verifyWithdrawalsInPool")
+	defer span.End()
+	beaconNodeHost := c.String(BeaconHostFlag.Name)
+	if !c.IsSet(PathFlag.Name) {
+		return fmt.Errorf("no --%s flag value was provided", PathFlag.Name)
+	}
+	client, err := beacon.NewClient(beaconNodeHost)
+	if err != nil {
+		return err
+	}
+
+	request, err := getWithdrawalMessagesFromPathFlag(c)
+	if err != nil {
+		return err
+	}
+	return checkIfWithdrawsAreInPool(ctx, client, request)
+}
