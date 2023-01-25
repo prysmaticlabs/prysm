c6338e3a31e2a923b97db5d600f726c631cbee98
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-01-25 11:42:03 +0100

    beacon-chain, config: get consensus values from beacon config (#11798)
    * beacon-chain, config: get consensus values from beacon config
    
    * review feedback
    
    Co-authored-by: Nishant Das <nishdas93@gmail.com>
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- config/params/config.go
+++ config/params/config.go
@@ -234,3 +234,23 @@ func configForkNames(b *BeaconChainConfig) map[[fieldparams.VersionLength]byte]s
 	fvn[bytesutil.ToBytes4(b.CapellaForkVersion)] = "capella"
 	return fvn
 }
+
+// Eth1DataVotesLength returns the maximum length of the votes on the Eth1 data,
+// computed from the parameters in BeaconChainConfig.
+func (b *BeaconChainConfig) Eth1DataVotesLength() uint64 {
+	return uint64(b.EpochsPerEth1VotingPeriod.Mul(uint64(b.SlotsPerEpoch)))
+}
+
+// PreviousEpochAttestationsLength returns the maximum length of the pending
+// attestation list for the previous epoch, computed from the parameters in
+// BeaconChainConfig.
+func (b *BeaconChainConfig) PreviousEpochAttestationsLength() uint64 {
+	return uint64(b.SlotsPerEpoch.Mul(b.MaxAttestations))
+}
+
+// CurrentEpochAttestationsLength returns the maximum length of the pending
+// attestation list for the current epoch, computed from the parameters in
+// BeaconChainConfig.
+func (b *BeaconChainConfig) CurrentEpochAttestationsLength() uint64 {
+	return uint64(b.SlotsPerEpoch.Mul(b.MaxAttestations))
+}
