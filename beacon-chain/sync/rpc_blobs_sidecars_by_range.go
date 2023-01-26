35d3707de7aefca3338bc0240cc39de1106f2cc1
Inphi <mlaw2501@gmail.com>
2023-01-26 01:48:39 -0500

    sync: Fix BlobsSidecarByRange rate limiting (#11920)

--- beacon-chain/sync/rpc_blobs_sidecars_by_range.go
+++ beacon-chain/sync/rpc_blobs_sidecars_by_range.go
@@ -7,7 +7,6 @@ import (
 	libp2pcore "github.com/libp2p/go-libp2p/core"
 	"github.com/pkg/errors"
 	p2ptypes "github.com/prysmaticlabs/prysm/v3/beacon-chain/p2p/types"
-	"github.com/prysmaticlabs/prysm/v3/cmd/beacon-chain/flags"
 	"github.com/prysmaticlabs/prysm/v3/config/params"
 	"github.com/prysmaticlabs/prysm/v3/encoding/bytesutil"
 	"github.com/prysmaticlabs/prysm/v3/monitoring/tracing"
@@ -30,18 +29,22 @@ func (s *Service) blobsSidecarsByRangeRPCHandler(ctx context.Context, msg interf
 		return errors.New("message is not type *pb.BlobsSidecarsByRangeRequest")
 	}
 
+	if err := s.rateLimiter.validateRequest(stream, 1); err != nil {
+		return err
+	}
+
+	sidecarLimiter, err := s.rateLimiter.topicCollector(string(stream.Protocol()))
+	if err != nil {
+		return err
+	}
+
 	startSlot := r.StartSlot
 	count := r.Count
 	endSlot := startSlot.Add(count)
 
-	allowedBlocksPerSecond := uint64(flags.Get().BlockBatchLimit)
 	var numBlobs uint64
 	maxRequestBlobsSidecars := params.BeaconNetworkConfig().MaxRequestBlobsSidecars
 	for slot := startSlot; slot < endSlot && numBlobs < maxRequestBlobsSidecars; slot = slot.Add(1) {
-		if err := s.rateLimiter.validateRequest(stream, allowedBlocksPerSecond); err != nil {
-			return err
-		}
-
 		sidecars, err := s.cfg.beaconDB.BlobsSidecarsBySlot(ctx, slot)
 		if err != nil {
 			s.writeErrorResponseToStream(responseCodeServerError, p2ptypes.ErrGeneric.Error(), stream)
@@ -52,8 +55,7 @@ func (s *Service) blobsSidecarsByRangeRPCHandler(ctx context.Context, msg interf
 			continue
 		}
 
-		var respondedBlobs int64
-		for _, sidecar := range sidecars {
+		for i, sidecar := range sidecars {
 			if bytesutil.ToBytes32(sidecar.BeaconBlockRoot) == params.BeaconConfig().ZeroHash {
 				continue
 			}
@@ -64,46 +66,32 @@ func (s *Service) blobsSidecarsByRangeRPCHandler(ctx context.Context, msg interf
 				tracing.AnnotateError(span, chunkErr)
 				return chunkErr
 			}
-			respondedBlobs++
-		}
-		numBlobs++
-		s.rateLimiter.add(stream, respondedBlobs)
+			s.rateLimiter.add(stream, 1)
+			numBlobs += 1
 
-		// Short-circuit immediately once we've sent the last blob.
-		if slot.Add(1) >= endSlot {
-			break
-		}
+			// Short-circuit immediately once we've sent the last blob
+			if slot.Add(1) >= endSlot && i == len(sidecars)-1 {
+				break
+			}
 
-		key := stream.Conn().RemotePeer().String()
-		sidecarLimiter, err := s.rateLimiter.topicCollector(string(stream.Protocol()))
-		if err != nil {
-			return err
-		}
-		// Throttling - wait until we have enough tokens to send the next blobs
-
-		allowedBlocksBurst := int64(flags.Get().BlockBatchLimitBurstFactor * flags.Get().BlockBatchLimit)
-		if sidecarLimiter.Remaining(key) < allowedBlocksBurst {
-			timer := time.NewTimer(sidecarLimiter.TillEmpty(key))
-			select {
-			case <-ctx.Done():
-				timer.Stop()
-				return ctx.Err()
-			case <-timer.C:
-				timer.Stop()
+			key := stream.Conn().RemotePeer().String()
+			for {
+				if sidecarLimiter.Remaining(key) != 0 {
+					break
+				}
+				// Throttling - wait until we have enough tokens to send the next blobs
+				timer := time.NewTimer(time.Second * 1)
+				select {
+				case <-ctx.Done():
+					timer.Stop()
+					return ctx.Err()
+				case <-timer.C:
+					timer.Stop()
+				}
 			}
 		}
 	}
 
 	closeStream(stream, log)
 	return nil
-}
-
-func estimateBlobsSidecarCost(sidecar *pb.BlobsSidecar) int {
-	// This represents the fixed cost (in bytes) of the beacon_block_root and beacon_block_slot fields in the sidecar
-	const overheadCost = 32 + 8
-	cost := overheadCost
-	for _, blob := range sidecar.Blobs {
-		cost += len(blob.Data)
-	}
-	return cost
 }
