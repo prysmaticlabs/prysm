38f52d4f7b23ff23fa5f2938d283cfc250bc1258
Potuz <potuz@prysmaticlabs.com>
2023-01-26 22:46:22 +0100

    Rate limit BLS changes broadcasting

--- beacon-chain/sync/broadcast_bls_changes.go
+++ beacon-chain/sync/broadcast_bls_changes.go
@@ -1,49 +1,36 @@
 package sync
 
 import (
-	"time"
-
 	"github.com/prysmaticlabs/prysm/v3/config/params"
 	types "github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
+	"github.com/prysmaticlabs/prysm/v3/crypto/rand"
+	ethpb "github.com/prysmaticlabs/prysm/v3/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/v3/time/slots"
 )
 
-const broadcastBLSChangesRateLimit = 1000
-
-// This routine broadcasts all known BLS changes at the Capella fork.
+// This routine broadcasts known BLS changes at the Capella fork.
 func (s *Service) broadcastBLSChanges(currSlot types.Slot) {
 	capellaSlotStart, err := slots.EpochStart(params.BeaconConfig().CapellaForkEpoch)
 	if err != nil {
 		// only possible error is an overflow, so we exit early from the method
 		return
 	}
-	if currSlot == capellaSlotStart {
-		changes, err := s.cfg.blsToExecPool.PendingBLSToExecChanges()
-		if err != nil {
-			log.WithError(err).Error("could not get BLS to execution changes")
-		}
-		go func() {
-			st := slots.NewSlotTicker(time.Now(), params.BeaconConfig().SecondsPerSlot)
-			for {
-				select {
-				case <-s.ctx.Done():
-					return
-				case <-st.C():
-					limit := broadcastBLSChangesRateLimit
-					if len(changes) < broadcastBLSChangesRateLimit {
-						limit = len(changes)
-					}
-					for _, ch := range changes[:limit] {
-						if err := s.cfg.p2p.Broadcast(s.ctx, ch); err != nil {
-							log.WithError(err).Error("could not broadcast BLS to execution changes.")
-						}
-						changes = changes[limit:]
-						if len(changes) == 0 {
-							return
-						}
-					}
-				}
-			}
-		}()
+	if currSlot != capellaSlotStart {
+		return
 	}
+	changes, err := s.cfg.blsToExecPool.PendingBLSToExecChanges()
+	if err != nil {
+		log.WithError(err).Error("could not get BLS to execution changes")
+	}
+	if len(changes) == 0 {
+		return
+	}
+	source := rand.NewGenerator()
+	broadcastChanges := make([]*ethpb.SignedBLSToExecutionChange, len(changes))
+	for i := 0; i < len(changes); i++ {
+		idx := source.Intn(len(changes))
+		broadcastChanges[i] = changes[idx]
+		changes = append(changes[:idx], changes[idx+1:]...)
+	}
+	s.cfg.p2p.BroadcastBLSChanges(s.ctx, changes)
 }
