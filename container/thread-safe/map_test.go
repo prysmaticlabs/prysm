41820b9edb01efd44bd06de653e65231144eb404
rauljordan <raul@prysmaticlabs.com>
2023-01-30 20:45:37 -0500

    push up benchmarks

--- container/thread-safe/map_test.go
+++ container/thread-safe/map_test.go
@@ -8,6 +8,55 @@ import (
 	"github.com/prysmaticlabs/prysm/v3/testing/require"
 )
 
+type safeMap struct {
+	items map[int]string
+	lock  sync.RWMutex
+}
+
+func (s *safeMap) Get(k int) (string, bool) {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	v, ok := s.items[k]
+	return v, ok
+}
+
+func (s *safeMap) Put(i int, str string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.items[i] = str
+}
+
+func (s *safeMap) Delete(i int) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	delete(s.items, i)
+}
+
+func BenchmarkMap_Concrete(b *testing.B) {
+	mm := &safeMap{
+		items: make(map[int]string),
+	}
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 1000; j++ {
+			mm.Put(j, "foo")
+			mm.Get(j)
+			mm.Delete(j)
+		}
+	}
+}
+
+func BenchmarkMap_Generic(b *testing.B) {
+	items := make(map[int]string)
+	mm := NewThreadSafeMap(items)
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 1000; j++ {
+			mm.Put(j, "foo")
+			mm.Get(j)
+			mm.Delete(j)
+		}
+	}
+}
+
 func TestMap(t *testing.T) {
 	m := map[int]string{
 		1:     "foo",
