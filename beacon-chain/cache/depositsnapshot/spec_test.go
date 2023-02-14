b346659e845de67fd2cba1333ce38118cadb522f
Saolyn <sammy@rosso.red>
2023-02-14 19:03:37 +0100

    Cleanup

--- beacon-chain/cache/depositsnapshot/spec_test.go
+++ beacon-chain/cache/depositsnapshot/spec_test.go
@@ -12,7 +12,6 @@ import (
 	eth "github.com/prysmaticlabs/prysm/v3/proto/eth/v1"
 	"github.com/prysmaticlabs/prysm/v3/testing/assert"
 	"gopkg.in/yaml.v3"
-	"reflect"
 )
 
 type testCase struct {
@@ -252,18 +251,6 @@ func TestDepositCases(t *testing.T) {
 	for _, c := range testCases {
 		err = tree.pushLeaf(c.DepositDataRoot)
 		assert.NoError(t, err)
-		if c.Eth1Data.DepositRoot != c.Snapshot.depositRoot {
-			t.Fatalf(
-				"Eth1data deposit root %#x != snapshot deposit root %#x",
-				c.Eth1Data.DepositRoot,
-				c.Snapshot.depositRoot,
-			)
-			t.Fatalf(
-				"Eth1data deposit root %#x != tree root %#x",
-				c.Eth1Data.DepositRoot,
-				tree.getRoot(),
-			)
-		}
 	}
 }
 
@@ -287,11 +274,7 @@ func TestFinalization(t *testing.T) {
 	assert.Equal(t, tree.getRoot(), originalRoot)
 	snapshotData, err := tree.getSnapshot()
 	assert.NoError(t, err)
-
-	want := testCases[100].Snapshot.DepositTreeSnapshot
-	if !reflect.DeepEqual(want, snapshotData) {
-		t.Fatalf("Snapshot and data not equal: %+v but got %+v", want, snapshotData)
-	}
+	assert.DeepEqual(t, testCases[100].Snapshot.DepositTreeSnapshot, snapshotData)
 	// create a copy of the tree from a snapshot by replaying
 	// the deposits after the finalized deposit
 	cp := cloneFromSnapshot(t, snapshotData, testCases[101:128])
