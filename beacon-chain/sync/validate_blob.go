cc4b3977d4bcc48c6e2641646b839ba6ad1128bb
terence tsao <terence@prysmaticlabs.com>
2023-02-16 18:49:25 -0800

    Starting validate blob

--- beacon-chain/sync/validate_blob.go
+++ beacon-chain/sync/validate_blob.go
@@ -7,6 +7,15 @@ import (
 	"github.com/libp2p/go-libp2p/core/peer"
 )
 
-func (s *Service) validateBlob(ctx context.Context, id peer.ID, message *pubsub.Message) (pubsub.ValidationResult, error) {
+func (s *Service) validateBlob(ctx context.Context, pid peer.ID, msg *pubsub.Message) (pubsub.ValidationResult, error) {
+	if pid == s.cfg.p2p.PeerID() {
+		return pubsub.ValidationAccept, nil
+	}
+	if s.cfg.initialSync.Syncing() {
+		return pubsub.ValidationIgnore, nil
+	}
+	if msg.Topic == nil {
+		return pubsub.ValidationReject, errInvalidTopic
+	}
 	return pubsub.ValidationAccept, nil
 }
