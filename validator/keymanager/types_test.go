753e285fb6f0b43c4870f5c5ff2854604103bf0f
james-prysm <90280386+james-prysm@users.noreply.github.com>
2023-03-08 21:21:12 -0600

    Prysm V4: Remove Prysm Remote Signer (#11895)
    * removing all prysm remote signer code
    
    * fixing unit tests
    
    * resolving more build issues
    
    * resolving deepsource complaint
    
    * fixing lint
    
    * trying to fix bazel library
    
    * trying testonly true
    
    * removing assert and require from non test settings
    
    * fixing bazel and tests
    
    * removing more unused files related to remote signer
    
    * fixing linting
    
    * reverting some changes
    
    * reverting a change that broke some code
    
    * removing typo
    
    * fixing unit test
    
    * fixing mnemonic information

--- validator/keymanager/types_test.go
+++ validator/keymanager/types_test.go
@@ -10,14 +10,12 @@ import (
 	"github.com/prysmaticlabs/prysm/v3/validator/keymanager"
 	"github.com/prysmaticlabs/prysm/v3/validator/keymanager/derived"
 	"github.com/prysmaticlabs/prysm/v3/validator/keymanager/local"
-	"github.com/prysmaticlabs/prysm/v3/validator/keymanager/remote"
 	remoteweb3signer "github.com/prysmaticlabs/prysm/v3/validator/keymanager/remote-web3signer"
 )
 
 var (
 	_ = keymanager.IKeymanager(&local.Keymanager{})
 	_ = keymanager.IKeymanager(&derived.Keymanager{})
-	_ = keymanager.IKeymanager(&remote.Keymanager{})
 
 	// More granular assertions.
 	_ = keymanager.KeysFetcher(&local.Keymanager{})
