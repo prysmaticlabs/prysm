1468a608aaecb1d03475f508a050c3cb8909a532
Potuz <potuz@prysmaticlabs.com>
2023-03-16 13:13:13 -0300

    rpc tests

--- beacon-chain/rpc/eth/beacon/config_test.go
+++ beacon-chain/rpc/eth/beacon/config_test.go
@@ -166,7 +166,7 @@ func TestGetSpec(t *testing.T) {
 		case "HYSTERESIS_UPWARD_MULTIPLIER":
 			assert.Equal(t, "11", v)
 		case "SAFE_SLOTS_TO_UPDATE_JUSTIFIED":
-			assert.Equal(t, "12", v)
+			assert.Equal(t, "0", v)
 		case "ETH1_FOLLOW_DISTANCE":
 			assert.Equal(t, "13", v)
 		case "TARGET_AGGREGATORS_PER_COMMITTEE":
