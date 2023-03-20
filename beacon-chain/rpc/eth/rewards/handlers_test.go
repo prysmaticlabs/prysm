9bac92aa1f077bb322cee99c13c3bbe4e8d348f7
rkapka <rkapka@wp.pl>
2023-03-20 17:23:36 +0100

    complete test

--- beacon-chain/rpc/eth/rewards/handlers_test.go
+++ beacon-chain/rpc/eth/rewards/handlers_test.go
@@ -2,19 +2,29 @@ package rewards
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/prysmaticlabs/go-bitfield"
 	mock "github.com/prysmaticlabs/prysm/v3/beacon-chain/blockchain/testing"
+	"github.com/prysmaticlabs/prysm/v3/beacon-chain/core/altair"
 	"github.com/prysmaticlabs/prysm/v3/beacon-chain/core/signing"
+	"github.com/prysmaticlabs/prysm/v3/beacon-chain/rpc/eth/helpers"
 	"github.com/prysmaticlabs/prysm/v3/beacon-chain/rpc/testutil"
 	mockstategen "github.com/prysmaticlabs/prysm/v3/beacon-chain/state/stategen/mock"
+	fieldparams "github.com/prysmaticlabs/prysm/v3/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/v3/config/params"
 	"github.com/prysmaticlabs/prysm/v3/consensus-types/blocks"
+	"github.com/prysmaticlabs/prysm/v3/consensus-types/interfaces"
+	"github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v3/crypto/bls"
+	"github.com/prysmaticlabs/prysm/v3/crypto/bls/blst"
 	"github.com/prysmaticlabs/prysm/v3/encoding/bytesutil"
 	eth "github.com/prysmaticlabs/prysm/v3/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/v3/testing/assert"
 	"github.com/prysmaticlabs/prysm/v3/testing/require"
 	"github.com/prysmaticlabs/prysm/v3/testing/util"
 )
@@ -27,11 +37,12 @@ func TestBlockRewards(t *testing.T) {
 	require.NoError(t, err)
 	validators := make([]*eth.Validator, 0, valCount)
 	balances := make([]uint64, 0, valCount)
-	keys := make([]bls.SecretKey, 0, valCount)
+	secretKeys := make([]bls.SecretKey, 0, valCount)
+	pubkeys := make([][]byte, 0, valCount)
 	for i := 0; i < valCount; i++ {
 		blsKey, err := bls.RandKey()
 		require.NoError(t, err)
-		keys = append(keys, blsKey)
+		secretKeys = append(secretKeys, blsKey)
 		validators = append(validators, &eth.Validator{
 			PublicKey:         blsKey.PublicKey().Marshal(),
 			ExitEpoch:         params.BeaconConfig().FarFutureEpoch,
@@ -39,12 +50,20 @@ func TestBlockRewards(t *testing.T) {
 			EffectiveBalance:  params.BeaconConfig().MaxEffectiveBalance,
 		})
 		balances = append(balances, params.BeaconConfig().MaxEffectiveBalance)
+		pubkeys = append(pubkeys, blsKey.PublicKey().Marshal())
 	}
 	require.NoError(t, st.SetValidators(validators))
 	require.NoError(t, st.SetBalances(balances))
 	require.NoError(t, st.SetCurrentParticipationBits(make([]byte, valCount)))
+	syncCommittee, err := altair.NextSyncCommittee(context.Background(), st)
+	require.NoError(t, err)
+	require.NoError(t, st.SetCurrentSyncCommittee(syncCommittee))
+	slot0bRoot := bytesutil.PadTo([]byte("slot0root"), 32)
+	bRoots := make([][]byte, fieldparams.BlockRootsLength)
+	bRoots[0] = slot0bRoot
+	require.NoError(t, st.SetBlockRoots(bRoots))
 
-	b := util.NewBeaconBlockAltair()
+	b := util.HydrateSignedBeaconBlockAltair(util.NewBeaconBlockAltair())
 	b.Block.Slot = 2
 	// we have to set the proposer index to the value that will be randomly chosen (fortunately it's deterministic)
 	b.Block.ProposerIndex = 12
@@ -52,10 +71,12 @@ func TestBlockRewards(t *testing.T) {
 		{
 			AggregationBits: bitfield.Bitlist{0b00000111},
 			Data:            util.HydrateAttestationData(&eth.AttestationData{}),
+			Signature:       make([]byte, fieldparams.BLSSignatureLength),
 		},
 		{
 			AggregationBits: bitfield.Bitlist{0b00000111},
 			Data:            util.HydrateAttestationData(&eth.AttestationData{}),
+			Signature:       make([]byte, fieldparams.BLSSignatureLength),
 		},
 	}
 	attData1 := util.HydrateAttestationData(&eth.AttestationData{BeaconBlockRoot: bytesutil.PadTo([]byte("root1"), 32)})
@@ -71,12 +92,12 @@ func TestBlockRewards(t *testing.T) {
 			Attestation_1: &eth.IndexedAttestation{
 				AttestingIndices: []uint64{0},
 				Data:             attData1,
-				Signature:        keys[0].Sign(sigRoot1[:]).Marshal(),
+				Signature:        secretKeys[0].Sign(sigRoot1[:]).Marshal(),
 			},
 			Attestation_2: &eth.IndexedAttestation{
 				AttestingIndices: []uint64{0},
 				Data:             attData2,
-				Signature:        keys[0].Sign(sigRoot2[:]).Marshal(),
+				Signature:        secretKeys[0].Sign(sigRoot2[:]).Marshal(),
 			},
 		},
 	}
@@ -104,31 +125,76 @@ func TestBlockRewards(t *testing.T) {
 		{
 			Header_1: &eth.SignedBeaconBlockHeader{
 				Header:    header1,
-				Signature: keys[1].Sign(sigRoot1[:]).Marshal(),
+				Signature: secretKeys[1].Sign(sigRoot1[:]).Marshal(),
 			},
 			Header_2: &eth.SignedBeaconBlockHeader{
 				Header:    header2,
-				Signature: keys[1].Sign(sigRoot2[:]).Marshal(),
+				Signature: secretKeys[1].Sign(sigRoot2[:]).Marshal(),
 			},
 		},
 	}
+	scBits := bitfield.NewBitvector512()
+	scBits.SetBitAt(10, true)
+	scBits.SetBitAt(100, true)
+	domain, err = signing.Domain(st.Fork(), 0, params.BeaconConfig().DomainSyncCommittee, st.GenesisValidatorsRoot())
+	require.NoError(t, err)
+	sszBytes := primitives.SSZBytes(slot0bRoot)
+	r, err := signing.ComputeSigningRoot(&sszBytes, domain)
+	require.NoError(t, err)
+	// Bits set in sync committee bits determine which validators will be treated as participating in sync committee.
+	// These validators have to sign the message.
+	sig1, err := blst.SignatureFromBytes(secretKeys[47].Sign(r[:]).Marshal())
+	require.NoError(t, err)
+	sig2, err := blst.SignatureFromBytes(secretKeys[19].Sign(r[:]).Marshal())
+	require.NoError(t, err)
+	aggSig := bls.AggregateSignatures([]bls.Signature{sig1, sig2}).Marshal()
+	b.Block.Body.SyncAggregate = &eth.SyncAggregate{SyncCommitteeBits: scBits, SyncCommitteeSignature: aggSig}
 
 	sbb, err := blocks.NewSignedBeaconBlock(b)
 	require.NoError(t, err)
-	mockChainService := &mock.ChainService{}
+	phase0block, err := blocks.NewSignedBeaconBlock(util.NewBeaconBlock())
+	require.NoError(t, err)
+	mockChainService := &mock.ChainService{Optimistic: true}
 	s := &Server{
-		BlockFetcher:          &testutil.MockBlockFetcher{BlockToReturn: sbb},
+		BlockFetcher: &testutil.MockBlockFetcher{SlotBlockMap: map[primitives.Slot]interfaces.ReadOnlySignedBeaconBlock{
+			0: phase0block,
+			2: sbb,
+		}},
 		OptimisticModeFetcher: mockChainService,
 		FinalizationFetcher:   mockChainService,
 		ReplayerBuilder:       mockstategen.NewMockReplayerBuilder(mockstategen.WithMockState(st)),
 	}
 
 	t.Run("ok", func(t *testing.T) {
+		url := "http://only.the.slot.number.at.the.end.is.important/2"
+		request := httptest.NewRequest("GET", url, nil)
+		writer := httptest.NewRecorder()
+		writer.Body = &bytes.Buffer{}
+
+		s.BlockRewards(writer, request)
+		assert.Equal(t, http.StatusOK, writer.Code)
+		resp := &BlockRewardsResponse{}
+		require.NoError(t, json.Unmarshal(writer.Body.Bytes(), resp))
+		assert.Equal(t, primitives.ValidatorIndex(12), resp.Data.ProposerIndex)
+		assert.Equal(t, uint64(125089490), resp.Data.Total)
+		assert.Equal(t, uint64(89442), resp.Data.Attestations)
+		assert.Equal(t, uint64(48), resp.Data.SyncAggregate)
+		assert.Equal(t, uint64(62500000), resp.Data.AttesterSlashings)
+		assert.Equal(t, uint64(62500000), resp.Data.ProposerSlashings)
+		assert.Equal(t, true, resp.ExecutionOptimistic)
+		assert.Equal(t, false, resp.Finalized)
+	})
+	t.Run("phase 0", func(t *testing.T) {
 		url := "http://only.the.slot.number.at.the.end.is.important/0"
 		request := httptest.NewRequest("GET", url, nil)
 		writer := httptest.NewRecorder()
 		writer.Body = &bytes.Buffer{}
 
 		s.BlockRewards(writer, request)
+		assert.Equal(t, http.StatusBadRequest, writer.Code)
+		e := &helpers.DefaultErrorJson{}
+		require.NoError(t, json.Unmarshal(writer.Body.Bytes(), e))
+		assert.Equal(t, http.StatusBadRequest, e.Code)
+		assert.Equal(t, "block rewards are not supported for Phase 0 blocks", e.Message)
 	})
 }
--- beacon-chain/rpc/testutil/mock_block_fetcher.go
+++ beacon-chain/rpc/testutil/mock_block_fetcher.go
@@ -2,19 +2,26 @@ package testutil
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/prysmaticlabs/prysm/v3/consensus-types/interfaces"
+	"github.com/prysmaticlabs/prysm/v3/consensus-types/primitives"
 )
 
 // MockBlockFetcher is a fake implementation of blockfetcher.Fetcher.
 type MockBlockFetcher struct {
 	BlockToReturn interfaces.ReadOnlySignedBeaconBlock
 	ErrorToReturn error
+	SlotBlockMap  map[primitives.Slot]interfaces.ReadOnlySignedBeaconBlock
 }
 
-func (m *MockBlockFetcher) Block(_ context.Context, _ []byte) (interfaces.ReadOnlySignedBeaconBlock, error) {
+func (m *MockBlockFetcher) Block(_ context.Context, b []byte) (interfaces.ReadOnlySignedBeaconBlock, error) {
 	if m.ErrorToReturn != nil {
 		return nil, m.ErrorToReturn
 	}
-	return m.BlockToReturn, nil
+	slotNumber, parseErr := strconv.ParseUint(string(b), 10, 64)
+	if parseErr != nil {
+		return m.BlockToReturn, nil
+	}
+	return m.SlotBlockMap[primitives.Slot(slotNumber)], nil
 }
