97d42f7f84f642180ee92e8c10bc3a60c6d0f778
nisdas <nishdas93@gmail.com>
2023-03-21 16:10:13 +0800

    gofmt

--- proto/eth/v2/beacon_block.pb.go
+++ proto/eth/v2/beacon_block.pb.go
@@ -7,14 +7,15 @@
 package eth
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	github_com_prysmaticlabs_prysm_v4_consensus_types_primitives "github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
 	v11 "github.com/prysmaticlabs/prysm/v4/proto/engine/v1"
 	_ "github.com/prysmaticlabs/prysm/v4/proto/eth/ext"
 	v1 "github.com/prysmaticlabs/prysm/v4/proto/eth/v1"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
--- proto/eth/v2/validator.pb.go
+++ proto/eth/v2/validator.pb.go
@@ -7,13 +7,14 @@
 package eth
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	github_com_prysmaticlabs_go_bitfield "github.com/prysmaticlabs/go-bitfield"
 	github_com_prysmaticlabs_prysm_v4_consensus_types_primitives "github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
 	_ "github.com/prysmaticlabs/prysm/v4/proto/eth/ext"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
