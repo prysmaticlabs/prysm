3f3fdb548f4ba1431fc47d723915cce1718beccc
rkapka <rkapka@wp.pl>
2023-04-03 16:16:21 +0200

    unblinder

--- beacon-chain/rpc/prysm/v1alpha1/validator/unblinder_test.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/unblinder_test.go
@@ -15,9 +15,11 @@ import (
 	"github.com/prysmaticlabs/prysm/v4/testing/util"
 )
 
-func TestServer_unblindBuilderCapellaBlock(t *testing.T) {
-	p := emptyPayloadCapella()
+func Test_unblindBuilderBlock(t *testing.T) {
+	p := emptyPayload()
 	p.GasLimit = 123
+	pCapella := emptyPayloadCapella()
+	pCapella.GasLimit = 123
 
 	tests := []struct {
 		name        string
@@ -26,11 +28,6 @@ func TestServer_unblindBuilderCapellaBlock(t *testing.T) {
 		err         string
 		returnedBlk interfaces.SignedBeaconBlock
 	}{
-		{
-			name: "nil block",
-			blk:  nil,
-			err:  "signed beacon block can't be nil",
-		},
 		{
 			name: "old block version",
 			blk: func() interfaces.SignedBeaconBlock {
@@ -63,7 +60,7 @@ func TestServer_unblindBuilderCapellaBlock(t *testing.T) {
 		{
 			name: "submit blind block error",
 			blk: func() interfaces.SignedBeaconBlock {
-				b := util.NewBlindedBeaconBlockCapella()
+				b := util.NewBlindedBeaconBlockBellatrix()
 				b.Block.Slot = 1
 				b.Block.ProposerIndex = 2
 				wb, err := blocks.NewSignedBeaconBlock(b)
@@ -71,21 +68,84 @@ func TestServer_unblindBuilderCapellaBlock(t *testing.T) {
 				return wb
 			}(),
 			mock: &builderTest.MockBuilderService{
-				PayloadCapella:        &v1.ExecutionPayloadCapella{},
+				Payload:               &v1.ExecutionPayload{},
 				HasConfigured:         true,
 				ErrSubmitBlindedBlock: errors.New("can't submit"),
 			},
 			err: "can't submit",
 		},
 		{
-			name: "can get payload",
+			name: "head and payload root mismatch",
+			blk: func() interfaces.SignedBeaconBlock {
+				b := util.NewBlindedBeaconBlockBellatrix()
+				b.Block.Slot = 1
+				b.Block.ProposerIndex = 2
+				wb, err := blocks.NewSignedBeaconBlock(b)
+				require.NoError(t, err)
+				return wb
+			}(),
+			mock: &builderTest.MockBuilderService{
+				HasConfigured: true,
+				Payload:       p,
+			},
+			returnedBlk: func() interfaces.SignedBeaconBlock {
+				b := util.NewBeaconBlockBellatrix()
+				b.Block.Slot = 1
+				b.Block.ProposerIndex = 2
+				b.Block.Body.ExecutionPayload = p
+				wb, err := blocks.NewSignedBeaconBlock(b)
+				require.NoError(t, err)
+				return wb
+			}(),
+			err: "header and payload root do not match",
+		},
+		{
+			name: "can get payload Bellatrix",
+			blk: func() interfaces.SignedBeaconBlock {
+				b := util.NewBlindedBeaconBlockBellatrix()
+				b.Block.Slot = 1
+				b.Block.ProposerIndex = 2
+				txRoot, err := ssz.TransactionsRoot([][]byte{})
+				require.NoError(t, err)
+				b.Block.Body.ExecutionPayloadHeader = &v1.ExecutionPayloadHeader{
+					ParentHash:       make([]byte, fieldparams.RootLength),
+					FeeRecipient:     make([]byte, fieldparams.FeeRecipientLength),
+					StateRoot:        make([]byte, fieldparams.RootLength),
+					ReceiptsRoot:     make([]byte, fieldparams.RootLength),
+					LogsBloom:        make([]byte, fieldparams.LogsBloomLength),
+					PrevRandao:       make([]byte, fieldparams.RootLength),
+					BaseFeePerGas:    make([]byte, fieldparams.RootLength),
+					BlockHash:        make([]byte, fieldparams.RootLength),
+					TransactionsRoot: txRoot[:],
+					GasLimit:         123,
+				}
+				wb, err := blocks.NewSignedBeaconBlock(b)
+				require.NoError(t, err)
+				return wb
+			}(),
+			mock: &builderTest.MockBuilderService{
+				HasConfigured: true,
+				Payload:       p,
+			},
+			returnedBlk: func() interfaces.SignedBeaconBlock {
+				b := util.NewBeaconBlockBellatrix()
+				b.Block.Slot = 1
+				b.Block.ProposerIndex = 2
+				b.Block.Body.ExecutionPayload = p
+				wb, err := blocks.NewSignedBeaconBlock(b)
+				require.NoError(t, err)
+				return wb
+			}(),
+		},
+		{
+			name: "can get payload Capella",
 			blk: func() interfaces.SignedBeaconBlock {
 				b := util.NewBlindedBeaconBlockCapella()
 				b.Block.Slot = 1
 				b.Block.ProposerIndex = 2
-				txRoot, err := ssz.TransactionsRoot(make([][]byte, 0))
+				txRoot, err := ssz.TransactionsRoot([][]byte{})
 				require.NoError(t, err)
-				wdRoot, err := ssz.WithdrawalSliceRoot([]*v1.Withdrawal{}, fieldparams.MaxWithdrawalsPerPayload)
+				withdrawalsRoot, err := ssz.WithdrawalSliceRoot([]*v1.Withdrawal{}, fieldparams.MaxWithdrawalsPerPayload)
 				require.NoError(t, err)
 				b.Block.Body.ExecutionPayloadHeader = &v1.ExecutionPayloadHeaderCapella{
 					ParentHash:       make([]byte, fieldparams.RootLength),
@@ -97,8 +157,8 @@ func TestServer_unblindBuilderCapellaBlock(t *testing.T) {
 					BaseFeePerGas:    make([]byte, fieldparams.RootLength),
 					BlockHash:        make([]byte, fieldparams.RootLength),
 					TransactionsRoot: txRoot[:],
+					WithdrawalsRoot:  withdrawalsRoot[:],
 					GasLimit:         123,
-					WithdrawalsRoot:  wdRoot[:],
 				}
 				wb, err := blocks.NewSignedBeaconBlock(b)
 				require.NoError(t, err)
@@ -106,13 +166,13 @@ func TestServer_unblindBuilderCapellaBlock(t *testing.T) {
 			}(),
 			mock: &builderTest.MockBuilderService{
 				HasConfigured:  true,
-				PayloadCapella: p,
+				PayloadCapella: pCapella,
 			},
 			returnedBlk: func() interfaces.SignedBeaconBlock {
 				b := util.NewBeaconBlockCapella()
 				b.Block.Slot = 1
 				b.Block.ProposerIndex = 2
-				b.Block.Body.ExecutionPayload = p
+				b.Block.Body.ExecutionPayload = pCapella
 				wb, err := blocks.NewSignedBeaconBlock(b)
 				require.NoError(t, err)
 				return wb
@@ -121,8 +181,9 @@ func TestServer_unblindBuilderCapellaBlock(t *testing.T) {
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			vs := &Server{BlockBuilder: tc.mock}
-			gotBlk, err := vs.unblindBuilderBlockCapella(context.Background(), tc.blk)
+			unblinder, err := newUnblinder(tc.blk, tc.mock)
+			require.NoError(t, err)
+			gotBlk, err := unblinder.unblindBuilderBlock(context.Background())
 			if tc.err != "" {
 				require.ErrorContains(t, tc.err, err)
 			} else {
