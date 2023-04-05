ffacd2735a8511dad86f0e9756451247eaf43739
Radosław Kapka <rkapka@wp.pl>
2023-04-05 15:49:18 +0200

    Merge all block unblinding code into a single `unblinder` struct (#12232)

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer.go
@@ -302,16 +302,13 @@ func (vs *Server) proposeGenericBeaconBlock(ctx context.Context, req *ethpb.Gene
 		return nil, fmt.Errorf("could not tree hash block: %v", err)
 	}
 
-	if slots.ToEpoch(blk.Block().Slot()) >= params.BeaconConfig().CapellaForkEpoch {
-		blk, err = vs.unblindBuilderBlockCapella(ctx, blk)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		blk, err = vs.unblindBuilderBlock(ctx, blk)
-		if err != nil {
-			return nil, err
-		}
+	unblinder, err := newUnblinder(blk, vs.BlockBuilder)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create unblinder")
+	}
+	blk, err = unblinder.unblindBuilderBlock(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not unblind builder block")
 	}
 
 	// Broadcast the new block to the network.
