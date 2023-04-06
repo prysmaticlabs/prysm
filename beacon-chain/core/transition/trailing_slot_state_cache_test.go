333ce44059c4ca6c23f9661a2988cf7969d591b8
nisdas <nishdas93@gmail.com>
2023-04-06 16:48:00 +0800

    add regression test

--- beacon-chain/core/transition/trailing_slot_state_cache_test.go
+++ beacon-chain/core/transition/trailing_slot_state_cache_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/prysmaticlabs/prysm/v4/beacon-chain/core/transition"
 	"github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
+	"github.com/prysmaticlabs/prysm/v4/testing/assert"
 	"github.com/prysmaticlabs/prysm/v4/testing/require"
 	"github.com/prysmaticlabs/prysm/v4/testing/util"
 )
@@ -32,4 +33,17 @@ func TestTrailingSlotState_RoundTrip(t *testing.T) {
 	lastRoot, lastState = transition.LastCachedState()
 	require.DeepEqual(t, r, lastRoot)
 	require.Equal(t, s.Slot(), lastState.Slot())
+}
+
+func TestTrailingSlotState_StateAdvancedBeyondRequest(t *testing.T) {
+	ctx := context.Background()
+	r := []byte{'a'}
+	s := transition.NextSlotState(r, 0)
+	require.Equal(t, nil, s)
+
+	s, _ = util.DeterministicGenesisState(t, 1)
+	assert.NoError(t, s.SetSlot(2))
+	require.NoError(t, transition.UpdateNextSlotCache(ctx, r, s))
+	s = transition.NextSlotState(r, 1)
+	require.Equal(t, nil, s)
 }
