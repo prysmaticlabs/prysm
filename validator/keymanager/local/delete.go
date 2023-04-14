3c84cc8a350231a26a915472530c9b9bbcc1a464
james-prysm <james@prysmaticlabs.com>
2023-04-14 11:57:34 -0500

    WIP changes for keymanager

--- validator/keymanager/local/delete.go
+++ validator/keymanager/local/delete.go
@@ -16,6 +16,12 @@ import (
 // DeleteKeystores takes in public keys and removes the accounts from the wallet.
 // This includes their disk keystore and cached keystore, but maintains the slashing
 // protection history in the database.
+// 1) Copy the in memory keystore
+// 2) Delete the keys from copied in memory keystore
+// 3) Save the copy to disk
+// 4) Reinitialize account store from disk
+// 5) Verify keys are indeed deleted
+// 6) Return API response
 func (km *Keymanager) DeleteKeystores(
 	ctx context.Context, publicKeys [][]byte,
 ) ([]*ethpbservice.DeletedKeystoreStatus, error) {
@@ -25,8 +31,15 @@ func (km *Keymanager) DeleteKeystores(
 	var store *AccountsKeystoreRepresentation
 	var err error
 	deletedKeys := make([][]byte, 0, len(publicKeys))
+	// 1) Copy the in memory keystore
+	storeCopy := km.accountsStore
+	storeCopy.PrivateKeys = make([][]byte, len(km.accountsStore.PrivateKeys))
+	copy(storeCopy.PrivateKeys, km.accountsStore.PrivateKeys)
+	storeCopy.PublicKeys = make([][]byte, len(km.accountsStore.PublicKeys))
+	copy(storeCopy.PublicKeys, km.accountsStore.PublicKeys)
+	//
 	for _, publicKey := range publicKeys {
-		// Check if the key in the request is a duplicate.
+		// Check if the key in the request is a duplicate or not found
 		if _, ok := trackedPublicKeys[bytesutil.ToBytes48(publicKey)]; ok {
 			statuses = append(statuses, &ethpbservice.DeletedKeystoreStatus{
 				Status: ethpbservice.DeletedKeystoreStatus_NOT_ACTIVE,
@@ -35,7 +48,7 @@ func (km *Keymanager) DeleteKeystores(
 		}
 		var index int
 		var found bool
-		for j, pubKey := range km.accountsStore.PublicKeys {
+		for j, pubKey := range storeCopy.PublicKeys {
 			if bytes.Equal(pubKey, publicKey) {
 				index = j
 				found = true
@@ -48,14 +61,18 @@ func (km *Keymanager) DeleteKeystores(
 			})
 			continue
 		}
-		deletedPublicKey := km.accountsStore.PublicKeys[index]
+		//
+		// 2) Delete the keys from copied in memory keystore
+		deletedPublicKey := storeCopy.PublicKeys[index]
 		deletedKeys = append(deletedKeys, deletedPublicKey)
-		km.accountsStore.PrivateKeys = append(km.accountsStore.PrivateKeys[:index], km.accountsStore.PrivateKeys[index+1:]...)
-		km.accountsStore.PublicKeys = append(km.accountsStore.PublicKeys[:index], km.accountsStore.PublicKeys[index+1:]...)
-		store, err = km.CreateAccountsKeystore(ctx, km.accountsStore.PrivateKeys, km.accountsStore.PublicKeys)
+
+		storeCopy.PrivateKeys = append(storeCopy.PrivateKeys[:index], storeCopy.PrivateKeys[index+1:]...)
+		storeCopy.PublicKeys = append(storeCopy.PublicKeys[:index], storeCopy.PublicKeys[index+1:]...)
+		store, err = km.CreateAccountsKeystore(ctx, storeCopy.PrivateKeys, storeCopy.PublicKeys)
 		if err != nil {
 			return nil, errors.Wrap(err, "could not rewrite accounts keystore")
 		}
+		//
 		statuses = append(statuses, &ethpbservice.DeletedKeystoreStatus{
 			Status: ethpbservice.DeletedKeystoreStatus_DELETED,
 		})
@@ -75,10 +92,7 @@ func (km *Keymanager) DeleteKeystores(
 		}
 	}
 
-	log.WithFields(logrus.Fields{
-		"publicKeys": deletedKeysStr,
-	}).Info("Successfully deleted validator key(s)")
-
+	// 3) Save the copy to disk
 	// Write the encoded keystore.
 	encoded, err := json.MarshalIndent(store, "", "\t")
 	if err != nil {
@@ -87,12 +101,18 @@ func (km *Keymanager) DeleteKeystores(
 	if err := km.wallet.WriteFileAtPath(ctx, AccountsPath, AccountsKeystoreFileName, encoded); err != nil {
 		return nil, errors.Wrap(err, "could not write keystore file for accounts")
 	}
-	err = km.initializeKeysCachesFromKeystore()
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to initialize key caches")
-	}
+	//
+	// 4) Reinitialize account store from disk
+
+	//// ) Reload keystore Cache
+	//err = km.initializeKeysCachesFromKeystore()
+	//if err != nil {
+	//	return nil, errors.Wrap(err, "failed to initialize key caches")
+	//}
+	////
 	log.WithFields(logrus.Fields{
 		"publicKeys": deletedKeysStr,
 	}).Info("Successfully deleted validator key(s)")
+	// 6) Return API response
 	return statuses, nil
 }
