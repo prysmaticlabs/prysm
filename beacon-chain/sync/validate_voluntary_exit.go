48c5b705e4502c0ac4299dc0be45617b3fbd5792
terence tsao <terence@prysmaticlabs.com>
2023-04-18 17:21:15 +0900

    Validate gossip exit with current slot

--- beacon-chain/sync/validate_voluntary_exit.go
+++ beacon-chain/sync/validate_voluntary_exit.go
@@ -62,7 +62,7 @@ func (s *Service) validateVoluntaryExit(ctx context.Context, pid peer.ID, msg *p
 	if err != nil {
 		return pubsub.ValidationIgnore, err
 	}
-	if err := blocks.VerifyExitAndSignature(val, headState.Slot(), headState.Fork(), exit, headState.GenesisValidatorsRoot()); err != nil {
+	if err := blocks.VerifyExitAndSignature(val, s.cfg.chain.CurrentSlot(), headState.Fork(), exit, headState.GenesisValidatorsRoot()); err != nil {
 		return pubsub.ValidationReject, err
 	}
 
