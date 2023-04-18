0ece2b49c9a6a6750add7c692be4bdcfe6f0b675
Preston Van Loon <pvanloon@offchainlabs.com>
2023-04-18 13:17:59 -0500

    Add more tracing data for builder code paths

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer_execution_payload.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer_execution_payload.go
@@ -25,6 +25,7 @@ import (
 	"github.com/prysmaticlabs/prysm/v4/runtime/version"
 	"github.com/prysmaticlabs/prysm/v4/time/slots"
 	"github.com/sirupsen/logrus"
+	"go.opencensus.io/trace"
 )
 
 var (
@@ -43,6 +44,9 @@ var (
 // This returns the execution payload of a given slot. The function has full awareness of pre and post merge.
 // The payload is computed given the respected time of merge.
 func (vs *Server) getExecutionPayload(ctx context.Context, slot primitives.Slot, vIdx primitives.ValidatorIndex, headRoot [32]byte, st state.BeaconState) (interfaces.ExecutionData, error) {
+	ctx, span := trace.StartSpan(ctx, "ProposerServer.getExecutionPayload")
+	defer span.End()
+
 	proposerID, payloadId, ok := vs.ProposerSlotIndexCache.GetProposerPayloadIDs(slot, headRoot)
 	feeRecipient := params.BeaconConfig().DefaultFeeRecipient
 	recipient, err := vs.BeaconDB.FeeRecipientByValidatorID(ctx, vIdx)
