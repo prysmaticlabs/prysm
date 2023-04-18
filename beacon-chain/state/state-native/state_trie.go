f45b16d5c4358f887be799b252bdd1409e6f40c4
rkapka <rkapka@wp.pl>
2023-04-18 14:07:41 +0200

    remove log

--- beacon-chain/state/state-native/state_trie.go
+++ beacon-chain/state/state-native/state_trie.go
@@ -17,7 +17,6 @@ import (
 	"github.com/prysmaticlabs/prysm/v4/encoding/ssz"
 	ethpb "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/v4/runtime/version"
-	"github.com/sirupsen/logrus"
 	"go.opencensus.io/trace"
 	"google.golang.org/protobuf/proto"
 )
@@ -595,9 +594,7 @@ func (b *BeaconState) HashTreeRoot(ctx context.Context) ([32]byte, error) {
 	if err := b.recomputeDirtyFields(ctx); err != nil {
 		return [32]byte{}, err
 	}
-	r := bytesutil.ToBytes32(b.merkleLayers[len(b.merkleLayers)-1][0])
-	logrus.Warnf("root: %v", r)
-	return r, nil
+	return bytesutil.ToBytes32(b.merkleLayers[len(b.merkleLayers)-1][0]), nil
 }
 
 // Initializes the Merkle layers for the beacon state if they are empty.
