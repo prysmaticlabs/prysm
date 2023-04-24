03636bf73fa872cca7eca642e2d5638c5acbeea4
james-prysm <90280386+james-prysm@users.noreply.github.com>
2023-04-24 13:51:59 -0500

    Update beacon-chain/cache/registration.go
    Co-authored-by: Preston Van Loon <preston@prysmaticlabs.com>

--- beacon-chain/cache/registration.go
+++ beacon-chain/cache/registration.go
@@ -49,7 +49,7 @@ func RegistrationTimeStampExpired(ts uint64) bool {
 	expiryDuration := time.Duration(params.BeaconConfig().SecondsPerSlot*uint64(params.BeaconConfig().SlotsPerEpoch)*3) * time.Second
 	// safely convert unint64 to int64
 	t := new(big.Int).SetUint64(ts).Int64()
-	return time.Unix(t, 0).Add(expiryDuration).Unix() < time.Now().Unix()
+	return time.Unix(t, 0).Add(expiryDuration).After(time.Now())
 }
 
 // UpdateIndexToRegisteredMap adds or updates values in the cache based on the argument.
