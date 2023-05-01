3c15b1ab96971f0af766930aace8ff2429ab766a
Kasey Kirkham <kasey@users.noreply.github.com>
2023-05-01 09:27:56 -0500

    remove accidental wip commit

--- beacon-chain/sync/initial-sync/semaphore.go
+++
@@ -1,7 +0,0 @@
-package initialsync
-
-// is a synchonization primitive for sync and initial-sync to coordinate control over the blockchain package.
-// Whichever service currently holds the Semaphore has exclusive writes to interact with blockchain.
-// If initial-sync has the Semaphore, sync should pause, and vice versa.
-type Status struct {
-}
