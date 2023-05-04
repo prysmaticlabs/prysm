53496fcc37ccdf3aee3eb3093bca837985c0304e
Radosław Kapka <rkapka@wp.pl>
2023-05-04 19:52:41 +0200

    Use v1alpha1 server in block production (#12336)
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer.go
@@ -37,7 +37,11 @@ import (
 // eth1DataNotification is a latch to stop flooding logs with the same warning.
 var eth1DataNotification bool
 
-const eth1dataTimeout = 2 * time.Second
+const (
+	// CouldNotDecodeBlock means that a signed beacon block couldn't be created from the block present in the request.
+	CouldNotDecodeBlock = "Could not decode block"
+	eth1dataTimeout     = 2 * time.Second
+)
 
 // GetBeaconBlock is called by a proposer during its assigned slot to request a block to sign
 // by passing in the slot and the signed randao reveal of the slot.
@@ -221,7 +225,7 @@ func (vs *Server) ProposeBeaconBlock(ctx context.Context, req *ethpb.GenericSign
 	defer span.End()
 	blk, err := blocks.NewSignedBeaconBlock(req.Block)
 	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "Could not decode block: %v", err)
+		return nil, status.Errorf(codes.InvalidArgument, "%s: %v", CouldNotDecodeBlock, err)
 	}
 	return vs.proposeGenericBeaconBlock(ctx, blk)
 }
