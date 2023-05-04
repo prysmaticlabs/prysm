69b0f1d02dc100343a5824cb8a785b7fca1b7ec5
Radosław Kapka <rkapka@wp.pl>
2023-05-04 19:52:41 +0200

    Use v1alpha1 server in block production (#12336)
    Co-authored-by: Raul Jordan <raul@prysmaticlabs.com>

--- beacon-chain/rpc/eth/beacon/server.go
+++ beacon-chain/rpc/eth/beacon/server.go
@@ -15,9 +15,9 @@ import (
 	"github.com/prysmaticlabs/prysm/v4/beacon-chain/operations/voluntaryexits"
 	"github.com/prysmaticlabs/prysm/v4/beacon-chain/p2p"
 	"github.com/prysmaticlabs/prysm/v4/beacon-chain/rpc/lookup"
-	v1alpha1validator "github.com/prysmaticlabs/prysm/v4/beacon-chain/rpc/prysm/v1alpha1/validator"
 	"github.com/prysmaticlabs/prysm/v4/beacon-chain/state/stategen"
 	"github.com/prysmaticlabs/prysm/v4/beacon-chain/sync"
+	eth "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 )
 
 // Server defines a server implementation of the gRPC Beacon Chain service,
@@ -37,8 +37,9 @@ type Server struct {
 	Stater                        lookup.Stater
 	Blocker                       lookup.Blocker
 	HeadFetcher                   blockchain.HeadFetcher
+	TimeFetcher                   blockchain.TimeFetcher
 	OptimisticModeFetcher         blockchain.OptimisticModeFetcher
-	V1Alpha1ValidatorServer       *v1alpha1validator.Server
+	V1Alpha1ValidatorServer       eth.BeaconNodeValidatorServer
 	SyncChecker                   sync.Checker
 	CanonicalHistory              *stategen.CanonicalHistory
 	ExecutionPayloadReconstructor execution.ExecutionPayloadReconstructor
