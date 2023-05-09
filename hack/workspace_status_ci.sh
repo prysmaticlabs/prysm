4f05a32c72c658a237fd8fd906124f8385e70d10
Preston Van Loon <pvanloon@offchainlabs.com>
2023-05-09 13:02:07 -0500

    fix branch

--- hack/workspace_status_ci.sh
+++ hack/workspace_status_ci.sh
@@ -6,8 +6,7 @@ echo "REPO_URL $repo_url"
 commit_sha=$(git rev-parse HEAD)
 echo "COMMIT_SHA $commit_sha"
 
-git_branch=$(git rev-parse --abbrev-ref HEAD)
-echo "GIT_BRANCH $git_branch"
+echo "GIT_BRANCH $BUILDKITE_BRANCH"
 
 git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
 echo "GIT_TREE_STATUS $git_tree_status"
