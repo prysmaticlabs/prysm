55b2c37887d94b5b995061bae19808dca3f99f10
james-prysm <james@prysmaticlabs.com>
2023-05-09 16:09:23 -0500

    attempting to resolve dependencies

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer_bellatrix_test.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer_bellatrix_test.go
@@ -24,7 +24,7 @@ import (
 	"github.com/prysmaticlabs/prysm/v4/crypto/bls"
 	"github.com/prysmaticlabs/prysm/v4/encoding/bytesutil"
 	"github.com/prysmaticlabs/prysm/v4/encoding/ssz"
-	"github.com/prysmaticlabs/prysm/v4/math"
+	m "github.com/prysmaticlabs/prysm/v4/math"
 	v1 "github.com/prysmaticlabs/prysm/v4/proto/engine/v1"
 	ethpb "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/v4/testing/require"
@@ -457,7 +457,7 @@ func TestServer_getPayloadHeader(t *testing.T) {
 					require.DeepEqual(t, want, h)
 				}
 				if tc.returnedHeaderCapella != nil {
-					want, err := blocks.WrappedExecutionPayloadHeaderCapella(tc.returnedHeaderCapella, math.WeiToGwei(big.NewInt(1000000)))
+					want, err := blocks.WrappedExecutionPayloadHeaderCapella(tc.returnedHeaderCapella, m.WeiToGwei(big.NewInt(1000000)))
 					require.NoError(t, err)
 					require.DeepEqual(t, want, h)
 				}
