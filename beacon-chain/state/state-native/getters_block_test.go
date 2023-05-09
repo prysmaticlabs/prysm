ef061d829b5de9344eb1dd0472f15386a8bb1f24
terencechain <terence@prysmaticlabs.com>
2023-05-09 14:38:13 -0700

    Add Deneb state (#12375)

--- beacon-chain/state/state-native/getters_block_test.go
+++ beacon-chain/state/state-native/getters_block_test.go
@@ -56,6 +56,18 @@ func TestBeaconState_LatestBlockHeader_Capella(t *testing.T) {
 	)
 }
 
+func TestBeaconState_LatestBlockHeader_Deneb(t *testing.T) {
+	testtmpl.VerifyBeaconStateLatestBlockHeader(
+		t,
+		func() (state.BeaconState, error) {
+			return InitializeFromProtoDeneb(&ethpb.BeaconStateDeneb{})
+		},
+		func(BH *ethpb.BeaconBlockHeader) (state.BeaconState, error) {
+			return InitializeFromProtoDeneb(&ethpb.BeaconStateDeneb{LatestBlockHeader: BH})
+		},
+	)
+}
+
 func TestBeaconState_BlockRoots_Phase0(t *testing.T) {
 	testtmpl.VerifyBeaconStateBlockRootsNative(
 		t,
@@ -104,6 +116,18 @@ func TestBeaconState_BlockRoots_Capella(t *testing.T) {
 	)
 }
 
+func TestBeaconState_BlockRoots_Deneb(t *testing.T) {
+	testtmpl.VerifyBeaconStateBlockRootsNative(
+		t,
+		func() (state.BeaconState, error) {
+			return InitializeFromProtoDeneb(&ethpb.BeaconStateDeneb{})
+		},
+		func(BR [][]byte) (state.BeaconState, error) {
+			return InitializeFromProtoDeneb(&ethpb.BeaconStateDeneb{BlockRoots: BR})
+		},
+	)
+}
+
 func TestBeaconState_BlockRootAtIndex_Phase0(t *testing.T) {
 	testtmpl.VerifyBeaconStateBlockRootAtIndexNative(
 		t,
@@ -148,6 +172,18 @@ func TestBeaconState_BlockRootAtIndex_Capella(t *testing.T) {
 		},
 		func(BR [][]byte) (state.BeaconState, error) {
 			return InitializeFromProtoCapella(&ethpb.BeaconStateCapella{BlockRoots: BR})
+		},
+	)
+}
+
+func TestBeaconState_BlockRootAtIndex_Deneb(t *testing.T) {
+	testtmpl.VerifyBeaconStateBlockRootAtIndexNative(
+		t,
+		func() (state.BeaconState, error) {
+			return InitializeFromProtoDeneb(&ethpb.BeaconStateDeneb{})
+		},
+		func(BR [][]byte) (state.BeaconState, error) {
+			return InitializeFromProtoDeneb(&ethpb.BeaconStateDeneb{BlockRoots: BR})
 		},
 	)
 }
