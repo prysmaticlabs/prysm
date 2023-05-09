9a6db10508d22b2c7f115bdee7317eeedd4f225e
Preston Van Loon <pvanloon@offchainlabs.com>
2023-05-09 18:13:52 -0500

    Update latest_version_tag.sh

--- hack/latest_version_tag.sh
+++ hack/latest_version_tag.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# Prints the latest BuildBuddy version tag, like "v2.12.8"
+# Prints the latest git version tag, like "v2.12.8"
 git tag -l 'v*' --sort=creatordate |
     perl -nle 'if (/^v\d+\.\d+\.\d+$/) { print $_ }' |
     tail -n1
