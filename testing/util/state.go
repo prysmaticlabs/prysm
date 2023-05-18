b4e68a2abfd1794fad90cf4610afdd953b31a642
Radosław Kapka <rkapka@wp.pl>
2023-05-18 16:44:38 +0200

    Deneb integration: Beacon API `getStateV2` (#12424)
    Co-authored-by: Nishant Das <nishdas93@gmail.com>
    Fix Migration Of State (#12423)

--- testing/util/state.go
+++ testing/util/state.go
@@ -71,6 +71,19 @@ func FillRootsNaturalOptCapella(state *ethpb.BeaconStateCapella) error {
 	return nil
 }
 
+// FillRootsNaturalOptDeneb is meant to be used as an option when calling NewBeaconStateDeneb.
+// It fills state and block roots with hex representations of natural numbers starting with 0.
+// Example: 16 becomes 0x00...0f.
+func FillRootsNaturalOptDeneb(state *ethpb.BeaconStateDeneb) error {
+	roots, err := PrepareRoots(int(params.BeaconConfig().SlotsPerHistoricalRoot))
+	if err != nil {
+		return err
+	}
+	state.StateRoots = roots
+	state.BlockRoots = roots
+	return nil
+}
+
 type NewBeaconStateOption func(state *ethpb.BeaconState) error
 
 // NewBeaconState creates a beacon state with minimum marshalable fields.
@@ -298,6 +311,75 @@ func NewBeaconStateCapella(options ...func(state *ethpb.BeaconStateCapella) erro
 	}
 
 	var st, err = state_native.InitializeFromProtoUnsafeCapella(seed)
+	if err != nil {
+		return nil, err
+	}
+
+	return st.Copy(), nil
+}
+
+// NewBeaconStateDeneb creates a beacon state with minimum marshalable fields.
+func NewBeaconStateDeneb(options ...func(state *ethpb.BeaconStateDeneb) error) (state.BeaconState, error) {
+	pubkeys := make([][]byte, 512)
+	for i := range pubkeys {
+		pubkeys[i] = make([]byte, 48)
+	}
+
+	seed := &ethpb.BeaconStateDeneb{
+		BlockRoots:                 filledByteSlice2D(uint64(params.BeaconConfig().SlotsPerHistoricalRoot), 32),
+		StateRoots:                 filledByteSlice2D(uint64(params.BeaconConfig().SlotsPerHistoricalRoot), 32),
+		Slashings:                  make([]uint64, params.BeaconConfig().EpochsPerSlashingsVector),
+		RandaoMixes:                filledByteSlice2D(uint64(params.BeaconConfig().EpochsPerHistoricalVector), 32),
+		Validators:                 make([]*ethpb.Validator, 0),
+		CurrentJustifiedCheckpoint: &ethpb.Checkpoint{Root: make([]byte, fieldparams.RootLength)},
+		Eth1Data: &ethpb.Eth1Data{
+			DepositRoot: make([]byte, fieldparams.RootLength),
+			BlockHash:   make([]byte, 32),
+		},
+		Fork: &ethpb.Fork{
+			PreviousVersion: make([]byte, 4),
+			CurrentVersion:  make([]byte, 4),
+		},
+		Eth1DataVotes:               make([]*ethpb.Eth1Data, 0),
+		HistoricalRoots:             make([][]byte, 0),
+		JustificationBits:           bitfield.Bitvector4{0x0},
+		FinalizedCheckpoint:         &ethpb.Checkpoint{Root: make([]byte, fieldparams.RootLength)},
+		LatestBlockHeader:           HydrateBeaconHeader(&ethpb.BeaconBlockHeader{}),
+		PreviousJustifiedCheckpoint: &ethpb.Checkpoint{Root: make([]byte, fieldparams.RootLength)},
+		PreviousEpochParticipation:  make([]byte, 0),
+		CurrentEpochParticipation:   make([]byte, 0),
+		CurrentSyncCommittee: &ethpb.SyncCommittee{
+			Pubkeys:         pubkeys,
+			AggregatePubkey: make([]byte, 48),
+		},
+		NextSyncCommittee: &ethpb.SyncCommittee{
+			Pubkeys:         pubkeys,
+			AggregatePubkey: make([]byte, 48),
+		},
+		LatestExecutionPayloadHeader: &enginev1.ExecutionPayloadHeaderDeneb{
+			ParentHash:       make([]byte, 32),
+			FeeRecipient:     make([]byte, 20),
+			StateRoot:        make([]byte, 32),
+			ReceiptsRoot:     make([]byte, 32),
+			LogsBloom:        make([]byte, 256),
+			PrevRandao:       make([]byte, 32),
+			ExtraData:        make([]byte, 0),
+			BaseFeePerGas:    make([]byte, 32),
+			BlockHash:        make([]byte, 32),
+			TransactionsRoot: make([]byte, 32),
+			WithdrawalsRoot:  make([]byte, 32),
+			ExcessDataGas:    make([]byte, 32),
+		},
+	}
+
+	for _, opt := range options {
+		err := opt(seed)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var st, err = state_native.InitializeFromProtoUnsafeDeneb(seed)
 	if err != nil {
 		return nil, err
 	}
