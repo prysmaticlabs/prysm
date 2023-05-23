2e41bc756a16873802872c7480e483e6890c54b5
terencechain <terence@prysmaticlabs.com>
2023-05-23 10:25:35 -0700

    P2P: broadcast blob  (#12419)

--- beacon-chain/p2p/monitoring.go
+++ beacon-chain/p2p/monitoring.go
@@ -60,9 +60,17 @@ var (
 			"the subnet. The beacon node increments this counter when the broadcast is blocked " +
 			"until a subnet peer can be found.",
 	})
+	blobSidecarCommitteeBroadcasts = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "p2p_blob_sidecar_committee_broadcasts",
+		Help: "The number of blob sidecar committee messages that were broadcast with no peer on.",
+	})
 	syncCommitteeBroadcastAttempts = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "p2p_sync_committee_subnet_attempted_broadcasts",
 		Help: "The number of sync committee that were attempted to be broadcast.",
+	})
+	blobSidecarCommitteeBroadcastAttempts = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "p2p_blob_sidecar_committee_attempted_broadcasts",
+		Help: "The number of blob sidecar committee messages that were attempted to be broadcast.",
 	})
 
 	// Gossip Tracer Metrics
