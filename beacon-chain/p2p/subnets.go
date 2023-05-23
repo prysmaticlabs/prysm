011b958479bcffb22fed98ce73b3005eb9e1e97c
terencechain <terence@prysmaticlabs.com>
2023-05-23 10:25:35 -0700

    P2P: broadcast blob  (#12419)

--- beacon-chain/p2p/subnets.go
+++ beacon-chain/p2p/subnets.go
@@ -31,6 +31,13 @@ var syncCommsSubnetEnrKey = params.BeaconNetworkConfig().SyncCommsSubnetKey
 // chosen as more than 64(attestation subnet count).
 const syncLockerVal = 100
 
+// The value used with the blob sidecar subnet, in order
+// to create an appropriate key to retrieve
+// the relevant lock. This is used to differentiate
+// blob subnets from others. This is deliberately
+// chosen more than sync and attestation subnet combined.
+const blobSubnetLockerVal = 110
+
 // FindPeersWithSubnet performs a network search for peers
 // subscribed to a particular subnet. Then we try to connect
 // with those peers. This method will block until the required amount of
