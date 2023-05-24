9b94e5d24457c5ad13b9369799aa077f7bfd9c2c
james-prysm <90280386+james-prysm@users.noreply.github.com>
2023-05-24 11:30:26 -0500

    validator signing deneb (#12449)
    * validator signing feature migrated from eip4844 branch, added unit tests
    
    * Update proto/prysm/v1alpha1/beacon_block.proto
    
    ---------
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- testing/util/merge.go
+++ testing/util/merge.go
@@ -1,6 +1,10 @@
 package util
 
 import (
+	"fmt"
+
+	fieldparams "github.com/prysmaticlabs/prysm/v4/config/fieldparams"
+	"github.com/prysmaticlabs/prysm/v4/config/params"
 	v2 "github.com/prysmaticlabs/prysm/v4/proto/eth/v2"
 	ethpb "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 )
@@ -43,4 +47,31 @@ func NewBlindedBeaconBlockDeneb() *ethpb.SignedBlindedBeaconBlockDeneb {
 // NewBlindedBeaconBlockCapellaV2 creates a blinded beacon block with minimum marshalable fields.
 func NewBlindedBeaconBlockCapellaV2() *v2.SignedBlindedBeaconBlockCapella {
 	return HydrateV2SignedBlindedBeaconBlockCapella(&v2.SignedBlindedBeaconBlockCapella{})
+}
+
+// NewBeaconBlockAndBlobsDeneb creates a beacon block content including blobs with minimum marshalable fields.
+func NewBeaconBlockAndBlobsDeneb(numOfBlobs uint64) (*ethpb.SignedBeaconBlockAndBlobsDeneb, error) {
+	if numOfBlobs > params.BeaconConfig().MaxBlobsPerBlock {
+		return nil, fmt.Errorf("declared too many blobs: %v", numOfBlobs)
+	}
+	blobs := make([]*ethpb.SignedBlobSidecar, numOfBlobs)
+	for i := range blobs {
+		blobs[i] = &ethpb.SignedBlobSidecar{
+			Message: &ethpb.BlobSidecar{
+				BlockRoot:       make([]byte, fieldparams.RootLength),
+				Index:           0,
+				Slot:            0,
+				BlockParentRoot: make([]byte, fieldparams.RootLength),
+				ProposerIndex:   0,
+				Blob:            make([]byte, fieldparams.BlobLength),
+				KzgCommitment:   make([]byte, 48),
+				KzgProof:        make([]byte, 48),
+			},
+			Signature: make([]byte, fieldparams.BLSSignatureLength),
+		}
+	}
+	return &ethpb.SignedBeaconBlockAndBlobsDeneb{
+		Block: HydrateSignedBeaconBlockDeneb(&ethpb.SignedBeaconBlockDeneb{}),
+		Blobs: blobs,
+	}, nil
 }
