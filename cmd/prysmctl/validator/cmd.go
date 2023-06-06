6672d1499a68ba844ce554c3812e7288478676e9
james-prysm <90280386+james-prysm@users.noreply.github.com>
2023-06-06 12:03:30 -0500

    prysmctl: output proposer settings (#12181)
    * wip proposer settings
    
    * WIP validator client APIs
    
    * adding proposer settings output
    
    * adding unit tests
    
    * fixing linting
    
    * fixing deepsource issues
    
    * fixing e2e
    
    * fixing deep source issue
    
    * updating naming to not stutter
    
    * updating bazel
    
    * fixing linting error
    
    * reverting comment
    
    * adding builder settings
    
    * gaz
    
    * Update validator/client/validator.go
    
    Co-authored-by: Sammy Rosso <15244892+saolyn@users.noreply.github.com>
    
    * adding comments
    
    * adding some tests
    
    * gaz
    
    * Update cmd/prysmctl/validator/proposer_settings.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/proposer_settings.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/proposer_settings.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/proposer_settings.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/options.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/options.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/errors.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/options.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/options.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/validator/client.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/cmd.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/validator/client.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/validator/client.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update cmd/prysmctl/validator/proposer_settings.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update api/client/errors.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * fixing feedback
    
    * fixing unit test
    
    * addressign comments
    
    ---------
    
    Co-authored-by: Sammy Rosso <15244892+saolyn@users.noreply.github.com>
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- cmd/prysmctl/validator/cmd.go
+++ cmd/prysmctl/validator/cmd.go
@@ -20,21 +20,55 @@ var (
 		Usage: "host:port for beacon node to query",
 		Value: "127.0.0.1:3500",
 	}
+
 	PathFlag = &cli.StringFlag{
 		Name:    "path",
 		Aliases: []string{"p"},
 		Usage:   "path to the signed withdrawal messages JSON",
 	}
+
 	ConfirmFlag = &cli.BoolFlag{
 		Name:    "confirm",
 		Aliases: []string{"c"},
 		Usage: "WARNING: User confirms and accepts responsibility of all input data provided and actions for setting their withdrawal address for their validator key. " +
 			"This action is not reversible and withdrawal addresses can not be changed once set.",
 	}
+
 	VerifyOnlyFlag = &cli.BoolFlag{
 		Name:    "verify-only",
 		Aliases: []string{"vo"},
 		Usage:   "overrides withdrawal command to only verify whether requests are in the pool and does not submit withdrawal requests",
+	}
+
+	ValidatorHostFlag = &cli.StringFlag{
+		Name:    "validator-host",
+		Aliases: []string{"vch"},
+		Usage:   "host:port for validator client.",
+		Value:   "http://127.0.0.1:7500",
+	}
+
+	ProposerSettingsOutputFlag = &cli.StringFlag{
+		Name:    "output-proposer-settings-path",
+		Aliases: []string{"settings-path"},
+		Usage:   "path to outputting a proposer settings file ( i.e. ./path/to/proposer-settings.json), file does not include builder settings and will need to be added for advanced users using those features",
+	}
+
+	WithBuilderFlag = &cli.BoolFlag{
+		Name:    "with-builder",
+		Aliases: []string{"wb"},
+		Usage:   "adds default builder options to proposer settings output, used for enabling mev-boost and relays",
+	}
+
+	DefaultFeeRecipientFlag = &cli.StringFlag{
+		Name:    "default-fee-recipient",
+		Aliases: []string{"dfr"},
+		Usage:   "default fee recipient used for proposer-settings, only used with --output-proposer-settings-path",
+	}
+
+	TokenFlag = &cli.StringFlag{
+		Name:    "token",
+		Aliases: []string{"t"},
+		Usage:   "keymanager API bearer token, note: currently required but may be removed in the future, this is the same token as the web ui token.",
 	}
 )
 
@@ -85,6 +119,27 @@ var Commands = []*cli.Command{
 						if err := setWithdrawalAddresses(cliCtx); err != nil {
 							log.WithError(err).Fatal("Could not set withdrawal addresses")
 						}
+					}
+					return nil
+				},
+			},
+			{
+				Name:    "proposer-settings",
+				Aliases: []string{"w"},
+				Usage:   "Display or recreate currently used proposer settings.",
+				Flags: []cli.Flag{
+					cmd.ConfigFileFlag,
+					DefaultFeeRecipientFlag,
+					TokenFlag,
+					ValidatorHostFlag,
+					ProposerSettingsOutputFlag,
+				},
+				Before: func(cliCtx *cli.Context) error {
+					return cmd.LoadFlagsFromConfig(cliCtx, cliCtx.Command.Flags)
+				},
+				Action: func(cliCtx *cli.Context) error {
+					if err := getProposerSettings(cliCtx, os.Stdin); err != nil {
+						log.WithError(err).Fatal("Could not get proposer settings")
 					}
 					return nil
 				},
