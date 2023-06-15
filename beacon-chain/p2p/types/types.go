0895b2977f98b7e563088fbfe874f9e023138aed
kasey <489222+kasey@users.noreply.github.com>
2023-06-15 14:38:08 -0500

    BlobSidecarsByRoot (#12420)
    * BlobSidecarsByRoot RPC handler
    
    * BlobSidecarsByRange rpc handler (#12499)
    
    Co-authored-by: Kasey Kirkham <kasey@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kasey Kirkham <kasey@users.noreply.github.com>

--- beacon-chain/p2p/types/types.go
+++ beacon-chain/p2p/types/types.go
@@ -4,9 +4,13 @@
 package types
 
 import (
+	"bytes"
+	"sort"
+
 	"github.com/pkg/errors"
 	ssz "github.com/prysmaticlabs/fastssz"
 	"github.com/prysmaticlabs/prysm/v4/config/params"
+	eth "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 )
 
 const rootLength = 32
@@ -64,7 +68,7 @@ func (r *BeaconBlockByRootsReq) UnmarshalSSZ(buf []byte) error {
 	bufLen := len(buf)
 	maxLength := int(params.BeaconNetworkConfig().MaxRequestBlocks * rootLength)
 	if bufLen > maxLength {
-		return errors.Errorf("expected buffer with length of upto %d but received length %d", maxLength, bufLen)
+		return errors.Errorf("expected buffer with length of up to %d but received length %d", maxLength, bufLen)
 	}
 	if bufLen%rootLength != 0 {
 		return ssz.ErrIncorrectByteSize
@@ -119,4 +123,91 @@ func (m *ErrorMessage) UnmarshalSSZ(buf []byte) error {
 	copy(errMsg, buf)
 	*m = errMsg
 	return nil
+}
+
+// BlobSidecarsByRootReq is used to specify a list of blob targets (root+index) in a BlobSidecarsByRoot RPC request.
+type BlobSidecarsByRootReq []*eth.BlobIdentifier
+
+// BlobIdentifier is a fixed size value, so we can compute its fixed size at start time (see init below)
+var blobIdSize int
+
+// SizeSSZ returns the size of the serialized representation.
+func (b *BlobSidecarsByRootReq) SizeSSZ() int {
+	return len(*b) * blobIdSize
+}
+
+// MarshalSSZTo appends the serialized BlobSidecarsByRootReq value to the provided byte slice.
+func (b *BlobSidecarsByRootReq) MarshalSSZTo(dst []byte) ([]byte, error) {
+	// A List without an enclosing container is marshaled exactly like a vector, no length offset required.
+	marshalledObj, err := b.MarshalSSZ()
+	if err != nil {
+		return nil, err
+	}
+	return append(dst, marshalledObj...), nil
+}
+
+// MarshalSSZ serializes the BlobSidecarsByRootReq value to a byte slice.
+func (b *BlobSidecarsByRootReq) MarshalSSZ() ([]byte, error) {
+	buf := make([]byte, len(*b)*blobIdSize)
+	for i, id := range *b {
+		by, err := id.MarshalSSZ()
+		if err != nil {
+			return nil, err
+		}
+		copy(buf[i*blobIdSize:(i+1)*blobIdSize], by)
+	}
+	return buf, nil
+}
+
+// UnmarshalSSZ unmarshals the provided bytes buffer into the
+// BlobSidecarsByRootReq value.
+func (b *BlobSidecarsByRootReq) UnmarshalSSZ(buf []byte) error {
+	bufLen := len(buf)
+	maxLength := int(params.BeaconNetworkConfig().MaxRequestBlobSidecars) * blobIdSize
+	if bufLen > maxLength {
+		return errors.Errorf("expected buffer with length of up to %d but received length %d", maxLength, bufLen)
+	}
+	if bufLen%blobIdSize != 0 {
+		return errors.Wrapf(ssz.ErrIncorrectByteSize, "size=%d", bufLen)
+	}
+	count := bufLen / blobIdSize
+	*b = make([]*eth.BlobIdentifier, count)
+	for i := 0; i < count; i++ {
+		id := &eth.BlobIdentifier{}
+		err := id.UnmarshalSSZ(buf[i*blobIdSize : (i+1)*blobIdSize])
+		if err != nil {
+			return err
+		}
+		(*b)[i] = id
+	}
+	return nil
+}
+
+var _ sort.Interface = BlobSidecarsByRootReq{}
+
+// Less reports whether the element with index i must sort before the element with index j.
+// BlobIdentifier will be sorted in lexicographic order by root, with Blob Index as tiebreaker for a given root.
+func (s BlobSidecarsByRootReq) Less(i, j int) bool {
+	rootCmp := bytes.Compare(s[i].BlockRoot, s[j].BlockRoot)
+	if rootCmp != 0 {
+		// They aren't equal; return true if i < j, false if i > j.
+		return rootCmp < 0
+	}
+	// They are equal; blob index is the tie breaker.
+	return s[i].Index < s[j].Index
+}
+
+// Swap swaps the elements with indexes i and j.
+func (s BlobSidecarsByRootReq) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// Len is the number of elements in the collection.
+func (s BlobSidecarsByRootReq) Len() int {
+	return len(s)
+}
+
+func init() {
+	sizer := &eth.BlobIdentifier{}
+	blobIdSize = sizer.SizeSSZ()
 }
