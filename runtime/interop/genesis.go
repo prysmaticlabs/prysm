bbe863b8a625e3a8af131cc2fcbc5ceaeddbb5b4
kasey <489222+kasey@users.noreply.github.com>
2023-06-15 15:35:46 -0500

    Integrate generate genesis (#12523)
    Co-authored-by: Kasey Kirkham <kasey@users.noreply.github.com>

--- runtime/interop/genesis.go
+++ runtime/interop/genesis.go
@@ -92,6 +92,21 @@ func GethShanghaiTime(genesisTime uint64, cfg *clparams.BeaconChainConfig) *uint
 	return shanghaiTime
 }
 
+// GethShanghaiTime calculates the absolute time of the shanghai (aka capella) fork block
+// by adding the relative time of the capella the fork epoch to the given genesis timestamp.
+func GethCancunTime(genesisTime uint64, cfg *clparams.BeaconChainConfig) *uint64 {
+	var cancunTime *uint64
+	if cfg.DenebForkEpoch != math.MaxUint64 {
+		startSlot, err := slots.EpochStart(cfg.DenebForkEpoch)
+		if err == nil {
+			startTime := slots.StartTime(genesisTime, startSlot)
+			newTime := uint64(startTime.Unix())
+			cancunTime = &newTime
+		}
+	}
+	return cancunTime
+}
+
 // GethTestnetGenesis creates a genesis.json for eth1 clients with a set of defaults suitable for ephemeral testnets,
 // like in an e2e test. The parameters are minimal but the full value is returned unmarshaled so that it can be
 // customized as desired.
