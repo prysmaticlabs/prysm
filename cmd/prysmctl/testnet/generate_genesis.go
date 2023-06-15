2121d1f4f4ca00d6ce7678d8cccd0a29f3ed7520
kasey <489222+kasey@users.noreply.github.com>
2023-06-15 15:35:46 -0500

    Integrate generate genesis (#12523)
    Co-authored-by: Kasey Kirkham <kasey@users.noreply.github.com>

--- cmd/prysmctl/testnet/generate_genesis.go
+++ cmd/prysmctl/testnet/generate_genesis.go
@@ -263,12 +263,16 @@ func generateGenesis(ctx context.Context) (state.BeaconState, error) {
 		// set timestamps for genesis and shanghai fork
 		gen.Timestamp = f.GenesisTime
 		gen.Config.ShanghaiTime = interop.GethShanghaiTime(f.GenesisTime, params.BeaconConfig())
+		//gen.Config.CancunTime = interop.GethCancunTime(f.GenesisTime, params.BeaconConfig())
+		gen.Config.CancunTime = interop.GethCancunTime(f.GenesisTime, params.BeaconConfig())
 		log.
 			WithField("shanghai", gen.Config.ShanghaiTime).
+			WithField("cancun", gen.Config.CancunTime).
 			Info("setting fork geth times")
 		if v > version.Altair {
 			// set ttd to zero so EL goes post-merge immediately
 			gen.Config.TerminalTotalDifficulty = big.NewInt(0)
+			gen.Config.TerminalTotalDifficultyPassed = true
 		}
 	} else {
 		gen = interop.GethTestnetGenesis(f.GenesisTime, params.BeaconConfig())
