d742ec1d9fd4f6746957b949523394fee8c1cdb1
terence tsao <terence@prysmaticlabs.com>
2023-06-20 10:00:56 -0700

    feat: aggregate atts using fixed pool of go routines

--- beacon-chain/operations/attestations/kv/aggregated_test.go
+++ beacon-chain/operations/attestations/kv/aggregated_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pkg/errors"
 	fssz "github.com/prysmaticlabs/fastssz"
 	"github.com/prysmaticlabs/go-bitfield"
+	"github.com/prysmaticlabs/prysm/v4/config/features"
 	"github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v4/crypto/bls"
 	ethpb "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
@@ -18,6 +19,11 @@ import (
 )
 
 func TestKV_Aggregated_AggregateUnaggregatedAttestations(t *testing.T) {
+	resetFn := features.InitWithReset(&features.Flags{
+		AggregateParallel: true,
+	})
+	defer resetFn()
+
 	cache := NewAttCaches()
 	priv, err := bls.RandKey()
 	require.NoError(t, err)
@@ -40,6 +46,11 @@ func TestKV_Aggregated_AggregateUnaggregatedAttestations(t *testing.T) {
 }
 
 func TestKV_Aggregated_AggregateUnaggregatedAttestationsBySlotIndex(t *testing.T) {
+	resetFn := features.InitWithReset(&features.Flags{
+		AggregateParallel: true,
+	})
+	defer resetFn()
+
 	cache := NewAttCaches()
 	genData := func(slot primitives.Slot, committeeIndex primitives.CommitteeIndex) *ethpb.AttestationData {
 		return util.HydrateAttestationData(&ethpb.AttestationData{
