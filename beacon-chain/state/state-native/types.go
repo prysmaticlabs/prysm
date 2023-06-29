3020ef43f64e013795417fe293473fb955a13fce
rkapka <rkapka@wp.pl>
2023-06-29 16:44:39 +0200

    initialize field map for tests

--- beacon-chain/state/state-native/types.go
+++ beacon-chain/state/state-native/types.go
@@ -11,6 +11,22 @@ import (
 // Ensure type BeaconState below implements BeaconState interface.
 var _ state.BeaconState = (*BeaconState)(nil)
 
+// initialization for tests
+func init() {
+	FieldMap = make(map[types.FieldIndex]types.FieldInfo)
+	// Initialize the fixed sized arrays.
+	FieldMap[types.BlockRoots] = types.FieldInfo{ArrayType: types.BasicArray, ValueType: types.SingleValue}
+	FieldMap[types.StateRoots] = types.FieldInfo{ArrayType: types.BasicArray, ValueType: types.SingleValue}
+	FieldMap[types.RandaoMixes] = types.FieldInfo{ArrayType: types.BasicArray, ValueType: types.SingleValue}
+
+	// Initialize the composite arrays.
+	FieldMap[types.Eth1DataVotes] = types.FieldInfo{ArrayType: types.CompositeArray, ValueType: types.SingleValue}
+	FieldMap[types.Validators] = types.FieldInfo{ArrayType: types.CompositeArray, ValueType: types.SingleValue}
+	FieldMap[types.PreviousEpochAttestations] = types.FieldInfo{ArrayType: types.CompositeArray, ValueType: types.SingleValue}
+	FieldMap[types.CurrentEpochAttestations] = types.FieldInfo{ArrayType: types.CompositeArray, ValueType: types.SingleValue}
+	FieldMap[types.Balances] = types.FieldInfo{ArrayType: types.CompressedArray, ValueType: types.SingleValue}
+}
+
 // FieldMap keeps track of each field
 // to its corresponding data type.
 var FieldMap map[types.FieldIndex]types.FieldInfo
