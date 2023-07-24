0dff954d452e57e879c8500a6ca9b9db9e3f3a3b
rkapka <rkapka@wp.pl>
2023-07-24 20:57:56 +0200

    ignore linter

--- beacon-chain/rpc/testutil/mock_blocker.go
+++ beacon-chain/rpc/testutil/mock_blocker.go
@@ -27,6 +27,7 @@ func (m *MockBlocker) Block(_ context.Context, b []byte) (interfaces.ReadOnlySig
 	}
 	slotNumber, parseErr := strconv.ParseUint(string(b), 10, 64)
 	if parseErr != nil {
+		//nolint:nilerr
 		return m.RootBlockMap[bytesutil.ToBytes32(b)], nil
 	}
 	return m.SlotBlockMap[primitives.Slot(slotNumber)], nil
