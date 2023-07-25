eece94ec5c4482efa8edafe5cc35e092a4fe8c37
rkapka <rkapka@wp.pl>
2023-07-25 17:26:40 +0200

    add comment to invalid test case

--- beacon-chain/rpc/eth/validator/handlers_test.go
+++ beacon-chain/rpc/eth/validator/handlers_test.go
@@ -391,6 +391,7 @@ const (
     "signature": "0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505"
   }
 ]`
+	// aggregator_index is invalid
 	invalidContribution = `[
   {
     "message": {
