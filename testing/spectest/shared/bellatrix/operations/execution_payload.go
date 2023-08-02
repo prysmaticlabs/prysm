371d59bad847cf8bda18702cd726d5c0df0b222e
terencechain <terence@prysmaticlabs.com>
2023-08-01 17:20:02 -0700

    Update testing/spectest/shared/bellatrix/operations/execution_payload.go

--- testing/spectest/shared/bellatrix/operations/execution_payload.go
+++ testing/spectest/shared/bellatrix/operations/execution_payload.go
@@ -24,7 +24,7 @@ func RunExecutionPayloadTest(t *testing.T, config string) {
 	require.NoError(t, utils.SetConfig(t, config))
 	testFolders, testsFolderPath := utils.TestFolders(t, config, "bellatrix", "operations/execution_payload/pyspec_tests")
 	if len(testFolders) == 0 {
-		t.Fatalf("No test folders found for %s/%s/%s", config, "bellatrix", "operations/block_header/pyspec_tests")
+		t.Fatalf("No test folders found for %s/%s/%s", config, "bellatrix", "operations/execution_payload/pyspec_tests")
 	}
 	for _, folder := range testFolders {
 		t.Run(folder.Name(), func(t *testing.T) {
