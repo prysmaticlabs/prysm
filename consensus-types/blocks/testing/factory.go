fe1b4c220bf570ac239cac351ff9ca69866426fb
terencechain <terence@prysmaticlabs.com>
2023-08-14 08:27:21 -0700

    feat: add validator blob signing (#12730)

--- consensus-types/blocks/testing/factory.go
+++ consensus-types/blocks/testing/factory.go
@@ -26,6 +26,10 @@ func NewSignedBeaconBlockFromGeneric(gb *eth.GenericSignedBeaconBlock) (interfac
 		return blocks.NewSignedBeaconBlock(bb.Capella)
 	case *eth.GenericSignedBeaconBlock_BlindedCapella:
 		return blocks.NewSignedBeaconBlock(bb.BlindedCapella)
+	case *eth.GenericSignedBeaconBlock_Deneb:
+		return blocks.NewSignedBeaconBlock(bb.Deneb.Block)
+	case *eth.GenericSignedBeaconBlock_BlindedDeneb:
+		return blocks.NewSignedBeaconBlock(bb.BlindedDeneb.Block)
 	// Generic Signed Beacon Block Deneb can't be used here as it is not a block, but block content with blobs
 	default:
 		return nil, errors.Wrapf(blocks.ErrUnsupportedSignedBeaconBlock, "unable to create block from type %T", gb)
