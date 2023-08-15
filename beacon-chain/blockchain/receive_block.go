851d8d969f8df44ad61c0360d923c4f08715a457
Potuz <potuz@prysmaticlabs.com>
2023-08-15 09:27:34 -0300

    more descriptive errors

--- beacon-chain/blockchain/receive_block.go
+++ beacon-chain/blockchain/receive_block.go
@@ -185,9 +185,11 @@ func (s *Service) ReceiveBlockBatch(ctx context.Context, blocks []interfaces.Rea
 		return err
 	}
 
-	optimistic, err := s.cfg.ForkChoiceStore.IsOptimistic(blkRoots[len(blkRoots)-1])
+	lastBR := blkRoots[len(blkRoots)-1]
+	optimistic, err := s.cfg.ForkChoiceStore.IsOptimistic(lastBR)
 	if err != nil {
-		log.WithError(err).Error("Could not check if block is optimistic")
+		lastSlot := blocks[len(blocks)-1].Block().Slot()
+		log.WithError(err).Errorf("Could not check if block is optimistic, Root: %#x, Slot: %d", lastBR, lastSlot)
 		optimistic = true
 	}
 	for i, b := range blocks {
