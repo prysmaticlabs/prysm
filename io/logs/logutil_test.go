c52cd9e5e7c82b8f4ef89af422e5700359d7e809
Bharath Vedartham <vedabharath12345@gmail.com>
2023-08-22 10:38:54 +0530

    add regression tests

--- io/logs/logutil_test.go
+++ io/logs/logutil_test.go
@@ -1,8 +1,11 @@
 package logs
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/v4/testing/require"
 )
 
@@ -23,4 +26,59 @@ func TestMaskCredentialsLogging(t *testing.T) {
 	for _, test := range urltests {
 		require.Equal(t, MaskCredentialsLogging(test.url), test.maskedUrl)
 	}
+}
+
+func TestConfigurePersistantLogging(t *testing.T) {
+	// 1. Test creation of file in an existing parent directory
+	logFileName := "test.log"
+	existingDirectory := "existing-testing-dir"
+	err := os.Mkdir(existingDirectory, 0700)
+	if err != nil {
+		return
+	}
+
+	err = ConfigurePersistentLogging(fmt.Sprintf("%s/%s/%s", t.TempDir(), existingDirectory, logFileName))
+	require.NoError(t, err)
+
+	err = os.RemoveAll(existingDirectory)
+	if err != nil {
+		return
+	}
+
+	// 2. Test creation of file along with parent directory
+	nonExistingDirectory := "non-existing-testing-dir"
+
+	err = ConfigurePersistentLogging(fmt.Sprintf("%s/%s/%s", t.TempDir(), nonExistingDirectory, logFileName))
+	require.NoError(t, err)
+
+	err = os.RemoveAll(nonExistingDirectory)
+	if err != nil {
+		return
+	}
+
+	// 3. Test creation of file in an existing parent directory with a non-existing sub-directory
+	existingDirectory = "existing-testing-dir"
+	nonExistingSubDirectory := "non-existing-sub-dir"
+	err = os.Mkdir(existingDirectory, 0700)
+	if err != nil {
+		return
+	}
+
+	err = ConfigurePersistentLogging(fmt.Sprintf("%s/%s/%s/%s", t.TempDir(), existingDirectory, nonExistingSubDirectory, logFileName))
+	require.NoError(t, err)
+
+	err = os.RemoveAll(existingDirectory)
+	if err != nil {
+		return
+	}
+
+	// 4. Create log file in a directory without 700 permissions
+	existingDirectory = "existing-testing-dir"
+	err = os.Mkdir(existingDirectory, 0750)
+	if err != nil {
+		return
+	}
+
+	err = ConfigurePersistentLogging(fmt.Sprintf("%s/%s/%s", t.TempDir(), existingDirectory, logFileName))
+	require.ErrorIs(t, err, errors.New("dir already exists without proper 0700 permissions"))
 }
