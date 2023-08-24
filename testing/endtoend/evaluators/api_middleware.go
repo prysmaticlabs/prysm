c72ff039669ee25e697c0350a6d3e5fed6e95e29
rkapka <rkapka@wp.pl>
2023-08-24 16:27:45 +0200

    remove apimiddleware from e2e

--- testing/endtoend/evaluators/api_middleware.go
+++ testing/endtoend/evaluators/api_middleware.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/pkg/errors"
-	"github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v4/proto/eth/service"
 	ethpbv1 "github.com/prysmaticlabs/prysm/v4/proto/eth/v1"
 	ethpbv2 "github.com/prysmaticlabs/prysm/v4/proto/eth/v2"
@@ -58,7 +57,6 @@ func apiMiddlewareVerify(_ *e2etypes.EvaluationContext, conns ...*grpc.ClientCon
 			conn,
 			withCompareValidatorsEth,
 			withCompareSyncCommittee,
-			withCompareAttesterDuties,
 		); err != nil {
 			return err
 		}
@@ -185,53 +183,6 @@ func withCompareSyncCommittee(beaconNodeIdx int, conn *grpc.ClientConn) error {
 			"API Middleware number of validator aggregates %d does not match gRPC %d",
 			len(respJSON.Data.ValidatorAggregates),
 			len(resp.Data.ValidatorAggregates),
-		)
-	}
-	return nil
-}
-
-func withCompareAttesterDuties(beaconNodeIdx int, conn *grpc.ClientConn) error {
-	type attesterDutyJson struct {
-		Pubkey                  string `json:"pubkey" hex:"true"`
-		ValidatorIndex          string `json:"validator_index"`
-		CommitteeIndex          string `json:"committee_index"`
-		CommitteeLength         string `json:"committee_length"`
-		CommitteesAtSlot        string `json:"committees_at_slot"`
-		ValidatorCommitteeIndex string `json:"validator_committee_index"`
-		Slot                    string `json:"slot"`
-	}
-	type attesterDutiesResponseJson struct {
-		DependentRoot string              `json:"dependent_root" hex:"true"`
-		Data          []*attesterDutyJson `json:"data"`
-	}
-	ctx := context.Background()
-	validatorClient := service.NewBeaconValidatorClient(conn)
-	resp, err := validatorClient.GetAttesterDuties(ctx, &ethpbv1.AttesterDutiesRequest{
-		Epoch: helpers.AltairE2EForkEpoch,
-		Index: []primitives.ValidatorIndex{0},
-	})
-	if err != nil {
-		return err
-	}
-	// We post a top-level array, not an object, as per the spec.
-	reqJSON := []string{"0"}
-	respJSON := &attesterDutiesResponseJson{}
-	if err := doMiddlewareJSONPostRequestV1(
-		"/validator/duties/attester/"+strconv.Itoa(helpers.AltairE2EForkEpoch),
-		beaconNodeIdx,
-		reqJSON,
-		respJSON,
-	); err != nil {
-		return err
-	}
-	if respJSON.DependentRoot != hexutil.Encode(resp.DependentRoot) {
-		return buildFieldError("DependentRoot", string(resp.DependentRoot), respJSON.DependentRoot)
-	}
-	if len(respJSON.Data) != len(resp.Data) {
-		return fmt.Errorf(
-			"API Middleware number of duties %d does not match gRPC %d",
-			len(respJSON.Data),
-			len(resp.Data),
 		)
 	}
 	return nil
