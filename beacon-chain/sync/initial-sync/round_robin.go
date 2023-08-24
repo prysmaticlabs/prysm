760062fd396f65278eed485e739ecae68bbd9358
Kasey Kirkham <kasey@users.noreply.github.com>
2023-08-24 12:38:15 -0500

    check that there are blobs before trying to save

--- beacon-chain/sync/initial-sync/round_robin.go
+++ beacon-chain/sync/initial-sync/round_robin.go
@@ -162,8 +162,10 @@ func (s *Service) processFetchedDataRegSync(
 	invalidBlocks := 0
 	blksWithoutParentCount := 0
 	for _, b := range data.bwb {
-		if err := s.cfg.DB.SaveBlobSidecar(ctx, b.Blobs); err != nil {
-			log.WithError(err).Warn("Failed to save blob sidecar")
+		if len(b.Blobs) > 0 {
+			if err := s.cfg.DB.SaveBlobSidecar(ctx, b.Blobs); err != nil {
+				log.WithError(err).Warn("Failed to save blob sidecar")
+			}
 		}
 
 		if err := s.processBlock(ctx, genesis, b, blockReceiver); err != nil {
