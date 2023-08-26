f6476a9a62bfece8768dd4d594b8f5c91bbf7bd4
terencechain <terence@prysmaticlabs.com>
2023-08-26 09:33:11 -0700

    chore: improve att error (#12813)

--- beacon-chain/core/helpers/attestation.go
+++ beacon-chain/core/helpers/attestation.go
@@ -191,7 +191,13 @@ func ValidateAttestationTime(attSlot primitives.Slot, genesisTime time.Time, clo
 	}
 	attSlotEpoch := slots.ToEpoch(attSlot)
 	if attSlotEpoch != currentEpoch && attSlotEpoch != prevEpoch {
-		return fmt.Errorf("attestation slot %d not within current epoch %d or previous epoch %d", attSlot, currentEpoch, prevEpoch)
+		attError = fmt.Errorf(
+			"attestation epoch %d not within current epoch %d or previous epoch %d",
+			attSlot/params.BeaconConfig().SlotsPerEpoch,
+			currentEpoch,
+			prevEpoch,
+		)
+		return errors.Join(ErrTooLate, attError)
 	}
 
 	return nil
