fbbd1c1fb9ab813c64e913a0e87b366560aea6e4
Kasey Kirkham <kasey@users.noreply.github.com>
2023-09-07 14:03:12 -0500

    add details of excess commitments to error

--- beacon-chain/sync/validate_beacon_blocks.go
+++ beacon-chain/sync/validate_beacon_blocks.go
@@ -284,7 +284,7 @@ func validateDenebBeaconBlock(blk interfaces.ReadOnlyBeaconBlock) error {
 	// [REJECT] The length of KZG commitments is less than or equal to the limitation defined in Consensus Layer
 	// -- i.e. validate that len(body.signed_beacon_block.message.blob_kzg_commitments) <= MAX_BLOBS_PER_BLOCK
 	if len(commits) > fieldparams.MaxBlobsPerBlock {
-		return errRejectCommitmentLen
+		return errors.Wrapf(errRejectCommitmentLen, "%d > %d", len(commits), fieldparams.MaxBlobsPerBlock)
 	}
 	return nil
 }
