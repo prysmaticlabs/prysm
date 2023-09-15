6bc4fe3bf91f2678fd4278da28461e4d78456154
Potuz <potuz@prysmaticlabs.com>
2023-09-15 09:58:25 -0300

    change godoc

--- beacon-chain/blockchain/chain_info.go
+++ beacon-chain/blockchain/chain_info.go
@@ -526,7 +526,7 @@ func (s *Service) recoverStateSummary(ctx context.Context, blockRoot [32]byte) (
 	return nil, errBlockDoesNotExist
 }
 
-// BlockBeingSynced returns the HTR of the block being currently synced
+// BlockBeingSynced returns whether the block with the given root is currently being synced
 func (s *Service) BlockBeingSynced(root [32]byte) bool {
 	return s.blockBeingSynced.isSyncing(root)
 }
