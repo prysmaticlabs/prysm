2f6f1e3baec6cdfcd46d279e977d6aeeb9cb42b9
Potuz <potuz@prysmaticlabs.com>
2023-09-28 14:04:29 -0300

    Terence's suggestion
    Co-authored-by: terencechain <terence@prysmaticlabs.com>

--- testing/spectest/minimal/deneb/forkchoice/forkchoice_test.go
+++ testing/spectest/minimal/deneb/forkchoice/forkchoice_test.go
@@ -5,6 +5,6 @@ import (
 )
 
 func TestMinimal_Deneb_Forkchoice(t *testing.T) {
-	// forkchoice.Run(t, "minimal", version.Deneb)
 	t.Skip("blocked by go-kzg-4844 minimal trusted setup")
+	forkchoice.Run(t, "minimal", version.Deneb)
 }
