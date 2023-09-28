65d1cea1c95f0d5faf553d800ea23acae4ae0f8e
Potuz <potuz@prysmaticlabs.com>
2023-09-28 14:18:56 -0300

    fix forkchoice minimal

--- testing/spectest/minimal/deneb/forkchoice/forkchoice_test.go
+++ testing/spectest/minimal/deneb/forkchoice/forkchoice_test.go
@@ -2,6 +2,9 @@ package forkchoice
 
 import (
 	"testing"
+
+	"github.com/prysmaticlabs/prysm/v4/runtime/version"
+	"github.com/prysmaticlabs/prysm/v4/testing/spectest/shared/common/forkchoice"
 )
 
 func TestMinimal_Deneb_Forkchoice(t *testing.T) {
