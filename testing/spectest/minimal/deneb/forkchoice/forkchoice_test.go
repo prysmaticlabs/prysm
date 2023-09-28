d71818e82afc822a4c522d4e74c9730814ed672a
Potuz <potuz@prysmaticlabs.com>
2023-09-28 12:06:29 -0300

    remove minimal tests

--- testing/spectest/minimal/deneb/forkchoice/forkchoice_test.go
+++ testing/spectest/minimal/deneb/forkchoice/forkchoice_test.go
@@ -2,11 +2,8 @@ package forkchoice
 
 import (
 	"testing"
-
-	"github.com/prysmaticlabs/prysm/v4/runtime/version"
-	"github.com/prysmaticlabs/prysm/v4/testing/spectest/shared/common/forkchoice"
 )
 
 func TestMinimal_Deneb_Forkchoice(t *testing.T) {
-	forkchoice.Run(t, "minimal", version.Deneb)
+	// forkchoice.Run(t, "minimal", version.Deneb)
 }
