f812bdcf6038ae6c94309f53ee5141e5d91cc053
Delweng <delweng@gmail.com>
2023-10-03 15:41:55 -0500

    beacon-node/state:  alloc 1more item for append case (#12832)
    * beacon-chain/state/attenstation: alloc +1 items for append
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    
    * beacon-chain/state/eth1: alloc +1 items for append
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    
    * beacon-chain/state/misc: alloc +1 items for append
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    
    * beacon-chain/state/participation: alloc +1 items for append
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    
    * beacon-chain/state/validator: alloc +1 items for append
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    
    * Add some benchmarks
    
    * Evaluate append vs copy. Apply results
    
    * fix copy issue
    
    * revert copy changes from a5ba8d4352f647ad384981264cb6e0553481f23b
    
    ---------
    
    Signed-off-by: jsvisa <delweng@gmail.com>
    Co-authored-by: Nishant Das <nishdas93@gmail.com>
    Co-authored-by: Preston Van Loon <pvanloon@offchainlabs.com>

--- beacon-chain/state/state-native/setters_validator.go
+++ beacon-chain/state/state-native/setters_validator.go
@@ -190,7 +190,8 @@ func (b *BeaconState) AppendBalance(bal uint64) error {
 
 	bals := b.balances
 	if b.sharedFieldReferences[types.Balances].Refs() > 1 {
-		bals = b.balancesVal()
+		bals = make([]uint64, 0, len(b.balances)+1)
+		bals = append(bals, b.balances...)
 		b.sharedFieldReferences[types.Balances].MinusRef()
 		b.sharedFieldReferences[types.Balances] = stateutil.NewRef(1)
 	}
@@ -213,7 +214,8 @@ func (b *BeaconState) AppendInactivityScore(s uint64) error {
 
 	scores := b.inactivityScores
 	if b.sharedFieldReferences[types.InactivityScores].Refs() > 1 {
-		scores = b.inactivityScoresVal()
+		scores = make([]uint64, 0, len(b.inactivityScores)+1)
+		scores = append(scores, b.inactivityScores...)
 		b.sharedFieldReferences[types.InactivityScores].MinusRef()
 		b.sharedFieldReferences[types.InactivityScores] = stateutil.NewRef(1)
 	}
