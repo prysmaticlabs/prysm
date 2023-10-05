275855faa779df1565cc53ec86b1f8f3e1b6fb2c
terence tsao <terence@prysmaticlabs.com>
2023-10-04 20:54:05 -0700

    Prune sidecars older than previous slot

--- beacon-chain/sync/pending_blobs_queue.go
+++ beacon-chain/sync/pending_blobs_queue.go
@@ -1,7 +1,6 @@
 package sync
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"sync"
@@ -9,135 +8,132 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/prysmaticlabs/prysm/v4/beacon-chain/core/feed"
 	statefeed "github.com/prysmaticlabs/prysm/v4/beacon-chain/core/feed/state"
+	"github.com/prysmaticlabs/prysm/v4/config/params"
 	"github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v4/encoding/bytesutil"
-	ethpb "github.com/prysmaticlabs/prysm/v4/proto/eth/v1"
 	eth "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/v4/time/slots"
 )
 
-// processPendingBlobs listens to state changes and handles pending blobs.
+// processPendingBlobs listens for state changes and handles pending blobs.
 func (s *Service) processPendingBlobs() {
-	ctx := context.Background()
 	eventFeed := make(chan *feed.Event, 1)
 	sub := s.stateNotifier.StateFeed().Subscribe(eventFeed)
 	defer sub.Unsubscribe()
 
+	slotTicker := slots.NewSlotTicker(s.cfg.chain.GenesisTime(), params.BeaconConfig().SecondsPerSlot)
+
 	for {
 		select {
-		case <-s.ctx.Done(): // exit if context is done
+		case <-s.ctx.Done():
 			return
-		case <-sub.Err(): // exit on subscription error
+		case <-sub.Err():
 			return
 		case e := <-eventFeed:
-			s.handleEvent(ctx, e)
+			s.handleEvent(s.ctx, e)
+		case slot := <-slotTicker.C():
+			// Prune sidecars older than previous slot.
+			if slot > 0 {
+				slot--
+			}
+			s.pruneOldSidecars(slot)
 		}
 	}
 }
 
-// handleEvent processes an incoming event, dispatching it to the appropriate handler function.
+// handleEvent processes incoming events.
 func (s *Service) handleEvent(ctx context.Context, e *feed.Event) {
-	switch e.Type {
-	case statefeed.BlockProcessed:
+	if e.Type == statefeed.BlockProcessed {
 		s.handleNewBlockEvent(ctx, e)
-	case statefeed.FinalizedCheckpoint:
-		s.pruneBlobsOnFinalization(e)
 	}
 }
 
-// handleNewBlockEvent processes blobs when a parent block is processed.
+// handleNewBlockEvent handles blobs when a parent block is processed.
 func (s *Service) handleNewBlockEvent(ctx context.Context, e *feed.Event) {
 	data, ok := e.Data.(*statefeed.BlockProcessedData)
 	if !ok {
-		// Ignore if data is of the wrong type
 		return
 	}
-	parentRoot := data.SignedBlock.Block().ParentRoot()
+	s.processBlobsFromSidecars(ctx, data.SignedBlock.Block().ParentRoot())
+}
 
-	s.pendingBlobSidecars.Lock()
-	blobs, exists := s.pendingBlobSidecars.blobSidecars[parentRoot]
-	if exists {
-		delete(s.pendingBlobSidecars.blobSidecars, parentRoot)
-	}
-	s.pendingBlobSidecars.Unlock()
-
+// processBlobsFromSidecars processes blobs for a given parent root.
+func (s *Service) processBlobsFromSidecars(ctx context.Context, parentRoot [32]byte) {
+	blobs := s.pendingBlobSidecars.pop(parentRoot)
 	for _, blob := range blobs {
 		if err := s.validateAndReceiveBlob(ctx, blob); err != nil {
-			log.WithError(err).Error("Blob validation failed in pending queue")
+			log.WithError(err).Error("Failed to validate blob in pending queue")
 		}
 	}
 }
 
-// validateAndReceiveBlob validates a blob and receives it if valid.
+// validateAndReceiveBlob validates and receives a blob if it's valid.
 func (s *Service) validateAndReceiveBlob(ctx context.Context, blob *eth.SignedBlobSidecar) error {
-	pubsubResult, err := s.validateBlobPostSeenParent(ctx, blob)
+	result, err := s.validateBlobPostSeenParent(ctx, blob)
 	if err != nil {
 		return err
 	}
-	if pubsubResult != pubsub.ValidationAccept {
-		return fmt.Errorf("pubsubResult is not ValidationAccept, got %d", pubsubResult)
+	if result != pubsub.ValidationAccept {
+		return fmt.Errorf("unexpected pubsub result: %d", result)
 	}
 	return s.cfg.chain.ReceiveBlob(ctx, blob.Message)
 }
 
-// pruneBlobsOnFinalization removes staled blobs older than the finalized checkpoint.
-func (s *Service) pruneBlobsOnFinalization(e *feed.Event) {
-	finalizedCheckpoint, ok := e.Data.(*ethpb.EventFinalizedCheckpoint)
-	if !ok {
-		// Ignore if data is of the wrong type
-		return
-	}
-	finalizedSlot, err := slots.EpochStart(finalizedCheckpoint.Epoch)
-	if err != nil {
-		log.WithError(err).Error("Could not get finalized slot")
-		return
-	}
-
-	// Remove blobs older than finalized checkpoint
-	s.pendingBlobSidecars.Lock()
-	for parentRoot, sidecars := range s.pendingBlobSidecars.blobSidecars {
-		if s.isOlderThanFinalizedSlot(sidecars, finalizedSlot) {
-			delete(s.pendingBlobSidecars.blobSidecars, parentRoot)
-		}
-	}
-	s.pendingBlobSidecars.Unlock()
+// pruneOldSidecars removes sidecars older than a given slot.
+func (s *Service) pruneOldSidecars(slot primitives.Slot) {
+	s.pendingBlobSidecars.pruneOlderThanSlot(slot)
 }
 
-// isOlderThanFinalizedSlot checks if all blobs are older than the finalized slot.
-func (s *Service) isOlderThanFinalizedSlot(sidecars []*eth.SignedBlobSidecar, finalizedSlot primitives.Slot) bool {
-	for _, sidecar := range sidecars {
-		if sidecar.Message.Slot > finalizedSlot {
-			return false
-		}
-	}
-	return true
-}
-
-// pendingBlobSidecars contains pending blob sidecars and a mutex for concurrent access.
+// pendingBlobSidecars holds pending blob sidecars.
 type pendingBlobSidecars struct {
 	sync.RWMutex
 	blobSidecars map[[32]byte][]*eth.SignedBlobSidecar
 }
 
-// newPendingBlobSidecars creates a new pendingBlobSidecars instance.
+// newPendingBlobSidecars initializes a new pendingBlobSidecars instance.
 func newPendingBlobSidecars() *pendingBlobSidecars {
-	return &pendingBlobSidecars{
-		blobSidecars: make(map[[32]byte][]*eth.SignedBlobSidecar),
-	}
+	return &pendingBlobSidecars{blobSidecars: make(map[[32]byte][]*eth.SignedBlobSidecar)}
 }
 
-// add adds a new blob sidecar to the pending list.
+// add adds a new blob sidecar to the pending queue.
 func (p *pendingBlobSidecars) add(blob *eth.SignedBlobSidecar) {
 	p.Lock()
 	defer p.Unlock()
 
 	parentRoot := bytesutil.ToBytes32(blob.Message.BlockParentRoot)
-	for _, existingBlob := range p.blobSidecars[parentRoot] {
-		// Check for duplicates
-		if existingBlob.Message.Index == blob.Message.Index && bytes.Equal(existingBlob.Message.BlockRoot, blob.Message.BlockRoot) {
-			return
+	p.blobSidecars[parentRoot] = append(p.blobSidecars[parentRoot], blob)
+}
+
+// pop removes and returns all blob sidecars for a given parent root.
+func (p *pendingBlobSidecars) pop(parentRoot [32]byte) []*eth.SignedBlobSidecar {
+	p.Lock()
+	defer p.Unlock()
+
+	blobs, exists := p.blobSidecars[parentRoot]
+	if exists {
+		delete(p.blobSidecars, parentRoot)
+	}
+	return blobs
+}
+
+// pruneOlderThanSlot removes all blob sidecars older than a given slot.
+func (p *pendingBlobSidecars) pruneOlderThanSlot(slot primitives.Slot) {
+	p.Lock()
+	defer p.Unlock()
+
+	for root, sidecars := range p.blobSidecars {
+		if allOlderThanSlot(sidecars, slot) {
+			delete(p.blobSidecars, root)
 		}
 	}
+}
 
-	p.blobSidecars[parentRoot] = append(p.blobSidecars[parentRoot], blob)
+// allOlderThanSlot checks if all blob sidecars are older than a given slot.
+func allOlderThanSlot(sidecars []*eth.SignedBlobSidecar, slot primitives.Slot) bool {
+	for _, sidecar := range sidecars {
+		if sidecar.Message.Slot > slot {
+			return false
+		}
+	}
+	return true
 }
