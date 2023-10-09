7cf32654e3fa2856370c5dcaf932e181bb7f7a81
jsvisa <delweng@gmail.com>
2023-10-09 14:32:54 +0000

    beacon-chain/p2p: ust atomic.Bool instead of bool
    Signed-off-by: jsvisa <delweng@gmail.com>

--- beacon-chain/p2p/testing/mock_broadcaster.go
+++ beacon-chain/p2p/testing/mock_broadcaster.go
@@ -2,6 +2,8 @@ package testing
 
 import (
 	"context"
+	"sync"
+	"sync/atomic"
 
 	ethpb "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 	"google.golang.org/protobuf/proto"
@@ -9,33 +11,39 @@ import (
 
 // MockBroadcaster implements p2p.Broadcaster for testing.
 type MockBroadcaster struct {
-	BroadcastCalled       bool
+	BroadcastCalled       atomic.Bool
 	BroadcastMessages     []proto.Message
 	BroadcastAttestations []*ethpb.Attestation
+	msgLock               sync.Mutex
+	attLock               sync.Mutex
 }
 
 // Broadcast records a broadcast occurred.
 func (m *MockBroadcaster) Broadcast(_ context.Context, msg proto.Message) error {
-	m.BroadcastCalled = true
+	m.BroadcastCalled.Store(true)
+	m.msgLock.Lock()
+	defer m.attLock.Unlock()
 	m.BroadcastMessages = append(m.BroadcastMessages, msg)
 	return nil
 }
 
 // BroadcastAttestation records a broadcast occurred.
 func (m *MockBroadcaster) BroadcastAttestation(_ context.Context, _ uint64, a *ethpb.Attestation) error {
-	m.BroadcastCalled = true
+	m.BroadcastCalled.Store(true)
+	m.attLock.Lock()
+	defer m.attLock.Unlock()
 	m.BroadcastAttestations = append(m.BroadcastAttestations, a)
 	return nil
 }
 
 // BroadcastSyncCommitteeMessage records a broadcast occurred.
 func (m *MockBroadcaster) BroadcastSyncCommitteeMessage(_ context.Context, _ uint64, _ *ethpb.SyncCommitteeMessage) error {
-	m.BroadcastCalled = true
+	m.BroadcastCalled.Store(true)
 	return nil
 }
 
 // BroadcastBlob broadcasts a blob for mock.
 func (m *MockBroadcaster) BroadcastBlob(context.Context, uint64, *ethpb.SignedBlobSidecar) error {
-	m.BroadcastCalled = true
+	m.BroadcastCalled.Store(true)
 	return nil
 }
