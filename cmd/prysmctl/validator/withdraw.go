55e4c6e1db2489aeed2e00d305d69a908d8670ea
james-prysm <90280386+james-prysm@users.noreply.github.com>
2023-10-11 09:08:06 -0500

    HTTP API: `/eth/v1/beacon/pool/bls_to_execution_changes` (#12963)
    * wip
    
    * wip changes for beacon handlers
    
    * wip
    
    * updating protos
    
    * fixing conflict
    
    * fixing protos
    
    * fixing more tests
    
    * migrating tests and removing one that is irrelevant
    
    * fixing e2e
    
    * fixing unit tests
    
    * missed adding routes
    
    * gaz
    
    * fixing more tests
    
    * Update beacon-chain/rpc/eth/beacon/handlers_pool.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update beacon-chain/rpc/eth/shared/structs.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update beacon-chain/rpc/eth/shared/structs.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update beacon-chain/rpc/eth/shared/structs.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * Update beacon-chain/rpc/eth/shared/structs.go
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>
    
    * radek's comments
    
    * fixing protos after conflict
    
    * accidently removed a proto type
    
    ---------
    
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- cmd/prysmctl/validator/withdraw.go
+++ cmd/prysmctl/validator/withdraw.go
@@ -15,6 +15,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/v4/api/client/beacon"
 	"github.com/prysmaticlabs/prysm/v4/beacon-chain/rpc/apimiddleware"
+	"github.com/prysmaticlabs/prysm/v4/beacon-chain/rpc/eth/shared"
 	fieldparams "github.com/prysmaticlabs/prysm/v4/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
 	log "github.com/sirupsen/logrus"
@@ -40,8 +41,8 @@ func setWithdrawalAddresses(c *cli.Context) error {
 	return callWithdrawalEndpoints(ctx, beaconNodeHost, setWithdrawalAddressJsons)
 }
 
-func getWithdrawalMessagesFromPathFlag(c *cli.Context) ([]*apimiddleware.SignedBLSToExecutionChangeJson, error) {
-	setWithdrawalAddressJsons := make([]*apimiddleware.SignedBLSToExecutionChangeJson, 0)
+func getWithdrawalMessagesFromPathFlag(c *cli.Context) ([]*shared.SignedBLSToExecutionChange, error) {
+	setWithdrawalAddressJsons := make([]*shared.SignedBLSToExecutionChange, 0)
 	foundFilePaths, err := findWithdrawalFiles(c.String(PathFlag.Name))
 	if err != nil {
 		return setWithdrawalAddressJsons, errors.Wrap(err, "failed to find withdrawal files")
@@ -67,8 +68,8 @@ func getWithdrawalMessagesFromPathFlag(c *cli.Context) ([]*apimiddleware.SignedB
 			if len(obj.Signature) == fieldparams.BLSSignatureLength*2 {
 				to[i].Signature = fmt.Sprintf("0x%s", obj.Signature)
 			}
-			setWithdrawalAddressJsons = append(setWithdrawalAddressJsons, &apimiddleware.SignedBLSToExecutionChangeJson{
-				Message: &apimiddleware.BLSToExecutionChangeJson{
+			setWithdrawalAddressJsons = append(setWithdrawalAddressJsons, &shared.SignedBLSToExecutionChange{
+				Message: &shared.BLSToExecutionChange{
 					ValidatorIndex:     to[i].Message.ValidatorIndex,
 					FromBLSPubkey:      to[i].Message.FromBLSPubkey,
 					ToExecutionAddress: to[i].Message.ToExecutionAddress,
@@ -83,7 +84,7 @@ func getWithdrawalMessagesFromPathFlag(c *cli.Context) ([]*apimiddleware.SignedB
 	return setWithdrawalAddressJsons, nil
 }
 
-func callWithdrawalEndpoints(ctx context.Context, host string, request []*apimiddleware.SignedBLSToExecutionChangeJson) error {
+func callWithdrawalEndpoints(ctx context.Context, host string, request []*shared.SignedBLSToExecutionChange) error {
 	client, err := beacon.NewClient(host)
 	if err != nil {
 		return err
@@ -119,7 +120,7 @@ func callWithdrawalEndpoints(ctx context.Context, host string, request []*apimid
 	return checkIfWithdrawsAreInPool(ctx, client, request)
 }
 
-func checkIfWithdrawsAreInPool(ctx context.Context, client *beacon.Client, request []*apimiddleware.SignedBLSToExecutionChangeJson) error {
+func checkIfWithdrawsAreInPool(ctx context.Context, client *beacon.Client, request []*shared.SignedBLSToExecutionChange) error {
 	log.Info("Verifying requested withdrawal messages known to node...")
 	poolResponse, err := client.GetBLStoExecutionChanges(ctx)
 	if err != nil {
