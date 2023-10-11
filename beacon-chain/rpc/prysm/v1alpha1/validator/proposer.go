83792ae4fa45b9401117d42fb8c2cd64ab2c9677
terence tsao <terence@prysmaticlabs.com>
2023-10-11 07:23:58 -0700

    Fix conflict

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer.go
@@ -112,7 +112,7 @@ func (vs *Server) GetBeaconBlock(ctx context.Context, req *ethpb.BlockRequest) (
 	}
 	sBlk.SetProposerIndex(idx)
 
-	if err = vs.BuildBlockParallel(ctx, sBlk, head); err != nil {
+	if err = vs.BuildBlockParallel(ctx, sBlk, head, false); err != nil {
 		return nil, errors.Wrap(err, "could not build block in parallel")
 	}
 
