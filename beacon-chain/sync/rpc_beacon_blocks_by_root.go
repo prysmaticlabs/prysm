91d85303a378424ba70385ab1770195190000f10
terence tsao <terence@prysmaticlabs.com>
2023-10-11 18:50:38 -0700

    Remove logs

--- beacon-chain/sync/rpc_beacon_blocks_by_root.go
+++ beacon-chain/sync/rpc_beacon_blocks_by_root.go
@@ -139,6 +139,9 @@ func (s *Service) requestPendingBlobs(ctx context.Context, block interfaces.Read
 	if err != nil {
 		return err
 	}
+	if len(request) == 0 {
+		return nil // No operation if all blobs are known.
+	}
 
 	return s.sendAndSaveBlobSidecars(ctx, request, contextByte, peerID)
 }
