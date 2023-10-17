ae61abd7190b88b24259842fa138bbe2c952bef7
Potuz <potuz@prysmaticlabs.com>
2023-10-17 16:19:12 -0300

    only measure last insertion

--- beacon-chain/forkchoice/doubly-linked-tree/benchmarks_test.go
+++ beacon-chain/forkchoice/doubly-linked-tree/benchmarks_test.go
@@ -14,16 +14,18 @@ import (
 func Benchmark_OnBlock(b *testing.B) {
 	steps := []int{100, 1000, 10000, 20000, 40000}
 
+	ctx := context.Background()
 	for i := range steps {
 		b.Run(fmt.Sprintf("non_finalizing_epochs_%d", steps[i]), func(b *testing.B) {
 			for j := 0; j < b.N; j++ {
 				b.StopTimer()
 				f := setup(0, 0)
-				ctx := context.Background()
 				parentRoot := [32]byte{}
 				newRoot := [32]byte{}
+				var slot primitives.Slot
+				var err error
 				for epoch := primitives.Epoch(0); epoch < primitives.Epoch(steps[i]); epoch++ {
-					slot, err := slots.EpochStart(epoch)
+					slot, err = slots.EpochStart(epoch)
 					require.NoError(b, err)
 					_, err = rand.Read(newRoot[:])
 					require.NoError(b, err)
@@ -33,8 +35,15 @@ func Benchmark_OnBlock(b *testing.B) {
 					require.NoError(b, f.SetOptimisticToValid(ctx, newRoot))
 					parentRoot = newRoot
 				}
+				slot++
+				_, err = rand.Read(newRoot[:])
+				require.NoError(b, err)
+				st, root, err := prepareForkchoiceState(ctx, slot, newRoot, parentRoot, [32]byte{}, 0, 0)
+				require.NoError(b, err)
 				b.StartTimer()
-				root, err := f.Head(ctx)
+				require.NoError(b, f.InsertNode(ctx, st, root))
+				require.NoError(b, f.SetOptimisticToValid(ctx, newRoot))
+				root, err = f.Head(ctx)
 				require.NoError(b, err)
 				require.Equal(b, newRoot, root)
 			}
