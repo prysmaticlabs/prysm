1f49f6b486e6f0b05edc045ef5a615cb28dc245d
Preston Van Loon <pvanloon@offchainlabs.com>
2023-10-18 15:56:51 -0500

    Add all paths registered test

---
+++ beacon-chain/rpc/apimiddleware/endpoint_factory_test.go
@@ -0,0 +1,17 @@
+package apimiddleware_test
+
+import (
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/v4/beacon-chain/rpc/apimiddleware"
+	"github.com/prysmaticlabs/prysm/v4/testing/require"
+)
+
+func TestBeaconEndpointFactory_AllPathsRegistered(t *testing.T) {
+  f := &apimiddleware.BeaconEndpointFactory{}
+
+  for _, p := range f.Paths() {
+    _, err := f.Create(p)
+    require.NoError(t, err, "failed to register %s", p)
+  }
+}
