01b34cb67b829a66f8e919a7815b61bb84d4ee3f
Sammy Rosso <15244892+saolyn@users.noreply.github.com>
2023-10-26 18:23:56 +0200

    Update cmd/beacon-chain/flags/base.go
    Co-authored-by: Preston Van Loon <pvanloon@offchainlabs.com>

--- cmd/beacon-chain/flags/base.go
+++ cmd/beacon-chain/flags/base.go
@@ -262,8 +262,8 @@ var (
 		Value: cmd.DefaultDataDir(),
 	}
 	BlobRetentionEpoch = &cli.Uint64Flag{
-		Name:    "blob-retention-epoch",
-		Usage:   "Extend blob retention epoch period to beyond default 4096 epochs (~18 days). The node will error at start if input value is less than 4096 epochs.",
+		Name:    "blob-retention-epochs",
+		Usage:   "Set the blob retention epoch period. The default is 4096 epochs (~18 days) and the node will error at start if input value is less than 4096 epochs.",
 		Value:   uint64(params.BeaconNetworkConfig().MinEpochsForBlobsSidecarsRequest),
 		Aliases: []string{"extend-blob-retention-epoch"},
 	}
