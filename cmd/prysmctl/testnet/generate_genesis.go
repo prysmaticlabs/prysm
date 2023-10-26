437899623aa19ba00e369a6e456ee32007bcfe79
Stefan <stefan@starflinger.eu>
2023-10-26 14:06:50 -0400

    fix segmentation fork when Capella for epoch is MaxUint64
    fix segmentation
    
    fix segmentation

--- cmd/prysmctl/testnet/generate_genesis.go
+++ cmd/prysmctl/testnet/generate_genesis.go
@@ -274,10 +274,17 @@ func generateGenesis(ctx context.Context) (state.BeaconState, error) {
 		gen.Config.ShanghaiTime = interop.GethShanghaiTime(f.GenesisTime, params.BeaconConfig())
 		//gen.Config.CancunTime = interop.GethCancunTime(f.GenesisTime, params.BeaconConfig())
 		gen.Config.CancunTime = interop.GethCancunTime(f.GenesisTime, params.BeaconConfig())
-		log.
-			WithField("shanghai", fmt.Sprintf("%d", *gen.Config.ShanghaiTime)).
-			WithField("cancun", fmt.Sprintf("%d", *gen.Config.CancunTime)).
-			Info("setting fork geth times")
+
+		if gen.Config.ShanghaiTime != nil {
+			log.
+				WithField("shanghai", fmt.Sprintf("%d", *gen.Config.ShanghaiTime)).
+				Info("setting fork geth times")
+		}
+		if gen.Config.CancunTime != nil {
+			log.
+				WithField("cancun", fmt.Sprintf("%d", *gen.Config.CancunTime)).
+				Info("setting fork geth times")
+		}
 		if v > version.Altair {
 			// set ttd to zero so EL goes post-merge immediately
 			gen.Config.TerminalTotalDifficulty = big.NewInt(0)
