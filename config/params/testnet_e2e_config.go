da26ef2beadf714bcd1407deffa2012221698ad0
nisdas <nishdas93@gmail.com>
2023-11-08 10:59:51 +0800

    remove deneb activation

--- config/params/testnet_e2e_config.go
+++ config/params/testnet_e2e_config.go
@@ -1,10 +1,12 @@
 package params
 
+import "math"
+
 const (
 	AltairE2EForkEpoch    = 6
 	BellatrixE2EForkEpoch = 8
 	CapellaE2EForkEpoch   = 10
-	DenebE2EForkEpoch     = 12
+	DenebE2EForkEpoch     = math.MaxUint64
 )
 
 // E2ETestConfig retrieves the configurations made specifically for E2E testing.
