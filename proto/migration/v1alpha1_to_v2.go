16c4f8681dda4c32fce7c4bf9406e374ec360597
Lizhang <lizhang@polymerlabs.org>
2023-11-11 04:36:58 +0800

    fix: NPE

--- proto/migration/v1alpha1_to_v2.go
+++ proto/migration/v1alpha1_to_v2.go
@@ -1294,9 +1294,25 @@ func V1Alpha1SignedBLSToExecChangeToV2(alphaChange *ethpbalpha.SignedBLSToExecut
 
 // V1Alpha1SyncCommitteeToV2 converts a v1alpha1 SyncCommittee object to its v2 equivalent.
 func V1Alpha1SyncCommitteeToV2(alphaCommittee *ethpbalpha.SyncCommittee) *ethpbv2.SyncCommittee {
+	if alphaCommittee == nil {
+		return nil
+	}
+
 	result := &ethpbv2.SyncCommittee{
 		Pubkeys:         bytesutil.SafeCopy2dBytes(alphaCommittee.Pubkeys),
 		AggregatePubkey: bytesutil.SafeCopyBytes(alphaCommittee.AggregatePubkey),
+	}
+	return result
+}
+
+func V2SyncCommitteeToV1Alpha1(committee *ethpbv2.SyncCommittee) *ethpbalpha.SyncCommittee {
+	if committee == nil {
+		return nil
+	}
+
+	result := &ethpbalpha.SyncCommittee{
+		Pubkeys:         bytesutil.SafeCopy2dBytes(committee.Pubkeys),
+		AggregatePubkey: bytesutil.SafeCopyBytes(committee.AggregatePubkey),
 	}
 	return result
 }
