19b9310b422ed21c9d94088a7506a49300b2af04
Manu NALEPA <enalepa@offchainlabs.com>
2023-11-27 14:10:07 +0100

    `SaveGenesisValidatorsRoot`: Allow to overwrite the genesis validator root if the root is the same.

--- validator/db/kv/genesis_test.go
+++ validator/db/kv/genesis_test.go
@@ -27,7 +27,14 @@ func TestStore_GenesisValidatorsRoot_ReadAndWrite(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:    "zero then overwrite rejected",
+			name:    "zero then overwrite with the same value",
+			init:    params.BeaconConfig().ZeroHash[:],
+			want:    params.BeaconConfig().ZeroHash[:],
+			write:   params.BeaconConfig().ZeroHash[:],
+			wantErr: false,
+		},
+		{
+			name:    "zero then overwrite with a different value",
 			init:    params.BeaconConfig().ZeroHash[:],
 			want:    params.BeaconConfig().ZeroHash[:],
 			write:   []byte{5},
@@ -42,6 +49,11 @@ func TestStore_GenesisValidatorsRoot_ReadAndWrite(t *testing.T) {
 			err := db.SaveGenesisValidatorsRoot(ctx, tt.init)
 			require.NoError(t, err)
 
+			// Read the value from the database (just to ensure our setup is OK).
+			got, err := db.GenesisValidatorsRoot(ctx)
+			require.NoError(t, err)
+			require.DeepEqual(t, tt.init, got)
+
 			// Write the value to the database.
 			err = db.SaveGenesisValidatorsRoot(ctx, tt.write)
 			if (err != nil) != tt.wantErr {
@@ -49,7 +61,7 @@ func TestStore_GenesisValidatorsRoot_ReadAndWrite(t *testing.T) {
 			}
 
 			// Read the value from the database.
-			got, err := db.GenesisValidatorsRoot(ctx)
+			got, err = db.GenesisValidatorsRoot(ctx)
 			require.NoError(t, err)
 			require.DeepEqual(t, tt.want, got)
 
