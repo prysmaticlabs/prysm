209dd2ec2dae5c34d29e49096c79b62f1110d661
Manu NALEPA <enalepa@offchainlabs.com>
2023-11-27 13:52:53 +0100

    `TestStore_GenesisValidatorsRoot_ReadAndWrite`: Make all test cases independents.
    In a test with multiple test cases, each test case should be independents.
    (aka: Removing test case `A` should not impact test case `B`)

--- validator/db/kv/genesis_test.go
+++ validator/db/kv/genesis_test.go
@@ -11,34 +11,50 @@ import (
 
 func TestStore_GenesisValidatorsRoot_ReadAndWrite(t *testing.T) {
 	ctx := context.Background()
-	db := setupDB(t, [][fieldparams.BLSPubkeyLength]byte{})
+
 	tests := []struct {
 		name    string
+		init    []byte
 		want    []byte
 		write   []byte
 		wantErr bool
 	}{
 		{
-			name:  "empty then write",
-			want:  nil,
-			write: params.BeaconConfig().ZeroHash[:],
+			name:    "empty then write",
+			init:    nil,
+			want:    params.BeaconConfig().ZeroHash[:],
+			write:   params.BeaconConfig().ZeroHash[:],
+			wantErr: false,
 		},
 		{
 			name:    "zero then overwrite rejected",
+			init:    params.BeaconConfig().ZeroHash[:],
 			want:    params.BeaconConfig().ZeroHash[:],
 			write:   []byte{5},
 			wantErr: true,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := db.GenesisValidatorsRoot(ctx)
+			// Initialize the database with the initial value.
+			db := setupDB(t, [][fieldparams.BLSPubkeyLength]byte{})
+			err := db.SaveGenesisValidatorsRoot(ctx, tt.init)
 			require.NoError(t, err)
-			require.DeepEqual(t, tt.want, got)
+
+			// Write the value to the database.
 			err = db.SaveGenesisValidatorsRoot(ctx, tt.write)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GenesisValidatorsRoot() error = %v, wantErr %v", err, tt.wantErr)
 			}
+
+			// Read the value from the database.
+			got, err := db.GenesisValidatorsRoot(ctx)
+			require.NoError(t, err)
+			require.DeepEqual(t, tt.want, got)
+
+			// Close the database.
+			require.NoError(t, db.Close())
 		})
 	}
 }
