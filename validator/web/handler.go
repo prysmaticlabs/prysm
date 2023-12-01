c3630baed5f3993e0cd1dc42e190e687b1977eaa
james-prysm <james@prysmaticlabs.com>
2023-12-01 10:39:43 -0600

    adding back deprecation comment notice

--- validator/web/handler.go
+++ validator/web/handler.go
@@ -10,6 +10,7 @@ import (
 const prefix = "prysm-web-ui"
 
 // Handler serves web requests from the bundled site data.
+// DEPRECATED: Prysm Web UI and associated endpoints will be fully removed in a future hard fork.
 var Handler = func(res http.ResponseWriter, req *http.Request) {
 	addSecurityHeaders(res)
 	u, err := url.ParseRequestURI(req.RequestURI)
