025c64500ebd8dc1c53fa3637b320a561877059b
Manu NALEPA <enalepa@offchainlabs.com>
2023-12-06 20:41:31 +0100

    `TestEIP3076SpecTests`: Test both database types
    Failing ATM

--- validator/client/slashing_protection_interchange_test.go
+++ validator/client/slashing_protection_interchange_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -15,6 +16,7 @@ import (
 	ethpb "github.com/prysmaticlabs/prysm/v4/proto/prysm/v1alpha1"
 	"github.com/prysmaticlabs/prysm/v4/testing/require"
 	"github.com/prysmaticlabs/prysm/v4/testing/util"
+	"github.com/prysmaticlabs/prysm/v4/validator/db/kv"
 	history "github.com/prysmaticlabs/prysm/v4/validator/slashing-protection-history"
 )
 
@@ -46,6 +48,7 @@ type eip3076TestCase struct {
 			Pubkey                string `json:"pubkey"`
 			Slot                  string `json:"slot"`
 			SigningRoot           string `json:"signing_root"`
+			ShouldSucceedMinimal  bool   `json:"should_succeed"`
 			ShouldSucceedComplete bool   `json:"should_succeed_complete"`
 		} `json:"blocks"`
 		Attestations []struct {
@@ -53,6 +56,7 @@ type eip3076TestCase struct {
 			SourceEpoch           string `json:"source_epoch"`
 			TargetEpoch           string `json:"target_epoch"`
 			SigningRoot           string `json:"signing_root"`
+			ShouldSucceedMinimal  bool   `json:"should_succeed"`
 			ShouldSucceedComplete bool   `json:"should_succeed_complete"`
 		} `json:"attestations"`
 	} `json:"steps"`
@@ -76,99 +80,110 @@ func setupEIP3076SpecTests(t *testing.T) []*eip3076TestCase {
 }
 
 func TestEIP3076SpecTests(t *testing.T) {
-	testCases := setupEIP3076SpecTests(t)
 	for _, tt := range testCases {
-		t.Run(tt.Name, func(t *testing.T) {
-			if tt.Name == "" {
-				t.Skip("Skipping eip3076TestCase with empty name")
-			}
-
-			// Set up validator client, one new validator client per eip3076TestCase.
-			// This ensures we initialize a new (empty) slashing protection database.
-			validator, _, _, _ := setup(t, nil)
-
-			for _, step := range tt.Steps {
-				if tt.GenesisValidatorsRoot != "" {
-					r, err := history.RootFromHex(tt.GenesisValidatorsRoot)
-					require.NoError(t, validator.db.SaveGenesisValidatorsRoot(context.Background(), r[:]))
-					require.NoError(t, err)
+		for _, st := range setupEIP3076SpecTests(t) {
+			t.Run(fmt.Sprintf("%s - %s", tt.name, st.Name), func(t *testing.T) {
+				if st.Name == "" {
+					t.Skip("Skipping eip3076TestCase with empty name")
 				}
 
-				// The eip3076TestCase config contains the interchange config in json.
-				// This loads the interchange data via ImportStandardProtectionJSON.
-				interchangeBytes, err := json.Marshal(step.Interchange)
-				if err != nil {
-					t.Fatal(err)
-				}
-				b := bytes.NewBuffer(interchangeBytes)
-				if err := history.ImportStandardProtectionJSON(context.Background(), validator.db, b); err != nil {
-					if step.ShouldSucceed {
+				// Set up validator client, one new validator client per eip3076TestCase.
+				// This ensures we initialize a new (empty) slashing protection database.
+				validator, _, _, _ := setup(t, &setupParams{slashingProtectionType: tt.slashingProtectionType})
+
+				for _, step := range st.Steps {
+					if st.GenesisValidatorsRoot != "" {
+						r, err := history.RootFromHex(st.GenesisValidatorsRoot)
+						require.NoError(t, validator.db.SaveGenesisValidatorsRoot(context.Background(), r[:]))
+						require.NoError(t, err)
+					}
+
+					// The eip3076TestCase config contains the interchange config in json.
+					// This loads the interchange data via ImportStandardProtectionJSON.
+					interchangeBytes, err := json.Marshal(step.Interchange)
+					if err != nil {
 						t.Fatal(err)
 					}
-				} else if !step.ShouldSucceed {
-					require.NotNil(t, err, "import standard protection json should have failed")
+					b := bytes.NewBuffer(interchangeBytes)
+					if err := history.ImportStandardProtectionJSON(context.Background(), validator.db, b); err != nil {
+						if step.ShouldSucceed {
+							t.Fatal(err)
+						}
+					} else if !step.ShouldSucceed {
+						require.NotNil(t, err, "import standard protection json should have failed")
+					}
+
+					// This loops through a list of block signings to attempt after importing the interchange data above.
+					for _, sb := range step.Blocks {
+						shouldSucceed := sb.ShouldSucceedComplete
+						if tt.slashingProtectionType == kv.Minimal {
+							shouldSucceed = sb.ShouldSucceedMinimal
+						}
+
+						bSlot, err := history.SlotFromString(sb.Slot)
+						require.NoError(t, err)
+						pk, err := history.PubKeyFromHex(sb.Pubkey)
+						require.NoError(t, err)
+						b := util.NewBeaconBlock()
+						b.Block.Slot = bSlot
+
+						var signingRoot [32]byte
+						if sb.SigningRoot != "" {
+							signingRootBytes, err := hex.DecodeString(strings.TrimPrefix(sb.SigningRoot, "0x"))
+							require.NoError(t, err)
+							copy(signingRoot[:], signingRootBytes)
+						}
+
+						wsb, err := blocks.NewSignedBeaconBlock(b)
+						require.NoError(t, err)
+						err = validator.slashableProposalCheck(context.Background(), pk, wsb, signingRoot)
+						if shouldSucceed {
+							require.NoError(t, err)
+						} else {
+							require.NotEqual(t, nil, err, "pre validation should have failed for block")
+						}
+					}
+
+					// This loops through a list of attestation signings to attempt after importing the interchange data above.
+					for _, sa := range step.Attestations {
+						shouldSucceed := sa.ShouldSucceedComplete
+						if tt.slashingProtectionType == kv.Minimal {
+							shouldSucceed = sa.ShouldSucceedMinimal
+						}
+
+						target, err := history.EpochFromString(sa.TargetEpoch)
+						require.NoError(t, err)
+						source, err := history.EpochFromString(sa.SourceEpoch)
+						require.NoError(t, err)
+						pk, err := history.PubKeyFromHex(sa.Pubkey)
+						require.NoError(t, err)
+						ia := &ethpb.IndexedAttestation{
+							Data: &ethpb.AttestationData{
+								BeaconBlockRoot: make([]byte, 32),
+								Target:          &ethpb.Checkpoint{Epoch: target, Root: make([]byte, 32)},
+								Source:          &ethpb.Checkpoint{Epoch: source, Root: make([]byte, 32)},
+							},
+							Signature: make([]byte, fieldparams.BLSSignatureLength),
+						}
+
+						var signingRoot [32]byte
+						if sa.SigningRoot != "" {
+							signingRootBytes, err := hex.DecodeString(strings.TrimPrefix(sa.SigningRoot, "0x"))
+							require.NoError(t, err)
+							copy(signingRoot[:], signingRootBytes)
+						}
+
+						err = validator.slashableAttestationCheck(context.Background(), ia, pk, signingRoot)
+						if shouldSucceed {
+							require.NoError(t, err)
+						} else {
+							require.NotNil(t, err, "pre validation should have failed for attestation")
+						}
+					}
 				}
 
-				// This loops through a list of block signings to attempt after importing the interchange data above.
-				for _, sb := range step.Blocks {
-					bSlot, err := history.SlotFromString(sb.Slot)
-					require.NoError(t, err)
-					pk, err := history.PubKeyFromHex(sb.Pubkey)
-					require.NoError(t, err)
-					b := util.NewBeaconBlock()
-					b.Block.Slot = bSlot
-
-					var signingRoot [32]byte
-					if sb.SigningRoot != "" {
-						signingRootBytes, err := hex.DecodeString(strings.TrimPrefix(sb.SigningRoot, "0x"))
-						require.NoError(t, err)
-						copy(signingRoot[:], signingRootBytes)
-					}
-
-					wsb, err := blocks.NewSignedBeaconBlock(b)
-					require.NoError(t, err)
-					err = validator.slashableProposalCheck(context.Background(), pk, wsb, signingRoot)
-					if sb.ShouldSucceedComplete {
-						require.NoError(t, err)
-					} else {
-						require.NotEqual(t, nil, err, "pre validation should have failed for block")
-					}
-				}
-
-				// This loops through a list of attestation signings to attempt after importing the interchange data above.
-				for _, sa := range step.Attestations {
-					target, err := history.EpochFromString(sa.TargetEpoch)
-					require.NoError(t, err)
-					source, err := history.EpochFromString(sa.SourceEpoch)
-					require.NoError(t, err)
-					pk, err := history.PubKeyFromHex(sa.Pubkey)
-					require.NoError(t, err)
-					ia := &ethpb.IndexedAttestation{
-						Data: &ethpb.AttestationData{
-							BeaconBlockRoot: make([]byte, 32),
-							Target:          &ethpb.Checkpoint{Epoch: target, Root: make([]byte, 32)},
-							Source:          &ethpb.Checkpoint{Epoch: source, Root: make([]byte, 32)},
-						},
-						Signature: make([]byte, fieldparams.BLSSignatureLength),
-					}
-
-					var signingRoot [32]byte
-					if sa.SigningRoot != "" {
-						signingRootBytes, err := hex.DecodeString(strings.TrimPrefix(sa.SigningRoot, "0x"))
-						require.NoError(t, err)
-						copy(signingRoot[:], signingRootBytes)
-					}
-
-					err = validator.slashableAttestationCheck(context.Background(), ia, pk, signingRoot)
-					if sa.ShouldSucceedComplete {
-						require.NoError(t, err)
-					} else {
-						require.NotNil(t, err, "pre validation should have failed for attestation")
-					}
-				}
-			}
-
-			require.NoError(t, validator.db.Close(), "failed to close slashing protection database")
-		})
+				require.NoError(t, validator.db.Close(), "failed to close slashing protection database")
+			})
+		}
 	}
 }
