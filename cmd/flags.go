f1c8d767f82210018925878e4129d09ed492162f
nisdas <nishdas93@gmail.com>
2023-12-06 16:48:20 +0800

    push up the defaults

--- cmd/flags.go
+++ cmd/flags.go
@@ -159,7 +159,7 @@ var (
 	P2PMaxPeers = &cli.IntFlag{
 		Name:  "p2p-max-peers",
 		Usage: "The max number of p2p peers to maintain.",
-		Value: 45,
+		Value: 70,
 	}
 	// P2PAllowList defines a CIDR subnet to exclusively allow connections.
 	P2PAllowList = &cli.StringFlag{
