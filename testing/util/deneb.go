1634720686d9180d15f888c6af5a89eec0e6b346
Kasey Kirkham <kasey@users.noreply.github.com>
2023-12-06 19:10:46 -0600

    update test for EIP-7044

--- testing/util/deneb.go
+++ testing/util/deneb.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"encoding/binary"
+	"math"
 	"math/big"
 	"testing"
 
@@ -185,4 +186,20 @@ func ExtendBlocksPlusBlobs(t *testing.T, blks []blocks.ROBlock, size int) ([]blo
 	}
 
 	return blks, blobs
+}
+
+// HackDenebForkEpoch is helpful for tests that need to set up cases where the deneb fork has passed.
+// We have unit tests that assert our config matches the upstream config, where the next fork is always
+// set to MaxUint64 until the fork epoch is formally set. This creates an issue for tests that want to
+// work with slots that are defined to be after deneb because converting the max epoch to a slot leads
+// to multiplication overflow.
+// Monkey patching tests with this function is the simplest workaround in these cases.
+func HackDenebMaxuint(t *testing.T) func() {
+	bc := params.MainnetConfig().Copy()
+	bc.DenebForkEpoch = math.MaxUint32
+	undo, err := params.SetActiveWithUndo(bc)
+	require.NoError(t, err)
+	return func() {
+		require.NoError(t, undo())
+	}
 }
