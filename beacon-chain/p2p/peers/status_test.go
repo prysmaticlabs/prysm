4b933656b0eb4b5dfdf788f23c28dd67d9a613f8
nisdas <nishdas93@gmail.com>
2023-12-08 15:54:43 +0800

    fix tests

--- beacon-chain/p2p/peers/status_test.go
+++ beacon-chain/p2p/peers/status_test.go
@@ -1108,7 +1108,7 @@ func TestInbound(t *testing.T) {
 
 	result := p.Inbound()
 	require.Equal(t, 1, len(result))
-	assert.Equal(t, inbound.Pretty(), result[0].Pretty())
+	assert.Equal(t, inbound.String(), result[0].String())
 }
 
 func TestOutbound(t *testing.T) {
@@ -1127,7 +1127,7 @@ func TestOutbound(t *testing.T) {
 
 	result := p.Outbound()
 	require.Equal(t, 1, len(result))
-	assert.Equal(t, outbound.Pretty(), result[0].Pretty())
+	assert.Equal(t, outbound.String(), result[0].String())
 }
 
 // addPeer is a helper to add a peer with a given connection state)
