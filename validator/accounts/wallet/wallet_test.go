8aa18076d29b91e4c234692a75521e716ba70443
james-prysm <james@prysmaticlabs.com>
2023-12-13 08:34:32 -0600

    fixing account import checking wallet twice, and adding sub folder search with a depth of 2

--- validator/accounts/wallet/wallet_test.go
+++ validator/accounts/wallet/wallet_test.go
@@ -2,19 +2,24 @@ package wallet_test
 
 import (
 	"context"
+	"flag"
 	"io"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/prysmaticlabs/prysm/v4/cmd/validator/flags"
 	"github.com/prysmaticlabs/prysm/v4/config/params"
 	"github.com/prysmaticlabs/prysm/v4/testing/assert"
 	"github.com/prysmaticlabs/prysm/v4/testing/require"
 	"github.com/prysmaticlabs/prysm/v4/validator/accounts/iface"
 	"github.com/prysmaticlabs/prysm/v4/validator/accounts/wallet"
+	"github.com/prysmaticlabs/prysm/v4/validator/keymanager"
 	remoteweb3signer "github.com/prysmaticlabs/prysm/v4/validator/keymanager/remote-web3signer"
 	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
 
 func init() {
@@ -83,4 +88,79 @@ func TestWallet_InitializeKeymanager_web3Signer_nilConfig(t *testing.T) {
 	km, err := w.InitializeKeymanager(ctx, config)
 	assert.NotNil(t, err)
 	assert.Equal(t, nil, km)
+}
+
+func TestOpenOrCreateNewWallet(t *testing.T) {
+	walletDir := filepath.Join(t.TempDir(), "wallet")
+	passwordFileDir := filepath.Join(t.TempDir(), "passwordFile")
+	require.NoError(t, os.MkdirAll(passwordFileDir, params.BeaconIoConfig().ReadWriteExecutePermissions))
+	passwordFilePath := filepath.Join(passwordFileDir, "password.txt")
+	require.NoError(t, os.WriteFile(passwordFilePath, []byte("existing"), os.ModePerm))
+
+	app := cli.App{}
+	set := flag.NewFlagSet("test", 0)
+	set.String(flags.WalletDirFlag.Name, walletDir, "")
+	set.String(flags.KeymanagerKindFlag.Name, keymanager.Local.String(), "")
+	set.String(flags.WalletPasswordFileFlag.Name, passwordFilePath, "")
+
+	type args struct {
+		cliCtx *cli.Context
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *wallet.Wallet
+		wantErr bool
+	}{
+		{
+			name: "New Wallet",
+			args: args{
+				cliCtx: func() *cli.Context {
+					assert.NoError(t, set.Set(flags.WalletDirFlag.Name, walletDir))
+					assert.NoError(t, set.Set(flags.KeymanagerKindFlag.Name, keymanager.Local.String()))
+					assert.NoError(t, set.Set(flags.WalletPasswordFileFlag.Name, passwordFilePath))
+					return cli.NewContext(&app, set, nil)
+				}(),
+			},
+			want: wallet.New(&wallet.Config{
+				WalletDir:      walletDir,
+				KeymanagerKind: keymanager.Local,
+				WalletPassword: "existing",
+			}),
+		},
+		{
+			name: "Existing Wallet",
+			args: args{
+				cliCtx: func() *cli.Context {
+					w := wallet.New(&wallet.Config{
+						WalletDir:      walletDir,
+						KeymanagerKind: keymanager.Local,
+						WalletPassword: "existing",
+					})
+					require.NoError(t, w.SaveWallet())
+					assert.NoError(t, set.Set(flags.WalletDirFlag.Name, walletDir))
+					assert.NoError(t, set.Set(flags.KeymanagerKindFlag.Name, keymanager.Local.String()))
+					assert.NoError(t, set.Set(flags.WalletPasswordFileFlag.Name, passwordFilePath))
+					return cli.NewContext(&app, set, nil)
+				}(),
+			},
+			want: wallet.New(&wallet.Config{
+				WalletDir:      walletDir,
+				KeymanagerKind: keymanager.Local,
+				WalletPassword: "existing",
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := wallet.OpenOrCreateNewWallet(tt.args.cliCtx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("OpenOrCreateNewWallet() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OpenOrCreateNewWallet() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
