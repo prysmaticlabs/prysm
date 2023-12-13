824ab4591bb484de371d0da7b7df831e521a7930
terence tsao <terence@prysmaticlabs.com>
2023-12-14 02:26:11 +0800

    Added start log

--- beacon-chain/db/filesystem/blob.go
+++ beacon-chain/db/filesystem/blob.go
@@ -232,6 +232,8 @@ func (p blobNamer) path() string {
 // It deletes blobs older than currentEpoch - (retentionEpochs+bufferEpochs).
 // This is so that we keep a slight buffer and blobs are deleted after n+2 epochs.
 func (bs *BlobStorage) Prune(currentSlot primitives.Slot) error {
+	log.Debug("Pruning blobs")
+
 	t := time.Now()
 	retentionSlots, err := slots.EpochStart(bs.retentionEpochs + bufferEpochs)
 	if err != nil {
