787389313c573093f4378cc427d18b8775006720
Manu NALEPA <enalepa@offchainlabs.com>
2023-12-17 23:11:43 +0100

    `importSlashingProtectionJSON`: Check database type.

--- cmd/validator/slashing-protection/slashing-protection.go
+++ cmd/validator/slashing-protection/slashing-protection.go
@@ -53,6 +53,7 @@ var Commands = &cli.Command{
 				features.PraterTestnet,
 				features.SepoliaTestnet,
 				features.HoleskyTestnet,
+				features.EnableMinimalSlashingProtection,
 				cmd.AcceptTosFlag,
 			}),
 			Before: func(cliCtx *cli.Context) error {
