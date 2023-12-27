b0e7a7b54ff39ea4245c3d694745b2e245425c17
rkapka <rkapka@wp.pl>
2023-12-27 16:37:18 +0100

    Add Inactivity field ro attestation rewards

--- beacon-chain/rpc/eth/rewards/structs.go
+++ beacon-chain/rpc/eth/rewards/structs.go
@@ -31,6 +31,7 @@ type IdealAttestationReward struct {
 	Head             string `json:"head"`
 	Target           string `json:"target"`
 	Source           string `json:"source"`
+	Inactivity       string `json:"inactivity"`
 }
 
 type TotalAttestationReward struct {
@@ -38,7 +39,7 @@ type TotalAttestationReward struct {
 	Head           string `json:"head"`
 	Target         string `json:"target"`
 	Source         string `json:"source"`
-	InclusionDelay string `json:"inclusion_delay"`
+	Inactivity     string `json:"inactivity"`
 }
 
 type SyncCommitteeRewardsResponse struct {
