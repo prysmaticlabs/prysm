5d3663ef8d52d30c318f237594cda1e6f37df210
Nishant Das <nishdas93@gmail.com>
2024-01-19 11:46:36 +0800

    update lighthouse and tests (#13470)

--- testing/endtoend/mainnet_scenario_e2e_test.go
+++ testing/endtoend/mainnet_scenario_e2e_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestEndToEnd_MultiScenarioRun_Multiclient(t *testing.T) {
-	runner := e2eMainnet(t, false, true, types.InitForkCfg(version.Phase0, version.Capella, params.E2EMainnetTestConfig()), types.WithEpochs(24))
+	runner := e2eMainnet(t, false, true, types.InitForkCfg(version.Phase0, version.Deneb, params.E2EMainnetTestConfig()), types.WithEpochs(24))
 	runner.config.Evaluators = scenarioEvalsMulti()
 	runner.config.EvalInterceptor = runner.multiScenarioMulticlient
 	runner.scenarioRunner()
