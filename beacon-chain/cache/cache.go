2aedf1d156b3862d24b3f06fb9df467217aa268a
Joel Rousseau <rousseau.joel@gmail.com>
2024-03-23 17:49:31 +0900

    define cache interface based on hashicorp v2

---
+++ beacon-chain/cache/cache.go
@@ -0,0 +1,38 @@
+package cache
+
+import (
+	"github.com/hashicorp/golang-lru/v2"
+)
+
+type Cache[K comparable, V any] interface {
+	get() *lru.Cache[K, V]
+	hitCache()
+	missCache()
+}
+
+func Get[K comparable, V any](c Cache[K, V], key K) (V, error) {
+	value, ok := c.get().Get(key)
+	if !ok {
+		c.missCache()
+		var zero V
+		return zero, ErrNotFound
+	}
+	c.hitCache()
+	return value, nil
+}
+
+func Add[K comparable, V any](c Cache[K, V], key K, value V) {
+	c.get().Add(key, value)
+}
+
+func Keys[K comparable, V any](c Cache[K, V]) []K {
+	return c.get().Keys()
+}
+
+func Remove[K comparable, V any](c Cache[K, V], key K) {
+	c.get().Remove(key)
+}
+
+func Purge[K comparable, V any](c Cache[K, V]) {
+	c.get().Purge()
+}
