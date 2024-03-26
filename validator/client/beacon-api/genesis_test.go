6de7df6b9d3216a5ac91e49ace522ed1a928fdb6
Radosław Kapka <rkapka@wp.pl>
2024-03-26 12:26:34 +0900

    Get genesis only once (#13796)

--- validator/client/beacon-api/genesis_test.go
+++ validator/client/beacon-api/genesis_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/v5/api/server/structs"
 	"github.com/prysmaticlabs/prysm/v5/testing/assert"
 	"github.com/prysmaticlabs/prysm/v5/testing/require"
@@ -65,4 +66,79 @@ func TestGetGenesis_NilData(t *testing.T) {
 	genesisProvider := &beaconApiGenesisProvider{jsonRestHandler: jsonRestHandler}
 	_, err := genesisProvider.GetGenesis(ctx)
 	assert.ErrorContains(t, "genesis data is nil", err)
+}
+
+func TestGetGenesis_EndpointCalledOnlyOnce(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	ctx := context.Background()
+
+	genesisResponseJson := structs.GetGenesisResponse{}
+	jsonRestHandler := mock.NewMockJsonRestHandler(ctrl)
+	jsonRestHandler.EXPECT().Get(
+		ctx,
+		"/eth/v1/beacon/genesis",
+		&genesisResponseJson,
+	).Return(
+		nil,
+	).SetArg(
+		2,
+		structs.GetGenesisResponse{
+			Data: &structs.Genesis{
+				GenesisTime:           "1234",
+				GenesisValidatorsRoot: "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+			},
+		},
+	).Times(1)
+
+	genesisProvider := &beaconApiGenesisProvider{jsonRestHandler: jsonRestHandler}
+	_, err := genesisProvider.GetGenesis(ctx)
+	assert.NoError(t, err)
+	resp, err := genesisProvider.GetGenesis(ctx)
+	assert.NoError(t, err)
+	require.NotNil(t, resp)
+	assert.Equal(t, "1234", resp.GenesisTime)
+	assert.Equal(t, "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2", resp.GenesisValidatorsRoot)
+}
+
+func TestGetGenesis_EndpointCanBeCalledAgainAfterError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	ctx := context.Background()
+
+	genesisResponseJson := structs.GetGenesisResponse{}
+	jsonRestHandler := mock.NewMockJsonRestHandler(ctrl)
+	jsonRestHandler.EXPECT().Get(
+		ctx,
+		"/eth/v1/beacon/genesis",
+		&genesisResponseJson,
+	).Return(
+		errors.New("foo"),
+	).Times(1)
+	jsonRestHandler.EXPECT().Get(
+		ctx,
+		"/eth/v1/beacon/genesis",
+		&genesisResponseJson,
+	).Return(
+		nil,
+	).SetArg(
+		2,
+		structs.GetGenesisResponse{
+			Data: &structs.Genesis{
+				GenesisTime:           "1234",
+				GenesisValidatorsRoot: "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+			},
+		},
+	).Times(1)
+
+	genesisProvider := &beaconApiGenesisProvider{jsonRestHandler: jsonRestHandler}
+	_, err := genesisProvider.GetGenesis(ctx)
+	require.ErrorContains(t, "foo", err)
+	resp, err := genesisProvider.GetGenesis(ctx)
+	assert.NoError(t, err)
+	require.NotNil(t, resp)
+	assert.Equal(t, "1234", resp.GenesisTime)
+	assert.Equal(t, "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2", resp.GenesisValidatorsRoot)
 }
