62a1ff96e245124ef27b27b078658f406810ac21
Joel Rousseau <rousseau.joel@gmail.com>
2024-03-27 15:03:40 +0900

    reactivate fuzz tests with the low level exception that we do not allow key -> nil(value)

--- beacon-chain/cache/committee_fuzz_test.go
+++ beacon-chain/cache/committee_fuzz_test.go
@@ -3,6 +3,8 @@
 package cache
 
 import (
+	"context"
+	"errors"
 	"testing"
 
 	fuzz "github.com/google/gofuzz"
@@ -22,34 +24,44 @@ func TestCommitteeKeyFuzz_OK(t *testing.T) {
 	}
 }
 
-//func TestCommitteeCache_FuzzCommitteesByEpoch(t *testing.T) {
-//	cache := NewCommitteesCache()
-//	fuzzer := fuzz.NewWithSeed(0)
-//	c := &Committees{}
-//
-//	for i := 0; i < 100000; i++ {
-//		fuzzer.Fuzz(c)
-//		require.NoError(t, cache.AddCommitteeShuffledList(context.Background(), c))
-//		_, err := cache.Committee(context.Background(), 0, c.Seed, 0)
-//		require.NoError(t, err)
-//	}
-//
-//	assert.Equal(t, maxCommitteesCacheSize, len(cache.CommitteeCache.Keys()), "Incorrect key size")
-//}
-//
-//func TestCommitteeCache_FuzzActiveIndices(t *testing.T) {
-//	cache := NewCommitteesCache()
-//	fuzzer := fuzz.NewWithSeed(0)
-//	c := &Committees{}
-//
-//	for i := 0; i < 100000; i++ {
-//		fuzzer.Fuzz(c)
-//		require.NoError(t, cache.AddCommitteeShuffledList(context.Background(), c))
-//
-//		indices, err := cache.ActiveIndices(context.Background(), c.Seed)
-//		require.NoError(t, err)
-//		assert.DeepEqual(t, c.SortedIndices, indices)
-//	}
-//
-//	assert.Equal(t, maxCommitteesCacheSize, len(cache.CommitteeCache.Keys()), "Incorrect key size")
-//}
+func TestCommitteeCache_FuzzCommitteesByEpoch(t *testing.T) {
+	cache, err := NewCommitteesCache()
+	require.NoError(t, err)
+
+	fuzzer := fuzz.NewWithSeed(0)
+	c := &Committees{}
+
+	for i := 0; i < 100000; i++ {
+		fuzzer.Fuzz(c)
+		err := cache.AddCommitteeShuffledList(context.Background(), c)
+		if err != nil {
+			require.Equal(t, false, !errors.Is(err, ErrNilValueProvided))
+		}
+		_, err = cache.Committee(context.Background(), 0, c.Seed, 0)
+		require.NoError(t, err)
+	}
+
+	assert.Equal(t, maxCommitteesCacheSize, len(keys[string, Committees](cache)), "Incorrect key size")
+}
+
+func TestCommitteeCache_FuzzActiveIndices(t *testing.T) {
+	cache, err := NewCommitteesCache()
+	require.NoError(t, err)
+	fuzzer := fuzz.NewWithSeed(0)
+	c := &Committees{}
+
+	for i := 0; i < 100000; i++ {
+		fuzzer.Fuzz(c)
+		err := cache.AddCommitteeShuffledList(context.Background(), c)
+		if err != nil {
+			require.Equal(t, false, !errors.Is(err, ErrNilValueProvided))
+			continue
+		}
+
+		indices, err := cache.ActiveIndices(context.Background(), c.Seed)
+		require.NoError(t, err)
+		assert.DeepEqual(t, c.SortedIndices, indices)
+	}
+
+	assert.Equal(t, maxCommitteesCacheSize, len(keys[string, Committees](cache)), "Incorrect key size")
+}
