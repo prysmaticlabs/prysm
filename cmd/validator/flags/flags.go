2b9eda695c43f87fcd86ede6c188b46430e3697a
james-prysm <james@prysmaticlabs.com>
2024-03-28 15:25:44 -0500

    updating to follow updates from https://github.com/ethereum/keymanager-APIs/pull/74

--- cmd/validator/flags/flags.go
+++ cmd/validator/flags/flags.go
@@ -137,9 +137,10 @@ var (
 
 	// AuthTokenPathFlag defines the path to the auth token used to secure the validator api.
 	AuthTokenPathFlag = &cli.StringFlag{
-		Name:  "token-file",
-		Usage: "Path to auth token file used for validator apis",
-		Value: filepath.Join(filepath.Join(DefaultValidatorDir(), WalletDefaultDirName), api.AuthTokenFileName),
+		Name:    "keymanager-token-file",
+		Usage:   "Path to auth token file used for validator apis",
+		Value:   filepath.Join(filepath.Join(DefaultValidatorDir(), WalletDefaultDirName), api.AuthTokenFileName),
+		Aliases: []string{"validator-api-bearer-file"},
 	}
 
 	// WalletDirFlag defines the path to a wallet directory for Prysm accounts.
