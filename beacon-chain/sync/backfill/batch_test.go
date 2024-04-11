c0acb7d35269fb77f9a4cf759c9ae2863326612e
kasey <489222+kasey@users.noreply.github.com>
2024-04-11 10:22:29 -0500

    Backfill throttling (#13855)
    * add a sleep between retries as a simple throttle
    
    * unit test
    
    * deepsource
    
    ---------
    
    Co-authored-by: Kasey Kirkham <kasey@users.noreply.github.com>

--- beacon-chain/sync/backfill/batch_test.go
+++ beacon-chain/sync/backfill/batch_test.go
@@ -1,8 +1,11 @@
 package backfill
 
 import (
+	"context"
 	"testing"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/v5/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v5/testing/require"
 )
@@ -18,4 +21,23 @@ func TestSortBatchDesc(t *testing.T) {
 	for i := range orderOut {
 		require.Equal(t, orderOut[i], batches[i].end)
 	}
+}
+
+func TestWaitUntilReady(t *testing.T) {
+	b := batch{}.withState(batchErrRetryable)
+	require.Equal(t, time.Time{}, b.retryAfter)
+	var got time.Duration
+	wur := batchBlockUntil
+	var errDerp = errors.New("derp")
+	batchBlockUntil = func(_ context.Context, ur time.Duration, _ batch) error {
+		got = ur
+		return errDerp
+	}
+	// retries counter and timestamp are set when we mark the batch for sequencing, if it is in the retry state
+	b = b.withState(batchSequenced)
+	require.ErrorIs(t, b.waitUntilReady(context.Background()), errDerp)
+	require.Equal(t, true, retryDelay-time.Until(b.retryAfter) < time.Millisecond)
+	require.Equal(t, true, got < retryDelay && got > retryDelay-time.Millisecond)
+	require.Equal(t, 1, b.retries)
+	batchBlockUntil = wur
 }
