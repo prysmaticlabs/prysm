d491c22858b6055c1aa016d25f6b6bb2691b21bb
nisdas <nishdas93@gmail.com>
2024-04-23 17:30:11 +0800

    Add RPC Handler

--- beacon-chain/db/filesystem/blob.go
+++ beacon-chain/db/filesystem/blob.go
@@ -242,6 +242,21 @@ func (bs *BlobStorage) Get(root [32]byte, idx uint64) (blocks.VerifiedROBlob, er
 	return verification.BlobSidecarNoop(ro)
 }
 
+// GetColumn retrieves a single BlobSidecar by its root and index.
+func (bs *BlobStorage) GetColumn(root [32]byte, idx uint64) (*ethpb.DataColumnSidecar, error) {
+	expected := blobNamer{root: root, index: idx}
+	encoded, err := afero.ReadFile(bs.fs, expected.path())
+	var v *ethpb.DataColumnSidecar
+	if err != nil {
+		return v, err
+	}
+	s := &ethpb.DataColumnSidecar{}
+	if err := s.UnmarshalSSZ(encoded); err != nil {
+		return v, err
+	}
+	return s, nil
+}
+
 // Remove removes all blobs for a given root.
 func (bs *BlobStorage) Remove(root [32]byte) error {
 	rootDir := blobNamer{root: root}.dir()
