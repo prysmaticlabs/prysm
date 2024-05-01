cb2d4c61b6c394920271d64325419ac0ea79037b
Preston Van Loon <preston@pvl.dev>
2024-05-01 15:54:39 -0500

    Split eip_7251_root.go into different files and reuse/share code with historical state summaries root. It's identical!

--- beacon-chain/state/stateutil/pending_partial_withdrawals_root.go
+++ beacon-chain/state/stateutil/pending_partial_withdrawals_root.go
@@ -5,6 +5,6 @@ import (
 	ethpb "github.com/prysmaticlabs/prysm/v5/proto/prysm/v1alpha1"
 )
 
-func HistoricalSummariesRoot(summaries []*ethpb.HistoricalSummary) ([32]byte, error) {
-	return SliceRoot(summaries, fieldparams.HistoricalRootsLength)
+func PendingPartialWithdrawalsRoot(slice []*ethpb.PendingPartialWithdrawal) ([32]byte, error) {
+	return SliceRoot(slice, fieldparams.PendingPartialWithdrawalsLimit)
 }
