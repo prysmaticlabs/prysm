03810d64f7847dc9d965cfa5aec7ded90da56c3d
Manu NALEPA <enalepa@offchainlabs.com>
2024-05-02 17:05:56 +0200

    Spectests (#13940)
    * Update `consensus_spec_version` to `v1.5.0-alpha.1`.
    
    * `CustodyColumns`: Fix and implement spec tests.
    
    * Make deepsource happy.
    
    * `^uint64(0)` => `math.MaxUint64`.
    
    * Fix `TestLoadConfigFile` test.

--- config/params/mainnet_config.go
+++ config/params/mainnet_config.go
@@ -297,6 +297,7 @@ var mainnetBeaconConfig = &BeaconChainConfig{
 	SamplesPerSlot:                        8,
 	CustodyRequirement:                    1,
 	MinEpochsForDataColumnSidecarsRequest: 4096,
+	MaxCellsInExtendMatrix:                768,
 
 	// Values related to networking parameters.
 	GossipMaxSize:                   10 * 1 << 20, // 10 MiB
