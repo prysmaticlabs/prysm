5b253d4e39941b328278e323cffb8a7800a599fc
james-prysm <james@prysmaticlabs.com>
2024-05-02 10:46:50 -0500

    adding tests

--- beacon-chain/state/interfaces.go
+++ beacon-chain/state/interfaces.go
@@ -209,6 +209,7 @@ type ReadOnlySyncCommittee interface {
 type ReadOnlyElectra interface {
 	ConsolidationBalanceToConsume() (uint64, error)
 	DepositBalanceToConsume() (uint64, error)
+	DepositReceiptsStartIndex() (uint64, error)
 	EarliestConsolidationEpoch() (primitives.Epoch, error)
 	PendingBalanceDeposits() ([]*ethpb.PendingBalanceDeposit, error)
 	PendingConsolidations() ([]*ethpb.PendingConsolidation, error)
@@ -305,6 +306,7 @@ type WriteOnlyPendingBalanceDeposits interface {
 
 type WriteOnlyElectra interface {
 	SetConsolidationBalanceToConsume(gwei uint64) error
+	SetDepositReceiptsStartIndex(index uint64) error
 	SetEarliestConsolidationEpoch(epoch primitives.Epoch) error
 	SetPendingBalanceDeposits(val []*ethpb.PendingBalanceDeposit) error
 	SetDepositBalanceToConsume(gwei uint64) error
