71ab24806bb16535be93c7f6117622009526e788
Kasey Kirkham <kasey@users.noreply.github.com>
2024-05-07 17:02:31 -0500

    rm cruft

--- consensus-types/experiment/generic_test.go
+++
@@ -1,9 +0,0 @@
-package experiment
-
-import "testing"
-
-func TestProcessAltair(t *testing.T) {
-	body := &AltairBody{}
-	block := &Block[*AltairBody]{body: body}
-	processAltair[BlockHasBody[*AltairBody], *AltairBody](block)
-}
