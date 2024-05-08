d9afce38610e4212586e63a0289e46d9215f18dd
jsvisa <delweng@gmail.com>
2024-05-08 03:18:42 +0000

    config/params: move SetActiveTestCleanup to develop only
    Signed-off-by: jsvisa <delweng@gmail.com>

--- config/params/testutils.go
+++ config/params/testutils.go
@@ -23,18 +23,3 @@ func SetupTestConfigCleanup(t testing.TB) {
 		networkConfig = prevNetworkCfg
 	})
 }
-
-// SetActiveTestCleanup sets an active config,
-// and adds a test cleanup hook to revert to the default config after the test completes.
-func SetActiveTestCleanup(t *testing.T, cfg *BeaconChainConfig) {
-	undo, err := SetActiveWithUndo(cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Cleanup(func() {
-		err = undo()
-		if err != nil {
-			t.Fatal(err)
-		}
-	})
-}
--- config/params/testutils_develop.go
+++ config/params/testutils_develop.go
@@ -30,3 +30,18 @@ func SetupTestConfigCleanup(t testing.TB) {
 		networkConfigLock.Unlock()
 	})
 }
+
+// SetActiveTestCleanup sets an active config,
+// and adds a test cleanup hook to revert to the default config after the test completes.
+func SetActiveTestCleanup(t *testing.T, cfg *BeaconChainConfig) {
+	undo, err := SetActiveWithUndo(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		err = undo()
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+}
