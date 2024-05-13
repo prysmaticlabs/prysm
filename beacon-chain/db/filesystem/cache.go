fd7bea5d8e8975e3886289ca82d13153c10a2480
Nishant Das <nishdas93@gmail.com>
2024-05-13 16:08:39 +0800

    Enable E2E For PeerDAS (#13945)
    * Enable E2E And Add Fixes
    
    * Register Same Topic For Data Columns
    
    * Initialize Capacity Of Slice
    
    * Fix Initialization of Data Column Receiver
    
    * Remove Mix In From Merkle Proof
    
    * E2E: Subscribe to all subnets.
    
    * Remove Index Check
    
    * Remaining Bug Fixes to Get It Working
    
    * Change Evaluator to Allow Test to Finish
    
    * Fix Build
    
    * Add Data Column Verification
    
    * Fix LoopVar Bug
    
    * Do Not Allocate Memory
    
    * Update beacon-chain/blockchain/process_block.go
    
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>
    
    * Update beacon-chain/core/peerdas/helpers.go
    
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>
    
    * Update beacon-chain/core/peerdas/helpers.go
    
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>
    
    * Gofmt
    
    * Fix It Again
    
    * Fix Test Setup
    
    * Fix Build
    
    * Fix Trusted Setup panic
    
    * Fix Trusted Setup panic
    
    * Use New Test
    
    ---------
    
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>

--- beacon-chain/db/filesystem/cache.go
+++ beacon-chain/db/filesystem/cache.go
@@ -9,7 +9,7 @@ import (
 )
 
 // blobIndexMask is a bitmask representing the set of blob indices that are currently set.
-type blobIndexMask [fieldparams.MaxBlobsPerBlock]bool
+type blobIndexMask [fieldparams.NumberOfColumns]bool
 
 // BlobStorageSummary represents cached information about the BlobSidecars on disk for each root the cache knows about.
 type BlobStorageSummary struct {
@@ -69,9 +69,12 @@ func (s *blobStorageCache) Summary(root [32]byte) BlobStorageSummary {
 }
 
 func (s *blobStorageCache) ensure(key string, slot primitives.Slot, idx uint64) error {
-	if idx >= fieldparams.MaxBlobsPerBlock {
-		return errIndexOutOfBounds
-	}
+	// TODO: Separate blob index checks from data column index checks
+	/*
+		if idx >= fieldparams.MaxBlobsPerBlock {
+			return errIndexOutOfBounds
+		}
+	*/
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	v := s.cache[key]
