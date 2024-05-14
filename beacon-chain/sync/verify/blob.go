f03726f7b30eeb483f9dc836bc10ce1b0d93c9c4
Nishant Das <nishdas93@gmail.com>
2024-05-15 00:01:56 +0800

    Implement Data Columns By Range Request And Response Methods (#13972)
    * Add Data Structure for New Request Type
    
    * Add Data Column By Range Handler
    
    * Add Data Column Request Methods
    
    * Add new validation for columns by range requests
    
    * Fix Build
    
    * Allow Prysm Node To Fetch Data Columns
    
    * Allow Prysm Node To Fetch Data Columns And Sync
    
    * Bug Fixes For Interop
    
    * GoFmt
    
    * Use different var
    
    * Manu's Review

--- beacon-chain/sync/verify/blob.go
+++ beacon-chain/sync/verify/blob.go
@@ -1,6 +1,8 @@
 package verify
 
 import (
+	"reflect"
+
 	"github.com/pkg/errors"
 	fieldparams "github.com/prysmaticlabs/prysm/v5/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/v5/consensus-types/blocks"
@@ -9,10 +11,17 @@ import (
 )
 
 var (
-	errBlobVerification          = errors.New("unable to verify blobs")
-	ErrIncorrectBlobIndex        = errors.New("incorrect blob index")
-	ErrBlobBlockMisaligned       = errors.Wrap(errBlobVerification, "root of block header in blob sidecar does not match block root")
-	ErrMismatchedBlobCommitments = errors.Wrap(errBlobVerification, "commitments at given slot, root and index do not match")
+	errBlobVerification   = errors.New("unable to verify blobs")
+	errColumnVerification = errors.New("unable to verify column")
+
+	ErrIncorrectBlobIndex   = errors.New("incorrect blob index")
+	ErrIncorrectColumnIndex = errors.New("incorrect column index")
+
+	ErrBlobBlockMisaligned   = errors.Wrap(errBlobVerification, "root of block header in blob sidecar does not match block root")
+	ErrColumnBlockMisaligned = errors.Wrap(errColumnVerification, "root of block header in column sidecar does not match block root")
+
+	ErrMismatchedBlobCommitments   = errors.Wrap(errBlobVerification, "commitments at given slot, root and index do not match")
+	ErrMismatchedColumnCommitments = errors.Wrap(errColumnVerification, "commitments at given slot, root and index do not match")
 )
 
 // BlobAlignsWithBlock verifies if the blob aligns with the block.
@@ -38,6 +47,29 @@ func BlobAlignsWithBlock(blob blocks.ROBlob, block blocks.ROBlock) error {
 	blobCommitment := bytesutil.ToBytes48(blob.KzgCommitment)
 	if blobCommitment != blockCommitment {
 		return errors.Wrapf(ErrMismatchedBlobCommitments, "commitment %#x != block commitment %#x, at index %d for block root %#x at slot %d ", blobCommitment, blockCommitment, blob.Index, block.Root(), blob.Slot())
+	}
+	return nil
+}
+
+func ColumnAlignsWithBlock(col blocks.RODataColumn, block blocks.ROBlock) error {
+	if block.Version() < version.Deneb {
+		return nil
+	}
+	if col.ColumnIndex >= fieldparams.NumberOfColumns {
+		return errors.Wrapf(ErrIncorrectColumnIndex, "index %d exceeds NUMBERS_OF_COLUMN %d", col.ColumnIndex, fieldparams.NumberOfColumns)
+	}
+
+	if col.BlockRoot() != block.Root() {
+		return ErrColumnBlockMisaligned
+	}
+
+	// Verify commitment byte values match
+	commits, err := block.Block().Body().BlobKzgCommitments()
+	if err != nil {
+		return err
+	}
+	if !reflect.DeepEqual(commits, col.KzgCommitments) {
+		return errors.Wrapf(ErrMismatchedColumnCommitments, "commitment %#v != block commitment %#v for block root %#x at slot %d ", col.KzgCommitments, commits, block.Root(), col.Slot())
 	}
 	return nil
 }
