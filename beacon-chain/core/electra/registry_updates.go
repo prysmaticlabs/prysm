74e30000b1bfbf187b6fab5cc736c34bd4f75fd2
Preston Van Loon <preston@pvl.dev>
2024-05-16 03:22:52 -0500

    Fix spectest violation

--- beacon-chain/core/electra/registry_updates.go
+++ beacon-chain/core/electra/registry_updates.go
@@ -16,23 +16,23 @@ import (
 //
 // Spec pseudocode definition:
 //
-//	def process_registry_updates(state: BeaconState) -> None:
-//	    # Process activation eligibility and ejections
-//	    for index, validator in enumerate(state.validators):
-//	        if is_eligible_for_activation_queue(validator):
-//	            validator.activation_eligibility_epoch = get_current_epoch(state) + 1
+//		def process_registry_updates(state: BeaconState) -> None:
+//		    # Process activation eligibility and ejections
+//		    for index, validator in enumerate(state.validators):
+//		        if is_eligible_for_activation_queue(validator):
+//		            validator.activation_eligibility_epoch = get_current_epoch(state) + 1
 //
-//	        if (
-//	            is_active_validator(validator, get_current_epoch(state))
-//	            and validator.effective_balance <= EJECTION_BALANCE
-//	        ):
-//	            initiate_validator_exit(state, ValidatorIndex(index))
+//		        if (
+//		            is_active_validator(validator, get_current_epoch(state))
+//		            and validator.effective_balance <= EJECTION_BALANCE
+//		        ):
+//		            initiate_validator_exit(state, ValidatorIndex(index))
 //
-//	    # Activate all eligible validators
-//	    activation_epoch = compute_activation_exit_epoch(get_current_epoch(state))
-//	    for validator in state.validators:
-//	        if is_eligible_for_activation(state, validator):
-//	            validator.activation_epoch = activation_epoch
+//	         # Activate all eligible validators
+//	         activation_epoch = compute_activation_exit_epoch(get_current_epoch(state))
+//	         for validator in state.validators:
+//	             if is_eligible_for_activation(state, validator):
+//	                 validator.activation_epoch = activation_epoch
 func ProcessRegistryUpdates(ctx context.Context, state state.BeaconState) (state.BeaconState, error) {
 	currentEpoch := time.CurrentEpoch(state)
 	ejectionBal := params.BeaconConfig().EjectionBalance
@@ -41,6 +41,9 @@ func ProcessRegistryUpdates(ctx context.Context, state state.BeaconState) (state
 	for idx, val := range vals {
 		if helpers.IsEligibleForActivationQueue(val, currentEpoch) {
 			val.ActivationEligibilityEpoch = currentEpoch + 1
+			if err := state.UpdateValidatorAtIndex(primitives.ValidatorIndex(idx), val); err != nil {
+				return nil, err
+			}
 		}
 		if helpers.IsActiveValidator(val, currentEpoch) && val.EffectiveBalance <= ejectionBal {
 			var err error
--- testing/spectest/mainnet/electra/epoch_processing/registry_updates_test.go
+++ testing/spectest/mainnet/electra/epoch_processing/registry_updates_test.go
@@ -6,6 +6,6 @@ import (
 	"github.com/prysmaticlabs/prysm/v5/testing/spectest/shared/electra/epoch_processing"
 )
 
-func TestMainnet_Electra_EpochProcessing_ResetRegistryUpdates(t *testing.T) {
+func TestMainnet_Electra_EpochProcessing_RegistryUpdates(t *testing.T) {
 	epoch_processing.RunRegistryUpdatesTests(t, "mainnet")
 }
