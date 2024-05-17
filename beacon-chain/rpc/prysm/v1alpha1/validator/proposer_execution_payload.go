7ce104371d2e1c253908feb6b4a5ddc5f7f39ea6
nisdas <nishdas93@gmail.com>
2024-05-17 18:34:04 +0800

    Remove

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer_execution_payload.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer_execution_payload.go
@@ -58,13 +58,6 @@ func (vs *Server) getLocalPayload(ctx context.Context, blk interfaces.ReadOnlyBe
 	slot := blk.Slot()
 	vIdx := blk.ProposerIndex()
 	headRoot := blk.ParentRoot()
-	var err error
-	if headRoot == [32]byte{} {
-		headRoot, err = vs.BeaconDB.GenesisBlockRoot(ctx)
-		if err != nil {
-			return nil, false, err
-		}
-	}
 	logFields := logrus.Fields{
 		"validatorIndex": vIdx,
 		"slot":           slot,
