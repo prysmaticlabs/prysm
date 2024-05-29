687e1644700560aef8a7b2ef3cc8977e4fe99c26
Manu NALEPA <enalepa@offchainlabs.com>
2024-05-29 10:03:21 +0200

    PeerDAS: Implement reconstruction. (#14036)
    * Wrap errors, add logs.
    
    * `missingColumnRequest`: Fix blobs <-> data columns mix.
    
    * `ColumnIndices`: Return `map[uint64]bool` instead of `[fieldparams.NumberOfColumns]bool`.
    
    * `DataColumnSidecars`: `interfaces.SignedBeaconBlock` ==> `interfaces.ReadOnlySignedBeaconBlock`.
    
    We don't need any of the non read-only methods.
    
    * Fix comments.
    
    * `handleUnblidedBlock` ==> `handleUnblindedBlock`.
    
    * `SaveDataColumn`: Move log from debug to trace.
    
    If we attempt to save an already existing data column sidecar,
    a debug log was printed.
    
    This case could be quite common now with the data column reconstruction enabled.
    
    * `sampling_data_columns.go` --> `data_columns_sampling.go`.
    
    * Reconstruct data columns.

--- beacon-chain/p2p/testing/fuzz_p2p.go
+++ beacon-chain/p2p/testing/fuzz_p2p.go
@@ -184,8 +184,8 @@ func (_ *FakeP2P) InterceptUpgraded(network.Conn) (allow bool, reason control.Di
 	return true, 0
 }
 
-func (_ *FakeP2P) CustodyCountFromRemotePeer(peer.ID) (uint64, error) {
-	return 0, nil
+func (_ *FakeP2P) CustodyCountFromRemotePeer(peer.ID) uint64 {
+	return 0
 }
 
 func (_ *FakeP2P) GetValidCustodyPeers(peers []peer.ID) ([]peer.ID, error) {
