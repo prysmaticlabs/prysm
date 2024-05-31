c021a873f24ff955fb15c71b64858a6a4c8380b1
james-prysm <james@prysmaticlabs.com>
2024-05-31 15:18:25 -0500

    gofmt

--- cmd/validator/usage.go
+++ cmd/validator/usage.go
@@ -127,7 +127,7 @@ var appHelpFlagGroups = []flagGroup{
 		Flags: []cli.Flag{
 			flags.Web3SignerURLFlag,
 			flags.Web3SignerPublicValidatorKeysFlag,
-      flags.Web3SignerKeyFileFlag,
+			flags.Web3SignerKeyFileFlag,
 		},
 	},
 	{
