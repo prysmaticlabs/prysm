586eaf2e1b694d07d03afda475c4b7efc8b9efb8
terence <terence@prysmaticlabs.com>
2024-05-31 08:51:33 -0700

    Radek's suggestion
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- hack/update-mockgen.sh
+++ hack/update-mockgen.sh
@@ -27,7 +27,7 @@ done
 # github.com/prysmaticlabs/prysm/v5/validator/client/iface
 # --------------------------------------------------------
 iface_mocks=(
-      "$iface_mock_path/beacon_chain_client_mock.go ChainClient"
+      "$iface_mock_path/chain_client_mock.go ChainClient"
       "$iface_mock_path/node_client_mock.go NodeClient"
       "$iface_mock_path/validator_client_mock.go ValidatorClient"
 )
