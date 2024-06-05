0362b4a04b36f8b0ac365982d62b810362d54d4b
jsvisa <delweng@gmail.com>
2024-06-05 11:18:53 +0800

    fix(config/paarams): fix the wrong consensus import path
    Signed-off-by: jsvisa <delweng@gmail.com>

--- config/params/network_config.go
+++ config/params/network_config.go
@@ -1,7 +1,7 @@
 package params
 
 import (
-	"github.com/prysmaticlabs/prysm/v4/consensus-types/primitives"
+	"github.com/prysmaticlabs/prysm/v5/consensus-types/primitives"
 )
 
 // NetworkConfig defines the spec based network parameters.
