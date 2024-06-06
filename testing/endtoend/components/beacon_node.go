a6496b940844b5dd3120aeefd7f747e709426692
Manu NALEPA <enalepa@offchainlabs.com>
2024-06-06 12:28:21 +0200

    PeerDAS: Gossip the reconstructed columns (#14079)
    * PeerDAS: Broadcast not seen via gossip but reconstructed data columns.
    
    * Address Nishant's comment.

--- testing/endtoend/components/beacon_node.go
+++ testing/endtoend/components/beacon_node.go
@@ -278,6 +278,7 @@ func (node *BeaconNode) Start(ctx context.Context) error {
 		"--" + flags.EnableDebugRPCEndpoints.Name,
 		"--" + features.EnableQUIC.Name,
 		"--" + flags.SubscribeToAllSubnets.Name,
+		fmt.Sprintf("--%s=%d", features.DataColumnsWithholdCount.Name, 3),
 	}
 	if config.UsePprof {
 		args = append(args, "--pprof", fmt.Sprintf("--pprofport=%d", e2e.TestParams.Ports.PrysmBeaconNodePprofPort+index))
