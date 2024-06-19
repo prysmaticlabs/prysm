4310f7ba94583f21006553ccf1c19528e0b60035
rkapka <radoslaw.kapka@gmail.com>
2024-06-19 16:06:27 +0200

    id comments and tests

--- proto/prysm/v1alpha1/attestation/id.go
+++ proto/prysm/v1alpha1/attestation/id.go
@@ -1,6 +1,7 @@
 package attestation
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/pkg/errors"
@@ -10,51 +11,57 @@ import (
 	"github.com/prysmaticlabs/prysm/v5/runtime/version"
 )
 
-type DigestSource uint8
+// IdSource represents the part of attestation that will be used to generate the Id.
+type IdSource uint8
 
 const (
-	Full DigestSource = iota
+	// Full generates the Id from the whole attestation.
+	Full IdSource = iota
+	// Data generates the Id from the tuple (slot, committee index, beacon block root, source, target).
 	Data
 )
 
+// Id represents an attestation ID. Its uniqueness depends on the IdSource provided when constructing the Id.
 type Id struct {
 	version int
-	digest  [32]byte
+	hash    [32]byte
 }
 
-func NewId(att ethpb.Att, digest DigestSource) (Id, error) {
+// NewId --
+func NewId(att ethpb.Att, source IdSource) (Id, error) {
 	if err := helpers.ValidateNilAttestation(att); err != nil {
 		return Id{}, err
 	}
 
-	switch digest {
+	switch source {
 	case Full:
-		d, err := att.HashTreeRoot()
+		h, err := att.HashTreeRoot()
 		if err != nil {
 			return Id{}, err
 		}
-		return Id{version: att.Version(), digest: d}, nil
+		return Id{version: att.Version(), hash: h}, nil
 	case Data:
 		data := att.GetData()
 		if att.Version() >= version.Electra {
-			cb := att.CommitteeBitsVal().BitIndices()
-			if len(cb) == 0 {
-				return Id{}, errors.New("no committee bits set")
+			committeeIndices := att.CommitteeBitsVal().BitIndices()
+			if len(committeeIndices) != 1 {
+				return Id{}, fmt.Errorf("%d committee bits are set instead of 1", len(committeeIndices))
 			}
 			dataCopy := ethpb.CopyAttestationData(att.GetData())
-			dataCopy.CommitteeIndex = primitives.CommitteeIndex(cb[0])
+			dataCopy.CommitteeIndex = primitives.CommitteeIndex(committeeIndices[0])
 			data = dataCopy
 		}
-		d, err := data.HashTreeRoot()
+		h, err := data.HashTreeRoot()
 		if err != nil {
 			return Id{}, err
 		}
-		return Id{version: att.Version(), digest: d}, nil
+		return Id{version: att.Version(), hash: h}, nil
 	default:
-		return Id{}, errors.New("invalid digest requested")
+		return Id{}, errors.New("invalid source requested")
 	}
 }
 
+// String --
 func (id Id) String() string {
-	return strconv.Itoa(id.version) + string(id.digest[:])
+	return strconv.Itoa(id.version) + string(id.hash[:])
 }
