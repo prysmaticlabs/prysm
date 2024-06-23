5877eaa8d08190ef5f5974f2ea5cda53669653c5
nisdas <nishdas93@gmail.com>
2024-06-23 22:25:58 +0800

    Add ZkSync

--- tools/blob-watcher/main.go
+++ tools/blob-watcher/main.go
@@ -190,6 +190,7 @@ var accountLabels = map[[20]byte]string{
 	mustDecode("0x5050f69a9786f081509234f1a7f4684b5e5b76c9"): "Base",
 	mustDecode("0x000000633b68f5d8d3a86593ebb815b4663bcbe0"): "Taiko",
 	mustDecode("0x2c169dfe5fbba12957bdd0ba47d9cedbfe260ca7"): "Starknet",
+	mustDecode("0x0D3250c3D5FAcb74Ac15834096397a3Ef790ec99"): "ZkSync",
 	mustDecode("0xcf2898225ed05be911d3709d9417e86e0b4cfc8f"): "Scroll",
 	mustDecode("0x415c8893d514f9bc5211d36eeda4183226b84aa7"): "Blast",
 	mustDecode("0xa9268341831efa4937537bc3e9eb36dbece83c7e"): "Linea",
