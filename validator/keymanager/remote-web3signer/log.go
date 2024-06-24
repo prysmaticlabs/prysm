dd9eefbc9c475885e903c465e546922231c2b9ec
james-prysm <90280386+james-prysm@users.noreply.github.com>
2024-06-24 08:59:34 -0500

    Update validator/keymanager/remote-web3signer/log.go
    Co-authored-by: Manu NALEPA <enalepa@offchainlabs.com>

--- validator/keymanager/remote-web3signer/log.go
+++ validator/keymanager/remote-web3signer/log.go
@@ -2,4 +2,4 @@ package remote_web3signer
 
 import "github.com/sirupsen/logrus"
 
-var log = logrus.WithField("prefix", "remote_keymanager")
+var log = logrus.WithField("prefix", "remote-keymanager")
