bbba4ab902d0dc342124122034203808027769ed
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-28 17:54:41 +0100

    chore: remove code that was doing the flattening outside of the kzg package

--- beacon-chain/sync/data_columns_reconstruct.go
+++ beacon-chain/sync/data_columns_reconstruct.go
@@ -46,7 +46,7 @@ func recoverBlobs(
 		start := time.Now()
 
 		cellsId := make([]uint64, 0, columnsCount)
-		cKzgCells := make([]kzg.Cell, 0, columnsCount)
+		cells := make([]kzg.Cell, 0, columnsCount)
 
 		for _, sidecar := range dataColumnSideCars {
 			// Build the cell ids.
@@ -56,17 +56,11 @@ func recoverBlobs(
 			column := sidecar.DataColumn
 			cell := column[blobIndex]
 
-			// Transform the cell as a cKzg cell.
-			var ckzgCell kzg.Cell
-			for i := 0; i < kzg.FieldElementsPerCell; i++ {
-				copy(ckzgCell[i][:], cell[32*i:32*(i+1)])
-			}
-
-			cKzgCells = append(cKzgCells, ckzgCell)
+			cells = append(cells, kzg.Cell(cell))
 		}
 
 		// Recover the blob.
-		recoveredCells, err := kzg.RecoverAllCells(cellsId, cKzgCells)
+		recoveredCells, err := kzg.RecoverAllCells(cellsId, cells)
 		if err != nil {
 			return nil, errors.Wrapf(err, "recover all cells for blob %d", blobIndex)
 		}
