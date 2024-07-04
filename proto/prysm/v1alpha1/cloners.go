6791dd23673cfb9843f550e4adf38d0670c9a689
terence <terence@prysmaticlabs.com>
2024-07-04 11:39:13 -0700

    Remove inclusion list from epbs (#14188)

--- proto/prysm/v1alpha1/cloners.go
+++ proto/prysm/v1alpha1/cloners.go
@@ -968,6 +968,7 @@ func CopyExecutionPayloadHeaderEPBS(payload *enginev1.ExecutionPayloadHeaderEPBS
 		Slot:                   payload.Slot,
 		Value:                  payload.Value,
 		BlobKzgCommitmentsRoot: bytesutil.SafeCopyBytes(payload.BlobKzgCommitmentsRoot),
+		GasLimit:               payload.GasLimit,
 	}
 }
 
