badcc31e410fcaf634f92b2d504094a14a5bdcad
Potuz <potuz@prysmaticlabs.com>
2024-07-04 09:09:17 -0300

    Add ePBS to state (#13926)

---
+++ beacon-chain/state/state-native/state_trie_epbs_test.go
@@ -0,0 +1,77 @@
+package state_native
+
+import (
+	"context"
+	"testing"
+
+	"github.com/prysmaticlabs/prysm/v5/testing/require"
+	"github.com/prysmaticlabs/prysm/v5/testing/util/random"
+)
+
+func Test_InitializeFromProtoEpbs(t *testing.T) {
+	st := random.BeaconState(t)
+
+	// Cache initial values to check against after initialization.
+	prevInclusionListProposer := st.PreviousInclusionListProposer
+	prevInclusionListSlot := st.PreviousInclusionListSlot
+	latestInclusionListProposer := st.LatestInclusionListProposer
+	latestInclusionListSlot := st.LatestInclusionListSlot
+	latestBlockHash := st.LatestBlockHash
+	latestFullSlot := st.LatestFullSlot
+	header := st.ExecutionPayloadHeader
+	lastWithdrawalsRoot := st.LastWithdrawalsRoot
+
+	s, err := InitializeFromProtoEpbs(st)
+	require.NoError(t, err)
+
+	// Assert that initial values match those in the new state.
+	gotPrevInclusionListProposer := s.PreviousInclusionListProposer()
+	require.Equal(t, prevInclusionListProposer, gotPrevInclusionListProposer)
+	gotPrevInclusionListSlot := s.PreviousInclusionListSlot()
+	require.Equal(t, prevInclusionListSlot, gotPrevInclusionListSlot)
+	gotLatestInclusionListProposer := s.LatestInclusionListProposer()
+	require.Equal(t, latestInclusionListProposer, gotLatestInclusionListProposer)
+	gotLatestInclusionListSlot := s.LatestInclusionListSlot()
+	require.Equal(t, latestInclusionListSlot, gotLatestInclusionListSlot)
+	gotLatestBlockHash := s.LatestBlockHash()
+	require.DeepEqual(t, latestBlockHash, gotLatestBlockHash)
+	gotLatestFullSlot := s.LatestFullSlot()
+	require.Equal(t, latestFullSlot, gotLatestFullSlot)
+	gotHeader := s.ExecutionPayloadHeader()
+	require.DeepEqual(t, header, gotHeader)
+	gotLastWithdrawalsRoot := s.LastWithdrawalsRoot()
+	require.DeepEqual(t, lastWithdrawalsRoot, gotLastWithdrawalsRoot)
+}
+
+func Test_CopyEpbs(t *testing.T) {
+	st := random.BeaconState(t)
+	s, err := InitializeFromProtoUnsafeEpbs(st)
+	require.NoError(t, err)
+
+	// Test shallow copy.
+	sNoCopy := s
+	require.DeepEqual(t, s.executionPayloadHeader, sNoCopy.executionPayloadHeader)
+
+	// Modify a field to check if it reflects in the shallow copy.
+	s.executionPayloadHeader.Slot = 100
+	require.Equal(t, s.executionPayloadHeader, sNoCopy.executionPayloadHeader)
+
+	// Copy the state
+	sCopy := s.Copy()
+	require.NoError(t, err)
+	header := sCopy.ExecutionPayloadHeader()
+	require.DeepEqual(t, s.executionPayloadHeader, header)
+
+	// Modify the original to check if the copied state is independent.
+	s.executionPayloadHeader.Slot = 200
+	require.DeepNotEqual(t, s.executionPayloadHeader, header)
+}
+
+func Test_HashTreeRootEpbs(t *testing.T) {
+	st := random.BeaconState(t)
+	s, err := InitializeFromProtoUnsafeEpbs(st)
+	require.NoError(t, err)
+
+	_, err = s.HashTreeRoot(context.Background())
+	require.NoError(t, err)
+}
