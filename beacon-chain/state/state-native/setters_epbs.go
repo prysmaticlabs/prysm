ae313155741b4833a865e33cf3c2aa6c43816fac
Potuz <potuz@prysmaticlabs.com>
2024-07-15 17:49:20 -0300

    Remove inclusion list from epbs (#14188)

--- beacon-chain/state/state-native/setters_epbs.go
+++ beacon-chain/state/state-native/setters_epbs.go
@@ -16,35 +16,6 @@ func (b *BeaconState) SetExecutionPayloadHeader(h *enginev1.ExecutionPayloadHead
 	b.markFieldAsDirty(types.ExecutionPayloadHeader)
 }
 
-// UpdatePreviousInclusionListData updates the data of previous inclusion list with latest values.
-func (b *BeaconState) UpdatePreviousInclusionListData() {
-	b.lock.Lock()
-	defer b.lock.Unlock()
-
-	b.previousInclusionListProposer = b.latestInclusionListProposer
-	b.previousInclusionListSlot = b.latestInclusionListSlot
-	b.markFieldAsDirty(types.PreviousInclusionListProposer)
-	b.markFieldAsDirty(types.PreviousInclusionListSlot)
-}
-
-// SetLatestInclusionListProposer sets the latest inclusion list proposer for the beacon state.
-func (b *BeaconState) SetLatestInclusionListProposer(i primitives.ValidatorIndex) {
-	b.lock.Lock()
-	defer b.lock.Unlock()
-
-	b.latestInclusionListProposer = i
-	b.markFieldAsDirty(types.LatestInclusionListProposer)
-}
-
-// SetLatestInclusionListSlot sets the latest inclusion list slot for the beacon state.
-func (b *BeaconState) SetLatestInclusionListSlot(s primitives.Slot) {
-	b.lock.Lock()
-	defer b.lock.Unlock()
-
-	b.latestInclusionListSlot = s
-	b.markFieldAsDirty(types.LatestInclusionListSlot)
-}
-
 // SetLatestBlockHash sets the latest block hash for the beacon state.
 func (b *BeaconState) SetLatestBlockHash(h []byte) {
 	b.lock.Lock()
