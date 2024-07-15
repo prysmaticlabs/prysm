f31bc632d77a7c4cd6711c0b37f151d4161c5876
Potuz <potuz@prysmaticlabs.com>
2024-07-15 13:20:06 -0300

    Enable validator client to submit payload attestation message (#14064)

--- validator/client/validator_test.go
+++ validator/client/validator_test.go
@@ -728,6 +728,7 @@ func TestRolesAt_OK(t *testing.T) {
 						AttesterSlot:    1,
 						PublicKey:       validatorKey.PublicKey().Marshal(),
 						IsSyncCommittee: true,
+						PtcSlot:         1,
 					},
 				},
 				NextEpochDuties: []*ethpb.DutiesResponse_Duty{
@@ -736,6 +737,7 @@ func TestRolesAt_OK(t *testing.T) {
 						AttesterSlot:    1,
 						PublicKey:       validatorKey.PublicKey().Marshal(),
 						IsSyncCommittee: true,
+						PtcSlot:         1,
 					},
 				},
 			}
@@ -759,6 +761,7 @@ func TestRolesAt_OK(t *testing.T) {
 			assert.Equal(t, iface.RoleAttester, roleMap[bytesutil.ToBytes48(validatorKey.PublicKey().Marshal())][0])
 			assert.Equal(t, iface.RoleAggregator, roleMap[bytesutil.ToBytes48(validatorKey.PublicKey().Marshal())][1])
 			assert.Equal(t, iface.RoleSyncCommittee, roleMap[bytesutil.ToBytes48(validatorKey.PublicKey().Marshal())][2])
+			assert.Equal(t, iface.RolePayloadTimelinessCommittee, roleMap[bytesutil.ToBytes48(validatorKey.PublicKey().Marshal())][3])
 
 			// Test sync committee role at epoch boundary.
 			v.duties = &ethpb.DutiesResponse{
