79bf03cbbc5e245f4794002accdf2d367acbf053
Manu NALEPA <enalepa@offchainlabs.com>
2024-07-18 14:55:42 +0200

    DeepSource: Pass heavy objects by pointers.

--- beacon-chain/das/availability_columns.go
+++ beacon-chain/das/availability_columns.go
@@ -100,7 +100,7 @@ func (s *LazilyPersistentStoreColumn) IsDataAvailable(ctx context.Context, curre
 	// Verify we have all the expected sidecars, and fail fast if any are missing or inconsistent.
 	// We don't try to salvage problematic batches because this indicates a misbehaving peer and we'd rather
 	// ignore their response and decrease their peer score.
-	sidecars, err := entry.filterColumns(root, blockCommitments)
+	sidecars, err := entry.filterColumns(root, &blockCommitments)
 	if err != nil {
 		return errors.Wrap(err, "incomplete BlobSidecar batch")
 	}
