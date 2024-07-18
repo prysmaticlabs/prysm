d155037508ee024fb179fafae65aa81531e95450
Md Amaan <intellectance022@gmail.com>
2024-07-19 00:39:53 +0530

    Rest tests added

--- consensus-types/payload-attestation/readonly_test.go
+++ consensus-types/payload-attestation/readonly_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	ethpb "github.com/prysmaticlabs/prysm/v5/proto/prysm/v1alpha1"
+	fieldparams "github.com/prysmaticlabs/prysm/v5/config/fieldparams"
 	"github.com/prysmaticlabs/prysm/v5/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v5/testing/assert"
 	"github.com/prysmaticlabs/prysm/v5/testing/require"
@@ -41,6 +42,18 @@ func TestValidatePayload(t *testing.T){
 			},
 			err:  errMissingPayloadSignature,
 		},
+		{
+			name: "Correct PayloadAttestationMessage",
+			bfunc: func(t *testing.T) *ethpb.PayloadAttestationMessage {
+				return &ethpb.PayloadAttestationMessage{
+					Signature: make([]byte, 96),
+					Data: &ethpb.PayloadAttestationData{
+					BeaconBlockRoot: make([]byte, fieldparams.RootLength),
+					},
+				}
+			},
+			err:  nil,
+		},
 	}
 		for _, tt := range tests {
 			t.Run(tt.name+" ReadOnlyPayloadAtt", func(t *testing.T) {
@@ -48,7 +61,7 @@ func TestValidatePayload(t *testing.T){
 				err := validatePayload(m)
 				if tt.err != nil {
 					require.ErrorIs(t, err, tt.err)
-				} else {
+				}else {
 					RoMess,err:=NewReadOnly(m)
 					assert.Equal(t,RoMess.message.Data,m.Data)
 					assert.Equal(t,RoMess.message.Signature,m.Signature)
@@ -68,11 +81,49 @@ func TestValidatorIndex(t *testing.T){
 	assert.Equal(t,ValIdx,m.ValidatorIndex())
 }
 func TestSignature(t *testing.T){
-	Sig:=[]byte{1}
+	Sig:=[96]byte{1}
 	m:=&ReadOnlyPayloadAtt{
 		message: &ethpb.PayloadAttestationMessage{
-		Signature: Sig,
+		Signature: Sig[:],
 		},
 	}
 	assert.Equal(t,Sig,m.Signature())
+}
+func TestBeaconBlockRoot(t *testing.T){
+	root := [32]byte{1}
+	m:=&ReadOnlyPayloadAtt{
+		message: &ethpb.PayloadAttestationMessage{
+			Data: &ethpb.PayloadAttestationData{
+				BeaconBlockRoot: root[:],
+			},
+		},
+	}
+	assert.Equal(t,root,m.BeaconBlockRoot())
+}
+
+func TestSlot(t *testing.T){
+	slot:=primitives.Slot(1)
+	m:=&ReadOnlyPayloadAtt{
+		message: &ethpb.PayloadAttestationMessage{
+			Data: &ethpb.PayloadAttestationData{
+				Slot: slot,
+			},
+		},
+	}
+	assert.Equal(t,slot,m.Slot())
+}
+func TestPayloadStatus(t *testing.T){
+
+	for i:=0;i<4;i++ {
+	status:=primitives.PTCStatus(i)
+	m:=&ReadOnlyPayloadAtt{
+		message: &ethpb.PayloadAttestationMessage{
+			Data: &ethpb.PayloadAttestationData{
+				PayloadStatus:status,
+			},
+		},
+	}
+		require.NoError(t,validatePayload(m.message))
+		assert.Equal(t,status,m.PayloadStatus())
+	}
 }
