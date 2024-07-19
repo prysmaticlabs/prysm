6e9cbcb608e7093300d135ddec14797790f969d2
rupam-04 <rpmdey2004@gmail.com>
2024-07-19 23:32:04 +0530

    feat: add tests for ``IsBetterUpdate``

--- beacon-chain/rpc/eth/light-client/helpers_test.go
+++ beacon-chain/rpc/eth/light-client/helpers_test.go
@@ -3,6 +3,8 @@ package lightclient
 import (
 	"testing"
 
+	"github.com/prysmaticlabs/prysm/v5/beacon-chain/blockchain"
+	fieldparams "github.com/prysmaticlabs/prysm/v5/config/fieldparams"
 	ethpbv1 "github.com/prysmaticlabs/prysm/v5/proto/eth/v1"
 	ethpbv2 "github.com/prysmaticlabs/prysm/v5/proto/eth/v2"
 	"github.com/prysmaticlabs/prysm/v5/testing/assert"
@@ -19,12 +21,12 @@ func TestIsBetterUpdate(t *testing.T) {
 			name: "new has supermajority but old doesn't",
 			oldUpdate: &ethpbv2.LightClientUpdate{
 				SyncAggregate: &ethpbv1.SyncAggregate{
-					SyncCommitteeBits: []byte{0b01111100, 0b1},
+					SyncCommitteeBits: []byte{0b01111100, 0b1}, // [0,0,1,1,1,1,1,0]
 				},
 			},
 			newUpdate: &ethpbv2.LightClientUpdate{
 				SyncAggregate: &ethpbv1.SyncAggregate{
-					SyncCommitteeBits: []byte{0b11111100, 0b1},
+					SyncCommitteeBits: []byte{0b11111100, 0b1}, // [0,0,1,1,1,1,1,1]
 				},
 			},
 			expectedResult: true,
@@ -33,98 +35,398 @@ func TestIsBetterUpdate(t *testing.T) {
 			name: "old has supermajority but new doesn't",
 			oldUpdate: &ethpbv2.LightClientUpdate{
 				SyncAggregate: &ethpbv1.SyncAggregate{
-					SyncCommitteeBits: []byte{0b11111100, 0b1},
+					SyncCommitteeBits: []byte{0b11111100, 0b1}, // [0,0,1,1,1,1,1,1]
 				},
 			},
 			newUpdate: &ethpbv2.LightClientUpdate{
 				SyncAggregate: &ethpbv1.SyncAggregate{
-					SyncCommitteeBits: []byte{0b01111100, 0b1},
+					SyncCommitteeBits: []byte{0b01111100, 0b1}, // [0,0,1,1,1,1,1,0]
 				},
 			},
 			expectedResult: false,
 		},
 		{
-			name:           "new doesn't have supermajority and newNumActiveParticipants is greater than oldNumActiveParticipants",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new doesn't have supermajority and newNumActiveParticipants is greater than oldNumActiveParticipants",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b01111100, 0b1}, // [0,0,1,1,1,1,1,0]
+				},
+			},
 			expectedResult: true,
 		},
 		{
-			name:           "new doesn't have supermajority and newNumActiveParticipants is lesser than oldNumActiveParticipants",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new doesn't have supermajority and newNumActiveParticipants is lesser than oldNumActiveParticipants",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b01111100, 0b1}, // [0,0,1,1,1,1,1,0]
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+			},
 			expectedResult: false,
 		},
 		{
-			name:           "new has relevant sync committee but old doesn't",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new has relevant sync committee but old doesn't",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, fieldparams.NextSyncCommitteeBranchDepth),
+				SignatureSlot:           9999,
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000001,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           1000000,
+			},
 			expectedResult: true,
 		},
 		{
-			name:           "old has relevant sync committee but new doesn't",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "old has relevant sync committee but new doesn't",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000001,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           1000000,
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, fieldparams.NextSyncCommitteeBranchDepth),
+				SignatureSlot:           9999,
+			},
 			expectedResult: false,
 		},
 		{
-			name:           "new has finality but old doesn't",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new has finality but old doesn't",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, blockchain.FinalityBranchNumOfLeaves),
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+			},
 			expectedResult: true,
 		},
 		{
-			name:           "old has finality but new doesn't",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "old has finality but new doesn't",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, blockchain.FinalityBranchNumOfLeaves),
+			},
 			expectedResult: false,
 		},
 		{
-			name:           "new has finality and sync committee finality both but old doesn't have sync committee finality",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new has finality and sync committee finality both but old doesn't have sync committee finality",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           999999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 999999,
+				},
+			},
 			expectedResult: true,
 		},
 		{
-			name:           "new has finality but doesn't have sync committee finality and old has sync committee finality",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new has finality but doesn't have sync committee finality and old has sync committee finality",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           999999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 999999,
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
 			expectedResult: false,
 		},
 		{
-			name:           "new has more active participants than old",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new has more active participants than old",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b01111100, 0b1}, // [0,1,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
 			expectedResult: true,
 		},
 		{
-			name:           "new has less active participants than old",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new has less active participants than old",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b01111100, 0b1}, // [0,1,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
 			expectedResult: false,
 		},
 		{
-			name:           "new's attested header's slot is lesser than old's attested header's slot",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new's attested header's slot is lesser than old's attested header's slot",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 999999,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
 			expectedResult: true,
 		},
 		{
-			name:           "new's attested header's slot is greater than old's attested header's slot",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "new's attested header's slot is greater than old's attested header's slot",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 999999,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
 			expectedResult: false,
 		},
 		{
-			name:           "none of the above conditions are met and new's signature slot is lesser than old's signature slot",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "none of the above conditions are met and new's signature slot is lesser than old's signature slot",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9998,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
 			expectedResult: true,
 		},
 		{
-			name:           "none of the above conditions are met and new's signature slot is greater than old's signature slot",
-			oldUpdate:      &ethpbv2.LightClientUpdate{},
-			newUpdate:      &ethpbv2.LightClientUpdate{},
+			name: "none of the above conditions are met and new's signature slot is greater than old's signature slot",
+			oldUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9998,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
+			newUpdate: &ethpbv2.LightClientUpdate{
+				SyncAggregate: &ethpbv1.SyncAggregate{
+					SyncCommitteeBits: []byte{0b00111100, 0b1}, // [0,0,1,1,1,1,0,0]
+				},
+				AttestedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 1000000,
+				},
+				NextSyncCommitteeBranch: make([][]byte, 4),
+				SignatureSlot:           9999,
+				FinalityBranch:          make([][]byte, 5),
+				FinalizedHeader: &ethpbv1.BeaconBlockHeader{
+					Slot: 9999,
+				},
+			},
 			expectedResult: false,
 		},
 	}
