1b767f6f846f135a27293d9a51fd5edd2b613a7c
james-prysm <james@prysmaticlabs.com>
2024-07-19 11:57:15 -0500

    fixing build

--- proto/engine/v1/execution_engine_fuzz_test.go
+++ proto/engine/v1/execution_engine_fuzz_test.go
@@ -1,18 +1,19 @@
-package enginev1
+package enginev1_test
 
 import (
 	"fmt"
 	"testing"
 
 	fuzz "github.com/google/gofuzz"
+	enginev1 "github.com/prysmaticlabs/prysm/v5/proto/engine/v1"
 	"github.com/prysmaticlabs/prysm/v5/testing/require"
 )
 
 func TestCopyExecutionPayload_Fuzz(t *testing.T) {
-	fuzzCopies(t, &ExecutionPayloadElectra{})
+	fuzzCopies(t, &enginev1.ExecutionPayloadElectra{})
 }
 
-func fuzzCopies[T any, C copier[T]](t *testing.T, obj C) {
+func fuzzCopies[T any, C enginev1.Copier[T]](t *testing.T, obj C) {
 	fuzzer := fuzz.NewWithSeed(0)
 	amount := 1000
 	t.Run(fmt.Sprintf("%T", obj), func(t *testing.T) {
