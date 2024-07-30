6e816f0352cc4a0959b90963cef714847b95e8b4
terence <terence@prysmaticlabs.com>
2024-07-30 16:25:28 -0700

    Ensure epbs state getters & setters check versions (#14276)
    * Ensure EPBS state getters and setters check versions
    
    * Rename to LatestExecutionPayloadHeaderEPBS
    
    * Add minimal beacon state

--- beacon-chain/state/state-native/getters_epbs.go
+++ beacon-chain/state/state-native/getters_epbs.go
@@ -4,48 +4,72 @@ import (
 	"github.com/prysmaticlabs/prysm/v5/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v5/encoding/bytesutil"
 	enginev1 "github.com/prysmaticlabs/prysm/v5/proto/engine/v1"
+	"github.com/prysmaticlabs/prysm/v5/runtime/version"
 )
 
-// ExecutionPayloadHeader retrieves a copy of the execution payload header.
+// LatestExecutionPayloadHeaderEPBS retrieves a copy of the execution payload header from epbs state.
 // It returns an error if the operation is not supported for the beacon state's version.
-func (b *BeaconState) ExecutionPayloadHeader() *enginev1.ExecutionPayloadHeaderEPBS {
+func (b *BeaconState) LatestExecutionPayloadHeaderEPBS() (*enginev1.ExecutionPayloadHeaderEPBS, error) {
+	if b.version < version.EPBS {
+		return nil, errNotSupported("LatestExecutionPayloadHeaderEPBS", b.version)
+	}
 	b.lock.RLock()
 	defer b.lock.RUnlock()
 
-	return b.executionPayloadHeaderVal()
+	return b.executionPayloadHeaderVal(), nil
 }
 
 // IsParentBlockFull checks if the last committed payload header was fulfilled.
 // Returns true if both the beacon block and payload were present.
 // Call this function on a beacon state before processing the execution payload header.
-func (b *BeaconState) IsParentBlockFull() bool {
+// It returns an error if the operation is not supported for the beacon state's version.
+func (b *BeaconState) IsParentBlockFull() (bool, error) {
+	if b.version < version.EPBS {
+		return false, errNotSupported("IsParentBlockFull", b.version)
+	}
+
 	b.lock.RLock()
 	defer b.lock.RUnlock()
 
-	headerBlockHash := bytesutil.ToBytes32(b.executionPayloadHeader.BlockHash)
-	return headerBlockHash == b.latestBlockHash
+	headerBlockHash := bytesutil.ToBytes32(b.latestExecutionPayloadHeaderEPBS.BlockHash)
+	return headerBlockHash == b.latestBlockHash, nil
 }
 
 // LatestBlockHash returns the latest block hash.
-func (b *BeaconState) LatestBlockHash() []byte {
+// It returns an error if the operation is not supported for the beacon state's version.
+func (b *BeaconState) LatestBlockHash() ([]byte, error) {
+	if b.version < version.EPBS {
+		return nil, errNotSupported("LatestBlockHash", b.version)
+	}
+
 	b.lock.RLock()
 	defer b.lock.RUnlock()
 
-	return b.latestBlockHash[:]
+	return b.latestBlockHash[:], nil
 }
 
 // LatestFullSlot returns the slot of the latest full block.
-func (b *BeaconState) LatestFullSlot() primitives.Slot {
+// It returns an error if the operation is not supported for the beacon state's version.
+func (b *BeaconState) LatestFullSlot() (primitives.Slot, error) {
+	if b.version < version.EPBS {
+		return 0, errNotSupported("LatestFullSlot", b.version)
+	}
+
 	b.lock.RLock()
 	defer b.lock.RUnlock()
 
-	return b.latestFullSlot
+	return b.latestFullSlot, nil
 }
 
 // LastWithdrawalsRoot returns the latest withdrawal root.
-func (b *BeaconState) LastWithdrawalsRoot() []byte {
+// It returns an error if the operation is not supported for the beacon state's version.
+func (b *BeaconState) LastWithdrawalsRoot() ([]byte, error) {
+	if b.version < version.EPBS {
+		return nil, errNotSupported("LastWithdrawalsRoot", b.version)
+	}
+
 	b.lock.RLock()
 	defer b.lock.RUnlock()
 
-	return b.lastWithdrawalsRoot[:]
+	return b.lastWithdrawalsRoot[:], nil
 }
