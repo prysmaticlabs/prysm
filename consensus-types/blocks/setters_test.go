390c5691d20fbebb30b582f31b319aba4e6e9d07
Potuz <potuz@prysmaticlabs.com>
2024-07-30 15:53:55 -0300

    Add ePBS stuff to consensus-types: block

---
+++ consensus-types/blocks/setters_test.go
@@ -0,0 +1,70 @@
+package blocks
+
+import (
+	"testing"
+
+	"github.com/prysmaticlabs/go-bitfield"
+	fieldparams "github.com/prysmaticlabs/prysm/v5/config/fieldparams"
+	consensus_types "github.com/prysmaticlabs/prysm/v5/consensus-types"
+	"github.com/prysmaticlabs/prysm/v5/encoding/bytesutil"
+	enginev1 "github.com/prysmaticlabs/prysm/v5/proto/engine/v1"
+	eth "github.com/prysmaticlabs/prysm/v5/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/v5/runtime/version"
+	"github.com/prysmaticlabs/prysm/v5/testing/require"
+)
+
+func Test_EpbsBlock_SetPayloadAttestations(t *testing.T) {
+	b := &SignedBeaconBlock{version: version.Deneb}
+	require.ErrorIs(t, b.SetPayloadAttestations(nil), consensus_types.ErrUnsupportedField)
+
+	b = &SignedBeaconBlock{version: version.EPBS,
+		block: &BeaconBlock{version: version.EPBS,
+			body: &BeaconBlockBody{version: version.EPBS}}}
+	aggregationBits := bitfield.NewBitvector512()
+	aggregationBits.SetBitAt(0, true)
+	payloadAttestation := []*eth.PayloadAttestation{
+		{
+			AggregationBits: aggregationBits,
+			Data: &eth.PayloadAttestationData{
+				BeaconBlockRoot: bytesutil.PadTo([]byte{123}, 32),
+				Slot:            1,
+				PayloadStatus:   2,
+			},
+			Signature: bytesutil.PadTo([]byte("signature"), fieldparams.BLSSignatureLength),
+		},
+		{
+			AggregationBits: aggregationBits,
+			Data: &eth.PayloadAttestationData{
+				BeaconBlockRoot: bytesutil.PadTo([]byte{123}, 32),
+				Slot:            1,
+				PayloadStatus:   3,
+			},
+		},
+	}
+
+	require.NoError(t, b.SetPayloadAttestations(payloadAttestation))
+	require.DeepEqual(t, b.block.body.PayloadAttestations(), payloadAttestation)
+}
+
+func Test_EpbsBlock_SetSignedExecutionPayloadHeader(t *testing.T) {
+	b := &SignedBeaconBlock{version: version.Deneb}
+	require.ErrorIs(t, b.SetSignedExecutionPayloadHeader(nil), consensus_types.ErrUnsupportedField)
+
+	b = &SignedBeaconBlock{version: version.EPBS,
+		block: &BeaconBlock{version: version.EPBS,
+			body: &BeaconBlockBody{version: version.EPBS}}}
+	signedExecutionPayloadHeader := &enginev1.SignedExecutionPayloadHeader{
+		Message: &enginev1.ExecutionPayloadHeaderEPBS{
+			ParentBlockHash:        []byte("parentBlockHash"),
+			ParentBlockRoot:        []byte("parentBlockRoot"),
+			BlockHash:              []byte("blockHash"),
+			BuilderIndex:           1,
+			Slot:                   2,
+			Value:                  3,
+			BlobKzgCommitmentsRoot: []byte("blobKzgCommitmentsRoot"),
+		},
+		Signature: []byte("signature"),
+	}
+	require.NoError(t, b.SetSignedExecutionPayloadHeader(signedExecutionPayloadHeader))
+	require.DeepEqual(t, b.block.body.SignedExecutionPayloadHeader(), signedExecutionPayloadHeader)
+}
