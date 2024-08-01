e301f92c48ef51a80c51ddc664c5799a8e70935c
rupam-04 <rpmdey2004@gmail.com>
2024-08-01 22:07:53 +0530

    calculate field roots upto ``deposits``

--- consensus-types/blocks/proofs.go
+++ consensus-types/blocks/proofs.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/prysmaticlabs/prysm/v5/beacon-chain/state/stateutil"
 	field_params "github.com/prysmaticlabs/prysm/v5/config/fieldparams"
+	"github.com/prysmaticlabs/prysm/v5/encoding/ssz"
 	"github.com/prysmaticlabs/prysm/v5/runtime/version"
 	"go.opencensus.io/trace"
 )
@@ -39,8 +40,11 @@ func ComputeFieldRootsForBlockBody(ctx context.Context, blockBody *BeaconBlockBo
 		return nil, fmt.Errorf("unknown block body version %s", version.String(blockBody.version))
 	}
 
-	var a [field_params.BLSSignatureLength]byte
-	copy(a[:], blockBody.randaoReveal[:])
+	chunks, err := ssz.PackByChunk([][]byte{blockBody.randaoReveal[:]})
+	if err != nil {
+		return nil, errors.Wrap(err, "could not pack randao reveal into chunks")
+	}
+	a, _ := ssz.BitwiseMerkleize(chunks, uint64(len(chunks)), uint64(len(chunks)))
 	fieldRoots[0] = a[:]
 
 	eth1DataRoot, err := stateutil.Eth1Root(blockBody.eth1Data)
@@ -52,4 +56,44 @@ func ComputeFieldRootsForBlockBody(ctx context.Context, blockBody *BeaconBlockBo
 	var b [field_params.RootLength]byte
 	copy(b[:], blockBody.graffiti[:])
 	fieldRoots[2] = b[:]
+
+	proposerSlashingsRoot, err := ssz.SliceRoot(blockBody.proposerSlashings, field_params.SlashingsLength)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not compute proposer slashings merkleization")
+	}
+	fieldRoots[3] = proposerSlashingsRoot[:]
+
+	attesterSlashingsRoot, err := ssz.SliceRoot(blockBody.attesterSlashings, field_params.SlashingsLength)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not compute attester slashings merkleization")
+	}
+	fieldRoots[4] = attesterSlashingsRoot[:]
+
+	if blockBody.version == version.Electra {
+		attesterSlashingsElectraRoot, err := ssz.SliceRoot(blockBody.attesterSlashingsElectra, field_params.SlashingsLength)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not compute attester slashings electra merkleization")
+		}
+		fieldRoots[5] = attesterSlashingsElectraRoot[:]
+	}
+
+	attestationsRoot, err := ssz.SliceRoot(blockBody.attestations, field_params.CurrentEpochAttestationsLength)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not compute attestations merkleization")
+	}
+	fieldRoots[6] = attestationsRoot[:]
+
+	if blockBody.version == version.Electra {
+		attestationsElectraRoot, err := ssz.SliceRoot(blockBody.attestationsElectra, field_params.CurrentEpochAttestationsLength)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not compute attestations electra merkleization")
+		}
+		fieldRoots[7] = attestationsElectraRoot[:]
+	}
+
+	depositsRoot, err := ssz.SliceRoot(blockBody.deposits, field_params.MaxDepositRequestsPerPayload)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not compute deposits merkleization")
+	}
+	fieldRoots[8] = depositsRoot[:]
 }
