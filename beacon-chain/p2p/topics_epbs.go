367791debbb55a5835ff84bd46b02b2fbb2ffc65
JihoonSong <jihoonsong@users.noreply.github.com>
2024-08-06 04:08:51 +0900

    Add `execution_payload` and `payload_attestation_message` topics (#14304)
    * Add `execution_payload` and `payload_attestation_message` topics
    
    * Set `SourcePubkey` to 48 bytes long
    
    * Add randomly populated `PayloadAttestationMessage` object
    
    * Add tests for `execution_payload` and `payload_attestation_message` topics

--- beacon-chain/p2p/topics_epbs.go
+++ beacon-chain/p2p/topics_epbs.go
@@ -1,6 +1,11 @@
 package p2p
 
 const (
-	GossipSignedExecutionPayloadHeader      = "signed_execution_payload_header"
-	SignedExecutionPayloadHeaderTopicFormat = GossipProtocolAndDigest + GossipSignedExecutionPayloadHeader
+	GossipSignedExecutionPayloadHeader   = "signed_execution_payload_header"
+	GossipSignedExecutionPayloadEnvelope = "signed_execution_payload_envelope"
+	GossipPayloadAttestationMessage      = "payload_attestation_message"
+
+	SignedExecutionPayloadHeaderTopicFormat   = GossipProtocolAndDigest + GossipSignedExecutionPayloadHeader
+	SignedExecutionPayloadEnvelopeTopicFormat = GossipProtocolAndDigest + GossipSignedExecutionPayloadEnvelope
+	PayloadAttestationMessageTopicFormat      = GossipProtocolAndDigest + GossipPayloadAttestationMessage
 )
