ed3e42ed3d27def51f6a802979dfcc256af975dc
rkapka <radoslaw.kapka@gmail.com>
2024-08-08 16:55:40 +0200

    feature flag

--- config/features/config.go
+++ config/features/config.go
@@ -48,7 +48,9 @@ type Flags struct {
 	EnableDoppelGanger                  bool // EnableDoppelGanger enables doppelganger protection on startup for the validator.
 	EnableHistoricalSpaceRepresentation bool // EnableHistoricalSpaceRepresentation enables the saving of registry validators in separate buckets to save space
 	EnableBeaconRESTApi                 bool // EnableBeaconRESTApi enables experimental usage of the beacon REST API by the validator when querying a beacon node
-	EnableCommitteeAwarePacking         bool // EnableCommitteeAwarePacking TODO
+	EnableCommitteeAwarePacking         bool // EnableCommitteeAwarePacking changes the attestation packing algorithm to one that is aware of attesting committees.
+	EnableExperimentalAttestationPool   bool // EnableExperimentalAttestationPool enables an experimental attestation pool design.
+
 	// Logging related toggles.
 	DisableGRPCConnectionLogs bool // Disables logging when a new grpc client has connected.
 	EnableFullSSZDataLogging  bool // Enables logging for full ssz data on rejected gossip messages
@@ -259,9 +261,11 @@ func ConfigureBeaconChain(ctx *cli.Context) error {
 		logEnabled(EnableCommitteeAwarePacking)
 		cfg.EnableCommitteeAwarePacking = true
 	}
+	if ctx.IsSet(enableExperimentalAttestationPool.Name) {
+		logEnabled(enableExperimentalAttestationPool)
+		cfg.EnableExperimentalAttestationPool = true
+	}
 
-	cfg.AggregateIntervals = [3]time.Duration{aggregateFirstInterval.Value, aggregateSecondInterval.Value, aggregateThirdInterval.Value}
-	Init(cfg)
 	return nil
 }
 
--- config/features/flags.go
+++ config/features/flags.go
@@ -170,6 +170,10 @@ var (
 		Name:  "enable-committee-aware-packing",
 		Usage: "Changes the attestation packing algorithm to one that is aware of attesting committees.",
 	}
+	enableExperimentalAttestationPool = &cli.BoolFlag{
+		Name:  "enable-experimental-attestation-pool",
+		Usage: "Enables an experimental attestation pool design.",
+	}
 )
 
 // devModeFlags holds list of flags that are set when development mode is on.
@@ -228,6 +232,7 @@ var BeaconChainFlags = append(deprecatedBeaconFlags, append(deprecatedFlags, []c
 	BlobSaveFsync,
 	EnableQUIC,
 	EnableCommitteeAwarePacking,
+	enableExperimentalAttestationPool,
 }...)...)
 
 // E2EBeaconChainFlags contains a list of the beacon chain feature flags to be tested in E2E.
