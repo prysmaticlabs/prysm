8a3e9677731cc68d320d6eea35dbf18f0c8a7d7d
rupam-04 <rpmdey2004@gmail.com>
2024-08-09 02:03:40 +0530

    add test for Phase0 block body

---
+++ consensus-types/blocks/proofs_test.go
@@ -0,0 +1,106 @@
+package blocks
+
+import (
+	"context"
+	"testing"
+
+	eth "github.com/prysmaticlabs/prysm/v5/proto/prysm/v1alpha1"
+	"github.com/prysmaticlabs/prysm/v5/testing/require"
+)
+
+func TestComputeBlockBodyFieldRoots_Phase0(t *testing.T) {
+	blockBodyPhase0 := hydrateBeaconBlockBody()
+	blockBodyPhase0.ProposerSlashings = []*eth.ProposerSlashing{
+		{
+			Header_1: &eth.SignedBeaconBlockHeader{
+				Header: &eth.BeaconBlockHeader{
+					Slot: 1,
+				},
+			},
+			Header_2: &eth.SignedBeaconBlockHeader{
+				Header: &eth.BeaconBlockHeader{
+					Slot: 2,
+				},
+			},
+		},
+	}
+	blockBodyPhase0.AttesterSlashings = []*eth.AttesterSlashing{
+		{
+			Attestation_1: &eth.IndexedAttestation{
+				AttestingIndices: []uint64{1, 2, 3},
+				Data: &eth.AttestationData{
+					Slot: 1,
+				},
+				Signature: make([]byte, 3),
+			},
+			Attestation_2: &eth.IndexedAttestation{
+				AttestingIndices: []uint64{1, 2, 3},
+				Data: &eth.AttestationData{
+					Slot: 1,
+				},
+				Signature: make([]byte, 3),
+			},
+		},
+	}
+	blockBodyPhase0.Attestations = []*eth.Attestation{
+		{
+			AggregationBits: []byte{0b01111100, 0b1},
+			Data: &eth.AttestationData{
+				Slot: 1,
+			},
+			Signature: make([]byte, 3),
+		},
+		{
+			AggregationBits: []byte{0b01111000, 0b1},
+			Data: &eth.AttestationData{
+				Slot: 1,
+			},
+			Signature: make([]byte, 3),
+		},
+	}
+	blockBodyPhase0.Deposits = []*eth.Deposit{
+		{
+			Proof: make([][]byte, 2),
+			Data: &eth.Deposit_Data{
+				PublicKey:             make([]byte, 8),
+				WithdrawalCredentials: make([]byte, 8),
+				Amount:                1,
+				Signature:             make([]byte, 8),
+			},
+		},
+		{
+			Proof: make([][]byte, 2),
+			Data: &eth.Deposit_Data{
+				PublicKey:             make([]byte, 8),
+				WithdrawalCredentials: make([]byte, 8),
+				Amount:                2,
+				Signature:             make([]byte, 8),
+			},
+		},
+	}
+	blockBodyPhase0.VoluntaryExits = []*eth.SignedVoluntaryExit{
+		{
+			Exit: &eth.VoluntaryExit{
+				Epoch:          1,
+				ValidatorIndex: 1,
+			},
+			Signature: make([]byte, 8),
+		},
+		{
+			Exit: &eth.VoluntaryExit{
+				Epoch:          2,
+				ValidatorIndex: 2,
+			},
+			Signature: make([]byte, 8),
+		},
+	}
+	body, err := NewBeaconBlockBody(blockBodyPhase0)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	fieldRoots, err := ComputeBlockBodyFieldRoots(ctx, (blockBodyPhase0))
+	require.NoError(t, err)
+	correctRoots, err := body.HashTreeRoot()
+	require.NoError(t, err)
+	require.Equal(t, fieldRoots, correctRoots)
+}
