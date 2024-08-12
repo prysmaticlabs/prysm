cf029aaadfc80daa2f9750d40b01d5ce4f7cdf72
Nishant Das <nishdas93@gmail.com>
2024-08-12 17:09:28 +0800

    Add Data Column Verification  (#14287)
    * Persist All Changes
    
    * Fix All Tests
    
    * Fix Build
    
    * Fix Build
    
    * Fix Build
    
    * Fix Test Again
    
    * Add missing verification
    
    * Add Test Cases for Data Column Validation
    
    * Fix comments for methods
    
    * Fix comments for methods
    
    * Fix Test
    
    * Manu's Review

--- beacon-chain/verification/error.go
+++ beacon-chain/verification/error.go
@@ -2,8 +2,30 @@ package verification
 
 import "github.com/pkg/errors"
 
-// ErrMissingVerification indicates that the given verification function was never performed on the value.
-var ErrMissingVerification = errors.New("verification was not performed for requirement")
+var (
+	// ErrFromFutureSlot means RequireSlotNotTooEarly failed.
+	ErrFromFutureSlot = errors.New("slot is too far in the future")
+	// ErrSlotNotAfterFinalized means RequireSlotAboveFinalized failed.
+	ErrSlotNotAfterFinalized = errors.New("slot <= finalized checkpoint")
+	// ErrInvalidProposerSignature means RequireValidProposerSignature failed.
+	ErrInvalidProposerSignature = errors.New("proposer signature could not be verified")
+	// ErrSidecarParentNotSeen means RequireSidecarParentSeen failed.
+	ErrSidecarParentNotSeen = errors.New("parent root has not been seen")
+	// ErrSidecarParentInvalid means RequireSidecarParentValid failed.
+	ErrSidecarParentInvalid = errors.New("parent block is not valid")
+	// ErrSlotNotAfterParent means RequireSidecarParentSlotLower failed.
+	ErrSlotNotAfterParent = errors.New("slot <= slot")
+	// ErrSidecarNotFinalizedDescendent means RequireSidecarDescendsFromFinalized failed.
+	ErrSidecarNotFinalizedDescendent = errors.New("parent is not descended from the finalized block")
+	// ErrSidecarInclusionProofInvalid means RequireSidecarInclusionProven failed.
+	ErrSidecarInclusionProofInvalid = errors.New("sidecar inclusion proof verification failed")
+	// ErrSidecarKzgProofInvalid means RequireSidecarKzgProofVerified failed.
+	ErrSidecarKzgProofInvalid = errors.New("sidecar kzg commitment proof verification failed")
+	// ErrSidecarUnexpectedProposer means RequireSidecarProposerExpected failed.
+	ErrSidecarUnexpectedProposer = errors.New("sidecar was not proposed by the expected proposer_index")
+	// ErrMissingVerification indicates that the given verification function was never performed on the value.
+	ErrMissingVerification = errors.New("verification was not performed for requirement")
+)
 
 // VerificationMultiError is a custom error that can be used to access individual verification failures.
 type VerificationMultiError struct {
