a1c85703a90000221cadc10a1dfa3e8cc34599b3
Potuz <potuz@prysmaticlabs.com>
2024-08-16 09:34:32 -0300

    Add hashtree for hashing

---
+++ crypto/hash/hashtree/hashtree.go
@@ -0,0 +1,47 @@
+package hashtree
+
+import (
+	"runtime"
+	"sync"
+
+	"github.com/prysmaticlabs/hashtree"
+)
+
+const minSliceSizeToParallelize = 5000
+
+func hashParallel(inputList [][32]byte, outputList [][32]byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	err := hashtree.Hash(outputList, inputList)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// VectorizedSha256 takes a list of roots and hashes them using CPU
+// specific vector instructions. Depending on host machine's specific
+// hardware configuration, using this routine can lead to a significant
+// performance improvement compared to the default method of hashing
+// lists.
+func VectorizedSha256(inputList [][32]byte) [][32]byte {
+	outputList := make([][32]byte, len(inputList)/2)
+	if len(inputList) < minSliceSizeToParallelize {
+		err := hashtree.Hash(outputList, inputList)
+		if err != nil {
+			panic(err)
+		}
+		return outputList
+	}
+	n := runtime.GOMAXPROCS(0) - 1
+	wg := sync.WaitGroup{}
+	wg.Add(n)
+	groupSize := len(inputList) / (2 * (n + 1))
+	for j := 0; j < n; j++ {
+		go hashParallel(inputList[j*2*groupSize:(j+1)*2*groupSize], outputList[j*groupSize:], &wg)
+	}
+	err := hashtree.Hash(outputList[n*groupSize:], inputList[n*2*groupSize:])
+	if err != nil {
+		panic(err)
+	}
+	wg.Wait()
+	return outputList
+}
