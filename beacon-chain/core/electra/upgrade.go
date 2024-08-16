9e9559df607843ab1cc5d3afa202f92c44ae6642
Jun Song <87601811+syjn99@users.noreply.github.com>
2024-08-17 04:46:00 +0900

    docs: Add consolidation_requests_root on comment (#14335)
    Co-authored-by: james-prysm <90280386+james-prysm@users.noreply.github.com>
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- beacon-chain/core/electra/upgrade.go
+++ beacon-chain/core/electra/upgrade.go
@@ -40,6 +40,7 @@ import (
 //	    excess_blob_gas=pre.latest_execution_payload_header.excess_blob_gas,
 //	    deposit_requests_root=Root(),  # [New in Electra:EIP6110]
 //	    withdrawal_requests_root=Root(),  # [New in Electra:EIP7002],
+//	    consolidation_requests_root=Root(),  # [New in Electra:EIP7251]
 //	)
 //
 //	exit_epochs = [v.exit_epoch for v in pre.validators if v.exit_epoch != FAR_FUTURE_EPOCH]
