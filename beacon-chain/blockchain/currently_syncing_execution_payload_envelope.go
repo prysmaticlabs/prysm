3b34ab0fb22a093c75e174fbcd93cca7ce44896e
Potuz <potuz@prysmaticlabs.com>
2024-08-22 15:54:18 -0300

    Refactor currentlySyncingPayload cache (#14350)

--- beacon-chain/blockchain/currently_syncing_execution_payload_envelope.go
+++ beacon-chain/blockchain/currently_syncing_execution_payload_envelope.go
@@ -1,16 +1,25 @@
 package blockchain
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/prysmaticlabs/prysm/v5/consensus-types/interfaces"
+	"github.com/prysmaticlabs/prysm/v5/consensus-types/primitives"
+)
 
 type currentlySyncingPayload struct {
 	sync.Mutex
-	roots map[[32]byte]struct{}
+	roots map[[32]byte]primitives.PTCStatus
 }
 
-func (b *currentlySyncingPayload) set(root [32]byte) {
+func (b *currentlySyncingPayload) set(envelope interfaces.ROExecutionPayloadEnvelope) {
 	b.Lock()
 	defer b.Unlock()
-	b.roots[root] = struct{}{}
+	if envelope.PayloadWithheld() {
+		b.roots[envelope.BeaconBlockRoot()] = primitives.PAYLOAD_WITHHELD
+	} else {
+		b.roots[envelope.BeaconBlockRoot()] = primitives.PAYLOAD_PRESENT
+	}
 }
 
 func (b *currentlySyncingPayload) unset(root [32]byte) {
@@ -19,9 +28,9 @@ func (b *currentlySyncingPayload) unset(root [32]byte) {
 	delete(b.roots, root)
 }
 
-func (b *currentlySyncingPayload) isSyncing(root [32]byte) bool {
+func (b *currentlySyncingPayload) isSyncing(root [32]byte) (status primitives.PTCStatus, isSyncing bool) {
 	b.Lock()
 	defer b.Unlock()
-	_, ok := b.roots[root]
-	return ok
+	status, isSyncing = b.roots[root]
+	return
 }
