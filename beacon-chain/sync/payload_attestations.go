aaa60012e1cea2cb0d5c9643da0ee97c93e913d8
Potuz <potuz@prysmaticlabs.com>
2024-08-29 09:38:18 -0300

    Receive ptc message (#14394)
    * Handle incoming ptc attestation messages in the chain package
    
    * fix double import
    
    * remove unused error

--- beacon-chain/sync/payload_attestations.go
+++ beacon-chain/sync/payload_attestations.go
@@ -2,15 +2,12 @@ package sync
 
 import (
 	"context"
-	"slices"
 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/pkg/errors"
-	"github.com/prysmaticlabs/prysm/v5/beacon-chain/core/helpers"
 	"github.com/prysmaticlabs/prysm/v5/beacon-chain/verification"
 	payloadattestation "github.com/prysmaticlabs/prysm/v5/consensus-types/epbs/payload-attestation"
-	"github.com/prysmaticlabs/prysm/v5/consensus-types/primitives"
 	"github.com/prysmaticlabs/prysm/v5/monitoring/tracing"
 	"github.com/prysmaticlabs/prysm/v5/monitoring/tracing/trace"
 	eth "github.com/prysmaticlabs/prysm/v5/proto/prysm/v1alpha1"
@@ -18,7 +15,6 @@ import (
 )
 
 var (
-	errInvalidValidatorIndex         = errors.New("invalid validator index")
 	errAlreadySeenPayloadAttestation = errors.New("payload attestation already seen for validator index")
 )
 
@@ -91,25 +87,5 @@ func (s *Service) payloadAttestationSubscriber(ctx context.Context, msg proto.Me
 	if !ok {
 		return errWrongMessage
 	}
-	if err := helpers.ValidateNilPayloadAttestationMessage(a); err != nil {
-		return err
-	}
-	root := [32]byte(a.Data.BeaconBlockRoot)
-	st, err := s.cfg.chain.HeadState(ctx)
-	if err != nil {
-		return err
-	}
-	ptc, err := helpers.GetPayloadTimelinessCommittee(ctx, st, a.Data.Slot)
-	if err != nil {
-		return err
-	}
-	idx := slices.Index(ptc, a.ValidatorIndex)
-	if idx == -1 {
-		return errInvalidValidatorIndex
-	}
-	if s.payloadAttestationCache.Seen(root, uint64(primitives.ValidatorIndex(idx))) {
-		return nil
-	}
-
-	return s.payloadAttestationCache.Add(a, uint64(idx))
+	return s.cfg.chain.ReceivePayloadAttestationMessage(ctx, a)
 }
