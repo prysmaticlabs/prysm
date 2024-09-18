edd736fb9f1d136acf1e0a385217c99962344251
terence <terence@prysmaticlabs.com>
2024-09-18 06:59:47 -0700

    Prysm rpc: submit signed execution payload header (#14441)

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer_epbs_test.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer_epbs_test.go
@@ -12,6 +12,7 @@ import (
 	p2ptest "github.com/prysmaticlabs/prysm/v5/beacon-chain/p2p/testing"
 	"github.com/prysmaticlabs/prysm/v5/beacon-chain/state/stategen"
 	"github.com/prysmaticlabs/prysm/v5/consensus-types/blocks"
+	"github.com/prysmaticlabs/prysm/v5/consensus-types/primitives"
 	enginev1 "github.com/prysmaticlabs/prysm/v5/proto/engine/v1"
 	"github.com/prysmaticlabs/prysm/v5/testing/require"
 	"github.com/prysmaticlabs/prysm/v5/testing/util"
@@ -44,6 +45,34 @@ func TestServer_SubmitSignedExecutionPayloadEnvelope(t *testing.T) {
 		}
 		_, err := s.SubmitSignedExecutionPayloadEnvelope(context.Background(), env)
 		require.ErrorContains(t, "failed to receive execution payload envelope: receive failed", err)
+	})
+}
+
+func TestServer_SubmitSignedExecutionPayloadHeader(t *testing.T) {
+	st, _ := util.DeterministicGenesisStateEpbs(t, 1)
+	h := &enginev1.SignedExecutionPayloadHeader{
+		Message: &enginev1.ExecutionPayloadHeaderEPBS{
+			Slot: 1,
+		},
+	}
+	slot := primitives.Slot(1)
+	server := &Server{
+		TimeFetcher: &mockChain.ChainService{Slot: &slot},
+		HeadFetcher: &mockChain.ChainService{State: st},
+		P2P:         p2ptest.NewTestP2P(t),
+	}
+
+	t.Run("Happy case", func(t *testing.T) {
+		h.Message.BuilderIndex = 1
+		_, err := server.SubmitSignedExecutionPayloadHeader(context.Background(), h)
+		require.NoError(t, err)
+		require.DeepEqual(t, server.signedExecutionPayloadHeader, h)
+	})
+
+	t.Run("Incorrect slot", func(t *testing.T) {
+		h.Message.Slot = 3
+		_, err := server.SubmitSignedExecutionPayloadHeader(context.Background(), h)
+		require.ErrorContains(t, "invalid slot: current slot 1, got 3", err)
 	})
 }
 
