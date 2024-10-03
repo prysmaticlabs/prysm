997fec52df7563a911c0c60d51a4f21588477593
rupam-04 <rpmdey2004@gmail.com>
2024-10-04 00:20:33 +0530

    remove unnecessary comments

--- beacon-chain/rpc/eth/light-client/helpers.go
+++ beacon-chain/rpc/eth/light-client/helpers.go
@@ -36,23 +36,6 @@ func createLightClientBootstrap(ctx context.Context, state state.BeaconState, bl
 	return nil, fmt.Errorf("unsupported block version %s", version.String(blk.Version()))
 }
 
-// createLightClientBootstrapAltair - implements https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/full-node.md#create_light_client_bootstrap
-//
-//	def create_light_client_bootstrap(state: BeaconState,
-//	block: SignedBeaconBlock) -> LightClientBootstrap:
-//	assert compute_epoch_at_slot(state.slot) >= ALTAIR_FORK_EPOCH
-//
-//	assert state.slot == state.latest_block_header.slot
-//	header = state.latest_block_header.copy()
-//	header.state_root = hash_tree_root(state)
-//	assert hash_tree_root(header) == hash_tree_root(block.message)
-//
-//	return LightClientBootstrap(
-//	header=block_to_light_client_header(block),
-//	current_sync_committee=state.current_sync_committee,
-//	current_sync_committee_branch=CurrentSyncCommitteeBranch(
-//	compute_merkle_proof(state, current_sync_committee_gindex_at_slot(state.slot))),
-//	)
 func createLightClientBootstrapAltair(ctx context.Context, state state.BeaconState, block interfaces.ReadOnlySignedBeaconBlock) (*structs.LightClientBootstrap, error) {
 	// assert compute_epoch_at_slot(state.slot) >= ALTAIR_FORK_EPOCH
 	if slots.ToEpoch(state.Slot()) < params.BeaconConfig().AltairForkEpoch {
