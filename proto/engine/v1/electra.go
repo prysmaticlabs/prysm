575b52813160db85844ca9a3c66eff27caf252d4
james-prysm <james@prysmaticlabs.com>
2024-10-11 13:39:16 -0500

    code cleanup

--- proto/engine/v1/electra.go
+++ proto/engine/v1/electra.go
@@ -1,6 +1,8 @@
 package enginev1
 
 import (
+	"fmt"
+
 	"github.com/pkg/errors"
 )
 
@@ -24,52 +26,25 @@ func (eee *ExecutionEnvelopeElectra) GetDecodedExecutionRequests() (*ExecutionRe
 	}
 
 	// deposit requests
-	drBytes := eee.ExecutionRequests[0]
-	if len(drBytes)%drSize != 0 {
-		return nil, errors.New("invalid ssz length for deposit requests")
+	drs, err := unmarshalItems(eee.ExecutionRequests[0], drSize, func() *DepositRequest { return &DepositRequest{} })
+	if err != nil {
+		return nil, err
 	}
-	numDepositRequests := len(drBytes) / drSize
-	requests.Deposits = make([]*DepositRequest, numDepositRequests)
-	for i := range requests.Deposits {
-		requestBytes := drBytes[i*drSize : (i+1)*drSize]
-		dr := &DepositRequest{}
-		if err := dr.UnmarshalSSZ(requestBytes); err != nil {
-			return nil, errors.Wrap(err, "failed to unmarshal deposit request")
-		}
-		requests.Deposits[i] = dr
-	}
+	requests.Deposits = drs
 
 	// withdrawal requests
-	wrBytes := eee.ExecutionRequests[1]
-	if len(wrBytes)%wrSize != 0 {
-		return nil, errors.New("invalid ssz length for withdrawal requests")
+	wrs, err := unmarshalItems(eee.ExecutionRequests[1], wrSize, func() *WithdrawalRequest { return &WithdrawalRequest{} })
+	if err != nil {
+		return nil, err
 	}
-	numWithdrawalRequests := len(wrBytes) / wrSize
-	requests.Withdrawals = make([]*WithdrawalRequest, numWithdrawalRequests)
-	for i := range requests.Withdrawals {
-		requestBytes := wrBytes[i*wrSize : (i+1)*wrSize]
-		wr := &WithdrawalRequest{}
-		if err := wr.UnmarshalSSZ(requestBytes); err != nil {
-			return nil, errors.Wrap(err, "failed to unmarshal withdrawal request")
-		}
-		requests.Withdrawals[i] = wr
-	}
+	requests.Withdrawals = wrs
 
 	// consolidation requests
-	crBytes := eee.ExecutionRequests[2]
-	if len(crBytes)%crSize != 0 {
-		return nil, errors.New("invalid ssz length for consolidation requests")
+	crs, err := unmarshalItems(eee.ExecutionRequests[2], crSize, func() *ConsolidationRequest { return &ConsolidationRequest{} })
+	if err != nil {
+		return nil, err
 	}
-	numConsolidationRequests := len(crBytes) / crSize
-	requests.Consolidations = make([]*ConsolidationRequest, numConsolidationRequests)
-	for i := range requests.Consolidations {
-		requestBytes := crBytes[i*wrSize : (i+1)*wrSize]
-		cr := &ConsolidationRequest{}
-		if err := cr.UnmarshalSSZ(requestBytes); err != nil {
-			return nil, errors.Wrap(err, "failed to unmarshal consolidation request")
-		}
-		requests.Consolidations[i] = cr
-	}
+	requests.Consolidations = crs
 
 	return requests, nil
 }
@@ -79,31 +54,65 @@ func EncodeExecutionRequests(requests *ExecutionRequests) ([][]byte, error) {
 		return nil, errors.New("invalid execution requests")
 	}
 
-	drBytes := make([]byte, len(requests.Deposits)*drSize)
-	for i, deposit := range requests.Deposits {
-		dr, err := deposit.MarshalSSZ()
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to marshal deposit request")
-		}
-		copy(drBytes[i:i+drSize], dr)
+	drBytes, err := marshalItems(requests.Deposits)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal deposit requests")
 	}
 
-	wrBytes := make([]byte, len(requests.Withdrawals)*wrSize)
-	for i, withdrawal := range requests.Withdrawals {
-		wr, err := withdrawal.MarshalSSZ()
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to marshal withdrawal request")
-		}
-		copy(wrBytes[i*wrSize:i+wrSize], wr)
+	wrBytes, err := marshalItems(requests.Withdrawals)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal withdrawal requests")
 	}
 
-	crBytes := make([]byte, len(requests.Consolidations)*crSize)
-	for i, consolidation := range requests.Consolidations {
-		cr, err := consolidation.MarshalSSZ()
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to marshal consolidation request")
-		}
-		copy(crBytes[i*crSize:i+crSize], cr)
+	crBytes, err := marshalItems(requests.Consolidations)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal consolidation requests")
 	}
 	return [][]byte{drBytes, wrBytes, crBytes}, nil
+}
+
+type sszUnmarshaler interface {
+	UnmarshalSSZ([]byte) error
+}
+
+type sszMarshaler interface {
+	MarshalSSZ() ([]byte, error)
+}
+
+func marshalItems[T sszMarshaler](items []T) ([]byte, error) {
+	var totalSize int
+	marshaledItems := make([][]byte, len(items))
+	for i, item := range items {
+		bytes, err := item.MarshalSSZ()
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal item at index %d: %w", i, err)
+		}
+		marshaledItems[i] = bytes
+		totalSize += len(bytes)
+	}
+
+	result := make([]byte, totalSize)
+
+	for i, bytes := range marshaledItems {
+		copy(result[i*len(bytes):(i+1)*len(bytes)], bytes)
+	}
+	return result, nil
+}
+
+// Generic function to unmarshal items
+func unmarshalItems[T sszUnmarshaler](data []byte, itemSize int, newItem func() T) ([]T, error) {
+	if len(data)%itemSize != 0 {
+		return nil, fmt.Errorf("invalid data length: data size (%d) is not a multiple of item size (%d)", len(data), itemSize)
+	}
+	numItems := len(data) / itemSize
+	items := make([]T, numItems)
+	for i := range items {
+		itemBytes := data[i*itemSize : (i+1)*itemSize]
+		item := newItem()
+		if err := item.UnmarshalSSZ(itemBytes); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal item at index %d: %w", i, err)
+		}
+		items[i] = item
+	}
+	return items, nil
 }
